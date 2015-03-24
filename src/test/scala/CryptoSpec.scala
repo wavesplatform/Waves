59aedd46545d96ccf3c1e9633081a19a7d74e354
kushti <kushtech@yahoo.com>
2015-03-23 23:22:06 -0700

    CryptoSpec

---
+++ src/test/scala/CryptoSpec.scala
@@ -0,0 +1,19 @@
+package scorex.test
+
+import org.scalatest.FunSuite
+import scorex.account.PrivateKeyAccount
+import scorex.crypto.Crypto
+
+import scala.util.Random
+
+class CryptoSpec extends FunSuite {
+
+  test("sign then verify") {
+    val acc = new PrivateKeyAccount(Random.nextString(20).getBytes)
+
+    val data = Random.nextString(30).getBytes
+
+    val sig = Crypto.sign(acc, data)
+    assert(Crypto.verify(sig, data, acc.publicKey))
+  }
+}
