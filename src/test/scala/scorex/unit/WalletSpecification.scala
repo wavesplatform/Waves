ae074e0d88d01a1b806b9319f57d95f05bc86832
kushti <kushtech@yahoo.com>
2015-04-10 15:51:47 -0700

    walletSize  everywhere in the test instead of a magic number

--- src/test/scala/scorex/unit/WalletSpecification.scala
+++ src/test/scala/scorex/unit/WalletSpecification.scala
@@ -7,26 +7,27 @@ import scorex.wallet.Wallet
 import scala.util.Random
 
 class WalletSpecification extends FunSuite {
-  val wf = new java.io.File(s"/tmp/wallet${Random.nextLong()}.dat")
+  private val wf = new java.io.File(s"/tmp/wallet${Random.nextLong()}.dat")
+  private val walletSize = 10
 
   test("double creation"){
-    Wallet.create(Base58.decode("FQgbSAm6swGbtqA3NE8PttijPhT4N3Ufh4bHFAkyVnQz"), "cookies", 10, wf)
+    Wallet.create(Base58.decode("FQgbSAm6swGbtqA3NE8PttijPhT4N3Ufh4bHFAkyVnQz"), "cookies", walletSize, wf)
     Wallet.close()
-    Wallet.create(Base58.decode("FQgbSAm6swGbtqA3NE8PttijPhT4N3Ufh4bHFAkyVnQz"), "cookies", 10, wf)
+    Wallet.create(Base58.decode("FQgbSAm6swGbtqA3NE8PttijPhT4N3Ufh4bHFAkyVnQz"), "cookies", walletSize, wf)
     assert(Wallet.privateKeyAccounts().head.address != null)
   }
 
   test("wallet deletion") {
-    val size0 = 10
-    Wallet.create(Base58.decode("FQgbSAm6swGbtqA3NE8PttijPhT4N3Ufh4bHFAkyVnQz"), "cookies", 10, wf)
-    assert(Wallet.privateKeyAccounts().size == size0)
+
+    Wallet.create(Base58.decode("FQgbSAm6swGbtqA3NE8PttijPhT4N3Ufh4bHFAkyVnQz"), "cookies", walletSize, wf)
+    assert(Wallet.privateKeyAccounts().size == walletSize)
 
     val head = Wallet.privateKeyAccounts().head
     Wallet.deleteAccount(head)
-    assert(Wallet.privateKeyAccounts().size == size0 - 1)
+    assert(Wallet.privateKeyAccounts().size == walletSize - 1)
 
     Wallet.deleteAccount(Wallet.privateKeyAccounts().head)
-    assert(Wallet.privateKeyAccounts().size == size0 - 2)
+    assert(Wallet.privateKeyAccounts().size == walletSize - 2)
 
     println(Wallet.privateKeyAccounts().mkString(" : "))
 
