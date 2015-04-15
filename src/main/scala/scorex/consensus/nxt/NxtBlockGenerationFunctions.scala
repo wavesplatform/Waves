0f4fdd67ec45c9bbe740a8343d63b977ebbb0414
kushti <kushtech@yahoo.com>
2015-04-15 08:19:14 +0300

    hit fixed in NxtBlockGenerationFunctions

--- src/main/scala/scorex/consensus/nxt/NxtBlockGenerationFunctions.scala
+++ src/main/scala/scorex/consensus/nxt/NxtBlockGenerationFunctions.scala
@@ -4,17 +4,20 @@ import ntp.NTP
 import scorex.account.PrivateKeyAccount
 import scorex.block.{Block, BlockStub}
 import scorex.consensus.BlockGenerationFunctions
-import scorex.crypto.Crypto
+import scorex.crypto.{Base58, Crypto}
+
+import scala.util.Random
 
 object NxtBlockGenerationFunctions extends BlockGenerationFunctions {
   private val AvgFrequency = 60 //the algo's goal is 1 block per minute in average
 
   override protected def generateNextBlock(account: PrivateKeyAccount, lastBlock: Block): Option[BlockStub] = ???
 
-  private def hit(lastBlockData: NxtBlockGenerationData, generator: PrivateKeyAccount): Long = {
-    val hash = Crypto.sha256(lastBlockData.generatorSignature ++ generator.publicKey)
-    BigInt(hash.take(8)).toLong
-  }
+  private def generatorSignature(lastBlockGeneratorSignature:Array[Byte], generator: PrivateKeyAccount) =
+    Crypto.sha256(lastBlockGeneratorSignature ++ generator.publicKey)
+
+  private def hit(lastBlockData: NxtBlockGenerationData, generator: PrivateKeyAccount): BigInt =
+    BigInt(1, generatorSignature(lastBlockData.generatorSignature, generator).take(8))
 
   private def baseTarget(lastBlockData: NxtBlockGenerationData, lastBlockTimestamp: Long): Long = {
     val eta = (NTP.getTime - lastBlockTimestamp) / 1000 //in seconds
@@ -23,11 +26,28 @@ object NxtBlockGenerationFunctions extends BlockGenerationFunctions {
     bound(t, 1, Long.MaxValue).toLong
   }
 
-  private def target(lastBlockData: NxtBlockGenerationData, lastBlockTimestamp: Long, generator: PrivateKeyAccount): Long = {
+  private def target(lastBlockData: NxtBlockGenerationData, lastBlockTimestamp: Long, generator: PrivateKeyAccount): BigInt = {
     val eta = (NTP.getTime - lastBlockTimestamp) / 1000 //in seconds
-    bound((lastBlockData.baseTarget * eta * generator.generatingBalance).toBigInt(), 1, Long.MaxValue).toLong
+    val effBalance:BigDecimal = 10000000// generator.generatingBalance
+    (lastBlockData.baseTarget * eta * effBalance).toBigInt()
   }
 
   private def bound(value: BigInt, min: BigInt, max: BigInt): BigInt =
     if (value < min) min else if (value > max) max else value
+
+  /*
+  todo: remove testing code
+  def main(args:Array[String]): Unit ={
+    val seed = Base58.decode("FQgbSAm6swGbtqA3NE8PttijPhT4N3Ufh4bHFAkyVnQz")
+    val acc = new PrivateKeyAccount(seed)
+    val ts = System.currentTimeMillis() - 10000
+
+    (1 to 1000).flatMap{_ =>
+        val data = new NxtBlockGenerationData(307445734, Crypto.sha256(Random.nextString(5).getBytes))
+        val h = hit(data, acc)
+        val t = target(data, ts, acc)
+        val bt = baseTarget(data, ts)
+        if(h<t) Some((h,t,bt)) else None
+    }.foreach{case (h,t,bt) => println(s"hit: $h target: $t, next base: $bt generation: ${h<t}`")}
+  } */
 }
