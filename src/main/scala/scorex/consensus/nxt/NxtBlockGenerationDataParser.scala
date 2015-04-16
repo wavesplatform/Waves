f82b920f64ce6d4d36588ea2c2c1218c3bec59f2
kushti <kushtech@yahoo.com>
2015-04-16 12:49:52 +0300

    NxtBlockGenerationDataParser.parse implemented, input length check added to QoraBlockGenerationDataParser

--- src/main/scala/scorex/consensus/nxt/NxtBlockGenerationDataParser.scala
+++ src/main/scala/scorex/consensus/nxt/NxtBlockGenerationDataParser.scala
@@ -1,7 +1,7 @@
 package scorex.consensus.nxt
 
+import com.google.common.primitives.Longs
 import scorex.consensus.BlockGenerationDataParser
-
 
 object NxtBlockGenerationDataParser extends BlockGenerationDataParser[NxtBlockGenerationData]{
   val BASE_TARGET_LENGTH = 8
@@ -9,5 +9,11 @@ object NxtBlockGenerationDataParser extends BlockGenerationDataParser[NxtBlockGe
 
   val GENERATION_DATA_LENGTH:Int = GENERATOR_SIGNATURE_LENGTH + BASE_TARGET_LENGTH
 
-  override def parse(bytes: Array[Byte]): NxtBlockGenerationData = ???
+  override def parse(bytes: Array[Byte]): NxtBlockGenerationData = {
+    require(bytes.length == GENERATION_DATA_LENGTH)
+
+    val baseTarget = Longs.fromByteArray(bytes.take(BASE_TARGET_LENGTH))
+    val generatorSignature = bytes.drop(BASE_TARGET_LENGTH)
+    new NxtBlockGenerationData(baseTarget, generatorSignature)
+  }
 }
--- src/main/scala/scorex/consensus/qora/QoraBlockGenerationDataParser.scala
+++ src/main/scala/scorex/consensus/qora/QoraBlockGenerationDataParser.scala
@@ -10,6 +10,8 @@ object QoraBlockGenerationDataParser extends BlockGenerationDataParser[QoraBlock
   override val GENERATION_DATA_LENGTH = GENERATING_BALANCE_LENGTH + GENERATOR_SIGNATURE_LENGTH
 
   def parse(bytes:Array[Byte]):QoraBlockGenerationData = {
+    require(bytes.length == GENERATION_DATA_LENGTH)
+
     val generatingBalance = Longs.fromByteArray(bytes.take(GENERATING_BALANCE_LENGTH))
     val generatorSignature = bytes.drop(GENERATING_BALANCE_LENGTH)
     new QoraBlockGenerationData(generatingBalance, generatorSignature)
