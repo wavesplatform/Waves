c7c24acb3301a1d9d77a6381b6476d891dd4eb96
kushti <kushtech@yahoo.com>
2015-05-21 20:41:07 +0300

    version updated

--- src/main/scala/settings/Constants.scala
+++ src/main/scala/settings/Constants.scala
@@ -10,7 +10,7 @@ object Constants {
 
   val Product = "Scorex"
   val Release = "Lagonaki"
-  val Version = (0, 0, 2)
+  val Version = (0, 0, 3)
 
   val AgentName = s"$Product - $Release v. ${Version._1}.${Version._2}.${Version._3}"
 
