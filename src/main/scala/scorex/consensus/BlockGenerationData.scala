b2aec3132dc5ec589f12a267da422fa9656dc74d
kushti <kushtech@yahoo.com>
2015-06-05 15:33:24 +0300

    crucial todo for isSignatureValid

--- src/main/scala/scorex/consensus/BlockGenerationData.scala
+++ src/main/scala/scorex/consensus/BlockGenerationData.scala
@@ -14,6 +14,11 @@ trait BlockGenerationData {
 
   def isValid(block: Block): Boolean
 
+  /* todo: remove from both Qora- & Nxt- like algos & implement non-buggy signature check!
+   It seems in Qora, timestamp(& maybe some other fields) are not signed,
+   only prev block generator, generating balance, current block generator & txs.
+   Should be fixed!
+   */
   def isSignatureValid(block: Block): Boolean
 
   //block score value, where blockchain quality measure is the sum of block scores.
