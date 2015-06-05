d0cc9b5f234efac8bffafbd21c22d44a2a5407ef
kushti <kushtech@yahoo.com>
2015-06-05 15:29:54 +0300

    isSignatureValid is implemented for Nxt-like algo(tests are needed to be fixed)

--- src/main/scala/scorex/consensus/nxt/NxtBlockGenerationData.scala
+++ src/main/scala/scorex/consensus/nxt/NxtBlockGenerationData.scala
@@ -4,7 +4,7 @@ import com.google.common.primitives.{Bytes, Longs}
 import play.api.libs.json.{JsObject, Json}
 import scorex.block.{Block, NxtGenesisBlockGenerationData}
 import scorex.consensus.BlockGenerationData
-import scorex.crypto.Base58
+import scorex.crypto.{Crypto, Base58}
 
 case class NxtBlockGenerationData(baseTarget: Long, generatorSignature: Array[Byte])
   extends BlockGenerationData {
@@ -24,8 +24,13 @@ case class NxtBlockGenerationData(baseTarget: Long, generatorSignature: Array[By
 
   override def signature(): Array[Byte] = generatorSignature.ensuring(_.length == NxtBlockGenerationDataParser.GENERATOR_SIGNATURE_LENGTH)
 
-  //todo: implement signature check!
-  override def isSignatureValid(block: Block): Boolean = true
+  override def isSignatureValid(block: Block): Boolean = {
+    val prevGs = block.parent()
+      .map(_.generationData.asInstanceOf[NxtBlockGenerationData].generatorSignature)
+      .getOrElse(NxtGenesisBlockGenerationData.generationData.generatorSignature)
+    val preImage = prevGs ++ block.generator.publicKey
+    signature().sameElements(Crypto.sha256(preImage))
+  }
 
   //todo: implement validity check!
   override def isValid(block: Block): Boolean = true
