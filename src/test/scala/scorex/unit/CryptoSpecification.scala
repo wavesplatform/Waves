572b3b367e7b86d9fd67d8ccf739582c3ea34dad
Dmitry Meshkov <canoneer@mail.ru>
2015-08-04 11:46:50 +0300

    Encode and decode test

--- src/test/scala/scorex/unit/CryptoSpecification.scala
+++ src/test/scala/scorex/unit/CryptoSpecification.scala
@@ -6,10 +6,17 @@ import scorex.crypto.{Base58, Crypto}
 
 import scala.util.Random
 
-class CryptoSpecification extends FunSuite with Matchers{
-  test("base58 roundtrip"){
+class CryptoSpecification extends FunSuite with Matchers {
+  test("base58 roundtrip") {
     val b58 = "1AGNa15ZQXAZUgFiqJ2i7Z2DPU2J6hW62i"
     Base58.encode(Base58.decode(b58).get) shouldBe b58
+  }
+
+  test("base58 encode and decode") {
+    val data = Random.nextString(50).getBytes
+    val encoded = Base58.encode(data)
+    val restored = Base58.decode(encoded).get
+    restored shouldBe data
   }
 
   test("sign then verify") {
