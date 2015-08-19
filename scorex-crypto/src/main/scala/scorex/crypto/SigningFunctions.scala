7a52b13a195f348f5f61254e50760d852e263c7e
kushti <kushtech@yahoo.com>
2015-08-19 19:11:26 +0300

    SigningFunctions.scala / HashFunctions.scala split

--- scorex-crypto/src/main/scala/scorex/crypto/SigningFunctions.scala
+++ scorex-crypto/src/main/scala/scorex/crypto/SigningFunctions.scala
@@ -1,27 +1,8 @@
 package scorex.crypto
 
-import java.security.MessageDigest
-
 import scorex.account.PrivateKeyAccount
 
 import scala.util.Try
-
-
-trait HashFunctions {
-  def doubleHash(input: Array[Byte]): Array[Byte]
-
-  def hash(input: Array[Byte]): Array[Byte]
-}
-
-/**
- * Hashing functions implementation with sha256 impl from Java SDK
- */
-object HashFunctionsImpl extends HashFunctions {
-  override def doubleHash(input: Array[Byte]) = hash(hash(input))
-
-  override def hash(input: Array[Byte]) = MessageDigest.getInstance("SHA-256").digest(input)
-}
-
 
 trait SigningFunctions {
   type PrivateKey = Array[Byte]
