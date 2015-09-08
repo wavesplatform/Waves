8c1776bf8fa5c1fe7b85ac6a005e0a63e35f3ab3
kushti <kushtech@yahoo.com>
2015-09-08 11:09:31 +0300

    BlockchainStorageSpecification fix

--- src/test/scala/scorex/unit/BlockchainStorageSpecification.scala
+++ src/test/scala/scorex/unit/BlockchainStorageSpecification.scala
@@ -1,12 +1,27 @@
 package scorex.unit
 
-import org.scalatest.FunSuite
+import org.scalatest.{Matchers, FunSuite}
+import play.api.libs.json.{Json, JsObject}
+import scorex.block.Block
+import scorex.consensus.nxt.NxtLikeConsensusModule
+import scorex.transaction.state.database.blockchain.StoredBlockchain
+import scorex.transaction.{SimpleTransactionModule, TransactionSettings}
 
-class BlockchainStorageSpecification extends FunSuite {
+
+class BlockchainStorageSpecification extends FunSuite with Matchers {
+
+  implicit object TestTransactionLayerSettings extends TransactionSettings{
+    override val settingsJSON: JsObject = Json.obj()
+  }
+
+  implicit val consensusModule = new NxtLikeConsensusModule
+  implicit val transactionModule = new SimpleTransactionModule()
+
+  val blockchainStorage = new StoredBlockchain(None)
+
+
   test("genesis block save & find") {
-    /*   todo: fix tests
-    assert(LagonakiApplication.blockchainStorage.appendBlock(NxtGenesisBlock)
-      .heightOf(NxtGenesisBlock.signature).get == 1)
-      */
+    blockchainStorage.appendBlock(Block.genesis())
+    blockchainStorage.height() should be (1)
   }
 }
