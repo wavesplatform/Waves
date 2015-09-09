58f2acc2cceaea2d5f20b718711f9123722cb47d
kushti <kushtech@yahoo.com>
2015-09-09 09:28:28 +0300

    TestingCommons

--- src/test/scala/scorex/unit/BlockchainStorageSpecification.scala
+++ src/test/scala/scorex/unit/BlockchainStorageSpecification.scala
@@ -2,17 +2,14 @@ package scorex.unit
 
 import org.scalatest.{Matchers, FunSuite}
 import play.api.libs.json.{Json, JsObject}
+import scorex.TestingCommons
 import scorex.block.Block
 import scorex.consensus.nxt.NxtLikeConsensusModule
 import scorex.transaction.state.database.blockchain.StoredBlockchain
 import scorex.transaction.{SimpleTransactionModule, TransactionSettings}
 
 
-class BlockchainStorageSpecification extends FunSuite with Matchers {
-
-  implicit object TestTransactionLayerSettings extends TransactionSettings{
-    override val settingsJSON: JsObject = Json.obj()
-  }
+class BlockchainStorageSpecification extends FunSuite with Matchers with TestingCommons {
 
   implicit val consensusModule = new NxtLikeConsensusModule
   implicit val transactionModule = new SimpleTransactionModule()
