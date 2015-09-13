98f2f3142ec16a2382ca8471ba91ed628ba065bc
kushti <kushtech@yahoo.com>
2015-09-14 00:08:19 +0300

    test("Qora block with txs bytes/parse roundtrip")

--- src/test/scala/scorex/unit/BlockSpecification.scala
+++ src/test/scala/scorex/unit/BlockSpecification.scala
@@ -5,6 +5,7 @@ import scorex.TestingCommons
 import scorex.account.PrivateKeyAccount
 import scorex.block.Block
 import scorex.consensus.nxt.{NxtLikeConsensusBlockData, NxtLikeConsensusModule}
+import scorex.consensus.qora.{QoraLikeConsensusBlockData, QoraLikeConsensusModule}
 import scorex.transaction.{Transaction, PaymentTransaction, SimpleTransactionModule}
 
 import scala.util.Random
@@ -45,6 +46,40 @@ class BlockSpecification extends FunSuite with Matchers with TestingCommons {
     assert(parsedBlock.signerDataField.value.generator.publicKey.sameElements(gen.publicKey))
   }
 
+  test("Qora block with txs bytes/parse roundtrip") {
+    implicit val consensusModule = new QoraLikeConsensusModule()
+    implicit val transactionModule = new SimpleTransactionModule()
+
+    val reference = Array.fill(Block.BlockIdLength)(Random.nextInt(100).toByte)
+    val gen = new PrivateKeyAccount(reference)
+
+    val gb = Random.nextLong()
+    val gs = Array.fill(QoraLikeConsensusModule.GeneratorSignatureLength)(Random.nextInt(100).toByte)
+
+    val sender = new PrivateKeyAccount(reference.dropRight(2))
+    val tx:Transaction = PaymentTransaction(sender, gen, 5, 1000, System.currentTimeMillis() - 5000)
+
+    val tbd = Seq(tx)
+    val cbd = new QoraLikeConsensusBlockData {
+      override val generatorSignature: Array[Byte] = gs
+      override val generatingBalance: Long = gb
+    }
+
+    val cbdBytes = consensusModule.formBlockData(cbd).bytes
+    assert(cbdBytes.takeRight(QoraLikeConsensusModule.GeneratorSignatureLength).sameElements(gs))
+
+    val version = 1:Byte
+    val timestamp = System.currentTimeMillis()
+
+    val block = Block.buildAndSign(version, timestamp, reference, cbd, tbd, gen)
+    val parsedBlock = Block.parse(block.bytes).get
+
+    val parsedCdf = parsedBlock.consensusDataField.value.asInstanceOf[QoraLikeConsensusBlockData]
+    assert(parsedCdf.generatingBalance == gb)
+    assert(parsedCdf.generatorSignature.sameElements(gs))
+    assert(parsedBlock.versionField.value == version)
+    assert(parsedBlock.signerDataField.value.generator.publicKey.sameElements(gen.publicKey))
+  }
 
   ignore("restoring from Base58 form") {
 
