17e8a5c40f6ad538defe37f07a097477ffca6f29
kushti <kushtech@yahoo.com>
2015-09-14 08:19:51 +0300

    call to CryptoTestSuite added to the ScorexTestSuite

--- src/test/scala/scorex/ScorexTestSuite.scala
+++ src/test/scala/scorex/ScorexTestSuite.scala
@@ -12,6 +12,7 @@ class ScorexTestSuite extends Suites(
   ,new BlockchainStorageSpecification
   ,new WalletSpecification
   ,new BlocksRoutingSpecification
+  ,new CryptoTestSuite
 
   //integration tests - slow!
   // todo:uncomment after fixing problems with test stopping
