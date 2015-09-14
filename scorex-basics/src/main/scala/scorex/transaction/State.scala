5adf54d7e6453bd9097549dd1d3e903e94a010d8
kushti <kushtech@yahoo.com>
2015-09-14 23:03:52 +0300

    State comment

--- scorex-basics/src/main/scala/scorex/transaction/State.scala
+++ scorex-basics/src/main/scala/scorex/transaction/State.scala
@@ -2,6 +2,9 @@ package scorex.transaction
 
 import scorex.block.Block
 
+/**
+ * Abstract functional interface of state which is a result of a sequential blocks applying
+ */
 trait State {
   def processBlock(block: Block, reversal: Boolean): Unit
 }
