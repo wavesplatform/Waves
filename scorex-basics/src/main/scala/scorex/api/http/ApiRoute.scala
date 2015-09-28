bda5af28c3da04d064e92464641ffb391d8e712f
kushti <kushtech@yahoo.com>
2015-09-29 01:06:01 +0300

    formatting / unused imports fix

--- scorex-basics/src/main/scala/scorex/api/http/ApiRoute.scala
+++ scorex-basics/src/main/scala/scorex/api/http/ApiRoute.scala
@@ -2,7 +2,6 @@ package scorex.api.http
 
 import spray.routing.Route
 
-
 trait ApiRoute {
   val route: Route
 }
--- scorex-basics/src/main/scala/scorex/api/http/CompositeHttpServiceActor.scala
+++ scorex-basics/src/main/scala/scorex/api/http/CompositeHttpServiceActor.scala
@@ -1,12 +1,12 @@
 package scorex.api.http
 
 import akka.actor.Actor
-import spray.routing.{HttpService, Route}
+import spray.routing.HttpService
 
 
 class CompositeHttpServiceActor(val routes: ApiRoute*) extends Actor with HttpService {
 
   override def actorRefFactory = context
 
-  override def receive = runRoute(routes.map(_.route).reduce(_~_))
+  override def receive = runRoute(routes.map(_.route).reduce(_ ~ _))
 }
