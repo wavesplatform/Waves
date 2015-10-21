a6cbaee8815fccaae14af3b02ac716f3b7af3cca
kushti <kushtech@yahoo.com>
2015-10-21 21:30:31 +0300

    BlockchainSyncer - todo added

--- src/main/scala/scorex/network/BlockchainSyncer.scala
+++ src/main/scala/scorex/network/BlockchainSyncer.scala
@@ -16,6 +16,7 @@ import scala.concurrent.duration._
 case class NewBlock(block: Block, sender: Option[InetSocketAddress])
 
 
+//todo: reduce boilerplate code
 case class BlockchainSyncer(application: LagonakiApplication) extends FSM[Status, Unit] {
 
   private lazy val networkController = application.networkController
