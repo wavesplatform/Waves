ad331803f193cc200049208b3dbea8634b6fc7a1
kushti <kushtech@yahoo.com>
2015-11-10 10:23:30 +0300

    retry -> untilTimeout, FiniteDuration instead of Int

--- scorex-basics/src/main/scala/scorex/utils/utils.scala
+++ scorex-basics/src/main/scala/scorex/utils/utils.scala
@@ -2,17 +2,20 @@ package scorex
 
 import scala.annotation.tailrec
 
+import scala.concurrent.duration._
+
 package object utils {
 
   @tailrec
-  final def retry[T](timeout: Int, delay: Int = 100)(fn: => T): T = {
+  final def untilTimeout[T](timeout: FiniteDuration,
+                            delay: FiniteDuration = 100.milliseconds)(fn: => T): T = {
     util.Try {
       fn
     } match {
       case util.Success(x) => x
       case _ if timeout > delay =>
-        Thread.sleep(delay)
-        retry(timeout - delay, delay)(fn)
+        Thread.sleep(delay.toMillis)
+        untilTimeout(timeout - delay, delay)(fn)
       case util.Failure(e) => throw e
     }
   }
--- src/test/scala/scorex/lagonaki/integration/BlockchainSyncerSpecification.scala
+++ src/test/scala/scorex/lagonaki/integration/BlockchainSyncerSpecification.scala
@@ -5,7 +5,8 @@ import akka.testkit._
 import org.scalatest.{Matchers, WordSpecLike}
 import scorex.lagonaki.network.BlockchainSyncer.{Generating, GetStatus, Offline}
 import scorex.lagonaki.server.LagonakiApplication
-import scorex.utils.retry
+import scorex.utils.untilTimeout
+import scala.concurrent.duration._
 
 class BlockchainSyncerSpecification(_system: ActorSystem)
   extends TestKit(_system)
@@ -27,11 +28,10 @@ class BlockchainSyncerSpecification(_system: ActorSystem)
     "generate after downloading state" in {
       bcs ! Unit
       //Wait up to 5 seconds to download blockchain and become generating
-      retry(5000) {
+      untilTimeout(5.seconds) {
         bcs ! GetStatus
         expectMsg(Generating.name)
       }
     }
   }
-
 }
