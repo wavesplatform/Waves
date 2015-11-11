df545d6bd05853fc6af01fdf97d8067c6786e4c3
catena <catena@protonmail.com>
2015-11-11 18:00:16 +0300

    Swagger for seed API

--- scorex-basics/src/main/scala/scorex/api/http/SeedApiRoute.scala
+++ scorex-basics/src/main/scala/scorex/api/http/SeedApiRoute.scala
@@ -1,11 +1,15 @@
 package scorex.api.http
 
 import java.security.SecureRandom
+import javax.ws.rs.Path
 
 import akka.actor.ActorRefFactory
+import com.wordnik.swagger.annotations._
 import play.api.libs.json.Json
 import scorex.crypto.Base58
+import spray.http.MediaTypes._
 
+@Api(value = "/seed", description = "Seed generation functions", position = 3)
 case class SeedApiRoute()(implicit val context: ActorRefFactory) extends ApiRoute with CommonApiFunctions {
   val SeedSize = 32
 
@@ -16,9 +20,42 @@ case class SeedApiRoute()(implicit val context: ActorRefFactory) extends ApiRout
   }
 
   override lazy val route =
-    path("seed"./) {
-      get(complete(seed(SeedSize)))
-    } ~ path("seed" / IntNumber) { case length =>
-      get(complete(seed(length)))
+    pathPrefix("seed") {
+      seedRoute ~ length
     }
+
+
+  @Path("/")
+  @ApiOperation(value = "Seed", notes = "Generate random seed", httpMethod = "GET")
+  @ApiResponses(Array(
+    new ApiResponse(code = 200, message = "Json with peer list or error")
+  ))
+  def seedRoute = path("") {
+    get {
+      respondWithMediaType(`application/json`) {
+        complete(
+          seed(SeedSize)
+        )
+      }
+    }
+  }
+
+  @Path("/{length}")
+  @ApiOperation(value = "Seed of specified length", notes = "Generate random seed of specified length", httpMethod = "GET")
+  @ApiImplicitParams(Array(
+    new ApiImplicitParam(name = "length", value = "Seed length ", required = true, dataType = "long", paramType = "path")
+  ))
+  @ApiResponses(Array(
+    new ApiResponse(code = 200, message = "Json with peer list or error")
+  ))
+  def length = path(IntNumber) { case length =>
+    get(
+      respondWithMediaType(`application/json`) {
+        complete(seed(length))
+      }
+    )
+  }
+
+
+
 }
