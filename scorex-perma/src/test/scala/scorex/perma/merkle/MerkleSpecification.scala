8146d629c4943bf2e24ae07cbfa6596a2078c189
pozharko <canoneer@mail.ru>
2015-11-14 19:56:03 +0300

    cleanup

--- scorex-perma/src/test/scala/scorex/perma/merkle/MerkleSpecification.scala
+++ scorex-perma/src/test/scala/scorex/perma/merkle/MerkleSpecification.scala
@@ -12,24 +12,15 @@ class MerkleSpecification extends FunSuite with Matchers {
     val Index = 3
 
     val rnd = new Random()
-//    val dataSet = (1 to TREE_SIZE).map(x => Random.alphanumeric.take(SEGMENT_SIZE).mkString).toArray
-    val dataSet = Array("1234", "5678", "9101", "1213")
+    val dataSet = (1 to TREE_SIZE).map(x => Random.alphanumeric.take(SEGMENT_SIZE).mkString).toArray
 
     val fullFile  = dataSet.mkString("")
 
     val tree = MerkleTree.create(dataSet)
-    println(tree)
-    println("---")
 
     val leaf = tree.byIndex(Index).get
     val data = leaf.data
     val path = leaf.merklePath
-    leaf.merklePath.map { x=>
-      println(x.mkString)
-    }
-    println("????")
-
-    val c = MerkleTree.check(Index, tree.hash, data, path)(HashImpl)
-    assert(c)
+    assert(MerkleTree.check(Index, tree.hash, data, path)(HashImpl))
   }
 }
