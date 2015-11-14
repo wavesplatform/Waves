e10e67d7d1639da52715577024099054e60f5b26
kushti <kushtech@yahoo.com>
2015-11-14 11:17:37 +0300

    MerkleTree interface

--- scorex-perma/src/main/scala/scorex/perma/merkle/MerkleTree.scala
+++ scorex-perma/src/main/scala/scorex/perma/merkle/MerkleTree.scala
@@ -3,20 +3,35 @@ package scorex.perma.merkle
 import scala.annotation.tailrec
 import scala.math
 
+trait MerkleTree[Data] {
+
+  case class AuthDataBlock(data: Data, merklePath: Seq[Digest])
+
+  type Digest = Array[Byte]
+
+  val Size: Int
+
+  def byIndex(n: Int): AuthDataBlock // require(n < Size)
+}
+
+
 object MerkleTree {
-  sealed trait Tree[+A, Hash <: CryptographicHash] { val hash: Vector[Byte] }
+
+  sealed trait Tree[+A, Hash <: CryptographicHash] {
+    val hash: Vector[Byte]
+  }
 
   case class Node[+A, Hash <: CryptographicHash](
-    leftChild: Tree[A, Hash],
-    rightChild: Tree[A, Hash])(hashFunction: Hash)
-      extends Tree[A, Hash] {
+                                                  leftChild: Tree[A, Hash],
+                                                  rightChild: Tree[A, Hash])(hashFunction: Hash)
+    extends Tree[A, Hash] {
 
     override val hash: Vector[Byte] =
       hashFunction.hash(leftChild.hash ++ rightChild.hash)
   }
 
   case class Leaf[+A, Hash <: CryptographicHash](data: A)(hashFunction: Hash)
-      extends Tree[A, Hash] {
+    extends Tree[A, Hash] {
 
     override val hash: Vector[Byte] = hashFunction.hash(data.toString.getBytes)
   }
@@ -26,8 +41,8 @@ object MerkleTree {
   }
 
   def create[A, Hash <: CryptographicHash](
-    dataBlocks: Seq[A],
-    hashFunction: Hash = HashImpl): Tree[A, Hash] = {
+                                            dataBlocks: Seq[A],
+                                            hashFunction: Hash = HashImpl): Tree[A, Hash] = {
     val level = calculateRequiredLevel(dataBlocks.size)
 
     val dataLeaves = dataBlocks.map(data => Leaf(data)(hashFunction))
@@ -41,9 +56,9 @@ object MerkleTree {
   }
 
   def merge[A, Hash <: CryptographicHash](
-    leftChild: Tree[A, Hash],
-    rightChild: Tree[A, Hash],
-    hashFunction: Hash): Node[A, Hash] = {
+                                           leftChild: Tree[A, Hash],
+                                           rightChild: Tree[A, Hash],
+                                           hashFunction: Hash): Node[A, Hash] = {
     Node(leftChild, rightChild)(hashFunction)
   }
 
@@ -55,8 +70,8 @@ object MerkleTree {
 
   @tailrec
   private def makeTree[A, Hash <: CryptographicHash](
-    trees: Seq[Tree[A, Hash]],
-    hashFunction: Hash): Tree[A, Hash] = {
+                                                      trees: Seq[Tree[A, Hash]],
+                                                      hashFunction: Hash): Tree[A, Hash] = {
     def createParent(treePair: Seq[Tree[A, Hash]]): Node[A, Hash] = {
       val leftChild +: rightChild +: _ = treePair
       merge(leftChild, rightChild, hashFunction)
