1d94de871c2107d69dab3fb725e5cb2af629473b
pozharko <canoneer@mail.ru>
2015-11-14 13:24:12 +0300

    calculate tree path

--- scorex-perma/src/main/scala/scorex/perma/merkle/MerkleTree.scala
+++ scorex-perma/src/main/scala/scorex/perma/merkle/MerkleTree.scala
@@ -16,27 +16,46 @@ trait MerkleTreeI[A] {
 object MerkleTree {
 
   type Block = Array[Byte]
-  type Digest = Array[Byte]
+  type Digest = Vector[Byte]
 
   class MerkleTree[A, Hash <: CryptographicHash](val tree: Tree[A, Hash], val leaves: Seq[Tree[A, Hash]]) extends MerkleTreeI[A] {
 
     val Size = leaves.size
+    lazy val rootNode:Node[A, Hash] = tree.asInstanceOf[Node[A, Hash]]
 
     def byIndex(n: Int): Option[AuthDataBlock[A]] = {
+      @tailrec
+      def calculateTreePath(n: Int, node:Node[A, Hash], levelSize: Int, acc: Seq[Digest] = Seq()): Seq[Digest] = {
+        if(n< levelSize/2) {
+          node.leftChild match {
+            case leftNode:Node[A, Hash] =>
+              calculateTreePath(n, leftNode, levelSize/2, node.hash +: acc)
+            case _ =>
+              acc
+          }
+        } else {
+          node.rightChild match {
+            case rightNode:Node[A, Hash] =>
+              calculateTreePath(n - levelSize/2, rightNode, levelSize/2, node.hash +: acc)
+            case _ =>
+              acc
+          }
+        }
+      }
+
       val leaf = leaves.lift(n)
       if (leaf.isEmpty) {
         None
       } else {
         leaf.get match {
           case Leaf(data) =>
-            val treePath = Seq()
+            val treePath = calculateTreePath(n, rootNode, Size)
             Some(AuthDataBlock(data, treePath))
-          case _ => None
+          case _ =>
+            None
         }
       }
     }
-
-
   }
 
 
@@ -49,18 +68,18 @@ object MerkleTree {
                                                   rightChild: Tree[A, Hash])(hashFunction: Hash)
     extends Tree[A, Hash] {
 
-    override val hash: Vector[Byte] =
+    override val hash: Digest =
       hashFunction.hash(leftChild.hash ++ rightChild.hash)
   }
 
   case class Leaf[+A, Hash <: CryptographicHash](data: A)(hashFunction: Hash)
     extends Tree[A, Hash] {
 
-    override val hash: Vector[Byte] = hashFunction.hash(data.toString.getBytes)
+    override val hash: Digest = hashFunction.hash(data.toString.getBytes)
   }
 
   case class EmptyLeaf[Hash <: CryptographicHash]()(hashFunction: Hash) extends Tree[Nothing, Hash] {
-    override val hash: Vector[Byte] = Vector.empty[Byte]
+    override val hash: Digest = Vector.empty[Byte]
   }
 
   def create[A, Hash <: CryptographicHash](
@@ -85,8 +104,9 @@ object MerkleTree {
     Node(leftChild, rightChild)(hashFunction)
   }
 
+  private def log2(x: Double): Double = math.log(x) / math.log(2)
+
   private def calculateRequiredLevel(numberOfDataBlocks: Int): Int = {
-    def log2(x: Double): Double = math.log(x) / math.log(2)
 
     math.ceil(log2(numberOfDataBlocks)).toInt
   }
