a2e7fda2d532e06162b0087840580ff01eeec570
kushti <kushtech@yahoo.com>
2015-11-14 22:26:12 +0300

    generate  / validate functions

--- scorex-perma/src/main/scala/scorex/perma/actors/Miner.scala
+++ scorex-perma/src/main/scala/scorex/perma/actors/Miner.scala
@@ -3,13 +3,13 @@ package scorex.perma.actors
 import java.security.SecureRandom
 
 import akka.actor.{Actor, ActorRef}
-import scorex.crypto.SigningFunctions.{PublicKey, Signature}
+import scorex.crypto.SigningFunctions.{PrivateKey, PublicKey, Signature}
 import scorex.crypto.{SigningFunctions, SigningFunctionsImpl}
 import scorex.perma.Parameters
 import scorex.perma.actors.MinerSpec._
 import scorex.perma.actors.TrustedDealerSpec.{SegmentsRequest, SegmentsToStore}
 import scorex.perma.merkle.HashImpl.hash
-import scorex.perma.merkle.{MerkleTree, AuthDataBlock, CryptographicHash}
+import scorex.perma.merkle.{AuthDataBlock, CryptographicHash, MerkleTree}
 
 import scala.util.Try
 
@@ -21,30 +21,18 @@ case class Ticket(publicKey: PublicKey,
 
 class Miner(trustedDealerRef: ActorRef, rootHash: CryptographicHash.Digest) extends Actor {
 
-  val NoSig = Array[Byte]()
+  import Miner._
 
-  private val (privateKey, publicKey) = SigningFunctionsImpl.createKeyPair(randomBytes(32))
+  private val keyPair = SigningFunctionsImpl.createKeyPair(randomBytes(32))
 
   private var segments: Subset = Map()
-
-  private def randomBytes(howMany: Int) = {
-    val r = new Array[Byte](howMany)
-    new SecureRandom().nextBytes(r) //overrides s
-    r
-  }
-
-  //calculate index of i-th segment
-  private def u(pubKey: SigningFunctions.PublicKey, i: Int): Int = {
-    val h = hash(pubKey ++ BigInt(i).toByteArray)
-    BigInt(h).mod(Parameters.n).toInt
-  }
 
   override def receive = {
 
     case Initialize =>
 
       val segmentIdsToDownload = 1.to(Parameters.l).map { i =>
-        u(publicKey, i)
+        u(keyPair._2, i)
       }.toArray
 
       trustedDealerRef ! SegmentsRequest(segmentIdsToDownload)
@@ -54,44 +42,76 @@ class Miner(trustedDealerRef: ActorRef, rootHash: CryptographicHash.Digest) exte
       segments = sgs
 
     case TicketGeneration(puz) =>
-      //scratch-off for the Local-POR lottery
-      val s = randomBytes(32)
+      val ticket = generate(keyPair, puz, segments)
+    //todo: check ticket
 
-      val sig0 = NoSig
-      val r1 = u(publicKey, BigInt(hash(puz ++ publicKey ++ s)).mod(Parameters.l).toInt)
 
-      val proofs = 1.to(Parameters.k).foldLeft(
-        (r1, sig0, Seq[PartialProof]())
-      ) {
-        case ((ri, sig_prev, seq), _) =>
-          val hi = hash(puz ++ publicKey ++ sig_prev ++ segments(ri).data)
-          val sig = SigningFunctionsImpl.sign(privateKey, hi)
-          val r_next = u(publicKey, BigInt(hash(puz ++ publicKey ++ sig)).mod(Parameters.l).toInt)
-          (r_next, sig, seq :+ PartialProof(sig, ri, segments(ri)))
-      }._3.toIndexedSeq.ensuring(_.size == Parameters.k)
+    case TicketValidation(puz, t: Ticket) =>
+      val res = validate(keyPair._2, puz, t, rootHash)
+
+  }
+}
+
+object Miner {
+
+  val NoSig = Array[Byte]()
+
+  //calculate index of i-th segment
+  private def u(pubKey: SigningFunctions.PublicKey, i: Int): Int = {
+    val h = hash(pubKey ++ BigInt(i).toByteArray)
+    BigInt(h).mod(Parameters.n).toInt
+  }
+
+
+  //todo: move to utils
+  def randomBytes(howMany: Int) = {
+    val r = new Array[Byte](howMany)
+    new SecureRandom().nextBytes(r) //overrides s
+    r
+  }
+
+  def generate(keyPair: (PrivateKey, PublicKey), puz: Array[Byte], segments: Subset): Ticket = {
+
+    val (privateKey, publicKey) = keyPair
+
+    //scratch-off for the Local-POR lottery
+    val s = randomBytes(32)
+
+    val sig0 = NoSig
+    val r1 = u(publicKey, BigInt(hash(puz ++ publicKey ++ s)).mod(Parameters.l).toInt)
+
+    val proofs = 1.to(Parameters.k).foldLeft(
+      (r1, sig0, Seq[PartialProof]())
+    ) {
+      case ((ri, sig_prev, seq), _) =>
+        val hi = hash(puz ++ publicKey ++ sig_prev ++ segments(ri).data)
+        val sig = SigningFunctionsImpl.sign(privateKey, hi)
+        val r_next = u(publicKey, BigInt(hash(puz ++ publicKey ++ sig)).mod(Parameters.l).toInt)
+        (r_next, sig, seq :+ PartialProof(sig, ri, segments(ri)))
+    }._3.toIndexedSeq.ensuring(_.size == Parameters.k)
 
     Ticket(publicKey, s, proofs)
-
-    case TicketValidation(puz, Ticket(pk, s, proofs)) =>
-
-      require(proofs.size == Parameters.k)
-      Try {
-
-        //Local-POR lottery verification
-
-        val sigs = NoSig +: proofs.map(_.signature)
-        val ris = proofs.map(_.segmentIndex)
-
-        1.to(Parameters.k).foldLeft(true){case (partialResult, i) =>
-            val segment = proofs(i-1).segment
-
-            MerkleTree.check(ris(i-1), rootHash, segment.data, segment.merklePath)() || {
-              val hi = hash(puz ++ publicKey ++ sigs(i - 1) ++ segment.data)
-              SigningFunctionsImpl.verify(sigs(i), hi, publicKey)
-            }
-        }
-      }
   }
+
+  //todo: validate ri
+  def validate(publicKey: PublicKey, puz: Array[Byte], t: Ticket, rootHash: CryptographicHash.Digest): Boolean = Try {
+    val proofs = t.proofs
+    require(proofs.size == Parameters.k)
+
+    //Local-POR lottery verification
+
+    val sigs = NoSig +: proofs.map(_.signature)
+    val ris = proofs.map(_.segmentIndex)
+
+    1.to(Parameters.k).foldLeft(true) { case (partialResult, i) =>
+      val segment = proofs(i - 1).segment
+
+      MerkleTree.check(ris(i - 1), rootHash, segment.data, segment.merklePath)() || {
+        val hi = hash(puz ++ publicKey ++ sigs(i - 1) ++ segment.data)
+        SigningFunctionsImpl.verify(sigs(i), hi, publicKey)
+      }
+    }
+  }.getOrElse(false)
 }
 
 //todo: split miner & fullnode roles: a fullnode makes validation, miner validation & generation
@@ -102,7 +122,6 @@ object MinerSpec {
   type Subset = Map[Index, AuthDataBlock[Parameters.DataSegment]]
 
   case class Initialize()
-
 
   case class TicketGeneration(puz: Array[Byte])
 
