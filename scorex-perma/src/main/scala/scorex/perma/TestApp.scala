d541cc82a274828d78d9dcc92de00d6ebece681e
kushti <kushtech@yahoo.com>
2015-11-17 09:15:08 +0300

    BlockchainBuilder.scala

--- scorex-perma/src/main/scala/scorex/perma/TestApp.scala
+++ scorex-perma/src/main/scala/scorex/perma/TestApp.scala
@@ -13,48 +13,6 @@ import scala.collection.mutable
 import scala.util.Random
 
 
-case class BlockHeaderLike(difficulty: BigInt, puz: Array[Byte], ticket: Ticket)
-
-class BlockchainBuilder(miners: Seq[ActorRef]) extends Actor with ScorexLogging {
-
-  var puz: Array[Byte] = calcPuz
-
-  val InitialDifficulty = BigInt(1, Array.fill(33)(1: Byte))
-  val blockchainLike = mutable.Buffer[BlockHeaderLike]()
-
-
-  private def calcPuz = 1.to(32).toArray.map(_ => Random.nextInt(256).toByte)
-
-  def difficulty = blockchainLike.headOption.map(_.difficulty).getOrElse(InitialDifficulty)
-
-  override def receive = {
-    case SendWorkToMiners =>
-      miners.foreach { minerRef =>
-        minerRef ! TicketGeneration(difficulty, puz)
-      }
-
-    //miners are honest (lol) in our setting, so no validation here
-    case WinningTicket(minerPuz, score, ticket) =>
-      if (minerPuz sameElements puz) {
-        val newBlock = BlockHeaderLike(score, puz, ticket)
-        log.info(s"Block generated: $newBlock, blockchain size: ${blockchainLike.size}")
-        blockchainLike += newBlock
-        puz = calcPuz
-        self ! SendWorkToMiners
-      } else {
-        sender() ! TicketGeneration(difficulty, puz)
-        log.debug("Wrong puz from miner: " + minerPuz.mkString)
-      }
-  }
-}
-
-object BlockchainBuilderSpec {
-
-  case object SendWorkToMiners
-
-  case class WinningTicket(puz: Array[Byte], score: BigInt, t: Ticket)
-
-}
 
 object TestApp extends App {
 
