0cac5910a00c4f5062153c63cde72c0a01888218
catena <catena@protonmail.com>
2015-11-18 16:08:18 +0300

    Reformat code

--- scorex-basics/src/test/scala/scorex/props/MerkleSpecification.scala
+++ scorex-basics/src/test/scala/scorex/props/MerkleSpecification.scala
@@ -2,11 +2,10 @@ package scorex.props
 
 import java.io.{File, FileInputStream, FileOutputStream}
 
-import org.scalacheck.{Gen, Arbitrary}
+import org.scalacheck.Gen
 import org.scalatest.prop.{GeneratorDrivenPropertyChecks, PropertyChecks}
 import org.scalatest.{Matchers, PropSpec}
 import scorex.crypto.Sha256
-import scorex.crypto.Sha256._
 import scorex.crypto.ads.merkle.MerkleTree
 
 import scala.util.Random
@@ -14,7 +13,6 @@ import scala.util.Random
 class MerkleSpecification extends PropSpec with PropertyChecks with GeneratorDrivenPropertyChecks with Matchers {
 
   property("value returned from byIndex() is valid for random dataset") {
-
 
 
     val smallInteger = Gen.choose(1, 200)
