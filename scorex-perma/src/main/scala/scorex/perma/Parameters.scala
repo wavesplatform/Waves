ecb49e5652f0b242276a5e68c618648d32399f78
catena <catena@protonmail.com>
2015-11-19 00:29:41 +0300

    Don't recreate existing Tree

--- scorex-perma/src/main/scala/scorex/perma/Parameters.scala
+++ scorex-perma/src/main/scala/scorex/perma/Parameters.scala
@@ -8,8 +8,8 @@ object Parameters {
   //few segments to be stored in a block, so segment size shouldn't be big
   val segmentSize = 1024 //segment size in bytes
 
-  val n = 1024*4 //how many segments in a dataset in total
-  val l = 16 //how many segments to store for an each miner
+  val n = 1024*1024 //how many segments in a dataset in total
+  val l = 1024 //how many segments to store for an each miner
 
   val k = 4 //number of iterations during scratch-off phase
 }
--- scorex-perma/src/main/scala/scorex/perma/TestApp.scala
+++ scorex-perma/src/main/scala/scorex/perma/TestApp.scala
@@ -1,10 +1,12 @@
 package scorex.perma
 
 import java.io.{File, RandomAccessFile}
+import java.nio.file.{Files, Paths}
 
 import akka.actor.{ActorSystem, Props}
 import akka.util.Timeout
 import org.slf4j.LoggerFactory
+import scorex.crypto.Sha256
 import scorex.crypto.ads.merkle.MerkleTree
 import scorex.perma.BlockchainBuilderSpec.SendWorkToMiners
 import scorex.perma.actors.{Miner, TrustedDealer}
@@ -20,16 +22,24 @@ object TestApp extends App {
 
   log.info("Generating random data set")
   val treeDirName = "/tmp/scorex/testApp/"
-  val treeDir = new File(treeDirName)
-  val datasetFile = treeDirName + "/data.file"
-
-  treeDir.mkdirs()
-  val f = new RandomAccessFile(datasetFile, "rw")
-  f.setLength(Parameters.n * Parameters.segmentSize)
 
   log.info("Calculate tree")
-  val tree = MerkleTree.fromFile(datasetFile, treeDirName, Parameters.segmentSize)
-  require(tree.nonEmptyBlocks == Parameters.n, s"${tree.nonEmptyBlocks} == ${Parameters.n}")
+  val tree = if (Files.exists(Paths.get(treeDirName + "/tree.mapDB"))) {
+    new MerkleTree(treeDirName, Parameters.n)
+  } else {
+    val treeDir = new File(treeDirName)
+    treeDir.mkdirs()
+    val datasetFile = treeDirName + "/data.file"
+    new RandomAccessFile(datasetFile, "rw").setLength(Parameters.n * Parameters.segmentSize)
+    val tree = MerkleTree.fromFile(datasetFile, treeDirName, Parameters.segmentSize)
+    require(tree.nonEmptyBlocks == Parameters.n, s"${tree.nonEmptyBlocks} == ${Parameters.n}")
+    tree
+  }
+
+  log.info("test tree")
+  val index = Parameters.n - 3
+  val leaf = tree.byIndex(index).get
+  require(leaf.check(index, tree.rootHash)(Sha256))
 
   log.info("start actor system")
   protected lazy val actorSystem = ActorSystem("lagonaki")
