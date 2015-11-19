2a747db21111c7d44c8ca5a6d50c6417890531d2
kushti <kushtech@yahoo.com>
2015-11-19 17:49:27 +0300

    unused commented code removed

--- scorex-perma/src/main/scala/scorex/perma/consensus/PermaLikeConsensusBlockData.scala
+++ scorex-perma/src/main/scala/scorex/perma/consensus/PermaLikeConsensusBlockData.scala
@@ -2,8 +2,6 @@ package scorex.perma.consensus
 
 import scorex.perma.actors.Ticket
 
-//case class BlockHeaderLike(difficulty: BigInt, puz: Array[Byte], ticket: Ticket)
-
 trait PermaLikeConsensusBlockData {
   val difficulty: BigInt
   val puz: Array[Byte]
