91836719fd178d25f51e9434596011dc38bc6bb6
kushti <kushtech@yahoo.com>
2015-11-24 12:39:33 +0300

    timeout/handler

--- scorex-basics/src/main/scala/scorex/network/SubProtocol.scala
+++ scorex-basics/src/main/scala/scorex/network/SubProtocol.scala
@@ -2,13 +2,19 @@ package scorex.network
 
 import scorex.network.message.MessageSpec
 
+import scala.concurrent.duration.Duration
+
 
 trait SubProtocol[Req] {
   val reqSpec: MessageSpec[Req]
 }
 
 trait AnswerableSubProtocol[Req, Rep] extends SubProtocol[Req] {
+  val timeout: Duration
+
   val replySpec: MessageSpec[Rep]
+
+  def handler: (Rep) => Unit
 }
 
 
