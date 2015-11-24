0a28184f80ccd97fd3d10a49b307e733fa33491e
kushti <kushtech@yahoo.com>
2015-11-24 12:34:43 +0300

    SubProtocol

--- scorex-basics/src/main/scala/scorex/network/SubProtocol.scala
+++ scorex-basics/src/main/scala/scorex/network/SubProtocol.scala
@@ -3,11 +3,13 @@ package scorex.network
 import scorex.network.message.MessageSpec
 
 
-trait SubProtocol[Req,Rep] {
-
-
+trait SubProtocol[Req] {
   val reqSpec: MessageSpec[Req]
-  val replySpec: MessageSpec[Rep]
-
-
 }
+
+trait AnswerableSubProtocol[Req, Rep] extends SubProtocol[Req] {
+  val replySpec: MessageSpec[Rep]
+}
+
+
+trait SyncSubProtocol[Req, Rep] extends AnswerableSubProtocol[Req, Rep]
