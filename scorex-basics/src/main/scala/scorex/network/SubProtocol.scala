b422b9353db88c99bf5a0f40da0a59db2c80f611
kushti <kushtech@yahoo.com>
2015-11-24 17:39:39 +0300

    send/handler, SyncSubProtocol

--- scorex-basics/src/main/scala/scorex/network/SubProtocol.scala
+++ scorex-basics/src/main/scala/scorex/network/SubProtocol.scala
@@ -3,10 +3,16 @@ package scorex.network
 import scorex.network.message.MessageSpec
 
 import scala.concurrent.duration.Duration
+import scala.util.{Success, Failure, Try}
 
+//todo: anti-ddos
 
 trait SubProtocol[Req] {
   val reqSpec: MessageSpec[Req]
+
+  def sendBytes: Array[Byte] => Try[Unit]
+
+  def send(req: Req): Try[Unit] = sendBytes(reqSpec.serializeData(req))
 }
 
 trait AnswerableSubProtocol[Req, Rep] extends SubProtocol[Req] {
@@ -14,8 +20,26 @@ trait AnswerableSubProtocol[Req, Rep] extends SubProtocol[Req] {
 
   val replySpec: MessageSpec[Rep]
 
-  def handler: (Rep) => Unit
+  lazy val messageCode = replySpec.messageCode
+
+  //handler to be defined. Should return Failure if data is semantically incorrect
+  def handler: (Rep) => Try[Unit]
+
+  def onResponse(bytes: Array[Byte]) = replySpec.deserializeData(bytes).flatMap { reply =>
+    handler(reply)
+  }
 }
 
 
-trait SyncSubProtocol[Req, Rep] extends AnswerableSubProtocol[Req, Rep]
+trait SyncSubProtocol[Req, Rep] extends AnswerableSubProtocol[Req, Rep] {
+  private var sentOut: Option[Req] = None
+
+  override def send(req: Req): Try[Unit] = sentOut.synchronized(sentOut match {
+    case Some(_) =>
+      Failure(new Exception("Still waiting for answer"))
+    case None =>
+      sentOut = Some(req)
+      super.send(req)
+      Success(Unit)
+  })
+}
