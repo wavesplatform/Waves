a13273c829ab70f8355693ebce995148406f47d9
catena <catena@protonmail.com>
2015-11-26 21:07:40 +0300

    Is valid block

--- scorex-perma/src/main/scala/scorex/perma/consensus/PermaConsensusModule.scala
+++ scorex-perma/src/main/scala/scorex/perma/consensus/PermaConsensusModule.scala
@@ -1,14 +1,16 @@
 package scorex.perma.consensus
 
-import com.google.common.primitives.Longs
 import scorex.account.{Account, PrivateKeyAccount, PublicKeyAccount}
 import scorex.block.{Block, BlockField}
 import scorex.consensus.ConsensusModule
+import scorex.crypto.SigningFunctions._
+import scorex.crypto.{CryptographicHash, EllipticCurveImpl, Sha256}
+import scorex.perma.settings.Constants
 import scorex.transaction.TransactionModule
-import play.api.libs.json.{JsSuccess, JsResult, JsObject, Json}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.util.Try
 
 /**
   * Data and functions related to a consensus algo
@@ -19,12 +21,14 @@ class PermaConsensusModule extends ConsensusModule[PermaLikeConsensusBlockData] 
   val MiningReward = 1000000
   val InitialDifficulty = BigInt(Array.fill(32)(1: Byte)).toLong
   val GenesisCreator = new PublicKeyAccount(Array())
+  val RootHash: Array[Byte] = "".getBytes
 
   private def blockGenerator(block: Block) = block.signerDataField.value.generator
 
   def isValid[TT](block: Block)(implicit transactionModule: TransactionModule[TT]): Boolean = {
-    //TODO
-    false
+    val f = block.consensusDataField.asInstanceOf[PermaConsensusBlockField]
+    val publicKey = blockGenerator(block).publicKey
+    validate(publicKey, f.value.puz, f.value.difficulty, f.value.ticket, RootHash)
   }
 
   /**
@@ -42,7 +46,9 @@ class PermaConsensusModule extends ConsensusModule[PermaLikeConsensusBlockData] 
     */
   def generators(block: Block): Seq[Account] = Seq(blockGenerator(block))
 
-  def blockScore(block: Block)(implicit transactionModule: TransactionModule[_]): BigInt = BigInt(1)
+  def blockScore(block: Block)(implicit transactionModule: TransactionModule[_]): BigInt = {
+    ticketScore(block.consensusDataField.asInstanceOf[PermaConsensusBlockField].value.ticket)
+  }
 
   def generateNextBlock[TT](account: PrivateKeyAccount)
                            (implicit transactionModule: TransactionModule[TT]): Future[Option[Block]] = {
@@ -63,11 +69,36 @@ class PermaConsensusModule extends ConsensusModule[PermaLikeConsensusBlockData] 
       Ticket(GenesisCreator.publicKey, Array(), IndexedSeq())
     ))
 
-  /*
-    PermaConsensusBlockField(new PermaLikeConsensusBlockData {
-      override val generatorSignature: Array[Byte] = Array.fill(32)(0: Byte)
-    })*/
-
   override def formBlockData(data: PermaLikeConsensusBlockData): BlockField[PermaLikeConsensusBlockData] =
     PermaConsensusBlockField(data)
+
+  private val NoSig = Array[Byte]()
+
+  //todo: validate r\i
+  private def validate(publicKey: PublicKey,
+                       puz: Array[Byte],
+                       difficulty: BigInt,
+                       t: Ticket,
+                       rootHash: CryptographicHash.Digest): Boolean = Try {
+    val proofs = t.proofs
+    require(proofs.size == Constants.k)
+
+    //Local-POR lottery verification
+
+    val sigs = NoSig +: proofs.map(_.signature)
+    val ris = proofs.map(_.segmentIndex)
+
+    val partialProofsCheck = 1.to(Constants.k).foldLeft(true) { case (partialResult, i) =>
+      val segment = proofs(i - 1).segment
+
+      segment.check(ris(i - 1), rootHash)() || {
+        val hi = Sha256.hash(puz ++ publicKey ++ sigs(i - 1) ++ segment.data)
+        EllipticCurveImpl.verify(sigs(i), hi, publicKey)
+      }
+    }
+    partialProofsCheck && (ticketScore(t) < difficulty)
+  }.getOrElse(false)
+
+  private def ticketScore(t: Ticket): BigInt = BigInt(1, Sha256.hash(t.proofs.map(_.signature).reduce(_ ++ _)))
+
 }
