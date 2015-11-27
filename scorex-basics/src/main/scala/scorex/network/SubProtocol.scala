931ec1124d395995705b63e8312c776f68dd4e01
kushti <kushtech@yahoo.com>
2015-11-27 13:45:22 +0300

    Interaction hierarchy

--- scorex-basics/src/main/scala/scorex/network/SubProtocol.scala
+++ scorex-basics/src/main/scala/scorex/network/SubProtocol.scala
@@ -3,43 +3,33 @@ package scorex.network
 import scorex.network.message.MessageSpec
 
 import scala.concurrent.duration.Duration
-import scala.util.{Success, Failure, Try}
+import scala.util.{Failure, Success, Try}
 
 //todo: anti-ddos
 
-trait SubProtocol[Req] {
-  val reqSpec: MessageSpec[Req]
+trait Interaction
 
-  def sendBytes: Array[Byte] => Try[Unit]
-
-  def send(req: Req): Try[Unit] = sendBytes(reqSpec.serializeData(req))
+trait StatelessInteraction {
 }
 
-trait AnswerableSubProtocol[Req, Rep] extends SubProtocol[Req] {
-  val timeout: Duration
-
-  val replySpec: MessageSpec[Rep]
-
-  lazy val messageCode = replySpec.messageCode
-
-  //handler to be defined. Should return Failure if data is semantically incorrect
-  def handler: (Rep) => Try[Unit]
-
-  def onResponse(bytes: Array[Byte]) = replySpec.deserializeData(bytes).flatMap { reply =>
-    handler(reply)
-  }
+trait OnewayInteraction[MsgType] extends StatelessInteraction {
+  val msgSpec: MessageSpec[MsgType]
 }
 
+trait IncomingInteraction[ReqMsg] extends OnewayInteraction[ReqMsg] {
+}
 
-trait SyncSubProtocol[Req, Rep] extends AnswerableSubProtocol[Req, Rep] {
-  private var sentOut: Option[Req] = None
+trait OutcoimngInteraction[RepMsg] extends OnewayInteraction[RepMsg] {
 
-  override def send(req: Req): Try[Unit] = sentOut.synchronized(sentOut match {
-    case Some(_) =>
-      Failure(new Exception("Still waiting for answer"))
-    case None =>
-      sentOut = Some(req)
-      super.send(req)
-      Success(Unit)
-  })
+}
+
+trait StatefulInteraction {
+
+}
+
+trait DuplexInteraction[ReqMsg, RepMsg] extends StatefulInteraction{
+
+  val reqSpec: MessageSpec[ReqMsg]
+  val repSpec: MessageSpec[RepMsg]
+
 }
