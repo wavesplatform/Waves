c1b8383b65096da3f1f0b89550666bc09cce1e86
catena <catena@protonmail.com>
2015-12-14 17:37:17 +0300

    Docs

--- scorex-basics/src/main/scala/scorex/network/ViewSynchronizer.scala
+++ scorex-basics/src/main/scala/scorex/network/ViewSynchronizer.scala
@@ -4,12 +4,12 @@ import akka.actor.{Actor, ActorRef}
 import scorex.network.message.MessageSpec
 
 
-/*  Synchronizing network & local views of an object, e.g. history(blockchain or blocktree), known peers list,
-  segments dataset subset etc.
-
- */
-
-//todo: anti-ddos?
+/**
+  * Synchronizing network & local views of an object, e.g. history(blockchain or blocktree), known peers list,
+  * segments dataset subset etc.
+  *
+  * todo: anti-ddos?
+  */
 trait ViewSynchronizer extends Actor {
 
   protected val networkControllerRef: ActorRef
