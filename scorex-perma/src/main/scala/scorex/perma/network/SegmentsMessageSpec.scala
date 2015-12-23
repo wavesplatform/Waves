03d194005de1a74714349b148f364b76f7efb7d6
catena <catena@protonmail.com>
2015-12-23 15:40:35 +0000

    SegmentsMessageSpec

--- scorex-perma/src/main/scala/scorex/perma/network/SegmentsMessageSpec.scala
+++ scorex-perma/src/main/scala/scorex/perma/network/SegmentsMessageSpec.scala
@@ -3,16 +3,35 @@ package scorex.perma.network
 import com.google.common.primitives.{Bytes, Ints, Longs}
 import scorex.network.message.Message.MessageCode
 import scorex.network.message.MessageSpec
+import scorex.perma.settings.Constants
 import scorex.perma.settings.Constants.{DataSegment, DataSegmentIndex}
 
 import scala.util.Try
 
 class SegmentsMessageSpec extends MessageSpec[Map[DataSegmentIndex, DataSegment]] {
   override val messageCode: MessageCode = 51: Byte
+  private lazy val SegmentSize = Constants.segmentSize
 
-  override def serializeData(data: Map[DataSegmentIndex, DataSegment]): Array[Byte] = ???
+  override def serializeData(data: Map[DataSegmentIndex, DataSegment]): Array[Byte] = {
+    val length = data.size
+    val lengthBytes = Bytes.ensureCapacity(Ints.toByteArray(length), 4, 0)
 
-  override def deserializeData(bytes: Array[Byte]): Try[Map[DataSegmentIndex, DataSegment]] = ???
+    data.foldLeft(lengthBytes) { case (bs, dataSegment) =>
+      Bytes.concat(bs, Bytes.ensureCapacity(Longs.toByteArray(dataSegment._1), 8, 0),
+        Bytes.ensureCapacity(dataSegment._2, SegmentSize, 0))
+    }
+  }
+
+  override def deserializeData(bytes: Array[Byte]): Try[Map[DataSegmentIndex, DataSegment]] = Try {
+    val length = Ints.fromByteArray(bytes.slice(0, 4))
+    require(bytes.length == 4 + length * (8 + SegmentSize))
+    (0 until length).map { i =>
+      val position = 4 + i * (8 + SegmentSize)
+      val index = Longs.fromByteArray(bytes.slice(position, position + 8))
+      val dataSegment = bytes.slice(position + 8, position + 8 + SegmentSize)
+      index -> dataSegment
+    }.toMap
+  }
 
   override val messageName: String = "SegmentsMessage"
 }
@@ -25,7 +44,7 @@ class GetSegmentsMessageSpec extends MessageSpec[Seq[DataSegmentIndex]] {
   private val DataLength = 8
 
   override def serializeData(idexes: Seq[DataSegmentIndex]): Array[Byte] = {
-    val length = idexes.size
+    val length = idexes.length
     val lengthBytes = Bytes.ensureCapacity(Ints.toByteArray(length), 4, 0)
 
     idexes.foldLeft(lengthBytes) { case (bs, index) =>
@@ -35,7 +54,7 @@ class GetSegmentsMessageSpec extends MessageSpec[Seq[DataSegmentIndex]] {
 
   override def deserializeData(bytes: Array[MessageCode]): Try[Seq[DataSegmentIndex]] = Try {
     val length = Ints.fromByteArray(bytes.slice(0, 4))
-    require(bytes.length == 4 + (length * DataLength))
+    require(bytes.length == 4 + length * DataLength)
     (0 until length).map { i =>
       val position = 4 + i * DataLength
       Longs.fromByteArray(bytes.slice(position, position + DataLength))
