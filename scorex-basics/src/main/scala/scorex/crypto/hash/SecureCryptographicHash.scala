3ba53a9cfd324243c9d590b9e6e397578d60bfe7
kushti <kushtech@yahoo.com>
2015-12-29 11:29:21 +0300

    Blake+Keccak as secure hash chain

--- scorex-basics/src/main/scala/scorex/crypto/hash/SecureCryptographicHash.scala
+++ scorex-basics/src/main/scala/scorex/crypto/hash/SecureCryptographicHash.scala
@@ -8,6 +8,5 @@ object SecureCryptographicHash extends CryptographicHash {
 
   override val DigestSize: Int = 32
 
-  override def hash(in: Message): Digest = applyHashes(in, Blake256, Groestl256, Keccak256, JH256, Skein256, Sha256)
-
+  override def hash(in: Message): Digest = applyHashes(in, Blake256, Keccak256)
 }
