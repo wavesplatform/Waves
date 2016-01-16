37f7abb200b2c872599f08836f144799728e5fd6
kushti <kushtech@yahoo.com>
2016-01-16 14:47:38 +0300

    ApplicationVersion.scala, formatting

--- scorex-basics/src/main/scala/scorex/app/Application.scala
+++ scorex-basics/src/main/scala/scorex/app/Application.scala
@@ -2,7 +2,6 @@ package scorex.app
 
 import akka.actor.{ActorSystem, Props}
 import akka.io.IO
-import com.google.common.primitives.Ints
 import scorex.api.http.{ApiRoute, CompositeHttpServiceActor}
 import scorex.block.Block
 import scorex.consensus.ConsensusModule
@@ -17,25 +16,6 @@ import spray.can.Http
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.reflect.runtime.universe.Type
-import scala.util.Try
-
-
-case class ApplicationVersion(firstDigit: Int, secondDigit: Int, thirdDigit: Int){
-  lazy val bytes:Array[Byte] = Ints.toByteArray(firstDigit) ++ Ints.toByteArray(secondDigit) ++ Ints.toByteArray(thirdDigit)
-}
-
-object ApplicationVersion{
-  val SerializedVersionLength = 4*3
-
-  def parse(bytes:Array[Byte]):Try[ApplicationVersion] = Try{
-    require(bytes.length == SerializedVersionLength, "Wrong bytes for application version")
-    ApplicationVersion(
-      Ints.fromByteArray(bytes.slice(0,4)),
-      Ints.fromByteArray(bytes.slice(4,8)),
-      Ints.fromByteArray(bytes.slice(8,12))
-    )
-  }
-}
 
 trait Application extends ScorexLogging {
   val ApplicationNameLimit = 50
---
+++ scorex-basics/src/main/scala/scorex/app/ApplicationVersion.scala
@@ -0,0 +1,22 @@
+package scorex.app
+
+import com.google.common.primitives.Ints
+
+import scala.util.Try
+
+case class ApplicationVersion(firstDigit: Int, secondDigit: Int, thirdDigit: Int) {
+  lazy val bytes: Array[Byte] = Ints.toByteArray(firstDigit) ++ Ints.toByteArray(secondDigit) ++ Ints.toByteArray(thirdDigit)
+}
+
+object ApplicationVersion {
+  val SerializedVersionLength = 4 * 3
+
+  def parse(bytes: Array[Byte]): Try[ApplicationVersion] = Try {
+    require(bytes.length == SerializedVersionLength, "Wrong bytes for application version")
+    ApplicationVersion(
+      Ints.fromByteArray(bytes.slice(0, 4)),
+      Ints.fromByteArray(bytes.slice(4, 8)),
+      Ints.fromByteArray(bytes.slice(8, 12))
+    )
+  }
+}
