d0feaf474ee4811b206efa09629ad53f1e305c59
_ <canoneer@mail.ru>
2016-01-27 12:47:02 +0300

    ValidChainGenerationSpecification init

--- src/test/scala/scorex/lagonaki/integration/ValidChainGenerationSpecification.scala
+++ src/test/scala/scorex/lagonaki/integration/ValidChainGenerationSpecification.scala
@@ -1,53 +1,37 @@
 package scorex.lagonaki.integration
 
-import java.nio.ByteBuffer
+import org.scalatest.{BeforeAndAfterAll, FunSuite, Matchers}
+import scorex.lagonaki.server.LagonakiApplication
+import scorex.utils.untilTimeout
 
-import org.scalatest.FunSuite
-import scorex.block.Block
-import scorex.lagonaki.TestingCommons
-import scorex.transaction.BlockChain
+import scala.concurrent.duration._
 
-class ValidChainGenerationSpecification extends FunSuite with TestingCommons {
+class ValidChainGenerationSpecification extends FunSuite with Matchers with BeforeAndAfterAll {
 
-  import TestingCommons._
 
-  ignore("retroactive chain test") {
-    implicit val consensusModule = application.consensusModule
-    implicit val transactionModule = application.transactionModule
+  val applications = List(new LagonakiApplication("settings-local1.json"),
+    new LagonakiApplication("settings-local2.json"))
 
-    application.run()
-    application.wallet.generateNewAccounts(10)
-    require(application.wallet.privateKeyAccounts().nonEmpty)
-
-    Thread.sleep(5000)
-    val bh = application.blockStorage.history.height()
-
-    application.blockStorage.history match {
-      case blochchain: BlockChain =>
-        //chain validity check
-        (2 to bh).foreach { h =>
-          assert(blochchain.blockAt(h).get.isValid)
-        }
-
-        val b1 = blochchain.blockAt(1).get
-        val b2 = blochchain.blockAt(2).get
-
-        //toBytes/parse roundtrip test
-        val bb2 = Block.parse(b2.bytes).get
-        assert(bb2.timestampField.value == b2.timestampField.value)
-        assert(b1.timestampField.value != b2.timestampField.value)
-        assert(b1 != b2)
-
-        //serialization/deserialization  thru BlockMessage roundtrip test
-
-        application.messagesHandler.parse(ByteBuffer.wrap(b2.bytes), None).get.data.get match {
-          case restored: Block =>
-            assert(restored.timestampField.value == b2.timestampField.value)
-            assert(restored.isValid)
-          case _ => fail("wrong data type")
-        }
-
-        application.stopAll()
-    }
+  override protected def beforeAll(): Unit = {
+    applications.foreach(_.run())
+    applications.foreach(_.wallet.generateNewAccounts(10))
+    applications.foreach(_.wallet.privateKeyAccounts().nonEmpty shouldBe true)
+    applications.foreach(_.blockStorage.history.height() should be > 0)
   }
+
+  override protected def afterAll(): Unit = {
+    applications.foreach(_.stopAll())
+
+  }
+
+
+  ignore("generate 5 blocks") {
+    val height = applications.head.blockStorage.history.height()
+    untilTimeout(5.minutes, 10.seconds) {
+      applications.foreach(_.blockStorage.history.height() should be > height + 5)
+    }
+
+
+  }
+
 }
