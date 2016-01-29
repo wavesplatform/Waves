ecee324a213996df777feecd0204fcf863f15c26
catena <catena@protonmail.com>
2016-01-29 13:31:31 +0300

    Custom application

---
+++ pack.sbt
@@ -0,0 +1,21 @@
+// Package description
+maintainer := "scorex <scorex-dev@groups.io>"
+packageSummary := "Permacoin implementation on top of Scorex framework"
+packageDescription := "Package for permacoin testnet"
+
+enablePlugins(JavaAppPackaging)
+
+//Debian settings
+enablePlugins(DebianPlugin)
+linuxPackageMappings in Debian := linuxPackageMappings.value
+name in Debian := name.value
+version in Debian := "0.1.0"
+genChanges in Debian := new File("changelog.md")
+
+name in Universal := name.value
+//
+//name in UniversalDocs <<= name in Universal
+//
+//name in UniversalSrc <<= name in Universal
+//
+//packageName in Universal := packageName.value
--- src/main/scala/scorex/perma/Application.scala
+++ src/main/scala/scorex/perma/Application.scala
@@ -1,20 +1,101 @@
 package scorex.perma
 
-import scorex.lagonaki.server.LagonakiApplication
+import akka.actor.Props
+import com.typesafe.config.ConfigFactory
+import scorex.api.http._
+import scorex.app.ApplicationVersion
+import scorex.crypto.ads.merkle.AuthDataBlock
+import scorex.lagonaki.api.http.{DebugApiRoute, PaymentApiRoute, PeersHttpService, ScorexApiRoute}
+import scorex.lagonaki.server.LagonakiSettings
+import scorex.network.{TransactionalMessagesRepo, UnconfirmedPoolSynchronizer}
+import scorex.perma.api.http.PermaConsensusApiRoute
+import scorex.perma.consensus.PermaConsensusModule
+import scorex.perma.network.{PermacoinMessagesRepo, SegmentsSynchronizer}
+import scorex.perma.settings.PermaConstants._
+import scorex.perma.storage.AuthDataStorage
+import scorex.storage.Storage
+import scorex.transaction.SimpleTransactionModule
+import scorex.transaction.state.database.blockchain.HistoryScavenger
 import scorex.utils.ScorexLogging
+
+import scala.reflect.runtime.universe._
 
 
 object Application extends App with ScorexLogging {
 
   log.debug("Start server with args: {} ", args)
-  val filename = if (args.length > 0) args(0) else "settings.json"
+  val filename = args.headOption.getOrElse("settings.json")
 
-  val application = new LagonakiApplication(filename)
+  val application = new Application(filename)
 
   log.debug("PermaScorex has been started")
   application.run()
   require(application.blockStorage.state(application.blockStorage.history.lastBlock.uniqueId).isDefined)
 
   if (application.wallet.privateKeyAccounts().isEmpty) application.wallet.generateNewAccounts(1)
+
+}
+
+class Application(val settingsFilename: String) extends scorex.app.Application {
+
+  override val applicationName = "lagonaki"
+
+  private val appConf = ConfigFactory.load().getConfig("app")
+
+  override val appVersion = {
+    val raw = appConf.getString("version")
+    val parts = raw.split("\\.")
+    ApplicationVersion(parts(0).toInt, parts(1).toInt, parts(2).split("-").head.toInt)
+  }
+
+  override implicit lazy val settings = new LagonakiSettings(settingsFilename)
+
+  override implicit lazy val consensusModule = {
+    val authDataStorage: Storage[Long, AuthDataBlock[DataSegment]] = new AuthDataStorage(settings.authDataStorage)
+    val rootHash = settings.rootHash
+    actorSystem.actorOf(Props(classOf[SegmentsSynchronizer], this, rootHash, authDataStorage))
+    new PermaConsensusModule(rootHash, Some(networkController))(authDataStorage)
+  }
+
+  override implicit lazy val transactionModule: SimpleTransactionModule = new SimpleTransactionModule()(settings, this)
+
+  override lazy val blockStorage = transactionModule.blockStorage
+
+  lazy val consensusApiRoute = new PermaConsensusApiRoute(consensusModule, blockStorage)
+
+  override lazy val apiRoutes = Seq(
+    BlocksApiRoute(blockStorage.history, wallet),
+    TransactionsApiRoute(blockStorage.state),
+    consensusApiRoute,
+    WalletApiRoute(wallet),
+    PaymentApiRoute(this),
+    ScorexApiRoute(this),
+    SeedApiRoute(),
+    PeersHttpService(this),
+    AddressApiRoute(wallet, blockStorage.state),
+    DebugApiRoute(this)
+  )
+
+  override lazy val apiTypes = Seq(
+    typeOf[BlocksApiRoute],
+    typeOf[TransactionsApiRoute],
+    typeOf[PermaConsensusApiRoute],
+    typeOf[WalletApiRoute],
+    typeOf[PaymentApiRoute],
+    typeOf[ScorexApiRoute],
+    typeOf[SeedApiRoute],
+    typeOf[PeersHttpService],
+    typeOf[AddressApiRoute],
+    typeOf[DebugApiRoute]
+  )
+
+  override lazy val additionalMessageSpecs = TransactionalMessagesRepo.specs ++ PermacoinMessagesRepo.specs
+
+  //checks
+  require(transactionModule.balancesSupport)
+  require(transactionModule.accountWatchingSupport)
+
+  actorSystem.actorOf(Props(classOf[UnconfirmedPoolSynchronizer], this))
+  actorSystem.actorOf(Props(classOf[HistoryScavenger], blockStorage))
 
 }
