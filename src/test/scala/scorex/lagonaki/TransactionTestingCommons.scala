e560720d16f314ace678338d88264dbb38bdfe3c
_ <catena@protonmail.com>
2016-02-02 17:35:34 +0300

    transaction testing commons

---
+++ src/test/scala/scorex/lagonaki/TransactionTestingCommons.scala
@@ -0,0 +1,55 @@
+package scorex.lagonaki
+
+import scorex.block.Block
+import scorex.lagonaki.TestingCommons._
+import scorex.transaction.LagonakiTransaction.ValidationResult
+import scorex.transaction.{BalanceSheet, GenesisTransaction, Transaction}
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.util.Random
+
+trait TransactionTestingCommons {
+  implicit val consensusModule = application.consensusModule
+  implicit val transactionModule = application.transactionModule
+  if (transactionModule.blockStorage.history.isEmpty) {
+    transactionModule.blockStorage.appendBlock(Block.genesis())
+  }
+  val wallet = application.wallet
+  if (wallet.privateKeyAccounts().isEmpty) {
+    wallet.generateNewAccounts(3)
+  }
+  val accounts = wallet.privateKeyAccounts()
+  val ab = accounts.map(a => transactionModule.blockStorage.state.asInstanceOf[BalanceSheet].generationBalance(a)).sum
+  require(ab > 2)
+
+  def genValidBlock(): Block = {
+    Await.result(consensusModule.generateNextBlocks(accounts)(transactionModule), 10.seconds).headOption match {
+      case Some(block: Block) if block.isValid => block
+      case None =>
+        Thread.sleep(500)
+        genValidBlock()
+    }
+  }
+
+  val genesisAccs = application.blockStorage.history.genesis.transactions.flatMap(_ match {
+    case gtx: GenesisTransaction => Some(gtx.recipient)
+    case _ => None
+  })
+
+  def genValidTransaction(randomAmnt: Boolean = true): Transaction = {
+    val senderAcc = accounts(Random.nextInt(accounts.size))
+    val senderBalance = transactionModule.blockStorage.state.asInstanceOf[BalanceSheet].generationBalance(senderAcc)
+    val fee = Random.nextInt(5).toLong + 1
+    if (senderBalance <= fee) {
+      genValidTransaction(randomAmnt)
+    } else {
+      val amt = if (randomAmnt) Math.abs(Random.nextLong() % (senderBalance - fee))
+      else senderBalance - fee
+      val tx = transactionModule.createPayment(senderAcc, accounts(Random.nextInt(accounts.size)), amt, fee)
+      if (tx.validate()(transactionModule) == ValidationResult.ValidateOke) tx
+      else genValidTransaction(randomAmnt)
+    }
+  }
+
+}
--- src/test/scala/scorex/lagonaki/integration/BlockGenerationSpecification.scala
+++ src/test/scala/scorex/lagonaki/integration/BlockGenerationSpecification.scala
@@ -1,62 +1,12 @@
 package scorex.lagonaki.integration
 
 import org.scalatest.{FunSuite, Matchers}
-import scorex.account.{Account, PrivateKeyAccount, PublicKeyAccount}
-import scorex.block.Block
-import scorex.lagonaki.TestingCommons
-import scorex.lagonaki.TestingCommons._
-import scorex.transaction.LagonakiTransaction.ValidationResult
+import scorex.account.PublicKeyAccount
+import scorex.lagonaki.TransactionTestingCommons
 import scorex.transaction._
 import scorex.transaction.state.database.UnconfirmedTransactionsDatabaseImpl
 
-import scala.concurrent.Await
-import scala.concurrent.duration._
-import scala.util.Random
-
-class BlockGenerationSpecification extends FunSuite with Matchers with TestingCommons {
-
-
-  implicit val consensusModule = application.consensusModule
-  implicit val transactionModule = application.transactionModule
-  if (transactionModule.blockStorage.history.isEmpty) {
-    transactionModule.blockStorage.appendBlock(Block.genesis())
-  }
-  val wallet = application.wallet
-  if (wallet.privateKeyAccounts().isEmpty) {
-    wallet.generateNewAccounts(3)
-  }
-  val accounts = wallet.privateKeyAccounts()
-  val ab = accounts.map(a => transactionModule.blockStorage.state.asInstanceOf[BalanceSheet].generationBalance(a)).sum
-  require(ab > 2)
-
-  def genValidBlock(): Block = {
-    Await.result(consensusModule.generateNextBlocks(accounts)(transactionModule), 10.seconds).headOption match {
-      case Some(block: Block) if block.isValid => block
-      case None =>
-        Thread.sleep(500)
-        genValidBlock()
-    }
-  }
-
-  val genesisAccs = application.blockStorage.history.genesis.transactions.flatMap(_ match {
-    case gtx: GenesisTransaction => Some(gtx.recipient)
-    case _ => None
-  })
-
-  def genValidTransaction(randomAmnt: Boolean = true): Transaction = {
-    val senderAcc = accounts(Random.nextInt(accounts.size))
-    val senderBalance = transactionModule.blockStorage.state.asInstanceOf[BalanceSheet].generationBalance(senderAcc)
-    val fee = Random.nextInt(5).toLong + 1
-    if (senderBalance <= fee) {
-      genValidTransaction(randomAmnt)
-    } else {
-      val amt = if (randomAmnt) Math.abs(Random.nextLong() % (senderBalance - fee))
-      else senderBalance - fee
-      val tx = transactionModule.createPayment(senderAcc, accounts(Random.nextInt(accounts.size)), amt, fee)
-      if (tx.validate()(transactionModule) == ValidationResult.ValidateOke) tx
-      else genValidTransaction(randomAmnt)
-    }
-  }
+class BlockGenerationSpecification extends FunSuite with Matchers with TransactionTestingCommons {
 
   var blokc2txs: Seq[Transaction] = Seq.empty
 
