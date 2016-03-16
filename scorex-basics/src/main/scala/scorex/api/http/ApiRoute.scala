37e2821d9dbd5c42374d5031d10079a21dc3a88a
kushti <kushtech@yahoo.com>
2016-03-16 15:51:20 +0300

    #61 fix: optional CORS for API

--- scorex-basics/src/main/scala/scorex/api/http/ApiRoute.scala
+++ scorex-basics/src/main/scala/scorex/api/http/ApiRoute.scala
@@ -1,22 +1,29 @@
 package scorex.api.http
 
 import akka.actor.ActorRefFactory
+import scorex.app.Application
+import spray.http.HttpHeaders.RawHeader
 import spray.http.MediaTypes._
 import spray.httpx.marshalling.ToResponseMarshallable
 import spray.routing._
 
 trait ApiRoute extends HttpService {
+  val application: Application
   val context: ActorRefFactory
   val route: Route
+
+  lazy val corsAllowed = application.settings.corsAllowed
 
   def actorRefFactory: ActorRefFactory = context
 
   def jsonRoute(fn: => ToResponseMarshallable, method: Directive0 = get): Route = method {
-    respondWithMediaType(`application/json`) {
+    val jsonResponse = respondWithMediaType(`application/json`) {
       complete(
         fn
       )
     }
-  }
 
+    if(corsAllowed) respondWithHeader(RawHeader("Access-Control-Allow-Origin", "*"))(jsonResponse)
+    else jsonResponse
+  }
 }
