d8408c2d435c08d20b9351bab1ec57dffa5be8a3
catena <catena@protonmail.com>
2016-04-12 12:36:02 +0300

    Remove perma settings

--- src/main/scala/scorex/waves/settings/WavesSettings.scala
+++ src/main/scala/scorex/waves/settings/WavesSettings.scala
@@ -3,4 +3,4 @@ package scorex.waves.settings
 import scorex.settings.Settings
 import scorex.transaction.TransactionSettings
 
-class WavesSettings(override val filename: String) extends Settings with TransactionSettings with PermaSettings
+class WavesSettings(override val filename: String) extends Settings with TransactionSettings
