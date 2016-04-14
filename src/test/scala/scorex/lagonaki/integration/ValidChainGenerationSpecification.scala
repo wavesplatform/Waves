6d4671234dc4b28f8884efc468e0bbde51f4b826
catena <catena@protonmail.com>
2016-04-14 11:14:11 +0300

    code coverage

--- src/test/scala/scorex/lagonaki/integration/ValidChainGenerationSpecification.scala
+++ src/test/scala/scorex/lagonaki/integration/ValidChainGenerationSpecification.scala
@@ -30,10 +30,10 @@ with TransactionTestingCommons {
     val height = maxHeight()
     untilTimeout(5.minutes, 10.seconds) {
       val heights = peers.map(_.blockStorage.history.height())
-      log.info(s"Current heights are: $heights. Waiting for ${height+howMany}")
+      log.info(s"Current heights are: $heights. Waiting for ${height + howMany}")
       heights.foreach(_ should be >= height + howMany)
     }
-
+  }
 
   def maxHeight(): Int = peers.map(_.blockStorage.history.height()).max
 
