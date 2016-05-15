013304daf78279328fecc81781e0ccf24def8f63
psdev <p+psdev@psdev.io>
2016-05-15 11:07:04 -0700

    upgrade 0.0.1 to 0.1.3

--- docker/initAndStart.sh
+++ docker/initAndStart.sh
@@ -3,6 +3,6 @@
 cd /tmp
 source ./bitcoin-bash-tools/bitcoin.sh
 export NEWSEED=`newBitcoinKey | grep address | cut -c 27- | head -1`
-sed -i.bak "s/<PUT YOUR OWN WALLET SEED HERE>/${SEED:-$NEWSEED}/g" waves-testnet-0.0.1/waves-testnet.json
-cat waves-testnet-0.0.1/waves-testnet.json
-waves -v /tmp/waves-testnet-0.0.1/waves-testnet.json
+sed -i.bak "s/<PUT YOUR OWN WALLET SEED HERE>/${SEED:-$NEWSEED}/g" waves-testnet-0.1.3/waves-testnet.json
+cat waves-testnet-0.1.3/waves-testnet.json
+waves -v /tmp/waves-testnet-0.1.3/waves-testnet.json
