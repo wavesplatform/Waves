0dff073a96b837700cadd2482695410b9dd286dd
catena <catena@protonmail.com>
2016-05-31 11:00:57 +0300

    object from string

--- scorex-basics/src/main/scala/scorex/utils/utils.scala
+++ scorex-basics/src/main/scala/scorex/utils/utils.scala
@@ -4,27 +4,36 @@ import java.security.SecureRandom
 
 import scala.annotation.tailrec
 import scala.concurrent.duration._
+import scala.reflect.runtime.universe
+import scala.util._
 
 package object utils {
 
   @tailrec
   final def untilTimeout[T](timeout: FiniteDuration,
                             delay: FiniteDuration = 100.milliseconds)(fn: => T): T = {
-    util.Try {
+    Try {
       fn
     } match {
-      case util.Success(x) => x
+      case Success(x) => x
       case _ if timeout > delay =>
         Thread.sleep(delay.toMillis)
         untilTimeout(timeout - delay, delay)(fn)
-      case util.Failure(e) => throw e
+      case Failure(e) => throw e
     }
   }
 
-  def randomBytes(howMany: Int = 32):Array[Byte] = {
+  def randomBytes(howMany: Int = 32): Array[Byte] = {
     val r = new Array[Byte](howMany)
     new SecureRandom().nextBytes(r) //overrides r
     r
+  }
+
+  def objectFromString[T](fullClassName: String): Try[T] = Try {
+    val runtimeMirror = universe.runtimeMirror(getClass.getClassLoader)
+    val module = runtimeMirror.staticModule(fullClassName)
+    val obj = runtimeMirror.reflectModule(module)
+    obj.instance.asInstanceOf[T]
   }
 
 }
