fc6ad482f4de382aec969b3028a9cbc9ddf0d190
catena <catena@protonmail.com>
2016-06-14 14:03:26 +0300

    Payment serialization vector

--- scorex-transaction/src/test/scala/scorex/transaction/TransactionSpecification.scala
+++ scorex-transaction/src/test/scala/scorex/transaction/TransactionSpecification.scala
@@ -3,6 +3,7 @@ package scorex.transaction
 import org.scalatest.prop.{GeneratorDrivenPropertyChecks, PropertyChecks}
 import org.scalatest.{Matchers, PropSpec}
 import scorex.account.PrivateKeyAccount
+import scorex.crypto.encode.Base58
 
 
 class TransactionSpecification extends PropSpec
@@ -85,6 +86,18 @@ with Matchers {
       tx.fee shouldEqual txAfter.fee
       txAfter.signatureValid shouldEqual true
     }
+  }
+
+  property("PaymentTransaction vector") {
+    val bytes = Base58.decode("CzPyJt4mEh3Q8rW3vJG6sdZFwwKDkoYkFDVXhTUUVVPqr3FfBW5UCJHTd3eoVrx1ukbuQiGM3953GB8VJ7yMCePHBMB5spX9D4F6vyBJWva7ZfbG9APje3iKNoBDJLSnPRQCcZUDRYBsSJYL1GGVay25CV9W8aRqjAuyvVVC54TiwExFbaNZ3MrmHaZRr2fS1rjRj38Z").get
+    val actualTransaction = PaymentTransaction.parseBytes(bytes).get
+
+    actualTransaction.fee shouldBe 2
+    actualTransaction.amount shouldBe 123L
+    actualTransaction.sender.address shouldBe "3MTNTc6nrfHrftucagvRyw9PfXffvgJXs6z"
+    actualTransaction.recipient.address shouldBe "3MV9Z4owZGKUGuvTStvb7ByeGTmjmXUaQSU"
+    actualTransaction.timestamp shouldBe 9223372036854775807L
+    Base58.encode(actualTransaction.signature) shouldBe "4ZA1mJgFRSwcCiKQ1Qhb6KKgTBJ1Zt714XWWq2rW6x45ywSfBunZoAuutWduynZBFx3qCb3t8dWnGw1pgtgShuND"
   }
 
   property("PaymentTransaction should deserialize to LagonakiTransaction") {
