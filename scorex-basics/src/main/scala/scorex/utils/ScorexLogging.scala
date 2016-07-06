564e2a96b55225a00aa404ae38af472bb544c20d
kiselev@bitrete.ru <kiselev@bitrete.ru>
2016-07-06 16:41:16 +0300

    Logging level check was added before logging and message construction.

--- scorex-basics/src/main/scala/scorex/utils/ScorexLogging.scala
+++ scorex-basics/src/main/scala/scorex/utils/ScorexLogging.scala
@@ -1,7 +1,49 @@
 package scorex.utils
 
-import org.slf4j.LoggerFactory
+import org.slf4j.{Logger, LoggerFactory}
+
+case class LoggerFacade(logger: Logger) {
+  def trace(message: => String): Unit = {
+    if (logger.isTraceEnabled)
+      logger.trace(message)
+  }
+
+  def debug(message: => String): Unit = {
+    if (logger.isDebugEnabled)
+      logger.debug(message)
+  }
+
+  def info(message: => String): Unit = {
+    if (logger.isInfoEnabled)
+      logger.info(message)
+  }
+
+  def info(message: => String, throwable: Throwable): Unit = {
+    if (logger.isInfoEnabled)
+      logger.info(message, throwable)
+  }
+
+  def warn(message: => String): Unit = {
+    if (logger.isWarnEnabled)
+      logger.warn(message)
+  }
+
+  def warn(message: => String, throwable: Throwable): Unit = {
+    if (logger.isWarnEnabled)
+      logger.warn(message, throwable)
+  }
+
+  def error(message: => String): Unit = {
+    if (logger.isErrorEnabled)
+      logger.error(message)
+  }
+
+  def error(message: => String, throwable: Throwable): Unit = {
+    if (logger.isErrorEnabled)
+      logger.error(message, throwable)
+  }
+}
 
 trait ScorexLogging {
-  protected def log = LoggerFactory.getLogger(this.getClass)
+  protected def log = LoggerFacade(LoggerFactory.getLogger(this.getClass))
 }
