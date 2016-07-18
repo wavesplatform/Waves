725c6fd44ce802bbfd09fba12f751d3f21e5f83d
gagarin55 <gagarin55@protonmail.com>
2016-07-18 23:13:30 +0300

    /waves/external-payment tests restored

--- src/test/scala/scorex/waves/WavesAPISpecification.scala
+++ src/test/scala/scorex/waves/WavesAPISpecification.scala
@@ -3,7 +3,7 @@ package scorex.waves
 import org.scalatest.{BeforeAndAfterAll, DoNotDiscover, FunSuite, Matchers}
 import play.api.libs.json.Json
 import scorex.api.http.{InvalidAddress, InvalidSender, NegativeFee}
-import scorex.waves.transaction.SignedPayment
+import scorex.waves.transaction.{ExternalPayment, SignedPayment}
 
 @DoNotDiscover
 class WavesAPISpecification extends FunSuite with Matchers with BeforeAndAfterAll {
@@ -16,6 +16,29 @@ class WavesAPISpecification extends FunSuite with Matchers with BeforeAndAfterAl
 
   override def afterAll: Unit = {
     stop()
+  }
+
+  test("/waves/external-payment API route can not send to address from another net") {
+    val senderPublicKey = "GvXeYd2iFJUNV7KgeGV2cdnScyrEvrr9uPYJeQFtvg21"
+    val recipient = "3PBWXDFUc86N2EQxKJmW8eFco65xTyMZx6J"
+    val timestamp = 1465391445252L
+    val amount = 10000000000000L
+    val payment = ExternalPayment(timestamp, amount, 400L, recipient, senderPublicKey, "1")
+    val json = Json.toJson(payment).toString
+
+    val response = postRequest(us = "/waves/external-payment", body = json)
+    assert(response.toString == InvalidAddress.json.toString)
+  }
+
+  test("/waves/external-payment can not send from suspendedAddresses") {
+    val senderPublicKey = "5JiSMVVvadkKt2K9dDJjiwLiDzuCMmzcHnNuEzct2LiY"
+    val recipient = "3N5jhcA7R98AUN12ee9pB7unvnAKfzb3nen"
+    val timestamp = 1465391445252L
+    val amount = 10000000000000L
+    val payment = ExternalPayment(timestamp, amount, 100000L, senderPublicKey, recipient, "1")
+    val json = Json.toJson(payment).toString
+    val response = postRequest(us = "/waves/external-payment", body = json)
+    assert(response.toString == InvalidSender.json.toString)
   }
 
   test("/waves/broadcast-signed-payment API route can not send to address from another net") {
