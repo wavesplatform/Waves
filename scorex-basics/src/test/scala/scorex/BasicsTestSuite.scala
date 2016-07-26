60cc7714aaf72513f22f34408eab2257ce08b108
gagarin55 <gagarin55@protonmail.com>
2016-07-26 17:51:19 +0300

    All unused Merkle stuff has been removed

--- scorex-basics/src/test/scala/scorex/BasicsTestSuite.scala
+++ scorex-basics/src/test/scala/scorex/BasicsTestSuite.scala
@@ -3,15 +3,11 @@ package scorex
 import org.scalatest.Suites
 import scorex.account.AccountSpecification
 import scorex.crypto.SigningFunctionsSpecification
-import scorex.crypto.ads.merkle.{AuthDataBlockSpecification, MerkleSpecification, MerkleTreeStorageSpecification}
 import scorex.network.{BlacklistParallelSpecification, BlacklistSpecification, HandshakeSpecification}
 
-class ScorexTestSuite extends Suites(
+class BasicsTestSuite extends Suites(
   new AccountSpecification,
-  new AuthDataBlockSpecification,
   new SigningFunctionsSpecification,
-  new MerkleSpecification,
-  new MerkleTreeStorageSpecification,
   new HandshakeSpecification,
   new BlacklistParallelSpecification,
   new BlacklistSpecification
