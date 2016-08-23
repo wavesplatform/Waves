ba4cce6560ca26f4c3467d0dd6f1347f22745237
Alexey Kiselev <alexey.kiselev@gmail.com>
2016-08-23 17:33:06 +0300

    PoolSynchronizer tests were updated.

--- scorex-basics/src/test/scala/scorex/network/SendingStrategySpecification.scala
+++ scorex-basics/src/test/scala/scorex/network/SendingStrategySpecification.scala
@@ -42,4 +42,18 @@ class SendingStrategySpecification extends FreeSpec
     selectedPeers.contains(peer2) shouldEqual true
     selectedPeers.contains(sender) shouldEqual false
   }
+
+
+  "Broadcast should work with None passed" in {
+    val sender = ConnectedPeer(new InetSocketAddress(1111), null)
+    val peer1 = ConnectedPeer(new InetSocketAddress(2222), null)
+    val peer2 = ConnectedPeer(new InetSocketAddress(3333), null)
+
+    val selectedPeers = BroadcastExceptOf(null).choose(Seq(sender, peer1, peer2))
+
+    selectedPeers.isEmpty shouldEqual false
+    selectedPeers.contains(peer1) shouldEqual true
+    selectedPeers.contains(peer2) shouldEqual true
+    selectedPeers.contains(sender) shouldEqual true
+  }
 }
