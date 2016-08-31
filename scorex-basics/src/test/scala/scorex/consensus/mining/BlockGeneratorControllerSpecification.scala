eba15e19f19d5d4757aea0b44f9ca8b0cfbceb3a
iguana <solovyov.a.g@gmail.com>
2016-08-31 08:52:09 +0300

    Tests

--- scorex-basics/src/test/scala/scorex/consensus/mining/BlockGeneratorControllerSpecification.scala
+++ scorex-basics/src/test/scala/scorex/consensus/mining/BlockGeneratorControllerSpecification.scala
@@ -1,19 +1,25 @@
 package scorex.consensus.mining
 
+import java.net.InetSocketAddress
+
 import akka.actor.{ActorRef, Props}
 import akka.testkit.TestProbe
 import scorex.ActorTestingCommons
 import scorex.consensus.mining.BlockGeneratorController._
+import scorex.network.peer.PeerManager.{ConnectedPeers, GetConnectedPeersTyped}
 import scorex.settings.SettingsMock
 
 import scala.language.postfixOps
 
 class BlockGeneratorControllerSpecification extends ActorTestingCommons {
 
+  val offlineGen = stubFunction[Boolean]
+  def setOfflineGeneration(value: Boolean) = offlineGen when() returns value
+
   object TestSettings extends SettingsMock {
     override lazy val miningThreads: Int = 0
     override lazy val quorum: Int = 1
-    override lazy val offlineGeneration: Boolean = false
+    override lazy val offlineGeneration: Boolean = offlineGen()
   }
 
   val testPeerManager = TestProbe("PeerManager")
@@ -49,6 +55,37 @@ class BlockGeneratorControllerSpecification extends ActorTestingCommons {
       assertStatusIs(Idle)
       actorRef ! StopGeneration
       assertStatusIs(Idle)
+    }
+
+    "suspended / resumed" - {
+      actorRef ! StartGeneration
+
+      testPeerManager.expectMsg(GetConnectedPeersTyped)
+
+      "offlineGeneration = false" - {
+
+        setOfflineGeneration(false)
+
+        "gen allowed" in {
+          testPeerManager.reply(ConnectedPeers(Seq((new InetSocketAddress(687), null))))
+          assertStatusIs(Generating)
+        }
+
+        "gen suspended" in {
+          testPeerManager.reply(ConnectedPeers(Seq.empty))
+          assertStatusIs(Suspended)
+        }
+      }
+
+      "offlineGeneration = true" - {
+
+        setOfflineGeneration(true)
+
+        "gen allowed even if no peers" in {
+          testPeerManager.reply(ConnectedPeers(Seq.empty))
+          assertStatusIs(Generating)
+        }
+      }
     }
   }
 }
