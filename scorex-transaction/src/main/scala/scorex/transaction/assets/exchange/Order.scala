6588e55abefe8589ca76d160be855f9413c614da
Alexey Koloskov <koloale@gmail.com>
2016-10-13 23:13:17 +0300

    Order additional fields

--- scorex-transaction/src/main/scala/scorex/transaction/assets/exchange/Order.scala
+++ scorex-transaction/src/main/scala/scorex/transaction/assets/exchange/Order.scala
@@ -7,37 +7,64 @@ import scorex.crypto.EllipticCurveImpl
 import scorex.crypto.encode.Base58
 import scorex.serialization.{BytesSerializable, Deser, JsonSerializable}
 import scorex.transaction.AssetId
-import scorex.utils.ByteArray
-
+import scorex.utils.{ByteArray, NTP}
 import scala.util.Try
+
+import io.swagger.annotations.ApiModelProperty
+import scorex.crypto.hash.FastCryptographicHash
+import scorex.transaction.assets.exchange.OrderType.OrderType
+
+object OrderType extends Enumeration {
+  type OrderType = Value
+  val BUY, SELL = Value
+}
 
 /**
   * Order to matcher service for asset exchange
   */
-case class Order(sender: PublicKeyAccount, matcher: PublicKeyAccount, spendAssetId: AssetId,
-                 receiveAssetId: AssetId, price: Long, amount: Long, maxTimestamp: Long, matcherFee: Long,
-                 signature: Array[Byte]) extends BytesSerializable with JsonSerializable {
+case class Order(@ApiModelProperty(dataType = "java.lang.String") sender: PublicKeyAccount,
+                 @ApiModelProperty(dataType = "java.lang.String", example = "") matcher: PublicKeyAccount,
+                 @ApiModelProperty(dataType = "java.lang.String") spendAssetId: AssetId,
+                 @ApiModelProperty(dataType = "java.lang.String") receiveAssetId: AssetId,
+                 price: Long,
+                 amount: Long,
+                 maxTimestamp: Long,
+                 @ApiModelProperty(example = "1000") matcherFee: Long,
+                 @ApiModelProperty(dataType = "java.lang.String") signature: Array[Byte])
+  extends BytesSerializable
+  with JsonSerializable {
 
   import Order._
 
   /**
     * In what assets is price
     */
-  lazy val priceAssetId: AssetId = if (ByteArray.compare(spendAssetId, receiveAssetId) > 0) receiveAssetId
+  def priceAssetId: AssetId = if (ByteArray.compare(spendAssetId, receiveAssetId) > 0) receiveAssetId
   else spendAssetId
+
+  def assetPair: AssetPair = if (ByteArray.compare(spendAssetId, receiveAssetId) > 0)
+    AssetPair(receiveAssetId, spendAssetId)
+  else AssetPair(spendAssetId, receiveAssetId)
+
+  def orderType: OrderType = if (spendAssetId sameElements assetPair.asset1) OrderType.SELL else OrderType.BUY
 
   def isValid(atTime: Long): Boolean = {
     amount > 0 && price > 0 && maxTimestamp - atTime <= MaxLiveTime && atTime <= maxTimestamp &&
       EllipticCurveImpl.verify(signature, toSign, sender.publicKey)
   }
 
+  @ApiModelProperty(hidden = true)
   lazy val toSign: Array[Byte] = sender.publicKey ++ matcher.publicKey ++ spendAssetId ++ receiveAssetId ++
     Longs.toByteArray(price) ++ Longs.toByteArray(amount) ++ Longs.toByteArray(maxTimestamp) ++
     Longs.toByteArray(matcherFee)
 
+  @ApiModelProperty(hidden = true)
+  lazy val id: Array[Byte] = FastCryptographicHash(toSign)
+
   override def bytes: Array[Byte] = toSign ++ signature
 
   override def json: JsObject = Json.obj(
+    "id" -> Base58.encode(id),
     "sender" -> sender.address,
     "matcher" -> matcher.address,
     "spendAssetId" -> Base58.encode(spendAssetId),
@@ -49,6 +76,25 @@ case class Order(sender: PublicKeyAccount, matcher: PublicKeyAccount, spendAsset
     "signature" -> Base58.encode(signature)
   )
 
+  override def canEqual(that: Any): Boolean = that.isInstanceOf[Order]
+
+  override def equals(obj: Any): Boolean = {
+    obj match {
+      case o:Order => o.canEqual(this) &&
+        sender == o.sender &&
+        matcher == o.matcher &&
+        (spendAssetId sameElements o.spendAssetId) &&
+        (receiveAssetId sameElements o.receiveAssetId) &&
+        price == o.price &&
+        amount == o.amount &&
+        maxTimestamp == o.maxTimestamp &&
+        matcherFee == o.matcherFee &&
+        (signature sameElements o.signature)
+      case _ => false
+    }
+  }
+
+  override def hashCode(): Int = super.hashCode()
 }
 
 object Order extends Deser[Order] {
