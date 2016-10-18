10625321dbfb8ee4a6d58597b41e046c3663e3ad
Alexey Koloskov <koloale@gmail.com>
2016-10-18 12:10:07 +0300

    OrderMatch test fix

--- scorex-transaction/src/test/scala/scorex/transaction/TransactionGen.scala
+++ scorex-transaction/src/test/scala/scorex/transaction/TransactionGen.scala
@@ -139,7 +139,7 @@ trait TransactionGen {
       MatchResult(
         left.status,
         left.toString + " was invalid",
-        left.toString + " was invalid"
+        left.toString + " was valid"
       )
   }
   val valid = new ValidationMatcher
