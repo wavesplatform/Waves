0bb265032828b7ba0ab2ada201590a91d14c16ef
B83YPoj <B83YPoj@protonmail.com>
2016-10-21 11:15:10 +0300

    feeCalculator

--- scorex-transaction/src/main/scala/scorex/transaction/TransactionSettings.scala
+++ scorex-transaction/src/main/scala/scorex/transaction/TransactionSettings.scala
@@ -1,15 +1,34 @@
 package scorex.transaction
 
 import play.api.libs.json.JsObject
+import scorex.crypto.encode.Base58
+
 import scala.concurrent.duration._
 
 trait TransactionSettings {
   val settingsJSON: JsObject
 
-  private val DefaultHistory = "blockchain"
   lazy val history = (settingsJSON \ "history").asOpt[String].getOrElse(DefaultHistory)
 
-  private val DefaultUtxRebroadcastInterval: FiniteDuration = 30.seconds
   lazy val utxRebroadcastInterval: FiniteDuration = (settingsJSON \ "utxRebroadcastInterval").asOpt[Int]
     .map(x => x.seconds).getOrElse(DefaultUtxRebroadcastInterval)
+
+  lazy val feeMap: Map[String, Long] = (settingsJSON \ "feeMap").validate[Map[String, Map[String, Long]]].map(_.flatMap { e =>
+    e._2.map { kv =>
+      val assetId: Option[AssetId] = if (kv._1 == "Waves") None else Some(Base58.decode(kv._1).get)
+      TransactionAssetFee(e._1.toInt, assetId).key -> kv._2
+    }
+  }).getOrElse(DefaultFeeMap)
+
+  private val DefaultFeeMap: Map[String, Long] = Map(
+    TransactionAssetFee(2, None).key -> 100000,
+    TransactionAssetFee(3, None).key -> 100000,
+    TransactionAssetFee(4, None).key -> 100000,
+    TransactionAssetFee(5, None).key -> 100000
+  )
+
+
+  private val DefaultHistory = "blockchain"
+  private val DefaultUtxRebroadcastInterval: FiniteDuration = 30.seconds
+
 }
---
+++ scorex-transaction/src/test/scala/scorex/transaction/FeeCalculatorSpecification.scala
@@ -0,0 +1,78 @@
+package scorex.transaction
+
+import java.net.InetSocketAddress
+
+import org.scalatest.prop.{GeneratorDrivenPropertyChecks, PropertyChecks}
+import org.scalatest.{Matchers, PropSpec}
+import play.api.libs.json.{JsObject, Json}
+import scorex.crypto.encode.Base58
+import scorex.settings.Settings
+import scorex.transaction.assets.{IssueTransaction, ReissueTransaction, TransferTransaction}
+
+
+class FeeCalculatorSpecification extends PropSpec with PropertyChecks with GeneratorDrivenPropertyChecks
+with Matchers with TransactionGen {
+
+  val WhitelistedAsset = Base58.decode("JAudr64y6YxTgLn9T5giKKqWGkbMfzhdRAxmNNfn6FJN").get
+
+  property("Transfer transaction ") {
+    val feeCalc = new FeeCalculator(MySettings)
+    forAll(transferGenerator) { tx: TransferTransaction =>
+      if (tx.feeAsset.isEmpty) {
+        feeCalc.enoughFee(tx) shouldBe (tx.fee >= 100000)
+      } else {
+        feeCalc.enoughFee(tx) shouldBe false
+        val tx2: TransferTransaction = tx.feeAsset.map(t => tx.copy(feeAsset = Some(WhitelistedAsset))).getOrElse(tx)
+        feeCalc.enoughFee(tx2) shouldBe (tx.fee >= 1002)
+      }
+    }
+  }
+
+  property("Payment transaction ") {
+    val feeCalc = new FeeCalculator(MySettings)
+    forAll(paymentGenerator) { tx: PaymentTransaction =>
+      feeCalc.enoughFee(tx) shouldBe (tx.fee >= 1000000)
+    }
+  }
+
+  property("Issue transaction ") {
+    val feeCalc = new FeeCalculator(MySettings)
+    forAll(issueGenerator) { tx: IssueTransaction =>
+      feeCalc.enoughFee(tx) shouldBe (tx.fee >= 20000000)
+    }
+  }
+
+  property("Reissue transaction ") {
+    val feeCalc = new FeeCalculator(MySettings)
+    forAll(reissueGenerator) { tx: ReissueTransaction =>
+      feeCalc.enoughFee(tx) shouldBe (tx.fee >= 200000)
+    }
+  }
+
+  private val str =
+    """{
+      |"feeMap": {
+      |  "2": {
+      |    "Waves": 100000
+      |  },
+      |  "3": {
+      |    "Waves": 10000000
+      |  },
+      |  "4": {
+      |    "Waves": 100000,
+      |    "JAudr64y6YxTgLn9T5giKKqWGkbMfzhdRAxmNNfn6FJN": 1002
+      |  },
+      |  "5": {
+      |    "Waves": 100000
+      |  }
+      |}}""".stripMargin
+  val feeMapJson: JsObject = Json.parse(str).as[JsObject]
+
+
+  object MySettings extends TransactionSettings with Settings {
+    override lazy val settingsJSON: JsObject = feeMapJson
+    override lazy val dataDirOpt: Option[String] = None
+    override val filename: String = ""
+    override lazy val knownPeers = Seq.empty[InetSocketAddress]
+  }
+}
