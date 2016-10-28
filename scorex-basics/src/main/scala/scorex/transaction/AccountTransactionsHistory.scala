aa43f195786dea340cc6d4744bf824cce6dc5a62
B83YPoj <B83YPoj@protonmail.com>
2016-10-28 13:52:02 +0300

    Limit account transactions by default

--- scorex-basics/src/main/scala/scorex/transaction/AccountTransactionsHistory.scala
+++ scorex-basics/src/main/scala/scorex/transaction/AccountTransactionsHistory.scala
@@ -3,5 +3,5 @@ package scorex.transaction
 import scorex.account.Account
 
 trait AccountTransactionsHistory {
-  def accountTransactions(account: Account): Array[_ <: Transaction]
+  def accountTransactions(account: Account, limit: Int = 50): Array[_ <: Transaction]
 }
