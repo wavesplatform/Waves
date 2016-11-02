ad30868a1fd173efe9dbd489a17bafe711f0dcce
Sergey Tolmachev <tolsi.ru@gmail.com>
2016-11-02 20:01:05 +0300

    Max bytes array is limited to short

--- scorex-basics/src/main/scala/scorex/serialization/BytesSerializable.scala
+++ scorex-basics/src/main/scala/scorex/serialization/BytesSerializable.scala
@@ -1,10 +1,10 @@
 package scorex.serialization
 
-import com.google.common.primitives.Ints
+import com.google.common.primitives.Shorts
 
 trait BytesSerializable extends Serializable {
 
   def bytes: Array[Byte]
 
-  protected def arrayWithSize(b: Array[Byte]): Array[Byte] = Ints.toByteArray(b.length) ++ b
+  protected def arrayWithSize(b: Array[Byte]): Array[Byte] = Shorts.toByteArray(b.length.toShort) ++ b
 }
