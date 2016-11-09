ebd24fbf952ed92bd79e5abc5a8b711dc4842c04
Sergey Tolmachev <tolsi.ru@gmail.com>
2016-11-09 23:07:13 +0300

    Require valid genesis signature

--- scorex-basics/src/main/scala/scorex/block/Block.scala
+++ scorex-basics/src/main/scala/scorex/block/Block.scala
@@ -85,11 +85,11 @@ trait Block extends ScorexLogging {
   def isValid: Boolean = {
     if (transactionModule.blockStorage.history.contains(this)) true //applied blocks are valid
     else {
-      lazy val history = transactionModule.blockStorage.history.contains(referenceField.value)
-      lazy val signature = EllipticCurveImpl.verify(signerDataField.value.signature, bytesWithoutSignature,
+      val history = transactionModule.blockStorage.history.contains(referenceField.value)
+      val signature = EllipticCurveImpl.verify(signerDataField.value.signature, bytesWithoutSignature,
         signerDataField.value.generator.publicKey)
-      lazy val consensus = consensusModule.isValid(this)
-      lazy val transaction = transactionModule.isValid(this)
+      val consensus = consensusModule.isValid(this)
+      val transaction = transactionModule.isValid(this)
 
       if (!history) log.debug(s"Invalid block $encodedId: no parent block in history")
       else if (!signature) log.debug(s"Invalid block $encodedId: signature is not valid")
@@ -248,7 +248,7 @@ object Block extends ScorexLogging {
       val signature = signatureStringOpt.map(Base58.decode(_).get)
         .getOrElse(EllipticCurveImpl.sign(genesisSigner, toSign))
 
-      EllipticCurveImpl.verify(signature, toSign, genesisSigner.publicKey)
+      require(EllipticCurveImpl.verify(signature, toSign, genesisSigner.publicKey), "Passed genesis signature is not valid")
 
       SignerDataBlockField("signature", SignerData(genesisSigner, signature))
     }
