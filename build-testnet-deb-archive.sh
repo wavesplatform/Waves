41acfc3cc339532e08c9eb3326986bf3b9880d48
Sergey Tolmachev <tolsi.ru@gmail.com>
2016-11-16 00:40:05 +0300

    Added deb build with upstart/systemd services

--- build-testnet-deb-archive.sh
+++ build-testnet-deb-archive.sh
@@ -4,8 +4,9 @@ WAVES_VERSION="$(git describe --tags --always)"
 
 echo "Building Waves version ${WAVES_VERSION}..."
 
-sbt clean debian:packageBin
+sbt clean debian:packageBin -Dloader=systemd -Dnetwork=testnet
+cp target/waves_*_all.deb target/waves-systemd-${WAVES_VERSION}.deb
+sbt clean debian:packageBin -Dloader=upstart -Dnetwork=testnet
+cp target/waves_*_all.deb target/waves-upstart-${WAVES_VERSION}.deb
 
-cp target/waves_*_all.deb target/waves-${WAVES_VERSION}.deb
-
-tar -czf waves-${WAVES_VERSION}-testnet-deb.tgz waves-testnet.json -C target/ waves-${WAVES_VERSION}.deb
+tar -czf waves-${WAVES_VERSION}-testnet-deb.tgz waves-testnet.json -C target/ target/waves-systemd-${WAVES_VERSION}.deb target/waves-upstart-${WAVES_VERSION}.deb
