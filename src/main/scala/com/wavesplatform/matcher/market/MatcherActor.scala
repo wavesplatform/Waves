96be4463448104ec3ad4bcbcd0b1cb45a05605a9
Alexey Koloskov <koloale@gmail.com>
2016-12-08 16:36:19 +0300

    Order validation on OrderBook creation.

--- src/main/scala/com/wavesplatform/matcher/market/MatcherActor.scala
+++ src/main/scala/com/wavesplatform/matcher/market/MatcherActor.scala
@@ -3,14 +3,15 @@ package com.wavesplatform.matcher.market
 import akka.actor.{ActorRef, Props}
 import akka.persistence.PersistentActor
 import com.wavesplatform.matcher.market.MatcherActor.OrderBookCreated
-import com.wavesplatform.matcher.market.OrderBookActor.{NotFoundPair, OrderBookRequest}
+import com.wavesplatform.matcher.market.OrderBookActor.{NotFoundPair, OrderBookRequest, OrderRejected}
 import com.wavesplatform.settings.WavesSettings
 import scorex.transaction.SimpleTransactionModule._
-import scorex.transaction.TransactionModule
-import scorex.transaction.assets.exchange.{AssetPair, Order}
+import scorex.transaction.{AssetAcc, TransactionModule}
+import scorex.transaction.assets.exchange.{AssetPair, Order, Validation}
 import scorex.transaction.state.database.blockchain.StoredState
-import scorex.utils.ScorexLogging
+import scorex.utils.{NTP, ScorexLogging}
 import scorex.wallet.Wallet
+import scorex.transaction.assets.exchange.Validation.BooleanOperators
 
 object MatcherActor {
   def name = "matcher"
@@ -28,11 +29,19 @@ class MatcherActor(storedState: StoredState, wallet: Wallet, settings: WavesSett
     context.actorOf(OrderBookActor.props(pair, storedState, wallet, settings, transactionModule),
       OrderBookActor.name(pair))
 
-  def createAndForward(pair: AssetPair, req: Any) = {
-    val orderBook = createOrderBook(pair)
-    persistAsync(OrderBookCreated(pair)) { _ =>
-      forwardReq(req)(orderBook)
-    }
+  def basicValidation(order: Order): Validation = {
+      order.isValid(NTP.correctedTime()) &&
+        (storedState.assetBalance(AssetAcc(order.sender, order.spendAssetId)) >= order.getSpendAmount) :| "Not enough balance"
+  }
+
+  def createAndForward(order: Order) = {
+    val v = basicValidation(order)
+    if (v) {
+      val orderBook = createOrderBook(order.assetPair)
+      persistAsync(OrderBookCreated(order.assetPair)) { _ =>
+        forwardReq(order)(orderBook)
+      }
+    } else sender() ! OrderRejected(v.messages)
   }
 
   def returNotFound() = {
@@ -44,7 +53,7 @@ class MatcherActor(storedState: StoredState, wallet: Wallet, settings: WavesSett
   def forwardToOrderBook: Receive = {
     case order: Order =>
       context.child(OrderBookActor.name(order.assetPair))
-        .fold(createAndForward(order.assetPair, order))(forwardReq(order))
+        .fold(createAndForward(order))(forwardReq(order))
     case ob: OrderBookRequest =>
       context.child(OrderBookActor.name(ob.pair))
         .fold(returNotFound())(forwardReq(ob))
