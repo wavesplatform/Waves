40a9054e95a13d7d156d3b7fdbd33f13ec885af3
Ilya Smagin <ilya.smagin@gmail.com>
2017-01-16 15:58:34 +0300

    #190 Fix PeerDatabaseImplSpecification test

--- src/main/scala/scorex/transaction/PaymentTransaction.scala
+++ src/main/scala/scorex/transaction/PaymentTransaction.scala
@@ -13,7 +13,6 @@ import scorex.transaction.ValidationResult.ValidationResult
 
 import scala.util.{Failure, Success, Try}
 
-@deprecated("Use TransferTransaction")
 sealed trait PaymentTransaction extends TypedTransaction {
   def sender: PublicKeyAccount
 
@@ -72,6 +71,7 @@ object PaymentTransaction extends Deser[PaymentTransaction] {
   private val SignatureLength = 64
   private val BaseLength = TimestampLength + SenderLength + RecipientLength + AmountLength + FeeLength + SignatureLength
 
+  @deprecated("Use TransferTransaction")
   def create(sender: PrivateKeyAccount, recipient: Account, amount: Long, fee: Long, timestamp: Long): Either[ValidationResult, PaymentTransaction] = {
     if (!Account.isValid(recipient)) {
       Left(ValidationResult.InvalidAddress) //CHECK IF RECIPIENT IS VALID ADDRESS
--- src/test/scala/scorex/network/peer/PeerDatabaseImplSpecification.scala
+++ src/test/scala/scorex/network/peer/PeerDatabaseImplSpecification.scala
@@ -11,6 +11,7 @@ import scala.language.{implicitConversions, postfixOps}
 class PeerDatabaseImplSpecification extends path.FreeSpecLike with Matchers {
 
   val database = new PeerDatabaseImpl(TestSettings, None)
+  val database2 = new PeerDatabaseImpl(TestSettings2, None)
   val host1 = "1.1.1.1"
   val host2 = "2.2.2.2"
   val address1 = new InetSocketAddress(host1, 1)
@@ -63,20 +64,6 @@ class PeerDatabaseImplSpecification extends path.FreeSpecLike with Matchers {
       database.getKnownPeers.keys shouldBe empty
     }
 
-    "random peer should return peers from both from database and buffer" in {
-      database.addPeer(address1, Some(0), None)
-      database.addPeer(address2, None, None)
-      database.getKnownPeers.keys should contain(address1)
-      database.getKnownPeers.keys should not contain (address2)
-
-      val set = scala.collection.mutable.Set.empty[InetSocketAddress]
-
-      (1 to 10).foreach(i => database.getRandomPeer(Set()).foreach(a => set.add(a)))
-
-      set should contain(address1)
-      set should contain(address2)
-    }
-
     "blacklisted peer should disappear from internal buffer and database" in {
       database.addPeer(address1, Some(0), None)
       database.addPeer(address2, None, None)
@@ -95,13 +82,33 @@ class PeerDatabaseImplSpecification extends path.FreeSpecLike with Matchers {
 
   }
 
+  "Peer database2" - {
+    "random peer should return peers from both from database and buffer" in {
+      database2.addPeer(address1, Some(0), None)
+      database2.addPeer(address2, None, None)
+      val keys = database2.getKnownPeers.keys
+      keys should contain(address1)
+      keys should not contain address2
+
+      val set = (1 to 10).flatMap(i => database2.getRandomPeer(Set())).toSet
+
+      set should contain(address1)
+      set should contain(address2)
+    }
+  }
+
   private def sleepLong() = Thread.sleep(1200)
 
-  private def sleepShort() = Thread.sleep(600)
+  private def sleepShort() = Thread.sleep(200)
 
   object TestSettings extends SettingsMock {
     override lazy val dataDirOpt: Option[String] = None
     override lazy val peersDataResidenceTime: FiniteDuration = 1 seconds
+  }
+
+  object TestSettings2 extends SettingsMock {
+    override lazy val dataDirOpt: Option[String] = None
+    override lazy val peersDataResidenceTime: FiniteDuration = 10 seconds
   }
 
 }
