a799f02b6b39021269b58f2232bf5d82b1ca8179
Ilya Smagin <ilya.smagin@gmail.com>
2017-02-08 21:21:05 +0300

    T316 - dropping self-calls from State

--- src/main/scala/scorex/transaction/State.scala
+++ src/main/scala/scorex/transaction/State.scala
@@ -5,15 +5,13 @@ import scorex.block.Block
 import scala.util.Try
 
 trait State {
-  private[transaction] def processBlock(block: Block): Try[State]
-
-  def isValid(tx: Transaction, blockTime: Long): Boolean = allValid(Seq(tx), blockTime)
-
   def allValid(txs: Seq[Transaction], blockTime: Long): Boolean = validate(txs, blockTime).size == txs.size
 
   def validate(txs: Seq[Transaction], blockTime: Long): Seq[Transaction]
 
   def included(signature: Array[Byte]): Option[Int]
+
+  private[transaction] def processBlock(block: Block): Try[State]
 
   private[transaction] def rollbackTo(height: Int): State
 }
