38090e5c1ad877f4b10b1b4f220cb4cbf4c9b526
Ilya Smagin <ilya.smagin@gmail.com>
2017-02-10 13:35:22 +0300

    T287: fixing StoredStateUnitTests

--- src/main/scala/scorex/transaction/state/database/state/extension/ActivatedValidator.scala
+++ src/main/scala/scorex/transaction/state/database/state/extension/ActivatedValidator.scala
@@ -11,12 +11,13 @@ class ActivatedValidator(allowBurnTransactionAfterTimestamp: Long) extends Valid
   override def validate(tx: Transaction, height: Int): Either[StateValidationError, Transaction] = tx match {
     case tx: BurnTransaction if tx.timestamp <= allowBurnTransactionAfterTimestamp =>
       Left(StateValidationError(s"BurnTranaction(time: ${tx.timestamp}) must not appear before time=$allowBurnTransactionAfterTimestamp"))
+    case _: BurnTransaction => Right(tx)
     case _: PaymentTransaction => Right(tx)
-    case gtx: GenesisTransaction => Right(tx)
-    case tx: TransferTransaction => Right(tx)
-    case tx: IssueTransaction => Right(tx)
-    case tx: ReissueTransaction => Right(tx)
-    case tx: ExchangeTransaction => Right(tx)
+    case _: GenesisTransaction => Right(tx)
+    case _: TransferTransaction => Right(tx)
+    case _: IssueTransaction => Right(tx)
+    case _: ReissueTransaction => Right(tx)
+    case _: ExchangeTransaction => Right(tx)
     case x => Left(StateValidationError(s"Unknown transaction $x must be explicitly registered within ActivatedValidator"))
   }
 
