59acdbbb2fa2dc395d84895597effb14b157a744
Sergey Tolmachev <tolsi.ru@gmail.com>
2017-02-21 17:15:27 +0300

    import fix

--- src/test/scala/scorex/api/http/RequestGen.scala
+++ src/test/scala/scorex/api/http/RequestGen.scala
@@ -6,7 +6,6 @@ import scorex.api.http.assets._
 import scorex.crypto.encode.Base58
 import scorex.transaction.{TransactionGen, TypedTransaction}
 import scorex.transaction.assets.{IssueTransaction, TransferTransaction}
-import scorex.transaction.state.wallet._
 
 trait RequestGen extends TransactionGen {
   val nonPositiveLong: G[Long] = choose(Long.MinValue, 0).label("non-positive value")
