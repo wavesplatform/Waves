e2b01240dc1f2cf697ca378b245879ff9a0b95a6
Sergey Nazarov <phearnot@renee.ru>
2017-03-21 15:50:46 +0300

    Don't return error objects when there're no blocks at height

--- src/main/scala/scorex/transaction/state/database/blockchain/StoredBlockchain.scala
+++ src/main/scala/scorex/transaction/state/database/blockchain/StoredBlockchain.scala
@@ -138,8 +138,6 @@ class StoredBlockchain(db: MVStore)
 
   override def blockById(blockId: BlockId): Option[Block] = heightOf(blockId).flatMap(blockAt)
 
-  override def children(block: Block): Seq[Block] = heightOf(block).flatMap(h => blockAt(h + 1)).toSeq
-
   override def generatedBy(account: Account, from: Int, to: Int): Seq[Block] = {
     (from to to).toStream.flatMap { h =>
       blockAt(h).flatMap { block =>
