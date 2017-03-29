9d3bccf0562ad98e4cefdd6801ca3251586a66e4
Sergey Nazarov <phearnot@renee.ru>
2017-03-29 15:11:14 +0300

    Handle rejections in json directive

--- src/main/scala/scorex/api/http/ApiRoute.scala
+++ src/main/scala/scorex/api/http/ApiRoute.scala
@@ -1,8 +1,8 @@
 package scorex.api.http
 
 import akka.http.scaladsl.marshalling.ToResponseMarshallable
-import akka.http.scaladsl.server.{Directive0, Directives, Route}
-import com.wavesplatform.http.{ApiMarshallers, api_key}
+import akka.http.scaladsl.server._
+import com.wavesplatform.http.{ApiMarshallers, PlayJsonException, api_key}
 import com.wavesplatform.settings.RestAPISettings
 import play.api.libs.json.Reads
 import scorex.crypto.encode.Base58
@@ -15,8 +15,12 @@ trait ApiRoute extends Directives with CommonApiFunctions with ApiMarshallers {
 
   private lazy val apiKeyHash = Base58.decode(settings.apiKeyHash).toOption
 
-  def json[A: Reads](f: A => ToResponseMarshallable): Route = entity(as[A]) { a =>
-    complete(f(a))
+  private val jsonRejectionHandler = RejectionHandler.newBuilder().handle {
+    case ValidationRejection(_, Some(PlayJsonException(cause, errors))) => complete(WrongJson(cause, errors))
+  }.result()
+
+  def json[A: Reads](f: A => ToResponseMarshallable): Route = handleRejections(jsonRejectionHandler) {
+    entity(as[A]) { a => complete(f(a)) }
   }
 
   def withAuth: Directive0 = apiKeyHash.fold(pass) { hashFromSettings =>
--- src/main/scala/scorex/api/http/CompositeHttpService.scala
+++ src/main/scala/scorex/api/http/CompositeHttpService.scala
@@ -1,17 +1,16 @@
 package scorex.api.http
 
-import scala.reflect.runtime.universe.Type
 import akka.actor.ActorSystem
-import akka.http.scaladsl.model.{HttpMethods, HttpResponse, StatusCodes}
 import akka.http.scaladsl.model.HttpMethods._
+import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.model.headers._
+import akka.http.scaladsl.server.Directive0
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.{Directive0, RejectionHandler, ValidationRejection}
 import akka.stream.ActorMaterializer
-import com.wavesplatform.http.PlayJsonException
-import com.wavesplatform.http.ApiMarshallers._
 import com.wavesplatform.settings.RestAPISettings
 import scorex.api.http.swagger.SwaggerDocService
+
+import scala.reflect.runtime.universe.Type
 
 case class CompositeHttpService(system: ActorSystem, apiTypes: Seq[Type], routes: Seq[ApiRoute], settings: RestAPISettings) {
 
@@ -20,14 +19,8 @@ case class CompositeHttpService(system: ActorSystem, apiTypes: Seq[Type], routes
   def withCors: Directive0 = if (settings.cors)
     respondWithHeader(`Access-Control-Allow-Origin`.*) else pass
 
-  private val defaultRejectionHandler = RejectionHandler.newBuilder().handle {
-    case ValidationRejection(_, Some(PlayJsonException(cause, errors))) => complete(WrongJson(cause, errors))
-  }.result()
-
   val compositeRoute =
-    handleRejections(defaultRejectionHandler) {
-      withCors(routes.map(_.route).reduce(_ ~ _))
-    } ~
+    withCors(routes.map(_.route).reduce(_ ~ _)) ~
     swaggerService.routes ~
     (pathEndOrSingleSlash | path("swagger")) {
       redirect("/api-docs/index.html", StatusCodes.PermanentRedirect)
