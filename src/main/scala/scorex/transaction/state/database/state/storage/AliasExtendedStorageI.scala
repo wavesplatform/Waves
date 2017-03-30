dbacc2dde74e9355e01d267c372a400e4cc8774e
Ilya Smagin <ilya.smagin@gmail.com>
2017-03-30 13:25:19 +0300

    T572: Return multiple aliases for address via rest

--- src/main/scala/scorex/transaction/state/database/state/storage/AliasExtendedStorageI.scala
+++ src/main/scala/scorex/transaction/state/database/state/storage/AliasExtendedStorageI.scala
@@ -3,7 +3,7 @@ package scorex.transaction.state.database.state.storage
 
 trait AliasExtendedStorageI {
   def addressByAlias(address: String): Option[String]
-  def aliasByAddress(alias: String): Option[String]
+  def aliasesByAddress(alias: String): Seq[String]
 
   def persistAlias(address: String, alias: String): Unit
   def removeAlias(alias: String): Unit
