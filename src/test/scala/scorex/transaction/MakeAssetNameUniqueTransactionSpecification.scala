34aa8cd968584f098a9ab6802bda445fb5eba9ec
Sergey Tolmachev <tolsi.ru@gmail.com>
2017-04-28 16:23:38 +0300

    T702: code review changes

--- src/test/scala/scorex/transaction/MakeAssetNameUniqueTransactionSpecification.scala
+++ src/test/scala/scorex/transaction/MakeAssetNameUniqueTransactionSpecification.scala
@@ -7,14 +7,14 @@ import scorex.transaction.assets.MakeAssetNameUniqueTransaction
 
 class MakeAssetNameUniqueTransactionSpecification extends PropSpec with PropertyChecks with Matchers with TransactionGen {
 
-  property("Make unique asset name serialization roundtrip") {
+  property("Make asset name unique serialization roundtrip") {
     forAll(makeAssetNameUniqueGen) { makeUnique: MakeAssetNameUniqueTransaction =>
       val recovered = MakeAssetNameUniqueTransaction.parseBytes(makeUnique.bytes).get
       recovered.bytes shouldEqual makeUnique.bytes
     }
   }
 
-  property("Make unique asset name serialization from TypedTransaction") {
+  property("Make asset name unique serialization from TypedTransaction") {
     forAll(makeAssetNameUniqueGen) { makeUnique: MakeAssetNameUniqueTransaction =>
       val recovered = TransactionParser.parseBytes(makeUnique.bytes).get
       recovered.bytes shouldEqual makeUnique.bytes
