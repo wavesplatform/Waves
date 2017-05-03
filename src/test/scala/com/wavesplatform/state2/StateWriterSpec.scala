860c1944d47ab867a33674ba5ca6ae0931d6ac88
Sergey Tolmachev <tolsi.ru@gmail.com>
2017-05-03 17:32:37 +0300

    T715: state writer should increase height when applying block diff

---
+++ src/test/scala/com/wavesplatform/state2/StateWriterSpec.scala
@@ -0,0 +1,30 @@
+package com.wavesplatform.state2
+
+import com.wavesplatform.state2.reader.StateReaderImpl
+import org.h2.mvstore.MVStore
+import org.scalacheck.Gen
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
+import org.scalatest.{Matchers, Outcome, fixture}
+import scorex.account.Account
+
+class StateWriterSpec extends fixture.FunSuite with Matchers with GeneratorDrivenPropertyChecks {
+  val acc: Account = Account.fromPublicKey(Array.emptyByteArray)
+  val stateHeight = 100
+
+  override type FixtureParam = StateStorage
+
+  override protected def withFixture(test: OneArgTest): Outcome = {
+    val storage = new StateStorage(new MVStore.Builder().open())
+    storage.setHeight(stateHeight)
+    test(storage)
+  }
+
+  test("increase height when applying block diff") { storage =>
+    val writer = new StateWriterImpl(storage)
+    forAll(Gen.choose(0, Int.MaxValue)) { heightDiff =>
+      val h = writer.height
+      writer.applyBlockDiff(BlockDiff(Diff.empty, heightDiff, Map.empty))
+      writer.height shouldBe h + heightDiff
+    }
+  }
+}
