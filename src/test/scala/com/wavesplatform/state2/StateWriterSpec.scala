a4d4587e513562385e7f4eecbeb88d4588df49c9
Sergey Tolmachev <tolsi.ru@gmail.com>
2017-05-04 18:37:37 +0300

    T715: code review fix

--- src/test/scala/com/wavesplatform/state2/StateWriterSpec.scala
+++ src/test/scala/com/wavesplatform/state2/StateWriterSpec.scala
@@ -2,18 +2,12 @@ package com.wavesplatform.state2
 
 import org.h2.mvstore.MVStore
 import org.scalacheck.Gen
+import org.scalatest.{FunSuite, Matchers}
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
-import org.scalatest.{Matchers, Outcome, fixture}
 
-class StateWriterSpec extends fixture.FunSuite with Matchers with GeneratorDrivenPropertyChecks {
-  override type FixtureParam = StateStorage
-
-  override protected def withFixture(test: OneArgTest): Outcome = {
+class StateWriterSpec extends FunSuite with Matchers with GeneratorDrivenPropertyChecks {
+  test("increase height when applying block diff") {
     val storage = new StateStorage(new MVStore.Builder().open())
-    test(storage)
-  }
-
-  test("increase height when applying block diff") { storage =>
     val writer = new StateWriterImpl(storage)
     forAll(Gen.choose(0, Int.MaxValue)) { heightDiff =>
       val h = writer.height
