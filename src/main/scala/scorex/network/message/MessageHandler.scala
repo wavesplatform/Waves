76f23fabe17b397d7213b0568e1c978d9ed645e3
Sergey Nazarov <phearnot@renee.ru>
2017-05-17 19:47:02 +0300

    Extra logging

--- src/main/scala/scorex/network/message/MessageHandler.scala
+++
@@ -1,7 +0,0 @@
-package scorex.network.message
-
-import java.net.InetSocketAddress
-
-object MessageHandler {
-  case class RawNetworkData(spec: MessageSpec[_ <: AnyRef], data: Array[Byte], remote: InetSocketAddress)
-}
