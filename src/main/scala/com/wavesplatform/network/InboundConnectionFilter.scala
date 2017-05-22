92d84f2e60a4277d092c3fdd1b61d740d0438316
Sergey Nazarov <phearnot@renee.ru>
2017-05-22 14:21:04 +0300

    Limit inbound connections

--- src/main/scala/com/wavesplatform/network/InboundConnectionFilter.scala
+++ src/main/scala/com/wavesplatform/network/InboundConnectionFilter.scala
@@ -1,15 +1,36 @@
 package com.wavesplatform.network
 
-import io.netty.channel.{ChannelHandlerContext, ChannelInboundHandlerAdapter}
+import java.net.{InetAddress, InetSocketAddress}
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.atomic.AtomicInteger
 
-class InboundConnectionFilter extends ChannelInboundHandlerAdapter {
-  private def allowConnection: Boolean = true
+import io.netty.channel.ChannelHandler.Sharable
+import io.netty.channel.{ChannelFuture, ChannelHandlerContext}
+import io.netty.handler.ipfilter.AbstractRemoteAddressFilter
+import scorex.utils.ScorexLogging
 
-  override def channelActive(ctx: ChannelHandlerContext) =
-    if (allowConnection) {
-      ctx.pipeline().remove(this)
-      super.channelActive(ctx)
-    } else {
-      ctx.close()
-    }
+@Sharable
+class InboundConnectionFilter(maxInboundConnections: Int, maxConnectionsPerHost: Int)
+  extends AbstractRemoteAddressFilter[InetSocketAddress] with ScorexLogging {
+  private val inboundConnectionCount = new AtomicInteger(0)
+  private val perHostConnectionCount = new ConcurrentHashMap[InetAddress, Int]
+
+  private def dec(remoteAddress: InetAddress) = {
+    inboundConnectionCount.decrementAndGet()
+    perHostConnectionCount.compute(remoteAddress, (_, cnt) => cnt - 1)
+    null.asInstanceOf[ChannelFuture]
+  }
+
+  override def accept(ctx: ChannelHandlerContext, remoteAddress: InetSocketAddress) = {
+    val newTotal = inboundConnectionCount.incrementAndGet()
+    val newCount = perHostConnectionCount.compute(remoteAddress.getAddress, (_, cnt) => Option(cnt).fold(1)(_ + 1))
+
+    newTotal <= maxInboundConnections && newCount <= maxConnectionsPerHost
+  }
+
+  override def channelAccepted(ctx: ChannelHandlerContext, remoteAddress: InetSocketAddress) =
+    ctx.channel().closeFuture().addListener((_: ChannelFuture) => dec(remoteAddress.getAddress))
+
+  override def channelRejected(ctx: ChannelHandlerContext, remoteAddress: InetSocketAddress) =
+    dec(remoteAddress.getAddress)
 }
