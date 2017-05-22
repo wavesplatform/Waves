3e59ff5bb6bff3887d1283b2ad50c448917c4843
Ilya Smagin <ilya.smagin@gmail.com>
2017-05-22 14:22:49 +0300

    T442: StateReaderEffectiveBalancePropertyTest

---
+++ src/test/scala/com/wavesplatform/state2/reader/StateReaderEffectiveBalancePropertyTest.scala
@@ -0,0 +1,34 @@
+package com.wavesplatform.state2.reader
+
+import com.wavesplatform.TransactionGen
+import com.wavesplatform.state2.diffs.{ENOUGH_AMT, assertDiffAndState}
+import org.scalacheck.{Gen, Shrink}
+import org.scalatest.prop.{GeneratorDrivenPropertyChecks, PropertyChecks}
+import org.scalatest.{Matchers, PropSpec}
+import scorex.lagonaki.mocks.TestBlock
+import scorex.transaction.GenesisTransaction
+
+
+class StateReaderEffectiveBalancePropertyTest extends PropSpec with PropertyChecks with GeneratorDrivenPropertyChecks with Matchers with TransactionGen {
+  private implicit def noShrink[A]: Shrink[A] = Shrink(_ => Stream.empty)
+
+  val setup: Gen[(GenesisTransaction, Int, Int, Int)] = for {
+    master <- accountGen
+    ts <- positiveIntGen
+    genesis: GenesisTransaction = GenesisTransaction.create(master, ENOUGH_AMT, ts).right.get
+    emptyBlocksAmt <- Gen.choose(1, 10)
+    atHeight <- Gen.choose(0, 20)
+    confirmations <- Gen.choose(1, 20)
+  } yield (genesis, emptyBlocksAmt, atHeight, confirmations)
+
+
+  property("No-interactions genesis account's effectiveBalance doesn't depend on depths") {
+    forAll(setup) { case ((genesis: GenesisTransaction, emptyBlocksAmt, atHeight, confirmations)) =>
+      val genesisBlock = TestBlock(Seq(genesis))
+      val nextBlocks = List.fill(emptyBlocksAmt - 1)(TestBlock(Seq.empty))
+      assertDiffAndState(genesisBlock +: nextBlocks, TestBlock(Seq.empty)) { (_, newState) =>
+        newState.effectiveBalanceAtHeightWithConfirmations(genesis.recipient, atHeight, confirmations) shouldBe genesis.amount
+      }
+    }
+  }
+}
