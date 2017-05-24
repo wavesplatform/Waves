bc6bfea4a020e5551cb6a4ba2b69a32f7cc0a91c
Sergey Nazarov <phearnot@renee.ru>
2017-05-24 14:37:53 +0300

    Peer synchronizer, improved extension loader

---
+++ src/main/scala/com/wavesplatform/network/PeerSynchronizer.scala
@@ -0,0 +1,47 @@
+package com.wavesplatform.network
+
+import java.net.InetSocketAddress
+
+import io.netty.channel.{ChannelHandlerContext, ChannelInboundHandlerAdapter}
+import io.netty.util.concurrent.ScheduledFuture
+import scorex.network.peer.PeerDatabase
+import scorex.utils.ScorexLogging
+
+import scala.concurrent.duration._
+
+class PeerSynchronizer(peerDatabase: PeerDatabase) extends ChannelInboundHandlerAdapter with ScorexLogging {
+  private var touchTask = Option.empty[ScheduledFuture[_]]
+  private var requestPeersTask = Option.empty[ScheduledFuture[_]]
+
+  override def channelActive(ctx: ChannelHandlerContext) = {
+    requestPeersTask = Some(ctx.executor().scheduleWithFixedDelay(1.second, 5.seconds) {
+      ctx.writeAndFlush(GetPeers)
+    })
+    super.channelActive(ctx)
+  }
+
+  override def channelRead(ctx: ChannelHandlerContext, msg: AnyRef) = msg match {
+    case Handshake(_, _, nodeName, nonce, Some(declaredAddress)) =>
+      if (sameAddresses(declaredAddress, ctx.channel().remoteAddress())) {
+        peerDatabase.addPeer(declaredAddress, Some(nonce), Some(nodeName))
+        touchTask = Some(ctx.executor().scheduleWithFixedDelay(1.second, 1.second) {
+          if (ctx.channel().isActive) {
+            peerDatabase.touch(ctx.channel().localAddress().asInstanceOf[InetSocketAddress])
+          }
+        })
+      } else {
+        log.debug(s"Declared remote address $declaredAddress does not match actual remote address ${ctx.channel().remoteAddress()}")
+      }
+
+    case GetPeers =>
+      ctx.writeAndFlush(peerDatabase.getKnownPeers.keys.toSeq)
+
+    case _ => super.channelRead(ctx, msg)
+  }
+
+  override def channelInactive(ctx: ChannelHandlerContext) = {
+    touchTask.foreach(_.cancel(false))
+    requestPeersTask.foreach(_.cancel(false))
+    super.channelInactive(ctx)
+  }
+}
