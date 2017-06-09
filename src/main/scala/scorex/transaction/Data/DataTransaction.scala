f6e0dd095a47564deaa537d50d655e3d31d0ccfd
nazeim <nazemfaour89@gmail.com>
2017-06-09 18:21:55 +0400

    T751: DataTransaction implemented

--- src/main/scala/scorex/transaction/Data/DataTransaction.scala
+++ src/main/scala/scorex/transaction/Data/DataTransaction.scala
@@ -1,6 +1,5 @@
 package scorex.transaction.Data
 
-import com.google.common.base.Charsets
 import com.google.common.primitives.{Bytes, Longs}
 import play.api.libs.json.{JsObject, Json}
 import scorex.account.{PrivateKeyAccount, PublicKeyAccount}
@@ -21,14 +20,13 @@ sealed trait DataTransaction extends SignedTransaction
 {
   def data: Array[Byte]
   def fee: Long
-  def dataLength: Long
+
 }
 
 object DataTransaction
 {
   private case class  DataTransactionImpl(sender: PublicKeyAccount,
                                           data: Array[Byte],
-                                          dataLength: Long,
                                           fee: Long,
                                           timestamp:Long,
                                           signature: Array[Byte])
@@ -40,7 +38,6 @@ object DataTransaction
     lazy val toSign: Array[Byte] = Bytes.concat(Array(transactionType.id.toByte),
                                                 sender.publicKey,
                                                 BytesSerializable.arrayWithSize(data),
-                                                Longs.toByteArray(dataLength),
                                                 Longs.toByteArray(fee),
                                                 Longs.toByteArray(timestamp))
 
@@ -58,37 +55,34 @@ object DataTransaction
     val txId      = bytes(SignatureLength)
     require(txId == TransactionType.DataTransaction.id.toByte, s"Signed tx id is not match")
     val sender                        = PublicKeyAccount(bytes.slice(SignatureLength + 1, SignatureLength + KeyLength + 1))
-    val (dataLength, dataStart)       = Deser.parseArraySize(bytes, SignatureLength + KeyLength + 1)
-    val data                          = Longs.fromByteArray(bytes.slice(dataStart, dataStart + 8))
-    val fee                           = Longs.fromByteArray(bytes.slice(dataStart + 8, dataStart + 16))
-    val timestamp                     = Longs.fromByteArray(bytes.slice(dataStart + 16, dataStart + 24))
-    DataTransaction.create(sender,dataLength,data, fee, timestamp, signature)
+    val (data, dataLength: Int)       = Deser.parseArraySize(bytes, SignatureLength + KeyLength + 1)
+    val fee                           = Longs.fromByteArray(bytes.slice(dataLength + 8, dataLength + 16))
+    val timestamp                     = Longs.fromByteArray(bytes.slice(dataLength + 16, dataLength + 24))
+    DataTransaction.create(sender, data, fee, timestamp, signature)
       .fold(left => Failure(new Exception(left.toString)), right => Success(right))
   }.flatten
 
   private def createUnverified(sender: PublicKeyAccount,
                                data: Array[Byte],
-                               dataLength: Long,
                                fee: Long,
                                timestamp:Long,
                                signature: Option[Array[Byte]] = None) =
 
-    if (dataLength > MaxDataSize) {
+    if (data.length > MaxDataSize) {
       Left(ValidationError.TooBigArray)
     }  else if (fee <= 0) {
       Left(ValidationError.InsufficientFee)
     } else {
-      Right(DataTransactionImpl(sender, data, dataLength, fee, timestamp, signature.orNull))
+      Right(DataTransactionImpl(sender, data, fee, timestamp, signature.orNull))
     }
 
 
   def create(sender: PublicKeyAccount,
              data: Array[Byte],
-             dataLength: Long,
              fee: Long,
              timestamp:Long,
              signature: Array[Byte]): Either[ValidationError, DataTransaction] =
-    createUnverified(sender, data, dataLength , fee, timestamp, Some(signature))
+    createUnverified(sender, data, fee, timestamp, Some(signature))
       .right.flatMap(SignedTransaction.verify)
 
   def create(sender: PrivateKeyAccount,
@@ -96,7 +90,7 @@ object DataTransaction
              dataLength: Long,
              fee: Long,
              timestamp: Long): Either[ValidationError, DataTransaction] =
-    createUnverified(sender, data, dataLength , fee, timestamp).right.map { unverified =>
+    createUnverified(sender, data, fee, timestamp).right.map { unverified =>
       unverified.copy(signature = EllipticCurveImpl.sign(sender, unverified.toSign))
     }
 
