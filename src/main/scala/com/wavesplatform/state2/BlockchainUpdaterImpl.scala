1524a55b610c78cb05d1965e1739041756c84190
Ilya Smagin <ilya.smagin@gmail.com>
2017-06-16 15:32:05 +0300

    T759: NgHistoryWriter introduced

--- src/main/scala/com/wavesplatform/state2/BlockchainUpdaterImpl.scala
+++ src/main/scala/com/wavesplatform/state2/BlockchainUpdaterImpl.scala
@@ -8,12 +8,14 @@ import com.wavesplatform.state2.BlockchainUpdaterImpl._
 import com.wavesplatform.state2.StateWriterImpl._
 import com.wavesplatform.state2.diffs.BlockDiffer
 import com.wavesplatform.state2.reader.{CompositeStateReader, StateReader}
-import scorex.block.Block
+import scorex.block.{Block, MicroBlock}
 import scorex.transaction._
 import scorex.utils.ScorexLogging
 
-class BlockchainUpdaterImpl private(persisted: StateWriter with StateReader, settings: FunctionalitySettings,
-                                    minimumInMemoryDiffSize: Int, bc: HistoryWriter with History,
+class BlockchainUpdaterImpl private(persisted: StateWriter with StateReader,
+                                    settings: FunctionalitySettings,
+                                    minimumInMemoryDiffSize: Int,
+                                    bc: HistoryWriter with History,
                                     val synchronizationToken: ReentrantReadWriteLock) extends BlockchainUpdater with ScorexLogging {
 
   private val MaxInMemDiffHeight = minimumInMemoryDiffSize * 2
@@ -85,6 +87,8 @@ class BlockchainUpdaterImpl private(persisted: StateWriter with StateReader, set
         false
     }
   }
+
+  override def processMicroBlock(microBlock: MicroBlock): Either[ValidationError, Unit] = ???
 }
 
 object BlockchainUpdaterImpl {
