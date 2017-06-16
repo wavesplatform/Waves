a66ffa88c22bcd5139f8902a5f634dfb64c56078
Ilya Smagin <ilya.smagin@gmail.com>
2017-06-16 16:38:48 +0300

    T759: forgeBlock implemented

--- src/main/scala/scorex/block/MicroBlock.scala
+++ src/main/scala/scorex/block/MicroBlock.scala
@@ -5,7 +5,7 @@ import scorex.account.PublicKeyAccount
 import scorex.block.Block.BlockId
 import scorex.transaction.{Signed, Transaction}
 
-case class MicroBlock(sender: PublicKeyAccount, transactions: Seq[Transaction], prevResBlockSig: BlockId,
+case class MicroBlock(generator: PublicKeyAccount, transactions: Seq[Transaction], prevResBlockSig: BlockId,
                       totalResBlockSig: BlockId, signature: ByteStr) extends Signed {
   override protected lazy val signatureValid: Boolean = true
 }
