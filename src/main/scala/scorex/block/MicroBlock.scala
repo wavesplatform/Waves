2848209dd9403a01b181ed8a9fa7e2a9789dfc29
Ilya Smagin <ilya.smagin@gmail.com>
2017-06-20 18:25:53 +0300

    T759: microblock smart constructor

--- src/main/scala/scorex/block/MicroBlock.scala
+++ src/main/scala/scorex/block/MicroBlock.scala
@@ -3,12 +3,21 @@ package scorex.block
 import com.wavesplatform.state2.ByteStr
 import scorex.account.PublicKeyAccount
 import scorex.block.Block.BlockId
-import scorex.transaction.{AssetAcc, Signed, Transaction}
+import scorex.transaction.ValidationError.GenericError
+import scorex.transaction.{Signed, Transaction, ValidationError}
 
-case class MicroBlock(generator: PublicKeyAccount, transactionData: Seq[Transaction], prevResBlockSig: BlockId,
-                      totalResBlockSig: BlockId, signature: ByteStr) extends  Signed {
+case class MicroBlock private(version: Byte, generator: PublicKeyAccount, transactionData: Seq[Transaction], prevResBlockSig: BlockId,
+                              totalResBlockSig: BlockId, signature: ByteStr) extends Signed {
   override protected lazy val signatureValid: Boolean = true
+}
 
-
+object MicroBlock {
+  def apply(generator: PublicKeyAccount, transactionData: Seq[Transaction], prevResBlockSig:
+  BlockId, totalResBlockSig: BlockId, signature: ByteStr): Either[ValidationError, MicroBlock] = {
+    if (transactionData.isEmpty)
+      Left(GenericError("cannot create empty MicroBlock"))
+    else
+      Right(new MicroBlock(version = 1: Byte, generator, transactionData, prevResBlockSig, totalResBlockSig, signature))
+  }
 }
 
