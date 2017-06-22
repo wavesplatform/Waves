aff45d74f81bcc09ba1caf601b996077e988644c
Ilya Smagin <ilya.smagin@gmail.com>
2017-06-22 17:30:44 +0300

    T759: block processing tests added

--- src/test/scala/com/wavesplatform/history/BlockStorageImplBlockBadReferencesTest.scala
+++ src/test/scala/com/wavesplatform/history/BlockStorageImplBlockBadReferencesTest.scala
@@ -8,7 +8,7 @@ import org.scalatest._
 import org.scalatest.prop.PropertyChecks
 import scorex.transaction.{GenesisTransaction, PaymentTransaction}
 
-class BlockStorageImplMicroblockBadReferencesTest extends PropSpec with PropertyChecks with
+class BlockStorageImplBlockBadReferencesTest extends PropSpec with PropertyChecks with
   DomainScenarioDrivenPropertyCheck with Matchers with TransactionGen {
   private implicit def noShrink[A]: Shrink[A] = Shrink(_ => Stream.empty)
 
@@ -23,7 +23,7 @@ class BlockStorageImplMicroblockBadReferencesTest extends PropSpec with Property
   } yield (genesis, payment, payment2, payment3)
 
 
-  property("referenced (micro)block doesn't exist") {
+  property("microBlock: referenced (micro)block doesn't exist") {
     scenario(preconditionsAndPayments) { case (domain, (genesis, payment, payment2, payment3)) =>
       val block0 = buildBlockOfTxs(randomSig, Seq(genesis))
       val (block1, microblocks1) = chainBaseAndMicro(block0.uniqueId, payment, Seq(payment2, payment3))
@@ -37,7 +37,7 @@ class BlockStorageImplMicroblockBadReferencesTest extends PropSpec with Property
     }
   }
 
-  property("first micro doesn't reference base block(references nothing)") {
+  property("microblock: first micro doesn't reference base block(references nothing)") {
     scenario(preconditionsAndPayments) { case (domain, (genesis, payment, payment2, payment3)) =>
       val blocks = chainBlocks(Seq(Seq(genesis), Seq(payment)))
       val block0 = blocks(0)
@@ -50,7 +50,7 @@ class BlockStorageImplMicroblockBadReferencesTest extends PropSpec with Property
     }
   }
 
-  property("first micro doesn't reference base block(references firm block)") {
+  property("microblock: first micro doesn't reference base block(references firm block)") {
     scenario(preconditionsAndPayments) { case (domain, (genesis, payment, payment2, payment3)) =>
       val blocks = chainBlocks(Seq(Seq(genesis), Seq(payment)))
       val block0 = blocks(0)
@@ -63,7 +63,7 @@ class BlockStorageImplMicroblockBadReferencesTest extends PropSpec with Property
     }
   }
 
-  property("no base block at all") {
+  property("microblock: no base block at all") {
     scenario(preconditionsAndPayments) { case (domain, (genesis, payment, payment2, payment3)) =>
       val block0 = buildBlockOfTxs(randomSig, Seq(genesis))
       val (block1, microblocks1) = chainBaseAndMicro(block0.uniqueId, payment, Seq(payment2))
@@ -74,8 +74,7 @@ class BlockStorageImplMicroblockBadReferencesTest extends PropSpec with Property
     }
   }
 
-
-  property("follow-up micro doesn't reference last known micro") {
+  property("microblock: follow-up micro doesn't reference last known micro") {
     scenario(preconditionsAndPayments) { case (domain, (genesis, payment, payment2, payment3)) =>
       val block0 = buildBlockOfTxs(randomSig, Seq(genesis))
       val (block1, microblocks1) = chainBaseAndMicro(block0.uniqueId, payment, Seq(payment2, payment3))
@@ -85,6 +84,48 @@ class BlockStorageImplMicroblockBadReferencesTest extends PropSpec with Property
       domain.blockchainUpdater.processBlock(block1).explicitGet()
       domain.blockchainUpdater.processMicroBlock(goodMicro).explicitGet()
       domain.blockchainUpdater.processMicroBlock(badRefMicro) should produce("doesn't reference last known microBlock")
+    }
+  }
+
+  property("block: second 'genesis' block") {
+    scenario(preconditionsAndPayments) { case (domain, (genesis, payment, payment2, payment3)) =>
+      val block0 = buildBlockOfTxs(randomSig, Seq(genesis, payment))
+      val block1 = buildBlockOfTxs(randomSig, Seq(genesis, payment2))
+      domain.blockchainUpdater.processBlock(block0) shouldBe 'right
+      domain.blockchainUpdater.processBlock(block1) should produce("References non-existing block")
+    }
+  }
+
+  property("block: incorrect or non-existing block when liquid is empty") {
+    scenario(preconditionsAndPayments) { case (domain, (genesis, payment, payment2, payment3)) =>
+      val blocks = chainBlocks(Seq(Seq(genesis), Seq(payment), Seq(payment2)))
+      domain.blockchainUpdater.processBlock(blocks.head) shouldBe 'right
+      domain.blockchainUpdater.processBlock(blocks(1)) shouldBe 'right
+      domain.blockchainUpdater.removeAfter(blocks.head.uniqueId) shouldBe true
+      val block2 = buildBlockOfTxs(randomSig, Seq(payment3))
+      domain.blockchainUpdater.processBlock(block2) should produce("References incorrect or non-existing block")
+    }
+  }
+
+  property("block: overwrite liquid block") {
+    scenario(preconditionsAndPayments) { case (domain, (genesis, payment, payment2, payment3)) =>
+      val blocks = chainBlocks(Seq(Seq(genesis), Seq(payment), Seq(payment2)))
+      val block2v2 = buildBlockOfTxs(blocks(1).uniqueId, Seq(payment3))
+      domain.blockchainUpdater.processBlock(blocks(0)) shouldBe 'right
+      domain.blockchainUpdater.processBlock(blocks(1)) shouldBe 'right
+      domain.blockchainUpdater.processBlock(blocks(2)) shouldBe 'right
+      domain.blockchainUpdater.processBlock(block2v2) shouldBe 'right
+    }
+  }
+
+  property("block: incorrect or non-existing block when liquid exists") {
+    scenario(preconditionsAndPayments) { case (domain, (genesis, payment, payment2, payment3)) =>
+      val blocks = chainBlocks(Seq(Seq(genesis), Seq(payment), Seq(payment2)))
+      val block1v2 = buildBlockOfTxs(blocks(0).uniqueId, Seq(payment3))
+      domain.blockchainUpdater.processBlock(blocks(0)) shouldBe 'right
+      domain.blockchainUpdater.processBlock(blocks(1)) shouldBe 'right
+      domain.blockchainUpdater.processBlock(blocks(2)) shouldBe 'right
+      domain.blockchainUpdater.processBlock(block1v2) should produce("References incorrect or non-existing block")
     }
   }
 }
