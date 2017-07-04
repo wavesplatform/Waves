663ed8a5d441beb86d0a084e62b0b11145a25f8f
Ilya Smagin <ilya.smagin@gmail.com>
2017-07-04 19:34:20 +0300

    Merge branch 'master' into T759-ng
    Conflicts:
    	src/main/scala/com/wavesplatform/Coordinator.scala
    	src/main/scala/com/wavesplatform/mining/Miner.scala
    	src/main/scala/scorex/transaction/PoSCalc.scala

--- src/main/scala/com/wavesplatform/mining/Miner.scala
+++ src/main/scala/com/wavesplatform/mining/Miner.scala
@@ -2,41 +2,49 @@ package com.wavesplatform.mining
 
 import java.time.{Duration, Instant}
 import java.util.concurrent._
+import java.util.concurrent.atomic.AtomicBoolean
 
 import com.google.common.util.concurrent.{FutureCallback, Futures, MoreExecutors}
-import com.wavesplatform.settings.{BlockchainSettings, MinerSettings}
+import com.wavesplatform.network._
+import com.wavesplatform.settings.WavesSettings
 import com.wavesplatform.state2.ByteStr
 import com.wavesplatform.state2.reader.StateReader
+import com.wavesplatform.{Coordinator, UtxPool}
+import io.netty.channel.group.ChannelGroup
 import scorex.account.PrivateKeyAccount
 import scorex.block.Block
 import scorex.consensus.nxt.NxtLikeConsensusBlockData
 import scorex.transaction.PoSCalc._
-import scorex.transaction.UnconfirmedTransactionsStorage.packUnconfirmed
-import scorex.transaction.{History, UnconfirmedTransactionsStorage}
-import scorex.utils.ScorexLogging
+import scorex.transaction.{BlockchainUpdater, CheckpointService, History}
+import scorex.utils.{ScorexLogging, Time}
+import scorex.wallet.Wallet
 
 import scala.collection.JavaConverters._
 import scala.math.Ordering.Implicits._
 
 
 class Miner(
-    history: History,
-    state: StateReader,
-    utx: UnconfirmedTransactionsStorage,
-    privateKeyAccounts: => Seq[PrivateKeyAccount],
-    blockchainSettings: BlockchainSettings,
-    minerSettings: MinerSettings,
-    time: => Long,
-    peerCount: => Int,
-    blockHandler: Block => Unit) extends ScorexLogging {
+               allChannels: ChannelGroup,
+               blockchainReadiness: AtomicBoolean,
+               blockchainUpdater: BlockchainUpdater,
+               checkpoint: CheckpointService,
+               history: History,
+               stateReader: StateReader,
+               settings: WavesSettings,
+               time: Time,
+               utx: UtxPool,
+               wallet: Wallet) extends ScorexLogging {
+
   import Miner._
 
   private val minerPool = MoreExecutors.listeningDecorator(Executors.newScheduledThreadPool(2))
   private val scheduledAttempts = new ConcurrentHashMap[ByteStr, ScheduledFuture[_]]()
 
+  private val minerSettings = settings.minerSettings
+
   private def checkAge(parentHeight: Int, parent: Block): Either[String, Unit] =
     Either
-      .cond(parentHeight == 1, (), Duration.between(Instant.ofEpochMilli(parent.timestamp), Instant.ofEpochMilli(time)))
+      .cond(parentHeight == 1, (), Duration.between(Instant.ofEpochMilli(parent.timestamp), Instant.ofEpochMilli(time.correctedTime())))
       .left.flatMap(blockAge => Either.cond(
       blockAge <= minerSettings.intervalAfterLastBlockThenGenerationIsAllowed,
       (),
@@ -44,29 +52,29 @@ class Miner(
     ))
 
   private def generateBlock(account: PrivateKeyAccount, parentHeight: Int, parent: Block, greatGrandParent: Option[Block], balance: Long): Callable[Option[Block]] = () => {
-    val pc = peerCount
+    val pc = allChannels.size()
     if (pc >= minerSettings.quorum) {
-        val lastBlockKernelData = parent.consensusData
-        val currentTime = time
-        val h = calcHit(lastBlockKernelData, account)
-        val t = calcTarget(parent, currentTime, balance)
-        if (h < t) {
-          log.debug(s"Forging with ${account.address}, H $h < T $t, balance $balance, prev block ${parent.uniqueId}")
-          log.debug(s"Previous block ID ${parent.encodedId} at $parentHeight with target ${lastBlockKernelData.baseTarget}")
+      val lastBlockKernelData = parent.consensusData
+      val currentTime = time.correctedTime()
+      val h = calcHit(lastBlockKernelData, account)
+      val t = calcTarget(parent, currentTime, balance)
+      if (h < t) {
+        log.debug(s"Forging with ${account.address}, H $h < T $t, balance $balance, prev block ${parent.uniqueId}")
+        log.debug(s"Previous block ID ${parent.uniqueId} at $parentHeight with target ${lastBlockKernelData.baseTarget}")
 
-          val avgBlockDelay = blockchainSettings.genesisSettings.averageBlockDelay
-          val btg = calcBaseTarget(avgBlockDelay, parentHeight, parent, greatGrandParent, currentTime)
-          val gs = calcGeneratorSignature(lastBlockKernelData, account)
-          val consensusData = NxtLikeConsensusBlockData(btg, gs)
+        val avgBlockDelay = settings.blockchainSettings.genesisSettings.averageBlockDelay
+        val btg = calcBaseTarget(avgBlockDelay, parentHeight, parent, greatGrandParent, currentTime)
+        val gs = calcGeneratorSignature(lastBlockKernelData, account)
+        val consensusData = NxtLikeConsensusBlockData(btg, gs)
 
-          val unconfirmed = packUnconfirmed(state, blockchainSettings.functionalitySettings, utx, time, parentHeight)
-          log.debug(s"Adding ${unconfirmed.size} unconfirmed transaction(s) to new block")
+        val unconfirmed = utx.packUnconfirmed()
+        log.debug(s"Adding ${unconfirmed.size} unconfirmed transaction(s) to new block")
 
-          Some(Block.buildAndSign(Version, currentTime, parent.uniqueId, consensusData, unconfirmed, account))
-        } else {
-          log.warn(s"Hit $h was NOT less than target $t, not forging block with ${account.address}")
-          None
-        }
+        Some(Block.buildAndSign(Version, currentTime, parent.uniqueId, consensusData, unconfirmed, account))
+      } else {
+        log.warn(s"Hit $h was NOT less than target $t, not forging block with ${account.address}")
+        None
+      }
     } else {
       log.debug(s"Quorum not available ($pc/${minerSettings.quorum}, not forging block with ${account.address}")
       None
@@ -74,22 +82,22 @@ class Miner(
   }
 
   private def retry(parentHeight: Int, parent: Block, greatGrandParent: Option[Block])(account: PrivateKeyAccount): Unit =
-    // Next block generation time can be None only when the account's generating balance is less than required. The
-    // balance, in turn, changes only when new block is appended to the blockchain, which causes all scheduled forging
-    // attempts to be cancelled and re-scheduled over the new parent.
+  // Next block generation time can be None only when the account's generating balance is less than required. The
+  // balance, in turn, changes only when new block is appended to the blockchain, which causes all scheduled forging
+  // attempts to be cancelled and re-scheduled over the new parent.
   {
     val calculatedTimestamp = for {
       _ <- checkAge(parentHeight, parent)
-      ts <- nextBlockGenerationTime(parentHeight, state, blockchainSettings.functionalitySettings, parent, account)
+      ts <- nextBlockGenerationTime(parentHeight, stateReader, settings.blockchainSettings.functionalitySettings, parent, account)
     } yield ts
 
     calculatedTimestamp match {
       case Left(e) => log.debug(s"NOT scheduling block generation: $e")
       case Right(ts) =>
         val generationInstant = Instant.ofEpochMilli(ts + 10)
-        val calculatedOffset = Duration.between(Instant.ofEpochMilli(time), generationInstant)
+        val calculatedOffset = Duration.between(Instant.ofEpochMilli(time.correctedTime()), generationInstant)
         val generationOffset = MinimalGenerationOffset.max(calculatedOffset)
-        val balance = generatingBalance(state, blockchainSettings.functionalitySettings, account, parentHeight)
+        val balance = generatingBalance(stateReader, settings.blockchainSettings.functionalitySettings, account, parentHeight)
 
         scheduledAttempts.compute(ByteStr(account.publicKey), { (key, prevTask) =>
           if (prevTask != null) {
@@ -103,7 +111,15 @@ class Miner(
 
             Futures.addCallback(thisAttempt, new FutureCallback[Option[Block]] {
               override def onSuccess(result: Option[Block]): Unit = result match {
-                case Some(block) => blockHandler(block)
+                case Some(block) =>
+                  CoordinatorHandler.loggingResult("local", s"applying locally mined block (${block.uniqueId})",
+                    Coordinator.processBlock(checkpoint, history, blockchainUpdater, time, stateReader,
+                      utx, blockchainReadiness, Miner.this, settings)(block, local = true)
+                      .map { score =>
+                        allChannels.broadcast(ScoreChanged(score))
+                        allChannels.broadcast(BlockForged(block))
+                        score
+                      })
                 case None =>
                   if (scheduledAttempts.remove(key, thisAttempt)) {
                     log.debug(s"No block generated: Retrying")
@@ -125,7 +141,7 @@ class Miner(
     scheduledAttempts.values.asScala.foreach(_.cancel(false))
     scheduledAttempts.clear()
     val greatGrandParent = history.parent(parent, 2)
-    privateKeyAccounts.foreach(retry(parentHeight, parent, greatGrandParent))
+    wallet.privateKeyAccounts().foreach(retry(parentHeight, parent, greatGrandParent))
   }
 
   def shutdown(): Unit = minerPool.shutdownNow()
