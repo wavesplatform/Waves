eded33af1ee58a53cf000435af738f25eda4ebe9
Alexey Kiselev <alexey.kiselev@gmail.com>
2017-07-04 11:28:22 +0300

    Compilation error fix

--- src/it/scala/com/wavesplatform/it/network/client/LegacyChannelInitializer.scala
+++ src/it/scala/com/wavesplatform/it/network/client/LegacyChannelInitializer.scala
@@ -5,12 +5,14 @@ import io.netty.channel.ChannelInitializer
 import io.netty.channel.socket.SocketChannel
 import io.netty.handler.codec.{LengthFieldBasedFrameDecoder, LengthFieldPrepender}
 
+import scala.concurrent.duration._
+
 class LegacyChannelInitializer(handshakeHandler: HandshakeHandler) extends ChannelInitializer[SocketChannel] {
   override def initChannel(ch: SocketChannel): Unit =
   ch.pipeline()
     .addLast(
       new HandshakeDecoder,
-      new HandshakeTimeoutHandler,
+      new HandshakeTimeoutHandler(30.seconds),
       handshakeHandler,
       new LengthFieldPrepender(4),
       new LengthFieldBasedFrameDecoder(1024*1024, 0, 4, 0, 4),
