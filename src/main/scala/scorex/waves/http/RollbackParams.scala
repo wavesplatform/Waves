57dcacd63380464405523cb9b318b0f9d608013f
Sergey Tolmachev <tolsi.ru@gmail.com>
2017-07-17 14:27:01 +0300

    NODE-51: code format

--- src/main/scala/scorex/waves/http/RollbackParams.scala
+++ src/main/scala/scorex/waves/http/RollbackParams.scala
@@ -5,4 +5,5 @@ import play.api.libs.json.{Format, Json}
 object RollbackParams {
   implicit val rollbackParamsReads: Format[RollbackParams] = Json.format
 }
+
 case class RollbackParams(rollbackTo: Int, returnTransactionsInUTX: Boolean)
