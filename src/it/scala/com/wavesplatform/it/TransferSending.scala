d3d1afaf8500c922c03712bab9f040dc01f20c2e
Alexey Kiselev <alexey.kiselev@gmail.com>
2017-08-01 17:49:29 +0300

    Integration test on wide (10k accounts) state was added

--- src/it/scala/com/wavesplatform/it/TransferSending.scala
+++ src/it/scala/com/wavesplatform/it/TransferSending.scala
@@ -2,19 +2,27 @@ package com.wavesplatform.it
 
 import com.wavesplatform.it.TransferSending.Req
 import com.wavesplatform.it.api.NodeApi.Transaction
+import scorex.account.{Address, AddressScheme}
 
 import scala.concurrent.Future
 import scala.util.Random
-import scala.concurrent.ExecutionContext.Implicits.global
 
 object TransferSending {
   case class Req(source: String, targetAddress: String, amount: Long, fee: Long)
 }
 
 trait TransferSending {
-  def nodes: Seq[Node]
 
-  def generateRequests(n: Int, balances: Map[String, Long]): Seq[Req] = {
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  AddressScheme.current = new AddressScheme {
+    override val chainId: Byte = 'I'.toByte
+  }
+
+  def nodes: Seq[Node]
+  private lazy val addressToNode = nodes.map(n => n.address -> n).toMap
+
+  def generateTransfersBetweenAccounts(n: Int, balances: Map[String, Long]): Seq[Req] = {
     val fee = 100000
     val addresses = nodes.map(_.address)
     val sourceAndDest = (1 to n).map { _ =>
@@ -30,12 +38,29 @@ trait TransferSending {
     requests
   }
 
+  def generateTransfersToRandomAddresses(n: Int, balances: Map[String, Long]): Seq[Req] = {
+    val fee = 100000
+    val seedSize = 32
+    val addresses = nodes.map(_.address)
+
+    val sourceAndDest = (1 to n).map { _ =>
+      val src = Random.shuffle(addresses).head
+      val pk = Array.fill[Byte](seedSize)(Random.nextInt(Byte.MaxValue).toByte)
+      val dst = Address.fromPublicKey(pk).address
+
+      (src, dst)
+    }
+    val requests = sourceAndDest.foldLeft(List.empty[Req]) {
+      case (rs, (src, dst)) =>
+        rs :+ Req(src, dst, fee, fee)
+    }
+
+    requests
+  }
+
   def balanceForNode(n: Node): Future[(String, Long)] = n.balance(n.address).map(b => b.address -> b.balance)
 
-  def makeTransfer(r: Req): Future[Transaction] = {
-    val addressToNode = nodes.map(n => n.address -> n).toMap
-    addressToNode(r.source).transfer(r.source, r.targetAddress, r.amount, r.fee)
-  }
+  def makeTransfer(r: Req): Future[Transaction] = addressToNode(r.source).transfer(r.source, r.targetAddress, r.amount, r.fee)
 
   def processRequests(requests: Seq[Req]): Future[Unit] = if (requests.isEmpty) {
     Future.successful(())
