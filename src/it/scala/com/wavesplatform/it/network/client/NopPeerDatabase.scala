62833f7dd78ff4a50fa5abe21db42ae12a8c1671
Ilya Smagin <ilya.smagin@gmail.com>
2017-08-08 15:33:20 +0300

    NODE-94: fix NopPeerDatabase

--- src/it/scala/com/wavesplatform/it/network/client/NopPeerDatabase.scala
+++ src/it/scala/com/wavesplatform/it/network/client/NopPeerDatabase.scala
@@ -7,8 +7,9 @@ import com.wavesplatform.network.PeerDatabase
 object NopPeerDatabase extends PeerDatabase {
   override def addCandidate(socketAddress: InetSocketAddress): Unit = {}
   override def touch(socketAddress: InetSocketAddress): Unit = {}
-  override def blacklist(host: InetAddress): Unit = {}
+  override def blacklist(host: InetAddress, reason: String): Unit = {}
   override def knownPeers: Map[InetSocketAddress, Long] = Map.empty
   override def blacklistedHosts: Set[InetAddress] = Set.empty
   override def randomPeer(excluded: Set[InetSocketAddress]): Option[InetSocketAddress] = None
+  override def detailedBlacklist: Map[InetAddress, (Long, String)] = Map.empty
 }
