d88924b1e7a1381ae90e5350347b87c4cfd74e74
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2017-08-22 15:11:13 +0300

    NODE-34 Removed outdated classes

--- src/main/scala/com/wavesplatform/network/MicroBlockSynchronizer.scala
+++ src/main/scala/com/wavesplatform/network/MicroBlockSynchronizer.scala
@@ -1,21 +1,14 @@
 package com.wavesplatform.network
 
-import java.util.concurrent._
-
-import com.google.common.cache.{Cache, CacheBuilder}
 import com.wavesplatform.job._
-import com.wavesplatform.network.MicroBlockSynchronizer.Settings
-import com.wavesplatform.network.MicroBlockSynchronizerNew.{Item, QueuedRequests}
+import com.wavesplatform.network.MicroBlockSynchronizer.{Item, QueuedRequests, Settings}
 import com.wavesplatform.state2.ByteStr
 import io.netty.channel.ChannelHandler.Sharable
 import io.netty.channel._
-import monix.execution.Cancelable
 import scorex.transaction.NgHistory
 import scorex.utils.ScorexLogging
 
-import scala.collection.immutable.{SortedSet, TreeMap}
 import scala.concurrent.duration.FiniteDuration
-import scala.util.{Failure, Success}
 
 @Sharable
 class MicroBlockSynchronizer(settings: Settings, history: NgHistory)
@@ -46,7 +39,10 @@ class MicroBlockSynchronizer(settings: Settings, history: NgHistory)
   }
 }
 
-private object MicroBlockSynchronizerNew {
+object MicroBlockSynchronizer {
+
+  case class Settings(waitResponseTimeout: FiniteDuration)
+
   case class Item(ctx: ChannelHandlerContext,
                   microBlockSig: ByteStr)
 
@@ -66,125 +62,6 @@ private object MicroBlockSynchronizerNew {
       val orig = new DelayedJobQueue[Request](waitResponseTimeout)(scheduler)
       new CachedJobQueue(orig, _.id)
     }
-  }
-}
-
-object MicroBlockSynchronizer {
-
-  type MicroBlockSignature = ByteStr
-
-  case class Settings(waitResponseTimeout: FiniteDuration)
-
-  private[network] class QueuedRequests(waitResponseTimeout: FiniteDuration) extends ScorexLogging {
-
-    // Possible issue: it has unbounded queue size
-    private val scheduler = monix.execution.Scheduler.singleThread("queued-requests")
-    private var info: Map[MicroBlockSignature, MicroBlockInfo] = Map.empty
-
-    private val processed = new ProcessedMicroblocks
-
-    def ask(ownerCtx: ChannelHandlerContext, sig: MicroBlockSignature): Unit = runInQueue {
-      if (!processed.has(sig)) {
-        val orig = info.getOrElse(sig, MicroBlockInfo.empty)
-        if (!orig.contains(ownerCtx)) {
-          val updated = if (orig.canDoNextRequest) {
-            orig.withNewMadeRequest(ownerCtx, newTask(ownerCtx, sig))
-          } else {
-            orig.withNewFreeCtx(ownerCtx)
-          }
-
-          info += sig -> updated
-        }
-      }
-    }
-
-    def complete(sig: MicroBlockSignature): Unit = runInQueue {
-      processed.add(sig)
-      info(sig).cancel()
-      info -= sig
-    }
-
-    protected def newRequest(ownerCtx: ChannelHandlerContext, sig: MicroBlockSignature): ChannelFuture = {
-      ownerCtx.writeAndFlush(MicroBlockRequest(sig))
-    }
-
-    private def newTask(ownerCtx: ChannelHandlerContext, sig: MicroBlockSignature) = Request(
-      sender = newRequest(ownerCtx, sig),
-      timeoutTimer = scheduleReRequest(ownerCtx, sig)
-    )
-
-    private def scheduleReRequest(ownerCtx: ChannelHandlerContext, sig: MicroBlockSignature): Cancelable = {
-      def reRequest(sig: MicroBlockSignature): Unit = info.get(sig).foreach { orig =>
-        val newMadeRequests = orig.madeRequests.updated(
-          ownerCtx,
-          orig.madeRequests(ownerCtx).copy(timedOut = true)
-        )
-
-        val updated = orig.freeOwnerCtxs.headOption match {
-          case Some(owner) =>
-            orig.copy(
-              freeOwnerCtxs = orig.freeOwnerCtxs - owner,
-              madeRequests = newMadeRequests.updated(owner.ctx, newTask(owner.ctx, sig))
-            )
-
-          case _ => orig.copy(
-            madeRequests = newMadeRequests
-          )
-        }
-
-        info += sig -> updated
-      }
-
-      scheduler.scheduleOnce(waitResponseTimeout)(reRequest(sig))
-    }
-
-    private def runInQueue(f: => Unit): Unit = monix.eval.Task(f).runOnComplete {
-      case Failure(e) => log.error("Failed to run in queue", e)
-      case Success(_) => log.info("test")
-    }(scheduler)
-  }
-
-  private case class MicroBlockInfo(freeOwnerCtxs: SortedSet[ChannelHandlerContext],
-                                    madeRequests: Map[ChannelHandlerContext, Request]) {
-    def canDoNextRequest: Boolean = madeRequests.values.forall(_.timedOut)
-    def cancel(): Unit = madeRequests.values.foreach(_.cancel())
-
-    // make tests for it
-    def contains(x: ChannelHandlerContext): Boolean = freeOwnerCtxs.contains(x) || madeRequests.contains(x)
-
-    def withNewMadeRequest(ownerCtx: ChannelHandlerContext, request: Request): MicroBlockInfo = {
-      copy(madeRequests = madeRequests.updated(ownerCtx, request))
-    }
-
-    def withNewFreeCtx(ownerCtx: ChannelHandlerContext): MicroBlockInfo = {
-      copy(freeOwnerCtxs = freeOwnerCtxs + ownerCtx)
-    }
-  }
-
-  private object MicroBlockInfo {
-    val empty = MicroBlockInfo(
-      freeOwnerCtxs = SortedSet.empty(Ordering.by[ChannelHandlerContext, String](_.name())),
-      madeRequests = TreeMap.empty(Ordering.by[ChannelHandlerContext, String](_.name()))
-    )
-  }
-
-  private case class Request(sender: ChannelFuture,
-                             timeoutTimer: Cancelable,
-                             timedOut: Boolean = false) {
-    def cancel(): Unit = {
-      timeoutTimer.cancel()
-      sender.cancel(false)
-    }
-  }
-
-  private class ProcessedMicroblocks {
-    private val dummy = new Object
-    private val processed: Cache[MicroBlockSignature, Object] = CacheBuilder.newBuilder()
-      .expireAfterWrite(1, TimeUnit.MINUTES)
-      .build[MicroBlockSignature, Object]()
-
-    def add(sig: MicroBlockSignature): Unit = processed.put(sig, dummy)
-    def has(sig: MicroBlockSignature): Boolean = Option(processed.getIfPresent(sig)).isDefined
   }
 
 }
