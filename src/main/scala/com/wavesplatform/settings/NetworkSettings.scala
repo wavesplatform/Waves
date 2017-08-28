314d5c498ae3938f26a564aa73909057fbe695eb
Alexey Kiselev <alexey.kiselev@gmail.com>
2017-08-28 16:49:36 +0300

    Throw IllegalArgumentException in case of incorrect byte size of node name in configuration file

--- src/main/scala/com/wavesplatform/settings/NetworkSettings.scala
+++ src/main/scala/com/wavesplatform/settings/NetworkSettings.scala
@@ -41,8 +41,9 @@ object NetworkSettings {
     val file = config.getAs[File]("file")
     val bindAddress = new InetSocketAddress(config.as[String]("bind-address"), config.as[Int]("port"))
     val nonce = config.getOrElse("nonce", randomNonce)
-    val nodeName = config.getAs[String]("node-name")
-      .filter(_.getBytes(Charsets.UTF_8).length <= MaxNodeNameBytesLength).getOrElse(s"Node-$nonce")
+    val nodeName = config.getOrElse("node-name", s"Node-$nonce")
+    require(nodeName.getBytes(Charsets.UTF_8).length <= MaxNodeNameBytesLength,
+      s"Node name should have length less than $MaxNodeNameBytesLength bytes")
     val declaredAddress = config.getAs[String]("declared-address").map { address =>
       val uri = new URI(s"my://$address")
       new InetSocketAddress(uri.getHost, uri.getPort)
