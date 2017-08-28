6939eb4c6ef862ea96e7ba91ebe905b56bc7aabd
Ilya Smagin <ilya.smagin@gmail.com>
2017-08-28 13:52:41 +0300

    NODE-34: time-out maps

--- src/main/scala/com/wavesplatform/network/MicroBlockSynchronizer.scala
+++ src/main/scala/com/wavesplatform/network/MicroBlockSynchronizer.scala
@@ -1,5 +1,8 @@
 package com.wavesplatform.network
 
+import java.util.concurrent.TimeUnit
+
+import com.google.common.cache.{Cache, CacheBuilder}
 import com.wavesplatform.network.MicroBlockSynchronizer._
 import com.wavesplatform.state2.ByteStr
 import io.netty.channel.ChannelHandler.Sharable
@@ -8,7 +11,7 @@ import monix.eval.Task
 import scorex.transaction.NgHistory
 import scorex.utils.ScorexLogging
 
-import scala.collection.mutable.{Map => MMap, Set => MSet}
+import scala.collection.mutable.{Set => MSet}
 import scala.concurrent.duration.FiniteDuration
 
 @Sharable
@@ -17,48 +20,48 @@ class MicroBlockSynchronizer(settings: Settings, history: NgHistory)
 
   private implicit val scheduler = monix.execution.Scheduler.singleThread("microblock-synchronizer")
 
-  def requestMicroblock(microBlockSig: MicroBlockSignature, attemptsAllowed: Int): Unit = if (attemptsAllowed > 0) {
-    val knownChannels = knownMicroblockOwners.getOrElse(microBlockSig, MSet.empty)
-    random(knownChannels) match {
-      case None => ()
-      case Some(ctx) =>
-        knownChannels -= ctx
-        ctx.writeAndFlush(MicroBlockRequest(microBlockSig))
-        awaitingMicroblocks += microBlockSig
-        Task { // still not received the microBlock
-          if (awaitingMicroblocks.contains(microBlockSig))
-            requestMicroblock(microBlockSig, attemptsAllowed - 1)
-        }.delayExecution(settings.waitResponseTimeout)
-          .runAsync
-    }
-  }
+  private val awaitingMicroBlocks = cache[MicroBlockSignature, Object](settings.invCacheTimeout)
+  private val knownMicroBlockOwners = cache[MicroBlockSignature, MSet[ChannelHandlerContext]](settings.invCacheTimeout)
+  private val successfullyRecievedMicroBlocks = cache[MicroBlockSignature, Object](settings.processedMicroBlocksCacheTimeout)
 
-  val knownMicroblockOwners: MMap[MicroBlockSignature, MSet[ChannelHandlerContext]] = MMap.empty
-  val awaitingMicroblocks: MSet[MicroBlockSignature] = MSet.empty
+  private def awaitingMicroBlockResponse(microBlockSig: MicroBlockSignature): Boolean = Option(awaitingMicroBlocks.getIfPresent(microBlockSig)).isDefined
+  private def alreadyProcessedMicroBlock(microBlockSig: MicroBlockSignature): Boolean = Option(successfullyRecievedMicroBlocks.getIfPresent(microBlockSig)).isDefined
+
+  def requestMicroBlock(microBlockSig: MicroBlockSignature, attemptsAllowed: Int): Unit =
+    if (attemptsAllowed > 0 && !alreadyProcessedMicroBlock(microBlockSig) && !awaitingMicroBlockResponse(microBlockSig)) {
+      val knownChannels = knownMicroBlockOwners.get(microBlockSig, () => MSet.empty)
+      random(knownChannels) match {
+        case Some(ctx) =>
+          knownChannels -= ctx
+          ctx.writeAndFlush(MicroBlockRequest(microBlockSig))
+          awaitingMicroBlocks.put(microBlockSig, dummy)
+          Task { // still not received the microBlock
+            if (!alreadyProcessedMicroBlock(microBlockSig))
+              requestMicroBlock(microBlockSig, attemptsAllowed - 1)
+          }.delayExecution(settings.waitResponseTimeout)
+            .runAsync
+        case None =>
+      }
+    }
+
 
   override def channelRead(ctx: ChannelHandlerContext, msg: AnyRef): Unit = msg match {
     case MicroBlockResponse(mb) => Task {
       log.trace(id(ctx) + "Received MicroBlockResponse " + mb)
-      knownMicroblockOwners -= mb.totalResBlockSig
-      awaitingMicroblocks -= mb.totalResBlockSig
+      knownMicroBlockOwners.invalidate(mb.totalResBlockSig)
+      awaitingMicroBlocks.invalidate(mb.totalResBlockSig)
+      successfullyRecievedMicroBlocks.put(mb.totalResBlockSig, dummy)
     }.runAsync
     case mi@MicroBlockInv(totalResBlockSig, prevResBlockSig) => Task {
       log.trace(id(ctx) + "Received " + mi)
       history.lastBlockId() match {
         case Some(lastBlockId) =>
           if (lastBlockId == prevResBlockSig) {
-            knownMicroblockOwners.get(totalResBlockSig) match {
-              case None => knownMicroblockOwners += (totalResBlockSig -> MSet(ctx))
-              case Some(set) => set += ctx
-            }
-            if (!awaitingMicroblocks.contains(totalResBlockSig)) {
-              requestMicroblock(totalResBlockSig, 2)
-            }
-          } else {
+            knownMicroBlockOwners.get(totalResBlockSig, () => MSet.empty) += ctx
+            requestMicroBlock(totalResBlockSig, 2)
+          } else
             log.trace(s"Discarding $mi because it doesn't match last (micro)block")
-          }
         case None =>
-          log.warn("History does not contain the last block!")
       }
     }.runAsync
     case _ => super.channelRead(ctx, msg)
@@ -79,4 +82,10 @@ object MicroBlockSynchronizer {
     if (ts.hasNext) Some(ts.next)
     else None
   }
+
+  def cache[K <: AnyRef, V<: AnyRef](timeout: FiniteDuration): Cache[K, V] = CacheBuilder.newBuilder()
+    .expireAfterWrite(timeout.toMillis, TimeUnit.MILLISECONDS)
+    .build[K, V]()
+
+  private val dummy = new Object()
 }
