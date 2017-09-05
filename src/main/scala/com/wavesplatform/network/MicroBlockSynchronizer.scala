a5ea9177b5705fc75d66f28f2e8560ad81320f3f
Ilya Smagin <ilya.smagin@gmail.com>
2017-09-05 16:54:41 +0300

    Revert "NODE-144: Restore microblock synchronization"
    This reverts commit 78086a5

--- src/main/scala/com/wavesplatform/network/MicroBlockSynchronizer.scala
+++ src/main/scala/com/wavesplatform/network/MicroBlockSynchronizer.scala
@@ -1,51 +1,95 @@
 package com.wavesplatform.network
 
+import java.util.concurrent.TimeUnit
+
+import com.google.common.cache.{Cache, CacheBuilder}
+import com.wavesplatform.network.MicroBlockSynchronizer._
+import com.wavesplatform.state2.ByteStr
 import io.netty.channel.ChannelHandler.Sharable
-import io.netty.channel.{Channel, ChannelHandlerContext, ChannelInboundHandlerAdapter}
+import io.netty.channel._
+import monix.eval.Task
 import kamon.Kamon
 import kamon.metric.instrument.Time
-import scorex.block.Block.BlockId
 import scorex.transaction.NgHistory
-import scorex.utils.{ScorexLogging, SynchronizedOne}
+import scorex.utils.ScorexLogging
+
+import scala.collection.mutable.{Set => MSet}
+import scala.concurrent.duration.FiniteDuration
 
 @Sharable
-class MircoBlockSynchronizer(history: NgHistory)
-  extends ChannelInboundHandlerAdapter with ScorexLogging with SynchronizedOne {
+class MicroBlockSynchronizer(settings: Settings, history: NgHistory) extends ChannelInboundHandlerAdapter with ScorexLogging {
 
-  private val awaitingResponse = Synchronized(scala.collection.mutable.Map.empty[BlockId, Channel])
+  private implicit val scheduler = monix.execution.Scheduler.singleThread("microblock-synchronizer")
+
+  private val awaitingMicroBlocks = cache[MicroBlockSignature, Object](settings.invCacheTimeout)
+  private val knownMicroBlockOwners = cache[MicroBlockSignature, MSet[ChannelHandlerContext]](settings.invCacheTimeout)
+  private val successfullyReceivedMicroBlocks = cache[MicroBlockSignature, Object](settings.processedMicroBlocksCacheTimeout)
+
+  private val microBlockCreationTime = cache[ByteStr, java.lang.Long](settings.invCacheTimeout)
+
+  private def alreadyRequested(microBlockSig: MicroBlockSignature): Boolean = Option(awaitingMicroBlocks.getIfPresent(microBlockSig)).isDefined
+
+  private def alreadyProcessed(microBlockSig: MicroBlockSignature): Boolean = Option(successfullyReceivedMicroBlocks.getIfPresent(microBlockSig)).isDefined
+
+  def requestMicroBlockTask(microBlockSig: MicroBlockSignature, attemptsAllowed: Int): Task[Unit] = Task {
+    if (attemptsAllowed > 0 && !alreadyProcessed(microBlockSig)) {
+      val knownChannels = knownMicroBlockOwners.get(microBlockSig, () => MSet.empty)
+      random(knownChannels) match {
+        case Some(ctx) =>
+          knownChannels -= ctx
+          ctx.writeAndFlush(MicroBlockRequest(microBlockSig))
+          awaitingMicroBlocks.put(microBlockSig, dummy)
+          requestMicroBlockTask(microBlockSig, attemptsAllowed - 1)
+            .delayExecution(settings.waitResponseTimeout)
+        case None => Task.unit
+      }
+    } else Task.unit
+  }.flatten
+
 
   override def channelRead(ctx: ChannelHandlerContext, msg: AnyRef): Unit = msg match {
-    case MicroBlockResponse(mb) =>
+    case MicroBlockResponse(mb) => Task {
       log.trace(id(ctx) + "Received MicroBlockResponse " + mb)
-      write { implicit l =>
-        awaitingResponse.mutate(_ -= mb.totalResBlockSig)
-      }
+      knownMicroBlockOwners.invalidate(mb.totalResBlockSig)
+      awaitingMicroBlocks.invalidate(mb.totalResBlockSig)
+      successfullyReceivedMicroBlocks.put(mb.totalResBlockSig, dummy)
 
-    case mi@MicroBlockInv(totalResBlockSig, prevResBlockSig, created) =>
+      Option(microBlockCreationTime.getIfPresent(mb.totalResBlockSig)).foreach { created =>
+        microBlockReceiveLagStats.record(System.currentTimeMillis() - created)
+        microBlockCreationTime.invalidate(mb.totalResBlockSig)
+      }
+    }.runAsync
+    case mi@MicroBlockInv(totalResBlockSig, prevResBlockSig, created) => Task {
       log.trace(id(ctx) + "Received " + mi)
       history.lastBlockId() match {
-        case Some(lbid) =>
-          if (lbid == prevResBlockSig) {
-            write { implicit l =>
-              awaitingResponse().get(totalResBlockSig) match {
-                case None =>
-                  awaitingResponse.mutate(_ += (totalResBlockSig -> ctx.channel()))
-                  ctx.writeAndFlush(MicroBlockRequest(totalResBlockSig))
-                case _ => // already requested
-              }
-            }
-          }
-          else {
-            log.trace(s"Discarding $mi because it doesn't match last (micro)block")
-          }
-        case None => ??? // Shouldn't happen
-      }
+        case Some(lastBlockId) =>
+          if (lastBlockId == prevResBlockSig) {
+            knownMicroBlockOwners.get(totalResBlockSig, () => MSet.empty) += ctx
 
+            microBlockInvStats.increment()
+            microBlockCreationTime.put(totalResBlockSig, created)
+
+            if (alreadyRequested(totalResBlockSig)) Task.unit
+            else requestMicroBlockTask(totalResBlockSig, 2)
+          } else {
+            notLastMicroblockStats.increment()
+            log.trace(s"Discarding $mi because it doesn't match last (micro)block")
+            Task.unit
+          }
+
+        case None =>
+          unknownMicroblockStats.increment()
+          Task.unit
+      }
+    }.flatten.runAsync
     case _ => super.channelRead(ctx, msg)
   }
 }
 
-object MircoBlockSynchronizer {
+object MicroBlockSynchronizer {
+
+  type MicroBlockSignature = ByteStr
+
   private val microBlockInvStats = Kamon.metrics.registerCounter("micro-inv")
   private val microBlockReceiveLagStats = Kamon.metrics.registerHistogram(
     name = "micro-receive-lag",
@@ -54,4 +98,21 @@ object MircoBlockSynchronizer {
 
   private val notLastMicroblockStats = Kamon.metrics.registerCounter("micro-not-last")
   private val unknownMicroblockStats = Kamon.metrics.registerCounter("micro-unknown")
+
+  case class Settings(waitResponseTimeout: FiniteDuration,
+                      processedMicroBlocksCacheTimeout: FiniteDuration,
+                      invCacheTimeout: FiniteDuration)
+
+  def random[T](s: MSet[T]): Option[T] = {
+    val n = util.Random.nextInt(s.size)
+    val ts = s.iterator.drop(n)
+    if (ts.hasNext) Some(ts.next)
+    else None
+  }
+
+  def cache[K <: AnyRef, V <: AnyRef](timeout: FiniteDuration): Cache[K, V] = CacheBuilder.newBuilder()
+    .expireAfterWrite(timeout.toMillis, TimeUnit.MILLISECONDS)
+    .build[K, V]()
+
+  private val dummy = new Object()
 }
