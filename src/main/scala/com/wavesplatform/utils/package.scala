8db8926c110557eaf03cbaa4e17409cc4e0b9c3f
Yuriy Naydenov <ebceu44@gmail.com>
2017-09-06 19:01:33 +0300

    NODE-112-errors-when-opening-wallet-file

--- src/main/scala/com/wavesplatform/utils/package.scala
+++ src/main/scala/com/wavesplatform/utils/package.scala
@@ -1,6 +1,6 @@
 package com.wavesplatform
 
-import java.io.File
+import java.io.{File, IOException}
 import java.nio.file.Files
 
 import org.h2.mvstore.MVStore
@@ -15,20 +15,29 @@ package object utils extends ScorexLogging {
   def createMVStore(file: Option[File], encryptionKey: Option[Array[Char]] = None): MVStore = {
     val builder = file.fold(new MVStore.Builder) { p =>
       p.getParentFile.mkdirs()
+
+      if(!p.canWrite || !p.canRead)
+        throw new IOException(s"Insufficient access rights to file: ${p.getCanonicalPath}" )
+
       new MVStore.Builder()
         .fileName(p.getCanonicalPath)
         .autoCommitDisabled()
         .compress()
     }
 
-    val store = encryptionKey match {
-      case Some(key) => builder.encryptionKey(key).open()
-      case _ => builder.open()
+    try {
+      val store = encryptionKey match {
+        case Some(key) => builder.encryptionKey(key).open()
+        case _ => builder.open()
+      }
+
+      store.rollback()
+      store
     }
-
-    store.rollback()
-
-    store
+    catch {
+      case e: IllegalStateException if e.getMessage.contains("corrupt") =>
+        throw new IllegalStateException("wallet.password is incorrect or " + e.getMessage)
+    }
   }
 
   def createWithStore[A <: AutoCloseable](storeFile: Option[File], f: => A, pred: A => Boolean, deleteExisting: Boolean = false): Try[A] = Try {
