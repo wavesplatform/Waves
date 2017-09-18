e1472d6837729a4a83e99cabeb22e9a6a3162fac
Alexey Kiselev <alexey.kiselev@gmail.com>
2017-09-18 15:41:26 +0300

    FeatureStatus enumeration improved

--- src/main/scala/com/wavesplatform/features/FeatureStatus.scala
+++ src/main/scala/com/wavesplatform/features/FeatureStatus.scala
@@ -1,11 +1,25 @@
 package com.wavesplatform.features
 
-sealed case class FeatureStatus(status: Byte)
+sealed trait FeatureStatus {
+  def status: Byte
+}
+
+case object FeatureDefined extends FeatureStatus {
+  override val status = 0
+}
+
+case object FeatureAccepted extends FeatureStatus {
+  override val status = 1
+}
+
+case object FeatureActivated extends FeatureStatus {
+  override val status = 2
+}
 
 object FeatureStatus {
-  object Defined extends FeatureStatus(0)
-  object Accepted extends FeatureStatus(1)
-  object Activated extends FeatureStatus(2)
-
-  val values = Seq(Defined, Accepted, Activated)
+  def apply(status: Byte): FeatureStatus = status match {
+    case 1 => FeatureAccepted
+    case 2 => FeatureActivated
+    case _ => FeatureDefined
+  }
 }
