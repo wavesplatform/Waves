02b8a46c2cd589c4e6eb372d3e0e7f49fa252cc2
Yuriy Naydenov <ebceu44@gmail.com>
2017-09-19 11:58:12 +0300

    NODE-174 review fixes

--- src/test/scala/scorex/settings/TestFunctionalitySettings.scala
+++ src/test/scala/scorex/settings/TestFunctionalitySettings.scala
@@ -17,17 +17,5 @@ object TestFunctionalitySettings {
     allowLeasedBalanceTransferUntil = 0L
   )
 
-   val Stub = FunctionalitySettings(
-    featureCheckBlocksPeriod = 100,
-    blocksForFeatureActivation = 90,
-    allowTemporaryNegativeUntil = 0L, allowInvalidPaymentTransactionsByTimestamp = 0L,
-    requireSortedTransactionsAfter = 0L, generationBalanceDepthFrom50To1000AfterHeight = 0L,
-    minimalGeneratingBalanceAfter = 0L,
-    allowTransactionsFromFutureUntil = Long.MaxValue, allowUnissuedAssetsUntil = 0L,
-    allowBurnTransactionAfter = 0L, allowLeaseTransactionAfter = 0L,
-    requirePaymentUniqueIdAfter = 0L, allowExchangeTransactionAfter = 0L,
-    allowInvalidReissueInSameBlockUntilTimestamp = 0L, allowCreatealiasTransactionAfter = 0L,
-    allowMultipleLeaseCancelTransactionUntilTimestamp = 0L, resetEffectiveBalancesAtHeight = 0,
-    allowLeasedBalanceTransferUntil = 0L
-  )
+   val Stub = Enabled.copy(featureCheckBlocksPeriod = 100, blocksForFeatureActivation = 90)
 }
