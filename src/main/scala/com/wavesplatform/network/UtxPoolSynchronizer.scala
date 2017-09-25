a0cb4264af1c0fbcd01882a42be5a58020e1c3cf
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2017-09-25 17:37:29 +0300

    NODE-185 NetworkSender - additional logging and flush at the end (not after each message)

--- src/main/scala/com/wavesplatform/network/UtxPoolSynchronizer.scala
+++ src/main/scala/com/wavesplatform/network/UtxPoolSynchronizer.scala
@@ -19,15 +19,15 @@ class UtxPoolSynchronizer(utx: UtxPool, allChannels: ChannelGroup)
   override def channelRead(ctx: ChannelHandlerContext, msg: AnyRef): Unit = msg match {
     case t: Transaction => Task(utx.putIfNew(t) match {
       case Left(TransactionValidationError(e, _)) =>
-          log.trace(s"==> ${id(ctx)} Error processing transaction ${t.id}: $e")
+    //      log.trace(s"${id(ctx)} Error processing transaction ${t.id}: $e")
       case Left(e) =>
-        log.trace(s"==> ${id(ctx)} Error processing transaction ${t.id}: $e")
+    //    log.trace(s"${id(ctx)} Error processing transaction ${t.id}: $e")
       case Right(true) =>
         allChannels.broadcast(RawBytes(TransactionMessageSpec.messageCode, t.bytes), Some(ctx.channel()))
-        log.trace(s"==> ${id(ctx)} Added transaction ${t.id} to UTX pool")
+     //   log.trace(s"${id(ctx)} Added transaction ${t.id} to UTX pool")
       case Right(false) =>
-        log.trace(s"==> ${id(ctx)} TX ${t.id} already known")
-    }).runAsync(executor)
+     //   log.trace(s"${id(ctx)} TX ${t.id} already known")
+    }).runAsync
     case _ => super.channelRead(ctx, msg)
   }
 }
--- src/main/scala/com/wavesplatform/network/client/NetworkSender.scala
+++ src/main/scala/com/wavesplatform/network/client/NetworkSender.scala
@@ -1,18 +1,19 @@
 package com.wavesplatform.network.client
 
+import java.io.IOException
 import java.net.InetSocketAddress
 import java.nio.channels.ClosedChannelException
+import java.util.concurrent.atomic.AtomicLong
 
 import com.wavesplatform.network.RawBytes
 import io.netty.channel.Channel
 import io.netty.channel.group.DefaultChannelGroup
 import io.netty.util.concurrent.GlobalEventExecutor
+import scorex.utils.ScorexLogging
 
 import scala.concurrent.{Future, Promise}
 
-class NetworkSender(chainId: Char, name: String, nonce: Long) {
-
-  import scala.concurrent.ExecutionContext.Implicits.global
+class NetworkSender(chainId: Char, name: String, nonce: Long) extends ScorexLogging {
 
   private val allChannels = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE)
   private val client = new NetworkClient(chainId, name, nonce, allChannels)
@@ -21,13 +22,26 @@ class NetworkSender(chainId: Char, name: String, nonce: Long) {
     client.connect(address)
   }
 
-  def send(channel: Channel, messages: RawBytes*): Future[Seq[Unit]] = {
+  def send(channel: Channel, messages: RawBytes*): Future[Unit] = {
     if (channel.isOpen) {
-      Future.traverse(messages) { msg =>
-        val p = Promise[Unit]
-        channel.writeAndFlush(msg).addListener((_: io.netty.util.concurrent.Future[Void]) => p.success(()))
-        p.future
+      val p = Promise[Unit]
+      val counter = new AtomicLong(messages.size)
+
+      messages.foreach { msg =>
+        channel.write(msg).addListener { (f: io.netty.util.concurrent.Future[Void]) =>
+          if (!f.isSuccess) {
+            val cause = Option(f.cause()).getOrElse(new IOException("Can't send a message to the channel"))
+            log.error(s"Can't send a message to the channel: $msg", cause)
+          }
+
+          if (counter.decrementAndGet() == 0) {
+            p.success(())
+          }
+        }
       }
+      channel.flush()
+
+      p.future
     } else {
       Future.failed(new ClosedChannelException)
     }
