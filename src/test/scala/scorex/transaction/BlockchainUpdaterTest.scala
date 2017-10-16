d9c4f777d529d7a79136b8cc99f81d167beecca8
Yuriy Naydenov <ebceu44@gmail.com>
2017-10-16 09:48:30 +0300

    NODE-247 shutdown test

--- src/test/scala/scorex/transaction/BlockchainUpdaterTest.scala
+++ src/test/scala/scorex/transaction/BlockchainUpdaterTest.scala
@@ -1,5 +1,8 @@
 package scorex.transaction
 
+import java.security.Permission
+import java.util.concurrent.{Semaphore, TimeUnit}
+
 import com.wavesplatform.features.BlockchainFeatureStatus
 import com.wavesplatform.state2._
 import com.wavesplatform.history._
@@ -7,6 +10,8 @@ import com.wavesplatform.state2.diffs.produce
 import org.scalatest.words.ShouldVerb
 import org.scalatest.{FunSuite, Matchers}
 import scorex.block.Block
+import scorex.transaction.ValidationError.GenericError
+import sun.misc.{Signal, SignalHandler}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -210,7 +215,25 @@ class BlockchainUpdaterTest extends FunSuite with Matchers with HistoryTest with
     fp.featureStatus(1, h.height()) shouldBe BlockchainFeatureStatus.Approved
   }
 
-  test("block processing should fail if unimplemented feature was activated on blockchaing when autoShutdownOnUnsupportedFeature = yes") {
+  test("block processing should fail if unimplemented feature was activated on blockchaing when autoShutdownOnUnsupportedFeature = yes and exit with code 38") {
+
+    val signal = new Semaphore(1)
+    signal.acquire()
+
+    System.setSecurityManager(new SecurityManager {
+      override def checkPermission(perm: Permission): Unit = {}
+
+      override def checkPermission(perm: Permission, context: Object): Unit = {}
+
+      override def checkExit(status: Int): Unit = signal.synchronized {
+        super.checkExit(status)
+        if(status == 38)
+          signal.release()
+        throw new SecurityException("System exit not allowed")
+      }
+    })
+
+
     val (h, fp, _, _, bu, _) = StorageFactory(WavesSettings).get
     bu.processBlock(genesisBlock)
 
@@ -219,6 +242,10 @@ class BlockchainUpdaterTest extends FunSuite with Matchers with HistoryTest with
     }
 
     bu.processBlock(getNextTestBlockWithVotes(h, Set(-1))) should produce("ACTIVATED ON BLOCKCHAIN")
+
+    signal.tryAcquire(10, TimeUnit.SECONDS)
+
+    System.setSecurityManager(null)
   }
 
   test("sunny day test when known feature activated") {
