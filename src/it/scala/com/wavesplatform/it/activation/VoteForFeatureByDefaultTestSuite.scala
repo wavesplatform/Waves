dc106d10bf2b74561098482c2ddd4e25db2175ac
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2017-11-17 12:06:34 +0300

    NODE-348 VoteForFeatureByDefaultTestSuite - fixed quorum settings

--- src/it/scala/com/wavesplatform/it/activation/VoteForFeatureByDefaultTestSuite.scala
+++ src/it/scala/com/wavesplatform/it/activation/VoteForFeatureByDefaultTestSuite.scala
@@ -70,20 +70,22 @@ class VoteForFeatureByDefaultTestSuite extends FreeSpec with Matchers with Befor
     val nonVotingFeatureNum: Short = 2
 
     private val baseConfig = ConfigFactory.parseString(
-      s"""waves.blockchain.custom {
-         |  functionality {
-         |    pre-activated-features = {}
-         |    feature-check-blocks-period = $votingInterval
-         |    blocks-for-feature-activation = $blocksForActivation
-         |  }
-         |  genesis {
-         |    signature: "zXBp6vpEHgtdsPjVHjSEwMeRiQTAu6DdX3qkJaCRKxgYJk26kazS2XguLYRvL9taHKxrZHNNA7X7LMVFavQzWpT"
-         |    transactions = [
-         |      {recipient: "3Hm3LGoNPmw1VTZ3eRA2pAfeQPhnaBm6YFC", amount: 250000000000000},
-         |      {recipient: "3HZxhQhpSU4yEGJdGetncnHaiMnGmUusr9s", amount: 270000000000000},
-         |      {recipient: "3HPG313x548Z9kJa5XY4LVMLnUuF77chcnG", amount: 260000000000000},
-         |      {recipient: "3HVW7RDYVkcN5xFGBNAUnGirb5KaBSnbUyB", amount: 2000000000000}
-         |    ]
+      s"""waves {
+         |  blockchain.custom {
+         |    functionality {
+         |      pre-activated-features = {}
+         |      feature-check-blocks-period = $votingInterval
+         |      blocks-for-feature-activation = $blocksForActivation
+         |    }
+         |    genesis {
+         |      signature: "zXBp6vpEHgtdsPjVHjSEwMeRiQTAu6DdX3qkJaCRKxgYJk26kazS2XguLYRvL9taHKxrZHNNA7X7LMVFavQzWpT"
+         |      transactions = [
+         |        {recipient: "3Hm3LGoNPmw1VTZ3eRA2pAfeQPhnaBm6YFC", amount: 250000000000000},
+         |        {recipient: "3HZxhQhpSU4yEGJdGetncnHaiMnGmUusr9s", amount: 270000000000000},
+         |        {recipient: "3HPG313x548Z9kJa5XY4LVMLnUuF77chcnG", amount: 260000000000000},
+         |        {recipient: "3HVW7RDYVkcN5xFGBNAUnGirb5KaBSnbUyB", amount: 2000000000000}
+         |      ]
+         |    }
          |  }
          |  miner.quorum = 3
          |}""".stripMargin
