a9f8e47e7631d0a4dc4f731f90d655eb811569d4
Ilya Smagin <ilya.smagin@gmail.com>
2017-11-23 21:44:30 +0300

    observables: DiscardingHandler

--- src/main/scala/com/wavesplatform/network/DiscardingHandler.scala
+++ src/main/scala/com/wavesplatform/network/DiscardingHandler.scala
@@ -9,11 +9,11 @@ import scorex.utils.ScorexLogging
 @Sharable
 class DiscardingHandler(blockchainReadiness: Observable[Boolean]) extends ChannelDuplexHandler with ScorexLogging {
 
-  private implicit val scheduler: SchedulerService = monix.execution.Scheduler.fixedPool("discarding-handler", 4)
+  private implicit val scheduler: SchedulerService = monix.execution.Scheduler.fixedPool("discarding-handler", 2)
   private val lastReadiness = lastSeen(blockchainReadiness)
 
   override def channelRead(ctx: ChannelHandlerContext, msg: AnyRef): Unit = msg match {
-    case RawBytes(code, _) if code == TransactionMessageSpec.messageCode && lastReadiness().contains(true) =>
+    case RawBytes(code, _) if code == TransactionMessageSpec.messageCode && !lastReadiness().contains(true) =>
       log.trace(s"${id(ctx)} Discarding incoming message $code")
     case _ => super.channelRead(ctx, msg)
   }
