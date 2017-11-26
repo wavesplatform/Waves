2016e79e6fd05e6c8bd656eb4994151700b83e89
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2017-11-27 00:45:43 +0300

    NODE-361 Fix

--- src/main/scala/com/wavesplatform/network/OptimisticExtensionLoader.scala
+++ src/main/scala/com/wavesplatform/network/OptimisticExtensionLoader.scala
@@ -20,7 +20,8 @@ class OptimisticExtensionLoader extends ChannelDuplexHandler with ScorexLogging 
 
   override def channelRead(ctx: ChannelHandlerContext, msg: AnyRef): Unit = msg match {
     case ExtensionBlocks(extension) if extension.isEmpty =>
-      stopLoading(ctx)
+      requestedLocalIds = Seq.empty
+      hopefullyNextIds = Seq.empty
       log.debug(s"${id(ctx)} Blockchain is up to date")
       super.channelRead(ctx, msg)
     case ExtensionBlocks(extension) if requestedLocalIds.isEmpty =>
@@ -37,7 +38,9 @@ class OptimisticExtensionLoader extends ChannelDuplexHandler with ScorexLogging 
 
   override def write(ctx: ChannelHandlerContext, msg: AnyRef, promise: ChannelPromise): Unit = msg match {
     case LoadBlockchainExtension(Seq()) =>
-      stopLoading(ctx)
+      hopefullyNextIds = Seq.empty
+      nextExtensionBlocks = Seq.empty
+      requestedLocalIds = Seq.empty
       super.write(ctx, msg, promise)
 
     case LoadBlockchainExtension(localIds) =>
@@ -55,11 +58,5 @@ class OptimisticExtensionLoader extends ChannelDuplexHandler with ScorexLogging 
       nextExtensionBlocks = Seq.empty
 
     case _ => super.write(ctx, msg, promise)
-  }
-
-  private def stopLoading(ctx: ChannelHandlerContext): Unit = {
-    hopefullyNextIds = Seq.empty
-    nextExtensionBlocks = Seq.empty
-    requestedLocalIds = Seq.empty
   }
 }
