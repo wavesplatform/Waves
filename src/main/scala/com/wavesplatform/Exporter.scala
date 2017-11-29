07b3d16b5affc0a96ea2fbd6d0fe367ed86ab0ba
Alexey Kiselev <alexey.kiselev@gmail.com>
2017-11-29 16:19:53 +0300

    Import/Export of blockchain binary file

---
+++ src/main/scala/com/wavesplatform/Exporter.scala
@@ -0,0 +1,70 @@
+package com.wavesplatform
+
+import java.io.{BufferedOutputStream, File, FileOutputStream}
+import java.util.concurrent.locks.ReentrantReadWriteLock
+
+import com.google.common.primitives.Ints
+import com.typesafe.config.ConfigFactory
+import com.wavesplatform.history.HistoryWriterImpl
+import com.wavesplatform.settings.{WavesSettings, loadConfig}
+import com.wavesplatform.utils._
+import org.slf4j.bridge.SLF4JBridgeHandler
+import scorex.account.AddressScheme
+import scorex.utils.ScorexLogging
+
+import scala.util.{Failure, Success, Try}
+
+object Exporter extends ScorexLogging {
+  def main(args: Array[String]): Unit = {
+    SLF4JBridgeHandler.removeHandlersForRootLogger()
+    SLF4JBridgeHandler.install()
+
+    val configFilename = Try(args(0)).toOption.getOrElse("waves-testnet.conf")
+    val outputFilenamePrefix = Try(args(1)).toOption.getOrElse("blockchain")
+    val exportHeight = Try(args(2)).toOption.flatMap(s => Try(s.toInt).toOption)
+
+    val settings = WavesSettings.fromConfig(loadConfig(ConfigFactory.parseFile(new File(configFilename))))
+    AddressScheme.current = new AddressScheme {
+      override val chainId: Byte = settings.blockchainSettings.addressSchemeCharacter.toByte
+    }
+
+    val tryHistory = HistoryWriterImpl(settings.blockchainSettings.blockchainFile, new ReentrantReadWriteLock(true), settings.blockchainSettings.functionalitySettings, settings.featuresSettings)
+    tryHistory match {
+      case Success(history) =>
+        val blockchainHeight = history.height()
+        val height = Math.min(blockchainHeight, exportHeight.getOrElse(blockchainHeight))
+        log.info(s"Blockchain height is $blockchainHeight exporting to $height")
+        val outputFilename = s"$outputFilenamePrefix-$height"
+        log.info(s"Output file: $outputFilename")
+
+        createOutputStream(outputFilename) match {
+          case Success(output) =>
+            var exportedBytes = 0
+            val bos = new BufferedOutputStream(output)
+            val start = System.currentTimeMillis()
+            (2 to height).foreach { h =>
+              val maybeBlockBytes = history.blockBytes(h)
+              maybeBlockBytes.foreach { bytes =>
+                val len = bytes.length
+                bos.write(Ints.toByteArray(len))
+                bos.write(bytes)
+                exportedBytes += len + Ints.BYTES
+                if (h % (height / 10) == 0)
+                  log.info(s"$h blocks exported, ${humanReadableSize(exportedBytes)} written")
+              }
+            }
+            val duration = System.currentTimeMillis() - start
+            log.info(s"Finished exporting $height blocks in ${humanReadableDuration(duration)}, ${humanReadableSize(exportedBytes)} written")
+            bos.close()
+            output.close()
+          case Failure(ex) => log.error(s"Failed to create file '$outputFilename': $ex")
+        }
+      case Failure(ex) => log.error(s"Failed to open history at '${settings.blockchainSettings.blockchainFile}': $ex")
+    }
+  }
+
+  def createOutputStream(filename: String): Try[FileOutputStream] =
+    Try {
+      new FileOutputStream(filename)
+    }
+}
---
+++ src/main/scala/com/wavesplatform/Importer.scala
@@ -0,0 +1,69 @@
+package com.wavesplatform
+
+import java.io._
+
+import com.google.common.primitives.Ints
+import com.typesafe.config.ConfigFactory
+import com.wavesplatform.history.StorageFactory
+import com.wavesplatform.settings.{WavesSettings, loadConfig}
+import com.wavesplatform.utils._
+import org.slf4j.bridge.SLF4JBridgeHandler
+import scorex.account.AddressScheme
+import scorex.block.Block
+import scorex.utils.ScorexLogging
+
+import scala.util.{Failure, Success, Try}
+
+object Importer extends ScorexLogging {
+  def main(args: Array[String]): Unit = {
+
+    SLF4JBridgeHandler.removeHandlersForRootLogger()
+    SLF4JBridgeHandler.install()
+
+    val configFilename = Try(args(0)).toOption.getOrElse("waves-testnet.conf")
+    val settings = WavesSettings.fromConfig(loadConfig(ConfigFactory.parseFile(new File(configFilename))))
+    AddressScheme.current = new AddressScheme {
+      override val chainId: Byte = settings.blockchainSettings.addressSchemeCharacter.toByte
+    }
+
+    Try(args(1)) match {
+      case Success(filename) =>
+        log.info(s"Loading file '$filename")
+        createInputStream(filename) match {
+          case Success(inputStream) =>
+            val (history, _, stateWriter, _, blockchainUpdater, _) = StorageFactory(settings).get
+            checkGenesis(history, settings, blockchainUpdater)
+            val bis = new BufferedInputStream(inputStream)
+            var quit = false
+            val lenBytes = new Array[Byte](Ints.BYTES)
+            val start = System.currentTimeMillis()
+            while (!quit) {
+              val red = bis.read(lenBytes)
+              if (red == Ints.BYTES) {
+                val len = Ints.fromByteArray(lenBytes)
+                val buffer = new Array[Byte](len)
+                val s2 = bis.read(buffer)
+                if (s2 == len) {
+                  val block = Block.parseBytes(buffer).get
+                  blockchainUpdater.processBlock(block)
+                } else quit = true
+              } else quit = true
+            }
+            bis.close()
+            inputStream.close()
+            stateWriter.close()
+            history.close()
+            val duration = System.currentTimeMillis() - start
+            log.info(s"Imported in ${humanReadableDuration(duration)}")
+          case Failure(ex) => log.error(s"Failed to open file '$filename")
+        }
+      case Failure(ex) => log.error(s"Failed to get input filename from second parameter: $ex")
+    }
+  }
+
+  def createInputStream(filename: String): Try[FileInputStream] =
+    Try {
+      new FileInputStream(filename)
+    }
+
+}
