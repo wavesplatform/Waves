ce6faa4e63bd08530645bf5f4aba61cb4bed5c69
Alexey Kiselev <alexey.kiselev@gmail.com>
2017-12-15 11:04:45 +0300

    Conflicts resolved after rebase

--- src/main/scala/com/wavesplatform/state2/reader/StateReaderImpl.scala
+++ src/main/scala/com/wavesplatform/state2/reader/StateReaderImpl.scala
@@ -11,77 +11,79 @@ import scorex.transaction.{Transaction, TransactionParser}
 
 class StateReaderImpl(p: StateStorage, val synchronizationToken: ReentrantReadWriteLock) extends SnapshotStateReader {
 
-  override def transactionInfo(id: ByteStr): Option[(Int, Transaction)] = read { implicit l =>
-    p.getTransaction(id).map {
-      case (h, bytes) => (h, TransactionParser.parseBytes(bytes).get)
+  val sp = Synchronized(p)
+
+  override def transactionInfo(id: ByteStr): Option[(Int, Option[Transaction])] = read { implicit l =>
+    sp().getTransaction(id).map {
+      case (h, bytes) => (h, if (bytes.length == 0) None else TransactionParser.parseBytes(bytes).toOption)
     }
   }
 
   override def accountPortfolio(a: Address): Portfolio = read { implicit l =>
-    val waves = p.getWavesBalance(a).map { case (b, li, lo) => Portfolio(b, LeaseInfo(li, lo), Map.empty) }.orEmpty
-    val assets = p.getAssetBalanceMap(a).map { f => Portfolio(0, LeaseInfo.empty, f) }.orEmpty
+    val waves = sp().getWavesBalance(a).map { case (b, li, lo) => Portfolio(b, LeaseInfo(li, lo), Map.empty) }.orEmpty
+    val assets = sp().getAssetBalanceMap(a).map { f => Portfolio(0, LeaseInfo.empty, f) }.orEmpty
 
     Monoid.combine(waves, assets)
   }
 
   override def assetInfo(id: ByteStr): Option[AssetInfo] = read { implicit l =>
-    p.getAssetInfo(id)
+    sp().getAssetInfo(id)
   }
 
-  override def height: Int = read { implicit l => p.getHeight }
+  override def height: Int = read { implicit l => sp().getHeight }
 
   override def accountTransactionIds(a: Address, limit: Int): Seq[ByteStr] = read { implicit l =>
-    val totalRecords = p.getAccountTransactionsLengths(a).getOrElse(0)
+    val totalRecords = sp().getAccountTransactionsLengths(a).getOrElse(0)
     Range(Math.max(0, totalRecords - limit), totalRecords)
-      .map(n => p.getAccountTransactionIds(a, n).get)
+      .map(n => sp().getAccountTransactionIds(a, n).get)
       .reverse
   }
 
   override def paymentTransactionIdByHash(hash: ByteStr): Option[ByteStr] = read { implicit l =>
-    p.getPaymentTransactionHashes(hash)
+    sp().getPaymentTransactionHashes(hash)
   }
 
   override def aliasesOfAddress(address: Address): Seq[Alias] = read { implicit l =>
-    p.getAddressAliases(address).getOrElse(Seq.empty)
+    sp().getAddressAliases(address).getOrElse(Seq.empty)
   }
 
   override def resolveAlias(alias: Alias): Option[Address] = read { implicit l =>
-    p.getAddressOfAlias(alias)
+    sp().getAddressOfAlias(alias)
   }
 
   override def accountPortfolios: Map[Address, Portfolio] = read { implicit l =>
-    p.allPortfolios
+    sp().allPortfolios
   }
 
   override def isLeaseActive(leaseTx: LeaseTransaction): Boolean = read { implicit l =>
-    p.getLeaseState(leaseTx.id()).getOrElse(false)
+    sp().getLeaseState(leaseTx.id()).getOrElse(false)
   }
 
   override def activeLeases(): Seq[ByteStr] = read { implicit l =>
-    p.getActiveLeases.getOrElse(Seq.empty[ByteStr])
+    sp().getActiveLeases.getOrElse(Seq.empty[ByteStr])
   }
 
   override def lastUpdateHeight(acc: Address): Option[Int] = read { implicit l =>
-    p.getLastBalanceSnapshotHeight(acc)
+    sp().getLastBalanceSnapshotHeight(acc)
   }
 
   override def snapshotAtHeight(acc: Address, h: Int): Option[Snapshot] = read { implicit l =>
-    p.getBalanceSnapshots(acc, h).map { case (ph, b, eb) => Snapshot(ph, b, eb) }
+    sp().getBalanceSnapshots(acc, h).map { case (ph, b, eb) => Snapshot(ph, b, eb) }
   }
 
   override def containsTransaction(id: ByteStr): Boolean = read { implicit l =>
-    p.getTransaction(id).isDefined
+    sp().getTransaction(id).isDefined
   }
 
   override def filledVolumeAndFee(orderId: ByteStr): OrderFillInfo = read { implicit l =>
-    p.getOrderFills(orderId).getOrElse(Monoid[OrderFillInfo].empty)
+    sp().getOrderFills(orderId).getOrElse(Monoid[OrderFillInfo].empty)
   }
 
   override def wavesBalance(a: Address): (Long, LeaseInfo) = read { implicit l =>
-    p.getWavesBalance(a).map { case (v1, v2, v3) => (v1, LeaseInfo(v2, v3)) }.getOrElse((0L, LeaseInfo(0L, 0L)))
+    sp().getWavesBalance(a).map { case (v1, v2, v3) => (v1, LeaseInfo(v2, v3)) }.getOrElse((0L, LeaseInfo(0L, 0L)))
   }
 
   override def assetBalance(a: Address, asset: ByteStr): Long = read { implicit l =>
-    p.getAssetBalance(a, asset).getOrElse(0L)
+    sp().getAssetBalance(a, asset).getOrElse(0L)
   }
 }
