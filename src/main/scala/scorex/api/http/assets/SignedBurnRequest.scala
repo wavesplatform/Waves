261af48af63bd0de905b7972a88151e718e82c37
Peter Zhelezniakov <peterz@rambler.ru>
2018-01-16 13:42:03 +0300

    NODE-387 Fixed broadcast of burn and lease cancel requests

--- src/main/scala/scorex/api/http/assets/SignedBurnRequest.scala
+++ src/main/scala/scorex/api/http/assets/SignedBurnRequest.scala
@@ -1,15 +1,25 @@
 package scorex.api.http.assets
 
 import io.swagger.annotations.ApiModelProperty
-import play.api.libs.json.{Format, Json}
+import play.api.libs.functional.syntax._
+import play.api.libs.json._
 import scorex.account.PublicKeyAccount
 import scorex.api.http.BroadcastRequest
 import scorex.transaction.TransactionParser.SignatureStringLength
-import scorex.transaction.{AssetIdStringLength, ValidationError}
 import scorex.transaction.assets.BurnTransaction
+import scorex.transaction.{AssetIdStringLength, ValidationError}
 
 object SignedBurnRequest {
-  implicit val assetBurnRequestReads: Format[SignedBurnRequest] = Json.format
+  implicit val reads: Reads[SignedBurnRequest] = (
+      (JsPath \ "senderPublicKey").read[String] and
+      (JsPath \ "assetId").read[String] and
+      (JsPath \ "quantity").read[Long].orElse((JsPath \ "amount").read[Long]) and
+      (JsPath \ "fee").read[Long] and
+      (JsPath \ "timestamp").read[Long] and
+      (JsPath \ "signature").read[String]
+    )(SignedBurnRequest.apply _)
+
+  implicit val writes: Writes[SignedBurnRequest] = Json.writes[SignedBurnRequest]
 }
 
 case class SignedBurnRequest(@ApiModelProperty(value = "Base58 encoded Issuer public key", required = true)
