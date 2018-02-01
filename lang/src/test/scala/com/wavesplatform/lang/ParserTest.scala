9717f4204ae35940312dab808017b641cb444309
Ilya Smagin <ilya.smagin@gmail.com>
2018-02-01 17:06:40 +0300

    NODE-524: language syntax: isDefined/get

--- lang/src/test/scala/com/wavesplatform/lang/ParserTest.scala
+++ lang/src/test/scala/com/wavesplatform/lang/ParserTest.scala
@@ -49,36 +49,42 @@ class ParserTest extends PropSpec with PropertyChecks with Matchers with ScriptG
   }
 
   property("let/ref constructs") {
-    parse("""let X = 10;
+    parse(
+      """let X = 10;
         |3 > 2
       """.stripMargin) shouldBe Block(Some(LET("X", CONST_INT(10))), GT(CONST_INT(3), CONST_INT(2)))
 
     parse("(let X = 10; 3 > 2)") shouldBe Block(Some(LET("X", CONST_INT(10))), GT(CONST_INT(3), CONST_INT(2)))
 
 
-    parse("""let X = 10;
-            |let Y = 10;
-            |X > Y
-          """.stripMargin) shouldBe Block(Some(LET("X", CONST_INT(10))), Block(Some(LET("Y", CONST_INT(10))), GT(REF("X"), REF("Y"))))
+    parse(
+      """let X = 10;
+        |let Y = 10;
+        |X > Y
+      """.stripMargin) shouldBe Block(Some(LET("X", CONST_INT(10))), Block(Some(LET("Y", CONST_INT(10))), GT(REF("X"), REF("Y"))))
   }
 
   property("multiline") {
-    parse("""
+    parse(
+      """
         |
         |false
         |
         |
       """.stripMargin) shouldBe FALSE
 
-    parse("""let X = 10;
+    parse(
+      """let X = 10;
         |
         |true
       """.stripMargin) shouldBe Block(Some(LET("X", CONST_INT(10))), TRUE)
-    parse("""let X = 11;
+    parse(
+      """let X = 11;
         |true
       """.stripMargin) shouldBe Block(Some(LET("X", CONST_INT(11))), TRUE)
 
-    parse("""
+    parse(
+      """
         |
         |let X = 12;
         |
@@ -92,22 +98,29 @@ class ParserTest extends PropSpec with PropertyChecks with Matchers with ScriptG
   property("if") {
     parse("if(true) then 1 else 2") shouldBe IF(TRUE, CONST_INT(1), CONST_INT(2))
     parse("if(true) then 1 else if(X==Y) then 2 else 3") shouldBe IF(TRUE, CONST_INT(1), IF(EQ_INT(REF("X"), REF("Y")), CONST_INT(2), CONST_INT(3)))
-    parse("""if ( true )
+    parse(
+      """if ( true )
         |then 1
         |else if(X== Y)
         |     then 2
         |       else 3""".stripMargin) shouldBe IF(TRUE, CONST_INT(1), IF(EQ_INT(REF("X"), REF("Y")), CONST_INT(2), CONST_INT(3)))
 
-    parse("if (true) then false else false==false") shouldBe IF(TRUE,FALSE, EQ_INT(FALSE,FALSE))
+    parse("if (true) then false else false==false") shouldBe IF(TRUE, FALSE, EQ_INT(FALSE, FALSE))
 
-    parse("""if
+    parse(
+      """if
 
              (true)
         |then let A = 10;
         |  1
-        |else if ( X == Y) then 2 else 3""".stripMargin) shouldBe IF(Block(None,TRUE),
-      Block(None,Block(Some(LET("A",Block(None,CONST_INT(10)))),CONST_INT(1))),
-      Block(None,IF(Block(None,EQ_INT(REF("X"),Block(None,REF("Y")))),Block(None,CONST_INT(2)),Block(None,CONST_INT(3)))))
+        |else if ( X == Y) then 2 else 3""".stripMargin) shouldBe IF(Block(None, TRUE),
+      Block(None, Block(Some(LET("A", Block(None, CONST_INT(10)))), CONST_INT(1))),
+      Block(None, IF(Block(None, EQ_INT(REF("X"), Block(None, REF("Y")))), Block(None, CONST_INT(2)), Block(None, CONST_INT(3)))))
 
+  }
+
+  property("isDefined/get") {
+    parse("isDefined(X)") shouldBe IS_DEFINED(REF("X"))
+    parse("if(isDefined(X)) then get(X) else Y") shouldBe IF(IS_DEFINED(REF("X")), GET(REF("X")), REF("Y"))
   }
 }
