09962724cd6d32a583f905634db51c91824e114f
Yuriy Naydenov <ebceu44@gmail.com>
2018-02-19 18:12:48 +0300

    js api with typechecker

--- lang/.js/src/main/scala/JsAPI.scala
+++ lang/.js/src/main/scala/JsAPI.scala
@@ -1,10 +1,18 @@
-import com.wavesplatform.lang.{Parser, Terms}
+import com.wavesplatform.lang.{Parser, Serde, Terms, TypeChecker}
 import fastparse.core.Parsed
+import scodec.bits.BitVector
 
 import scala.scalajs.js.annotation.JSExportTopLevel
 
 object JsAPI {
   @JSExportTopLevel("parse")
-  def parse(input: String): Parsed[Terms.Expr, Char, String] =
+  def parse(input: String): Parsed[Terms.Untyped.EXPR, Char, String] =
     Parser(input)
+
+  @JSExportTopLevel("compile")
+  def compile(input: String): Option[BitVector] = {
+    parse(input).fold[Option[Terms.Untyped.EXPR]]((_, _, _) => None, (x, _) => Some(x))
+      .flatMap(TypeChecker(TypeChecker.Context.empty, _).toOption)
+      .flatMap(Serde.codec.encode(_).toOption)
+  }
 }
--- lang/.js/src/main/scala/com/wavesplatform/lang/js/Base58.scala
+++ lang/.js/src/main/scala/com/wavesplatform/lang/js/Base58.scala
@@ -1,20 +1,9 @@
 package com.wavesplatform.lang.js
 
-import com.wavesplatform.lang.js
-
-import scala.scalajs.js
-import scala.scalajs.js.annotation.JSGlobalScope
 import scala.util.{Success, Try}
-
-@js.native
-@JSGlobalScope
-object JSGlobalScope extends js.Object {
-  def base58Encode(input: Array[Byte]): String = js.native
-  def base58Decode(input: String): js.Array[Byte] = js.native
-}
 
 trait Base58 extends com.wavesplatform.lang.traits.Base58{
   protected val Base58Chars = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
-  protected def base58Encode(input: Array[Byte]): String = JSGlobalScope.base58Encode(input)
-  protected def base58Decode(input: String): Try[Array[Byte]] = Success(JSGlobalScope.base58Decode(input).toArray)
+  protected def base58Encode(input: Array[Byte]): String = Global.base58Encode(input)
+  protected def base58Decode(input: String): Try[Array[Byte]] = Success(Global.base58Decode(input).toArray)
 }
