d93dd86ef7693237db8d522deb64c0f2ed3953a2
Alexey Kiselev <alexey.kiselev@gmail.com>
2018-02-28 11:27:33 +0300

    Fee value parsing by Ficus was added

--- src/main/scala/com/wavesplatform/settings/FeesSettings.scala
+++ src/main/scala/com/wavesplatform/settings/FeesSettings.scala
@@ -1,12 +1,11 @@
 package com.wavesplatform.settings
 
-import java.util.Map.Entry
-import scala.collection.JavaConverters._
-import scala.util.Try
 import com.google.common.base.CaseFormat
-import com.typesafe.config.ConfigException.BadValue
-import com.typesafe.config.{Config, ConfigValue}
+import com.typesafe.config.Config
+import net.ceedubs.ficus.Ficus._
 import scorex.transaction.TransactionParser.TransactionType
+
+import scala.collection.JavaConverters._
 
 case class FeeSettings(asset: String, fee: Long)
 
@@ -16,30 +15,24 @@ object FeesSettings {
   val configPath: String = "waves.fees"
 
   private val converter = CaseFormat.LOWER_HYPHEN.converterTo(CaseFormat.UPPER_CAMEL)
-  private def toTxType(key: String): TransactionType.Value =
-    TransactionType.withName(s"${converter.convert(key)}Transaction")
 
   def fromConfig(config: Config): FeesSettings = {
     val feesEntries = config.entrySet().asScala.filter(_.getKey startsWith configPath)
     val fees = feesEntries.foldLeft(Map[Int, List[FeeSettings]]()) { (map, e) =>
-      val p = toFeeSettings(e)
-      map.updated(p._1, map.getOrElse(p._1, List()) :+ p._2)
+      val (transactionType, asset) = extractTransactionTypeAndAsset(e.getKey)
+      val maybeFee = config.getAs[Long](e.getKey)
+      maybeFee.map(v => map.updated(transactionType.id, map.getOrElse(transactionType.id, List()) :+ FeeSettings(asset, v))).getOrElse(map)
     }
-
     FeesSettings(fees)
   }
 
-  private def toFeeSettings(e: Entry[String, ConfigValue]): (Int, FeeSettings) = {
-    val s = e.getKey.replace(s"$configPath.", "").trim
+  private def toTxType(key: String): TransactionType.Value =
+    TransactionType.withName(s"${converter.convert(key)}Transaction")
+
+  private def extractTransactionTypeAndAsset(key: String): (TransactionType.Value, String) = {
+    val s = key.replace(s"$configPath.", "").trim
     val parts = s.split("\\.", 2)
     val (transactionTypeName, asset) = (parts(0), parts(1))
-    val transactionType = toTxType(transactionTypeName).id
-
-    val feeString = e.getValue.render
-    val triedFee = Try(feeString.toLong)
-
-    if (triedFee.isFailure) throw new BadValue(e.getKey, s"Failed to convert $feeString to long value", triedFee.failed.get)
-
-    transactionType -> FeeSettings(asset, triedFee.get)
+    (toTxType(transactionTypeName), asset)
   }
 }
