0228ed91b3c338af3d38f1085198598f72699287
ebceu4 <ebceu44@gmail.com>
2018-02-28 15:52:33 +0300

    WIP

--- src/main/scala/scorex/transaction/smart/Script.scala
+++ src/main/scala/scorex/transaction/smart/Script.scala
@@ -1,25 +1,37 @@
 package scorex.transaction.smart
 
+import com.wavesplatform.crypto
 import com.wavesplatform.lang.Serde
 import com.wavesplatform.lang.Terms.Typed
 import com.wavesplatform.state2.ByteStr
 import monix.eval.Coeval
-import scodec.Attempt.{Failure, Successful}
-import scodec.DecodeResult
 import scorex.transaction.ValidationError.ScriptParseError
+import scorex.transaction.smart.Script.checksumLength
 
 case class Script(script: Typed.EXPR) {
-  val bytes: Coeval[ByteStr] = Coeval.evalOnce(ByteStr(Serde.codec.encode(script).require.toByteArray))
+  val bytes: Coeval[ByteStr] = Coeval.evalOnce {
+    val s = Serde.codec.encode(script).require.toByteArray
+    ByteStr(s ++ crypto.secureHash(s).take(checksumLength))
+  }
   val version = 1
   val text: String = script.toString
-
   override def toString: String = s"Script(base58=${bytes()}, $text"
 }
 
 object Script {
-  def fromBytes(arr: Array[Byte]): Either[ScriptParseError, Script] = Serde.codec.decode(scodec.bits.BitVector(arr)) match {
-    case Successful(value: DecodeResult[Typed.EXPR]) => Right(Script(value.value))
-    case Failure(cause) => Left(ScriptParseError(cause.toString))
-  }
+  val checksumLength = 4
+
+  def fromBytes(scriptBytes: Array[Byte]): Either[ScriptParseError, Script] =
+
+    //val checkSum = scriptBytes.takeRight(checksumLength)
+
+    //val checkSumGenerated = crypto.secureHash(scriptBytes.dropRight(checksumLength)).take(checksumLength)
+
+    //Either.cond(checkSum.sameElements(checkSumGenerated), (), Left(ScriptParseError("Invalid checksum")))
+      //.flatMap(_ =>
+      Serde.codec.decode(scodec.bits.BitVector(scriptBytes.dropRight(checksumLength))) match {
+        case Successful(value: DecodeResult[Typed.EXPR]) => Right(Script(value.value))
+        case Failure(cause) => Left(ScriptParseError(cause.toString))
+      }
 
 }
--- src/main/scala/scorex/transaction/smart/SetScriptTransaction.scala
+++ src/main/scala/scorex/transaction/smart/SetScriptTransaction.scala
@@ -14,6 +14,7 @@ import scorex.transaction._
 import scala.util.{Failure, Success, Try}
 
 case class SetScriptTransaction private(version: Byte,
+                                        chainId: Byte,
                                         sender: PublicKeyAccount,
                                         script: Option[Script],
                                         fee: Long,
@@ -21,6 +22,7 @@ case class SetScriptTransaction private(version: Byte,
                                         proofs: Proofs) extends ProvenTransaction with FastHashId {
 
   val bodyBytes: Coeval[Array[Byte]] = Coeval.evalOnce(Bytes.concat(Array(version),
+    Array(chainId),
     sender.publicKey,
     Deser.serializeOption(script)(s => Deser.serializeArray(s.bytes().arr)),
     Longs.toByteArray(fee),
@@ -39,8 +41,11 @@ case class SetScriptTransaction private(version: Byte,
 
 object SetScriptTransaction {
 
+  private val networkByte = AddressScheme.current.chainId
+
   def parseTail(bytes: Array[Byte]): Try[SetScriptTransaction] = Try {
     val version = bytes(0)
+    val chainId = bytes(1)
     val sender = PublicKeyAccount(bytes.slice(1, KeyLength + 1))
     val (scriptOptEi: Option[Either[ValidationError.ScriptParseError, Script]], scriptEnd) = Deser.parseOption(bytes, KeyLength + 1)(str => Script.fromBytes(Deser.parseArraySize(str, 0)._1))
     val scriptEiOpt = scriptOptEi match {
@@ -54,11 +59,11 @@ object SetScriptTransaction {
     (for {
       scriptOpt <- scriptEiOpt
       _ <- Either.cond(version == 1, (), GenericError(s"Unsupported SetScriptTransaction version ${version.toInt}"))
+      _ <- Either.cond(chainId == networkByte, (), GenericError(s"Wrong chainId ${chainId.toInt}"))
       proofs <- Proofs.fromBytes(bytes.drop(scriptEnd + 16))
       tx <- create(sender, scriptOpt, fee, timestamp, proofs)
     } yield tx).fold(left => Failure(new Exception(left.toString)), right => Success(right))
   }.flatten
-
 
   def create(sender: PublicKeyAccount,
              script: Option[Script],
@@ -68,7 +73,7 @@ object SetScriptTransaction {
     if (fee <= 0) {
       Left(ValidationError.InsufficientFee)
     } else {
-      Right(new SetScriptTransaction(1, sender, script, fee, timestamp, proofs))
+      Right(new SetScriptTransaction(1, networkByte, sender, script, fee, timestamp, proofs))
     }
 
 
