6827e2b23de2218a538ba65bfdcc3f3a519bdaaf
ebceu4 <ebceu44@gmail.com>
2018-02-28 18:25:11 +0300

    + NODE-514 script version

--- src/main/scala/scorex/transaction/smart/Script.scala
+++ src/main/scala/scorex/transaction/smart/Script.scala
@@ -8,32 +8,39 @@ import monix.eval.Coeval
 import scodec.Attempt.{Failure, Successful}
 import scodec.DecodeResult
 import scorex.transaction.ValidationError.ScriptParseError
-import scorex.transaction.smart.Script.checksumLength
+import scorex.transaction.smart.Script._
 
 case class Script(script: Typed.EXPR) {
+
+  val version: Byte = 1
+
+  val text: String = script.toString
+
   val bytes: Coeval[ByteStr] = Coeval.evalOnce {
     val s = Serde.codec.encode(script).require.toByteArray
-    ByteStr(s ++ crypto.secureHash(s).take(checksumLength))
+    ByteStr(Array(version) ++ s ++ crypto.secureHash(s).take(checksumLength))
   }
-  val version = 1
-  val text: String = script.toString
 
   override def toString: String = s"Script(base58=${bytes()}, $text"
 }
 
 object Script {
+
   val checksumLength = 4
 
-  def fromBytes(scriptBytes: Array[Byte]): Either[ScriptParseError, Script] = {
+  def fromBytes(bytes: Array[Byte]): Either[ScriptParseError, Script] = {
+    val checkSum = bytes.takeRight(checksumLength)
+    val computedCheckSum = crypto.secureHash(bytes.dropRight(checksumLength)).take(checksumLength)
+    val version = bytes.head
+    val scriptBytes = bytes.drop(1).dropRight(checksumLength)
 
-    val checkSum = scriptBytes.takeRight(checksumLength)
-    val computedCheckSum = crypto.secureHash(scriptBytes.dropRight(checksumLength)).take(checksumLength)
-
-    Either.cond[ScriptParseError, Unit](checkSum.sameElements(computedCheckSum), (), ScriptParseError("Invalid checksum"))
-      .flatMap(_ =>
-        Serde.codec.decode(scodec.bits.BitVector(scriptBytes.dropRight(checksumLength))) match {
-          case Successful(value: DecodeResult[Typed.EXPR]) => Right(Script(value.value))
-          case Failure(cause) => Left(ScriptParseError(cause.toString))
-        })
+    for {
+      _ <- Either.cond(checkSum.sameElements(computedCheckSum), (), ScriptParseError("Invalid checksum"))
+      _ <- Either.cond(version == 1, (), ScriptParseError(s"Invalid version: $version"))
+      r <- Serde.codec.decode(scodec.bits.BitVector(scriptBytes.dropRight(checksumLength))) match {
+        case Successful(value: DecodeResult[Typed.EXPR]) => Right(Script(value.value))
+        case Failure(cause) => Left(ScriptParseError(cause.toString))
+      }
+    } yield r
   }
 }
