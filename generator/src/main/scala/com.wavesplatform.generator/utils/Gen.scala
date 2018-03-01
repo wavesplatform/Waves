856dbdefe9ef66b6dc344e0462e7c7861263b2da
peterz <peterz@rambler.ru>
2018-03-01 12:15:29 +0300

    NODE-566 Removed stray changes

--- generator/src/main/scala/com.wavesplatform.generator/utils/Gen.scala
+++ generator/src/main/scala/com.wavesplatform.generator/utils/Gen.scala
@@ -17,7 +17,9 @@ object Gen {
           recipientGen: Iterator[Address]): Iterator[Transaction] = {
     val senderGen = Iterator.randomContinually(senderAccounts)
     val feeGen = Iterator.continually(minFee + random.nextLong(maxFee - minFee))
-    massTransfers(senderGen, recipientGen, feeGen)
+    transfers(senderGen, recipientGen, feeGen)
+      .zip(massTransfers(senderGen, recipientGen, feeGen))
+      .flatMap { case (tt, mtt) => Iterator(mtt, tt) }
   }
 
   def transfers(senderGen: Iterator[PrivateKeyAccount],
