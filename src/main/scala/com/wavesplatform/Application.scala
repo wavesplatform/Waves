ad72b2b363b5fceb9646b3b8f5aaab01e6dd9a57
Yuriy Naydenov <ebceu44@gmail.com>
2018-03-22 14:33:34 +0300

    node-565 API method to return the current Node time (UTC)

--- src/main/scala/com/wavesplatform/Application.scala
+++ src/main/scala/com/wavesplatform/Application.scala
@@ -193,7 +193,7 @@ class Application(val actorSystem: ActorSystem, val settings: WavesSettings, con
         NxtConsensusApiRoute(settings.restAPISettings, state, history, settings.blockchainSettings.functionalitySettings),
         WalletApiRoute(settings.restAPISettings, wallet),
         PaymentApiRoute(settings.restAPISettings, wallet, utxStorage, allChannels, time),
-        UtilsApiRoute(settings.restAPISettings),
+        UtilsApiRoute(time, settings.restAPISettings),
         PeersApiRoute(settings.restAPISettings, network.connect, peerDatabase, establishedConnections),
         AddressApiRoute(settings.restAPISettings, wallet, state, utxStorage, allChannels, time, settings.blockchainSettings.functionalitySettings),
         DebugApiRoute(settings.restAPISettings, wallet, state, history, peerDatabase, establishedConnections, blockchainUpdater, allChannels,
