24ca392911735150f68fb3afb525eed300cc1774
Ilya Smagin <ilya.smagin@gmail.com>
2018-03-28 18:24:11 +0300

    NODE-643: SetScriptTransactionSuite

--- it/src/test/scala/com/wavesplatform/it/sync/SetScriptTransactionSuite.scala
+++ it/src/test/scala/com/wavesplatform/it/sync/SetScriptTransactionSuite.scala
@@ -9,7 +9,7 @@ import com.wavesplatform.state2._
 import com.wavesplatform.utils.dummyTypeCheckerContext
 import org.scalatest.CancelAfterFailure
 import play.api.libs.json.JsNumber
-import scorex.account.PrivateKeyAccount
+import scorex.account.{AddressScheme, PrivateKeyAccount}
 import scorex.transaction.Proofs
 import scorex.transaction.assets.VersionedTransferTransaction
 import scorex.transaction.smart.{Script, SetScriptTransaction}
@@ -20,19 +20,35 @@ class SetScriptTransactionSuite extends BaseTransactionSuite with CancelAfterFai
 
   private def randomPk = PrivateKeyAccount(Array.fill[Byte](32)(Random.nextInt(Byte.MaxValue).toByte))
 
+  AddressScheme.current = new AddressScheme {
+    override val chainId: Byte = 'H'
+  }
+
   private val acc0 = randomPk
   private val acc1 = randomPk
   private val acc2 = randomPk
   private val acc3 = randomPk
 
-  private val transferAmount: Long          = 1.waves
-  private val fee: Long                     = 0.001.waves
-  private val senderPublicKeyString: String = ByteStr(sender.publicKey.publicKey).base58
-  private val acc0PublicKeyString: String = ByteStr(acc0.publicKey).base58
+  private val transferAmount: Long = 1.waves
+  private val fee: Long            = 0.001.waves
 
   test("setup acc0 with 1 waves") {
+    val tx =
+      VersionedTransferTransaction
+        .selfSigned(
+          version = 2,
+          assetId = None,
+          sender = sender.privateKey,
+          recipient = acc0,
+          amount = transferAmount,
+          timestamp = System.currentTimeMillis(),
+          feeAmount = fee,
+          attachment = Array.emptyByteArray
+        )
+        .explicitGet()
+
     val transferId = sender
-      .transfer(sourceAddress = sender.address, recipient = acc0.address, amount = transferAmount, fee = fee, assetId = None, feeAssetId = None)
+      .signedBroadcast(tx.json() + ("type" -> JsNumber(VersionedTransferTransaction.typeId.toInt)))
       .id
     nodes.waitForHeightAriseAndTxPresent(transferId)
   }
@@ -68,7 +84,6 @@ class SetScriptTransactionSuite extends BaseTransactionSuite with CancelAfterFai
   }
 
   test("can't send from acc0 using old pk") {
-
     val tx =
       VersionedTransferTransaction
         .selfSigned(
@@ -79,7 +94,8 @@ class SetScriptTransactionSuite extends BaseTransactionSuite with CancelAfterFai
           amount = transferAmount,
           timestamp = System.currentTimeMillis(),
           feeAmount = fee,
-          attachment = Array.emptyByteArray)
+          attachment = Array.emptyByteArray
+        )
         .explicitGet()
     assertBadRequest(sender.signedBroadcast(tx.json() + ("type" -> JsNumber(VersionedTransferTransaction.typeId.toInt))))
   }
@@ -145,7 +161,8 @@ class SetScriptTransactionSuite extends BaseTransactionSuite with CancelAfterFai
           amount = transferAmount,
           timestamp = System.currentTimeMillis(),
           feeAmount = fee,
-          attachment = Array.emptyByteArray)
+          attachment = Array.emptyByteArray
+        )
         .explicitGet()
     val txId = sender.signedBroadcast(tx.json() + ("type" -> JsNumber(VersionedTransferTransaction.typeId.toInt))).id
     nodes.waitForHeightAriseAndTxPresent(txId)
