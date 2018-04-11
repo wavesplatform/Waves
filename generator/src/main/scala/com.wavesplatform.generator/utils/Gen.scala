37c0ceffc7bb8c4b51f2c71c60d0fdc4eebc85b8
peterz <peterz@rambler.ru>
2018-04-11 15:20:00 +0300

    Updated Dyn-Wide generator to generate only Transfer transactions

--- generator/src/main/scala/com.wavesplatform.generator/utils/Gen.scala
+++ generator/src/main/scala/com.wavesplatform.generator/utils/Gen.scala
@@ -16,8 +16,6 @@ object Gen {
     val senderGen = Iterator.randomContinually(senderAccounts)
     val feeGen    = Iterator.continually(minFee + random.nextLong(maxFee - minFee))
     transfers(senderGen, recipientGen, feeGen)
-      .zip(massTransfers(senderGen, recipientGen, feeGen))
-      .flatMap { case (tt, mtt) => Iterator(mtt, tt) }
   }
 
   def transfers(senderGen: Iterator[PrivateKeyAccount], recipientGen: Iterator[Address], feeGen: Iterator[Long]): Iterator[Transaction] = {
