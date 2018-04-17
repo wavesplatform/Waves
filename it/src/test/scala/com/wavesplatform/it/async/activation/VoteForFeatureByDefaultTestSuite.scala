57addd720650b4fa2e54b9e23b33282c75a19668
Sergey Nazarov <phearnot@renee.ru>
2018-04-17 18:36:58 +0300

    Fixed feature activation tests

--- it/src/test/scala/com/wavesplatform/it/async/activation/VoteForFeatureByDefaultTestSuite.scala
+++ it/src/test/scala/com/wavesplatform/it/async/activation/VoteForFeatureByDefaultTestSuite.scala
@@ -1,6 +1,7 @@
 package com.wavesplatform.it.async.activation
 
 import com.typesafe.config.Config
+import com.typesafe.config.ConfigFactory.{parseString => cfg}
 import com.wavesplatform.features.BlockchainFeatureStatus
 import com.wavesplatform.features.api.NodeFeatureStatus
 import com.wavesplatform.it.api.AsyncHttpApi._
@@ -20,64 +21,61 @@ class VoteForFeatureByDefaultTestSuite
     with ActivationStatusRequest
     with ReportingTestName {
 
-  private val votingInterval                 = 25
-  private val blocksForActivation            = 18
-  private val defaultVotingFeatureNum: Short = 1
-  private val nonVotingFeatureNum: Short     = 2
+  import VoteForFeatureByDefaultTestSuite._
 
   override protected def nodeConfigs: Seq[Config] =
-    NodeConfigs.newBuilder
-      .overrideBase(
-        _.raw(
-          s"""waves {
-         |  blockchain.custom {
-         |    functionality {
-         |      pre-activated-features = {}
-         |      feature-check-blocks-period = $votingInterval
-         |      blocks-for-feature-activation = $blocksForActivation
-         |    }
-         |  }
-         |  features.supported = [$defaultVotingFeatureNum]
-         |  miner.quorum = 3
-         |}""".stripMargin
-        ))
-      .withDefault(3)
-      .withSpecial(_.raw(s"waves.features.supported=[$nonVotingFeatureNum]"))
-      .buildNonConflicting()
+    (cfg(s"waves.features.supported=[$nonVotingFeatureNum]") +: Seq.fill(3)(cfg(s"waves.features.supported=[$defaultVotingFeatureNum]")))
+      .zip(Seq(NodeConfigs.Default.head, NodeConfigs.Default(6), NodeConfigs.Default(7), NodeConfigs.Default(8)))
+      .map { case (o, t) => o.withFallback(sharedConfig).withFallback(t) }
 
-  private def supportedNodes   = nodes.init
-  private def notSupportedNode = nodes.last
+  private def supportingNodes   = nodes.tail
+  private def notSupportingNode = nodes.head
 
   "supported blocks increased when voting starts, one node votes against, three by default" in {
     val checkHeight: Int = votingInterval * 2 / 3
 
-    val supportedNodeActivationInfo = activationStatus(supportedNodes, checkHeight, defaultVotingFeatureNum, 4.minute)
-    val nonSupportedNodeActivationInfo = Await
-      .result(notSupportedNode.activationStatus, 1.minute)
+    val supportingNodeActivationInfo = activationStatus(supportingNodes, checkHeight, defaultVotingFeatureNum, 4.minute)
+    val nonSupportingNodeActivationInfo = Await
+      .result(notSupportingNode.activationStatus, 1.minute)
       .features
       .find(_.id == defaultVotingFeatureNum)
       .get
 
     val generatedBlocks              = Await.result(nodes.last.blockSeq(1, checkHeight), 2.minute)
-    val featuresMapInGeneratedBlocks = generatedBlocks.flatMap(b => b.features.getOrElse(Seq.empty)).groupBy(x => x)
-    val votesForFeature1             = featuresMapInGeneratedBlocks.getOrElse(defaultVotingFeatureNum, Seq.empty).length
+    val featuresMapInGeneratedBlocks = generatedBlocks.flatMap(b => b.features.getOrElse(Seq.empty)).groupBy(identity).mapValues(_.length)
+    val votesForFeature1             = featuresMapInGeneratedBlocks.getOrElse(defaultVotingFeatureNum, 0)
 
-    assertVotingStatus(supportedNodeActivationInfo, votesForFeature1, BlockchainFeatureStatus.Undefined, NodeFeatureStatus.Voted)
-    nonSupportedNodeActivationInfo.nodeStatus shouldBe NodeFeatureStatus.Implemented
+    assertVotingStatus(supportingNodeActivationInfo, votesForFeature1, BlockchainFeatureStatus.Undefined, NodeFeatureStatus.Voted)
+    nonSupportingNodeActivationInfo.nodeStatus shouldBe NodeFeatureStatus.Implemented
   }
 
   "blockchain status is APPROVED in second voting interval, one node votes against, three by default" in {
     val checkHeight: Int = votingInterval * 2 - blocksForActivation / 2
-
-    val supportedNodeActivationInfo = activationStatus(supportedNodes, checkHeight, defaultVotingFeatureNum, 3.minute)
-    assertApprovedStatus(supportedNodeActivationInfo, votingInterval * 2, NodeFeatureStatus.Voted)
+    val supportingNodeActivationInfo = activationStatus(supportingNodes, checkHeight, defaultVotingFeatureNum, 3.minute)
+    assertApprovedStatus(supportingNodeActivationInfo, votingInterval * 2, NodeFeatureStatus.Voted)
   }
 
   "blockchain status is ACTIVATED in the end of second voting interval, one node votes against, three by default" in {
     val checkHeight: Int = votingInterval * 2
 
-    val supportedNodeActivationInfo = activationStatus(supportedNodes, checkHeight, defaultVotingFeatureNum, 2.minute)
+    val supportedNodeActivationInfo = activationStatus(supportingNodes, checkHeight, defaultVotingFeatureNum, 2.minute)
     assertActivatedStatus(supportedNodeActivationInfo, checkHeight, NodeFeatureStatus.Voted)
   }
 
+}
+
+object VoteForFeatureByDefaultTestSuite {
+  private val defaultVotingFeatureNum: Short = 1
+  private val nonVotingFeatureNum: Short     = 2
+  private val votingInterval                 = 25
+  private val blocksForActivation            = 18
+
+  private val sharedConfig = cfg(s"""waves {
+      |  blockchain.custom.functionality {
+      |    pre-activated-features = null
+      |    feature-check-blocks-period = $votingInterval
+      |    blocks-for-feature-activation = $blocksForActivation
+      |  }
+      |  miner.quorum = 3
+      |}""".stripMargin)
 }
--- src/main/scala/com/wavesplatform/settings/package.scala
+++ src/main/scala/com/wavesplatform/settings/package.scala
@@ -2,10 +2,8 @@ package com.wavesplatform
 
 import java.io.File
 
-import com.typesafe.config.{Config, ConfigFactory, ConfigUtil}
+import com.typesafe.config.{Config, ConfigFactory}
 import com.wavesplatform.state2.ByteStr
-
-import collection.JavaConverters._
 import net.ceedubs.ficus.readers.namemappers.HyphenNameMapper
 import net.ceedubs.ficus.readers.{NameMapper, ValueReader}
 
@@ -15,18 +13,6 @@ package object settings {
   implicit val fileReader: ValueReader[File]        = (cfg, path) => new File(cfg.getString(path))
   implicit val byteStrReader: ValueReader[ByteStr]  = (cfg, path) => ByteStr.decodeBase58(cfg.getString(path)).get
   implicit val shortValueReader: ValueReader[Short] = (cfg, path) => cfg.getLong(path).toShort
-  implicit val mapShortIntReader: ValueReader[Map[Short, Int]] = { (cfg, path) =>
-    val relativeConfig = cfg.getConfig(path)
-    relativeConfig
-      .root()
-      .entrySet()
-      .asScala
-      .map { entry =>
-        val key = entry.getKey
-        key.toShort -> relativeConfig.getInt(ConfigUtil.quoteString(key))
-      }
-      .toMap
-  }
 
   def loadConfig(userConfig: Config): Config = {
     ConfigFactory
