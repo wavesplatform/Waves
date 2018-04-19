d678ae905cd4d89d6c042c354d89c5b9ab928ac4
Nastya.Urlapova <aurlapova@wavesplatform.com>
2018-04-19 19:07:49 +0300

    NODE-434 Additional integrations tests

--- lang/jvm/src/test/scala/com/wavesplatform/lang/IntegrationTest.scala
+++ lang/jvm/src/test/scala/com/wavesplatform/lang/IntegrationTest.scala
@@ -34,4 +34,13 @@ class IntegrationTest extends PropSpec with PropertyChecks with Matchers with No
   property("equals shouldn't compile on option") {
     eval[Boolean]("Some(1) == Some(2)") should produce("Can't find a function '=='")
   }
+
+  property("equals some lang structure") {
+    eval[Boolean]("let x = (-7763390488025868909>-1171895536391400041); let v = false; (v&&true)") shouldBe Right(false)
+    eval[Boolean]("let mshUmcl = (if(true) then true else true) true || mshUmcl") shouldBe Right(true)
+    eval[Long]("""if(((1+-1)==-1)) then 1 else (1+1)""") shouldBe Right(2)
+    eval[Boolean]("""((((if(true) then 1 else 1)==2)||((if(true)
+                    |then true else true)&&(true||true)))||(if(((1>1)||(-1>=-1)))
+                    |then (-1>=1) else false))""".stripMargin) shouldBe Right(true)
+  }
 }
