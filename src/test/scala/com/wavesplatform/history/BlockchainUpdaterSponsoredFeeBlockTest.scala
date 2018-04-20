58634de047c324489f318d443c9215a9d1b137e2
Eugene Nichegovskiy <eugene.nichegovskiy@gmail.com>
2018-04-20 16:16:35 +0300

    https://wavesplatform.atlassian.net/browse/NODE-687 added unit-test "Not enought waves for sponsored transfer"

--- src/test/scala/com/wavesplatform/history/BlockchainUpdaterSponsoredFeeBlockTest.scala
+++ src/test/scala/com/wavesplatform/history/BlockchainUpdaterSponsoredFeeBlockTest.scala
@@ -8,6 +8,8 @@ import com.wavesplatform.state2.diffs._
 import org.scalacheck.Gen
 import org.scalatest._
 import org.scalatest.prop.PropertyChecks
+import scorex.account.PrivateKeyAccount
+import scorex.crypto.signatures.Curve25519.KeyLength
 import scorex.transaction.GenesisTransaction
 import scorex.transaction.assets.{IssueTransaction, SponsorFeeTransaction, TransferTransaction}
 
@@ -18,7 +20,7 @@ class BlockchainUpdaterSponsoredFeeBlockTest
     with Matchers
     with TransactionGen {
 
-  type Setup = (GenesisTransaction, IssueTransaction, SponsorFeeTransaction, TransferTransaction)
+  type Setup = (GenesisTransaction, TransferTransaction, IssueTransaction, SponsorFeeTransaction, TransferTransaction)
 
   val sponsorPreconditions: Gen[Setup] = for {
 
@@ -26,28 +28,48 @@ class BlockchainUpdaterSponsoredFeeBlockTest
     ts                          <- timestampGen
     fee                         <- smallFeeGen
     alice                       <- accountGen
-    (feeAsset, sponsorTx, _, _) <- sponsorFeeCancelSponsorFeeGen(master)
-    genesis: GenesisTransaction = GenesisTransaction.create(master, feeAsset.fee + sponsorTx.fee, ts).right.get
-    transferTx: TransferTransaction = TransferTransaction
-      .create(Some(feeAsset.id()), master, alice, 1 * Constants.UnitsInWave, ts, Some(feeAsset.id()), fee, Array.emptyByteArray)
+    (feeAsset, sponsorTx, _, _) <- sponsorFeeCancelSponsorFeeGen(alice)
+    genesis: GenesisTransaction = GenesisTransaction.create(master, ENOUGH_AMT, ts).right.get
+    masterToAlice: TransferTransaction = TransferTransaction
+      .create(None, master, alice, feeAsset.fee + sponsorTx.fee, ts + 1, None, fee, Array.emptyByteArray)
       .right
       .get
-  } yield (genesis, feeAsset, sponsorTx, transferTx)
+    aliceToMaster: TransferTransaction = TransferTransaction
+      .create(Some(feeAsset.id()), alice, master, 100, ts + 100, Some(feeAsset.id()), fee, Array.emptyByteArray)
+      .right
+      .get
+  } yield (genesis, masterToAlice, feeAsset, sponsorTx, aliceToMaster)
 
   val SponsoredFeeActivatedAt0BlockchainSettings: BlockchainSettings = DefaultBlockchainSettings.copy(
-    functionalitySettings = DefaultBlockchainSettings.functionalitySettings.copy(
-      preActivatedFeatures = Map(BlockchainFeatures.FeeSponsorship.id -> 0, BlockchainFeatures.NG.id -> 0)))
+    functionalitySettings = DefaultBlockchainSettings.functionalitySettings
+      .copy(blocksForFeatureActivation = 1, preActivatedFeatures = Map(BlockchainFeatures.FeeSponsorship.id -> 0, BlockchainFeatures.NG.id -> 0)))
 
   val SponsoredActivatedAt0WavesSettings: WavesSettings = settings.copy(blockchainSettings = SponsoredFeeActivatedAt0BlockchainSettings)
 
-  property("not enough waves to sponsor") {
+  property("not enough waves to sponsor sponsored tx") {
     scenario(sponsorPreconditions, SponsoredActivatedAt0WavesSettings) {
-      case (domain, (genesis, feeAsset, sponsor, transfer)) =>
-        val (block, microBlocks) = chainBaseAndMicro(randomSig, genesis, Seq(feeAsset, sponsor, transfer).map(Seq(_)))
-        domain.blockchainUpdater.processBlock(block).explicitGet()
+      case (domain, (genesis, masterToAlice, feeAsset, sponsor, aliceToMaster)) =>
+        val (block0, microBlocks) = chainBaseAndMicro(randomSig, genesis, Seq(masterToAlice, feeAsset, sponsor).map(Seq(_)))
+        val block1 = customBuildBlockOfTxs(microBlocks.last.totalResBlockSig,
+                                           Seq.empty,
+                                           PrivateKeyAccount(Array.fill(KeyLength)(1)),
+                                           3: Byte,
+                                           sponsor.timestamp + 1)
+        val block2 = customBuildBlockOfTxs(microBlocks.last.totalResBlockSig,
+                                           Seq.empty,
+                                           PrivateKeyAccount(Array.fill(KeyLength)(1)),
+                                           3: Byte,
+                                           sponsor.timestamp + 1)
+        val block3 = buildBlockOfTxs(block2.uniqueId, Seq(aliceToMaster))
+
+        domain.blockchainUpdater.processBlock(block0).explicitGet()
         domain.blockchainUpdater.processMicroBlock(microBlocks(0)).explicitGet()
         domain.blockchainUpdater.processMicroBlock(microBlocks(1)).explicitGet()
-        domain.blockchainUpdater.processMicroBlock(microBlocks(2)) should produce("unavailable funds")
+        domain.blockchainUpdater.processMicroBlock(microBlocks(2)).explicitGet()
+        domain.blockchainUpdater.processBlock(block1).explicitGet()
+        domain.blockchainUpdater.processBlock(block2).explicitGet()
+        domain.blockchainUpdater.processBlock(block3) should produce("unavailable funds")
+
     }
   }
 
