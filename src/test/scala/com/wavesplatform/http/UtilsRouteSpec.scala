680de09e928d3712ee94a487024c61aa42047dd6
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2018-04-23 16:54:46 +0300

    NODE-603 Test for /utils/script/compile

--- src/test/scala/com/wavesplatform/http/UtilsRouteSpec.scala
+++ src/test/scala/com/wavesplatform/http/UtilsRouteSpec.scala
@@ -2,12 +2,16 @@ package com.wavesplatform.http
 
 import com.wavesplatform.crypto
 import com.wavesplatform.http.ApiMarshallers._
+import com.wavesplatform.lang.v1.FunctionHeader
+import com.wavesplatform.lang.v1.Terms.{BOOLEAN, Typed}
 import com.wavesplatform.settings.{FeesSettings, SmartAccountSettings}
 import org.scalacheck.Gen
 import org.scalatest.prop.PropertyChecks
 import play.api.libs.json.{JsObject, JsValue}
 import scorex.api.http.{TooBigArrayAllocation, UtilsApiRoute}
 import scorex.crypto.encode.Base58
+import scorex.transaction.smart.script.Script
+import scorex.transaction.smart.script.v1.ScriptV1
 import scorex.utils.Time
 
 class UtilsRouteSpec extends RouteSpec("/utils") with RestAPISettingsHelper with PropertyChecks {
@@ -17,8 +21,24 @@ class UtilsRouteSpec extends RouteSpec("/utils") with RestAPISettingsHelper with
       def getTimestamp(): Long  = System.currentTimeMillis()
     },
     restAPISettings,
-    FeesSettings(SmartAccountSettings(1000, 1), Map.empty)
+    FeesSettings(SmartAccountSettings(1000, 2), Map.empty)
   ).route
+
+  routePath("/script/compile") in {
+    Post(routePath("/script/compile"), "1 == 2") ~> route ~> check {
+      val json = responseAs[JsValue]
+      val expectedScript = ScriptV1(
+        Typed.FUNCTION_CALL(
+          function = FunctionHeader("==", List(FunctionHeader.FunctionHeaderType.LONG, FunctionHeader.FunctionHeaderType.LONG)),
+          args = List(Typed.CONST_LONG(1), Typed.CONST_LONG(2)),
+          tpe = BOOLEAN
+        ))
+
+      Script.fromBase58String((json \ "script").as[String]) shouldBe Right(expectedScript)
+      (json \ "complexity").as[Long] shouldBe 3
+      (json \ "extraFee").as[Long] shouldBe 1006
+    }
+  }
 
   routePath("/seed") in {
     Get(routePath("/seed")) ~> route ~> check {
