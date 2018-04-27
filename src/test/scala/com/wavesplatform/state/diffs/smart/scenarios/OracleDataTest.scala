c65c93f392cb7d488877ad7c537f7a337b0b465f
peterz <peterz@rambler.ru>
2018-04-27 17:20:31 +0300

    NODE-557 Fixed OracleDataTest

--- src/test/scala/com/wavesplatform/state/diffs/smart/scenarios/OracleDataTest.scala
+++ src/test/scala/com/wavesplatform/state/diffs/smart/scenarios/OracleDataTest.scala
@@ -26,7 +26,7 @@ class OracleDataTest extends PropSpec with PropertyChecks with Matchers with Tra
       genesis2 = GenesisTransaction.create(oracle, ENOUGH_AMT, ts).explicitGet()
       long            <- longEntryGen(dataAsciiKeyGen)
       bool            <- booleanEntryGen(dataAsciiKeyGen).filter(_.key != long.key)
-      bin             <- binaryEntryGen(100, dataAsciiKeyGen).filter(e => e.key != long.key && e.key != bool.key)
+      bin             <- binaryEntryGen(500, dataAsciiKeyGen).filter(e => e.key != long.key && e.key != bool.key)
       dataTransaction <- dataTransactionGenP(oracle, List(long, bool, bin))
       allFieldsRequiredScript        = s"""
                     |
