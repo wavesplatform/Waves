28d5c18d5d70798430ecb8d0841a7023da52f56c
Sergey Nazarov <phearnot@renee.ru>
2018-04-28 12:27:48 +0300

    Explorer improvements

--- src/main/scala/com/wavesplatform/Explorer.scala
+++ src/main/scala/com/wavesplatform/Explorer.scala
@@ -8,7 +8,7 @@ import com.wavesplatform.db.openDB
 import com.wavesplatform.settings.{WavesSettings, loadConfig}
 import com.wavesplatform.state.ByteStr
 import org.slf4j.bridge.SLF4JBridgeHandler
-import scorex.account.AddressScheme
+import scorex.account.{Address, AddressScheme}
 import scorex.crypto.encode.Base58
 import scorex.utils.ScorexLogging
 
@@ -20,37 +20,70 @@ object Explorer extends ScorexLogging {
     SLF4JBridgeHandler.install()
 
     val configFilename = Try(args(0)).toOption.getOrElse("waves-testnet.conf")
-    val maybeOrderId   = Try(args(1)).toOption
 
     val settings = WavesSettings.fromConfig(loadConfig(ConfigFactory.parseFile(new File(configFilename))))
     AddressScheme.current = new AddressScheme {
       override val chainId: Byte = settings.blockchainSettings.addressSchemeCharacter.toByte
     }
 
+    log.info(s"Data directory: ${settings.dataDirectory}")
+
     val db     = openDB(settings.dataDirectory, settings.levelDbCacheSize)
     val reader = new LevelDBWriter(db, settings.blockchainSettings.functionalitySettings)
 
     val blockchainHeight = reader.height
     log.info(s"Blockchain height is $blockchainHeight")
+    try {
 
-    val orderId = maybeOrderId.flatMap(Base58.decode(_).toOption).map(ByteStr.apply)
-    if (orderId.isDefined) {
-      val kVolumeAndFee = LevelDBWriter.k.filledVolumeAndFee(blockchainHeight, orderId.get)
-      val bytes1        = db.get(kVolumeAndFee.keyBytes)
-      val v             = kVolumeAndFee.parse(bytes1)
-      log.info(s"OrderId = ${Base58.encode(orderId.get.arr)}: Volume = ${v.volume}, Fee = ${v.fee}")
+      val flag = args(1).toUpperCase
+      val id   = args(2)
 
-      val kVolumeAndFeeHistory = LevelDBWriter.k.filledVolumeAndFeeHistory(orderId.get)
-      val bytes2               = db.get(kVolumeAndFeeHistory.keyBytes)
-      val value2               = kVolumeAndFeeHistory.parse(bytes2)
-      val value2Str            = value2.mkString("[", ", ", "]")
-      log.info(s"OrderId = ${Base58.encode(orderId.get.arr)}: History = $value2Str")
-      value2.foreach { h =>
-        val k = LevelDBWriter.k.filledVolumeAndFee(h, orderId.get)
-        val v = k.parse(db.get(k.keyBytes))
-        log.info(s"\t h = $h: Volume = ${v.volume}, Fee = ${v.fee}")
+      flag match {
+        case "O" =>
+          val orderId = Base58.decode(id).toOption.map(ByteStr.apply)
+          if (orderId.isDefined) {
+            val kVolumeAndFee = LevelDBWriter.k.filledVolumeAndFee(blockchainHeight, orderId.get)
+            val bytes1        = db.get(kVolumeAndFee.keyBytes)
+            val v             = kVolumeAndFee.parse(bytes1)
+            log.info(s"OrderId = ${Base58.encode(orderId.get.arr)}: Volume = ${v.volume}, Fee = ${v.fee}")
+
+            val kVolumeAndFeeHistory = LevelDBWriter.k.filledVolumeAndFeeHistory(orderId.get)
+            val bytes2               = db.get(kVolumeAndFeeHistory.keyBytes)
+            val value2               = kVolumeAndFeeHistory.parse(bytes2)
+            val value2Str            = value2.mkString("[", ", ", "]")
+            log.info(s"OrderId = ${Base58.encode(orderId.get.arr)}: History = $value2Str")
+            value2.foreach { h =>
+              val k = LevelDBWriter.k.filledVolumeAndFee(h, orderId.get)
+              val v = k.parse(db.get(k.keyBytes))
+              log.info(s"\t h = $h: Volume = ${v.volume}, Fee = ${v.fee}")
+            }
+          } else log.error("No order ID was provided")
+        case "A" =>
+          val address   = Address.fromString(id).right.get
+          val aid       = LevelDBWriter.k.addressId(address)
+          val addressId = aid.parse(db.get(aid.keyBytes)).get
+          log.info(s"Address id = $addressId")
+
+          val kwbh = LevelDBWriter.k.wavesBalanceHistory(addressId)
+          val wbh  = kwbh.parse(db.get(kwbh.keyBytes))
+
+          val balances = wbh.map { h =>
+            val k = LevelDBWriter.k.wavesBalance(h, addressId)
+            h -> k.parse(db.get(k.keyBytes))
+          }
+
+          balances.foreach(b => log.info(s"h = ${b._1}: balance = ${b._2}"))
+        case "T" =>
+          val address   = Address.fromString(id).right.get
+          val aid       = LevelDBWriter.k.addressId(address)
+          val addressId = aid.parse(db.get(aid.keyBytes)).get
+          log.info(s"Address id = $addressId")
+          val ktxidh = LevelDBWriter.k.addressTransactionIds(args(3).toInt, addressId)
+
+          for ((h, id) <- ktxidh.parse(db.get(ktxidh.keyBytes))) {
+            log.info(s"$id at height $h")
+          }
       }
-    } else log.error("No order ID was provided")
-
+    } finally db.close()
   }
 }
