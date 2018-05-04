7dba36189dc8fe951687e7bada17f9763a476778
alexandr <disfenoid@gmail.com>
2018-05-04 15:26:58 +0300

    NODE 727: Fix Lease Cancel v1 request

--- src/main/scala/scorex/api/http/leasing/LeaseCancelV1Request.scala
+++ src/main/scala/scorex/api/http/leasing/LeaseCancelV1Request.scala
@@ -6,7 +6,7 @@ import play.api.libs.json.{Format, Json}
 case class LeaseCancelV1Request(@ApiModelProperty(value = "Base58 encoded sender public key", required = true)
                                 sender: String,
                                 @ApiModelProperty(value = "Base58 encoded lease transaction id", required = true)
-                                leaseId: String,
+                                txId: String,
                                 @ApiModelProperty(required = true)
                                 fee: Long,
                                 timestamp: Option[Long] = None)
