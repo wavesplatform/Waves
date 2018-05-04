f9b32008a17277e43abd9da5f0bb4c96c9baa8e5
alexandr <disfenoid@gmail.com>
2018-05-04 17:00:43 +0300

    NODE 727: Change txId to LeaseId

--- src/main/scala/scorex/api/http/leasing/SignedLeaseCancelV2Request.scala
+++ src/main/scala/scorex/api/http/leasing/SignedLeaseCancelV2Request.scala
@@ -17,7 +17,7 @@ case class SignedLeaseCancelV2Request(@ApiModelProperty(required = true)
                                       @ApiModelProperty(value = "Base58 encoded sender public key", required = true)
                                       senderPublicKey: String,
                                       @ApiModelProperty(value = "Base58 encoded lease transaction id", required = true)
-                                      txId: String,
+                                      leaseId: String,
                                       @ApiModelProperty(required = true)
                                       timestamp: Long,
                                       @ApiModelProperty(required = true)
@@ -28,7 +28,7 @@ case class SignedLeaseCancelV2Request(@ApiModelProperty(required = true)
   def toTx: Either[ValidationError, LeaseCancelTransactionV2] =
     for {
       _sender     <- PublicKeyAccount.fromBase58String(senderPublicKey)
-      _leaseTx    <- parseBase58(txId, "invalid.leaseTx", SignatureStringLength)
+      _leaseTx    <- parseBase58(leaseId, "invalid.leaseTx", SignatureStringLength)
       _proofBytes <- proofs.traverse(s => parseBase58(s, "invalid proof", Proofs.MaxProofStringSize))
       _proofs     <- Proofs.create(_proofBytes)
       _t          <- LeaseCancelTransactionV2.create(version, chainId, _sender, _leaseTx, fee, timestamp, _proofs)
@@ -39,7 +39,7 @@ object SignedLeaseCancelV2Request {
     (JsPath \ "version").read[Byte] and
       (JsPath \ "chainId").read[Byte] and
       (JsPath \ "senderPublicKey").read[String] and
-      (JsPath \ "txId").read[String].orElse((JsPath \ "leaseId").read[String]) and
+      (JsPath \ "leaseId").read[String] and
       (JsPath \ "timestamp").read[Long] and
       (JsPath \ "proofs").read[List[String]] and
       (JsPath \ "fee").read[Long]
