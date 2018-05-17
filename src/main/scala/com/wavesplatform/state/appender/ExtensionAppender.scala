0760ccd5ac4e9b9a402c6fbea8d269d67cf0bd42
Alexandr <disfenoid@gmail.com>
2018-05-18 02:21:17 +0300

    Revert "Revert "Node 129 fair proof of stake""
    This reverts commit 466da3cd378f51039bc158d98485a8173c6ece10.

--- src/main/scala/com/wavesplatform/state/appender/ExtensionAppender.scala
+++ src/main/scala/com/wavesplatform/state/appender/ExtensionAppender.scala
@@ -1,5 +1,6 @@
 package com.wavesplatform.state.appender
 
+import com.wavesplatform.consensus.PoSCalculator
 import com.wavesplatform.metrics.{BlockStats, Instrumented, Metrics}
 import com.wavesplatform.mining.Miner
 import com.wavesplatform.network.{InvalidBlockStorage, PeerDatabase, formatBlocks, id}
@@ -23,6 +24,7 @@ object ExtensionAppender extends ScorexLogging with Instrumented {
   def apply(checkpoint: CheckpointService,
             blockchainUpdater: BlockchainUpdater with Blockchain,
             utxStorage: UtxPool,
+            pos: PoSCalculator,
             time: Time,
             settings: WavesSettings,
             invalidBlocks: InvalidBlockStorage,
@@ -43,9 +45,10 @@ object ExtensionAppender extends ScorexLogging with Instrumented {
               val forkApplicationResultEi = Coeval {
                 extension.view
                   .map { b =>
-                    b -> appendBlock(checkpoint, blockchainUpdater, utxStorage, time, settings)(b).right.map {
-                      _.foreach(bh => BlockStats.applied(b, BlockStats.Source.Ext, bh))
-                    }
+                    b -> appendBlock(checkpoint, blockchainUpdater, utxStorage, pos, time, settings)(b).right
+                      .map {
+                        _.foreach(bh => BlockStats.applied(b, BlockStats.Source.Ext, bh))
+                      }
                   }
                   .zipWithIndex
                   .collectFirst { case ((b, Left(e)), i) => (i, b, e) }
