2d50dd151ae709901fa78a408c713be91f4095f8
peterz <peterz@rambler.ru>
2018-05-28 13:49:59 +0300

    NODE-763 Fixed langJS compilation

--- lang/js/src/main/scala/com/wavesplatform/lang/Global.scala
+++ lang/js/src/main/scala/com/wavesplatform/lang/Global.scala
@@ -3,7 +3,7 @@ package com.wavesplatform.lang
 import com.wavesplatform.lang.v1.BaseGlobal
 
 object Global extends BaseGlobal {
-  def base58Encode(input: Array[Byte]): String                 = impl.Global.base58Encode(input)
+  def base58Encode(input: Array[Byte]): Either[String, String] = impl.Global.base58Encode(input)
   def base58Decode(input: String): Either[String, Array[Byte]] = impl.Global.base58Decode(input)
 
   def curve25519verify(message: Array[Byte], sig: Array[Byte], pub: Array[Byte]): Boolean = impl.Global.curve25519verify(message, sig, pub)
