1239bc1697fc913e3f63c1a12db97f33ca5932f0
Ilya Smagin <ilya.smagin@gmail.com>
2018-05-30 15:00:35 +0300

    NODE-730: fixing unit tests

--- src/test/scala/scorex/transaction/smart/script/ScriptV1Test.scala
+++ src/test/scala/scorex/transaction/smart/script/ScriptV1Test.scala
@@ -45,16 +45,18 @@ class ScriptV1Test extends PropSpec with PropertyChecks with Matchers with Typed
   property("ScriptV1.apply should deny too big scripts") {
     val bigSum = (1 to 100).foldLeft[EXPR](CONST_LONG(0)) { (r, i) =>
       FUNCTION_CALL(
-        function = FunctionHeader(name = "+", List(FHT.LONG, FHT.LONG)),
+        function = FunctionHeader(name = "==", List(FHT.LONG, FHT.LONG)),
         args = List(r, CONST_LONG(i)),
         LONG
       )
     }
-    val expr = FUNCTION_CALL(
-      function = FunctionHeader(name = "==", List(FHT.LONG, FHT.LONG)),
-      args = List(CONST_LONG(1), bigSum),
-      BOOLEAN
-    )
+    val expr = (1 to 9).foldLeft[EXPR](CONST_LONG(0)) { (r, i) =>
+      FUNCTION_CALL(
+        function = FunctionHeader(name = "==", List(FHT.LONG, FHT.LONG)),
+        args = List(r, bigSum),
+        BOOLEAN
+      )
+    }
 
     ScriptV1(expr) should produce("Script is too large")
   }
