910a7b3270b021538ed09d579499b58b8c217136
peterz <peterz@rambler.ru>
2018-05-31 17:22:04 +0300

    NODE-777 Some tests

--- lang/jvm/src/test/scala/com/wavesplatform/lang/v1/ScriptEstimatorTest.scala
+++ lang/jvm/src/test/scala/com/wavesplatform/lang/v1/ScriptEstimatorTest.scala
@@ -18,22 +18,70 @@ class ScriptEstimatorTest extends PropSpec with PropertyChecks with Matchers wit
     ScriptEstimator(Map(sumHeader -> 1L), expr) shouldBe 'right
   }
 
-  property("handles let expression correctly") {
-    val Plus = FunctionHeader("+", List(FunctionHeaderType.LONG, FunctionHeaderType.LONG)) ///refac?
-    val expr =
-      BLOCK(
-        LET("a", CONST_LONG(1)),
-        BLOCK(LET("b", CONST_LONG(2)), BLOCK(LET("c", FUNCTION_CALL(Plus, List(REF("a", LONG), REF("b", LONG)), LONG)), REF("c", LONG), LONG), LONG),
-        LONG
-      )
-    val estimate = ScriptEstimator(Map(Plus -> 3L), expr)
-    estimate shouldBe 'right
-    estimate.right.get shouldBe 26
-  }
-
   property("handles const expression correctly") {
     val estimate = ScriptEstimator(Map.empty, FALSE)
-    estimate shouldBe 'right
     estimate.right.get shouldBe 1
+  }
+
+  property("handles let expression correctly") {
+    val Plus     = FunctionHeader("+", List(FunctionHeaderType.LONG, FunctionHeaderType.LONG)) ///refac?
+    val expr     = BLOCK(LET("t", TRUE), REF("t", BOOLEAN), BOOLEAN)
+    val estimate = ScriptEstimator(Map(Plus -> 3L), expr)
+    estimate.right.get shouldBe 8
+  }
+
+  property("evaluates let statement lazily") {
+    val eager = BLOCK(LET("txtype", GETTER(REF("tx", CASETYPEREF("Transaction")), "type", LONG)), REF("txtype", LONG), BOOLEAN)
+    ScriptEstimator(Map.empty, eager).right.get shouldBe 11
+
+    val lzy = BLOCK(LET("txtype", GETTER(REF("tx", CASETYPEREF("Transaction")), "type", LONG)), TRUE, BOOLEAN) // does not reference `txtype`
+    ScriptEstimator(Map.empty, lzy).right.get shouldBe 6
+  }
+
+  property("evaluates if statement lazily") {
+    val GetElement = FunctionHeader("GetElement", List(FunctionHeaderType.LIST(FunctionHeaderType.BYTEVECTOR), FunctionHeaderType.LONG))
+    val Gt         = FunctionHeader(">", List(FunctionHeaderType.LONG, FunctionHeaderType.LONG))
+    val Size       = FunctionHeader("size", List(FunctionHeaderType.BYTEVECTOR))
+    val txRef      = REF("tx", CASETYPEREF("Transaction"))
+
+    val script = BLOCK(
+      LET("a", FUNCTION_CALL(GetElement, List(GETTER(txRef, "proofs", LIST(BYTEVECTOR)), CONST_LONG(0)), BYTEVECTOR)),
+      BLOCK(
+        LET("b", FUNCTION_CALL(GetElement, List(GETTER(txRef, "proofs", LIST(BYTEVECTOR)), CONST_LONG(1)), BYTEVECTOR)),
+        BLOCK(
+          LET("c",
+              IF(FUNCTION_CALL(Gt, List(GETTER(txRef, "type", LONG), CONST_LONG(5)), BOOLEAN),
+                 REF("a", BYTEVECTOR),
+                 REF("b", BYTEVECTOR),
+                 BYTEVECTOR)),
+          FUNCTION_CALL(Gt, List(FUNCTION_CALL(Size, List(REF("c", BYTEVECTOR)), LONG), CONST_LONG(0)), BOOLEAN),
+          BOOLEAN
+        ),
+        BOOLEAN
+      ),
+      BOOLEAN
+    )
+    val estimate = ScriptEstimator(Map(GetElement -> 10L, Gt -> 10L, Size -> 10L), script)
+    estimate.right.get shouldBe 71
+  }
+
+  property("///") {
+    val Plus  = FunctionHeader("+", List(FunctionHeaderType.LONG, FunctionHeaderType.LONG))
+    val Gt    = FunctionHeader(">", List(FunctionHeaderType.LONG, FunctionHeaderType.LONG))
+    val txRef = REF("tx", CASETYPEREF("Transaction"))
+
+    val script = BLOCK(
+      LET("a", GETTER(txRef, "type", LONG)),
+      BLOCK(
+        LET("b", GETTER(txRef, "amount", LONG)),
+        BLOCK(LET("c", FUNCTION_CALL(Plus, List(REF("a", LONG), REF("b", LONG)), LONG)),
+              FUNCTION_CALL(Gt, List(REF("b", LONG), CONST_LONG(0)), BOOLEAN),
+              BOOLEAN),
+        BOOLEAN
+      ),
+      BOOLEAN
+    )
+    val estimate = ScriptEstimator(Map(Plus -> 10L, Gt -> 10L), script)
+    estimate.right.get shouldBe 32
   }
 }
--- lang/shared/src/main/scala/com/wavesplatform/lang/v1/ScriptEstimator.scala
+++ lang/shared/src/main/scala/com/wavesplatform/lang/v1/ScriptEstimator.scala
@@ -8,52 +8,54 @@ object ScriptEstimator {
   def apply(functionCosts: Map[FunctionHeader, Long], t: EXPR): Either[String, Long] = {
     type Result[T] = EitherT[Coeval, String, T]
 
-    def aux(t: Result[EXPR], syms: Map[String, Long]): Result[(Long, Set[String])] = t.flatMap {
+    def aux(t: Result[EXPR], syms: Map[String, Long]): Result[(Long, Map[String, Long])] = t.flatMap {
       case _: CONST_LONG | _: CONST_BYTEVECTOR | _: CONST_STRING | TRUE | FALSE =>
         Console.err.println(s"CONST")///
-        EitherT.pure((1, Set.empty))
+        EitherT.pure((1, syms))
 
       case t: GETTER =>
         Console.err.println(s"GETTER")///
-        aux(EitherT.pure(t.expr), syms).map { case (comp, used) => (comp + 2, used) }
+        aux(EitherT.pure(t.expr), syms).map { case (comp, out) => (comp + 2, out) }
 
       case t: BLOCK =>
         Console.err.println(s"BLK")///
         for {
           let <- aux(EitherT.pure(t.let.value), syms)
           (letComp, letSyms) = let
-          body <- aux(EitherT.pure(t.body), syms -- letSyms + (t.let.name -> letComp))
+          body <- aux(EitherT.pure(t.body), letSyms + (t.let.name -> letComp)) /// let a=b+c; b
           (bodyComp, bodySyms) = body
-        } yield (bodyComp + 5, letSyms ++ bodySyms) ///+5?
+        } yield (bodyComp + 5, bodySyms)
 
       case t: IF =>
         Console.err.println(s"IF")///
         for {
           cond <- aux(EitherT.pure(t.cond), syms)
           (condComp, condSyms) = cond
-          right <- aux(EitherT.pure(t.ifTrue), syms -- condSyms)
+          right <- aux(EitherT.pure(t.ifTrue), condSyms)
           (rightComp, rightSyms) = right
-          left <- aux(EitherT.pure(t.ifFalse), syms -- condSyms)
+          left <- aux(EitherT.pure(t.ifFalse), condSyms)
           (leftComp, leftSyms) = left
           (bodyComp, bodySyms) = if (rightComp > leftComp) (rightComp, rightSyms) else (leftComp, leftSyms)
-        } yield (1 + condComp + bodyComp, condSyms ++ bodySyms)
+        } yield (1 + condComp + bodyComp, bodySyms)
+
       case t: REF =>
         Console.err.println(s"REF")///
-        EitherT.pure((2 + syms.getOrElse(t.key, 0L), Set(t.key)))
+        EitherT.pure((2 + syms.getOrElse(t.key, 0L), syms - t.key))
+
       case t: FUNCTION_CALL =>
         Console.err.println(s"FUNC ${t.function.name}(${t.args.size} args)")///
         for {
           callCost <- EitherT.fromOption[Coeval](functionCosts.get(t.function), s"Unknown function ${t.function}")
-          args <- t.args.foldLeft(EitherT.pure[Coeval, String]((0L, syms, Set.empty[String]))) { case (accEi, arg) =>
+          args <- t.args.foldLeft(EitherT.pure[Coeval, String]((0L, syms))) { case (accEi, arg) =>
             for {
               acc <- accEi
-              (accComp, accSyms, accUsed) = acc
-              call <- aux(EitherT.pure[Coeval, String](arg), accSyms)
-              (comp, used) = call
-            } yield (accComp + comp, accSyms -- used, accUsed ++ used)
+              (accComp, accSyms) = acc
+              v <- aux(EitherT.pure[Coeval, String](arg), accSyms)
+              (comp, out) = v
+            } yield (accComp + comp, out)
           }
-          (argsComp, _, argsUsed) = args
-        } yield (callCost + argsComp, argsUsed)
+          (argsComp, argsSyms) = args
+        } yield (callCost + argsComp, argsSyms)
     }
 
     aux(EitherT.pure(t), Map.empty).value().map(_._1)
