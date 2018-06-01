d684f6e5a6fc9e4122ca560ef0ffaf5158adf33c
peterz <peterz@rambler.ru>
2018-06-01 16:34:53 +0300

    NODE-777 Added compilation step to ScriptEstimatorTest

--- lang/jvm/src/test/scala/com/wavesplatform/lang/v1/ScriptEstimatorTest.scala
+++ lang/jvm/src/test/scala/com/wavesplatform/lang/v1/ScriptEstimatorTest.scala
@@ -1,8 +1,15 @@
 package com.wavesplatform.lang.v1
 
+import cats.data.EitherT
+import cats.syntax.semigroup._
 import com.wavesplatform.lang.Common._
 import com.wavesplatform.lang.v1.FunctionHeader.FunctionHeaderType
 import com.wavesplatform.lang.v1.compiler.Terms._
+import com.wavesplatform.lang.v1.compiler.{CompilerContext, CompilerV1}
+import com.wavesplatform.lang.v1.evaluator.ctx._
+import com.wavesplatform.lang.v1.evaluator.ctx.impl.PureContext
+import com.wavesplatform.lang.v1.evaluator.ctx.impl.waves.Types.transferTransactionType
+import com.wavesplatform.lang.v1.parser.Parser
 import com.wavesplatform.lang.v1.testing.ScriptGen
 import org.scalatest.prop.PropertyChecks
 import org.scalatest.{Matchers, PropSpec}
@@ -14,67 +21,52 @@ class ScriptEstimatorTest extends PropSpec with PropertyChecks with Matchers wit
 
   val FunctionCosts: Map[FunctionHeader, Long] = Map(Plus -> 100, Minus -> 10, Gt -> 10)
 
+  private val ctx: CompilerContext = {
+    // make up a `tx` object
+    val tx = CaseObj(transferTransactionType.typeRef, Map("amount" -> Val(LONG)(100000000)))
+    val txCtx = EvaluationContext(
+      caseTypeDefs = Map(transferTransactionType.name -> transferTransactionType),
+      letDefs = Map("tx"                              -> LazyVal(transferTransactionType.typeRef)(EitherT.pure(tx))),
+      functions = Map.empty
+    )
+    CompilerContext.fromEvaluationContext(PureContext.instance |+| txCtx, Map.empty)
+  }
+
+  private def compile(code: String): EXPR = {
+    val untyped = Parser(code).get.value
+    require(untyped.size == 1)
+    CompilerV1(ctx, untyped.head).right.get
+  }
+
   property("successful on very deep expressions(stack overflow check)") {
     val expr = (1 to 100000).foldLeft[EXPR](CONST_LONG(0)) { (acc, _) =>
       FUNCTION_CALL(Plus, List(CONST_LONG(1), acc), LONG)
     }
-
     ScriptEstimator(FunctionCosts, expr) shouldBe 'right
   }
 
   property("handles const expression correctly") {
-    ScriptEstimator(Map.empty, FALSE).right.get shouldBe 1
+    ScriptEstimator(Map.empty, compile("false")).right.get shouldBe 1
   }
 
   property("handles getter expression correctly") {
-    // tx.type
-    val expr = GETTER(REF("tx", CASETYPEREF("Transaction")), "type", LONG)
-    ScriptEstimator(Map.empty, expr).right.get shouldBe 2 + 2
+    ScriptEstimator(Map.empty, compile("tx.amount")).right.get shouldBe 2 + 2
   }
 
   property("evaluates let statement lazily") {
-    // let t = 1 + 1
-    // t
-    val eager = BLOCK(LET("x", FUNCTION_CALL(Plus, List(CONST_LONG(1), CONST_LONG(1)), LONG)), REF("x", LONG), LONG)
-    ScriptEstimator(FunctionCosts, eager).right.get shouldBe 109
+    val eager = "let t = 1+1; t"
+    ScriptEstimator(FunctionCosts, compile(eager)).right.get shouldBe 5 + 102 + 2
 
-    // let t = 1 + 1  // unused
-    // 2
-    val lzy = BLOCK(LET("x", FUNCTION_CALL(Plus, List(CONST_LONG(1), CONST_LONG(1)), LONG)), CONST_LONG(2), LONG)
-    ScriptEstimator(FunctionCosts, lzy).right.get shouldBe 6
+    val lzy = "let t = 1+1; 2" // `t` is unused
+    ScriptEstimator(FunctionCosts, compile(lzy)).right.get shouldBe 5 + 1
 
-    // let x = 2 + 2  // evaluated once only
-    // let y = x - x
-    // x - y
-    val onceOnly = BLOCK(
-      LET("x", FUNCTION_CALL(Plus, List(CONST_LONG(2), CONST_LONG(2)), LONG)),
-      BLOCK(LET("y", FUNCTION_CALL(Minus, List(REF("x", LONG), REF("x", LONG)), LONG)),
-            FUNCTION_CALL(Minus, List(REF("x", LONG), REF("y", LONG)), LONG),
-            LONG),
-      LONG
-    )
-    ScriptEstimator(FunctionCosts, onceOnly).right.get shouldBe 140
+    val onceOnly = "let x = 2+2; let y = x-x; x-y" // evaluated once only
+    ScriptEstimator(FunctionCosts, compile(onceOnly)).right.get shouldBe (5 + 102) + (5 + 14) + 14
   }
 
   property("ignores unused let statements") {
-    // let a = 1 + 2  // unused
-    // let b = 2
-    // let c = a + b  // unused
-    // b
-    val script = BLOCK(
-      LET("a", FUNCTION_CALL(Plus, List(CONST_LONG(1), CONST_LONG(2)), LONG)),
-      BLOCK(
-        LET("b", CONST_LONG(2)),
-        BLOCK(
-          LET("c", FUNCTION_CALL(Plus, List(REF("a", LONG), REF("b", LONG)), LONG)),
-          REF("b", LONG),
-          LONG
-        ),
-        LONG
-      ),
-      LONG
-    )
-    ScriptEstimator(FunctionCosts, script).right.get shouldBe 18
+    val script = "let a = 1+2; let b = 2; let c = a+b; b" // `a` and `c` are unused
+    ScriptEstimator(FunctionCosts, compile(script)).right.get shouldBe 5 + (5 + 1) + 5 + 2
   }
 
   property("recursive let statement") {
@@ -84,27 +76,7 @@ class ScriptEstimatorTest extends PropSpec with PropertyChecks with Matchers wit
   }
 
   property("evaluates if statement lazily") {
-    // let a = 1 + 2
-    // let b = 3 + 4
-    // let c = if tx.type > 5 then a else b
-    // c
-    val script = BLOCK(
-      LET("a", FUNCTION_CALL(Plus, List(CONST_LONG(1), CONST_LONG(2)), LONG)),
-      BLOCK(
-        LET("b", FUNCTION_CALL(Plus, List(CONST_LONG(3), CONST_LONG(4)), LONG)),
-        BLOCK(
-          LET("c",
-              IF(FUNCTION_CALL(Gt, List(GETTER(REF("tx", CASETYPEREF("Transaction")), "type", LONG), CONST_LONG(5)), BOOLEAN),
-                 REF("a", LONG),
-                 REF("b", LONG),
-                 LONG)),
-          REF("c", LONG),
-          LONG
-        ),
-        LONG
-      ),
-      LONG
-    )
-    ScriptEstimator(FunctionCosts, script).right.get shouldBe 137
+    val script = "let a = 1+2; let b = 3+4; let c = if (tx.amount > 5) then a else b; c"
+    ScriptEstimator(FunctionCosts, compile(script)).right.get shouldBe (5 + 102) + 5 + (5 + 16 + 2) + 2
   }
 }
