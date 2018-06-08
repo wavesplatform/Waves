a71ca23d8390b665fcd60b0c17fba88d2d7b986f
Alexandr Makoed <me@dsfn.gq>
2018-06-08 18:04:48 +0300

    NODE-813: Pos instead of start & end

--- lang/shared/src/main/scala/com/wavesplatform/lang/v1/parser/Expressions.scala
+++ lang/shared/src/main/scala/com/wavesplatform/lang/v1/parser/Expressions.scala
@@ -1,42 +1,59 @@
 package com.wavesplatform.lang.v1.parser
 
+import com.wavesplatform.lang.v1.parser.Expressions.Pos.FakePos
 import scodec.bits.ByteVector
 
 object Expressions {
 
-  trait Positioned {
-    def start: Int
+  sealed trait Pos { self =>
+    override def equals(obj: scala.Any): Boolean = {
+      obj == FakePos || obj == self
+    }
     def end: Int
+    def start: Int
+  }
+
+  object Pos {
+    def apply(start: Int, end: Int): Pos = RealPos(start, end)
+    final case class RealPos(start: Int, end: Int) extends Pos
+    private[lang] case object FakePos extends Pos {
+      override def start: Int = -1
+      override def end: Int   = -1
+    }
+  }
+
+  trait Positioned {
+    def position: Pos
   }
 
   sealed trait PART[+T] extends Positioned
   object PART {
-    case class VALID[T](start: Int, end: Int, v: T)           extends PART[T]
-    case class INVALID(start: Int, end: Int, message: String) extends PART[Nothing]
+    case class VALID[T](position: Pos, v: T)           extends PART[T]
+    case class INVALID(position: Pos, message: String) extends PART[Nothing]
   }
 
-  case class LET(start: Int, end: Int, name: PART[String], value: EXPR, types: Seq[PART[String]], allowShadowing: Boolean = false) extends Positioned
+  case class LET(position: Pos, name: PART[String], value: EXPR, types: Seq[PART[String]], allowShadowing: Boolean = false) extends Positioned
 
-  sealed trait EXPR                                                                    extends Positioned
-  case class CONST_LONG(start: Int, end: Int, value: Long)                             extends EXPR
-  case class GETTER(start: Int, end: Int, ref: EXPR, field: PART[String])              extends EXPR
-  case class CONST_BYTEVECTOR(start: Int, end: Int, value: PART[ByteVector])           extends EXPR
-  case class CONST_STRING(start: Int, end: Int, value: PART[String])                   extends EXPR
-  case class BINARY_OP(start: Int, end: Int, a: EXPR, kind: BinaryOperation, b: EXPR)  extends EXPR
-  case class BLOCK(start: Int, end: Int, let: LET, body: EXPR)                         extends EXPR
-  case class IF(start: Int, end: Int, cond: EXPR, ifTrue: EXPR, ifFalse: EXPR)         extends EXPR
-  case class REF(start: Int, end: Int, key: PART[String])                              extends EXPR
-  case class TRUE(start: Int, end: Int)                                                extends EXPR
-  case class FALSE(start: Int, end: Int)                                               extends EXPR
-  case class FUNCTION_CALL(start: Int, end: Int, name: PART[String], args: List[EXPR]) extends EXPR
-  case class MATCH_CASE(start: Int, end: Int, newVarName: Option[PART[String]], types: Seq[PART[String]], expr: EXPR)
-  case class MATCH(start: Int, end: Int, expr: EXPR, cases: Seq[MATCH_CASE]) extends EXPR
+  sealed trait EXPR                                                             extends Positioned
+  case class CONST_LONG(position: Pos, value: Long)                             extends EXPR
+  case class GETTER(position: Pos, ref: EXPR, field: PART[String])              extends EXPR
+  case class CONST_BYTEVECTOR(position: Pos, value: PART[ByteVector])           extends EXPR
+  case class CONST_STRING(position: Pos, value: PART[String])                   extends EXPR
+  case class BINARY_OP(position: Pos, a: EXPR, kind: BinaryOperation, b: EXPR)  extends EXPR
+  case class BLOCK(position: Pos, let: LET, body: EXPR)                         extends EXPR
+  case class IF(position: Pos, cond: EXPR, ifTrue: EXPR, ifFalse: EXPR)         extends EXPR
+  case class REF(position: Pos, key: PART[String])                              extends EXPR
+  case class TRUE(position: Pos)                                                extends EXPR
+  case class FALSE(position: Pos)                                               extends EXPR
+  case class FUNCTION_CALL(position: Pos, name: PART[String], args: List[EXPR]) extends EXPR
+  case class MATCH_CASE(position: Pos, newVarName: Option[PART[String]], types: Seq[PART[String]], expr: EXPR)
+  case class MATCH(position: Pos, expr: EXPR, cases: Seq[MATCH_CASE]) extends EXPR
 
-  case class INVALID(start: Int, end: Int, message: String) extends EXPR
+  case class INVALID(position: Pos, message: String) extends EXPR
   implicit class PartOps[T](val self: PART[T]) extends AnyVal {
     def toEither: Either[String, T] = self match {
-      case Expressions.PART.VALID(_, _, x)         => Right(x)
-      case Expressions.PART.INVALID(s, e, message) => Left(s"Can't compile an invalid instruction: $message in $s-$e")
+      case Expressions.PART.VALID(_, x)         => Right(x)
+      case Expressions.PART.INVALID(p, message) => Left(s"Can't compile an invalid instruction: $message in $s-$e")
     }
   }
 
