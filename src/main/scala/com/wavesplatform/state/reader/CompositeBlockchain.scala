1ccf5b96f36cfbe96e1ad77d8145c27092b0b4e5
Sergey Nazarov <phearnot@renee.ru>
2018-06-22 19:15:46 +0300

    NODE-864: Validate transactions against proper block timestamp

--- src/main/scala/com/wavesplatform/state/reader/CompositeBlockchain.scala
+++ src/main/scala/com/wavesplatform/state/reader/CompositeBlockchain.scala
@@ -116,6 +116,9 @@ class CompositeBlockchain(inner: Blockchain, maybeDiff: => Option[Diff]) extends
 
   override def containsTransaction(id: ByteStr): Boolean = diff.transactions.contains(id) || inner.containsTransaction(id)
 
+  override def forgetTransactions(pred: (AssetId, Long) => Boolean) = inner.forgetTransactions(pred)
+  override def learnTransactions(values: Map[AssetId, Long]): Unit  = inner.learnTransactions(values)
+
   override def filledVolumeAndFee(orderId: ByteStr): VolumeAndFee =
     diff.orderFills.get(orderId).orEmpty.combine(inner.filledVolumeAndFee(orderId))
 
