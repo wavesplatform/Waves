04707d6819eaefbacbc2c4db99c1e7f12a4ca0a0
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2018-07-16 16:41:11 +0300

    NODE-448 Implemented a first version of elimination of already received transactions

--- src/main/scala/com/wavesplatform/network/LegacyFrameCodec.scala
+++ src/main/scala/com/wavesplatform/network/LegacyFrameCodec.scala
@@ -1,8 +1,11 @@
 package com.wavesplatform.network
 
 import java.util
+import java.util.concurrent.TimeUnit
 
+import com.google.common.cache.CacheBuilder
 import com.wavesplatform.crypto
+import com.wavesplatform.utils.Base64
 import io.netty.buffer.ByteBuf
 import io.netty.buffer.Unpooled._
 import io.netty.channel.ChannelHandlerContext
@@ -10,6 +13,7 @@ import io.netty.handler.codec.ByteToMessageCodec
 import scorex.network.message.Message._
 import scorex.utils.ScorexLogging
 
+import scala.concurrent.duration.DurationInt
 import scala.util.control.NonFatal
 
 class LegacyFrameCodec(peerDatabase: PeerDatabase) extends ByteToMessageCodec[RawBytes] with ScorexLogging {
@@ -17,7 +21,12 @@ class LegacyFrameCodec(peerDatabase: PeerDatabase) extends ByteToMessageCodec[Ra
   import BasicMessagesRepo.specsByCodes
   import LegacyFrameCodec._
 
-  override def decode(ctx: ChannelHandlerContext, in: ByteBuf, out: util.List[AnyRef]) =
+  private val cache = CacheBuilder
+    .newBuilder()
+    .expireAfterWrite(5.minutes.toMillis, TimeUnit.MILLISECONDS)
+    .build[String, Object]()
+
+  override def decode(ctx: ChannelHandlerContext, in: ByteBuf, out: util.List[AnyRef]): Unit =
     try {
       require(in.readInt() == Magic, "invalid magic number")
 
@@ -29,24 +38,32 @@ class LegacyFrameCodec(peerDatabase: PeerDatabase) extends ByteToMessageCodec[Ra
       require(length <= spec.maxLength, s"${spec.messageName} message length $length exceeds ${spec.maxLength}")
 
       val dataBytes = new Array[Byte](length)
-      if (length > 0) {
+      val pushToPipeline = length == 0 || {
         val declaredChecksum = in.readSlice(ChecksumLength)
         in.readBytes(dataBytes)
-        val actualChecksum = wrappedBuffer(crypto.fastHash(dataBytes), 0, ChecksumLength)
+        val rawChecksum    = crypto.fastHash(dataBytes)
+        val actualChecksum = wrappedBuffer(rawChecksum, 0, ChecksumLength)
 
         require(declaredChecksum.equals(actualChecksum), "invalid checksum")
         actualChecksum.release()
 
+        spec != TransactionSpec || {
+          val actualChecksumStr = Base64.encode(rawChecksum)
+          if (cache.getIfPresent(actualChecksumStr) == null) {
+            cache.put(actualChecksumStr, LegacyFrameCodec.dummy)
+            true
+          } else false
+        }
       }
 
-      out.add(RawBytes(code, dataBytes))
+      if (pushToPipeline) out.add(RawBytes(code, dataBytes))
     } catch {
       case NonFatal(e) =>
         log.warn(s"${id(ctx)} Malformed network message", e)
         peerDatabase.blacklistAndClose(ctx.channel(), s"Malformed network message: $e")
     }
 
-  override def encode(ctx: ChannelHandlerContext, msg: RawBytes, out: ByteBuf) = {
+  override def encode(ctx: ChannelHandlerContext, msg: RawBytes, out: ByteBuf): Unit = {
     out.writeInt(Magic)
     out.writeByte(msg.code)
     if (msg.data.length > 0) {
@@ -60,5 +77,6 @@ class LegacyFrameCodec(peerDatabase: PeerDatabase) extends ByteToMessageCodec[Ra
 }
 
 object LegacyFrameCodec {
-  val Magic = 0x12345678
+  val Magic         = 0x12345678
+  private val dummy = new Object()
 }
