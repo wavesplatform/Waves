32187d3f57176f21d602bbb5a8ed161b2e80e5de
Sergey Nazarov <phearnot@renee.ru>
2018-08-08 17:20:16 +0300

    Removed legacy codecs

--- src/main/scala/com/wavesplatform/matcher/MigrationTool.scala
+++ src/main/scala/com/wavesplatform/matcher/MigrationTool.scala
@@ -3,171 +3,25 @@ package com.wavesplatform.matcher
 import java.io.File
 import java.util.{HashMap => JHashMap}
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.google.common.base.Charsets.UTF_8
 import com.google.common.primitives.Shorts
 import com.typesafe.config.ConfigFactory
 import com.wavesplatform.crypto.DigestSize
 import com.wavesplatform.database.DBExt
-import com.wavesplatform.db.{AssetIdOrderIdSetCodec, OrderIdsCodec, OrderToTxIdsCodec, PortfolioCodec, openDB}
+import com.wavesplatform.db.openDB
 import com.wavesplatform.matcher.api.DBUtils
 import com.wavesplatform.matcher.model.{LimitOrder, OrderInfo}
 import com.wavesplatform.settings.{WavesSettings, loadConfig}
 import com.wavesplatform.state.{ByteStr, EitherExt2}
-import com.wavesplatform.utils.Base58
 import org.iq80.leveldb.DB
-import scorex.account.{Address, AddressScheme, PublicKeyAccount}
+import scorex.account.{Address, AddressScheme}
 import scorex.transaction.AssetId
-import scorex.transaction.assets.exchange.{AssetPair, Order, OrderType}
+import scorex.transaction.assets.exchange.AssetPair
 import scorex.utils.ScorexLogging
 
 import scala.collection.JavaConverters._
 
 object MigrationTool extends ScorexLogging {
-  private def loadOrderInfo(db: DB, om: ObjectMapper): Map[ByteStr, OrderInfo] = {
-    log.info("Loading order info")
-    val result = Map.newBuilder[ByteStr, OrderInfo]
-
-    db.iterateOver(SK.OrdersInfo.keyBytes) { e =>
-      val SK.OrdersInfo(id) = e.getKey
-      val t                 = om.readTree(e.getValue)
-      val oi                = OrderInfo(t.get("amount").asLong, t.get("filled").asLong, t.get("canceled").asBoolean, None, 0)
-      if (!oi.canceled && oi.amount != oi.filled) {
-        result += id -> oi
-      }
-    }
-
-    log.info("Loaded all order infos")
-    result.result()
-  }
-
-  private def parseOrderJson(om: ObjectMapper, bytes: Array[Byte]): Order = {
-    val t = om.readTree(bytes)
-    val p = t.get("assetPair")
-    Order(
-      PublicKeyAccount.fromBase58String(t.get("senderPublicKey").asText()).explicitGet(),
-      PublicKeyAccount.fromBase58String(t.get("matcherPublicKey").asText()).explicitGet(),
-      AssetPair
-        .createAssetPair(
-          p.get("amountAsset").asText("WAVES"),
-          p.get("priceAsset").asText("WAVES")
-        )
-        .get,
-      OrderType(t.get("orderType").asText()),
-      t.get("price").asLong(),
-      t.get("amount").asLong(),
-      t.get("timestamp").asLong(),
-      t.get("expiration").asLong(),
-      t.get("matcherFee").asLong(),
-      Base58.decode(t.get("signature").asText()).get
-    )
-  }
-
-  private def performMigration(db: DB): Unit = {
-    val addressOrders     = Seq.newBuilder[(Address, Seq[ByteStr])]
-    val orderTransactions = Seq.newBuilder[(ByteStr, Set[String])]
-    val orders            = Map.newBuilder[ByteStr, Order]
-    val transactions      = Seq.newBuilder[(ByteStr, Array[Byte])]
-    val portfolios        = Seq.newBuilder[(Address, Map[String, Long])]
-
-    val om = new ObjectMapper()
-
-    val orderInfo = loadOrderInfo(db, om)
-
-    val iterator = db.iterator()
-    try {
-      iterator.seekToFirst()
-      while (iterator.hasNext) {
-        val entry = iterator.next()
-        entry.getKey match {
-          case SK.OrdersInfo(_) => // order info has already been processed
-          case SK.AddressToOrders(address) =>
-            addressOrders += address -> OrderIdsCodec
-              .decode(entry.getValue)
-              .explicitGet()
-              .value
-              .map(ByteStr.decodeBase58(_).get)
-              .filter(orderInfo.keySet)
-              .toSeq
-          case SK.OrdersToTxIds(orderId) =>
-            if (orderInfo.contains(orderId)) {
-              orderTransactions += orderId -> OrderToTxIdsCodec.decode(entry.getValue).explicitGet().value
-            }
-          case SK.Orders(orderId) =>
-            if (orderInfo.contains(orderId)) {
-              orders += orderId -> parseOrderJson(om, entry.getValue)
-            }
-          case SK.Transactions(txId) =>
-            transactions += txId -> entry.getValue
-          case SK.AddressPortfolio(address) =>
-            portfolios += address -> PortfolioCodec.decode(entry.getValue).explicitGet().value
-          case SK.AddressToActiveOrders(_) =>
-          case _                           =>
-        }
-      }
-    } finally iterator.close()
-
-    val allOrders            = orders.result()
-    val allTransactions      = transactions.result()
-    val allAddressOrders     = addressOrders.result()
-    val allOpenVolume        = portfolios.result()
-    val allOrderTransactions = orderTransactions.result()
-    log.info(s"""Done reading data:
-                |
-        |order infos:        ${orderInfo.size}
-                |address orders:     ${allAddressOrders.length}
-                |order transactions: ${allOrderTransactions.length}
-                |orders:             ${allOrders.size}
-                |transactions:       ${allTransactions.length}
-                |portfolios:         ${allOpenVolume.length}
-      """.stripMargin)
-
-    db.readWrite { rw =>
-      log.info("Saving orders")
-      for ((id, order) <- allOrders) {
-        rw.put(MatcherKeys.order(id), Some(order))
-      }
-      log.info("Saving order info")
-      for ((id, info) <- orderInfo) {
-        rw.put(MatcherKeys.orderInfo(id), info)
-      }
-      log.info("Saving orders for address")
-      for ((address, orderIds) <- allAddressOrders) {
-        val activeOrderIds = orderIds.filter(orderInfo.keySet)
-        rw.put(MatcherKeys.addressOrdersSeqNr(address), activeOrderIds.size)
-        for ((id, offset) <- activeOrderIds.zipWithIndex) {
-          rw.put(MatcherKeys.addressOrders(address, offset + 1), Some(OrderAssets(id, allOrders(id).getSpendAssetId)))
-        }
-      }
-      log.info("Saving open volume")
-      for ((address, ov) <- allOpenVolume) {
-        val assetCount = ov.size
-        rw.put(MatcherKeys.openVolumeSeqNr(address), assetCount)
-        for (((assetIdStr, v), offset) <- ov.zipWithIndex) {
-          val assetId = AssetPair.extractAssetId(assetIdStr).get
-          rw.put(MatcherKeys.openVolume(address, assetId), Some(v))
-          rw.put(MatcherKeys.openVolumeAsset(address, offset + 1), assetId)
-        }
-      }
-      log.info("Saving order transactions")
-      for ((orderId, txIds) <- allOrderTransactions) {
-        val txCount = txIds.size
-        rw.put(MatcherKeys.orderTxIdsSeqNr(orderId), txCount)
-        for ((id, offset) <- txIds.flatMap(ByteStr.decodeBase58(_).toOption).zipWithIndex) {
-          rw.put(MatcherKeys.orderTxId(orderId, offset + 1), id)
-        }
-      }
-      log.info("Saving transactions")
-      for ((id, txBytes) <- allTransactions) {
-        rw.put(MatcherKeys.exchangeTransaction(id).keyBytes, txBytes)
-      }
-
-      log.info("Writing changes")
-    }
-
-    log.info("Migration complete")
-  }
-
   private def collectStats(db: DB): Unit = {
     log.info("Collecting stats")
     val iterator = db.iterator()
@@ -210,60 +64,13 @@ object MigrationTool extends ScorexLogging {
   }
 
   private def deleteLegacyEntries(db: DB): Unit = {
-    val iterator     = db.iterator()
-    val prefix       = "matcher:".getBytes(UTF_8)
     val keysToDelete = Seq.newBuilder[Array[Byte]]
-    iterator.seekToFirst()
-    try {
-      while (iterator.hasNext) {
-        val e = iterator.next()
-        if (e.getKey.startsWith(prefix)) keysToDelete += e.getKey
-      }
-    } finally iterator.close()
 
-    db.readWrite { rw =>
-      for (k <- keysToDelete.result()) rw.delete(k)
+    db.iterateOver("matcher:".getBytes(UTF_8)) { e =>
+      keysToDelete += e.getKey
     }
-  }
 
-  private def migrateActiveOrders(db: DB): Unit = {
-    log.info("Migrating active orders")
-    val prefix        = "matcher:a-addr-orders:".getBytes(UTF_8)
-    val iterator      = db.iterator()
-    val addressOrders = Seq.newBuilder[(Address, Set[OrderAssets])]
-    try {
-      iterator.seek(prefix)
-      while (iterator.hasNext && iterator.peekNext().getKey.startsWith(prefix)) {
-        val e                              = iterator.next()
-        val SK.AddressToActiveOrders(addr) = e.getKey
-        val orderIds = AssetIdOrderIdSetCodec.decode(e.getValue).explicitGet().value.map {
-          case (assetId, orderIdStr) => OrderAssets(ByteStr.decodeBase58(orderIdStr).get, assetId)
-        }
-
-        addressOrders += addr -> orderIds
-      }
-    } finally iterator.close()
-
-    val r = addressOrders.result()
-
-    log.info(s"Collected active orders for ${r.size} addresses")
-
-    db.readWrite { rw =>
-      for ((addr, migratedOrders) <- r) {
-        val currentOrderCount = rw.get(MatcherKeys.addressOrdersSeqNr(addr))
-        val currentOrderAssets = (1 to currentOrderCount).flatMap { i =>
-          rw.get(MatcherKeys.addressOrders(addr, i))
-        }.toSet
-
-        val ordersToAdd = migratedOrders.diff(currentOrderAssets)
-        if (ordersToAdd.nonEmpty) {
-          rw.put(MatcherKeys.addressOrdersSeqNr(addr), currentOrderCount + ordersToAdd.size)
-          for ((oa, offset) <- ordersToAdd.zipWithIndex) {
-            rw.put(MatcherKeys.addressOrders(addr, currentOrderCount + offset + 1), Some(oa))
-          }
-        }
-      }
-    }
+    db.readWrite(rw => keysToDelete.result().foreach(rw.delete))
   }
 
   private def recalculateReservedBalance(db: DB): Unit = {
@@ -321,7 +128,7 @@ object MigrationTool extends ScorexLogging {
     val allReservedBalances = addresses.result().map(a => a -> DBUtils.reservedBalance(db, a)).toMap
 
     if (allReservedBalances.size != calculatedReservedBalances.size()) {
-      log.info(s"Calculated: ${calculatedReservedBalances.size()}, stored: ${allReservedBalances.size}")
+      log.info(s"Calculated balances: ${calculatedReservedBalances.size()}, stored balances: ${allReservedBalances.size}")
     }
 
     val corrections = Seq.newBuilder[((Address, Option[AssetId]), Long)]
@@ -388,13 +195,8 @@ object MigrationTool extends ScorexLogging {
       override val chainId: Byte = settings.blockchainSettings.addressSchemeCharacter.toByte
     }
 
-    if (args.length < 2) {
-      log.info("Performing migration")
-      performMigration(db)
-    } else if (args(1) == "stats") {
+    if (args(1) == "stats") {
       collectStats(db)
-    } else if (args(1) == "active-orders") {
-      migrateActiveOrders(db)
     } else if (args(1) == "ao") {
       val o = DBUtils.ordersByAddress(db, Address.fromString(args(2)).explicitGet(), Set.empty, false, Int.MaxValue)
       println(o.mkString("\n"))
@@ -407,7 +209,13 @@ object MigrationTool extends ScorexLogging {
     } else if (args(1) == "ddd") {
       log.warn("DELETING LEGACY ENTRIES")
       deleteLegacyEntries(db)
+      log.info("Finished deleting legacy entries")
+    } else if (args(1) == "compact") {
+      log.info("Compacting database")
+      db.compactRange(null, null)
+      log.info("Compaction complete")
     }
+
     db.close()
   }
 
