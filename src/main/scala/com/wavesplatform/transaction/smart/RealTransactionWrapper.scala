a5104edce6385023781f2b17923911441d2c238c
Mike Potanin <mpotanin@wavesplatform.com>
2018-08-08 12:44:02 +0300

    NODE-961-process-proofs-from-order bodyBytes for order.

--- src/main/scala/com/wavesplatform/transaction/smart/RealTransactionWrapper.scala
+++ src/main/scala/com/wavesplatform/transaction/smart/RealTransactionWrapper.scala
@@ -49,7 +49,7 @@ object RealTransactionWrapper {
       timestamp = o.timestamp,
       expiration = o.expiration,
       matcherFee = o.matcherFee,
-      bodyBytes = ByteVector(o.bytes()), //TODO: Change to real bodyBytes
+      bodyBytes = ByteVector(o.toSign),
       proofs = o.proofs.proofs.map(a => ByteVector(a.arr)).toIndexedSeq
     )
 
