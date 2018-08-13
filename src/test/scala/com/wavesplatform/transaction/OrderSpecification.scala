a23a0de7fded582c54c9e636b594eab5734694ab
Ilya Smagin <ilya.smagin@gmail.com>
2018-08-13 14:18:52 +0300

    NODE-962: fix unit test

--- src/test/scala/com/wavesplatform/transaction/OrderSpecification.scala
+++ src/test/scala/com/wavesplatform/transaction/OrderSpecification.scala
@@ -94,28 +94,26 @@ class OrderSpecification extends PropSpec with PropertyChecks with Matchers with
   }
 
   property("Order signature validation") {
+    val err = "proof doesn't validate as signature"
     forAll(orderGen, accountGen) {
       case (order, pka) =>
         Verifier.verifyAsEllipticCurveSignature(order) shouldBe an[Right[_, _]]
-        Verifier.verifyAsEllipticCurveSignature(order.updateSender(pka)) should produce("Proofs cannot be verified as signature")
-        Verifier.verifyAsEllipticCurveSignature(order.updateMatcher(pka)) should produce("Proofs cannot be verified as signature")
+        Verifier.verifyAsEllipticCurveSignature(order.updateSender(pka)) should produce(err)
+        Verifier.verifyAsEllipticCurveSignature(order.updateMatcher(pka)) should produce(err)
         val assetPair = order.assetPair
         Verifier.verifyAsEllipticCurveSignature(
           order
             .updatePair(assetPair.copy(
-              amountAsset = assetPair.amountAsset.map(Array(0: Byte) ++ _.arr).orElse(Some(Array(0: Byte))).map(ByteStr(_))))) should produce(
-          "Proofs cannot be verified as signature")
+              amountAsset = assetPair.amountAsset.map(Array(0: Byte) ++ _.arr).orElse(Some(Array(0: Byte))).map(ByteStr(_))))) should produce(err)
         Verifier.verifyAsEllipticCurveSignature(order
           .updatePair(assetPair.copy(priceAsset = assetPair.priceAsset.map(Array(0: Byte) ++ _.arr).orElse(Some(Array(0: Byte))).map(ByteStr(_))))) should produce(
-          "Proofs cannot be verified as signature")
-        Verifier.verifyAsEllipticCurveSignature(order.updateType(OrderType.reverse(order.orderType))) should produce(
-          "Proofs cannot be verified as signature")
-        Verifier.verifyAsEllipticCurveSignature(order.updatePrice(order.price + 1)) should produce("Proofs cannot be verified as signature")
-        Verifier.verifyAsEllipticCurveSignature(order.updateAmount(order.amount + 1)) should produce("Proofs cannot be verified as signature")
-        Verifier.verifyAsEllipticCurveSignature(order.updateExpiration(order.expiration + 1)) should produce("Proofs cannot be verified as signature")
-        Verifier.verifyAsEllipticCurveSignature(order.updateFee(order.matcherFee + 1)) should produce("Proofs cannot be verified as signature")
-        Verifier.verifyAsEllipticCurveSignature(order.updateProofs(Proofs(Seq(ByteStr(pka.publicKey ++ pka.publicKey))))) should produce(
-          "proof doesn't validate as signature")
+          err)
+        Verifier.verifyAsEllipticCurveSignature(order.updateType(OrderType.reverse(order.orderType))) should produce(err)
+        Verifier.verifyAsEllipticCurveSignature(order.updatePrice(order.price + 1)) should produce(err)
+        Verifier.verifyAsEllipticCurveSignature(order.updateAmount(order.amount + 1)) should produce(err)
+        Verifier.verifyAsEllipticCurveSignature(order.updateExpiration(order.expiration + 1)) should produce(err)
+        Verifier.verifyAsEllipticCurveSignature(order.updateFee(order.matcherFee + 1)) should produce(err)
+        Verifier.verifyAsEllipticCurveSignature(order.updateProofs(Proofs(Seq(ByteStr(pka.publicKey ++ pka.publicKey))))) should produce(err)
     }
   }
 
