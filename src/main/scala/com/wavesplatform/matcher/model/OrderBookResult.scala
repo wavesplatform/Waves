580881ae92bd5d45ab04c911bb008899d2c83ae6
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2018-08-21 17:23:44 +0300

    NODE-1075 Unit tests for OrderBookSnapshotHttpCache

--- src/main/scala/com/wavesplatform/matcher/model/OrderBookResult.scala
+++ src/main/scala/com/wavesplatform/matcher/model/OrderBookResult.scala
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.SerializerProvider
 import com.fasterxml.jackson.databind.annotation.JsonSerialize
 import com.fasterxml.jackson.databind.ser.std.StdSerializer
 import com.wavesplatform.matcher.api.JsonSerializer
-import play.api.libs.json.{Json, Writes}
 import scorex.transaction.assets.exchange.AssetPair
 import scorex.utils.NTP
 
@@ -15,29 +14,6 @@ case class OrderBookResult(timestamp: Long, pair: AssetPair, bids: Seq[LevelAgg]
 object OrderBookResult {
 
   def empty(pair: AssetPair) = OrderBookResult(NTP.correctedTime(), pair, Seq.empty, Seq.empty)
-
-  implicit val assetPairWrites = new Writes[AssetPair] {
-    def writes(pair: AssetPair) = Json.obj(
-      "amountAsset" -> pair.amountAssetStr,
-      "priceAsset"  -> pair.priceAssetStr
-    )
-  }
-
-  implicit val levelAggtWrites = new Writes[LevelAgg] {
-    def writes(a: LevelAgg) = Json.obj(
-      "price"  -> a.price,
-      "amount" -> a.amount
-    )
-  }
-
-  implicit val OrderBookResultWrites = new Writes[OrderBookResult] {
-    def writes(ob: OrderBookResult) = Json.obj(
-      "timestamp" -> ob.timestamp,
-      "pair"      -> ob.pair,
-      "bids"      -> ob.bids,
-      "asks"      -> ob.asks
-    )
-  }
 
   def toJson(x: OrderBookResult): String = JsonSerializer.serialize(x)
 
