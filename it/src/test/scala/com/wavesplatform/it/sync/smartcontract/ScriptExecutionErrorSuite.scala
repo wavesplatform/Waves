b260c3ffe45cfaf2ab94b412cb749e7172b01d48
Alexandr Makoed <me@dsfn.gq>
2018-08-27 14:42:42 +0300

    Simple test for custom throw(..) message added

--- it/src/test/scala/com/wavesplatform/it/sync/smartcontract/ScriptExecutionErrorSuite.scala
+++ it/src/test/scala/com/wavesplatform/it/sync/smartcontract/ScriptExecutionErrorSuite.scala
@@ -1,21 +1,61 @@
 package com.wavesplatform.it.sync.smartcontract
 
-import com.wavesplatform.it.api.SyncHttpApi.assertBadRequestAndResponse
+import com.wavesplatform.it.api.SyncHttpApi.{assertBadRequestAndResponse, _}
 import com.wavesplatform.it.sync.minFee
 import com.wavesplatform.it.transactions.BaseTransactionSuite
 import com.wavesplatform.lang.v1.FunctionHeader
-import com.wavesplatform.lang.v1.compiler.Terms
+import com.wavesplatform.lang.v1.compiler.{CompilerV1, Terms}
+import com.wavesplatform.state._
 import com.wavesplatform.transaction.smart.SetScriptTransaction
 import com.wavesplatform.transaction.smart.script.v1.ScriptV1
+import com.wavesplatform.utils.dummyCompilerContext
 import org.scalatest.CancelAfterFailure
 import play.api.libs.json.JsNumber
-import com.wavesplatform.it.api.SyncHttpApi._
-import com.wavesplatform.state._
 
 class ScriptExecutionErrorSuite extends BaseTransactionSuite with CancelAfterFailure {
 
   private val acc0 = pkByAddress(firstAddress)
   private val acc1 = pkByAddress(secondAddress)
+  private val acc2 = pkByAddress(thirdAddress)
+
+  test("custom throw message") {
+    val scriptSrc =
+      """
+        |match tx {
+        |  case t : TransferTransaction =>
+        |    let res = if isDefined(t.assetId) then extract(t.assetId) == base58'' else isDefined(t.assetId) == false
+        |    res
+        |  case t : SetScriptTransaction => true
+        |  case other => throw("Your transaction has incorrect type.")
+        |}
+      """.stripMargin
+
+    val compiler = new CompilerV1(dummyCompilerContext)
+
+    val compiled =
+      ScriptV1(
+        compiler
+          .compile(scriptSrc, Nil)
+          .explicitGet(),
+        false
+      ).explicitGet()
+
+    val tx = sender.signedBroadcast(
+      SetScriptTransaction
+        .selfSigned(
+          1,
+          acc2,
+          Some(compiled),
+          minFee,
+          System.currentTimeMillis()
+        )
+        .explicitGet()
+        .json() + ("type" -> JsNumber(SetScriptTransaction.typeId.toInt)))
+
+    nodes.waitForHeightAriseAndTxPresent(tx.id)
+
+    assertBadRequestAndResponse(sender.createAlias(acc2.address, "asdasdasdv", minFee), "Your transaction has incorrect type.")
+  }
 
   test("wrong type of script return value") {
     val script = ScriptV1(
