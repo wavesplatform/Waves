d0465d34cc55af325cac6ff6c5bf03fde62bd02a
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2018-09-20 16:50:22 +0300

    NODE-1128 DBUtils.ordersByAddress could return less than maxOrders orders if activeOnly = false

--- src/main/scala/com/wavesplatform/matcher/api/DBUtils.scala
+++ src/main/scala/com/wavesplatform/matcher/api/DBUtils.scala
@@ -21,22 +21,31 @@ object DBUtils {
     } yield (order, orderInfo)).take(maxOrders).sortBy { case (order, info) => (info.status, -order.timestamp) }
   }
 
-  def ordersByAddress(db: DB, address: Address, changedAssets: Set[Option[AssetId]], activeOnly: Boolean, maxOrders: Int): Seq[(Order, OrderInfo)] =
+  /**
+    * @param activeOnly If false - returns all active orders and the (maxOrders - allActiveOrders.size) recent of others
+    */
+  def ordersByAddress(db: DB, address: Address, activeOnly: Boolean, maxOrders: Int): Seq[(Order, OrderInfo)] =
     db.readOnly { ro =>
-      val fromIndex = db.get(MatcherKeys.addressOrdersSeqNr(address))
-      val toIndex = math.min(
-        math.max(1, fromIndex - maxOrders + 1),
-        db.get(MatcherKeys.addressOldestActiveOrderSeqNr(address)).getOrElse(Int.MaxValue)
-      )
+      val oldestActiveIdx = db.get(MatcherKeys.addressOldestActiveOrderSeqNr(address))
+      val lastIdx         = db.get(MatcherKeys.addressOrdersSeqNr(address))
 
-      (for {
-        idx             <- (fromIndex to toIndex by -1).view
+      val orders = for {
+        idx             <- (lastIdx to 1 by -1).toStream
         orderSpendAsset <- ro.get(MatcherKeys.addressOrders(address, idx))
-        if changedAssets.isEmpty || changedAssets(orderSpendAsset.spendAsset)
-        order <- ro.get(MatcherKeys.order(orderSpendAsset.orderId))
-        orderInfo = ro.get(MatcherKeys.orderInfo(orderSpendAsset.orderId))
-        if !(activeOnly && orderInfo.status.isFinal)
-      } yield (order, orderInfo)).sortBy { case (order, info) => (info.status, -order.timestamp) }.take(maxOrders)
+        orderInfo       <- ro.get(MatcherKeys.orderInfoOpt(orderSpendAsset.orderId))
+      } yield (orderSpendAsset.orderId, orderInfo)
+
+      val activeNumber        = oldestActiveIdx.fold(0)(x => math.min(lastIdx - x + 1, maxOrders))
+      val (finalized, active) = orders.partition(_._2.status.isFinal)
+      val activeEager         = active.take(activeNumber).force
+      val finalizedEager      = if (activeOnly) Stream.empty else finalized.take(maxOrders - activeEager.size).force
+
+      val r = for {
+        x     <- activeEager ++ finalizedEager
+        order <- ro.get(MatcherKeys.order(x._1))
+      } yield (order, x._2)
+
+      r.toVector.sortBy { case (order, info) => (info.status, -order.timestamp) }
     }
 
   def reservedBalance(db: DB, address: Address): Map[Option[AssetId], Long] = db.readOnly { ro =>
