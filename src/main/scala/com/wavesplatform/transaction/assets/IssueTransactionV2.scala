1e184ce6d4972450906ae9e6b6973c45143cdce4
Dmitry Kiselev <dkiselev@wavesplatform.com>
2018-10-09 13:29:42 +0300

    reuse

--- src/main/scala/com/wavesplatform/transaction/assets/IssueTransactionV2.scala
+++ src/main/scala/com/wavesplatform/transaction/assets/IssueTransactionV2.scala
@@ -40,17 +40,7 @@ case class IssueTransactionV2 private (version: Byte,
   override val bytes: Coeval[Array[Byte]] = Coeval.evalOnce(Bytes.concat(Array(0: Byte), bodyBytes(), proofs.bytes()))
 
   override val json = Coeval.evalOnce(
-    jsonBase() ++ Json.obj(
-      "version"     -> version,
-      "assetId"     -> assetId().base58,
-      "name"        -> new String(name, StandardCharsets.UTF_8),
-      "quantity"    -> quantity,
-      "reissuable"  -> reissuable,
-      "decimals"    -> decimals,
-      "description" -> new String(description, StandardCharsets.UTF_8),
-      "script"      -> script.map(_.bytes().base64),
-    ))
-
+    super.json() ++ Json.obj("script" -> script.map(_.bytes().base64)))
 }
 
 object IssueTransactionV2 extends TransactionParserFor[IssueTransactionV2] with TransactionParser.MultipleVersions {
