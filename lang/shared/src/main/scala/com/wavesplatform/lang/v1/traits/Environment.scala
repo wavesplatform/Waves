8ccc5451c35f0461251410469e9becd1af109dff
Mike Potanin <mpotanin@wavesplatform.com>
2018-10-24 15:12:20 +0300

    NODE-1242-remove-transactionHeightById Fix

--- lang/shared/src/main/scala/com/wavesplatform/lang/v1/traits/Environment.scala
+++ lang/shared/src/main/scala/com/wavesplatform/lang/v1/traits/Environment.scala
@@ -5,7 +5,7 @@ trait Environment {
   def networkByte: Byte
   def transaction: Tx
   def transactionById(id: Array[Byte]): Option[Tx]
-  def transactionHeightById(id: Array[Byte]): Option[Int]
+//  def transactionHeightById(id: Array[Byte]): Option[Int]
   def data(addressOrAlias: Recipient, key: String, dataType: DataType): Option[Any]
   def resolveAlias(name: String): Either[String, Recipient.Address]
   def accountBalanceOf(addressOrAlias: Recipient, assetId: Option[Array[Byte]]): Either[String, Long]
