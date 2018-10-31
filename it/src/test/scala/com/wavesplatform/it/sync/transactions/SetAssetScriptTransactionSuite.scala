9defa3bf542d812866e6736414ef771c560db06e
Mike Potanin <mpotanin@wavesplatform.com>
2018-10-31 16:25:53 +0300

    Constant for invalid assetId.

--- it/src/test/scala/com/wavesplatform/it/sync/transactions/SetAssetScriptTransactionSuite.scala
+++ it/src/test/scala/com/wavesplatform/it/sync/transactions/SetAssetScriptTransactionSuite.scala
@@ -105,7 +105,7 @@ class SetAssetScriptTransactionSuite extends BaseTransactionSuite {
     val invalidTxs = Seq(
       (sastx(timestamp = System.currentTimeMillis + 1.day.toMillis), "Transaction .* is from far future"),
       (sastx(fee = 9999999), "Fee .* does not exceed minimal value"),
-      (sastx(assetId = ByteStr.decodeBase64(testAssetWScript).get), "invalid.assetId"),
+      (sastx(assetId = ByteStr.decodeBase58("9ekQuYn92natMnMq8KqeGK3Nn7cpKd3BvPEGgD6fFyyz9ekQuYn92natMnMq8").get), "invalid.assetId"),
       (sastx(assetId = ByteStr.decodeBase58("9ekQuYn92natMnMq8KqeGK3Nn7cpKd3BvPEGgD6fFyyz").get), "Referenced assetId not found")
     )
 
