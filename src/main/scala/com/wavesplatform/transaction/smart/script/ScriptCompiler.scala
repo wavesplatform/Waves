78861ed0d2c84699c1ba832e93451c93a4a84467
Nastya.Urlapova <aurlapova@wavesplatform.com>
2018-10-31 12:55:02 +0300

    NODE-1214 Script versioning to V2

--- src/main/scala/com/wavesplatform/transaction/smart/script/ScriptCompiler.scala
+++ src/main/scala/com/wavesplatform/transaction/smart/script/ScriptCompiler.scala
@@ -3,13 +3,12 @@ package com.wavesplatform.transaction.smart.script
 import cats.implicits._
 import com.wavesplatform.lang.ScriptVersion
 import com.wavesplatform.lang.ScriptVersion.Versions.V1
-import com.wavesplatform.lang.directives.{Directive, DirectiveKey, DirectiveParser}
+import com.wavesplatform.lang.directives.{Directive, DirectiveParser, DirectiveKey}
 import com.wavesplatform.lang.v1.ScriptEstimator
 import com.wavesplatform.lang.v1.compiler.CompilerV1
 import com.wavesplatform.lang.v1.compiler.Terms.EXPR
 import com.wavesplatform.utils._
 import com.wavesplatform.transaction.smart.script.v1.ScriptV1
-
 import scala.util.{Failure, Success, Try}
 
 object ScriptCompiler extends ScorexLogging {
@@ -24,9 +23,9 @@ object ScriptCompiler extends ScorexLogging {
 
     for {
       ver        <- extractVersion(directives)
-      expr       <- tryCompile(scriptWithoutDirectives, ver, directives)
-      script     <- ScriptV1(ver, expr)
-      complexity <- ScriptEstimator(varNames(ver), functionCosts(ver), expr)
+      expr       <- tryCompile(scriptWithoutDirectives, ScriptVersion.Versions.V2, directives)
+      script     <- ScriptV1(ScriptVersion.Versions.V2, expr)
+      complexity <- ScriptEstimator(varNames(ScriptVersion.Versions.V2), functionCosts(ScriptVersion.Versions.V2), expr)
     } yield (script, complexity)
   }
 
