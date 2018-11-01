3f733bba46e2b4a2d9fd277f6ba4bfa4dd98015e
Dmitry Shuranov <dvshur@gmail.com>
2018-11-01 13:00:33 +0300

    NODE-1234 fixed transactions ordering and pagination logic in ngState, added test for BlockchainUpdaterImpl

--- src/main/scala/com/wavesplatform/state/package.scala
+++ src/main/scala/com/wavesplatform/state/package.scala
@@ -18,21 +18,20 @@ package object state {
       b: Blockchain,
       d: Option[Diff])(address: Address, types: Set[Type], count: Int, fromId: Option[ByteStr]): Either[String, Seq[(Int, Transaction)]] = {
 
-    def transactionsFromDiff(d: Diff): Iterable[(Int, Transaction, Set[Address])] = d.transactions.values.view
+    def transactionsFromDiff(d: Diff): Seq[(Int, Transaction, Set[Address])] = d.transactions.values.view.toSeq.reverse
 
-    def withPagination(s: Iterable[(Int, Transaction, Set[Address])]): Iterable[(Int, Transaction, Set[Address])] =
+    def withPagination(s: Seq[(Int, Transaction, Set[Address])]): Seq[(Int, Transaction, Set[Address])] =
       fromId match {
         case None     => s
-        case Some(id) => s.dropWhile(_._2.id() == id).drop(1)
+        case Some(id) => s.dropWhile(_._2.id() != id).drop(1)
       }
 
-    def withFilterAndLimit(txs: Iterable[(Int, Transaction, Set[Address])]): Seq[(Int, Transaction)] =
+    def withFilterAndLimit(txs: Seq[(Int, Transaction, Set[Address])]): Seq[(Int, Transaction)] =
       txs
         .collect {
           case (height, tx, addresses) if addresses(address) && (types.isEmpty || types.contains(tx.builder.typeId)) => (height, tx)
         }
         .take(count)
-        .toSeq
 
     def withRestFromBlockchain(s: Seq[(Int, Transaction)]): Seq[(Int, Transaction)] =
       s.length match {
