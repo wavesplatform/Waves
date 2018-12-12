37d84d5bc83af1eaf42431eef6009c62c667eb66
Mike Potanin <mpotanin@wavesplatform.com>
2018-12-12 18:04:42 +0300

    NODE-1329-remember-blocks Fix: remove defaults.

--- src/main/scala/com/wavesplatform/database/LevelDBWriter.scala
+++ src/main/scala/com/wavesplatform/database/LevelDBWriter.scala
@@ -62,11 +62,7 @@ object LevelDBWriter {
 
 }
 
-class LevelDBWriter(writableDB: DB,
-                    fs: FunctionalitySettings,
-                    val maxCacheSize: Int = 100000,
-                    val maxRollbackDepth: Int = 2000,
-                    val rememberBlocksInterval: Long = 120 * 60 * 1000)
+class LevelDBWriter(writableDB: DB, fs: FunctionalitySettings, val maxCacheSize: Int, val maxRollbackDepth: Int, val rememberBlocksInterval: Long)
     extends Caches
     with ScorexLogging {
 
