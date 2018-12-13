b05433bcd254f8f4eaab10d7e2061a7bc561c8e2
Eugene Nichegovskiy <eugene.nichegovskiy@gmail.com>
2018-12-13 16:27:21 +0300

    added 2 sponsorship tests https://wavesplatform.atlassian.net/browse/NODE-1362

--- it/src/test/scala/com/wavesplatform/it/sync/smartcontract/smartasset/AssetUnsupportedTransactionsSuite.scala
+++ it/src/test/scala/com/wavesplatform/it/sync/smartcontract/smartasset/AssetUnsupportedTransactionsSuite.scala
@@ -5,6 +5,7 @@ import com.wavesplatform.it.sync.{someAssetAmount, _}
 import com.wavesplatform.it.transactions.BaseTransactionSuite
 import com.wavesplatform.transaction.smart.script.ScriptCompiler
 import org.scalatest.prop.TableDrivenPropertyChecks
+import com.wavesplatform.it.util._
 
 class AssetUnsupportedTransactionsSuite extends BaseTransactionSuite with TableDrivenPropertyChecks {
 
@@ -48,6 +49,26 @@ class AssetUnsupportedTransactionsSuite extends BaseTransactionSuite with TableD
         case _: Throwable            => fail("ScriptCompiler works incorrect for orders with smart assets")
       }
     }
+  }
+
+  test("cannot sponsor scripted asset") {
+    val assetId = sender
+      .issue(
+        firstAddress,
+        "MyAsset",
+        "Test Asset",
+        someAssetAmount,
+        0,
+        reissuable = true,
+        issueFee,
+        2,
+        Some(scriptBase64),
+        waitForTx = true
+      )
+      .id
+    assertBadRequestAndMessage(sender.sponsorAsset(firstAddress, assetId, 100, sponsorFee + smartFee),
+                               "State check failed. Reason: Sponsorship smart assets is disabled.")
+
   }
 
 }
