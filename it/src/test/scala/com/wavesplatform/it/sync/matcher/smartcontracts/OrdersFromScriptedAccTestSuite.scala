07b80bd79aa8b3c7aa33b90437413d748645ee96
Nastya.Urlapova <aurlapova@wavesplatform.com>
2018-12-14 18:07:08 +0300

    NODE-1355 Increase timetest for feature activation

--- it/src/test/scala/com/wavesplatform/it/sync/matcher/smartcontracts/OrdersFromScriptedAccTestSuite.scala
+++ it/src/test/scala/com/wavesplatform/it/sync/matcher/smartcontracts/OrdersFromScriptedAccTestSuite.scala
@@ -75,7 +75,7 @@ class OrdersFromScriptedAccTestSuite extends MatcherSuiteBase {
     }
 
     "invalid setScript at account" in {
-      matcherNode.waitForHeight(ActivationHeight, 5.minutes)
+      matcherNode.waitForHeight(ActivationHeight, 6.minutes)
       setContract(Some("true && (height > 0)"), bobAcc)
       assertBadRequestAndResponse(
         matcherNode.placeOrder(bobAcc, aliceWavesPair, OrderType.BUY, 500, 2.waves * Order.PriceConstant, smartTradeFee, version = 2, 10.minutes),
