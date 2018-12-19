4e5e849057b2c1f1920ec8602f974991058d0e44
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2018-12-19 16:20:43 +0300

    DEX-183 Catch consumer failures

--- src/main/scala/com/wavesplatform/matcher/queue/KafkaMatcherQueue.scala
+++ src/main/scala/com/wavesplatform/matcher/queue/KafkaMatcherQueue.scala
@@ -1,8 +1,10 @@
 package com.wavesplatform.matcher.queue
 
+import java.util.concurrent.atomic.AtomicReference
+
 import akka.kafka._
 import akka.kafka.scaladsl.{Consumer, Producer}
-import akka.stream.scaladsl.{Keep, Sink, Source}
+import akka.stream.scaladsl.{Keep, RestartSource, Sink, Source}
 import akka.stream.{ActorMaterializer, OverflowStrategy}
 import com.wavesplatform.matcher.queue.KafkaMatcherQueue.Settings
 import com.wavesplatform.utils.ScorexLogging
@@ -28,7 +30,7 @@ class KafkaMatcherQueue(settings: Settings)(implicit mat: ActorMaterializer) ext
     override def close(): Unit                                                      = {}
   }
 
-  private var consumer = Option.empty[Consumer.Control]
+  private val consumerControl = new AtomicReference[Consumer.Control](Consumer.NoopControl)
 
   private val consumerSettings = {
     val config = mat.system.settings.config.getConfig("akka.kafka.consumer")
@@ -57,25 +59,31 @@ class KafkaMatcherQueue(settings: Settings)(implicit mat: ActorMaterializer) ext
 
   override def startConsume(fromOffset: QueueEventWithMeta.Offset, process: QueueEventWithMeta => Future[Unit]): Unit = {
     log.info(s"Start consuming from $fromOffset")
-    consumer = Some {
-      Consumer
-        .committableSource(consumerSettings, Subscriptions.assignmentWithOffset(new TopicPartition(settings.topic, 0) -> fromOffset))
-        .buffer(settings.consumerBufferSize, OverflowStrategy.backpressure)
-        .mapAsync(1) { msg =>
-          val req = QueueEventWithMeta(msg.record.offset(), msg.record.timestamp(), msg.record.value())
-          process(req)
-            .recoverWith {
-              case e =>
-                log.error(s"Matcher: Failed to process event at ${msg.record.offset()} offset: ${msg.record}")
-                Future.failed(e)
-            }
-            .map(_ => msg.committableOffset)
-        }
-        .batch(max = settings.consumerBufferSize, ConsumerMessage.CommittableOffsetBatch(_))(_.updated(_))
-        .mapAsync(5)(_.commitScaladsl())
-        .toMat(Sink.ignore)(Keep.left)
-        .run()
-    }
+    RestartSource
+      .onFailuresWithBackoff(
+        minBackoff = settings.consumer.minBackoff,
+        maxBackoff = settings.consumer.maxBackoff,
+        randomFactor = 0.2,
+        maxRestarts = -1
+      ) { () =>
+        Consumer
+          .committableSource(consumerSettings, Subscriptions.assignmentWithOffset(new TopicPartition(settings.topic, 0) -> fromOffset))
+          .mapMaterializedValue(consumerControl.set)
+          .buffer(settings.consumer.bufferSize, OverflowStrategy.backpressure)
+          .mapAsync(1) { msg =>
+            val req = QueueEventWithMeta(msg.record.offset(), msg.record.timestamp(), msg.record.value())
+            process(req)
+              .recoverWith {
+                case e =>
+                  log.error(s"Matcher: Failed to process event at ${msg.record.offset()} offset: ${msg.record}")
+                  Future.failed(e)
+              }
+              .map(_ => msg.committableOffset)
+          }
+          .batch(max = settings.consumer.bufferSize, ConsumerMessage.CommittableOffsetBatch(_))(_.updated(_))
+          .mapAsync(2)(_.commitScaladsl())
+      }
+      .runWith(Sink.ignore)
   }
 
   override def storeEvent(event: QueueEvent): Future[QueueEventWithMeta.Offset] = {
@@ -87,11 +95,12 @@ class KafkaMatcherQueue(settings: Settings)(implicit mat: ActorMaterializer) ext
   override def close(timeout: FiniteDuration): Unit = {
     producer.complete()
     Await.result(producer.watchCompletion(), timeout)
-    consumer.foreach(x => Await.result(x.shutdown(), timeout))
+    Await.result(consumerControl.get().shutdown(), timeout)
   }
 
 }
 
 object KafkaMatcherQueue {
-  case class Settings(topic: String, consumerBufferSize: Int)
+  case class Settings(topic: String, consumer: ConsumerSettings)
+  case class ConsumerSettings(bufferSize: Int, minBackoff: FiniteDuration, maxBackoff: FiniteDuration)
 }
