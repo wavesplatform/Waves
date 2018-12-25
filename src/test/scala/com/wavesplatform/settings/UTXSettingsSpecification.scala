4e9ea6049b953d62ee6711b1e4c064bf161cb429
peterz <peterz@rambler.ru>
2018-12-25 14:21:28 +0300

    NODE-1269 Transaction put to UTX but not in blockchain if tx is more then 1,5 hours old Replaced waves.utx.max-transaction-age setting with CommonValidation.MaxTimePrevBlockOverTransactionDiff

--- src/test/scala/com/wavesplatform/settings/UTXSettingsSpecification.scala
+++ src/test/scala/com/wavesplatform/settings/UTXSettingsSpecification.scala
@@ -12,7 +12,6 @@ class UTXSettingsSpecification extends FlatSpec with Matchers {
     val config   = ConfigFactory.parseString("""waves {
         |  utx {
         |    max-size = 100
-        |    max-transaction-age = 100m
         |    cleanup-interval = 10m
         |    blacklist-sender-addresses = ["a"]
         |    allow-blacklisted-transfer-to = ["b"]
@@ -21,7 +20,6 @@ class UTXSettingsSpecification extends FlatSpec with Matchers {
         |}""".stripMargin).resolve()
     val settings = config.as[UtxSettings]("waves.utx")
     settings.maxSize should be(100)
-    settings.maxTransactionAge shouldBe 100.minutes
     settings.cleanupInterval shouldBe 10.minutes
     settings.blacklistSenderAddresses shouldBe Set("a")
     settings.allowBlacklistedTransferTo shouldBe Set("b")
