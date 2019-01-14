07e9e2276e721db0a1a9955f8b9c0c6249fe5c93
vgavrilin <vgavrilin@wavesplatform.com>
2019-01-14 13:59:36 +0300

    NODE-1400: UTX pool max bytes size setting

--- src/main/scala/com/wavesplatform/settings/UtxSettings.scala
+++ src/main/scala/com/wavesplatform/settings/UtxSettings.scala
@@ -3,7 +3,12 @@ package com.wavesplatform.settings
 import scala.concurrent.duration.FiniteDuration
 
 case class UtxSettings(maxSize: Int,
+                       maxBytesSize: Long,
                        blacklistSenderAddresses: Set[String],
                        allowBlacklistedTransferTo: Set[String],
                        cleanupInterval: FiniteDuration,
                        allowTransactionsFromSmartAccounts: Boolean)
+
+object UtxSettings {
+  val DefaultMaxBytes = 100L * 1024 * 1024 // 100 MB
+}
