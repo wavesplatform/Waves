93e7ec59deaaef790d958892d484bc37e661a7d5
Karasiq <yoba123@yandex.ru>
2019-01-17 10:38:56 +0300

    NODE-1400: UTX pool bytes metric

--- src/main/scala/com/wavesplatform/utx/UtxPoolImpl.scala
+++ src/main/scala/com/wavesplatform/utx/UtxPoolImpl.scala
@@ -50,14 +50,27 @@ class UtxPoolImpl(time: Time, blockchain: Blockchain, fs: FunctionalitySettings,
 
   private val cleanup: CancelableFuture[Unit] = removeInvalidTask.runAsyncLogErr
 
+  private[this] object PoolMetrics {
+    private[this] val sizeStats = Kamon.rangeSampler("utx-pool-size", MeasurementUnit.none, Duration.of(500, ChronoUnit.MILLIS))
+    private[this] val bytesStats = Kamon.rangeSampler("utx-pool-bytes", MeasurementUnit.information.bytes, Duration.of(500, ChronoUnit.MILLIS))
+    val processingTimeStats = Kamon.histogram("utx-transaction-processing-time", MeasurementUnit.time.milliseconds)
+    val putRequestStats     = Kamon.counter("utx-pool-put-if-new")
+
+    def addTransaction(tx: Transaction): Unit = {
+      sizeStats.increment()
+      bytesStats.increment(tx.bytes().size)
+    }
+
+    def removeTransaction(tx: Transaction): Unit = {
+      sizeStats.decrement()
+      bytesStats.decrement(tx.bytes().size)
+    }
+  }
+
   override def close(): Unit = {
     cleanup.cancel()
     scheduler.shutdown()
   }
-
-  private val utxPoolSizeStats    = Kamon.rangeSampler("utx-pool-size", MeasurementUnit.none, Duration.of(500, ChronoUnit.MILLIS))
-  private val processingTimeStats = Kamon.histogram("utx-transaction-processing-time", MeasurementUnit.time.milliseconds)
-  private val putRequestStats     = Kamon.counter("utx-pool-put-if-new")
 
   private def removeExpired(currentTs: Long): Unit = {
     def isExpired(tx: Transaction) = (currentTs - tx.timestamp).millis > fs.maxTransactionTimeBackOffset
@@ -102,7 +115,7 @@ class UtxPoolImpl(time: Time, blockchain: Blockchain, fs: FunctionalitySettings,
   }
 
   private def remove(txId: ByteStr): Unit = {
-    Option(transactions.remove(txId)).foreach(_ => utxPoolSizeStats.decrement())
+    Option(transactions.remove(txId)).foreach(PoolMetrics.removeTransaction)
     pessimisticPortfolios.remove(txId)
   }
 
@@ -180,9 +193,9 @@ class UtxPoolImpl(time: Time, blockchain: Blockchain, fs: FunctionalitySettings,
     }
 
   private def putIfNew(b: Blockchain, tx: Transaction): Either[ValidationError, (Boolean, Diff)] = {
-    putRequestStats.increment()
+    PoolMetrics.putRequestStats.increment()
     val result = measureSuccessful(
-      processingTimeStats, {
+      PoolMetrics.processingTimeStats, {
         for {
           _ <- Either.cond(transactions.size < utxSettings.maxSize, (), GenericError("Transaction pool size limit is reached"))
 
@@ -201,7 +214,7 @@ class UtxPoolImpl(time: Time, blockchain: Blockchain, fs: FunctionalitySettings,
         } yield {
           pessimisticPortfolios.add(tx.id(), diff)
           val isNew = Option(transactions.put(tx.id(), tx)).isEmpty
-          if (isNew) utxPoolSizeStats.increment()
+          if (isNew) PoolMetrics.addTransaction(tx)
           (isNew, diff)
         }
       }
