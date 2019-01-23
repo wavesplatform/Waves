23386e507a2e340323395b14629cfcf7912a58b0
Mikhail Glukhov <mglukhov@wavesplatform.com>
2019-01-23 15:53:10 +0300

    decompler: added more complicated constructions

--- lang/jvm/src/test/scala/com/wavesplatform/lang/compiler/DecompilerTest.scala
+++ lang/jvm/src/test/scala/com/wavesplatform/lang/compiler/DecompilerTest.scala
@@ -1,7 +1,9 @@
 package com.wavesplatform.lang.compiler
 
+import com.wavesplatform.lang.v1.FunctionHeader
 import com.wavesplatform.lang.v1.compiler.{Decompiler, Terms}
 import com.wavesplatform.lang.v1.compiler.Terms._
+import com.wavesplatform.lang.v1.evaluator.ctx.impl.PureContext
 import org.scalatest.{Matchers, PropSpec}
 import org.scalatest.prop.PropertyChecks
 
@@ -9,26 +11,72 @@ class DecompilerTest extends PropSpec with PropertyChecks with Matchers {
 
   property("simple if") {
     val expr = IF(TRUE, CONST_LONG(1), CONST_STRING("XXX"))
-    Decompiler(expr) shouldBe """if (true) then 1 else "XXX"""".stripMargin
+    Decompiler(expr) shouldBe """if (true) then 1 else "XXX""""
   }
 
   property("simple let") {
     val expr = Terms.BLOCKV1(LET("a", CONST_LONG(1)), TRUE)
-    Decompiler(expr) shouldBe("""let a = 1; true""".stripMargin)
+    Decompiler(expr) shouldBe "let a = 1; true"
   }
-//
-//  property("simple function") {
-//    val expr = FUNC("alfa", List("a", "b"), IF(TRUE, CONST_LONG(1), CONST_STRING("XXX")))
-//    Decompiler(expr) shouldBe("""let a = 1""".stripMargin)
-//  }
-//
-//  property("simple function call") {
-//    val expr = FUNCTION_CALL(
-//      AnyPos,
-//      PART.VALID(AnyPos, functionWithTwoPrarmsOfTheSameType.name),
-//      List(CONST_LONG(AnyPos, 1), CONST_BYTEVECTOR(AnyPos, PART.VALID(AnyPos, ByteVector.empty)))
-//    )
-//    Decompiler(expr) shouldBe("""???""".stripMargin)
-//  }
+
+  property("native function call without args") {
+    val expr = Terms.FUNCTION_CALL(
+      function = FunctionHeader.Native(1),
+      args = List(TRUE)
+    )
+    Decompiler(expr) shouldBe "Native_1(true)"
+  }
+
+  property("user function call without args") {
+    val expr = Terms.FUNCTION_CALL(
+      function = FunctionHeader.User("foo"),
+      args = List(TRUE)
+    )
+    Decompiler(expr) shouldBe "foo(true)"
+  }
+
+  property("user function call with empty args") {
+    val expr = Terms.FUNCTION_CALL(
+      function = FunctionHeader.User("foo"),
+      args = List.empty
+    )
+    Decompiler(expr) shouldBe "foo()"
+  }
+
+  property("user function declaration") {
+    val expr = Terms.FUNC("foo", List("bar", "buz"), CONST_BOOLEAN(true))
+    Decompiler(expr) shouldBe "func foo (bar,buz) = TRUE"
+  }
+
+
+  property("v2 with LET") {
+    val expr = Terms.BLOCKV2(
+      LET("vari", REF("p")),
+      TRUE
+    )
+    Decompiler(expr) shouldBe "let vari = p; true"
+  }
+
+  property("match") {
+    val expr = Terms.BLOCKV2(
+      LET("v", REF("p")),
+      IF(
+        IF(
+          FUNCTION_CALL(
+            PureContext._isInstanceOf.header,
+            List(REF("v"), CONST_STRING("a"))
+          ),
+          TRUE,
+          FUNCTION_CALL(
+            PureContext._isInstanceOf.header,
+            List(REF("v"), CONST_STRING("b"))
+          )
+        ),
+        BLOCKV2(LET("p", REF("v")), TRUE),
+        FALSE
+      )
+    )
+    Decompiler(expr) shouldBe """let v = p; if (if (Native_1(v,"a")) then true else Native_1(v,"b")) then let p = v; true else false"""
+  }
 
 }
--- lang/shared/src/main/scala/com/wavesplatform/lang/v1/compiler/Decompiler.scala
+++ lang/shared/src/main/scala/com/wavesplatform/lang/v1/compiler/Decompiler.scala
@@ -1,26 +1,35 @@
 package com.wavesplatform.lang.v1.compiler
 
+import com.wavesplatform.lang.v1.FunctionHeader
 import com.wavesplatform.lang.v1.compiler.Terms._
-import com.wavesplatform.lang.v1.parser.Expressions
 
 object Decompiler {
 
-  def apply(e :EXPR): String =
+  def apply(e: EXPR): String =
     e match {
-      case x: Terms.CONST_BOOLEAN => x.toString().toLowerCase()
+      case Terms.TRUE => "true"
+      case Terms.FALSE => "false"
+      case Terms.CONST_BOOLEAN(b) => b.toString().toLowerCase()
       case Terms.IF(cond, it, iff) => "if (" + Decompiler(cond) + ") then " + Decompiler(it) + " else " + Decompiler(iff)
-        // отсутствие ветви else?
       case Terms.CONST_LONG(t) => t.toLong.toString()
       case Terms.CONST_STRING(s) => """"""" + s + """""""
       case x: Terms.ARR => ???
-      case Terms.BLOCKV1(let,expr) => "let " + let.name + " = " + Decompiler(let.value) + "; " + Decompiler(expr)
-      case x: Terms.BLOCKV2 => "v2let " + x.dec + " = " + Decompiler(x.body) // test it
+      case Terms.BLOCKV1(let, expr) => "let " + let.name + " = " + Decompiler(let.value) + "; " + Decompiler(expr)
+      case Terms.BLOCKV2(decl, body) => Decompiler(decl) + "; " + Decompiler(body)
       case x: Terms.CONST_BYTEVECTOR => "'" + x.bs + "'"
       case x: Terms.CaseObj => ??? // never happens
-      case x: Terms.DECLARATION => "TODO: declaration?"
-      case x: Terms.EVALUATED => "TODO: evaluated"
-      //case x: Terms.FUNC => "def" + x.name + "(" + x.args map { case (arg, typearg) => arg + " : " + typearg } + x.body
-      case Terms.FUNCTION_CALL(func, args) => func + "(" + args.foreach((arg)=>arg.toString()) + ")"
+      case Terms.FUNCTION_CALL(func, args) => func match {
+        case FunctionHeader.Native(name) => "Native_" + name + "(" + args.map(Decompiler(_)).mkString(",") + ")"
+        case FunctionHeader.User(name) => name + "(" + args.map(Decompiler(_)).mkString(",") + ")"
+      }
       case Terms.REF(ref) => ref
     }
+
+  def apply (e: DECLARATION): String =
+    e match {
+      case Terms.FUNC(name, args, body) => "func " + name + " (" +
+        args.map((x => x.toString)).mkString(",") + ") = " + body
+      case Terms.LET(name, value) => "let " + name + " = " + Decompiler(value)
+    }
+
 }
