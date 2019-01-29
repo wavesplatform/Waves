3c584c4de8b86051773b31fdc74a8e49d19d603b
Valeriy <yoba123@yandex.ru>
2019-01-29 17:18:43 +0300

    NODE-702: Temporary DB in every generator iteration

--- src/test/scala/com/wavesplatform/utx/UtxPoolSpecification.scala
+++ src/test/scala/com/wavesplatform/utx/UtxPoolSpecification.scala
@@ -1,11 +1,16 @@
 package com.wavesplatform.utx
 
+import java.nio.file.Files
+import java.util.concurrent.atomic.AtomicBoolean
+
 import com.typesafe.config.ConfigFactory
 import com.wavesplatform._
 import com.wavesplatform.account.{Address, PrivateKeyAccount, PublicKeyAccount}
 import com.wavesplatform.block.Block
 import com.wavesplatform.common.state.ByteStr
 import com.wavesplatform.common.utils.EitherExt2
+import com.wavesplatform.database.LevelDBWriter
+import com.wavesplatform.db.openDB
 import com.wavesplatform.features.BlockchainFeatures
 import com.wavesplatform.history.StorageFactory
 import com.wavesplatform.lagonaki.mocks.TestBlock
@@ -23,6 +28,7 @@ import com.wavesplatform.transaction.smart.script.v1.ExprScript
 import com.wavesplatform.transaction.transfer.MassTransferTransaction.ParsedTransfer
 import com.wavesplatform.transaction.transfer._
 import com.wavesplatform.utils.Time
+import com.wavesplatform.utx.UtxPoolSpecification.TempDB
 import org.scalacheck.Gen
 import org.scalacheck.Gen._
 import org.scalamock.scalatest.MockFactory
@@ -31,7 +37,28 @@ import org.scalatest.{FreeSpec, Matchers}
 
 import scala.concurrent.duration._
 
-class UtxPoolSpecification extends FreeSpec with Matchers with MockFactory with PropertyChecks with TransactionGen with NoShrink with WithDB {
+private object UtxPoolSpecification {
+  final case class TempDB(fs: FunctionalitySettings) extends AutoCloseable {
+    private[this] val closed = new AtomicBoolean(false)
+    val path = Files.createTempDirectory("leveldb-test")
+    val db   = openDB(path.toAbsolutePath.toString)
+    val writer = new LevelDBWriter(db, fs, 100000, 2000, 120 * 60 * 1000)
+
+    def close(): Unit = {
+      if (closed.compareAndSet(false, true)) {
+        db.close()
+        TestHelpers.deleteRecursively(path)
+      }
+    }
+
+    override def finalize(): Unit = {
+      close()
+      super.finalize()
+    }
+  }
+}
+
+class UtxPoolSpecification extends FreeSpec with Matchers with MockFactory with PropertyChecks with TransactionGen with NoShrink {
   val PoolDefaultMaxBytes = 50 * 1024 * 1024 // 50 MB
 
   import CommonValidation.{ScriptExtraFee => extraFee}
@@ -55,7 +82,8 @@ class UtxPoolSpecification extends FreeSpec with Matchers with MockFactory with 
       featuresSettings = origSettings.featuresSettings.copy(autoShutdownOnUnsupportedFeature = false)
     )
 
-    val bcu = StorageFactory(settings, db, new TestTime())
+    val dbContext = TempDB(settings.blockchainSettings.functionalitySettings)
+    val bcu = StorageFactory(settings, dbContext.db, new TestTime())
     bcu.processBlock(Block.genesis(genesisSettings).explicitGet()).explicitGet()
     bcu
   }
