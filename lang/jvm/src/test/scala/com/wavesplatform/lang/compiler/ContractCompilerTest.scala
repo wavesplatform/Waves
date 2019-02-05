5079d0cf2d7739673f0cc738ee0e9c4336dd7b79
Ivan Rakitnykh <mrkr.reg@gmail.com>
2019-02-05 16:58:10 +0300

    SC-57 Contract compiles if it has funcs with the same name

--- lang/jvm/src/test/scala/com/wavesplatform/lang/compiler/ContractCompilerTest.scala
+++ lang/jvm/src/test/scala/com/wavesplatform/lang/compiler/ContractCompilerTest.scala
@@ -192,7 +192,6 @@ class ContractCompilerTest extends PropSpec with PropertyChecks with Matchers wi
     compiler.ContractCompiler(ctx, expr) should produce("must have 0 arguments")
   }
 
-
   property("hodlContract") {
     val ctx = Monoid
       .combineAll(
@@ -274,5 +273,32 @@ class ContractCompilerTest extends PropSpec with PropertyChecks with Matchers wi
       Parser.parseContract(script).get.value
     }
     compiler.ContractCompiler(ctx, expr) shouldBe 'right
+  }
+
+  property("contract compilation fails if functions has the same name") {
+    val ctx = Monoid.combine(compilerContext, WavesContext.build(StdLibVersion.V3, Common.emptyBlockchainEnvironment(), false).compilerContext)
+    val expr = {
+      val script =
+        """
+          |
+          |@Callable(i)
+          |func sameName() = {
+          |   WriteSet(List(DataEntry("a", "a")))
+          |}
+          |
+          |@Callable(i)
+          |func sameName() = {
+          |   WriteSet(List(DataEntry("b", "b")))
+          |}
+          |
+          |@Verifier(i)
+          |func sameName() = {
+          |   true
+          |}
+          |
+        """.stripMargin
+      Parser.parseContract(script).get.value
+    }
+    compiler.ContractCompiler(ctx, expr) should produce("Contract functions must have unique names")
   }
 }
