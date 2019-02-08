811c269f1474f10d81109eb788a0f682e5e375dc
Valeriy <yoba123@yandex.ru>
2019-02-08 13:51:25 +0300

    Protobuf transactions WIP

--- src/main/scala/com/wavesplatform/transaction/protobuf/PBTransaction.scala
+++ src/main/scala/com/wavesplatform/transaction/protobuf/PBTransaction.scala
@@ -1,3 +1,21 @@
 package com.wavesplatform.transaction.protobuf
+import com.wavesplatform.account.PublicKeyAccount
+import com.wavesplatform.common.state.ByteStr
+import com.wavesplatform.serialization.protobuf.ProtobufTransactionParser
+import com.wavesplatform.serialization.protobuf.utils.PBUtils
+import com.wavesplatform.transaction.{AssetId, FastHashId, Proofs, SignedTransaction, TransactionParser, Transaction => VanillaTransaction}
+import monix.eval.Coeval
+import play.api.libs.json.{JsObject, Json}
+import scalapb.json4s.JsonFormat
 
-trait PBTransaction {}
+trait PBTransaction extends VanillaTransaction with SignedTransaction with FastHashId { self: Transaction =>
+  override def proofs: Proofs                    = new Proofs(self.getWrappedProofs.proofs.map(_.data).toList)
+  override val signature: ByteStr                = self.getWrappedProofs.proofs.headOption.fold(ByteStr.empty)(_.data)
+  override val sender: PublicKeyAccount          = self.getBody.sender
+  override def builder: TransactionParser        = ProtobufTransactionParser
+  override def assetFee: (Option[AssetId], Long) = (Some(self.getBody.feeAssetId).filterNot(_.isEmpty), self.getBody.fee)
+  override def timestamp: Long                   = self.getBody.timestamp
+  override val bodyBytes: Coeval[Array[Byte]]    = Coeval.evalOnce(self.body.fold(Array.emptyByteArray)(PBUtils.encodeDeterministic))
+  override val bytes: Coeval[Array[Byte]]        = Coeval.evalOnce(self.toByteArray)
+  override val json: Coeval[JsObject]            = Coeval.evalOnce(Json.parse(JsonFormat.toJsonString(self)).as[JsObject])
+}
--- src/main/scala/com/wavesplatform/transaction/protobuf/PBTransactionCompanion.scala
+++ src/main/scala/com/wavesplatform/transaction/protobuf/PBTransactionCompanion.scala
@@ -1,24 +1,3 @@
 package com.wavesplatform.transaction.protobuf
-import com.wavesplatform.account.PublicKeyAccount
-import com.wavesplatform.common.state.ByteStr
-import com.wavesplatform.serialization.protobuf.ProtobufTransactionParser
-import com.wavesplatform.serialization.protobuf.utils.PBUtils
-import com.wavesplatform.transaction.{AssetId, FastHashId, SignedTransaction, TransactionParser}
-import monix.eval.Coeval
-import play.api.libs.json.{JsObject, Json}
-import scalapb.json4s.JsonFormat
 
-trait PBTransactionCompanion {
-  implicit class SignedPBTransaction(tx: Transaction) extends SignedTransaction with FastHashId {
-    override lazy val signature: ByteStr = tx.getProofs.proofs.headOption.fold(ByteStr.empty)(_.data)
-    override val sender: PublicKeyAccount          = tx.getBody.sender
-    override def builder: TransactionParser        = ProtobufTransactionParser
-    override def assetFee: (Option[AssetId], Long) = (Some(tx.getBody.feeAssetId).filterNot(_.isEmpty), tx.getBody.fee)
-    override def timestamp: Long                   = tx.getBody.timestamp
-    override val bodyBytes: Coeval[Array[Byte]]    = Coeval.evalOnce(tx.body.fold(Array.emptyByteArray)(PBUtils.encodeDeterministic))
-    override val bytes: Coeval[Array[Byte]]        = Coeval.evalOnce(tx.toByteArray)
-    override val json: Coeval[JsObject]            = Coeval.evalOnce(Json.parse(JsonFormat.toJsonString(tx)).as[JsObject])
-
-    def legacy: SignedPBTransaction = this
-  }
-}
+trait PBTransactionCompanion {}
