9142dcfad1686cfd869099642666a53842304665
Valeriy <yoba123@yandex.ru>
2019-02-12 14:07:55 +0300

    NODE-1465: Protobuf transactions WIP

--- src/main/scala/com/wavesplatform/transaction/protobuf/PBTransactionImplicits.scala
+++ src/main/scala/com/wavesplatform/transaction/protobuf/PBTransactionImplicits.scala
@@ -14,6 +14,48 @@ trait PBTransactionImplicits {
   private[this] val FeeAssetId   = WavesAssetId
   private[this] lazy val ChainId = AddressScheme.current.chainId
 
+  implicit class VanillaOrderImplicitConversionOps(order: vt.assets.exchange.Order) {
+    def toPB: ExchangeTransactionData.Order = {
+      ExchangeTransactionData.Order(
+        order.senderPublicKey,
+        order.matcherPublicKey,
+        Some(ExchangeTransactionData.Order.AssetPair(order.assetPair.amountAsset, order.assetPair.priceAsset)),
+        order.orderType match {
+          case vt.assets.exchange.OrderType.BUY  => ExchangeTransactionData.Order.Type.BUY
+          case vt.assets.exchange.OrderType.SELL => ExchangeTransactionData.Order.Type.SELL
+        },
+        order.amount,
+        order.price,
+        order.timestamp,
+        order.expiration,
+        order.matcherFee,
+        order.proofs.proofs,
+        order.version
+      )
+    }
+  }
+
+  implicit class PBOrderImplicitConversionOps(order: ExchangeTransactionData.Order) {
+    def toVanilla: vt.assets.exchange.Order = {
+      vt.assets.exchange.Order(
+        order.senderPublicKey,
+        order.matcherPublicKey,
+        vt.assets.exchange.AssetPair(order.getAssetPair.amountAssetId, order.getAssetPair.priceAssetId),
+        order.orderType match {
+          case ExchangeTransactionData.Order.Type.BUY => vt.assets.exchange.OrderType.BUY
+          case ExchangeTransactionData.Order.Type.SELL => vt.assets.exchange.OrderType.SELL
+        },
+        order.amount,
+        order.price,
+        order.timestamp,
+        order.expiration,
+        order.matcherFee,
+        vt.Proofs(order.proofs),
+        order.version.toByte
+      )
+    }
+  }
+
   implicit class VanillaTransactionImplicitConversionOps(tx: VanillaTransaction) {
     def toPB: Transaction = tx match {
       case MassTransferTransaction(assetId, sender, transfers, timestamp, fee, attachment, proofs) =>
@@ -27,6 +69,16 @@ trait PBTransactionImplicits {
       case vt.transfer.TransferTransactionV2(sender, recipient, assetId, amount, timestamp, feeAssetId, fee, attachment, proofs) =>
         val data = TransferTransactionData(recipient, amount)
         Transaction(assetId, sender, ChainId, fee, feeAssetId, ByteStr(attachment), timestamp, 2, proofs.proofs, Data.Transfer(data))
+
+      case tx @ vt.assets.exchange.ExchangeTransactionV1(buyOrder, sellOrder, amount, price, buyMatcherFee, sellMatcherFee, fee, timestamp, signature) =>
+        val data = ExchangeTransactionData(Some(buyOrder.toPB), Some(sellOrder.toPB), amount, price, buyMatcherFee, sellMatcherFee)
+        Transaction(None, tx.sender, ChainId, fee, tx.assetFee._1, None, timestamp, 1, Seq(signature), Data.Exchange(data))
+
+      case tx @ vt.assets.exchange.ExchangeTransactionV2(buyOrder, sellOrder, amount, price, buyMatcherFee, sellMatcherFee, fee, timestamp, proofs) =>
+        val data = ExchangeTransactionData(Some(buyOrder.toPB), Some(sellOrder.toPB), amount, price, buyMatcherFee, sellMatcherFee)
+        Transaction(None, tx.sender, ChainId, fee, tx.assetFee._1, None, timestamp, 2, proofs.proofs, Data.Exchange(data))
+
+
 
       case _ =>
         throw new IllegalArgumentException(s"Unsupported transaction: $tx")
