a4c9595ee0c0be3ab79f59ac36fa48523e3134b6
Valeriy <yoba123@yandex.ru>
2019-02-14 16:20:46 +0300

    NODE-1465: Compatibility fixes

--- src/main/scala/com/wavesplatform/account/OptionalAddress.scala
+++ src/main/scala/com/wavesplatform/account/OptionalAddress.scala
@@ -3,19 +3,18 @@ package com.wavesplatform.account
 import com.wavesplatform.common.state.ByteStr
 import com.wavesplatform.common.utils._
 
-final case class OptionalAddress(bytes: Array[Byte]) {
+final case class OptionalAddress(bytes: ByteStr) {
   def isEmpty = bytes.isEmpty
 
   lazy val toAddressOrAlias: AddressOrAlias = AddressOrAlias.fromBytes(bytes, 0).explicitGet()._1
   def toAddress: Address                    = Address.fromBytes(bytes).explicitGet()
   def toAlias: Alias                        = Alias.fromBytes(bytes).explicitGet()
-  def toByteStr: ByteStr                    = ByteStr(bytes)
 }
 
 object OptionalAddress {
-  val empty = OptionalAddress(Array.emptyByteArray)
+  val empty = OptionalAddress(ByteStr.empty)
 
-  implicit def apply(addressOrAlias: AddressOrAlias): OptionalAddress = OptionalAddress(addressOrAlias.bytes.arr)
-  implicit def apply(address: Address): OptionalAddress               = OptionalAddress(address.bytes.arr)
-  implicit def apply(alias: Alias): OptionalAddress                   = OptionalAddress(alias.bytes.arr)
+  implicit def apply(addressOrAlias: AddressOrAlias): OptionalAddress = OptionalAddress(addressOrAlias.bytes)
+  implicit def apply(address: Address): OptionalAddress               = OptionalAddress(address.bytes)
+  implicit def apply(alias: Alias): OptionalAddress                   = OptionalAddress(alias.bytes)
 }
