6d51cb3784027635707fb03040571697cdf87b42
Valeriy <yoba123@yandex.ru>
2019-02-18 16:24:01 +0300

    NODE-1465: Protobuf refactoring

--- src/main/scala/com/wavesplatform/account/protobuf/PBRecipientCompanionBase.scala
+++ src/main/scala/com/wavesplatform/account/protobuf/PBRecipientCompanionBase.scala
@@ -4,7 +4,7 @@ import com.wavesplatform.account.{Address, AddressOrAlias, Alias}
 import com.wavesplatform.common.utils._
 
 trait PBRecipientCompanionBase {
-  def empty = Recipient.defaultInstance
+  def empty: Recipient = Recipient.defaultInstance
 
   implicit def fromAddressOrAlias(addressOrAlias: AddressOrAlias): Recipient = addressOrAlias match {
     case a: Address => a
@@ -21,12 +21,14 @@ trait PBRecipientCompanionBase {
 
   implicit class PBRecipientImplicitConversionOps(recipient: Recipient) {
     def toAddress: Address = {
-      Address.fromBytes(Bytes.concat(Array(Address.AddressVersion, recipient.chainId.toByte), recipient.getAddress.address.arr), recipient.chainId.toByte)
+      Address
+        .fromBytes(Bytes.concat(Array(Address.AddressVersion, recipient.chainId.toByte), recipient.getAddress.address.arr), recipient.chainId.toByte)
         .explicitGet()
     }
 
     def toAlias: Alias = {
-      Alias.buildAlias(recipient.chainId.toByte, recipient.getAlias.name)
+      Alias
+        .buildAlias(recipient.chainId.toByte, recipient.getAlias.name)
         .explicitGet()
     }
 
