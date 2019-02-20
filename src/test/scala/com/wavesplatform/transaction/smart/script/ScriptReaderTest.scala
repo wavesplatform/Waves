05d93973048e5411a85fb7eb1c03174335e65d0a
Ilya Smagin <ilya.smagin@gmail.com>
2019-02-20 17:33:46 +0300

    sc-17: better scriptReader tests

--- src/test/scala/com/wavesplatform/transaction/smart/script/ScriptReaderTest.scala
+++ src/test/scala/com/wavesplatform/transaction/smart/script/ScriptReaderTest.scala
@@ -1,33 +1,28 @@
 package com.wavesplatform.transaction.smart.script
 
-import com.wavesplatform.common.utils.EitherExt2
-import com.wavesplatform.crypto
-import com.wavesplatform.lang.StdLibVersion.{V3, V1}
-import com.wavesplatform.lang.contract.Contract
-import com.wavesplatform.lang.contract.Contract.{CallableAnnotation, CallableFunction}
+import com.wavesplatform.lang.StdLibVersion._
+import com.wavesplatform.common.utils._
 import com.wavesplatform.lang.v1.Serde
-import com.wavesplatform.lang.v1.compiler.Terms
-import com.wavesplatform.lang.v1.compiler.Terms.TRUE
 import com.wavesplatform.state.diffs.produce
-import org.scalatest.{FreeSpec, Matchers}
+import com.wavesplatform.{NoShrink, TransactionGen, crypto}
+import org.scalatest.prop.PropertyChecks
+import org.scalatest.{Inside, Matchers, PropSpec}
 
-class ScriptReaderTest extends FreeSpec with Matchers {
+class ScriptReaderTest extends PropSpec with PropertyChecks with Matchers with TransactionGen with Inside with NoShrink {
   val checksumLength = 4
 
-  "should parse all bytes for V1" in {
-    val body     = Array(V1.toByte) ++ Serde.serialize(TRUE) ++ "foo".getBytes
-    val allBytes = body ++ crypto.secureHash(body).take(checksumLength)
-    ScriptReader.fromBytes(allBytes) should produce("bytes left")
+  property("should parse all bytes for V1") {
+    forAll(exprGen) { sc =>
+      val body     = Array(V1.toByte) ++ Serde.serialize(sc) ++ "foo".getBytes
+      val allBytes = body ++ crypto.secureHash(body).take(checksumLength)
+      ScriptReader.fromBytes(allBytes) should produce("bytes left")
+    }
   }
 
-  "should parse all bytes for V3" in {
-    val sc = ContractScript(
-      V3,
-      Contract(List.empty, List(CallableFunction(CallableAnnotation("sender"), Terms.FUNC("foo", List("a"), Terms.REF("a")))), None)
-    ).explicitGet()
-
-    val allBytes = sc.bytes().arr
-    ScriptReader.fromBytes(allBytes) shouldBe Right(sc)
+  property("should parse all bytes for V3") {
+    forAll(contractGen) { sc =>
+      val allBytes = ContractScript.apply(V3, sc).explicitGet().bytes().arr
+      ScriptReader.fromBytes(allBytes).explicitGet().expr shouldBe sc
+    }
   }
-
 }
