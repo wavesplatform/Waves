1d672dbf7169ffb65925bcc7f6ee0b778bd05c72
Valeriy <yoba123@yandex.ru>
2019-02-21 16:20:41 +0300

    NODE-1465: Capnp WIP

--- src/main/java/com/wavesplatform/transactions/capnp/CAPNPTest.scala
+++ src/main/java/com/wavesplatform/transactions/capnp/CAPNPTest.scala
@@ -9,21 +9,18 @@ import com.wavesplatform.transactions.capnp.Transactions.Transaction
 import org.capnproto.{MessageBuilder, Serialize}
 
 object CAPNPTest extends App {
-  val defaultRecipient = PublicKeyAccount(Array.fill(32)(0: Byte))
-
-
   val msg = new MessageBuilder()
 
   val transaction = msg.initRoot(Transaction.factory)
   transaction.setChainId(100)
-  transaction.setTimestamp(System.currentTimeMillis())
+  transaction.setTimestamp(0)
   transaction.setVersion(1)
 
   val data = transaction.initData().initGenesis()
   data.setAmount(1000)
 
   val recipient = data.initRecipient()
-  recipient.setAddress(defaultRecipient.bytes.arr)
+  recipient.setAddress(Array.fill(32)(0: Byte))
 
 
   val buffer = ByteBuffer.allocate(Serialize.computeSerializedSizeInWords(msg).toInt * 8)
@@ -33,6 +30,6 @@ object CAPNPTest extends App {
 
   val bytes = ByteStr(buffer.array())
   println(bytes)
-  println(bytes.toString == "1111JCbt7hujK9qHboWCAJiqo6RGecABtMfh4BptmQdG2KKdfE2DHXN1T4NqpRefR5yN37knY4Frb5b6vo8XomDSKYYyKBnzzStGd1tTdBM4pXU6EtXrKXkLuoDrRrJiVtFDnyTUXXdmdZh72xjiXvpKFML4Qsjc4P")
+  println(bytes.toString == "1111JCbt7hujK9qHboWCAJiqo6RGecABtMfh4BptmQXsivQ9PggHamKLPXSozVs4mYbPEpEMj99dxu55TjoW1kUbE7JSP3Gm9niiuNvu1kRsqbfcgU1n1YFwua6oxEmYCz3fpkLMJPZPmMRdshZ8jbVkvtxJXzCyBM")
   println(bytes.hashCode())
 }
