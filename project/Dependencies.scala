eacc996aac03ab434949808a809445b626f2462f
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2019-02-25 14:00:19 +0300

    DEX-244 Update Kafka dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -67,7 +67,7 @@ object Dependencies {
     akkaModule("persistence"),
     akkaModule("persistence-tck") % "test",
     "com.github.dnvriend"         %% "akka-persistence-inmemory" % "2.5.15.1" % "test",
-    "com.typesafe.akka"           %% "akka-stream-kafka" % "1.0-RC1",
+    "com.typesafe.akka"           %% "akka-stream-kafka" % "1.0-RC2",
     "org.ethereum"                % "leveldbjni-all" % "1.18.3"
   )
 
