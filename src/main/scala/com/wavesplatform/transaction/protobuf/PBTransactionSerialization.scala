636bc7eddd319f921344499ff6673d965971755f
Valeriy <yoba123@yandex.ru>
2019-02-27 12:28:00 +0300

    NODE-1492: Protobuf schema amendments

--- src/main/scala/com/wavesplatform/transaction/protobuf/PBTransactionSerialization.scala
+++ src/main/scala/com/wavesplatform/transaction/protobuf/PBTransactionSerialization.scala
@@ -1,33 +1,24 @@
 package com.wavesplatform.transaction.protobuf
 import com.google.protobuf.CodedOutputStream
+import com.wavesplatform.serialization.protobuf.utils.PBUtils
 
 object PBTransactionSerialization {
   def signedBytes(tx: PBSignedTransaction): Array[Byte] = {
     val outArray     = new Array[Byte](tx.serializedSize + 2)
     val outputStream = CodedOutputStream.newInstance(outArray)
+    outputStream.useDeterministicSerialization()
 
     outputStream.write(PBTransaction.typeId)
     outputStream.write(PBTransaction.version)
     tx.writeTo(outputStream)
 
-    outputStream.checkNoSpaceLeft()
-    outArray
-  }
-
-  def unsignedBytes(unsignedTx: PBTransaction): Array[Byte] = {
-    val prefixLength = 3 // "WTX"
-    val outArray     = new Array[Byte](unsignedTx.serializedSize + prefixLength)
-    val outputStream = CodedOutputStream.newInstance(outArray)
-
-    outputStream.useDeterministicSerialization() // Do not remove
-    outputStream.write('W'.toByte)
-    outputStream.write('T'.toByte)
-    outputStream.write(unsignedTx.chainId.byte)
-
-    unsignedTx.writeTo(outputStream)
     outputStream.flush()
     outputStream.checkNoSpaceLeft()
 
     outArray
+  }
+
+  def unsignedBytes(unsignedTx: PBTransaction): Array[Byte] = {
+    PBUtils.encodeDeterministic(unsignedTx)
   }
 }
