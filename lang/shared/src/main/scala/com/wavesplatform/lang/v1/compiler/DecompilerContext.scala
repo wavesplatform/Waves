c0cd415eafc3a381211a22bac5d87d44cb8746c6
Ilya Smagin <ilya.smagin@gmail.com>
2019-03-04 18:49:11 +0300

    sc-90: cleanup

--- lang/shared/src/main/scala/com/wavesplatform/lang/v1/compiler/DecompilerContext.scala
+++ lang/shared/src/main/scala/com/wavesplatform/lang/v1/compiler/DecompilerContext.scala
@@ -2,5 +2,4 @@ package com.wavesplatform.lang.v1.compiler
 
 case class DecompilerContext(opCodes: Map[Short, String], binaryOps: Map[Short, String], ident: Int) {
   def incrementIdent(): DecompilerContext = this.copy(ident = this.ident + 1)
-  def zero(): DecompilerContext = this.copy(ident = 0)
 }
