75467ac5ae48f3017b22e568f3336fa5fa75a2c7
Valeriy <yoba123@yandex.ru>
2019-03-12 12:24:42 +0300

    NODE-1489: Fix blacklisting on invalid message frame

--- src/main/scala/com/wavesplatform/network/ReadErrorHandler.scala
+++ src/main/scala/com/wavesplatform/network/ReadErrorHandler.scala
@@ -3,12 +3,12 @@ package com.wavesplatform.network
 import com.wavesplatform.utils.ScorexLogging
 import io.netty.channel.ChannelHandler.Sharable
 import io.netty.channel.{ChannelHandlerContext, ChannelInboundHandlerAdapter}
-import io.netty.handler.codec.CorruptedFrameException
+import io.netty.handler.codec.DecoderException
 
 @Sharable
 class ReadErrorHandler(peerDatabase: PeerDatabase) extends ChannelInboundHandlerAdapter with ScorexLogging {
   override def exceptionCaught(ctx: ChannelHandlerContext, cause: Throwable): Unit = cause match {
-    case e: CorruptedFrameException =>
+    case e: DecoderException =>
       peerDatabase.blacklistAndClose(ctx.channel(), s"Corrupted message frame: $e")
 
     case e: IndexOutOfBoundsException =>
