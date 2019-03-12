770713d198463cb8fe527aab4188532a6c192607
Valeriy <yoba123@yandex.ru>
2019-03-12 14:33:39 +0300

    NODE-1489: Fix blacklisting on invalid message frame

--- src/main/scala/com/wavesplatform/network/LegacyFrameCodec.scala
+++ src/main/scala/com/wavesplatform/network/LegacyFrameCodec.scala
@@ -5,12 +5,12 @@ import java.util
 import com.google.common.cache.CacheBuilder
 import com.wavesplatform.common.utils.Base64
 import com.wavesplatform.crypto
+import com.wavesplatform.network.message.Message._
 import com.wavesplatform.utils.ScorexLogging
 import io.netty.buffer.ByteBuf
 import io.netty.buffer.Unpooled._
 import io.netty.channel.ChannelHandlerContext
 import io.netty.handler.codec.ByteToMessageCodec
-import com.wavesplatform.network.message.Message._
 
 import scala.concurrent.duration.FiniteDuration
 import scala.util.control.NonFatal
@@ -60,6 +60,7 @@ class LegacyFrameCodec(peerDatabase: PeerDatabase, receivedTxsCacheTimeout: Fini
       case NonFatal(e) =>
         log.warn(s"${id(ctx)} Malformed network message", e)
         peerDatabase.blacklistAndClose(ctx.channel(), s"Malformed network message: $e")
+        in.resetReaderIndex() // Cancels subsequent read tries, see Netty decode() documentation
     }
 
   override def encode(ctx: ChannelHandlerContext, msg: RawBytes, out: ByteBuf): Unit = {
