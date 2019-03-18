a276ec896f7bf560edf6a84b9924eaf56d4710cb
Denis Serebryansky <denis.serebryansky.eu@gmail.com>
2019-03-18 18:45:38 +0300

    DEX-260 Fixed-Waves settings renamed to Waves, fixed mode now accepts Waves

--- src/main/scala/com/wavesplatform/settings/utils/ConfigSettingsValidator.scala
+++ src/main/scala/com/wavesplatform/settings/utils/ConfigSettingsValidator.scala
@@ -10,8 +10,15 @@ import net.ceedubs.ficus.readers.ValueReader
 import scala.util.Try
 
 object ConfigSettingsValidator {
+
   type ErrorsListOr[A] = Validated[List[String], A]
+
   def apply(config: Config): ConfigSettingsValidator = new ConfigSettingsValidator(config)
+
+  def createInvalidSettingValueError[T](settingName: String, settingValue: T, additionalErrorInfo: String = ""): List[String] = {
+    val errorMsg = Option(additionalErrorInfo).filter(_.nonEmpty).fold("")(m => s", $m")
+    List(s"Invalid setting $settingName value: $settingValue$errorMsg")
+  }
 }
 
 class ConfigSettingsValidator(config: Config) {
@@ -19,24 +26,20 @@ class ConfigSettingsValidator(config: Config) {
   import ConfigSettingsValidator._
 
   def validateByPredicate[T: ValueReader](settingName: String)(predicate: T => Boolean, errorMsg: String): ErrorsListOr[T] = {
-
-    val settingValue   = config.as[T](settingName)
-    val additionalInfo = Option(errorMsg).filter(_.nonEmpty).fold("")(m => s", $m")
-
-    Validated.cond(predicate(settingValue), settingValue, List(s"Invalid setting $settingName value: $settingValue$additionalInfo"))
+    val settingValue = config.as[T](settingName)
+    Validated cond (predicate(settingValue), settingValue, createInvalidSettingValueError(settingName, settingValue, errorMsg))
   }
 
   def validateSafe[T: ValueReader](settingName: String): ErrorsListOr[T] = {
-    Validated.fromTry(Try(config.as[T](settingName))).leftMap(_ => List(s"Invalid setting $settingName value: ${config.getString(settingName)}"))
+    Validated fromTry Try(config.as[T](settingName)) leftMap (_ => createInvalidSettingValueError(settingName, config getString settingName))
   }
 
-  def validateAsset(settingName: String)(assetIdStr: String): ErrorsListOr[Asset] = {
-    Validated
-      .fromTry(AssetPair.extractAssetId(assetIdStr))
-      .leftMap(_ => List(s"Invalid setting $settingName value: $assetIdStr"))
+  def validateAsset(settingName: String): ErrorsListOr[Asset] = {
+    val assetStr = config getString settingName
+    Validated fromTry (AssetPair extractAssetId assetStr) leftMap (_ => createInvalidSettingValueError(settingName, assetStr))
   }
 
   def validatePercent(settingName: String): ErrorsListOr[Double] = {
-    validateByPredicate[Double](settingName)(p => 0 < p && p <= 100, "required 0 < p <= 100")
+    validateByPredicate[Double](settingName)(p => 0 < p && p <= 100, "required 0 < percent <= 100")
   }
 }
--- src/test/scala/com/wavesplatform/settings/MatcherSettingsSpecification.scala
+++ src/test/scala/com/wavesplatform/settings/MatcherSettingsSpecification.scala
@@ -6,7 +6,7 @@ import com.wavesplatform.matcher.MatcherSettings
 import com.wavesplatform.matcher.MatcherSettings.EventsQueueSettings
 import com.wavesplatform.matcher.api.OrderBookSnapshotHttpCache
 import com.wavesplatform.matcher.queue.{KafkaMatcherQueue, LocalMatcherQueue}
-import com.wavesplatform.settings.fee.AssetType
+import com.wavesplatform.settings.fee.{AssetType, OrderFeeSettings}
 import com.wavesplatform.settings.fee.OrderFeeSettings._
 import org.scalatest.{FlatSpec, Matchers}
 
@@ -85,11 +85,11 @@ class MatcherSettingsSpecification extends FlatSpec with Matchers {
       s"""
          |order-fee {
          |  mode = percent
-         |  fixed-waves {
+         |  waves {
          |    base-fee = 300000
          |  }
          |  fixed {
-         |    asset-id = WAVES
+         |    asset = WAVES
          |    min-fee = 300000
          |  }
          |  percent {
@@ -153,11 +153,11 @@ class MatcherSettingsSpecification extends FlatSpec with Matchers {
       s"""
          |order-fee {
          |  mode = invalid
-         |  fixed-waves {
+         |  waves {
          |    base-fee = 300000
          |  }
          |  fixed {
-         |    asset-id = WAVES
+         |    asset = WAVES
          |    min-fee = 300000
          |  }
          |  percent {
@@ -171,11 +171,11 @@ class MatcherSettingsSpecification extends FlatSpec with Matchers {
       s"""
          |order-fee {
          |  mode = percent
-         |  fixed-waves {
+         |  waves {
          |    base-fee = 300000
          |  }
          |  fixed {
-         |    asset-id = WAVES
+         |    asset = WAVES
          |    min-fee = 300000
          |  }
          |  percent {
@@ -189,30 +189,12 @@ class MatcherSettingsSpecification extends FlatSpec with Matchers {
       s"""
          |order-fee {
          |  mode = fixed
-         |  fixed-waves {
+         |  waves {
          |    base-fee = 300000
          |  }
          |  fixed {
-         |    asset-id = ;;;;
+         |    asset = ;;;;
          |    min-fee = -300000
-         |  }
-         |  percent {
-         |    asset-type = test
-         |    min-fee = 121
-         |  }
-         |}
-       """.stripMargin
-
-    val invalidWavesAsset =
-      s"""
-         |order-fee {
-         |  mode = fixed
-         |  fixed-waves {
-         |    base-fee = 300000
-         |  }
-         |  fixed {
-         |    asset-id = WAVES
-         |    min-fee = 300000
          |  }
          |  percent {
          |    asset-type = test
@@ -224,12 +206,12 @@ class MatcherSettingsSpecification extends FlatSpec with Matchers {
     val invalidFeeInWaves =
       s"""
          |order-fee {
-         |  mode = fixed-waves
-         |  fixed-waves {
+         |  mode = waves
+         |  waves {
          |    base-fee = -350000
          |  }
          |  fixed {
-         |    asset-id = ;;;;
+         |    asset = ;;;;
          |    min-fee = -300000
          |  }
          |  percent {
@@ -242,26 +224,22 @@ class MatcherSettingsSpecification extends FlatSpec with Matchers {
     val settingsInvalidMode           = getSettingByConfig(configStrWithOrderFeeSettings(invalidMode))
     val settingsInvalidTypeAndPercent = getSettingByConfig(configStrWithOrderFeeSettings(invalidAssetTypeAndPercent))
     val settingsInvalidAssetAndFee    = getSettingByConfig(configStrWithOrderFeeSettings(invalidAssetAndFee))
-    val settingsInvalidWavesAsset     = getSettingByConfig(configStrWithOrderFeeSettings(invalidWavesAsset))
     val settingsInvalidFeeInWaves     = getSettingByConfig(configStrWithOrderFeeSettings(invalidFeeInWaves))
 
     settingsInvalidMode shouldBe Left("Invalid setting waves.matcher.order-fee.mode value: invalid")
 
     settingsInvalidTypeAndPercent shouldBe
       Left(
-        "Invalid setting waves.matcher.order-fee.percent.asset-type value: test\n" +
-          "Invalid setting waves.matcher.order-fee.percent.min-fee value: 121.0, required 0 < p <= 100")
+        "Invalid setting waves.matcher.order-fee.percent.asset-type value: test, " +
+          "Invalid setting waves.matcher.order-fee.percent.min-fee value: 121.0, required 0 < percent <= 100")
 
     settingsInvalidAssetAndFee shouldBe
       Left(
-        "Invalid setting waves.matcher.order-fee.fixed.asset-id value: ;;;;\n" +
-          "Invalid setting waves.matcher.order-fee.fixed.min-fee value: -300000, must be > 0")
-
-    settingsInvalidWavesAsset shouldBe
-      Left("Invalid setting waves.matcher.order-fee.fixed.asset-id value: WAVES, asset must not be Waves")
+        "Invalid setting waves.matcher.order-fee.fixed.asset value: ;;;;, " +
+          "Invalid setting waves.matcher.order-fee.fixed.min-fee value: -300000, required 0 < fee")
 
     settingsInvalidFeeInWaves shouldBe Left(
-      s"Invalid setting waves.matcher.order-fee.fixed-waves.base-fee value: -350000, required 0 < base fee <= ${Constants.UnitsInWave * Constants.TotalWaves}"
+      s"Invalid setting waves.matcher.order-fee.waves.base-fee value: -350000, required 0 < base fee <= ${OrderFeeSettings.totalWavesAmount}"
     )
   }
 }
