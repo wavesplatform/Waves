38e0fd9284201228be22eaedd079608ffb357aeb
Valeriy <yoba123@yandex.ru>
2019-03-18 12:54:28 +0300

    NODE-1526: UTX pool overflow bypass fixes

--- src/main/scala/com/wavesplatform/settings/UtxSettings.scala
+++ src/main/scala/com/wavesplatform/settings/UtxSettings.scala
@@ -1,3 +1,9 @@
 package com.wavesplatform.settings
 
-case class UtxSettings(maxSize: Int, maxBytesSize: Long, maxScriptedSize: Int, blacklistSenderAddresses: Set[String], allowBlacklistedTransferTo: Set[String], allowTransactionsFromSmartAccounts: Boolean)
+case class UtxSettings(maxSize: Int,
+                       maxBytesSize: Long,
+                       maxScriptedSize: Int,
+                       blacklistSenderAddresses: Set[String],
+                       allowBlacklistedTransferTo: Set[String],
+                       allowTransactionsFromSmartAccounts: Boolean,
+                       allowSkipChecks: Boolean)
