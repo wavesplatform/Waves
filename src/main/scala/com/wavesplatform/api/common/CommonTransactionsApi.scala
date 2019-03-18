a2a1dde10b6ff8a1a22e3344a9d3020e5594803f
Valeriy <yoba123@yandex.ru>
2019-03-18 13:51:21 +0300

    NODE-1495: gRPC fixes

--- src/main/scala/com/wavesplatform/api/common/CommonTransactionsApi.scala
+++ src/main/scala/com/wavesplatform/api/common/CommonTransactionsApi.scala
@@ -6,12 +6,11 @@ import com.wavesplatform.protobuf.transaction.VanillaTransaction
 import com.wavesplatform.settings.FunctionalitySettings
 import com.wavesplatform.state.Blockchain
 import com.wavesplatform.state.diffs.CommonValidation
-import com.wavesplatform.transaction.transfer.MassTransferTransaction
 import com.wavesplatform.transaction.{Asset, ValidationError}
 import com.wavesplatform.utx.UtxPool
 import com.wavesplatform.wallet.Wallet
 import io.netty.channel.group.ChannelGroup
-import monix.eval.{Coeval, Task}
+import monix.eval.Task
 import monix.reactive.Observable
 
 private[api] class CommonTransactionsApi(functionalitySettings: FunctionalitySettings,
@@ -23,26 +22,15 @@ private[api] class CommonTransactionsApi(functionalitySettings: FunctionalitySet
   private[this] val TransactionsBatchLimit = 100
 
   def transactionsByAddress(address: Address, fromId: Option[ByteStr] = None): Observable[(Int, VanillaTransaction)] = {
-    val cachedAddresses = Coeval.evalOnce(concurrent.blocking((blockchain.aliasesOfAddress(address) :+ address).toSet))
+    def getTransactionsList(address: Address, limit: Int, fromId: Option[ByteStr]): Either[String, Seq[(Int, VanillaTransaction)]] =
+      concurrent.blocking {
+        blockchain.addressTransactions(address, Set.empty, limit, fromId)
+      }
 
-    def getResponse(address: Address, limit: Int, fromId: Option[ByteStr]): Either[String, Seq[(Int, VanillaTransaction)]] = {
-      blockchain.addressTransactions(address, Set.empty, limit, fromId)
-    }
-
-    def filterTxByAliases(tx: VanillaTransaction) = tx match {
-      case mtt: MassTransferTransaction if mtt.sender.toAddress != address =>
-        val recipients = cachedAddresses()
-        mtt.copy(transfers = mtt.transfers.filter(t => recipients.contains(t.address)))
-
-      case _ => tx
-    }
-
-    val observableTask = Task(getResponse(address, TransactionsBatchLimit, fromId)) map {
+    val observableTask = Task(getTransactionsList(address, TransactionsBatchLimit, fromId)) map {
       case Right(transactions) =>
         if (transactions.isEmpty) Observable.empty
-        else
-          Observable(transactions: _*).map(ht => (ht._1, filterTxByAliases(ht._2))) ++ Observable.defer(
-            transactionsByAddress(address, Some(transactions.last._2.id())))
+        else Observable(transactions: _*) ++ Observable.defer(transactionsByAddress(address, Some(transactions.last._2.id())))
 
       case Left(err) =>
         Observable.raiseError(new IllegalArgumentException(err))
