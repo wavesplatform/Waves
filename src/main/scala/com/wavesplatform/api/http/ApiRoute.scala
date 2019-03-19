fe51f10e037fec2657d900c618b86d76f33a40d4
Valeriy <yoba123@yandex.ru>
2019-03-19 15:37:30 +0300

    NODE-1529: Fix REST API execution contexts

--- src/main/scala/com/wavesplatform/api/http/ApiRoute.scala
+++ src/main/scala/com/wavesplatform/api/http/ApiRoute.scala
@@ -26,7 +26,8 @@ trait ApiRoute extends Directives with CommonApiFunctions with ApiMarshallers {
     }
     .result()
 
-  def _json[A: Reads](f: A => Route): Route                 = (handleRejections(jsonRejectionHandler) & entity(as[A])).apply(f)
+  def jsonEntity[A: Reads]: Directive1[A] = handleRejections(jsonRejectionHandler) & entity(as[A])
+  def _json[A: Reads](f: A => Route): Route                 = jsonEntity.apply(f)
   def json[A: Reads](f: A => ToResponseMarshallable): Route = _json[A](a => complete(f(a)))
 
   val jsonExceptionHandler: ExceptionHandler = ExceptionHandler {
