f8eb5a2fb0fc74804320f4e41bbb794e42c9c9be
Valeriy <yoba123@yandex.ru>
2019-03-27 16:18:21 +0300

    NODE-1529: Fix REST API execution contexts configs refactoring

--- src/main/scala/com/wavesplatform/settings/BlockchainSettings.scala
+++ src/main/scala/com/wavesplatform/settings/BlockchainSettings.scala
@@ -148,11 +148,10 @@ object BlockchainSettings {
     (cfg: Config, path: String) => fromConfig(cfg.getConfig(path))
 
 
-  @deprecated("Use config.as[BlockchainSettings]", "0.17.0")
+  // @deprecated("Use config.as[BlockchainSettings]", "0.17.0")
   def fromRootConfig(config: Config): BlockchainSettings = config.as[BlockchainSettings]("waves.blockchain")
 
-  @deprecated("Use config.as[BlockchainSettings]", "0.17.0")
-  def fromConfig(config: Config): BlockchainSettings = {
+  private[this] def fromConfig(config: Config): BlockchainSettings = {
     val blockchainType = config.as[BlockchainType.Value]("type")
     val (addressSchemeCharacter, functionalitySettings, genesisSettings) = blockchainType match {
       case BlockchainType.TESTNET =>
