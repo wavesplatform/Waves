717ac294d037f17f310129f9264488c9b094f5e0
Le Karasique <Karasiq@users.noreply.github.com>
2019-03-27 16:17:31 +0300

    NODE-1539: Log packUnconfirmed time (#2094)

--- src/main/scala/com/wavesplatform/utx/UtxPoolImpl.scala
+++ src/main/scala/com/wavesplatform/utx/UtxPoolImpl.scala
@@ -81,11 +81,13 @@ class UtxPoolImpl(time: Time,
   private def removeExpired(currentTs: Long): Unit = {
     def isExpired(tx: Transaction) = (currentTs - tx.timestamp).millis > fs.maxTransactionTimeBackOffset
 
-    transactions.values.asScala
-      .collect {
-        case tx if isExpired(tx) => tx.id()
-      }
-      .foreach(remove)
+    val toRemove = transactions.values.asScala
+      .collect { case tx if isExpired(tx) => tx.id() }
+
+    if (toRemove.nonEmpty) {
+      log.trace(s"Removing expired transactions: [${toRemove.mkString(", ")}]")
+      toRemove.foreach(remove)
+    }
   }
 
   override def putIfNew(tx: Transaction): Either[ValidationError, (Boolean, Diff)] = putIfNew(blockchain, tx)
@@ -130,7 +132,11 @@ class UtxPoolImpl(time: Time,
     val transactionsToRemove = transactions.values.asScala.filter { t =>
       TransactionDiffer(fs, b.lastBlockTimestamp, time.correctedTime(), b.height)(b, t).isLeft
     }
-    removeAll(transactionsToRemove)
+
+    if (transactionsToRemove.nonEmpty) {
+      log.trace(s"Cleaning up invalid transactions: [${transactionsToRemove.mkString(", ")}]")
+      removeAll(transactionsToRemove)
+    }
   }
 
   override def spendableBalance(addr: Address, assetId: Option[AssetId]): Long =
@@ -174,8 +180,13 @@ class UtxPoolImpl(time: Time,
       .takeWhile(!_._5)
       .reduce((_, s) => s)
 
-    invalidTxs.foreach(remove)
+    if (invalidTxs.nonEmpty) {
+      log.trace(s"Removing ${invalidTxs.length} invalid transactions from UTX: [${invalidTxs.mkString(", ")}]")
+      invalidTxs.foreach(remove)
+    }
+
     val txs = reversedValidTxs.reverse
+    if (txs.nonEmpty) log.trace(s"Packed ${txs.length} unconfirmed transactions, final constraint = $finalConstraint")
     (txs, finalConstraint)
   }
 
