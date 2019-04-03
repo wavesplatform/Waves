77fc110ba56750d82185654c5bf0f87ac3ef077f
finnoio <ndementyev@yandex.ru>
2019-04-03 15:26:53 +0300

    added matcherFeeAssetId tests in ExchangeTransactionSuite and ExchangeWithContractsSuite

--- it/src/test/scala/com/wavesplatform/it/sync/smartcontract/package.scala
+++ it/src/test/scala/com/wavesplatform/it/sync/smartcontract/package.scala
@@ -155,4 +155,29 @@ package object smartcontract {
 
     (buy, sell)
   }
+
+  def orders(pair: AssetPair,
+             ord1Ver: Byte,
+             ord2Ver: Byte,
+             fee: Long,
+             time: Time,
+             matcherFeeOrder1: Asset,
+             matcherFeeOrder2: Asset,
+             accounts: PrivateKeyAccount*): (Order, Order) = {
+
+    val buyer               = accounts.head // first one
+    val seller              = accounts.tail.head // second one
+    val matcher             = accounts.last
+    val ts                  = time.correctedTime()
+    val expirationTimestamp = ts + Order.MaxLiveTime
+    val buyPrice            = 1 * Order.PriceConstant
+    val sellPrice           = (0.50 * Order.PriceConstant).toLong
+    val buyAmount           = 2
+    val sellAmount          = 3
+
+    val buy = Order.buy(buyer, matcher, pair, buyAmount, buyPrice, ts, expirationTimestamp, fee, ord1Ver, matcherFeeOrder1)
+    val sell = Order.sell(seller, matcher, pair, sellAmount, sellPrice, ts, expirationTimestamp, fee, ord2Ver, matcherFeeOrder2)
+
+    (buy, sell)
+  }
 }
