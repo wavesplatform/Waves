bdb06f3bb9edb06288ea4150d4f7a491cb52f213
Mike Potanin <mpotanin@gmail.com>
2019-04-03 18:43:48 +0300

    SC-128: Limit base58 usage for long byte arrays (#2128)

--- common/shared/src/main/scala/com/wavesplatform/common/state/ByteStr.scala
+++ common/shared/src/main/scala/com/wavesplatform/common/state/ByteStr.scala
@@ -15,13 +15,24 @@ case class ByteStr(arr: Array[Byte]) {
 
   lazy val base58: String = Base58.encode(arr)
 
-  lazy val base64: String = "base64:" + Base64.encode(arr)
+  lazy val base64Raw: String = Base64.encode(arr)
+  lazy val base64: String = "base64:" + base64Raw
 
-  lazy val trim: String = base58.toString.take(7) + "..."
+  lazy val trim: String = (if (arr.length < 1024) {
+    base58.toString.take(7)
+  } else {
+    base64Raw
+  }) + "..."
 
-  override lazy val toString: String = base58
+  override lazy val toString: String = if (arr.length < 1024) {
+    base58
+  } else {
+    base64
+  }
 
   def isEmpty: Boolean = arr.length == 0
+
+  def size: Int = arr.length
 
   def ++(other: ByteStr): ByteStr = if (this.isEmpty) other else ByteStr(this.arr ++ other.arr)
 
