5298dc45a384b53a90da84063ead4a6f8e001c8f
Mike Potanin <mpotanin@wavesplatform.com>
2019-04-03 16:49:15 +0300

    SC-128-limit-base58-use Fix style

--- common/shared/src/main/scala/com/wavesplatform/common/state/ByteStr.scala
+++ common/shared/src/main/scala/com/wavesplatform/common/state/ByteStr.scala
@@ -15,13 +15,13 @@ case class ByteStr(arr: Array[Byte]) {
 
   lazy val base58: String = Base58.encode(arr)
 
-  lazy val base64_row: String = Base64.encode(arr)
-  lazy val base64: String = "base64:" + base64_row
+  lazy val base64Raw: String = Base64.encode(arr)
+  lazy val base64: String = "base64:" + base64Raw
 
   lazy val trim: String = (if (arr.length < 1024) {
     base58.toString.take(7)
   } else {
-    base64_row
+    base64Raw
   }) + "..."
 
   override lazy val toString: String = if (arr.length < 1024) {
--- lang/shared/src/main/scala/com/wavesplatform/lang/v1/compiler/Decompiler.scala
+++ lang/shared/src/main/scala/com/wavesplatform/lang/v1/compiler/Decompiler.scala
@@ -65,7 +65,7 @@ object Decompiler {
       case Terms.CONST_BOOLEAN(b)        => pureOut(b.toString.toLowerCase(), i)
       case Terms.CONST_LONG(t)           => pureOut(t.toLong.toString, i)
       case Terms.CONST_STRING(s)         => pureOut('"' + s + '"', i)
-      case Terms.CONST_BYTESTR(bs)       => pureOut(if(bs.size <= 128) { "base58'" ++ bs.toString ++ "'" } else { "base64'" ++ bs.base64_row ++ "'" }, i)
+      case Terms.CONST_BYTESTR(bs)       => pureOut(if(bs.size <= 128) { "base58'" ++ bs.toString ++ "'" } else { "base64'" ++ bs.base64Raw ++ "'" }, i)
       case Terms.REF(ref)                => pureOut(ref, i)
       case Terms.GETTER(getExpr, fld)    => expr(pure(getExpr), ctx, BracesWhenNeccessary, firstLinePolicy).map(a => a + "." + fld)
       case Terms.IF(cond, it, iff) =>
