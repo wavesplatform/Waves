7693fa682a8248ec160bd3250c2dae912c01ca5c
Ivan Rakitnykh <mrkr.reg@gmail.com>
2019-04-04 11:21:26 +0300

    Merge fix.

--- lang/js/src/main/scala/JsAPI.scala
+++ lang/js/src/main/scala/JsAPI.scala
@@ -91,7 +91,7 @@ object JsAPI {
 
   @JSExportTopLevel("getTypes")
   def getTypes(ver: Int = 2, isTokenContext: Boolean = false, isContract: Boolean = false): js.Array[js.Object with js.Dynamic] =
-    buildScriptContext(StdLibVersion(ver), isTokenContext, isContract).types
+    buildScriptContext(DirectiveDictionary[StdLibVersion].idMap(ver), isTokenContext, isContract).types
       .map(v => js.Dynamic.literal("name" -> v.name, "type" -> typeRepr(v)))
       .toJSArray
 
