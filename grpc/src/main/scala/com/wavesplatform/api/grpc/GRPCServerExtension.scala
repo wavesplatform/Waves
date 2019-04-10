da71d71fd7986af2f3169bd7004a077bcedc5a90
Valeriy Gavrilin <yoba123@yandex.ru>
2019-04-10 13:58:59 +0300

    Fixes

--- grpc/src/main/scala/com/wavesplatform/api/grpc/GRPCServerExtension.scala
+++ grpc/src/main/scala/com/wavesplatform/api/grpc/GRPCServerExtension.scala
@@ -6,11 +6,11 @@ import com.typesafe.config.ConfigFactory
 import com.wavesplatform.extensions.{Extension, Context => ExtensionContext}
 import com.wavesplatform.settings.GRPCSettings
 import com.wavesplatform.utils.ScorexLogging
+import io.grpc.Server
 import io.grpc.netty.NettyServerBuilder
+import monix.execution.Scheduler
 import net.ceedubs.ficus.Ficus._
 import net.ceedubs.ficus.readers.ArbitraryTypeReader._
-import io.grpc.{Server, ServerBuilder}
-import monix.execution.Scheduler
 
 import scala.concurrent.Future
 
