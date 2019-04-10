4566c5009ea9819fbd6916534704286fc8ae333a
Alexandr Makoed <me@dsfn.gq>
2019-04-10 16:14:45 +0300

    NODE-1550: Fixes

--- it/src/test/scala/com/wavesplatform/it/sync/smartcontract/smartasset/ExchangeSmartAssetsSuite.scala
+++ it/src/test/scala/com/wavesplatform/it/sync/smartcontract/smartasset/ExchangeSmartAssetsSuite.scala
@@ -124,7 +124,7 @@ class ExchangeSmartAssetsSuite extends BaseTransactionSuite with CancelAfterFail
 
       assertBadRequestAndMessage(
         sender.signedBroadcast(exchangeTx(smartAssetPair, smartMatcherFee + smartFee, smartMatcherFee + smartFee, ntpTime, acc1, acc0, acc2)),
-        "com.wavesplatform.transaction.assets.exchange.ExchangeTransactionV2 does not exceed minimal value of 1500000"
+        "does not exceed minimal value of 1500000"
       )
 
       sender.signedBroadcast(exchangeTx(smartAssetPair, smartMatcherFee + 2 * smartFee, smartMatcherFee + 2 * smartFee, ntpTime, acc1, acc0, acc2),
