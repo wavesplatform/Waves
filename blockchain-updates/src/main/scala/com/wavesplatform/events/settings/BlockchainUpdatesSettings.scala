3372d4fa577d05b41f30c8f7b74fc711b75d06d7
Dmitry Shuranov <dvshur@gmail.com>
2019-04-18 13:49:33 +0300

    DATA-291 explicit settings reading

--- blockchain-updates/src/main/scala/com/wavesplatform/events/settings/BlockchainUpdatesSettings.scala
+++ blockchain-updates/src/main/scala/com/wavesplatform/events/settings/BlockchainUpdatesSettings.scala
@@ -1,23 +1,23 @@
 package com.wavesplatform.events.settings
 
+import com.typesafe.config.Config
+
 case class BlockchainUpdatesSettings(bootstrapServers: String, topic: String, clientId: String)
 
 object BlockchainUpdatesSettings {
-  import net.ceedubs.ficus.readers.{NameMapper, ValueReader}
-  import net.ceedubs.ficus.readers.ArbitraryTypeReader.arbitraryTypeValueReader
+  import net.ceedubs.ficus.Ficus._
+  import net.ceedubs.ficus.readers.ValueReader
 
-  implicit val chosenCase: NameMapper = net.ceedubs.ficus.readers.namemappers.implicits.hyphenCase
+  implicit val valueReader: ValueReader[BlockchainUpdatesSettings] = (cfg, path) => fromConfig(cfg getConfig path)
 
-  implicit val valueReader: ValueReader[BlockchainUpdatesSettings] = arbitraryTypeValueReader
+  private val configPath = "blockchain-updates"
+  private def fromConfig(c: Config): BlockchainUpdatesSettings = {
+    val config = c.getConfig(configPath)
 
-//  val configPath = "blockchain-updates"
-//  def fromConfig(c: Config): BlockchainUpdatesSettings = {
-//    val config = c.getConfig(configPath)
-//
-//    BlockchainUpdatesSettings(
-//      config.as[String]("bootstrap-servers"),
-//      config.as[String]("topic"),
-//      config.as[String]("client-id")
-//    )
-//  }
+    BlockchainUpdatesSettings(
+      config.as[String]("bootstrap-servers"),
+      config.as[String]("topic"),
+      config.as[String]("client-id")
+    )
+  }
 }
