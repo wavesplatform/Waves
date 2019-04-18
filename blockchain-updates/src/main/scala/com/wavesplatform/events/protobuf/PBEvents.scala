0bb7ab7b632eb28cea5a68e44ca76d9486ffc645
Dmitry Shuranov <dvshur@gmail.com>
2019-04-18 15:40:42 +0300

    DATA-308 added DataEntries sending in blockchain updates

--- blockchain-updates/src/main/scala/com/wavesplatform/events/protobuf/PBEvents.scala
+++ blockchain-updates/src/main/scala/com/wavesplatform/events/protobuf/PBEvents.scala
@@ -1,14 +1,20 @@
 package com.wavesplatform.events.protobuf
 
 import com.wavesplatform.common.state.ByteStr
-import com.wavesplatform.protobuf.block.{PBBlocks, PBMicroBlocks}
-import com.wavesplatform.events.protobuf.StateUpdated.ReasonType.{BLOCK, MICROBLOCK, TRANSACTION}
-import com.wavesplatform.events.protobuf.StateUpdated.{BalanceUpdated => PBBalanceUpdated, LeasingUpdated => PBLeasingUpdated}
 import com.wavesplatform.state.{BlockAdded, MicroBlockAdded, RollbackCompleted}
 
-object PBEvents {
-  import com.wavesplatform.protobuf.utils.PBImplicitConversions._
+import com.wavesplatform.protobuf.block.{PBBlocks, PBMicroBlocks}
+import com.wavesplatform.protobuf.transaction.PBTransactions
+import com.wavesplatform.protobuf.utils.PBImplicitConversions._
 
+import com.wavesplatform.events.protobuf.StateUpdated.ReasonType.{BLOCK, MICROBLOCK, TRANSACTION}
+import com.wavesplatform.events.protobuf.StateUpdated.{
+  BalanceUpdated => PBBalanceUpdated,
+  LeasingUpdated => PBLeasingUpdated,
+  DataEntryUpdated => PBDataEntryUpdated
+}
+
+object PBEvents {
   private def protobufStateUpdated(reason: (Short, ByteStr), su: VanillaStateUpdated): PBStateUpdated = {
     val reasonType = reason._1 match {
       case 0 => BLOCK
@@ -25,6 +31,9 @@ object PBEvents {
       leases = su.leases.map {
         case (addr, leaseBalance) =>
           PBLeasingUpdated(address = addr, in = leaseBalance.in, out = leaseBalance.out)
+      },
+      dataEntries = su.dataEntries.map {
+        case (addr, entry) => PBDataEntryUpdated(address = addr, dataEntry = Some(PBTransactions.toPBDataEntry(entry)))
       }
     )
   }
