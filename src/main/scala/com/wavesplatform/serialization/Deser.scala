5cdb3ba4e5977e12e7043dd2c659065202d45f2f
Alexandr Makoed <me@dsfn.gq>
2019-04-22 13:33:43 +0300

    NODE-1580: Create proofs with bytes

--- src/main/scala/com/wavesplatform/serialization/Deser.scala
+++ src/main/scala/com/wavesplatform/serialization/Deser.scala
@@ -6,7 +6,10 @@ object Deser {
 
   def serializeBoolean(b: Boolean): Array[Byte] = if (b) Array(1: Byte) else Array(0: Byte)
 
-  def serializeArray(b: Array[Byte]): Array[Byte] = Shorts.toByteArray(b.length.toShort) ++ b
+  def serializeArray(b: Array[Byte]): Array[Byte] = {
+    val lengthBytes = Shorts.toByteArray(b.length.toShort)
+    Bytes.concat(lengthBytes, b)
+  }
 
   def parseArraySize(bytes: Array[Byte], position: Int): (Array[Byte], Int) = {
     val length = Shorts.fromByteArray(bytes.slice(position, position + 2))
@@ -39,5 +42,10 @@ object Deser {
 
   def serializeOption[T](b: Option[T])(ser: T => Array[Byte]): Array[Byte] = b.map(a => (1: Byte) +: serializeArray(ser(a))).getOrElse(Array(0: Byte))
 
-  def serializeArrays(bs: Seq[Array[Byte]]): Array[Byte] = Shorts.toByteArray(bs.length.toShort) ++ Bytes.concat(bs.map(serializeArray): _*)
+  def serializeArrays(bs: Seq[Array[Byte]]): Array[Byte] = {
+    val countBytes  = Shorts.toByteArray(bs.length.toShort)
+    val arraysBytes = Bytes.concat(bs.map(serializeArray): _*)
+
+    Bytes.concat(countBytes, arraysBytes)
+  }
 }
