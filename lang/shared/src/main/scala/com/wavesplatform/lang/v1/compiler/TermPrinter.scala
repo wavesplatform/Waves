4069ec6cf2d178ec53eae7d8c989fb7fded35edc
Artyom Sayadyan <xrtm000@gmail.com>
2019-04-24 14:32:51 +0300

    SC-171 Optimize Verifier.logged

---
+++ lang/shared/src/main/scala/com/wavesplatform/lang/v1/compiler/TermPrinter.scala
@@ -0,0 +1,64 @@
+package com.wavesplatform.lang.v1.compiler
+
+import com.wavesplatform.lang.v1.compiler.Terms.{ARR, CaseObj, EVALUATED}
+
+object TermPrinter {
+  def string(e: EVALUATED): String = {
+    val sb = new StringBuilder()
+    print(s => sb.append(s), e)
+    sb.mkString
+  }
+
+  def indentObjString(e: CaseObj, depth: Int): String = {
+    val sb = new StringBuilder()
+    printObj(s => sb.append(s), e, depth)
+    sb.mkString
+  }
+
+  def print(toDest: String => Unit, e: EVALUATED, depth: Int = 0): Unit =
+    e match {
+      case obj: CaseObj => printObj(toDest, obj, depth)
+      case arr: ARR     => printArr(toDest, arr)
+      case a            =>
+        val str = a.toString
+        if (str.isEmpty) toDest("<empty>")
+        else toDest(str)
+    }
+
+  private def printArr(toDest: String => Unit, arr: ARR): Unit = {
+    toDest("[")
+    var i = 0
+    while (i < arr.xs.length - 1) {
+      print(toDest, arr.xs(i))
+      toDest(", ")
+      i = i + 1
+    }
+    print(toDest, arr.xs(arr.xs.length - 1))
+    toDest("]")
+  }
+
+  private def printObj(toDest: String => Unit, obj: CaseObj, depth: Int): Unit = {
+    toDest(obj.caseType.name)
+    if (obj.fields.nonEmpty) {
+      toDest("(\n")
+      obj.fields
+        .foreach { case (name, value) =>
+          indent(toDest, depth + 1)
+          toDest(name)
+          toDest(" = ")
+          this.print(toDest, value, depth + 1)
+          toDest("\n")
+        }
+      indent(toDest, depth)
+      toDest(")")
+    }
+  }
+
+  private def indent(toDest: String => Unit, n: Int): Unit = {
+    var i = n
+    while (i > 0) {
+      toDest("\t")
+      i = i - 1
+    }
+  }
+}
