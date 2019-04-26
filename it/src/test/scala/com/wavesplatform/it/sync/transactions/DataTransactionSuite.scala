3beefee2acc3f6513dc3895f13c0614fa40f5821
Alexandr M <me@dsfn.gq>
2019-04-26 14:04:32 +0300

    NODE-1580: Proofs ser/de (#2183)
    * NODE-1580: Create proofs with bytes
    * NODE-1580: Add overflow check

--- it/src/test/scala/com/wavesplatform/it/sync/transactions/DataTransactionSuite.scala
+++ it/src/test/scala/com/wavesplatform/it/sync/transactions/DataTransactionSuite.scala
@@ -238,19 +238,19 @@ class DataTransactionSuite extends BaseTransactionSuite {
                                 "Duplicate keys found")
 
     val extraValueData = List(BinaryDataEntry("key", ByteStr(Array.fill(MaxValueSize + 1)(1.toByte))))
-    assertBadRequestAndResponse(sender.putData(firstAddress, extraValueData, calcDataFee(extraValueData)), TooBig)
+    assertBadRequestAndResponse(sender.putData(firstAddress, extraValueData, 1.waves), TooBig)
     nodes.waitForHeightArise()
 
     val largeBinData = List.tabulate(5)(n => BinaryDataEntry(extraKey, ByteStr(Array.fill(MaxValueSize)(n.toByte))))
-    assertBadRequestAndResponse(sender.putData(firstAddress, largeBinData, calcDataFee(largeBinData)), TooBig)
+    assertBadRequestAndResponse(sender.putData(firstAddress, largeBinData, 1.waves), TooBig)
     nodes.waitForHeightArise()
 
     val largeStrData = List.tabulate(5)(n => StringDataEntry(extraKey, "A" * MaxValueSize))
-    assertBadRequestAndResponse(sender.putData(firstAddress, largeStrData, calcDataFee(largeStrData)), TooBig)
+    assertBadRequestAndResponse(sender.putData(firstAddress, largeStrData, 1.waves), TooBig)
     nodes.waitForHeightArise()
 
     val tooManyEntriesData = List.tabulate(MaxEntryCount + 1)(n => IntegerDataEntry("key", 88))
-    assertBadRequestAndResponse(sender.putData(firstAddress, tooManyEntriesData, calcDataFee(tooManyEntriesData)), TooBig)
+    assertBadRequestAndResponse(sender.putData(firstAddress, tooManyEntriesData, 1.waves), TooBig)
     nodes.waitForHeightArise()
   }
 
