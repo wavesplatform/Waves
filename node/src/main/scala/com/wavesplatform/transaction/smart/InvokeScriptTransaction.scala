b2ccea11a128be74f5e3153a3f10b9f669a1a332
Ivan Rakitnykh <mrkr.reg@gmail.com>
2019-05-16 13:23:30 +0300

    SC-108 ARR, CaseObj, REF(unit) for args of contractinvocation should be dropped.

--- node/src/main/scala/com/wavesplatform/transaction/smart/InvokeScriptTransaction.scala
+++ node/src/main/scala/com/wavesplatform/transaction/smart/InvokeScriptTransaction.scala
@@ -8,7 +8,7 @@ import com.wavesplatform.common.utils.EitherExt2
 import com.wavesplatform.crypto
 import com.wavesplatform.lang.ValidationError
 import com.wavesplatform.lang.v1.compiler.Terms
-import com.wavesplatform.lang.v1.compiler.Terms.{EVALUATED, REF}
+import com.wavesplatform.lang.v1.compiler.Terms.{ARR, CaseObj, EVALUATED}
 import com.wavesplatform.lang.v1.{ContractLimits, FunctionHeader, Serde}
 import com.wavesplatform.serialization.Deser
 import com.wavesplatform.transaction.Asset._
@@ -114,9 +114,10 @@ object InvokeScriptTransaction extends TransactionParserFor[InvokeScriptTransact
             .cond(tx.payment.forall(_.amount > 0), (), NonPositiveAmount(0, tx.payment.find(_.amount <= 0).get.assetId.fold("Waves")(_.toString))))
         .flatMap(_ =>
           Either.cond(
-            tx.funcCallOpt.isEmpty || tx.funcCallOpt.get.args.forall(x => x.isInstanceOf[EVALUATED] || x == REF("unit")),
+            tx.funcCallOpt.isEmpty || tx.funcCallOpt.get.args.forall(x =>
+              x.isInstanceOf[EVALUATED] && !x.isInstanceOf[CaseObj] && !x.isInstanceOf[ARR]),
             (),
-            GenericError("all arguments of invokeScript must be EVALUATED")
+            GenericError("All arguments of invokeScript must be one of the types: Int, ByteVector, String, Boolean")
         ))
         .map(_ => tx)
         .foldToTry
@@ -149,10 +150,11 @@ object InvokeScriptTransaction extends TransactionParserFor[InvokeScriptTransact
       _ <- checkAmounts(p)
       _ <- Either.cond(p.length <= 1, (), GenericError("Multiple payment isn't allowed now"))
       _ <- Either.cond(p.map(_.assetId).distinct.length == p.length, (), GenericError("duplicate payments"))
-
-      _ <- Either.cond(fc.isEmpty || fc.get.args.forall(x => x.isInstanceOf[EVALUATED] || x == REF("unit")),
-                       (),
-                       GenericError("all arguments of invokeScript must be EVALUATED"))
+      _ <- Either.cond(
+        fc.isEmpty || fc.get.args.forall(x => x.isInstanceOf[EVALUATED] && !x.isInstanceOf[CaseObj] && !x.isInstanceOf[ARR]),
+        (),
+        GenericError("All arguments of invokeScript must be one of the types: Int, ByteVector, String, Boolean")
+      )
       tx   = new InvokeScriptTransaction(currentChainId, sender, dappAddress, fc, p, fee, feeAssetId, timestamp, proofs)
       size = tx.bytes().length
       _ <- Either.cond(size <= ContractLimits.MaxInvokeScriptSizeInBytes, (), TooBigArray)
