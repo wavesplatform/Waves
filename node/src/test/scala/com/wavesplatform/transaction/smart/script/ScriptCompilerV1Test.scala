09b6fa3eafe2ff7a03e15a6f136ee6889a1691db
Artyom Sayadyan <xrtm000@gmail.com>
2019-05-21 17:43:07 +0300

    SC-94 Test complexity (#2262)

--- node/src/test/scala/com/wavesplatform/transaction/smart/script/ScriptCompilerV1Test.scala
+++ node/src/test/scala/com/wavesplatform/transaction/smart/script/ScriptCompilerV1Test.scala
@@ -9,10 +9,10 @@ import com.wavesplatform.lang.v1.compiler.Terms._
 import com.wavesplatform.lang.v1.evaluator.FunctionIds._
 import com.wavesplatform.lang.v1.evaluator.ctx.impl.PureContext
 import com.wavesplatform.state.diffs._
-import org.scalatest.{Matchers, PropSpec}
+import org.scalatest.{Inside, Matchers, PropSpec}
 import org.scalatestplus.scalacheck.{ScalaCheckPropertyChecks => PropertyChecks}
 
-class ScriptCompilerV1Test extends PropSpec with PropertyChecks with Matchers {
+class ScriptCompilerV1Test extends PropSpec with PropertyChecks with Matchers with Inside {
 
   property("compile script with specified version") {
     val script = scriptWithVersion("1".some)
@@ -180,6 +180,66 @@ class ScriptCompilerV1Test extends PropSpec with PropertyChecks with Matchers {
     ScriptCompiler.compile(script) shouldBe 'left
   }
 
+  property("max dapp complexity") {
+    def scriptWithHighComplexity(assigns: Int): String =
+      s"""
+         | {-# STDLIB_VERSION 3       #-}
+         | {-# CONTENT_TYPE   DAPP    #-}
+         | {-# SCRIPT_TYPE    ACCOUNT #-}
+         |
+         | @Verifier(tx)
+         | func verify() = {
+         |   let a0 = [base58'']
+         |   ${1 to assigns map (i => s"let a$i = [a${i - 1}[0] + a0[1]]") mkString " "}
+         |   let c0 = 1
+         |   let c1 = c0 + c0
+         |   let c2 = c0 + c0
+         |   let c3 = c0 + c0
+         |   a$assigns == a$assigns && true && true && true && true
+         | }
+         |
+      """.stripMargin
+
+    val count = 343
+
+    inside(ScriptCompiler.compile(scriptWithHighComplexity(count))) {
+      case Right((_, complexity)) => complexity shouldBe 10000
+    }
+
+    inside(ScriptCompiler.compile(scriptWithHighComplexity(count + 1))) {
+      case Left(msg) => msg shouldBe "Contract function (verify) is too complex: 10029 > 10000"
+    }
+  }
+
+  property("transactionByID complexity") {
+    def transactionByIdComplexity(version: Int) = {
+      val scriptWithoutTransactionById =
+        s"""
+          | {-# STDLIB_VERSION $version #-}
+          |
+          | let a = base64''
+          | a == a
+          |
+      """.stripMargin
+
+      val scriptWithTransactionById =
+        s"""
+          | {-# STDLIB_VERSION $version #-}
+          |
+          | let a = transactionById(base64'')
+          | a == a
+          |
+      """.stripMargin
+
+      val c1 = ScriptCompiler.compile(scriptWithoutTransactionById).explicitGet()._2
+      val c2 = ScriptCompiler.compile(scriptWithTransactionById).explicitGet()._2
+      c2 - c1
+    }
+
+    transactionByIdComplexity(2) shouldBe 100
+    transactionByIdComplexity(3) shouldBe 500
+  }
+
   private val expectedExpr = LET_BLOCK(
     LET("x", CONST_LONG(10)),
     FUNCTION_CALL(
@@ -203,7 +263,7 @@ class ScriptCompilerV1Test extends PropSpec with PropertyChecks with Matchers {
     s"""
        | $directive
        |
-      | let x = 10
+       | let x = 10
        | 20 == x + x
        |
       """.stripMargin
@@ -218,7 +278,7 @@ class ScriptCompilerV1Test extends PropSpec with PropertyChecks with Matchers {
     s"""
        | $directive
        |
-      | let x = 10
+       | let x = 10
        | 20 == x + x
        |
       """.stripMargin
@@ -233,7 +293,7 @@ class ScriptCompilerV1Test extends PropSpec with PropertyChecks with Matchers {
     s"""
        | $directive
        |
-      | let x = 10
+       | let x = 10
        | 20 == x + x
        |
       """.stripMargin
