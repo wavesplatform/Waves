b864002cb9ffef6d5c238c69bc5a862eee5c4525
Alexandr Makoed <me@dsfn.gq>
2019-05-22 15:40:06 +0300

    NODE-1609: IT

--- node/src/main/scala/com/wavesplatform/features/BlockchainFeature.scala
+++ node/src/main/scala/com/wavesplatform/features/BlockchainFeature.scala
@@ -16,7 +16,7 @@ object BlockchainFeatures {
   val SmartAccountTrading             = BlockchainFeature(10, "Smart Account Trading")
   val Ride4DApps                      = BlockchainFeature(11, "RIDE 4 DAPPS")
   val OrderV3                         = BlockchainFeature(12, "Order Version 3")
-  val ReduceNFTFee                    = BlockchainFeature(12, "Reduce NFT fee")
+  val ReduceNFTFee                    = BlockchainFeature(13, "Reduce NFT fee")
 
   // When next fork-parameter is created, you must replace all uses of the DummyFeature with the new one.
   val DummyFeature = BlockchainFeature(-1, "Non Votable!")
