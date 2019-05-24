bea8a99f11330675f5af2bf38cd787c23dc2529b
Valeriy Gavrilin <yoba123@yandex.ru>
2019-05-24 18:46:50 +0300

    Revert: Fixes

--- node/src/main/scala/com/wavesplatform/state/reader/CompositeBlockchain.scala
+++ node/src/main/scala/com/wavesplatform/state/reader/CompositeBlockchain.scala
@@ -261,6 +261,6 @@ class CompositeBlockchain(inner: Blockchain, maybeDiff: => Option[Diff], carry: 
 
 object CompositeBlockchain {
   def composite(inner: Blockchain, diff: => Option[Diff]): Blockchain = new CompositeBlockchain(inner, diff)
-  def composite(inner: Blockchain, diff: Diff, carryFee: Long = 0, block: Option[Block] = None): Blockchain =
-    new CompositeBlockchain(inner, Some(diff), carryFee)
+  def composite(inner: Blockchain, diff: Diff, carryFee: Long = 0): Blockchain = new CompositeBlockchain(inner, Some(diff), carryFee)
+  def composite(inner: Blockchain, block: Block): Blockchain = new CompositeBlockchain(inner, None, 0, Some(block))
 }
