d6130a99c419d2bb909ed3117a7914d4f5d3b7ed
Mike Potanin <mpotanin@wavesplatform.com>
2019-06-11 12:13:01 +0300

    remove-unreachable-code Fix

--- lang/shared/src/main/scala/com/wavesplatform/lang/v1/compiler/TypeInferrer.scala
+++ lang/shared/src/main/scala/com/wavesplatform/lang/v1/compiler/TypeInferrer.scala
@@ -88,11 +88,6 @@ object TypeInferrer {
     case (t1, t2) if t1 == t2 => t1
 
     case (LIST(it1), LIST(it2)) => LIST(findCommonType(it1, it2))
-      findCommonType(it1, it2) match {
-        case NOTHING    => NOTHING
-        case u: UNION   => LIST(u)
-        case p: SINGLE  => LIST(p)
-      }
     case (p1: SINGLE, p2: SINGLE) => UNION.create(List(p1, p2))
     case (r: UNION, a: UNION)     => UNION.create((r.typeList.toSet ++ a.typeList.toSet).toSeq)
     case (u: UNION, t: SINGLE)    => if (u.typeList.contains(t)) u else UNION.create(u.typeList :+ t)
