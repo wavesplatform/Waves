3bbc9bcbb09646434025baef610adca6efde08e6
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2019-06-14 15:34:34 +0300

    DEX-274 New inaccessible functions and fields

--- dex/src/main/scala/com/wavesplatform/matcher/smart/MatcherContext.scala
+++ dex/src/main/scala/com/wavesplatform/matcher/smart/MatcherContext.scala
@@ -4,11 +4,12 @@ import cats.Eval
 import cats.data.EitherT
 import cats.implicits._
 import cats.kernel.Monoid
-import com.wavesplatform.lang.Global
+import com.wavesplatform.lang.{ExecutionError, Global}
 import com.wavesplatform.lang.directives.DirectiveDictionary
 import com.wavesplatform.lang.directives.values._
-import com.wavesplatform.lang.v1.compiler.Terms.{CONST_LONG, CaseObj}
-import com.wavesplatform.lang.v1.compiler.Types.{CASETYPEREF, FINAL, UNIT}
+import StdLibVersion.VersionDic
+import com.wavesplatform.lang.v1.compiler.Terms.{CaseObj, EVALUATED}
+import com.wavesplatform.lang.v1.compiler.Types.{CASETYPEREF, FINAL, LONG, UNIT}
 import com.wavesplatform.lang.v1.evaluator.FunctionIds._
 import com.wavesplatform.lang.v1.evaluator.ctx._
 import com.wavesplatform.lang.v1.evaluator.ctx.impl.waves.Bindings.{ordType, orderObject}
@@ -24,30 +25,25 @@ object MatcherContext {
   def build(version: StdLibVersion, nByte: Byte, in: Eval[Order], proofsEnabled: Boolean): EvaluationContext = {
     val baseContext = Monoid.combine(PureContext.build(Global, version), CryptoContext.build(Global, version)).evaluationContext
 
-    val inputEntityCoeval: Eval[Either[String, CaseObj]] =
+    val inputEntityCoeval: Eval[Either[ExecutionError, CaseObj]] =
       Eval.defer(in.map(o => Right(orderObject(RealTransactionWrapper.ord(o), proofsEnabled, version))))
 
-    val sellOrdTypeCoeval: Eval[Either[String, CaseObj]] = Eval.now(Right(ordType(OrdType.Sell)))
-    val buyOrdTypeCoeval: Eval[Either[String, CaseObj]]  = Eval.now(Right(ordType(OrdType.Buy)))
-
-    val heightCoeval: Eval[Either[String, CONST_LONG]] = Eval.now(Left("height is inaccessible when running script on matcher"))
+    def constVal[T <: EVALUATED](x: T): LazyVal = LazyVal(EitherT(Eval.now[Either[ExecutionError, T]](Right(x))))
+    def inaccessibleVal(name: String): LazyVal =
+      LazyVal(EitherT(Eval.now[Either[ExecutionError, Nothing]](Left(s"$name is inaccessible when running script on matcher"))))
 
     val orderType: CASETYPEREF = buildOrderType(proofsEnabled)
     val matcherTypes           = Seq(addressType, orderType, assetPairType)
 
-    val txMap: Map[String, ((FINAL, String), LazyVal)] =
-      if (version == V1 || version == V2)
-        Map(("tx", ((orderType, "Processing order"), LazyVal(EitherT(inputEntityCoeval)))))
-      else Map.empty
+    val vars: Map[String, ((FINAL, String), LazyVal)] = Seq[(String, FINAL, String, LazyVal)](
+      ("tx", orderType, "Processing order", LazyVal(EitherT(inputEntityCoeval))),
+      ("height", LONG, "undefined height placeholder", inaccessibleVal("height")),
+      ("lastBlock", blockInfo, "undefined lastBlock placeholder", inaccessibleVal("lastBlock")),
+      ("Sell", ordTypeType, "Sell OrderType", constVal(ordType(OrdType.Sell))),
+      ("Buy", ordTypeType, "Buy OrderType", constVal(ordType(OrdType.Buy)))
+    ).map { case (name, retType, desc, value) => (name, ((retType, desc), value)) }(collection.breakOut)
 
-    val commonMatcherVars: Map[String, ((FINAL, String), LazyVal)] = Map(
-      ("height", ((com.wavesplatform.lang.v1.compiler.Types.LONG, "undefined height placeholder"), LazyVal(EitherT(heightCoeval)))),
-      ("Sell", ((ordTypeType, "Sell OrderType"), LazyVal(EitherT(sellOrdTypeCoeval)))),
-      ("Buy", ((ordTypeType, "Buy OrderType"), LazyVal(EitherT(buyOrdTypeCoeval))))
-    )
-    val matcherVars = commonMatcherVars ++ txMap
-
-    def inaccessibleFunction(name: String, internalName: Short): BaseFunction = {
+    def inaccessibleFunction(internalName: Short, name: String): BaseFunction = {
       val msg = s"Function $name is inaccessible when running script on matcher"
       NativeFunction(name, 1, internalName, UNIT, msg, Seq.empty: _*) { case _ => msg.asLeft }
     }
@@ -64,33 +60,31 @@ object MatcherContext {
       )
     }
 
-    val getIntegerF: BaseFunction           = inaccessibleFunction("getInteger", DATA_LONG_FROM_STATE)
-    val getBooleanF: BaseFunction           = inaccessibleFunction("getBoolean", DATA_BOOLEAN_FROM_STATE)
-    val getBinaryF: BaseFunction            = inaccessibleFunction("getBinary", DATA_BYTES_FROM_STATE)
-    val getStringF: BaseFunction            = inaccessibleFunction("getString", DATA_STRING_FROM_STATE)
-    val txByIdF: BaseFunction               = inaccessibleFunction("txByIdF", GETTRANSACTIONBYID)
-    val txHeightByIdF: BaseFunction         = inaccessibleFunction("txHeightByIdF", TRANSACTIONHEIGHTBYID)
-    val addressFromPublicKeyF: BaseFunction = inaccessibleUserFunction("addressFromPublicKeyF")
-    val addressFromStringF: BaseFunction    = inaccessibleUserFunction("addressFromStringF")
-    val addressFromRecipientF: BaseFunction = inaccessibleFunction("addressFromRecipientF", ADDRESSFROMRECIPIENT)
-    val assetBalanceF: BaseFunction         = inaccessibleFunction("assetBalanceF", ACCOUNTASSETBALANCE)
-    val wavesBalanceF: BaseFunction         = inaccessibleUserFunction("wavesBalanceF")
+    val nativeFunctions = Array(
+      DATA_LONG_FROM_STATE    -> "getInteger",
+      DATA_BOOLEAN_FROM_STATE -> "getBoolean",
+      DATA_BYTES_FROM_STATE   -> "getBinary",
+      DATA_STRING_FROM_STATE  -> "getString",
+      GETTRANSACTIONBYID      -> "txById",
+      ADDRESSFROMRECIPIENT    -> "addressFromRecipient",
+      ACCOUNTASSETBALANCE     -> "assetBalance",
+      GETASSETINFOBYID        -> "assetInfo",
+      TRANSFERTRANSACTIONBYID -> "transferTransactionById",
+      TRANSACTIONHEIGHTBYID   -> "transactionHeightById",
+      BLOCKINFOBYHEIGHT       -> "blockInfoByHeight"
+    ).map(Function.tupled(inaccessibleFunction))
 
-    val functions = Array(
-      txByIdF,
-      txHeightByIdF,
-      getIntegerF,
-      getBooleanF,
-      getBinaryF,
-      getStringF,
-      addressFromPublicKeyF,
-      addressFromStringF,
-      addressFromRecipientF,
-      assetBalanceF,
-      wavesBalanceF
-    )
+    val userFunctions = Array(
+      "getIntegerValue",
+      "getBooleanValue",
+      "getBinaryValue",
+      "getStringValue",
+      "addressFromPublicKey",
+      "addressFromString",
+      "wavesBalance"
+    ).map(inaccessibleUserFunction)
 
-    val matcherContext = CTX(matcherTypes, matcherVars, functions).evaluationContext
+    val matcherContext = CTX(matcherTypes, vars, nativeFunctions ++ userFunctions).evaluationContext
 
     baseContext |+| matcherContext
   }
