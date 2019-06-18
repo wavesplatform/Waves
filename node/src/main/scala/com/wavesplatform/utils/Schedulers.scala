8d229a592706a66eae4a3cc2d8d4b021ca07b0e4
Alexandr Makoed <me@dsfn.gq>
2019-06-18 13:25:22 +0300

    NODE-1772: Note about AdaptedMixin, custom rejected execution handler

--- node/src/main/scala/com/wavesplatform/utils/Schedulers.scala
+++ node/src/main/scala/com/wavesplatform/utils/Schedulers.scala
@@ -1,12 +1,16 @@
 package com.wavesplatform.utils
 
-import java.util.concurrent.ThreadPoolExecutor.DiscardOldestPolicy
-import java.util.concurrent.{ScheduledThreadPoolExecutor, ThreadFactory}
+import java.util.concurrent.ThreadPoolExecutor.{DiscardOldestPolicy, DiscardPolicy}
+import java.util.concurrent.{RejectedExecutionHandler, ScheduledThreadPoolExecutor, ThreadFactory}
 
 import monix.execution.UncaughtExceptionReporter.LogExceptionsToStandardErr
 import monix.execution.schedulers.{ExecutorScheduler, SchedulerService}
 import monix.execution.{ExecutionModel, UncaughtExceptionReporter}
 
+/** Helper methods to create schedulers with custom DiscardPolicy
+  * We doesn't use [[monix.execution.schedulers.AdaptedThreadPoolExecutorMixin]]
+  * because we will not submit Runnables to schedulers
+  */
 object Schedulers {
 
   private[this] def threadFactory(name: String, daemonic: Boolean, reporter: UncaughtExceptionReporter): ThreadFactory = { r: Runnable =>
@@ -22,9 +26,10 @@ object Schedulers {
 
   def singleThread(name: String,
                    reporter: UncaughtExceptionReporter = LogExceptionsToStandardErr,
-                   executionModel: ExecutionModel = ExecutionModel.Default): SchedulerService = {
+                   executionModel: ExecutionModel = ExecutionModel.Default,
+                   rejectedExecutionHandler: RejectedExecutionHandler = new DiscardOldestPolicy): SchedulerService = {
     val factory  = threadFactory(name, daemonic = true, reporter)
-    val executor = new ScheduledThreadPoolExecutor(1, factory, new DiscardOldestPolicy)
+    val executor = new ScheduledThreadPoolExecutor(1, factory, rejectedExecutionHandler)
 
     ExecutorScheduler(executor, reporter, executionModel)
   }
@@ -32,9 +37,10 @@ object Schedulers {
   def fixedPool(poolSize: Int,
                 name: String,
                 reporter: UncaughtExceptionReporter = LogExceptionsToStandardErr,
-                executionModel: ExecutionModel = ExecutionModel.Default): SchedulerService = {
+                executionModel: ExecutionModel = ExecutionModel.Default,
+                rejectedExecutionHandler: RejectedExecutionHandler = new DiscardOldestPolicy): SchedulerService = {
     val factory  = threadFactory(name, daemonic = true, reporter)
-    val executor = new ScheduledThreadPoolExecutor(poolSize, factory, new DiscardOldestPolicy)
+    val executor = new ScheduledThreadPoolExecutor(poolSize, factory, rejectedExecutionHandler)
 
     ExecutorScheduler(executor, reporter, executionModel)
   }
