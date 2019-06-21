3ad336fd00d0f9b0c6b70a5611ba349cd4e99703
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2019-06-21 13:20:35 +0300

    DEX-324 Issues after order book deletion (#2387)

--- src/main/scala/com/wavesplatform/matcher/WatchDistributedCompletionActor.scala
+++ src/main/scala/com/wavesplatform/matcher/WatchDistributedCompletionActor.scala
@@ -1,20 +1,29 @@
 package com.wavesplatform.matcher
 
-import akka.actor.{Actor, ActorRef, Props}
+import akka.actor.{Actor, ActorRef, Props, Terminated}
 
 class WatchDistributedCompletionActor(workers: Set[ActorRef], completionReceiver: ActorRef, startWorkCommand: Any, workCompleted: Any) extends Actor {
-  workers.foreach(_ ! startWorkCommand)
+  workers.foreach { x =>
+    context.watch(x)
+    x ! startWorkCommand
+  }
 
   override def receive: Receive = state(workers)
 
   private def state(rest: Set[ActorRef]): Receive = {
     case `workCompleted` =>
-      val updatedRest = rest - sender()
-      if (updatedRest.isEmpty) {
-        completionReceiver ! workCompleted
-        context.stop(self)
-      } else context.become(state(updatedRest))
+      switchTo(rest - sender())
+      context.unwatch(sender())
+
+    case Terminated(ref) =>
+      switchTo(rest - ref)
   }
+
+  private def switchTo(updatedRest: Set[ActorRef]): Unit =
+    if (updatedRest.isEmpty) {
+      completionReceiver ! workCompleted
+      context.stop(self)
+    } else context.become(state(updatedRest))
 }
 
 object WatchDistributedCompletionActor {
