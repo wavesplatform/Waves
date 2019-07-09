ad8b345c087fb2b21e1d113ee384baba1f896567
nmikhaylov <nmikhaylov@wavesplatform.com>
2019-07-09 14:04:57 +0300

    NODE-1775: Key regex filtering: tests

--- node-it/src/test/scala/com/wavesplatform/it/sync/AddressApiSuite.scala
+++ node-it/src/test/scala/com/wavesplatform/it/sync/AddressApiSuite.scala
@@ -1,5 +1,7 @@
 package com.wavesplatform.it.sync
 
+import java.net.URLDecoder
+
 import com.wavesplatform.it.transactions.BaseTransactionSuite
 import com.wavesplatform.state.StringDataEntry
 import com.wavesplatform.it.api.SyncHttpApi._
@@ -9,9 +11,9 @@ import scala.util.Random
 class AddressApiSuite extends BaseTransactionSuite {
 
   test("filter accounts data by regexp") {
-    val dataKeys         = List("1aB1cD!@#$", "\"\\", "\u0000qweqwe", "\t\r\n")
-    val regexps          = List("1aB1cD!@#$", "[a-zA-Z0-9!-/:-@\\\\]{0,15}", "\\s{0,}")
-    val invalidRegexps   = List("[a-z", "[a-z]{0", "[a-z]{,5}")
+    val dataKeys         = List("1aB1cD!@#$", "\"\\", "\u0000qweqwe", "\t\r\n", "reeeee", "rerere", "rerrre", "rre", "eeeee")
+    val regexps          = List("1aB1cD!%40%23%24", "%5Ba-zA-Z0-9!-%2F%3A-%40%5C%5C%5C%5C%5D%7B0%2C15%7D", "%5Cs%7B0%2C%7D", "re%2B", "re*re", "r%3Feeeee")
+    val invalidRegexps   = List("%5Ba-z", "%5Ba-z%5D%7B0", "%5Ba-z%5D%7B%2C5%7D")
     val data             = dataKeys.map(str => StringDataEntry(str, Random.nextString(16)))
     val transferFee      = calcDataFee(data)
     val txId             = sender.putData(firstAddress, data, transferFee).id
@@ -20,7 +22,7 @@ class AddressApiSuite extends BaseTransactionSuite {
     for (regexp <- regexps) {
       val matchedDataKeys = sender.getData(firstAddress, regexp).sortBy(_.key)
 
-      val regexpPattern = regexp.r.pattern
+      val regexpPattern = URLDecoder.decode(regexp, "UTF-8").r.pattern
       withClue(s"regexp: $regexp\n") {
         data.filter(d => regexpPattern.matcher(d.key).matches).sortBy(_.key) shouldEqual matchedDataKeys
       }
