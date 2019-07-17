389fb24fadb8ad64a9302102343e1d4534f9c9cd
Le Karasique <Karasiq@users.noreply.github.com>
2019-07-17 11:42:28 +0300

    NODE-1833: Fix blocks by address (#2447)

--- node/src/main/scala/com/wavesplatform/api/http/BlocksApiRoute.scala
+++ node/src/main/scala/com/wavesplatform/api/http/BlocksApiRoute.scala
@@ -17,7 +17,9 @@ import play.api.libs.json._
 
 @Path("/blocks")
 @Api(value = "/blocks")
-case class BlocksApiRoute(settings: RestAPISettings, blockchain: Blockchain, allChannels: ChannelGroup)(implicit sc: Scheduler) extends ApiRoute with WithSettings {
+case class BlocksApiRoute(settings: RestAPISettings, blockchain: Blockchain, allChannels: ChannelGroup)(implicit sc: Scheduler)
+    extends ApiRoute
+    with WithSettings {
   private[this] val MaxBlocksPerRequest = 100 // todo: make this configurable and fix integration tests
   private[this] val commonApi           = new CommonBlocksApi(blockchain)
 
@@ -39,9 +41,11 @@ case class BlocksApiRoute(settings: RestAPISettings, blockchain: Blockchain, all
       if (end >= 0 && start >= 0 && end - start >= 0 && end - start < MaxBlocksPerRequest) {
         val result = for {
           address <- Address.fromString(address)
-          pairs     = commonApi.blocksRange(start, end).filter(_._1.signerData.generator.address == address)
-          jsonPairs = pairs.map(pair => pair._1.json().addBlockFields(pair._2))
-          result    = jsonPairs.toListL.map(JsArray(_))
+          jsonBlocks = commonApi.blockHeadersRange(start, end).filter(_._1.signerData.generator.toAddress == address).map {
+            case (_, _, h) =>
+              blockchain.blockAt(h).get.json().addBlockFields(h)
+          }
+          result = jsonBlocks.toListL.map(JsArray(_))
         } yield result.runAsync
 
         complete(result)
