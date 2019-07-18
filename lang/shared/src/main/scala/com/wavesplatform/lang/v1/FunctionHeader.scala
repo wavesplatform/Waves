056ee56a448f5181e75e9164c55de0d4d4b2da2f
Artyom Sayadyan <xrtm000@gmail.com>
2019-07-18 14:09:22 +0300

    Restore equals and hashCode sematics

--- lang/shared/src/main/scala/com/wavesplatform/lang/v1/FunctionHeader.scala
+++ lang/shared/src/main/scala/com/wavesplatform/lang/v1/FunctionHeader.scala
@@ -3,7 +3,15 @@ package com.wavesplatform.lang.v1
 sealed abstract class FunctionHeader(val funcName: String)
 object FunctionHeader {
   case class Native(name: Short) extends FunctionHeader(name.toString)
-  case class User(internalName: String, name: String)  extends FunctionHeader(internalName)
+
+  case class User(internalName: String, name: String) extends FunctionHeader(internalName) {
+    override def hashCode(): Int = internalName.##
+    override def equals(obj: Any): Boolean =
+      obj match {
+        case User(`internalName`, _) => true
+        case _                       => false
+      }
+  }
   object User {
     def apply(internalName: String): User = User(internalName, internalName)
   }
