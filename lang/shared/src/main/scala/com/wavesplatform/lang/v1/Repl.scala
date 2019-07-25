06c7ea08dd5b42c77c26647409a73254be351708
Artyom Sayadyan <xrtm000@gmail.com>
2019-07-25 18:04:11 +0300

    More convenient parsing

--- lang/shared/src/main/scala/com/wavesplatform/lang/v1/Repl.scala
+++ lang/shared/src/main/scala/com/wavesplatform/lang/v1/Repl.scala
@@ -1,54 +1,37 @@
 package com.wavesplatform.lang.v1
 
-import com.wavesplatform.lang.ExecutionError
 import com.wavesplatform.lang.directives.values.{StdLibVersion, V3}
-import com.wavesplatform.lang.v1.compiler.{CompilerContext, ExpressionCompiler, Terms, Types}
+import com.wavesplatform.lang.v1.compiler.{ExpressionCompiler}
 import com.wavesplatform.lang.v1.evaluator.EvaluatorV1
-import com.wavesplatform.lang.v1.evaluator.ctx.EvaluationContext
 import com.wavesplatform.lang.v1.evaluator.ctx.impl.{CryptoContext, PureContext}
-import com.wavesplatform.lang.v1.parser.Expressions.PART.VALID
-import com.wavesplatform.lang.v1.parser.Expressions.{BLOCK, EXPR, INVALID, REF}
 import com.wavesplatform.lang.v1.parser.Parser
 import monix.execution.atomic.Atomic
 import cats.implicits._
 import com.wavesplatform.lang.v1.compiler.Terms.EVALUATED
+import com.wavesplatform.lang.v1.compiler.Types.{FINAL, UNIT}
 
 case class Repl(ver: StdLibVersion = V3) {
   private val Global: BaseGlobal = com.wavesplatform.lang.Global
-  private val initialCtx         = CryptoContext.build(Global, ver) |+| PureContext.build(Global, ver)
-  private val scriptAcc          = Atomic("")
+  private val ctx         = CryptoContext.build(Global, ver) |+| PureContext.build(Global, ver)
+  private val scriptAcc   = Atomic("")
 
   def execute(expr: String): Either[String, String] =
     scriptAcc.transformAndExtract(acc => {
       val newScript = acc + "\n" + expr
       evalExpr(newScript) match {
-        case Left(e)           => (Left(e), acc)
-        case Right((r, false)) => (Right(r), acc)
-        case Right((r, true))  => (Right(r), newScript)
+        case Left(e)          => (Left(e), acc)
+        case Right((r, UNIT)) => (Right(r), newScript)
+        case Right((r, _))    => (Right(r), acc)
       }
     })
 
-  private def evalExpr(expr: String): Either[String, (String, Boolean)] =
+  private def evalExpr(expr: String): Either[String, (String, FINAL)] =
     for {
-      parsed <- Parser.parseExpr(expr).fold(
+      parsed <- Parser.parseExprOrDecl(expr).fold(
         { case (_, _, err) => Left(err.traced.toString) },
-        { case (result, _) => Right(result)             }
+        { case (result, _) => Right(result) }
       )
-      (compiled, isDecl) <- compile(parsed)
-      eval               <- EvaluatorV1[EVALUATED](initialCtx.evaluationContext, compiled)
-    } yield (eval.prettyString(0), isDecl)
-
-  private def compile(parsed: EXPR): Either[String, (Terms.EXPR, Boolean)] = {
-    val (expr, isDecl) = resolveDeclaration(parsed)
-    ExpressionCompiler(initialCtx.compilerContext, expr).map(r => (r._1, isDecl))
-  }
-
-  private def resolveDeclaration(parsed: EXPR): (EXPR, Boolean) =
-    parsed match {
-      case BLOCK(bpos, decl, b: BLOCK)        =>
-        val (expr, isDecl) = resolveDeclaration(b)
-        (BLOCK(bpos, decl, expr), isDecl)
-      case BLOCK(bpos, decl, INVALID(pos, _)) => (BLOCK(bpos, decl, REF(pos, VALID(pos, "unit"))), true)
-      case a                                  => (a, false)
-    }
+      (compiled, cType) <- ExpressionCompiler(ctx.compilerContext, parsed)
+      eval              <- EvaluatorV1[EVALUATED](ctx.evaluationContext, compiled)
+    } yield (eval.prettyString(0), cType)
 }
