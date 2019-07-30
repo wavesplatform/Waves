1d2bc0a7e6e589d8b52ac38195f8b840c8d09f68
Artyom Sayadyan <xrtm000@gmail.com>
2019-07-30 11:07:47 +0300

    SC-365 V3 doc categories (#2443)
    * Categorized functions with template
    
    * Remove links via regex
    
    * Separate V3 doc export
    
    * Multiline support via HJson

--- lang/jvm/src/test/scala/com/wavesplatform/utils/DocExportTest.scala
+++ lang/jvm/src/test/scala/com/wavesplatform/utils/DocExportTest.scala
@@ -1,26 +1,18 @@
 package com.wavesplatform.utils
 
-import com.wavesplatform.common.utils.EitherExt2
-import com.wavesplatform.lang.{Common, ExecutionError, Global}
-import com.wavesplatform.lang.directives.{DirectiveDictionary, DirectiveSet}
-import com.wavesplatform.lang.directives.values.{Account, DApp, Expression, StdLibVersion, V1, V2, V3}
-import com.wavesplatform.lang.v1.evaluator.ctx.impl.waves.WavesContext
-import com.wavesplatform.lang.v1.evaluator.ctx.impl.{CryptoContext, PureContext}
+import com.wavesplatform.lang.directives.DirectiveDictionary
+import com.wavesplatform.lang.directives.values.StdLibVersion
 import org.scalatest.{Matchers, PropSpec}
 import org.scalatestplus.scalacheck.{ScalaCheckPropertyChecks => PropertyChecks}
 import cats.implicits._
 import com.wavesplatform.DocSource
+import com.wavesplatform.utils.doc.RideFullContext
 
 class DocExportTest extends PropSpec with PropertyChecks with Matchers {
   property("declared ride funcs and vars have doc for all versions") {
     DirectiveDictionary[StdLibVersion]
       .all
-      .map(v => {
-        val cryptoCtx = CryptoContext.build(Global, v)
-        val wavesCtx = WavesContext.build(directives(v).explicitGet(), Common.emptyBlockchainEnvironment())
-        val pureCtx = PureContext.build(Global, v)
-        (cryptoCtx |+| wavesCtx |+| pureCtx, v)
-      })
+      .map(ver => (RideFullContext.build(ver), ver))
       .flatMap { case (ctx, ver) =>
         val vars = ctx.vars.keys
           .map(k => DocSource.varData.get((k, ver.value.asInstanceOf[Int])))
@@ -33,13 +25,5 @@ class DocExportTest extends PropSpec with PropertyChecks with Matchers {
       }
       .toList
       .sequence shouldBe defined
-  }
-
-  private def directives(v: StdLibVersion): Either[ExecutionError, DirectiveSet] = {
-    val contentType = v match {
-      case V1 | V2 => Expression
-      case V3      => DApp
-    }
-    DirectiveSet(v, Account, contentType)
   }
 }
--- project/Tasks.scala
+++ project/Tasks.scala
@@ -1,11 +1,15 @@
 import java.io.File
+import java.nio.file.{Files, Paths}
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
+import org.hjson.JsonValue
 import sbt.{Def, IO}
 import sbt._
 import sbt.Keys.{sourceManaged, version}
+
+import scala.collection.JavaConverters._
 
 object Tasks {
   lazy val versionSource = (path: String) => Def.task {
@@ -38,54 +42,113 @@ object Tasks {
     val mapper = new ObjectMapper() with ScalaObjectMapper
     mapper.registerModule(DefaultScalaModule)
 
-    def readDocData(ver: Int): (Map[String, VarSourceData], Map[(String, List[String]), FuncSourceData]) = {
-      val DocSourceData(vars, funcs) = mapper.readValue[DocSourceData](new File(s"lang/doc-data-v$ver.json"))
-      (
-        toMapChecked(vars, (v: VarSourceData) => v.name),
-        toMapChecked(funcs, (f: FuncSourceData) => (f.name, f.params))
-      )
-    }
-
-    def toMapChecked[K, V](list: List[V], key: V => K): Map[K, V] =
-      list
+    def toMapChecked[K, V](data: Seq[V], key: V => K): Map[K, V] =
+      data
         .distinct
         .groupBy(key)
         .ensuring(_.forall { case (_, v) => if (v.size == 1) true else { println(v); false } }, "Duplicate detected")
         .mapValues(_.head)
 
-    def listStr(l: List[String]) = l.map("\"" + _ + "\"").mkString("List(", ", ", ")")
+    def str(s: String): String = "\"" + s + "\""
 
-    val (varData, funcData) =
-      Seq(1, 2, 3)
+    def tupleStr(l: Seq[String]): String = l.mkString("(", ", ", ")")
+
+    def listStr(l: Seq[String]): String = "List" + tupleStr(l)
+
+    def mapStr(kv: Map[Seq[String], Seq[String]]): String = {
+      val inner = kv
+        .map { case (k, v) => Seq(tupleStr(k), tupleStr(v)) }
+        .map(tupleStr)
+      "Map" + tupleStr(inner.toSeq)
+    }
+    
+    def sumMapStr(m1: String, m2: String): String = s"$m1 ++ $m2"
+    
+    def kvStr[K, V](
+      seq:      Seq[V],
+      key:      V => K,
+      keyStr:   V => Seq[String],
+      valueStr: V => Seq[String]
+    ): String =
+      mapStr(
+        toMapChecked(seq, key).map { case (_, v) => (keyStr(v), valueStr(v)) }
+      )
+
+    def buildVarsStr(vars: Seq[VarSourceData], ver: String): String =
+      kvStr[String, VarSourceData](
+        vars,
+        _.name,
+        v => Seq(str(v.name), ver),
+        v => Seq(str(v.doc))
+      )
+    
+    def buildFuncsStr(funcs: Seq[FuncSourceData], ver: String): String =
+      kvStr[(String, List[String]), FuncSourceData](
+        funcs,
+        f => (f.name, f.params),
+        f => Seq(str(f.name), listStr(f.params.map(str)), ver),
+        f => Seq(str(f.doc), listStr(f.paramsDoc.map(str)))
+      )
+
+    def readV1V2Data(): (String, String) =
+      Seq("1", "2")
         .map { ver =>
-          val (varData, funcData) = readDocData(ver)
-
-          val varDataStr = varData
-            .map { case (k, v) => s"""	(("$k", $ver), "${v.doc}")""" }
-            .mkString("Map(\n", ",\n", "\n)")
-
-          val funcDataStr = funcData
-            .map { case (k, v) => s"""	(("${v.name}", ${listStr(v.params)}, $ver), ("${v.doc}", ${listStr(v.paramsDoc)}))""" }
-            .mkString("Map(\n", ",\n", "\n)")
-
+          val DocSourceData(vars, funcs) = mapper.readValue[DocSourceData](new File(s"lang/doc/v$ver/data.json"))
+          val varDataStr = buildVarsStr(vars, ver)
+          val funcDataStr = buildFuncsStr(funcs, ver)
           (varDataStr, funcDataStr)
         }
         .reduce { (a, b) =>
           val (v1, f1) = a
           val (v2, f2) = b
           (
-            v1 + " ++ " + v2,
-            f1 + " ++ " + f2,
+            sumMapStr(v1, v2),
+            sumMapStr(f1, f2),
           )
         }
+
+    def buildCategorizedFuncsStr(funcs: Seq[(FuncSourceData, String)], ver: String): String =
+      kvStr[(String, List[String]), (FuncSourceData, String)](
+        funcs,
+        f => (f._1.name, f._1.params),
+        f => Seq(str(f._1.name), listStr(f._1.params.map(str)), ver),
+        f => Seq(
+          str(f._1.doc.replace("\n", "\\n")),
+          listStr(f._1.paramsDoc.map(str).map(_.replace("\n", "\\n"))),
+          str(f._2)
+        )
+      )
+
+    def readV3Data(): (String, String) = {
+      val ver = "3"
+      val funcs = for {
+        path  <- Files.list(Paths.get("lang/doc/v3/funcs")).iterator.asScala
+        json  = JsonValue.readHjson(Files.newBufferedReader(path)).asObject().toString
+        funcs <- mapper.readValue[Map[String, List[FuncSourceData]]](json).head._2
+        category = path.getName(path.getNameCount - 1).toString.split('.').head
+      } yield (funcs, category)
+
+      val funcsStr = buildCategorizedFuncsStr(funcs.toSeq, ver)
+
+      val vars = mapper.readValue[Map[String, List[VarSourceData]]](new File("lang/doc/v3/vars.json")).head._2
+      val varsStr = buildVarsStr(vars, ver)
+
+      (varsStr, funcsStr)
+    }
+
+    val (vars, funcs) = readV1V2Data()
+    val (varsV3, funcsV3) = readV3Data()
 
     val sourceStr =
       s"""
          | package com.wavesplatform
          |
          | object DocSource {
-         |   val varData  = $varData
-         |   val funcData = $funcData
+         |   private val regex = "\\\\[(.+?)\\\\]\\\\(.+?\\\\)".r
+         |
+         |   lazy val varData  = $vars ++ $varsV3
+         |   lazy val funcData = $funcs ++ categorizedfuncData.mapValues(v => (regex.replaceAllIn(v._1, _.group(1)), v._2))
+         |   lazy val categorizedfuncData = $funcsV3
          | }
       """.stripMargin
 
