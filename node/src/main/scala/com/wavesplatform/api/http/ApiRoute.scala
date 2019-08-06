69e58b09d033d5db77564b755e25646befeb2cbb
Anvar Kiekbaev <a.kiekbaev@chepiov.org>
2019-08-06 12:01:36 +0300

    NODE-1726: API uncaught exceptions handler (#2484)
    * NODE-1726: Uncaught exceptions handler
    
    * NODE-1726: Test for exception handler
    
    * NODE-1726: Fixing test
    
    * NODE-1726: Renaming directive

--- node/src/main/scala/com/wavesplatform/api/http/ApiRoute.scala
+++ node/src/main/scala/com/wavesplatform/api/http/ApiRoute.scala
@@ -11,13 +11,19 @@ import com.wavesplatform.crypto
 import com.wavesplatform.http.{ApiMarshallers, PlayJsonException, `X-Api-Key`, api_key}
 import com.wavesplatform.settings.RestAPISettings
 import com.wavesplatform.transaction.TxValidationError.GenericError
+import com.wavesplatform.utils.ScorexLogging
 import monix.execution.Scheduler
 import play.api.libs.json.{JsResultException, Reads}
 
-trait ApiRoute extends Directives with CommonApiFunctions with ApiMarshallers {
-  val route: Route
+import scala.concurrent.Future
+import scala.util.control.NonFatal
+
+trait ApiRoute extends Directives with CommonApiFunctions with ApiMarshallers with ScorexLogging {
+  protected val route: Route
 
   protected def apiKeyHash: Option[Array[Byte]]
+
+  def getRoute: Route = handleAllExceptions(uncaughtExceptionHandler)(route)
 
   private val jsonRejectionHandler = RejectionHandler
     .newBuilder()
@@ -35,6 +41,36 @@ trait ApiRoute extends Directives with CommonApiFunctions with ApiMarshallers {
     case e: AssertionError                                              => complete(ApiError.fromValidationError(GenericError(e)))
     case e: ExecutionException if e.getCause != null && e.getCause != e => jsonExceptionHandler(e.getCause)
   }
+
+  private val uncaughtExceptionHandler: ExceptionHandler = ExceptionHandler {
+    case e: StackOverflowError => log.error("Stack overflow error", e); complete(ApiError.Unknown)
+    case NonFatal(e)           => log.error("Uncaught error", e); complete(ApiError.Unknown)
+  }
+
+  /** Handles all [[scala.util.control.NonFatal non-fatal]] exceptions and tries to handle fatal errors.
+    *
+    * This directive can't handle __fatal__ errors from:
+    *
+    *   - Monix [[monix.eval.Task tasks]] with async boundaries:
+    *     {{{
+    *       get(complete(Task(throw new StackOverflowError()).executeAsync.runToFuture))
+    *       get(complete(Task.evalAsync(throw new StackOverflowError()).runToFuture))
+    *       get(complete(Task.deferFuture(Future(throw new StackOverflowError())).runToFuture))
+    *     }}}
+    *   - Async futures (i.e. which are not available at the time of handling):
+    *     {{{
+    *       get(complete(Future(throw new StackOverflowException())))
+    *     }}}
+    */
+  private def handleAllExceptions(handler: ExceptionHandler): Directive0 =
+    Directive { inner =>
+      ctx =>
+        val handleExceptions = handler.andThen(_ (ctx))
+        try inner(())(ctx).recoverWith(handleExceptions)(ctx.executionContext)
+        catch {
+          case thr: Throwable => handler.andThen(_ (ctx)).applyOrElse[Throwable, Future[RouteResult]](thr, throw _)
+        }
+    }
 
   def withAuth: Directive0 = apiKeyHash.fold[Directive0](complete(ApiKeyNotValid)) { hashFromSettings =>
     optionalHeaderValueByType[`X-Api-Key`](()).flatMap {
