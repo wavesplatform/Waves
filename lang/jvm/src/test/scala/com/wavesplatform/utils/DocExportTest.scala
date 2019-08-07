e4fc935f0ccdabc3794bf24fa0ee452663544461
Artyom Sayadyan <xrtm000@gmail.com>
2019-08-07 16:50:53 +0300

    SC-425 JsAPI docs error

--- lang/jvm/src/test/scala/com/wavesplatform/utils/DocExportTest.scala
+++ lang/jvm/src/test/scala/com/wavesplatform/utils/DocExportTest.scala
@@ -1,29 +1,39 @@
 package com.wavesplatform.utils
 
-import com.wavesplatform.lang.directives.DirectiveDictionary
-import com.wavesplatform.lang.directives.values.StdLibVersion
+import com.wavesplatform.common.utils.EitherExt2
+import com.wavesplatform.lang.directives.{DirectiveDictionary, DirectiveSet}
+import com.wavesplatform.lang.directives.values.{Account, ContentType, DApp, Expression, StdLibVersion}
 import org.scalatest.{Matchers, PropSpec}
 import org.scalatestplus.scalacheck.{ScalaCheckPropertyChecks => PropertyChecks}
 import cats.implicits._
 import com.wavesplatform.DocSource
+import com.wavesplatform.lang.v1.CTX
 import com.wavesplatform.utils.doc.RideFullContext
 
 class DocExportTest extends PropSpec with PropertyChecks with Matchers {
-  property("declared ride funcs and vars have doc for all versions") {
-    DirectiveDictionary[StdLibVersion]
-      .all
-      .map(ver => (RideFullContext.build(ver), ver))
-      .flatMap { case (ctx, ver) =>
-        val vars = ctx.vars.keys
-          .map(k => DocSource.varData.get((k, ver.value.asInstanceOf[Int])))
+  property("declared ride funcs and vars have doc for all contexts") {
+    val totalDocs = for {
+      ds <- directives
+      ctx = RideFullContext.build(ds)
+      doc <- varsDoc(ctx, ds.stdLibVersion) ++ funcDoc(ctx, ds.stdLibVersion)
+    } yield doc
 
-        val funcs = ctx.functions
-          .map(f => (f.name, f.signature.args.map(_._2.toString).toList))
-          .map(k => DocSource.funcData.get((k._1, k._2, ver.value.asInstanceOf[Int])))
-
-        vars ++ funcs
-      }
-      .toList
-      .sequence shouldBe defined
+    totalDocs.toList.sequence shouldBe defined
   }
+
+  lazy val directives: Seq[DirectiveSet] =
+    DirectiveSet.contractDirectiveSet +:
+      DirectiveDictionary[StdLibVersion].all
+        .map(DirectiveSet(_, Account, Expression).explicitGet())
+        .toSeq
+
+
+  def varsDoc(ctx: CTX, ver: StdLibVersion): Iterable[Option[String]] =
+    ctx.vars.keys
+      .map(k => DocSource.varData.get((k, ver.value.asInstanceOf[Int])))
+
+  def funcDoc(ctx: CTX, ver: StdLibVersion): Array[Option[(String, List[String])]] =
+    ctx.functions
+      .map(f => (f.name, f.signature.args.map(_._2.toString).toList))
+      .map(k => DocSource.funcData.get((k._1, k._2, ver.value.asInstanceOf[Int])))
 }
