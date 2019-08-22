a5abf278e6eb504c02f03602d68d2dc6d4626772
Valeriy Gavrilin <yoba123@yandex.ru>
2019-08-22 15:56:16 +0300

    PB external schemas updates

--- node/src/main/scala/com/wavesplatform/protobuf/transaction/PBAmounts.scala
+++ node/src/main/scala/com/wavesplatform/protobuf/transaction/PBAmounts.scala
@@ -1,26 +1,26 @@
 package com.wavesplatform.protobuf.transaction
 import com.google.protobuf.ByteString
+import com.wavesplatform.protobuf.Amount
 import com.wavesplatform.transaction.Asset
 import com.wavesplatform.transaction.Asset.{IssuedAsset, Waves}
 
 object PBAmounts {
-  def toPBAssetId(asset: Asset): AssetId = asset match {
+  def toPBAssetId(asset: Asset): ByteString = asset match {
     case Asset.IssuedAsset(id) =>
-      AssetId().withIssuedAsset(ByteString.copyFrom(id))
+      ByteString.copyFrom(id)
 
     case Asset.Waves =>
-      AssetId().withWaves(com.google.protobuf.empty.Empty())
+      ByteString.EMPTY
   }
 
-  def toVanillaAssetId(assetId: AssetId): Asset = assetId.asset match {
-    case AssetId.Asset.Waves(_)             => Waves
-    case AssetId.Asset.IssuedAsset(assetId) => IssuedAsset(assetId.toByteArray)
-    case _ => throw new IllegalArgumentException
+  def toVanillaAssetId(byteStr: ByteString): Asset = {
+    if (byteStr.isEmpty) Waves
+    else IssuedAsset(byteStr.toByteArray)
   }
 
   def fromAssetAndAmount(asset: Asset, amount: Long): Amount =
-    Amount(Some(toPBAssetId(asset)), amount)
+    Amount(toPBAssetId(asset), amount)
 
   def toAssetAndAmount(value: Amount): (Asset, Long) =
-    (toVanillaAssetId(value.getAssetId), value.amount)
+    (toVanillaAssetId(value.assetId), value.amount)
 }
