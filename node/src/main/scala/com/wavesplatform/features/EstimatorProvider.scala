29a74a5b541c280fd37f4c2c10af5c6d23fe11d7
Artyom Sayadyan <xrtm000@gmail.com>
2019-08-26 17:04:37 +0300

    Correct fork-param name at Estimator Provider

--- node/src/main/scala/com/wavesplatform/features/EstimatorProvider.scala
+++ node/src/main/scala/com/wavesplatform/features/EstimatorProvider.scala
@@ -1,7 +1,7 @@
 package com.wavesplatform.features
 
 import com.wavesplatform.state.Blockchain
-import BlockchainFeatures.ChangeMinimalFees
+import BlockchainFeatures.ChangeMinimumFees
 import FeatureProvider._
 import com.wavesplatform.lang.v1.estimator.{ScriptEstimator, ScriptEstimatorV1}
 import com.wavesplatform.lang.v2.estimator.ScriptEstimatorV2
@@ -10,13 +10,13 @@ import com.wavesplatform.settings.WavesSettings
 object EstimatorProvider {
   implicit class EstimatorBlockchainExt(b: Blockchain) {
     val estimator: ScriptEstimator =
-      if (b.isFeatureActivated(ChangeMinimalFees)) ScriptEstimatorV2
+      if (b.isFeatureActivated(ChangeMinimumFees)) ScriptEstimatorV2
       else ScriptEstimatorV1
   }
 
   implicit class EstimatorWavesSettingsExt(ws: WavesSettings) {
     val estimator: ScriptEstimator =
-      if (ws.featuresSettings.supported.contains(ChangeMinimalFees.id)) ScriptEstimatorV2
+      if (ws.featuresSettings.supported.contains(ChangeMinimumFees.id)) ScriptEstimatorV2
       else ScriptEstimatorV1
   }
 }
