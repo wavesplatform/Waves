3bc7488ce39059c7f5c19fce5d071297c7824d2e
Valeriy Gavrilin <yoba123@yandex.ru>
2019-09-02 14:44:15 +0300

    NODE-1872: Add error logging to PB serialization

--- node/src/main/scala/com/wavesplatform/protobuf/utils/PBUtils.scala
+++ node/src/main/scala/com/wavesplatform/protobuf/utils/PBUtils.scala
@@ -1,12 +1,11 @@
 package com.wavesplatform.protobuf.utils
 import com.google.protobuf.CodedOutputStream
 import com.wavesplatform.common.state.ByteStr
-import com.wavesplatform.utils.ScorexLogging
 import scalapb.GeneratedMessage
 
 import scala.util.control.NonFatal
 
-object PBUtils extends ScorexLogging {
+object PBUtils {
   def encodeDeterministic(msg: GeneratedMessage): Array[Byte] = {
     val outArray     = new Array[Byte](msg.serializedSize)
     val outputStream = CodedOutputStream.newInstance(outArray)
@@ -17,8 +16,7 @@ object PBUtils extends ScorexLogging {
     try outputStream.checkNoSpaceLeft()
     catch {
       case NonFatal(e) =>
-        log.error(s"Error serializing PB message: $msg (bytes = ${ByteStr(msg.toByteArray)})", e)
-        throw e
+        throw new RuntimeException(s"Error serializing PB message: $msg (bytes = ${ByteStr(msg.toByteArray)})", e)
     }
 
     outArray
