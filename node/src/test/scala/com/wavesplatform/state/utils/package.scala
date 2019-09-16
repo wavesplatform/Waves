9b2fd38be11e351dbf8e23bca46606b32fda821c
Le Karasique <Karasiq@users.noreply.github.com>
2019-09-16 15:33:37 +0300

    WithDomain fixes (#2592)

--- node/src/test/scala/com/wavesplatform/state/utils/package.scala
+++ node/src/test/scala/com/wavesplatform/state/utils/package.scala
@@ -2,11 +2,33 @@ package com.wavesplatform.state
 
 import com.wavesplatform.account.Address
 import com.wavesplatform.common.state.ByteStr
-import com.wavesplatform.transaction.{Transaction, TransactionParsers}
+import com.wavesplatform.database.LevelDBWriter
+import com.wavesplatform.settings.{BlockchainSettings, DBSettings, FunctionalitySettings, GenesisSettings, RewardsSettings}
+import com.wavesplatform.transaction.{Asset, Transaction, TransactionParsers}
+import monix.reactive.Observer
+import org.iq80.leveldb.DB
 
 import scala.concurrent.duration.Duration
 
 package object utils {
+  object TestLevelDB {
+    def withFunctionalitySettings(
+        writableDB: DB,
+        spendableBalanceChanged: Observer[(Address, Asset)],
+        fs: FunctionalitySettings,
+        dbSettings: DBSettings
+    ): LevelDBWriter =
+      new LevelDBWriter(
+        writableDB,
+        spendableBalanceChanged,
+        createTestBlockchainSettings(fs),
+        dbSettings
+      )
+
+    def createTestBlockchainSettings(fs: FunctionalitySettings): BlockchainSettings =
+      BlockchainSettings('T', fs, GenesisSettings.TESTNET, RewardsSettings.TESTNET)
+  }
+
   implicit class BlockchainAddressTransactionsList(b: Blockchain) {
     def addressTransactions(address: Address,
                             types: Set[Transaction.Type],
