31cbd22565a3ed98f9584dd27fd0ab996ab7881c
Valeriy Gavrilin <yoba123@yandex.ru>
2019-09-19 17:34:12 +0300

    Fixes

--- node/src/main/scala/com/wavesplatform/network/RxExtensionLoader.scala
+++ node/src/main/scala/com/wavesplatform/network/RxExtensionLoader.scala
@@ -73,7 +73,6 @@ object RxExtensionLoader extends ScorexLogging {
                   )
 
                   val blacklisting = scheduleBlacklist(ch, s"Timeout loading extension").runAsyncLogErr
-                  ch.close()
                   ch.writeAndFlush(GetSignatures(knownSigs)).addListener { f: ChannelFuture =>
                     if (!f.isSuccess) log.error(s"Error requesting signatures: $ch", f.cause())
                   }
