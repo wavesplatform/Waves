26b838c6c1668da0205ab3b2c2c9cc02524a508f
Artyom Sayadyan <xrtm000@gmail.com>
2019-10-16 15:39:09 +0300

    Update Estimator provider

--- node/src/main/scala/com/wavesplatform/features/EstimatorProvider.scala
+++ node/src/main/scala/com/wavesplatform/features/EstimatorProvider.scala
@@ -1,22 +1,24 @@
 package com.wavesplatform.features
 
-import com.wavesplatform.state.Blockchain
-import BlockchainFeatures.BlockReward
-import FeatureProvider._
+import com.wavesplatform.features.BlockchainFeatures.{BlockReward, MultiPaymentInvokeScript}
+import com.wavesplatform.features.FeatureProvider._
 import com.wavesplatform.lang.v1.estimator.{ScriptEstimator, ScriptEstimatorV1}
-import com.wavesplatform.lang.v2.estimator.ScriptEstimatorV2
+import com.wavesplatform.lang.v2.estimator.{ScriptEstimatorV2, ScriptEstimatorV3}
 import com.wavesplatform.settings.WavesSettings
+import com.wavesplatform.state.Blockchain
 
 object EstimatorProvider {
   implicit class EstimatorBlockchainExt(b: Blockchain) {
     val estimator: ScriptEstimator =
-      if (b.isFeatureActivated(BlockReward)) ScriptEstimatorV2
+      if (b.isFeatureActivated(MultiPaymentInvokeScript)) ScriptEstimatorV3
+      else if (b.isFeatureActivated(BlockReward)) ScriptEstimatorV2
       else ScriptEstimatorV1
   }
 
   implicit class EstimatorWavesSettingsExt(ws: WavesSettings) {
     val estimator: ScriptEstimator =
-      if (ws.featuresSettings.supported.contains(BlockReward.id)) ScriptEstimatorV2
+      if (ws.featuresSettings.supported.contains(MultiPaymentInvokeScript.id)) ScriptEstimatorV3
+      else if (ws.featuresSettings.supported.contains(BlockReward.id)) ScriptEstimatorV2
       else ScriptEstimatorV1
   }
 }
