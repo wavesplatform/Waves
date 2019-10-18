b5f2a568906b8050f708d5519269265dd14c8e25
Valeriy Gavrilin <yoba123@yandex.ru>
2019-10-18 18:21:20 +0300

    NFT list unit test

--- node/src/test/scala/com/wavesplatform/BlocksTransactionsHelpers.scala
+++ node/src/test/scala/com/wavesplatform/BlocksTransactionsHelpers.scala
@@ -6,7 +6,8 @@ import com.wavesplatform.common.utils._
 import com.wavesplatform.consensus.nxt.NxtLikeConsensusBlockData
 import com.wavesplatform.history.DefaultBaseTarget
 import com.wavesplatform.state.StringDataEntry
-import com.wavesplatform.transaction.Asset.Waves
+import com.wavesplatform.transaction.Asset.{IssuedAsset, Waves}
+import com.wavesplatform.transaction.assets.{IssueTransaction, IssueTransactionV1}
 import com.wavesplatform.transaction.lease.{LeaseCancelTransactionV1, LeaseTransactionV1}
 import com.wavesplatform.transaction.transfer.{TransferTransactionV1, TransferTransactionV2}
 import com.wavesplatform.transaction.{DataTransaction, Transaction}
@@ -16,26 +17,43 @@ trait BlocksTransactionsHelpers { self: TransactionGen =>
   object QuickTX {
     val FeeAmount = 400000
 
-    def transfer(from: KeyPair,
-                 to: AddressOrAlias = accountGen.sample.get,
-                 amount: Long = smallFeeGen.sample.get,
-                 timestamp: Gen[Long] = timestampGen): Gen[Transaction] =
+    def transfer(
+        from: KeyPair,
+        to: AddressOrAlias = accountGen.sample.get,
+        amount: Long = smallFeeGen.sample.get,
+        timestamp: Gen[Long] = timestampGen
+    ): Gen[Transaction] =
       for {
         timestamp <- timestamp
       } yield TransferTransactionV1.selfSigned(Waves, from, to, amount, timestamp, Waves, FeeAmount, Array.empty).explicitGet()
 
-    def transferV2(from: KeyPair,
-                   to: AddressOrAlias = accountGen.sample.get,
-                   amount: Long = smallFeeGen.sample.get,
-                   timestamp: Gen[Long] = timestampGen): Gen[Transaction] =
+    def transferV2(
+        from: KeyPair,
+        to: AddressOrAlias = accountGen.sample.get,
+        amount: Long = smallFeeGen.sample.get,
+        timestamp: Gen[Long] = timestampGen
+    ): Gen[Transaction] =
       for {
         timestamp <- timestamp
       } yield TransferTransactionV2.selfSigned(Waves, from, to, amount, timestamp, Waves, FeeAmount, Array.empty).explicitGet()
 
-    def lease(from: KeyPair,
-              to: AddressOrAlias = accountGen.sample.get,
-              amount: Long = smallFeeGen.sample.get,
-              timestamp: Gen[Long] = timestampGen): Gen[LeaseTransactionV1] =
+    def transferAsset(
+        asset: IssuedAsset,
+        from: KeyPair,
+        to: AddressOrAlias = accountGen.sample.get,
+        amount: Long = smallFeeGen.sample.get,
+        timestamp: Gen[Long] = timestampGen
+    ): Gen[Transaction] =
+      for {
+        timestamp <- timestamp
+      } yield TransferTransactionV1.selfSigned(asset, from, to, amount, timestamp, Waves, FeeAmount, Array.empty).explicitGet()
+
+    def lease(
+        from: KeyPair,
+        to: AddressOrAlias = accountGen.sample.get,
+        amount: Long = smallFeeGen.sample.get,
+        timestamp: Gen[Long] = timestampGen
+    ): Gen[LeaseTransactionV1] =
       for {
         timestamp <- timestamp
       } yield LeaseTransactionV1.selfSigned(from, amount, FeeAmount, timestamp, to).explicitGet()
@@ -49,15 +67,22 @@ trait BlocksTransactionsHelpers { self: TransactionGen =>
       for {
         timestamp <- timestamp
       } yield DataTransaction.selfSigned(from, List(StringDataEntry(dataKey, Gen.numStr.sample.get)), FeeAmount, timestamp).explicitGet()
+
+    def nftIssue(from: KeyPair, timestamp: Gen[Long] = timestampGen): Gen[IssueTransaction] =
+      for {
+        timestamp <- timestamp
+      } yield IssueTransactionV1.selfSigned(from, "test".getBytes(), "".getBytes(), 1, 0, reissuable = false, 100000000L, timestamp).explicitGet()
   }
 
   object UnsafeBlocks {
-    def unsafeChainBaseAndMicro(totalRefTo: ByteStr,
-                                base: Seq[Transaction],
-                                micros: Seq[Seq[Transaction]],
-                                signer: KeyPair,
-                                version: Byte,
-                                timestamp: Long): (Block, Seq[MicroBlock]) = {
+    def unsafeChainBaseAndMicro(
+        totalRefTo: ByteStr,
+        base: Seq[Transaction],
+        micros: Seq[Seq[Transaction]],
+        signer: KeyPair,
+        version: Byte,
+        timestamp: Long
+    ): (Block, Seq[MicroBlock]) = {
       val block = unsafeBlock(totalRefTo, base, signer, version, timestamp)
       val microBlocks = micros
         .foldLeft((block, Seq.empty[MicroBlock])) {
@@ -69,12 +94,7 @@ trait BlocksTransactionsHelpers { self: TransactionGen =>
       (block, microBlocks)
     }
 
-    def unsafeMicro(totalRefTo: ByteStr,
-                    prevTotal: Block,
-                    txs: Seq[Transaction],
-                    signer: KeyPair,
-                    version: Byte,
-                    ts: Long): (Block, MicroBlock) = {
+    def unsafeMicro(totalRefTo: ByteStr, prevTotal: Block, txs: Seq[Transaction], signer: KeyPair, version: Byte, ts: Long): (Block, MicroBlock) = {
       val newTotalBlock = unsafeBlock(totalRefTo, prevTotal.transactionData ++ txs, signer, version, ts)
       val unsigned      = new MicroBlock(version, signer, txs, prevTotal.uniqueId, newTotalBlock.uniqueId, ByteStr.empty)
       val signature     = crypto.sign(signer, unsigned.bytes())
@@ -82,12 +102,14 @@ trait BlocksTransactionsHelpers { self: TransactionGen =>
       (newTotalBlock, signed)
     }
 
-    def unsafeBlock(reference: ByteStr,
-                    txs: Seq[Transaction],
-                    signer: KeyPair,
-                    version: Byte,
-                    timestamp: Long,
-                    bTarget: Long = DefaultBaseTarget): Block = {
+    def unsafeBlock(
+        reference: ByteStr,
+        txs: Seq[Transaction],
+        signer: KeyPair,
+        version: Byte,
+        timestamp: Long,
+        bTarget: Long = DefaultBaseTarget
+    ): Block = {
       val unsigned = Block(
         version = version,
         timestamp = timestamp,
