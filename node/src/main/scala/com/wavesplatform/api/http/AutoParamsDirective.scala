2dd8fac691ec8016437fd60971e08a1e154feae9
Valeriy Gavrilin <yoba123@yandex.ru>
2019-10-21 13:51:05 +0300

    Fixes

--- node/src/main/scala/com/wavesplatform/api/http/AutoParamsDirective.scala
+++ node/src/main/scala/com/wavesplatform/api/http/AutoParamsDirective.scala
@@ -3,6 +3,8 @@ package com.wavesplatform.api.http
 import akka.http.scaladsl.server.{Directive1, MissingQueryParamRejection}
 import play.api.libs.json.{JsArray, Json}
 
+import scala.util.Try
+
 trait AutoParamsDirective { self: ApiRoute =>
   def paramList(name: String): Directive1[Seq[String]] = {
     import scala.concurrent.duration._
@@ -14,7 +16,7 @@ trait AutoParamsDirective { self: ApiRoute =>
     val fromQuery = fromParamSeq(parameterSeq)
     val fromPost  = fromParamSeq(formFieldSeq)
     val fromJson = extractStrictEntity(5 seconds)
-      .map(entity => Json.parse(entity.data.toArray).as[JsArray].value.map(_.as[String]))
+      .map(entity => Try(Json.parse(entity.data.toArray).as[JsArray].value.map(_.as[String])).getOrElse(Nil))
       .recover(_ => provide(Seq.empty[String]))
 
     (for {
