3ae28c884eb527ceefc4574bc1979210135aac36
Valeriy Gavrilin <yoba123@yandex.ru>
2019-10-23 19:11:14 +0300

    Warning fix

--- node-it/src/test/scala/com/wavesplatform/it/sync/transactions/DataTransactionSuite.scala
+++ node-it/src/test/scala/com/wavesplatform/it/sync/transactions/DataTransactionSuite.scala
@@ -120,7 +120,7 @@ class DataTransactionSuite extends BaseTransactionSuite {
     val boolEntry2         = BooleanDataEntry("bool", false)
     val blobEntry2         = BinaryDataEntry("blob", ByteStr(Array[Byte](127.toByte, 0, 1, 1)))
     val stringEntry2       = StringDataEntry("str", "BBBB")
-    val unicodeStringEntry = StringDataEntry("?&$#^123\\/.a:;'\"\r\n\t\0|%è&", "specïal")
+    val unicodeStringEntry = StringDataEntry("?&$#^123\\/.a:;'\"\r\n\t\u0000|%è&", "specïal")
     val dataAllTypes       = List(intEntry2, boolEntry2, blobEntry2, stringEntry2, unicodeStringEntry)
     val fee                = calcDataFee(dataAllTypes)
     val txId               = sender.putData(secondAddress, dataAllTypes, fee).id
@@ -140,7 +140,7 @@ class DataTransactionSuite extends BaseTransactionSuite {
 
   test("queries for multiple keys") {
     val tooBigKey = "toobigkeytoobigkeytoobigkeytoobigkeytoobigkeytoobigkeytoobigkeytoobigkeytoobigkeytoobigkeytoobigkeytoobigkey"
-    val keys   = Seq("int", "bool", "int", "blob", "?&$#^123\\/.a:;'\"\r\n\t\0|%è&", "str", "inexisted_key", tooBigKey)
+    val keys   = Seq("int", "bool", "int", "blob", "?&$#^123\\/.a:;'\"\r\n\t\u0000|%è&", "str", "inexisted_key", tooBigKey)
     val values = Seq[Any](-127, false, -127, ByteStr(Array[Byte](127.toByte, 0, 1, 1)), "specïal","BBBB")
 
     val list     = sender.getDataList(secondAddress, keys: _*).map(_.value)
