5c7e24d21265dde43cab63b2ce38e7123df8242d
Sergey Nazarov <snazarov@web3tech.ru>
2019-11-01 12:13:29 +0300

    Improved locking mechanism (#2696)

--- node/src/main/scala/com/wavesplatform/state/BlockchainUpdaterImpl.scala
+++ node/src/main/scala/com/wavesplatform/state/BlockchainUpdaterImpl.scala
@@ -42,15 +42,12 @@ class BlockchainUpdaterImpl(
   import com.wavesplatform.state.BlockchainUpdaterImpl._
   import wavesSettings.blockchainSettings.functionalitySettings
 
-  private def inLock[R](l: Lock, f: => R) = {
-    try {
-      l.lock()
-      val res = f
-      res
-    } finally {
-      l.unlock()
-    }
+  private def inLock[R](l: Lock, f: => R): R = {
+    l.lockInterruptibly()
+    try f
+    finally l.unlock()
   }
+
   private val lock                     = new ReentrantReadWriteLock
   private def writeLock[B](f: => B): B = inLock(lock.writeLock(), f)
   private def readLock[B](f: => B): B  = inLock(lock.readLock(), f)
@@ -708,7 +705,7 @@ class BlockchainUpdaterImpl(
         .data
         .keySet
 
-      (fromInner ++ fromDiff)
+      fromInner ++ fromDiff
     }
   }
 
