8d28ea251f21e8be88ccea08b8c1955400ee8562
Anvar Kiekbaev <a.kiekbaev@chepiov.org>
2019-11-08 16:56:35 +0300

    NODE-1927: MicroBlock refactoring

--- node/src/main/scala/com/wavesplatform/block/MicroBlock.scala
+++ node/src/main/scala/com/wavesplatform/block/MicroBlock.scala
@@ -5,11 +5,8 @@ import com.wavesplatform.block.Block.BlockId
 import com.wavesplatform.block.serialization.MicroBlockSerializer
 import com.wavesplatform.common.state.ByteStr
 import com.wavesplatform.crypto
-import com.wavesplatform.crypto._
 import com.wavesplatform.lang.ValidationError
-import com.wavesplatform.mining.Miner.MaxTransactionsPerMicroblock
 import com.wavesplatform.state._
-import com.wavesplatform.transaction.TxValidationError.GenericError
 import com.wavesplatform.transaction._
 import com.wavesplatform.utils.ScorexLogging
 import monix.eval.Coeval
@@ -27,9 +24,9 @@ case class MicroBlock(
 
   val bytes: Coeval[Array[Byte]] = Coeval.evalOnce(MicroBlockSerializer.toBytes(this))
 
-  private val bytesToSign: Coeval[Array[Byte]] = Coeval.evalOnce(bytes().dropRight(SignatureLength))
+  private[block] val bytesWithoutSignature: Coeval[Array[Byte]] = Coeval.evalOnce(copy(signature = ByteStr.empty).bytes())
 
-  override val signatureValid: Coeval[Boolean]        = Coeval.evalOnce(crypto.verify(signature.arr, bytesToSign(), sender))
+  override val signatureValid: Coeval[Boolean]        = Coeval.evalOnce(crypto.verify(signature.arr, bytesWithoutSignature(), sender))
   override val signedDescendants: Coeval[Seq[Signed]] = Coeval.evalOnce(transactionData.flatMap(_.cast[Signed]))
 
   override def toString: String = s"MicroBlock(${totalResBlockSig.trim} -> ${prevResBlockSig.trim}, txs=${transactionData.size})"
@@ -43,34 +40,16 @@ object MicroBlock extends ScorexLogging {
       prevResBlockSig: BlockId,
       totalResBlockSig: BlockId
   ): Either[ValidationError, MicroBlock] =
-    // format: off
-    for {
-      _         <- Either.cond(prevResBlockSig.arr.length == SignatureLength, (), GenericError(s"Incorrect prevResBlockSig: ${prevResBlockSig.arr.length}"))
-      _         <- Either.cond(totalResBlockSig.arr.length == SignatureLength, (), GenericError(s"Incorrect totalResBlockSig: ${totalResBlockSig.arr.length}"))
-      _         <- Either.cond(generator.publicKey.length == KeyLength, (), GenericError(s"Incorrect generator.publicKey: ${generator.publicKey.length}"))
-      nonSigned <- validate(MicroBlock(version = 3.toByte, generator, transactionData, prevResBlockSig, totalResBlockSig, ByteStr.empty))
-    } yield {
-      val toSign    = nonSigned.bytes
-      val signature = crypto.sign(generator, toSign())
-      nonSigned.copy(signature = ByteStr(signature))
-    }
-    // format: on
+    MicroBlock(version = 3.toByte, generator, transactionData, prevResBlockSig, totalResBlockSig, ByteStr.empty).validate
+      .map(_.sign(generator))
 
   def parseBytes(bytes: Array[Byte]): Try[MicroBlock] =
     MicroBlockSerializer
       .parseBytes(bytes)
-      .flatMap(validate(_).asTry)
+      .flatMap(_.validateToTry)
       .recoverWith {
         case t: Throwable =>
           log.error("Error when parsing microblock", t)
           Failure(t)
       }
-
-  private def validate(block: MicroBlock): Either[ValidationError, MicroBlock] =
-    if (block.transactionData.isEmpty)
-      Left(GenericError("cannot create empty MicroBlock"))
-    else if (block.transactionData.size > MaxTransactionsPerMicroblock)
-      Left(GenericError(s"too many txs in MicroBlock: allowed: $MaxTransactionsPerMicroblock, actual: ${block.transactionData.size}"))
-    else
-      Right(block)
 }
--- node/src/main/scala/com/wavesplatform/block/package.scala
+++ node/src/main/scala/com/wavesplatform/block/package.scala
@@ -3,7 +3,8 @@ package com.wavesplatform
 import cats.syntax.either._
 import com.wavesplatform.account.PrivateKey
 import com.wavesplatform.block.Block.{GenerationSignatureLength, GenerationVRFSignatureLength, MaxFeaturesInBlock, ProtoBlockVersion}
-import com.wavesplatform.lang.ValidationError
+import com.wavesplatform.crypto.{KeyLength, SignatureLength}
+import com.wavesplatform.mining.Miner.MaxTransactionsPerMicroblock
 import com.wavesplatform.settings.GenesisSettings
 import com.wavesplatform.transaction.GenesisTransaction
 import com.wavesplatform.transaction.TxValidationError.GenericError
@@ -11,19 +12,20 @@ import com.wavesplatform.transaction.TxValidationError.GenericError
 import scala.util.Try
 
 package object block {
+  type Validation[A] = Either[GenericError, A]
 
   // format: off
-  private[block] def validateBlock(b: Block): Either[GenericError, Block] =
+  private[block] def validateBlock(b: Block): Validation[Block] =
     (for {
-      _ <- Either.cond(b.header.reference.arr.length == crypto.SignatureLength, (), "Incorrect reference")
+      _ <- Either.cond(b.header.reference.arr.length == SignatureLength, (), "Incorrect reference")
       genSigLength = if (b.header.version < ProtoBlockVersion) GenerationSignatureLength else GenerationVRFSignatureLength
       _ <- Either.cond(b.header.generationSignature.arr.length == genSigLength, (), "Incorrect generationSignature")
-      _ <- Either.cond(b.header.generator.length == crypto.KeyLength, (), "Incorrect signer")
+      _ <- Either.cond(b.header.generator.length == KeyLength, (), "Incorrect signer")
       _ <- Either.cond(b.header.version > 2 || b.header.featureVotes.isEmpty,(),s"Block version ${b.header.version} could not contain feature votes")
       _ <- Either.cond(b.header.featureVotes.size <= MaxFeaturesInBlock, (), s"Block could not contain more than $MaxFeaturesInBlock feature votes")
     } yield b).leftMap(GenericError(_))
 
-  def validateGenesisBlock(block: Block, genesisSettings: GenesisSettings): Either[GenericError, Block] =
+  def validateGenesisBlock(block: Block, genesisSettings: GenesisSettings): Validation[Block] =
     for {
       // Common validation
       _ <- validateBlock(block)
@@ -33,21 +35,29 @@ package object block {
       txsSum = block.transactionData.collect { case tx: GenesisTransaction => tx.amount }.reduce(Math.addExact(_: Long, _: Long))
       _ <- Either.cond(txsSum == genesisSettings.initialBalance, (), GenericError(s"Initial balance ${genesisSettings.initialBalance} did not match the distributions sum $txsSum"))
     } yield block
+
+  def validateMicroBlock(mb: MicroBlock): Validation[MicroBlock] =
+    (for {
+      _ <- Either.cond(mb.prevResBlockSig.arr.length == SignatureLength, (), s"Incorrect prevResBlockSig: ${mb.prevResBlockSig.arr.length}")
+      _ <- Either.cond(mb.totalResBlockSig.arr.length == SignatureLength, (), s"Incorrect totalResBlockSig: ${mb.totalResBlockSig.arr.length}")
+      _ <- Either.cond(mb.sender.length == KeyLength, (), s"Incorrect generator.publicKey: ${mb.sender.length}")
+      _ <- Either.cond(mb.transactionData.nonEmpty, (), "cannot create empty MicroBlock")
+      _ <- Either.cond(mb.transactionData.size <= MaxTransactionsPerMicroblock, (), s"too many txs in MicroBlock: allowed: $MaxTransactionsPerMicroblock, actual: ${mb.transactionData.size}")
+    } yield mb).leftMap(GenericError(_))
   // format: on
 
-  private[block] implicit class ValidationOps[A](v: Either[ValidationError, A]) {
-    def asTry: Try[A] = v.left.map(ve => new IllegalArgumentException(ve.toString)).toTry
-  }
-
   private[block] implicit class BlockOps(block: Block) {
-
-    def sign(signer: PrivateKey): Block =
-      block.copy(signature = crypto.sign(signer, block.bytesWithoutSignature()))
-
-    def validate: Either[GenericError, Block] = validateBlock(block)
-
-    def validateToTry: Try[Block] = validateBlock(block).leftMap(ge => new IllegalArgumentException(ge.err)).toTry
-
-    def validateGenesis(genesisSettings: GenesisSettings): Either[GenericError, Block] = validateGenesisBlock(block, genesisSettings)
+    def sign(signer: PrivateKey): Block                         = block.copy(signature = crypto.sign(signer, block.bytesWithoutSignature()))
+    def validate: Validation[Block]                             = validateBlock(block)
+    def validateToTry: Try[Block]                               = toTry(validateBlock(block))
+    def validateGenesis(gs: GenesisSettings): Validation[Block] = validateGenesisBlock(block, gs)
   }
+
+  private[block] implicit class MicroBlockOps(microBlock: MicroBlock) {
+    def sign(signer: PrivateKey): MicroBlock = microBlock.copy(signature = crypto.sign(signer, microBlock.bytesWithoutSignature()))
+    def validate: Validation[MicroBlock]     = validateMicroBlock(microBlock)
+    def validateToTry: Try[MicroBlock]       = toTry(validateMicroBlock(microBlock))
+  }
+
+  private def toTry[A](result: Validation[A]): Try[A] = result.leftMap(ge => new IllegalArgumentException(ge.err)).toTry
 }
