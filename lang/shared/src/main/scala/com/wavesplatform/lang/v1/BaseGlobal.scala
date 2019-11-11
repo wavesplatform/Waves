923be8b6b4bafc21ea3a68b5fea608a4cfeab4ff
Ivan Rakitnykh <mrkr.reg@gmail.com>
2019-11-11 13:46:28 +0300

    Sc 490 native median func (#2716)
    * SC-490 Add native `median` function to V4
    
    * SC-490 Add native `median` function to V4
    
    * SC-490 Add native `median` function to V4
    
    * SC-490 Add native `median` function to V4

--- lang/shared/src/main/scala/com/wavesplatform/lang/v1/BaseGlobal.scala
+++ lang/shared/src/main/scala/com/wavesplatform/lang/v1/BaseGlobal.scala
@@ -4,12 +4,12 @@ import java.math.RoundingMode
 
 import cats.implicits._
 import com.wavesplatform.lang.ValidationError.ScriptParseError
-import com.wavesplatform.lang.contract.meta.{Chain, Dic, MetaMapper, MetaMapperStrategyV1, MetaVersion}
+import com.wavesplatform.lang.contract.meta.{Chain, Dic, MetaMapper, MetaMapperStrategyV1}
 import com.wavesplatform.lang.contract.{ContractSerDe, DApp}
 import com.wavesplatform.lang.directives.values.{Expression, StdLibVersion, DApp => DAppType}
 import com.wavesplatform.lang.script.ContractScript.ContractScriptImpl
 import com.wavesplatform.lang.script.{ContractScript, Script}
-import com.wavesplatform.lang.{ValidationError, utils}
+import com.wavesplatform.lang.utils
 import com.wavesplatform.lang.v1.compiler.Terms.EXPR
 import com.wavesplatform.lang.v1.compiler.{CompilerContext, ContractCompiler, ExpressionCompiler, Terms}
 import com.wavesplatform.lang.v1.estimator.ScriptEstimator
