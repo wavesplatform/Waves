7d910c9714ffaa057e89a4675aa646c8331f8869
Valeriy Gavrilin <yoba123@yandex.ru>
2019-11-18 15:02:07 +0300

    NODE-1928: SponsorFee V4 request fixes

--- node/src/main/scala/com/wavesplatform/api/http/requests/SponsorFeeRequest.scala
+++ node/src/main/scala/com/wavesplatform/api/http/requests/SponsorFeeRequest.scala
@@ -14,36 +14,39 @@ object SponsorFeeRequest {
   implicit val signedSponsorRequestFormat   = Json.format[SignedSponsorFeeRequest]
 }
 
-case class SponsorFeeRequest(@ApiModelProperty(value = "Sender address", required = true)
-                             sender: String,
-                             @ApiModelProperty(value = "Asset to be sponsored", required = true)
-                             assetId: String,
-                             @ApiModelProperty(value = "Asset amount per fee unit", required = true)
-                             minSponsoredAssetFee: Option[Long],
-                             @ApiModelProperty(required = true)
-                             fee: Long,
-                             timestamp: Option[Long] = None)
+case class SponsorFeeRequest(
+    @ApiModelProperty(value = "Sender address", required = true)
+    sender: String,
+    @ApiModelProperty(value = "Asset to be sponsored", required = true)
+    assetId: String,
+    @ApiModelProperty(value = "Asset amount per fee unit", required = true)
+    minSponsoredAssetFee: Option[Long],
+    @ApiModelProperty(required = true)
+    fee: Long,
+    timestamp: Option[Long] = None
+)
 
 @ApiModel(value = "Signed Sponsorship Transaction")
-case class SignedSponsorFeeRequest(@ApiModelProperty(value = "Base58 encoded sender public key", required = true)
-                                   senderPublicKey: String,
-                                   @ApiModelProperty(value = "Asset to be sponsored", required = true)
-                                   assetId: String,
-                                   @ApiModelProperty(required = true)
-                                   minSponsoredAssetFee: Option[Long],
-                                   @ApiModelProperty(required = true)
-                                   fee: Long,
-                                   @ApiModelProperty(required = true)
-                                   timestamp: Long,
-                                   @ApiModelProperty(required = true)
-                                   proofs: List[String])
-    {
+case class SignedSponsorFeeRequest(
+    @ApiModelProperty(value = "Base58 encoded sender public key", required = true)
+    senderPublicKey: String,
+    @ApiModelProperty(value = "Asset to be sponsored", required = true)
+    assetId: String,
+    @ApiModelProperty(required = true)
+    minSponsoredAssetFee: Option[Long],
+    @ApiModelProperty(required = true)
+    fee: Long,
+    @ApiModelProperty(required = true)
+    timestamp: Long,
+    @ApiModelProperty(required = true)
+    proofs: List[String]
+) {
   def toTx: Either[ValidationError, SponsorFeeTransaction] =
     for {
       _sender     <- PublicKey.fromBase58String(senderPublicKey)
       _asset      <- parseBase58(assetId, "invalid.assetId", AssetIdStringLength).map(IssuedAsset)
       _proofBytes <- proofs.traverse(s => parseBase58(s, "invalid proof", Proofs.MaxProofStringSize))
       _proofs     <- Proofs.create(_proofBytes)
-      t           <- SponsorFeeTransaction.create(1.toByte, _sender, _asset, minSponsoredAssetFee, fee, timestamp, _proofs)
+      t           <- SponsorFeeTransaction.create(1.toByte, _sender, _asset, minSponsoredAssetFee.filterNot(_ == 0), fee, timestamp, _proofs)
     } yield t
 }
