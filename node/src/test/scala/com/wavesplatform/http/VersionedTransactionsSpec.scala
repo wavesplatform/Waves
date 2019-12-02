631f4570d5105340faaced9c240392f088e083d4
Anvar Kiekbaev <a.kiekbaev@chepiov.org>
2019-12-02 20:25:46 +0300

    NODE-1987: Adds versioning in transaction route test

---
+++ node/src/test/scala/com/wavesplatform/http/VersionedTransactionsSpec.scala
@@ -0,0 +1,11 @@
+package com.wavesplatform.http
+
+import com.wavesplatform.TransactionGen
+
+class VersionedTransactionsSpec extends RouteSpec("/transactions") with TransactionGen {
+  "Versioned transactions handled by route" - {
+    "TransferTransaction" in {
+
+    }
+  }
+}
