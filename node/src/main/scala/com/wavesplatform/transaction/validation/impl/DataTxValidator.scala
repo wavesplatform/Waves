6b272cd0be8ceb56d3fc7e1a409bdbc647e9b5ce
Anvar Kiekbaev <a.kiekbaev@chepiov.org>
2019-12-03 12:28:21 +0300

    NODE-1987: Versioned transactions test (#2771)
    * Adds fixes for protobuf transactions serialization
    * NODE-1987: Adds versioning in transaction route test

--- node/src/main/scala/com/wavesplatform/transaction/validation/impl/DataTxValidator.scala
+++ node/src/main/scala/com/wavesplatform/transaction/validation/impl/DataTxValidator.scala
@@ -1,5 +1,7 @@
 package com.wavesplatform.transaction.validation.impl
 
+import com.wavesplatform.protobuf.transaction.DataTransactionData
+import com.wavesplatform.protobuf.transaction.PBTransactions.toPBDataEntry
 import com.wavesplatform.transaction.DataTransaction.MaxEntryCount
 import com.wavesplatform.transaction.validation.{TxValidator, ValidatedV}
 import com.wavesplatform.transaction.{DataTransaction, TxValidationError}
@@ -9,12 +11,17 @@ import scala.util.Try
 object DataTxValidator extends TxValidator[DataTransaction] {
   override def validate(tx: DataTransaction): ValidatedV[DataTransaction] = {
     import tx._
+
+    lazy val validLength =
+      if (tx.isProtobufVersion) DataTransactionData(data.map(toPBDataEntry)).toByteArray.length <= DataTransaction.MaxBytes
+      else Try(bytes().length <= DataTransaction.MaxBytes).getOrElse(false)
+
     V.seq(tx)(
       V.cond(data.length <= MaxEntryCount && data.forall(_.valid), TxValidationError.TooBigArray),
       V.cond(data.forall(_.key.nonEmpty), TxValidationError.EmptyDataKey),
       V.cond(data.map(_.key) == data.map(_.key).distinct, TxValidationError.DuplicatedDataKeys),
       V.fee(fee),
-      V.cond(Try(bytes().length <= DataTransaction.MaxBytes).getOrElse(false), TxValidationError.TooBigArray)
+      V.cond(validLength, TxValidationError.TooBigArray)
     )
   }
 }
