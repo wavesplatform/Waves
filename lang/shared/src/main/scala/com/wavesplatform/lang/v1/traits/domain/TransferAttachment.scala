19877af8ecd831cd2b47a2982a54979df95fa1e6
Artyom Sayadyan <xrtm000@gmail.com>
2019-12-03 12:57:28 +0300

    SC-512 Tx unit attachment (#2774)
    * SC-512 Tx unit attachment
    
    * Improve naming

--- lang/shared/src/main/scala/com/wavesplatform/lang/v1/traits/domain/TransferAttachment.scala
+++ lang/shared/src/main/scala/com/wavesplatform/lang/v1/traits/domain/TransferAttachment.scala
@@ -3,6 +3,7 @@ package com.wavesplatform.lang.v1.traits.domain
 import com.wavesplatform.common.state.ByteStr
 import com.wavesplatform.common.utils.EitherExt2
 import com.wavesplatform.lang.v1.compiler.Terms._
+import com.wavesplatform.lang.v1.evaluator.ctx.impl
 
 sealed trait TransferAttachment {
   val evaluated: EVALUATED =
@@ -11,9 +12,11 @@ sealed trait TransferAttachment {
       case BooleanValue(value) => CONST_BOOLEAN(value)
       case StringValue(value)  => CONST_STRING(value).explicitGet()
       case ByteStrValue(value) => CONST_BYTESTR(value).explicitGet()
+      case EmptyAttachment     => impl.unit
     }
 }
 case class IntValue(value: Int)         extends TransferAttachment
 case class BooleanValue(value: Boolean) extends TransferAttachment
 case class StringValue(value: String)   extends TransferAttachment
 case class ByteStrValue(value: ByteStr) extends TransferAttachment
+case object EmptyAttachment             extends TransferAttachment
