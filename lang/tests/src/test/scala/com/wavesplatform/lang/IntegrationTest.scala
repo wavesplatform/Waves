f5f1c08d499011fd576fa5077af076ca69cf69a0
Artyom Sayadyan <xrtm000@gmail.com>
2019-12-09 12:33:50 +0300

    SC-513 Zk snarks functions (#2791)
    * Feature/zk snarks functions (#2781)
    
    * add jar
    * add function descriptions
    
    * Correct functions definition
    
    * Correct context definition
    
    * Add checks and tests
    
    * Correct tests and signatures
    
    * Add benchmark and correct js
    
    * Correct gitignore
    
    * update jar
    
    * Add sigVerify benchmark
    
    * Get rid of Pedersen function and imrpove limits
    
    * Clean-up Global classes
    
    * Clean-up function ids

--- lang/tests/src/test/scala/com/wavesplatform/lang/IntegrationTest.scala
+++ lang/tests/src/test/scala/com/wavesplatform/lang/IntegrationTest.scala
@@ -52,6 +52,7 @@ class IntegrationTest extends PropSpec with PropertyChecks with ScriptGen with M
       Monoid.combineAll(
         Seq(
           PureContext.build(Global, version).withEnvironment[C],
+          CryptoContext.build(Global, version).withEnvironment[C],
           addCtx.withEnvironment[C],
           CTX[C](sampleTypes, stringToTuple, Array.empty),
           ctxt
@@ -1257,5 +1258,16 @@ class IntegrationTest extends PropSpec with PropertyChecks with ScriptGen with M
     val src =
       s"""["1", "2"].median()"""
     eval(src, version = V4) should produce("Compilation failed: Non-matching types: expected: Int, actual: String")
+  }
+
+  property("groth16Verify") {
+    val src =
+      s"""
+         | let key = base64'hwk883gUlTKCyXYA6XWZa8H9/xKIYZaJ0xEs0M5hQOMxiGpxocuX/8maSDmeCk3bo5ViaDBdO7ZBxAhLSe5k/5TFQyF5Lv7KN2tLKnwgoWMqB16OL8WdbePIwTCuPtJNAFKoTZylLDbSf02kckMcZQDPF9iGh+JC99Pio74vDpwTEjUx5tQ99gNQwxULtztsqDRsPnEvKvLmsxHt8LQVBkEBm2PBJFY+OXf1MNW021viDBpR10mX4WQ6zrsGL5L0GY4cwf4tlbh+Obit+LnN/SQTnREf8fPpdKZ1sa/ui3pGi8lMT6io4D7Ujlwx2RdCkBF+isfMf77HCEGsZANw0hSrO2FGg14Sl26xLAIohdaW8O7gEaag8JdVAZ3OVLd5Df1NkZBEr753Xb8WwaXsJjE7qxwINL1KdqA4+EiYW4edb7+a9bbBeOPtb67ZxmFqgyTNS/4obxahezNkjk00ytswsENg//Ee6dWBJZyLH+QGsaU2jO/W4WvRyZhmKKPdipOhiz4Rlrd2XYgsfHsfWf5v4GOTL+13ZB24dW1/m39n2woJ+v686fXbNW85XP/r'
+         | let proof = base64'lvQLU/KqgFhsLkt/5C/scqs7nWR+eYtyPdWiLVBux9GblT4AhHYMdCgwQfSJcudvsgV6fXoK+DUSRgJ++Nqt+Wvb7GlYlHpxCysQhz26TTu8Nyo7zpmVPH92+UYmbvbQCSvX2BhWtvkfHmqDVjmSIQ4RUMfeveA1KZbSf999NE4qKK8Do+8oXcmTM4LZVmh1rlyqznIdFXPN7x3pD4E0gb6/y69xtWMChv9654FMg05bAdueKt9uA4BEcAbpkdHF'
+         | let input = base64'LcMT3OOlkHLzJBKCKjjzzVMg+r+FVgd52LlhZPB4RFg='
+         | groth16Verify(key, proof, input)
+       """.stripMargin
+    eval(src, version = V4) shouldBe Right(CONST_BOOLEAN(true))
   }
 }
