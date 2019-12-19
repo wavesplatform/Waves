1d27d403ddb1b44b43bbe869329838bc778bc47a
Valeriy Gavrilin <yoba123@yandex.ru>
2019-12-19 14:59:59 +0300

    Script estimations counter

--- node/src/main/scala/com/wavesplatform/Importer.scala
+++ node/src/main/scala/com/wavesplatform/Importer.scala
@@ -169,7 +169,7 @@ object Importer extends ScorexLogging {
     val start    = System.currentTimeMillis()
     var counter  = 0
 
-    val startHeight   = blockchainUpdater.height
+    val startHeight   = 1 // blockchainUpdater.height
     var blocksToSkip  = startHeight - 1
     val blocksToApply = importOptions.importHeight - startHeight + 1
 
