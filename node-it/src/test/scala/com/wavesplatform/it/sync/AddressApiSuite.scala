e6fe48756bc50d49cb74379046eebdd3ac73a5e6
Anvar Kiekbaev <a.kiekbaev@chepiov.org>
2019-12-19 14:38:44 +0300

    NODE-1974: Adds integration test for GET '/addresses/balance?height='

--- node-it/src/test/scala/com/wavesplatform/it/sync/AddressApiSuite.scala
+++ node-it/src/test/scala/com/wavesplatform/it/sync/AddressApiSuite.scala
@@ -2,21 +2,31 @@ package com.wavesplatform.it.sync
 
 import java.net.URLDecoder
 
+import com.wavesplatform.it.NTPTime
+import com.wavesplatform.it.api.SyncHttpApi._
 import com.wavesplatform.it.transactions.BaseTransactionSuite
 import com.wavesplatform.state.StringDataEntry
-import com.wavesplatform.it.api.SyncHttpApi._
+import play.api.libs.json._
 
 import scala.util.Random
 
-class AddressApiSuite extends BaseTransactionSuite {
+class AddressApiSuite extends BaseTransactionSuite with NTPTime {
 
   test("filter accounts data by regexp") {
-    val dataKeys         = List("1aB1cD!@#$", "\"\\", "\u0000qweqwe", "\t\r\n", "reeeee", "rerere", "rerrre", "rre", "eeeee")
-    val regexps          = List("1aB1cD!%40%23%24", "%5Ba-zA-Z0-9!-%2F%3A-%40%5C%5C%5C%5C%5D%7B0%2C15%7D", "%5Cs%7B0%2C%7D", "re%2B", "re*re", "r%3Feeeee", "%5B%5Ct%5Cr%5Cn%5D")
-    val invalidRegexps   = List("%5Ba-z", "%5Ba-z%5D%7B0", "%5Ba-z%5D%7B%2C5%7D")
-    val data             = dataKeys.map(str => StringDataEntry(str, Random.nextString(16)))
-    val transferFee      = calcDataFee(data)
-    val txId             = sender.putData(firstAddress, data, transferFee).id
+    val dataKeys = List("1aB1cD!@#$", "\"\\", "\u0000qweqwe", "\t\r\n", "reeeee", "rerere", "rerrre", "rre", "eeeee")
+    val regexps = List(
+      "1aB1cD!%40%23%24",
+      "%5Ba-zA-Z0-9!-%2F%3A-%40%5C%5C%5C%5C%5D%7B0%2C15%7D",
+      "%5Cs%7B0%2C%7D",
+      "re%2B",
+      "re*re",
+      "r%3Feeeee",
+      "%5B%5Ct%5Cr%5Cn%5D"
+    )
+    val invalidRegexps = List("%5Ba-z", "%5Ba-z%5D%7B0", "%5Ba-z%5D%7B%2C5%7D")
+    val data           = dataKeys.map(str => StringDataEntry(str, Random.nextString(16)))
+    val transferFee    = calcDataFee(data)
+    val txId           = sender.putData(firstAddress, data, transferFee).id
     nodes.waitForHeightAriseAndTxPresent(txId)
 
     for (regexp <- regexps) {
@@ -33,10 +43,27 @@ class AddressApiSuite extends BaseTransactionSuite {
         sender.getData(firstAddress, invalidRegexp)
         fail("RegexCompiler didn't throw expected error")
       } catch {
-        case err: Throwable => if (!err.getMessage.contains("Cannot compile regex")) {
-          throw err
-        }
+        case err: Throwable =>
+          if (!err.getMessage.contains("Cannot compile regex")) {
+            throw err
+          }
       }
     }
+  }
+
+  test("balances should be correct") {
+    val assetId   = miner.issue(miner.address, "Test", "Test", 10000, 0, waitForTx = true).id
+    val addresses = (1 to 5).map(i => (notMiner.createAddress(), i * 100)).toList
+
+    addresses.foreach { case (address, a) => miner.transfer(miner.address, address, a, minFee, Some(assetId), waitForTx = true) }
+
+    val height = miner.height
+
+    val response = Json.parse(
+      miner.get(s"/addresses/balance?height=$height&asset=$assetId&${addresses.map(a => s"address=${a._1}").mkString("&")}").getResponseBody
+    )
+
+    val result = response.as[List[JsObject]].map(r => ((r \ "id").as[String], (r \ "balance").as[Long]))
+    result should contain theSameElementsAs addresses
   }
 }
