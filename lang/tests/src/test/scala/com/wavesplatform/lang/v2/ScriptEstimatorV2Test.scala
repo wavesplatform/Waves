3b8f0758139749a40d1ea1d7865985455fb25150
Artyom Sayadyan <xrtm000@gmail.com>
2020-01-14 18:40:27 +0300

    SC-529 Interrupt Estimator V2 (#2864)
    * SC-529 Interrupt Estimaror V2
    
    * Remove unactual test
    
    * Add test
    
    * Add test
    
    * Stop thread after test
    
    * Check thread state in test
    
    * Reflective stop in test
    
    * Remove test
    
    * Revert "Remove test"
    
    This reverts commit 499531b404b195e7700bf7bb08cf29e4a664d996.
    
    * Use daemon thread
    
    * Adapt utx thread test

--- lang/tests/src/test/scala/com/wavesplatform/lang/v2/ScriptEstimatorV2Test.scala
+++ lang/tests/src/test/scala/com/wavesplatform/lang/v2/ScriptEstimatorV2Test.scala
@@ -1,5 +1,7 @@
 package com.wavesplatform.lang.v2
 
+import java.util.concurrent.Executors
+
 import com.wavesplatform.common.utils.EitherExt2
 import com.wavesplatform.lang.directives.values.V3
 import com.wavesplatform.lang.utils._
@@ -51,5 +53,28 @@ class ScriptEstimatorV2Test extends ScriptEstimatorTest(ScriptEstimatorV2) {
       firstCallCount - 1 /* pluses */     +
       5 * 4              /* all blocks */ +
       7                  /* calc 1 + 1 + 1 + 1 exactly once */
+  }
+
+  property("interrupting") {
+    val count = 30
+    val hangingScript =
+      s"""
+         | func f0() = 0
+         | ${(1 to count).map(i => s"func f$i() = if (true) then f${i-1}() else f${i-1}()").mkString("\n")}
+         | f$count()
+       """.stripMargin
+
+    @volatile var r: Either[String, Long] = Right(0)
+    val run: Runnable = { () => r = estimate(functionCosts(V3), compile(hangingScript)) }
+    val t = new Thread(run)
+    t.setDaemon(true)
+
+    t.start()
+    Thread.sleep(5000)
+    t.interrupt()
+    Thread.sleep(500)
+
+    r shouldBe Left("Script estimation was interrupted")
+    t.getState shouldBe Thread.State.TERMINATED
   }
 }
