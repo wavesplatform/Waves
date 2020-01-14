92bd09403c0c15769e24615c56e35d9b6d65462d
Karasiq <yoba123@yandex.ru>
2020-01-14 16:16:47 +0300

    Fix UTX pool synchronizer stop

--- node/src/main/scala/com/wavesplatform/network/UtxPoolSynchronizer.scala
+++ node/src/main/scala/com/wavesplatform/network/UtxPoolSynchronizer.scala
@@ -12,7 +12,8 @@ import com.wavesplatform.utils.ScorexLogging
 import com.wavesplatform.utx.UtxPool
 import io.netty.channel.Channel
 import io.netty.channel.group.ChannelGroup
-import monix.execution.{AsyncQueue, CancelableFuture, Scheduler}
+import monix.eval.Task
+import monix.execution.{AsyncQueue, Scheduler}
 import monix.reactive.Observable
 
 import scala.concurrent.duration._
@@ -28,39 +29,52 @@ class UtxPoolSynchronizerImpl(
     val settings: UtxSynchronizerSettings,
     putIfNew: Transaction => TracedResult[ValidationError, Boolean],
     broadcast: (Transaction, Option[Channel]) => Unit,
-    blockSource: Observable[LastBlockInfo]
-)(
-    implicit val scheduler: Scheduler
+    blockSource: Observable[LastBlockInfo],
+    timedScheduler: Scheduler
 ) extends UtxPoolSynchronizer
     with ScorexLogging
     with AutoCloseable {
 
-  val queue = AsyncQueue.bounded[(Transaction, Channel)](settings.maxQueueSize)
+  import Scheduler.Implicits.global
 
-  val dummy = new Object()
-  val knownTransactions = CacheBuilder
+  private[this] val queue = AsyncQueue.bounded[(Transaction, Channel)](settings.maxQueueSize)
+
+  private[this] val dummy = new Object()
+  private[this] val knownTransactions = CacheBuilder
     .newBuilder()
     .maximumSize(settings.networkTxCacheSize)
     .build[ByteStr, Object]
 
-  val cancelableFuture = pollTransactions()
+  private[this] val pollLoopCancelable = Observable
+    .repeatEvalF(Task.deferFuture(queue.poll()))
+    .observeOn(Scheduler.global)
+    .mapParallelUnordered(settings.maxThreads) {
+      case (tx, source) =>
+        Task
+          .deferFuture(validateFuture(tx, allowRebroadcast = false, Some(source)))
+          .timeout(10 seconds)
+          .onErrorRecover { case err => TracedResult.wrapE(Left(GenericError(err.toString))) }
+    }
+    .doOnComplete(Task(log.info("UtxPoolSynchronizer stopped")))
+    .doOnError(e => Task(log.warn("UtxPoolSynchronizer stopped abnormally", e)))
+    .subscribe()
 
   blockSource.map(_.height).distinctUntilChanged.foreach(_ => knownTransactions.invalidateAll())
 
-  private def transactionIsNew(txId: ByteStr): Boolean = {
-    var isNew = false
-    knownTransactions.get(txId, { () =>
-      isNew = true; dummy
-    })
-    isNew
-  }
+  override def tryPublish(tx: Transaction, source: Channel): Unit = {
+    def transactionIsNew(txId: ByteStr): Boolean = {
+      var isNew = false
+      knownTransactions.get(txId, { () =>
+        isNew = true; dummy
+      })
+      isNew
+    }
 
-  override def tryPublish(tx: Transaction, source: Channel): Unit = if (transactionIsNew(tx.id())) {
-    queue.tryOffer(tx -> source)
+    if (transactionIsNew(tx.id())) queue.tryOffer(tx -> source)
   }
 
   private def validateFuture(tx: Transaction, allowRebroadcast: Boolean, source: Option[Channel]): Future[TracedResult[ValidationError, Boolean]] =
-    Future(putIfNew(tx))(scheduler)
+    Future(putIfNew(tx))(timedScheduler)
       .recover {
         case t =>
           log.warn(s"Error validating transaction ${tx.id()}", t)
@@ -73,18 +87,16 @@ class UtxPoolSynchronizerImpl(
   override def publish(tx: Transaction): TracedResult[ValidationError, Boolean] =
     Await.result(validateFuture(tx, settings.allowTxRebroadcasting, None), 10.seconds)
 
-  override def close(): Unit = cancelableFuture.cancel()
-
-  private def pollTransactions(): CancelableFuture[Unit] = queue.poll().flatMap {
-    case (tx, source) =>
-      validateFuture(tx, allowRebroadcast = false, Some(source))
-      pollTransactions()
-  }
+  override def close(): Unit = pollLoopCancelable.cancel()
 }
 
 object UtxPoolSynchronizer extends ScorexLogging {
-  def apply(utx: UtxPool, settings: UtxSynchronizerSettings, allChannels: ChannelGroup, blockSource: Observable[LastBlockInfo])(
-      implicit sc: Scheduler
-  ): UtxPoolSynchronizer = new UtxPoolSynchronizerImpl(settings, tx => utx.putIfNew(tx), (tx, ch) => allChannels.broadcast(tx, ch), blockSource)
+  def apply(
+      utx: UtxPool,
+      settings: UtxSynchronizerSettings,
+      allChannels: ChannelGroup,
+      blockSource: Observable[LastBlockInfo],
+      sc: Scheduler
+  ): UtxPoolSynchronizer = new UtxPoolSynchronizerImpl(settings, tx => utx.putIfNew(tx), (tx, ch) => allChannels.broadcast(tx, ch), blockSource, sc)
 
 }
