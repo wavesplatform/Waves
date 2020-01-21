2dc7883c24d672df138fd302e8b8a1c38f71a2bd
Karasiq <yoba123@yandex.ru>
2020-01-21 12:23:08 +0300

    Node-IT thread dump on failed test

--- node-it/src/test/scala/com/wavesplatform/it/DockerBased.scala
+++ node-it/src/test/scala/com/wavesplatform/it/DockerBased.scala
@@ -1,12 +1,24 @@
 package com.wavesplatform.it
 
+import com.wavesplatform.it.Docker.DockerNode
 import monix.eval.Coeval
-import org.scalatest.{BeforeAndAfterAll, Suite}
+import org.scalatest.{Args, BeforeAndAfterAll, Status, Suite}
 
 trait DockerBased extends BeforeAndAfterAll {
-  this: Suite =>
+  this: Suite with Nodes =>
+
   protected val dockerSingleton: Coeval[Docker] = Coeval.evalOnce(createDocker)
-  final def docker: Docker            = dockerSingleton()
+  final def docker: Docker                      = dockerSingleton()
+
+  abstract override protected def runTest(testName: String, args: Args): Status = {
+    def printThreadDump(): Unit = nodes.collect {
+      case node: DockerNode =>
+        docker.printThreadDump(node)
+    }
+    val r = super.runTest(testName, args)
+    if (!r.succeeds()) printThreadDump()
+    r
+  }
 
   protected def createDocker: Docker = Docker(getClass)
   override protected def afterAll(): Unit = {
--- node-it/src/test/scala/com/wavesplatform/it/ReportingTestName.scala
+++ node-it/src/test/scala/com/wavesplatform/it/ReportingTestName.scala
@@ -1,7 +1,6 @@
 package com.wavesplatform.it
 
 import com.wavesplatform.http.DebugMessage
-import com.wavesplatform.it.Docker.DockerNode
 import com.wavesplatform.it.api.AsyncHttpApi._
 import com.wavesplatform.utils.ScorexLogging
 import org.scalatest.{Args, Status, Suite, SuiteMixin}
@@ -15,7 +14,6 @@ trait ReportingTestName extends SuiteMixin with ScorexLogging {
   abstract override protected def runTest(testName: String, args: Args): Status = {
     print(s"Test '$testName' started")
     val r = super.runTest(testName, args)
-    if (!r.succeeds()) printThreadDump()
     print(s"Test `$testName` finished with $r")
     r
   }
@@ -28,16 +26,6 @@ trait ReportingTestName extends SuiteMixin with ScorexLogging {
       Await.result(Future.traverse(nodes)(_.printDebugMessage(DebugMessage(formatted))), 10.seconds)
     } catch {
       case _: Throwable => ()
-    }
-  }
-
-  private def printThreadDump(): Unit = {
-    nodes.collect {
-      case node: DockerNode =>
-        this match {
-          case db: DockerBased => db.docker.printThreadDump(node)
-          case _               => // Ignore
-        }
     }
   }
 }
