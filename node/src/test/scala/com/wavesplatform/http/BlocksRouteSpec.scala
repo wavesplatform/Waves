f93339abb5ded924ed879898b03091fb9f4ccf62
Karasiq <yoba123@yandex.ru>
2020-01-22 19:06:26 +0300

    Blocks api route spec

--- node/src/test/scala/com/wavesplatform/http/BlocksRouteSpec.scala
+++ node/src/test/scala/com/wavesplatform/http/BlocksRouteSpec.scala
@@ -1,6 +1,8 @@
 package com.wavesplatform.http
 
 import com.wavesplatform.api.http.BlocksApiRoute
+import com.wavesplatform.block.Block.BlockInfo
+import com.wavesplatform.block.serialization.BlockHeaderSerializer
 import com.wavesplatform.common.state.ByteStr
 import com.wavesplatform.features.BlockchainFeatures
 import com.wavesplatform.http.ApiMarshallers._
@@ -24,11 +26,29 @@ class BlocksRouteSpec extends RouteSpec("/blocks") with PathMockFactory with Pro
   val testBlock1Json = testBlock1.json() ++ Json.obj("height" -> 1, "totalFee" -> 10L)
   val testBlock2Json = testBlock2.json() ++ Json.obj("height" -> 2, "totalFee" -> 10L, "reward" -> 5, "VRF" -> testBlock2.uniqueId.toString)
 
+  val testBlock1HeaderJson = BlockHeaderSerializer.toJson(testBlock1.header, testBlock1.bytes().size, 0, testBlock1.signature) ++ Json.obj(
+    "height"   -> 1,
+    "totalFee" -> 10L
+  )
+  val testBlock2HeaderJson = BlockHeaderSerializer.toJson(testBlock2.header, testBlock2.bytes().size, 0, testBlock2.signature) ++ Json.obj(
+    "height"   -> 2,
+    "totalFee" -> 10L,
+    "reward"   -> 5,
+    "VRF"      -> testBlock2.uniqueId.toString
+  )
+
   (blockchain.blockBytes(_: Int)).when(1).returning(Some(testBlock1.bytes()))
   (blockchain.blockBytes(_: Int)).when(2).returning(Some(testBlock2.bytes()))
 
+  (blockchain.blockInfo(_: Int)).when(1).returning(Some(BlockInfo(testBlock1.header, testBlock1.bytes().size, 0, testBlock1.signature)))
+  (blockchain.blockInfo(_: Int)).when(2).returning(Some(BlockInfo(testBlock2.header, testBlock2.bytes().size, 0, testBlock2.signature)))
+
   (blockchain.blockBytes(_: ByteStr)).when(testBlock1.uniqueId).returning(Some(testBlock1.bytes()))
   (blockchain.blockBytes(_: ByteStr)).when(testBlock2.uniqueId).returning(Some(testBlock2.bytes()))
+
+  (blockchain.heightOf(_: ByteStr)).when(testBlock1.uniqueId).returning(Some(1))
+  (blockchain.heightOf(_: ByteStr)).when(testBlock2.uniqueId).returning(Some(2))
+
   (blockchain.lastBlock _).when().returning(Some(testBlock2))
 
   (blockchain.blockReward _).when(*).returning(Some(5L))
@@ -52,6 +72,72 @@ class BlocksRouteSpec extends RouteSpec("/blocks") with PathMockFactory with Pro
     Get(routePath("/last")) ~> route ~> check {
       val response = responseAs[JsObject]
       response shouldBe testBlock2Json
+      response
+    }
+  }
+
+  routePath("/at") in {
+    Get(routePath("/at/1")) ~> route ~> check {
+      val response = responseAs[JsObject]
+      response shouldBe testBlock1Json
+      response
+    }
+
+    Get(routePath("/at/2")) ~> route ~> check {
+      val response = responseAs[JsObject]
+      response shouldBe testBlock2Json
+      response
+    }
+  }
+
+  routePath("/signature") in {
+    Get(routePath(s"/signature/${testBlock1.uniqueId}")) ~> route ~> check {
+      val response = responseAs[JsObject]
+      response shouldBe testBlock1Json
+      response
+    }
+
+    Get(routePath(s"/signature/${testBlock2.uniqueId}")) ~> route ~> check {
+      val response = responseAs[JsObject]
+      response shouldBe testBlock2Json
+      response
+    }
+  }
+
+  routePath("/seq/1/2") in {
+    Get(routePath("/seq/1/2")) ~> route ~> check {
+      val response = responseAs[Seq[JsObject]]
+      response shouldBe Seq(testBlock1Json, testBlock2Json)
+      response
+    }
+  }
+
+  routePath("/headers/last") in {
+    Get(routePath("/headers/last")) ~> route ~> check {
+      val response = responseAs[JsObject]
+      response shouldBe testBlock2HeaderJson
+      response
+    }
+  }
+
+  routePath("/headers/at") in {
+    Get(routePath("/headers/at/1")) ~> route ~> check {
+      val response = responseAs[JsObject]
+      response shouldBe testBlock1HeaderJson
+      response
+    }
+
+    Get(routePath("/headers/at/2")) ~> route ~> check {
+      val response = responseAs[JsObject]
+      response shouldBe testBlock2HeaderJson
+      response
+    }
+  }
+
+  routePath("/headers/seq/1/2") in {
+    Get(routePath("/headers/seq/1/2")) ~> route ~> check {
+      val response = responseAs[Seq[JsObject]]
+      response shouldBe Seq(testBlock1HeaderJson, testBlock2HeaderJson)
       response
     }
   }
