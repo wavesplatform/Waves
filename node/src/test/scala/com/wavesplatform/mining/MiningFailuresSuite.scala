37085da2297bdc57c35ad721241bc858a5df0de8
Karasiq <yoba123@yandex.ru>
2020-01-24 12:00:45 +0300

    Refactoring

--- node/src/test/scala/com/wavesplatform/mining/MiningFailuresSuite.scala
+++ node/src/test/scala/com/wavesplatform/mining/MiningFailuresSuite.scala
@@ -1,23 +1,16 @@
 package com.wavesplatform.mining
 
-import java.io.{PrintWriter, StringWriter}
-
-import cats.effect.Resource
 import com.typesafe.config.ConfigFactory
 import com.wavesplatform.account.KeyPair
-import com.wavesplatform.block.Block
-import com.wavesplatform.common.state.ByteStr
-import com.wavesplatform.common.utils._
 import com.wavesplatform.consensus.PoSSelector
-import com.wavesplatform.database.LevelDBWriter
+import com.wavesplatform.consensus.nxt.NxtLikeConsensusBlockData
 import com.wavesplatform.db.DBCacheSettings
-import com.wavesplatform.features.BlockchainFeatures
 import com.wavesplatform.lagonaki.mocks.TestBlock
 import com.wavesplatform.settings._
 import com.wavesplatform.state.diffs.ENOUGH_AMT
-import com.wavesplatform.state.{BlockchainUpdaterImpl, NG}
-import com.wavesplatform.transaction.{BlockchainUpdater, GenesisTransaction, Transaction}
-import com.wavesplatform.utils.Time
+import com.wavesplatform.state.{BalanceSnapshot, BlockMinerInfo, NG}
+import com.wavesplatform.transaction.BlockchainUpdater
+import com.wavesplatform.transaction.TxValidationError.BlockFromFuture
 import com.wavesplatform.utx.UtxPoolImpl
 import com.wavesplatform.wallet.Wallet
 import com.wavesplatform.{TransactionGen, WithDB}
@@ -25,133 +18,73 @@ import io.netty.channel.group.DefaultChannelGroup
 import io.netty.util.concurrent.GlobalEventExecutor
 import monix.eval.Task
 import monix.execution.Scheduler
-import org.iq80.leveldb.DB
-import org.scalacheck.{Arbitrary, Gen}
-import org.scalatest.compatible.Assertion
+import monix.execution.Scheduler.Implicits.global
+import org.scalamock.scalatest.PathMockFactory
 import org.scalatest.{AsyncFlatSpec, Matchers, PrivateMethodTester}
 
-import scala.concurrent.Future
-import scala.concurrent.duration._
+class MiningFailuresSuite
+    extends AsyncFlatSpec
+    with Matchers
+    with WithDB
+    with TransactionGen
+    with PrivateMethodTester
+    with DBCacheSettings
+    with PathMockFactory {
 
-class MiningFailuresSuite extends AsyncFlatSpec with Matchers with WithDB with TransactionGen with PrivateMethodTester with DBCacheSettings {
-  import MiningFailuresSuite._
+  trait BlockchainUpdaterNG extends BlockchainUpdater with NG
 
   behavior of "Miner"
 
   it should "generate valid blocks ignoring time errors " in {
-    withEnv(Seq.empty) {
-      case Env(_, account, miner, blockchain, fakeTime) =>
-        val generateBlock = generateBlockTask(miner)(account)
-        fakeTime.fromFutureGenEnabled = true
-        Scheduler.global.scheduleOnce(3 seconds)(fakeTime.fromFutureGenEnabled = false)
-        for {
-          _ <- generateBlock
-        } yield blockchain.height should be(2)
+    val blockchainUpdater = stub[BlockchainUpdaterNG]
+
+    val wavesSettings = {
+      val config = ConfigFactory.parseString("""
+          |waves.miner {
+          |  quorum = 0
+          |  interval-after-last-block-then-generation-is-allowed = 0
+          |}""".stripMargin).withFallback(ConfigFactory.load())
+
+      WavesSettings.fromRootConfig(loadConfig(config))
     }
+
+    val miner = {
+      val scheduler   = Scheduler.singleThread("appender")
+      val allChannels = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE)
+      val wallet      = Wallet(WalletSettings(None, Some("123"), None))
+      val utxPool     = new UtxPoolImpl(ntpTime, blockchainUpdater, ignoreSpendableBalanceChanged, wavesSettings.utxSettings)
+      val pos         = new PoSSelector(blockchainUpdater, wavesSettings.blockchainSettings, wavesSettings.synchronizationSettings)
+      new MinerImpl(allChannels, blockchainUpdater, wavesSettings, ntpTime, utxPool, wallet, pos, scheduler, scheduler)
+    }
+
+    val genesis = TestBlock.create(System.currentTimeMillis(), Nil)
+    (blockchainUpdater.isLastBlockId _).when(genesis.uniqueId).returning(true)
+    (blockchainUpdater.heightOf _).when(genesis.uniqueId).returning(Some(1))
+    (blockchainUpdater.height _).when().returning(1)
+    (blockchainUpdater.settings _).when().returning(wavesSettings.blockchainSettings)
+    (blockchainUpdater.lastBlock _).when().returning(Some(genesis))
+    (blockchainUpdater.parentHeader _).when(*, *).returning(Some(genesis.getHeader()))
+    (blockchainUpdater.activatedFeatures _).when().returning(Map.empty)
+    (blockchainUpdater.bestLastBlockInfo _)
+      .when(*)
+      .returning(
+        Some(
+          BlockMinerInfo(
+            NxtLikeConsensusBlockData(genesis.consensusData.baseTarget, genesis.consensusData.generationSignature),
+            genesis.timestamp,
+            genesis.uniqueId
+          )
+        )
+      )
+    (blockchainUpdater.processBlock _).when(*, *).returning(Left(BlockFromFuture(100))).repeated(10)
+    (blockchainUpdater.processBlock _).when(*, *).returning(Right(None)).once()
+    (blockchainUpdater.balanceSnapshots _).when(*, *, *).returning(Seq(BalanceSnapshot(1, ENOUGH_AMT, 0, 0)))
+
+    val account       = accountGen.sample.get
+    val generateBlock = generateBlockTask(miner)(account)
+    generateBlock.runSyncUnsafe() shouldBe ()
   }
 
-  private def withEnv(bps: Seq[BlockProducer], txs: Seq[TransactionProducer] = Seq(), times: Seq[Long] = Seq(System.currentTimeMillis()))(
-      f: Env => Task[Assertion]
-  ): Task[Assertion] =
-    resources.use {
-      case (blockchainUpdater, _, fakeTime) =>
-        for {
-          _ <- Task.unit
-          pos          = new PoSSelector(blockchainUpdater, blockchainSettings, synchronizationSettings)
-          utxPool      = new UtxPoolImpl(fakeTime, blockchainUpdater, ignoreSpendableBalanceChanged, utxSettings)
-          scheduler    = Scheduler.singleThread("appender")
-          allChannels  = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE)
-          wallet       = Wallet(WalletSettings(None, Some("123"), None))
-          miner        = new MinerImpl(allChannels, blockchainUpdater, settings, fakeTime, utxPool, wallet, pos, scheduler, scheduler)
-          account      = createAccount
-          ts           = fakeTime.correctedTime() - 60000
-          genesisBlock = TestBlock.create(ts + 2, List(GenesisTransaction.create(account, ENOUGH_AMT, ts + 1).explicitGet()))
-          _ <- Task(blockchainUpdater.processBlock(genesisBlock))
-          blocks = bps.foldLeft {
-            (ts + 1, Seq[Block](genesisBlock))
-          } {
-            case ((ts, chain), bp) =>
-              (ts + 3, bp(ts + 3, chain.head.uniqueId, account) +: chain)
-          }._2
-          added <- Task.traverse(blocks.reverse)(b => Task(blockchainUpdater.processBlock(b)))
-          _   = added.foreach(_.explicitGet())
-          _   = txs.foreach(tx => utxPool.putIfNew(tx(ts + 6, account)).resultE.explicitGet())
-          env = Env(blocks, account, miner, blockchainUpdater, fakeTime)
-          r <- f(env)
-        } yield r
-    }
-
-  private def generateBlockTask(miner: MinerImpl)(account: KeyPair): Task[Unit] =
+  private[this] def generateBlockTask(miner: MinerImpl)(account: KeyPair): Task[Unit] =
     miner.invokePrivate(PrivateMethod[Task[Unit]]('generateBlockTask)(account))
-
-  private def resources: Resource[Task, (BlockchainUpdaterImpl, DB, FakeTimeService)] =
-    Resource.make {
-      val fakeTime          = new FakeTimeService
-      val defaultWriter     = new LevelDBWriter(db, ignoreSpendableBalanceChanged, blockchainSettings, dbSettings)
-      val blockchainUpdater = new BlockchainUpdaterImpl(defaultWriter, ignoreSpendableBalanceChanged, settings, fakeTime)
-      Task.now((blockchainUpdater, db, fakeTime))
-    } {
-      case (blockchainUpdater, db, _) =>
-        Task {
-          blockchainUpdater.shutdown()
-          db.close()
-        }
-    }
-}
-
-object MiningFailuresSuite {
-  class FakeTimeService extends Time {
-    @volatile
-    var fromFutureGenEnabled = false
-
-    private[this] def nextTime(): Long = {
-      val stackTrace = {
-        val err = new RuntimeException()
-        val sw  = new StringWriter()
-        val pw  = new PrintWriter(sw)
-        err.printStackTrace(pw)
-        pw.close()
-        sw.toString
-      }
-
-      val shouldPatch = stackTrace.contains("com.wavesplatform.mining.MinerImpl.currentTime$1")
-      val time        = System.currentTimeMillis()
-      if (shouldPatch && fromFutureGenEnabled) time + 1000000 else time
-    }
-
-    override def correctedTime(): Long = nextTime()
-    override def getTimestamp(): Long  = nextTime()
-  }
-
-  import TestFunctionalitySettings.Enabled
-  import monix.execution.Scheduler.Implicits.global
-
-  type BlockProducer       = (Long, ByteStr, KeyPair) => Block
-  type TransactionProducer = (Long, KeyPair) => Transaction
-
-  case class Env(blocks: Seq[Block], account: KeyPair, miner: MinerImpl, blockchain: BlockchainUpdater with NG, time: FakeTimeService)
-
-  val commonSettings: WavesSettings                    = WavesSettings.fromRootConfig(loadConfig(ConfigFactory.load()))
-  val minerSettings: MinerSettings                     = commonSettings.minerSettings.copy(quorum = 0, intervalAfterLastBlockThenGenerationIsAllowed = 1 hour)
-  val synchronizationSettings: SynchronizationSettings = commonSettings.synchronizationSettings
-  val utxSettings: UtxSettings                         = commonSettings.utxSettings
-
-  val functionalitySettings: FunctionalitySettings =
-    Enabled
-      .copy(preActivatedFeatures = Enabled.preActivatedFeatures + (BlockchainFeatures.BlockReward.id -> 0))
-  val blockchainSettings: BlockchainSettings =
-    commonSettings.blockchainSettings
-      .copy(functionalitySettings = functionalitySettings)
-      .copy(rewardsSettings = RewardsSettings.TESTNET)
-
-  val settings: WavesSettings = commonSettings.copy(minerSettings = minerSettings, blockchainSettings = blockchainSettings)
-
-  def createAccount: KeyPair =
-    Gen
-      .containerOfN[Array, Byte](32, Arbitrary.arbitrary[Byte])
-      .map(bs => KeyPair(bs))
-      .sample
-      .get
-
-  private implicit def taskToFuture(task: Task[Assertion]): Future[Assertion] = task.runToFuture
 }
