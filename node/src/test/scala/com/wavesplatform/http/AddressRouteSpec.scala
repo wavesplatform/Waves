a1a888c153bcc5751c8655c49d7d49c4b000cc49
Artyom Sayadyan <xrtm000@gmail.com>
2020-01-29 11:54:47 +0300

    Fix types

--- node/src/test/scala/com/wavesplatform/http/AddressRouteSpec.scala
+++ node/src/test/scala/com/wavesplatform/http/AddressRouteSpec.scala
@@ -14,7 +14,7 @@ import com.wavesplatform.lang.directives.values.V3
 import com.wavesplatform.lang.script.ContractScript
 import com.wavesplatform.protobuf.dapp.DAppMeta
 import com.wavesplatform.protobuf.dapp.DAppMeta.CallableFuncSignature
-import com.wavesplatform.state.StringDataEntry
+import com.wavesplatform.state.{AccountScriptInfo, StringDataEntry}
 
 import scala.util.Random
 // [WAIT] import com.wavesplatform.lang.{Global, StdLibVersion}
@@ -161,7 +161,9 @@ class AddressRouteSpec
   routePath(s"/scriptInfo/${allAddresses(1)}") in {
     (blockchain.accountScriptWithComplexity _)
       .when(allAccounts(1).toAddress)
-      .onCall((a: AddressOrAlias) => Some((address2account(a.toString).publicKey, ExprScript(TRUE).explicitGet(), 1L, Map[String, Long]())))
+      .onCall((a: AddressOrAlias) =>
+        Some(AccountScriptInfo(address2account(a.toString).publicKey, ExprScript(TRUE).explicitGet(), 1L, Map()))
+      )
 
     Get(routePath(s"/scriptInfo/${allAddresses(1)}")) ~> route ~> check {
       val response = responseAs[JsObject]
@@ -214,7 +216,9 @@ class AddressRouteSpec
 
     (blockchain.accountScriptWithComplexity _)
       .when(allAccounts(3).toAddress)
-      .onCall((a: AddressOrAlias) => Some((address2account(a.toString).publicKey, ContractScript(V3, contractWithMeta).explicitGet(), 11L, Map[String, Long]())))
+      .onCall((a: AddressOrAlias) =>
+        Some(AccountScriptInfo(address2account(a.toString).publicKey, ContractScript(V3, contractWithMeta).explicitGet(), 11L, Map()))
+      )
     (blockchain.accountScript _)
       .when(allAccounts(3).toAddress)
       .onCall((_: AddressOrAlias) => Some(ContractScript(V3, contractWithMeta).explicitGet()))
