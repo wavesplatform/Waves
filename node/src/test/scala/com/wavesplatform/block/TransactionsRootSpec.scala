c0129309d208bc2a14ea01e17c62e004f6f55f04
Anvar Kiekbaev <a.kiekbaev@chepiov.org>
2020-02-03 13:09:28 +0300

    NODE-2043: Merkle proofs in Base58 (#2907)
    * NODE-2043: Merkle proofs in Base58
    * NODE-2043: Adding test case for single transaction in block

--- node/src/test/scala/com/wavesplatform/block/TransactionsRootSpec.scala
+++ node/src/test/scala/com/wavesplatform/block/TransactionsRootSpec.scala
@@ -2,6 +2,7 @@ package com.wavesplatform.block
 
 import com.wavesplatform.account.KeyPair
 import com.wavesplatform.block.merkle.Merkle._
+import com.wavesplatform.protobuf.transaction.PBTransactions
 import com.wavesplatform.transaction.Asset.Waves
 import com.wavesplatform.transaction.Transaction
 import com.wavesplatform.transaction.transfer.TransferTransaction
@@ -9,6 +10,9 @@ import com.wavesplatform.{BlockGen, NoShrink, TransactionGen}
 import org.scalacheck.Gen
 import org.scalatest.{FreeSpec, Matchers, OptionValues}
 import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+import scorex.crypto.authds.LeafData
+import scorex.crypto.authds.merkle.{Leaf, MerkleTree}
+import scorex.crypto.hash.Digest32
 
 class TransactionsRootSpec
     extends FreeSpec
@@ -38,10 +42,10 @@ class TransactionsRootSpec
     case (block, transaction) =>
       block.transactionsRootValid() shouldBe true
 
-      val merkleProof2 = block.transactionProof(transaction)
+      val merkleProof = block.transactionProof(transaction)
 
-      merkleProof2 shouldBe 'defined
-      block.verifyTransactionProof(merkleProof2.value) shouldBe true
+      merkleProof shouldBe 'defined
+      block.verifyTransactionProof(merkleProof.value) shouldBe true
   }
 
   val emptyTxsDataScenario: Gen[(Block, TransferTransaction)] =
@@ -70,6 +74,35 @@ class TransactionsRootSpec
     case (block, transaction) =>
       block.transactionsRootValid() shouldBe true
       block.transactionProof(transaction) shouldBe 'empty
+  }
+
+  val singleTransactionScenario: Gen[(Block, Transaction)] =
+    for {
+      (signer, txs) <- commonGen
+      tx            <- Gen.oneOf(txs)
+      block         <- versionedBlockGen(Seq(tx), signer, Block.ProtoBlockVersion)
+    } yield (block, tx)
+
+  "Merkle tree for block with single transaction should validate it" in forAll(singleTransactionScenario) {
+    case (block, transaction) =>
+      block.transactionsRootValid() shouldBe true
+
+      val merkleProof = block.transactionProof(transaction)
+
+      merkleProof shouldBe 'defined
+      block.verifyTransactionProof(merkleProof.value) shouldBe true
+      merkleProof.value.digests.head shouldBe Array.emptyByteArray
+
+      val nativeLeafData    = LeafData @@ PBTransactions.protobuf(transaction).toByteArray
+      val nativeMerkleTree  = MerkleTree(Seq(nativeLeafData))
+      val nativeMerkleProof = nativeMerkleTree.proofByElement(Leaf(nativeLeafData))
+      nativeMerkleProof shouldBe 'defined
+      nativeMerkleProof.value.valid(Digest32 @@ block.header.transactionsRoot.arr)
+
+      // it's okay to have empty digest in a single level for the tree with the only one leaf
+      nativeMerkleProof.value.levels.map(_._1.toList) shouldBe merkleProof.value.digests.map(_.toList)
+      merkleProof.value.digests.nonEmpty shouldBe true
+      merkleProof.value.digests.head.isEmpty shouldBe true
   }
 
   val incorrectProofScenario: Gen[(Block, Transaction, TransactionProof)] =
