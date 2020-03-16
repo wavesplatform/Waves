2216e0a7a090ff3b1a26a64149a9c10d7ea646f3
Anvar Kiekbaev <a.kiekbaev@chepiov.org>
2020-03-16 11:52:23 +0300

    Adding odd case

--- benchmark/src/test/scala/com/wavesplatform/transaction/merkle/MerkleBenchmark.scala
+++ benchmark/src/test/scala/com/wavesplatform/transaction/merkle/MerkleBenchmark.scala
@@ -2,7 +2,7 @@ package com.wavesplatform.transaction.merkle
 
 import java.util.concurrent.TimeUnit
 
-import com.wavesplatform.block.merkle.Merkle
+import com.wavesplatform.common.merkle.Merkle
 import org.openjdk.jmh.annotations._
 import org.openjdk.jmh.infra.Blackhole
 
@@ -18,33 +18,65 @@ import scala.util.Random
 @Measurement(iterations = 10)
 class MerkleBenchmark {
 
-  val messages: Seq[Array[Byte]] = (0 until 2048).map { _ =>
-    val message = new Array[Byte](2048)
+  val messageSize = 2048
+  val evenSize    = 2048
+  val oddSize     = 2047
+
+  val messagesEven: Seq[Array[Byte]] = (0 until evenSize).map { _ =>
+    val message = new Array[Byte](messageSize)
     Random.nextBytes(message)
     message
   }
 
-  val levels: Seq[Seq[Array[Byte]]] = Merkle.mkLevels(messages)
+  val messagesOdd: Seq[Array[Byte]] = (0 until oddSize).map { _ =>
+    val message = new Array[Byte](messageSize)
+    Random.nextBytes(message)
+    message
+  }
 
-  val messageIdx: Int            = Random.nextInt(2048)
-  val messageDigest: Array[Byte] = Merkle.hash(messages(messageIdx))
-  val proof: Seq[Array[Byte]]    = Merkle.mkProofs(messageIdx, levels)
+  val levelsEven: Seq[Seq[Array[Byte]]] = Merkle.mkLevels(messagesEven)
+  val levelsOdd: Seq[Seq[Array[Byte]]]  = Merkle.mkLevels(messagesOdd)
+
+  val messageIdxEven: Int            = Random.nextInt(evenSize)
+  val messageIdxOdd: Int             = Random.nextInt(oddSize)
+  val messageDigestEven: Array[Byte] = Merkle.hash(messagesEven(messageIdxEven))
+  val messageDigestOdd: Array[Byte]  = Merkle.hash(messagesOdd(messageIdxOdd))
+  val proofEven: Seq[Array[Byte]]    = Merkle.mkProofs(messageIdxEven, levelsEven)
+  val proofOdd: Seq[Array[Byte]]     = Merkle.mkProofs(messageIdxOdd, levelsOdd)
 
   @Benchmark
-  def merkleMkLevels_test(bh: Blackhole): Unit = {
-    val tree = Merkle.mkLevels(messages)
+  def merkleMkLevelsEven_test(bh: Blackhole): Unit = {
+    val tree = Merkle.mkLevels(messagesEven)
     bh.consume(tree)
   }
 
   @Benchmark
-  def merkleMkProof_test(bh: Blackhole): Unit = {
-    val proof = Merkle.mkProofs(Random.nextInt(2048), levels)
+  def merkleMkProofEven_test(bh: Blackhole): Unit = {
+    val proof = Merkle.mkProofs(Random.nextInt(evenSize), levelsEven)
     bh.consume(proof)
   }
 
   @Benchmark
-  def merkleVerifyProof_test(bh: Blackhole): Unit = {
-    val result = Merkle.verify(messageDigest, messageIdx, proof, levels.head.head)
+  def merkleVerifyProofEven_test(bh: Blackhole): Unit = {
+    val result = Merkle.verify(messageDigestEven, messageIdxEven, proofEven, levelsEven.head.head)
+    bh.consume(result)
+  }
+
+  @Benchmark
+  def merkleMkLevelsOdd_test(bh: Blackhole): Unit = {
+    val tree = Merkle.mkLevels(messagesOdd)
+    bh.consume(tree)
+  }
+
+  @Benchmark
+  def merkleMkProofOdd_test(bh: Blackhole): Unit = {
+    val proof = Merkle.mkProofs(Random.nextInt(oddSize), levelsOdd)
+    bh.consume(proof)
+  }
+
+  @Benchmark
+  def merkleVerifyProofOdd_test(bh: Blackhole): Unit = {
+    val result = Merkle.verify(messageDigestOdd, messageIdxOdd, proofOdd, levelsOdd.head.head)
     bh.consume(result)
   }
 }
