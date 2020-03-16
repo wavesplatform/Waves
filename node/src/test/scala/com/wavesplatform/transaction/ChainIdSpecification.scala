a366b4e091787b8bfc63693aa70717a29dc99586
Anvar Kiekbaev <a.kiekbaev@chepiov.org>
2020-03-16 11:32:21 +0300

    NODE-2051: Parsing transaction from other network unit test

--- node/src/test/scala/com/wavesplatform/transaction/ChainIdSpecification.scala
+++ node/src/test/scala/com/wavesplatform/transaction/ChainIdSpecification.scala
@@ -4,10 +4,11 @@ import com.wavesplatform.TransactionGen
 import com.wavesplatform.account.{Address, AddressScheme, Alias, PublicKey}
 import com.wavesplatform.common.state.ByteStr
 import com.wavesplatform.common.utils.{Base58, EitherExt2}
+import com.wavesplatform.protobuf.transaction.{PBTransactions, SignedTransaction}
 import com.wavesplatform.transaction.Asset.Waves
 import com.wavesplatform.transaction.TxValidationError.GenericError
 import com.wavesplatform.transaction.lease.LeaseTransaction
-import com.wavesplatform.transaction.smart.InvokeScriptTransaction
+import com.wavesplatform.transaction.smart.{InvokeScriptTransaction, Verifier}
 import com.wavesplatform.transaction.transfer.TransferTransaction
 import org.scalacheck.Gen
 import org.scalatest._
@@ -122,5 +123,37 @@ class ChainIdSpecification extends PropSpec with PropertyChecks with Matchers wi
       ByteStr.empty,
       AddressScheme.current.chainId
     ).validatedEither shouldBe Left(GenericError("Address or alias from other network"))
+  }
+
+  property("TransferTransaction from other network") {
+    val otherChainId = 'W'.toByte
+    val sender       = accountGen.sample.get
+    val tx = TransferTransaction(
+      TxVersion.V3,
+      sender.publicKey,
+      Alias.createWithChainId("sasha", otherChainId).explicitGet(),
+      Waves,
+      100000000,
+      Waves,
+      100000000,
+      None,
+      1526641218066L,
+      Proofs.empty,
+      otherChainId
+    ).signWith(sender.privateKey).validatedEither.right.get
+
+    tx.chainId should not be AddressScheme.current.chainId
+
+    val protoTx = PBTransactions.protobuf(tx)
+
+    val recoveredTxEi = PBTransactions.vanilla(SignedTransaction.parseFrom(protoTx.toByteArray))
+
+    recoveredTxEi shouldBe 'right
+
+    val recoveredTx = recoveredTxEi.right.get.asInstanceOf[TransferTransaction]
+
+    recoveredTx shouldBe tx
+
+    Verifier.verifyAsEllipticCurveSignature(recoveredTx) shouldBe 'right
   }
 }
