2194a70e5f3d811373a175582472556d374d4d6c
Gadiyak Nikita <ngadiyak@web3tech.ru>
2020-03-16 22:42:17 +0300

    Fixes

--- node-it/src/test/scala/com/wavesplatform/it/account/storage/RemoveEntrySuite.scala
+++ node-it/src/test/scala/com/wavesplatform/it/account/storage/RemoveEntrySuite.scala
@@ -9,105 +9,121 @@ import com.wavesplatform.it.sync._
 import com.wavesplatform.it.util._
 import com.wavesplatform.lang.v1.compiler.Terms.{CONST_BOOLEAN, CONST_BYTESTR, CONST_LONG, CONST_STRING}
 import com.wavesplatform.lang.v1.estimator.v2.ScriptEstimatorV2
+import com.wavesplatform.state.{BinaryDataEntry, BooleanDataEntry, IntegerDataEntry, StringDataEntry}
 import com.wavesplatform.transaction.smart.SetScriptTransaction
 import com.wavesplatform.transaction.smart.script.ScriptCompiler
 
+case class WriteEntry(ct: String, t: String, v: Any, k: String = "somekey")
+
 class RemoveEntrySuite extends BaseSuite {
 
-  def writeEntry(t: String, v: String) =
-    s"""
-       |@Callable(i) func write$t(k: String, v: ${if (t.contains("Int")) "Int" else t}) = [${t}Entry(k, v${if (t.equals("Binary")) ".toBytes" else ""})]
-       """.stripMargin
+  val stringTestData  = WriteEntry("String", "String", "somevalue")
+  val integerTestData = WriteEntry("Integer", "Int", 1)
+  val booleanTestData = WriteEntry("Boolean", "Boolean", true)
+  val binaryTestData  = WriteEntry("Binary", "ByteVector", "bytes")
 
-  val writeEntriesFunc = s"@Callable(i) func write%sEntries() = FOLD<%s>(a, [], writeEntry)"
-
-  def deleteEntriesFunc(c: Int) = s"@Callable(i) func delete${c}Entries() = FOLD<$c>(a, [], deleteEntry)"
+  def writeEntry(we: WriteEntry): String = s"@Callable(i) func write${we.ct}(k: String, v: ${we.t}) = [${we.ct}Entry(k, v)]"
 
   val script = """
                  |{-# STDLIB_VERSION 4 #-}
                  |{-# SCRIPT_TYPE ACCOUNT #-}
                  |{-# CONTENT_TYPE DAPP #-}
                  |
-                 |let a = [
-                 |  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-                 |  21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-                 |  41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
-                 |  61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
-                 |  81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99
+                 |let a100 = [
+                 |  "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
+                 |  "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40",
+                 |  "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60",
+                 |  "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80",
+                 |  "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99"
                  |]
+                 |
+                 |let a101 = a100 ++ ["100"]
                  |
                  |func writeEntry(acc: List[StringEntry], e: Int) = StringEntry(e.toString(), "value") :: acc
                  |
-                 |func deleteEntry(acc: List[DeleteEntry], e: Int) = DeleteEntry(e.toString()) :: acc
+                 |func deleteEntry(acc: List[DeleteEntry], e: String) = DeleteEntry(e) :: acc
+                 |
+                 |@Callable(i) func delete100Entries() = { FOLD<100>(a100, [], deleteEntry) }
+                 |
+                 |@Callable(i) func delete101Entries() = { FOLD<101>(a101, [], deleteEntry) }
                  |
                  |@Callable(i) func write(k: String, v: String) = [StringEntry(k, v)]
+                 |
+                 |@Callable(i) func write4() = [
+                 |   StringEntry("96", "1"),
+                 |   IntegerEntry("97", 1),
+                 |   BooleanEntry("98", true),
+                 |   BinaryEntry("99", "1".toBytes()),
+                 |   BinaryEntry("100", "1".toBytes())
+                 |]
                  |
                  |@Callable(i) func delete(k: String) = [DeleteEntry(k)]
                  |
        """.stripMargin
 
   "Remove entry from account storage" - {
-    val stringTestData  = "String"  -> "someValue"
-    val integerTestData = "Integer" -> 1
-    val booleanTestData = "Boolean" -> true
-    val binaryTestData  = "Binary"  -> ""
+    for (data <- Seq(stringTestData, integerTestData, booleanTestData, binaryTestData)) s"${data.ct}Entry could be removed from account storage" in {
+      val address = createDapp(script, writeEntry(data))
+      val v       = if (data.ct.equals("Binary")) ByteStr(data.v.toString.getBytes()) else data.v
 
-    for (data <- Seq(stringTestData, integerTestData, booleanTestData, binaryTestData)) s"${data._1} entry could be removed from account storage" in {
-      val t = data._1
-      val k = "someKey"
-      val v = data._2
-      val address =
-        createDapp(script, writeEntry(t, v.toString))
-
-      invokeScript(address, s"write$t", k, v.toString)
+      invokeScript(address, s"write${data.ct}", data.k, data.v.toString)
 
       miner.getData(address) should have size 1
-      miner.getDataByKey(address, k).key shouldBe k
-      miner.getDataByKey(address, k).value shouldBe v
-      miner.getDataByKey(address, k).getClass.getCanonicalName shouldBe s"com.wavesplatform.state.${t}DataEntry"
+      miner.getDataByKey(address, data.k).key shouldBe data.k
+      miner.getDataByKey(address, data.k).value shouldBe v
+      miner.getDataByKey(address, data.k).getClass.getCanonicalName shouldBe s"com.wavesplatform.state.${data.ct}DataEntry"
 
-      invokeScript(address, "delete", k)
+      invokeScript(address, "delete", data.k)
 
       miner.getData(address) should have size 0
     }
 
     "Removing nonexistent entry should not produce an error" in {
-      val address = createDapp(script)
-
-      invokeScript(address, "delete", "nonexistent-key")
+      invokeScript(createDapp(script), "delete", "nonexistent-key")
     }
 
     "Could remove 100 entries" in {
-      val address = createDapp(script, deleteEntriesFunc(100))
+      val address = createDapp(script)
 
-      miner.waitForTransaction((0 to 99).map(i => invokeScript(address, s"write", s"key-$i", "value", wait = false)).last)
-      miner.getData(address) should have size 100
+      invokeScript(address, s"write4")
 
-      miner.waitForTransaction(invokeScript(address, s"delete100Entries", wait = false))
+      val data = (0 to 92).map { i =>
+        StringDataEntry(s"$i", "q")
+      } ++ List(IntegerDataEntry("93", 1), BooleanDataEntry("94", true), BinaryDataEntry("95", ByteStr("1212".getBytes())))
 
-      miner.getData(address) should have size 0
+      miner.putData(address, data.toList, 1.waves, true)
+
+      miner.getData(address) should have size 101
+
+      miner.waitForTransaction(invokeScript(address, s"delete100Entries"))
+
+      miner.getData(address) should have size 1
     }
 
     "Removing more than 100 entries should produce an error" in {
-      val address = createDapp(script, deleteEntriesFunc(101))
+      val address = createDapp(script)
+      invokeScript(address, s"write4")
+      val data = (100 to 196).map { i =>
+        StringDataEntry(s"$i", "q")
+      }
 
-      miner.waitForTransaction((0 to 100).map(i => invokeScript(address, s"write", s"key-$i", "value", wait = false)).last)
+      miner.putData(address, data.toList, 1.waves, true)
       miner.getData(address) should have size 101
 
       assertBadRequestAndMessage(
-        miner.waitForTransaction(invokeScript(address, s"delete101Entries", wait = false)),
-        "Error while executing account-script: List size exceed 101"
+        miner.waitForTransaction(invokeScript(address, s"delete101Entries")),
+        "State check failed. Reason: WriteSet can't contain more than 100 entries"
       )
 
-      miner.getData(address) should have size 0
+      miner.getData(address) should have size 101
     }
 
     "Trying of writing key longer than 400 bytes and removing it should produce an error" in {
-      val address    = createDapp(script, deleteEntriesFunc(101))
+      val address    = createDapp(script)
       val tooLongKey = new scala.util.Random().nextString(401)
 
       assertBadRequestAndMessage(
-        miner.waitForTransaction(invokeScript(address, s"write", tooLongKey, "value", wait = false)),
+        miner.waitForTransaction(invokeScript(address, s"write", tooLongKey, "value")),
         "State check failed. Reason: Key size must be less than 100"
       )
     }
@@ -126,7 +142,7 @@ class RemoveEntrySuite extends BaseSuite {
 
     miner.transfer(sender.address, address, 10.waves, minFee, waitForTx = true)
 
-    nodes.waitForHeightAriseAndTxPresent(
+    nodes.waitForTransaction(
       miner
         .signedBroadcast(
           SetScriptTransaction
@@ -149,11 +165,11 @@ class RemoveEntrySuite extends BaseSuite {
       case "writeBoolean"     => List(CONST_STRING(key).explicitGet(), CONST_BOOLEAN(value.toBoolean))
       case "writeBinary"      => List(CONST_STRING(key).explicitGet(), CONST_BYTESTR(value.getBytes()).explicitGet)
       case "delete"           => List(CONST_STRING(key).explicitGet())
+      case "write4"           => List.empty
       case "delete100Entries" => List.empty
       case "delete101Entries" => List.empty
       case _                  => List.empty
     }
-
 
     val tx = miner
       .invokeScript(
@@ -167,7 +183,7 @@ class RemoveEntrySuite extends BaseSuite {
       ._1
       .id
 
-    if (wait) nodes.waitForHeightAriseAndTxPresent(tx)
+    if (wait) nodes.waitForTransaction(tx)
     tx
   }
 }
