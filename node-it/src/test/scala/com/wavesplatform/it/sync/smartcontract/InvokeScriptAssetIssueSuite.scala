b53d02aad19d32310db6d1a25000034567511ac7
Nikita Gadiyak <ngadiyak@gmail.com>
2020-03-19 15:11:42 +0300

    Delete InvokeScriptAssetIssueSuite.scala

--- node-it/src/test/scala/com/wavesplatform/it/sync/smartcontract/InvokeScriptAssetIssueSuite.scala
+++
@@ -1,6 +0,0 @@
-package com.wavesplatform.it.sync.smartcontract
-
-import com.wavesplatform.it.transactions.BaseTransactionSuite
-import org.scalatest.{CancelAfterFailure, Matchers, OptionValues}
-
-class InvokeScriptAssetIssueSuite extends BaseTransactionSuite with Matchers with CancelAfterFailure with OptionValues { }
