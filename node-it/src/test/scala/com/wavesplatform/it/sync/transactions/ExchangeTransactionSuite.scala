b9b3ff0274f64362df27a78d8fa0586ce8abe14a
Evgeniy Nichegovskiy <enichegovskiy@web3tech.ru>
2020-03-24 10:48:27 +0300

    NODE-2076 changed places of some buy\sell orders in suites, fix tx version in test

--- node-it/src/test/scala/com/wavesplatform/it/sync/transactions/ExchangeTransactionSuite.scala
+++ node-it/src/test/scala/com/wavesplatform/it/sync/transactions/ExchangeTransactionSuite.scala
@@ -67,7 +67,7 @@ class ExchangeTransactionSuite extends BaseTransactionSuite with NTPTime {
       if (exchangeVersion != 1) {
         val tx = ExchangeTransaction
           .signed(
-            TxVersion.V2,
+            TxVersion.V3,
             matcher = matcher,
             order1 = sell,
             order2 = buy,
@@ -182,7 +182,7 @@ class ExchangeTransactionSuite extends BaseTransactionSuite with NTPTime {
       val tx =
         ExchangeTransaction
           .signed(
-            2.toByte,
+            3.toByte,
             matcher = matcher,
             order1 = buy,
             order2 = sell,
