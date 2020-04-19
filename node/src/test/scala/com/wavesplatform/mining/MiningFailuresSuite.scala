5041c2eaa9464e7c153b555ecdc19aabca7c07f2
Artyom Sayadyan <xrtm000@gmail.com>
2020-04-19 15:40:50 +0300

    Mock continuations for MiningSuite

--- node/src/test/scala/com/wavesplatform/mining/MiningFailuresSuite.scala
+++ node/src/test/scala/com/wavesplatform/mining/MiningFailuresSuite.scala
@@ -76,6 +76,7 @@ class MiningFailuresSuite extends FlatSpec with Matchers with PrivateMethodTeste
     (blockchainUpdater.blockHeader _).when(*).returns(Some(SignedBlockHeader(genesis.header, genesis.signature)))
     (blockchainUpdater.activatedFeatures _).when().returning(Map.empty)
     (blockchainUpdater.approvedFeatures _).when().returning(Map.empty)
+    (blockchainUpdater.continuationStates _).when().returning(Map.empty)
     (blockchainUpdater.hitSource _).when(*).returns(Some(new Array[Byte](32)))
     (blockchainUpdater.bestLastBlockInfo _)
       .when(*)
