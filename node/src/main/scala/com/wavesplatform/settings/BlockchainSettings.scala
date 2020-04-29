accdad8bdbf979a3285958eef2e2e66728e1fcc2
Artyom Sayadyan <xrtm000@gmail.com>
2020-04-29 23:51:19 +0300

    Adapt config

--- node/src/main/scala/com/wavesplatform/settings/BlockchainSettings.scala
+++ node/src/main/scala/com/wavesplatform/settings/BlockchainSettings.scala
@@ -239,13 +239,14 @@ object BlockchainSettings {
         val rewardsSettings        = config.as[RewardsSettings](s"custom.rewards")
         (addressSchemeCharacter, functionalitySettings, genesisSettings, rewardsSettings)
     }
+    val useEvaluatorV2 = config.getAs[Boolean]("use-evaluator-v2").getOrElse(false)
 
     BlockchainSettings(
       addressSchemeCharacter = addressSchemeCharacter,
       functionalitySettings = functionalitySettings,
       genesisSettings = genesisSettings,
       rewardsSettings = rewardsSettings,
-      useEvaluatorV2 = false
+      useEvaluatorV2
     )
   }
 }
