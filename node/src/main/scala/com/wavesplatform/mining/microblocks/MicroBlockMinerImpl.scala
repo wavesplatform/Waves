f8108c759454eb53441ccd905b6320b53d9db3e3
Le Karasique <Karasiq@users.noreply.github.com>
2020-05-01 01:08:32 +0300

    NODE-1919: Block apply span metrics (#2929)
    * NODE-1919: Block apply span wip
    
    * NODE-1919: Block apply span wip
    
    * NODE-1919: Block apply span wip
    
    * Custom influxdb span reporter
    
    * NODE-1919: Block apply span
    
    * Fixes
    
    * Fixes
    
    * Fixes
    
    * Fixes
    
    * Fixes
    
    * Fixes
    
    * Fixes
    
    * Fixes
    
    * Remove prop-time
    
    * Refactoring
    
    * Option to specify InfluxDB retention policy
    
    * Enhanced span reporting
    
    * Fix warning
    
    * Build fix
    
    * NTP time in spans
    
    * Time in logs
    
    * Revert "Time in logs"
    
    This reverts commit 9f778a68
    
    * Update kamon

--- node/src/main/scala/com/wavesplatform/mining/microblocks/MicroBlockMinerImpl.scala
+++ node/src/main/scala/com/wavesplatform/mining/microblocks/MicroBlockMinerImpl.scala
@@ -18,7 +18,6 @@ import com.wavesplatform.utils.ScorexLogging
 import com.wavesplatform.utx.UtxPool
 import io.netty.channel.group.ChannelGroup
 import kamon.Kamon
-import kamon.metric.TimerMetric
 import monix.eval.Task
 import monix.execution.schedulers.SchedulerService
 
@@ -35,7 +34,7 @@ class MicroBlockMinerImpl(
 ) extends MicroBlockMiner
     with ScorexLogging {
 
-  val microBlockBuildTimeStats: TimerMetric = Kamon.timer("miner.forge-microblock-time")
+  val microBlockBuildTimeStats = Kamon.timer("miner.forge-microblock-time")
 
   def generateMicroBlockSequence(
       account: KeyPair,
