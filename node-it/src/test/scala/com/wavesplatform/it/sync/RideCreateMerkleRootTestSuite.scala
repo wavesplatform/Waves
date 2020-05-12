089504e38784365e3b5a7d400dc1238b157fe649
Mike Potanin <mpotanin@wavesplatform.com>
2020-05-12 23:10:40 +0300

    SC-558-fix Fix merging

--- node-it/src/test/scala/com/wavesplatform/it/sync/RideCreateMerkleRootTestSuite.scala
+++ node-it/src/test/scala/com/wavesplatform/it/sync/RideCreateMerkleRootTestSuite.scala
@@ -35,7 +35,6 @@ class RideCreateMerkleRootTestSuite
       .overrideBase(_.quorum(0))
       .overrideBase(_.preactivatedFeatures((14, 1000000), BlockchainFeatures.NG.id.toInt -> 0, BlockchainFeatures.FairPoS.id.toInt -> 0, BlockchainFeatures.Ride4DApps.id.toInt -> 0, BlockchainFeatures.BlockV5.id.toInt -> 0))
       .withDefault(1)
-      .withSpecial(1, _.nonMiner)
       .buildNonConflicting()
 
   private def sender: Node         = nodes.last
@@ -53,15 +52,15 @@ class RideCreateMerkleRootTestSuite
     val cscript = ScriptCompiler.compile(script, ScriptEstimatorV3).explicitGet()._1.bytes().base64
     val node = nodes.head
     nodes.waitForHeightArise()
-    val tx1 = node.broadcastTransfer(node.privateKey, sender.address, setScriptFee, minFee, None, None, version = TxVersion.V3, waitForTx = false)
+    val tx1 = node.broadcastTransfer(node.keyPair, sender.address, setScriptFee, minFee, None, None, version = TxVersion.V3, waitForTx = false)
     val txId1 = tx1.id
-    val tx2 = node.broadcastTransfer(node.privateKey, node.address, 1, minFee, None, None, version = TxVersion.V3, waitForTx = false)
+    val tx2 = node.broadcastTransfer(node.keyPair, node.address, 1, minFee, None, None, version = TxVersion.V3, waitForTx = false)
     val txId2 = tx2.id
-    val tx3 = node.broadcastTransfer(node.privateKey, node.address, 1, minFee, None, None, version = TxVersion.V3, waitForTx = false)
+    val tx3 = node.broadcastTransfer(node.keyPair, node.address, 1, minFee, None, None, version = TxVersion.V3, waitForTx = false)
     val txId3 = tx3.id
-    val tx4 = node.broadcastTransfer(node.privateKey, node.address, 1, minFee, None, None, version = TxVersion.V3, waitForTx = false)
+    val tx4 = node.broadcastTransfer(node.keyPair, node.address, 1, minFee, None, None, version = TxVersion.V3, waitForTx = false)
     val txId4 = tx4.id
-    val tx5 = node.broadcastTransfer(node.privateKey, node.address, 1, minFee, None, None, version = TxVersion.V3, waitForTx = false)
+    val tx5 = node.broadcastTransfer(node.keyPair, node.address, 1, minFee, None, None, version = TxVersion.V3, waitForTx = false)
     val txId5 = tx5.id
 
     val height = node.height
@@ -80,7 +79,7 @@ class RideCreateMerkleRootTestSuite
       tx.timestamp,
       Proofs(tx.proofs.get.map(v => ByteStr(Base58.decode(v))))
       ).explicitGet()
-    val natives = Seq(tx1, tx2, tx3, tx4, tx5).map(tt).map(t => Base58.encode(t.id()) -> t).toMap
+    val natives = Seq(tx1, tx2, tx3, tx4, tx5).map(tt).map(t => Base58.encode(t.id().arr) -> t).toMap
 
     val root = Base58.decode(node.blockAt(height).transactionsRoot.get)
 
@@ -93,14 +92,14 @@ class RideCreateMerkleRootTestSuite
         node.address,
         sender.address,
         func = Some("foo"),
-        args = List(ARR(p.merkleProof.map(v => CONST_BYTESTR(Base58.decode(v)).explicitGet()).toIndexedSeq, false).explicitGet(),
-                    CONST_BYTESTR(Merkle.hash(natives(p.id).bytes())).explicitGet(),
+        args = List(ARR(p.merkleProof.map(v => CONST_BYTESTR(ByteStr(Base58.decode(v))).explicitGet()).toIndexedSeq, false).explicitGet(),
+                    CONST_BYTESTR(ByteStr(Merkle.hash(natives(p.id).bytes()))).explicitGet(),
                     CONST_LONG(p.transactionIndex.toLong)),
         payment = Seq(),
         fee = 2*smartFee+minFee,
         waitForTx = true
       )
-      node.getDataByKey(sender.address, "root") shouldBe BinaryDataEntry("root", root)
+      node.getDataByKey(sender.address, "root") shouldBe BinaryDataEntry("root", ByteStr(root))
     }
   }
 }
