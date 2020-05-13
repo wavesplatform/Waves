729d394f64fd9587d09a86c6ad98b804df6ec971
Karasiq <yoba123@yandex.ru>
2020-05-13 20:24:37 +0300

    Remove unnecessary code

--- node/src/main/scala/com/wavesplatform/state/patch/PatchLoader.scala
+++ node/src/main/scala/com/wavesplatform/state/patch/PatchLoader.scala
@@ -5,25 +5,14 @@ import java.nio.file.{Files, Paths}
 
 import com.wavesplatform.account.AddressScheme
 import com.wavesplatform.utils.ScorexLogging
-import play.api.libs.json.{Json, Reads, Writes}
+import play.api.libs.json.{Json, Reads}
 
 object PatchLoader extends ScorexLogging {
   final case class Patch(name: String, chainId: Byte, height: Int) {
     private[PatchLoader] def fileName: String = s"$name-${chainId.toChar}-patch.json"
   }
 
-  lazy val patches: Seq[Patch] = {
-    def getResourceFolderFiles(folder: String) = {
-      val path = getResourcePath(folder)
-      new File(path).listFiles.toVector.map(_.getName)
-    }
-
-    val regex = "(\\w+)-(\\w)-(\\d+)-patch.json".r
-    getResourceFolderFiles("patches").collect {
-      case regex(name, chainId, height) =>
-        Patch(name, chainId.head.toByte, height.toInt)
-    }.filter(_.chainId == AddressScheme.current.chainId)
-  }
+  lazy val patches: Seq[Patch] = loadPatches()
 
   def isDiffPatch(name: String): Boolean =
     name != "DisableHijackedAliases"
@@ -34,12 +23,19 @@ object PatchLoader extends ScorexLogging {
     Json.parse(bytes).as[T]
   }
 
-  def write[T: Writes](name: String, height: Int, value: T): Unit = {
-    val chainId = AddressScheme.current.chainId.toChar
-    val path    = Paths.get(sys.props("user.home"), s"$name-$chainId-$height-patch.json")
-    val json    = Json.prettyPrint(Json.toJson(value))
-    log.info(s"Patch $name at height $height saved to $path")
-    Files.write(path, json.getBytes("UTF-8"))
+  private[this] def loadPatches() = {
+    def getResourceFolderFiles(folder: String) = {
+      val path = getResourcePath(folder)
+      new File(path).listFiles.toVector.map(_.getName)
+    }
+
+    val regex = "(\\w+)-(\\w)-(\\d+)-patch.json".r
+    getResourceFolderFiles("patches")
+      .collect {
+        case regex(name, chainId, height) =>
+          Patch(name, chainId.head.toByte, height.toInt)
+      }
+      .filter(_.chainId == AddressScheme.current.chainId)
   }
 
   private[this] def getResourcePath(path: String) = {
