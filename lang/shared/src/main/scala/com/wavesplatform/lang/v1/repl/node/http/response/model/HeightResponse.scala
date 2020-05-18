b1e1b2821cc926858c4f6a4c747a9188d6dc6537
Mike Potanin <mpotanin@wavesplatform.com>
2020-05-18 14:29:18 +0300

    SC-584-WebEnvironment Check applicationStatus in transactionHeightById

--- lang/shared/src/main/scala/com/wavesplatform/lang/v1/repl/node/http/response/model/HeightResponse.scala
+++ lang/shared/src/main/scala/com/wavesplatform/lang/v1/repl/node/http/response/model/HeightResponse.scala
@@ -1,3 +1,15 @@
 package com.wavesplatform.lang.v1.repl.node.http.response.model
 
+import io.circe.{Decoder, DecodingFailure, HCursor}
+
 private[node] case class HeightResponse(height: Long)
+
+object HeightResponse {
+  implicit val decoder: Decoder[HeightResponse] = (c: HCursor) =>
+      for {
+        applicationStatus <- c.downField("applicationStatus").as[Option[String]]
+        succeed = applicationStatus.fold(true)(_ == "succeed")
+        _ <- Either.cond(succeed, (), DecodingFailure.apply("Failed transaction", List()))
+        h <- c.downField("height").as[Int]
+      } yield HeightResponse(h)
+}
