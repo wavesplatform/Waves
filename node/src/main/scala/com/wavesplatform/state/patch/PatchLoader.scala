35e1c328f7c11ff75df0ff6a18b12baee78111d1
Karasiq <yoba123@yandex.ru>
2020-05-22 11:58:17 +0300

    Refactoring

--- node/src/main/scala/com/wavesplatform/state/patch/PatchLoader.scala
+++ node/src/main/scala/com/wavesplatform/state/patch/PatchLoader.scala
@@ -1,22 +1,12 @@
 package com.wavesplatform.state.patch
 
-import java.io.File
-import java.nio.file.Files
-
 import com.wavesplatform.account.AddressScheme
 import com.wavesplatform.utils.ScorexLogging
 import play.api.libs.json.{Json, Reads}
 
 object PatchLoader extends ScorexLogging {
   def read[T: Reads](name: AnyRef): T = {
-    val path  = getResourcePath(s"patches/$name-${AddressScheme.current.chainId.toChar}.json")
-    val bytes = Files.readAllBytes(path.toPath)
-    Json.parse(bytes).as[T]
-  }
-
-  private[this] def getResourcePath(path: String) = {
-    val loader = Thread.currentThread.getContextClassLoader
-    val url    = loader.getResource(path)
-    new File(url.toURI)
+    val inputStream = getClass.getResourceAsStream(s"patches/$name-${AddressScheme.current.chainId.toChar}.json")
+    Json.parse(inputStream).as[T]
   }
 }
