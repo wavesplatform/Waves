76139c949f07a36cc0760d549534ec1d35293b09
Karasiq <yoba123@yandex.ru>
2020-05-22 13:46:09 +0300

    Fix loading

--- node/src/main/scala/com/wavesplatform/state/patch/PatchLoader.scala
+++ node/src/main/scala/com/wavesplatform/state/patch/PatchLoader.scala
@@ -6,7 +6,7 @@ import play.api.libs.json.{Json, Reads}
 
 object PatchLoader extends ScorexLogging {
   def read[T: Reads](name: AnyRef): T = {
-    val inputStream = getClass.getResourceAsStream(s"patches/$name-${AddressScheme.current.chainId.toChar}.json")
+    val inputStream = Thread.currentThread().getContextClassLoader.getResourceAsStream(s"patches/$name-${AddressScheme.current.chainId.toChar}.json")
     Json.parse(inputStream).as[T]
   }
 }
