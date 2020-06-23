6f48a38533aca7ba56d00f59dcceb46845365d6f
Karasiq <yoba123@yandex.ru>
2020-06-23 12:21:18 +0300

    NODE-1926: Remove unused API errors

--- node/src/main/scala/com/wavesplatform/api/http/ApiError.scala
+++ node/src/main/scala/com/wavesplatform/api/http/ApiError.scala
@@ -69,7 +69,10 @@ object ApiError {
     override val message = "Error is unknown"
   }
 
-  final case class WrongJson(cause: Option[Throwable] = None, errors: scala.collection.Seq[(JsPath, scala.collection.Seq[JsonValidationError])] = Seq.empty) extends ApiError {
+  final case class WrongJson(
+      cause: Option[Throwable] = None,
+      errors: scala.collection.Seq[(JsPath, scala.collection.Seq[JsonValidationError])] = Seq.empty
+  ) extends ApiError {
     override val id              = WrongJson.Id
     override val code            = StatusCodes.BadRequest
     override val message: String = WrongJson.Message
@@ -92,12 +95,6 @@ object ApiError {
     override val message: String = "Provided API key is not correct"
   }
 
-  case object DiscontinuedApi extends ApiError {
-    override val id      = 3
-    override val code    = StatusCodes.BadRequest
-    override val message = "This API is no longer supported"
-  }
-
   case object TooBigArrayAllocation extends ApiError {
     override val id: Int          = 10
     override val message: String  = "Too big sequences requested"
@@ -117,46 +114,10 @@ object ApiError {
     override val message = "invalid address"
   }
 
-  case object InvalidSeed extends ApiError {
-    override val id      = 103
-    override val code    = StatusCodes.BadRequest
-    override val message = "invalid seed"
-  }
-
-  case object InvalidAmount extends ApiError {
-    override val id      = 104
-    override val code    = StatusCodes.BadRequest
-    override val message = "invalid amount"
-  }
-
-  case object InvalidFee extends ApiError {
-    override val id      = 105
-    override val code    = StatusCodes.BadRequest
-    override val message = "invalid fee"
-  }
-
-  case object InvalidSender extends ApiError {
-    override val id      = 106
-    override val code    = StatusCodes.BadRequest
-    override val message = "invalid sender"
-  }
-
-  case object InvalidRecipient extends ApiError {
-    override val id      = 107
-    override val code    = StatusCodes.BadRequest
-    override val message = "invalid recipient"
-  }
-
   case object InvalidPublicKey extends ApiError {
     override val id      = 108
     override val code    = StatusCodes.BadRequest
     override val message = "invalid public key"
-  }
-
-  case object InvalidNotNumber extends ApiError {
-    override val id      = 109
-    override val code    = StatusCodes.BadRequest
-    override val message = "argument is not a number"
   }
 
   case object InvalidMessage extends ApiError {
@@ -312,46 +273,6 @@ object ApiError {
     val Code    = StatusCodes.BadRequest
   }
 
-  final case class SignatureError(error: String) extends ApiError {
-    override val id: Int          = SignatureError.Id
-    override val code: StatusCode = StatusCodes.InternalServerError
-    override val message: String  = s"Signature error: $error"
-  }
-
-  case object SignatureError {
-    val Id = 309
-  }
-
-  case object WalletNotExist extends ApiError {
-    override val id: Int          = 201
-    override val message: String  = "wallet does not exist"
-    override val code: StatusCode = StatusCodes.NotFound
-  }
-
-  case object WalletAddressDoesNotExist extends ApiError {
-    override val id: Int          = 202
-    override val message: String  = "private key for the public key does not exist in wallet"
-    override val code: StatusCode = StatusCodes.NotFound
-  }
-
-  case object WalletLocked extends ApiError {
-    override val id: Int          = 203
-    override val message: String  = "wallet is locked"
-    override val code: StatusCode = StatusCodes.UnprocessableEntity
-  }
-
-  case object WalletAlreadyExists extends ApiError {
-    override val id: Int          = 204
-    override val message: String  = "wallet already exists"
-    override val code: StatusCode = StatusCodes.Conflict
-  }
-
-  case object WalletSeedExportFailed extends ApiError {
-    override val id: Int          = 205
-    override val message: String  = "seed exporting failed"
-    override val code: StatusCode = StatusCodes.InternalServerError
-  }
-
   //TRANSACTIONS
   case object TransactionDoesNotExist extends ApiError {
     override val id: Int          = 311
@@ -363,12 +284,6 @@ object ApiError {
     override val id: Int         = 312
     override val code            = StatusCodes.NotImplemented
     override val message: String = "transaction type not supported"
-  }
-
-  case object NoBalance extends ApiError {
-    override val id: Int          = 313
-    override val message: String  = "not enough balance"
-    override val code: StatusCode = StatusCodes.BadRequest
   }
 
   final case class NegativeAmount(msg: String) extends ApiError {
@@ -390,18 +305,7 @@ object ApiError {
     val Id = 112
   }
 
-  final case class WrongTransactionJson(err: JsError) extends ApiError {
-    override val id: Int = WrongTransactionJson.Id
-    override val message: String =
-      err.errors.map(e => s"Validation failed for field '${e._1}', errors:${e._2}. ").mkString("\n")
-    override val code: StatusCode = StatusCodes.UnprocessableEntity
-  }
-
-  case object WrongTransactionJson {
-    val Id = 113
-  }
-
-  final case class NegativeMinFee(msg: String) extends ApiError {
+  final case class NegativeMinFee(msg: String) extends ApiError { // not tested
     override val id: Int          = NegativeMinFee.Id
     override val message: String  = s"negative fee per: $msg"
     override val code: StatusCode = StatusCodes.BadRequest
@@ -475,12 +379,6 @@ object ApiError {
         "message"     -> message,
         "transaction" -> tx.json()
       )
-  }
-
-  case object InvalidBase58 extends ApiError {
-    override val id      = 406
-    override val message = "Invalid Base58 string"
-    override val code    = StatusCodes.BadRequest
   }
 
   case class InvalidTransactionId(message: String) extends ApiError {
