377d85d69164c0f8fba62c254245750661eb4592
Artyom Sayadyan <xrtm000@gmail.com>
2020-07-02 17:24:40 +0300

    SC Filter internal declarations for JS API

--- lang/js/src/main/scala/JsAPI.scala
+++ lang/js/src/main/scala/JsAPI.scala
@@ -56,6 +56,7 @@ object JsAPI {
   @JSExportTopLevel("getVarsDoc")
   def getVarsDoc(ver: Int = 2, isTokenContext: Boolean = false, isContract: Boolean = false): js.Array[js.Object with js.Dynamic] =
     buildScriptContext(DirectiveDictionary[StdLibVersion].idMap(ver), isTokenContext, isContract).vars
+      .filterNot(_._1.startsWith("_"))
       .map(
         v =>
           js.Dynamic.literal(
@@ -69,6 +70,7 @@ object JsAPI {
   @JSExportTopLevel("getFunctionsDoc")
   def getFunctionsDoc(ver: Int = 2, isTokenContext: Boolean = false, isContract: Boolean = false): js.Array[js.Object with js.Dynamic] =
     buildScriptContext(DirectiveDictionary[StdLibVersion].idMap(ver), isTokenContext, isContract).functions
+      .filterNot(_.name.startsWith("_"))
       .map(f => {
         val (funcDoc, paramsDoc) = DocSource.funcData((f.name, f.signature.args.map(_._2.toString).toList, ver))
         js.Dynamic.literal(
