8660d6b29280d838f8f470f73009efd2c20548a3
Mike Potanin <mpotanin@gmail.com>
2020-07-08 12:00:04 +0300

    SC-430-limit-of-FOLD-iterations (#3169)
    * SC-430-limit-of-FOLD-iterations
    
    * SC-430-limit-of-FOLD-iterations
    Fix warning
    
    Co-authored-by: Mike Potanin <mpotanin@wavesplatform.com>

--- lang/tests/src/test/scala/com/wavesplatform/lang/v1/FoldTest.scala
+++ lang/tests/src/test/scala/com/wavesplatform/lang/v1/FoldTest.scala
@@ -69,6 +69,34 @@ class FoldTest extends PropSpec with PropertyChecks with Matchers with NoShrink 
     eval(script) shouldBe Left("List size exceed 4")
   }
 
+  property("limit for limit") {
+    val script =
+      s"""
+         | func sum(a:Int, b:Int) = a + b
+         | let arr = [1, 2, 3, 4, 5]
+         | let total = FOLD<1001>(arr, 9, sum)
+         | total
+         |
+      """.stripMargin
+
+    val index = script.indexOf("1001")
+    eval(script) shouldBe Left(s"Compilation failed: [List size limit in FOLD is oversized, 1001 must be less or equal 1000 in $index-${index+4}]")
+  }
+
+  property("Maximun limit") {
+    val script =
+      s"""
+         | func sum(a:Int, b:Int) = a + b
+         | let arr = [${"1,"*999}1]
+         | let total = FOLD<1000>(arr, 2, sum)
+         | total
+         |
+      """.stripMargin
+
+    eval(script) shouldBe Right(CONST_LONG(1002L))
+  }
+
+
   property("FOLD as FOLD param") {
     val script =
       s"""
