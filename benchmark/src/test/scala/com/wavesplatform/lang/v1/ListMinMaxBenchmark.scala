8aaeb5bec4d93986035e2e8a217636d4ed49fc1c
Artyom Sayadyan <xrtm000@gmail.com>
2020-07-08 18:41:34 +0300

    SC Clean up SigVerify benchmarks (#3179)

--- benchmark/src/test/scala/com/wavesplatform/lang/v1/ListMinMaxBenchmark.scala
+++ benchmark/src/test/scala/com/wavesplatform/lang/v1/ListMinMaxBenchmark.scala
@@ -2,13 +2,10 @@ package com.wavesplatform.lang.v1
 
 import java.util.concurrent.TimeUnit
 
-import com.wavesplatform.lang.v1.EnvironmentFunctionsBenchmark.{curve25519, randomBytes}
-import com.wavesplatform.lang.v1.ListIndexOfBenchmark.CurveSt32k
 import com.wavesplatform.lang.v1.ListMinMaxBenchmark.ListMinMaxSt
 import com.wavesplatform.lang.v1.evaluator.ctx.impl.PureContext
 import org.openjdk.jmh.annotations._
 import org.openjdk.jmh.infra.Blackhole
-import scorex.crypto.signatures.{Curve25519, Signature}
 
 @OutputTimeUnit(TimeUnit.MICROSECONDS)
 @BenchmarkMode(Array(Mode.AverageTime))
@@ -24,23 +21,12 @@ class ListMinMaxBenchmark {
   @Benchmark
   def min(st: ListMinMaxSt, bh: Blackhole): Unit =
     bh.consume(st.list.min)
-
-  @Benchmark
-  def sigVerify32Kb(st: CurveSt32k, bh: Blackhole): Unit =
-    bh.consume(Curve25519.verify(Signature @@ st.signature, st.message, st.publicKey))
 }
 
 object ListMinMaxBenchmark {
   @State(Scope.Benchmark)
   class ListMinMaxSt {
     val list = (Long.MinValue to Long.MinValue + PureContext.MaxListLengthV4).toVector
-  }
-
-  @State(Scope.Benchmark)
-  class CurveSt32k {
-    val (privateKey, publicKey) = curve25519.generateKeypair
-    val message                 = randomBytes(32 * 1024)
-    val signature               = curve25519.sign(privateKey, message)
   }
 }
 
