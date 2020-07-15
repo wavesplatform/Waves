848f3a1482212f3941a7fdc5fd37c7b5a2d23f7a
Dmitry Shuranov <dvshur@gmail.com>
2020-07-15 13:26:50 +0300

    added blockchainUpdateTriggers to extension interface

--- node/src/main/scala/com/wavesplatform/extensions/Extension.scala
+++ node/src/main/scala/com/wavesplatform/extensions/Extension.scala
@@ -1,8 +1,11 @@
 package com.wavesplatform.extensions
 
+import com.wavesplatform.events.BlockchainUpdateTriggers
+
 import scala.concurrent.Future
 
 trait Extension {
   def start(): Unit
   def shutdown(): Future[Unit]
+  def blockchainUpdateTriggers: BlockchainUpdateTriggers = BlockchainUpdateTriggers.noop
 }
