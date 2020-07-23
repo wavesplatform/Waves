b09ecc8188bc876023a6b68907b5c6a77ab2c979
Dmitry Shuranov <dvshur@gmail.com>
2020-07-23 14:04:22 +0300

    observable from leveldb iterator

--- blockchain-updates/src/main/scala/com/wavesplatform/events/repo/UpdatesRepoImpl.scala
+++ blockchain-updates/src/main/scala/com/wavesplatform/events/repo/UpdatesRepoImpl.scala
@@ -9,19 +9,21 @@ import com.wavesplatform.events.{BlockAppended, BlockchainUpdated, MicroBlockApp
 import com.wavesplatform.utils.ScorexLogging
 import monix.eval.Task
 import monix.execution.{Ack, Cancelable, Scheduler}
+import monix.reactive.OverflowStrategy.BackPressure
 import monix.reactive.observers.Subscriber
 import monix.reactive.{Observable, OverflowStrategy}
 import monix.reactive.subjects.ConcurrentSubject
 import org.iq80.leveldb.DB
 
 import scala.annotation.tailrec
-import scala.concurrent.Future
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration.Duration
 import scala.util.{Failure, Success, Try}
 
 class UpdatesRepoImpl(db: DB, currentUpdates: Observable[BlockchainUpdated])(implicit val scheduler: Scheduler)
     extends UpdatesRepo
     with ScorexLogging {
-  import UpdatesRepoImpl.blockKey
+  import UpdatesRepoImpl._
 
   // no need to persist liquid state. Keeping it mutable in a class
   private[this] var liquidState: Option[LiquidState] = None
@@ -92,80 +94,109 @@ class UpdatesRepoImpl(db: DB, currentUpdates: Observable[BlockchainUpdated])(imp
     }
   }
 
-  override def getRange(from: Int, to: Int): Try[Seq[BlockAppended]] = Try {
-    val iterator = db.iterator()
-    iterator.seek(blockKey(from))
-
-    var results = Seq.newBuilder[BlockAppended]
-    results.sizeHint(to - from + 1)
-
-    @tailrec
-    def go(remaining: Int): Unit = {
-      if (remaining > 0) {
-        val blockBytes = iterator.next().getValue
-        if (blockBytes.nonEmpty) {
-          PBBlockchainUpdated.parseFrom(blockBytes).vanilla.get match {
-            case b: BlockAppended => results += b
-            case _                => ()
-          }
-          if (iterator.hasNext) go(remaining - 1)
-        }
-      }
-    }
-
-    go(to - from + 1)
-
-    iterator.close()
-    results.result()
+  override def getRange(from: Int, to: Int): Try[Seq[BlockAppended]] = {
+    // todo error handling, limits and timeouts
+    log.info("Requesting stream")
+    Try(Await.result(streamRangeOneIter(from, to).bufferTumbling(to - from + 1).runAsyncGetLast, Duration.Inf).get)
   }
 
-  val BATCH_SIZE = 10
+  private[repo] def streamRangeOneIter(from: Int, to: Int): Observable[BlockAppended] = {
+    Observable.create(OverflowStrategy.Unbounded) { sub =>
+      val iterator = db.iterator()
+      iterator.seek(blockKey(from))
 
-  override def stream(from: Int): Observable[BlockchainUpdated] = {
-    val subject = ConcurrentSubject.publish[BlockchainUpdated]
+      var cancelled = false
+      val cancel = new Cancelable {
+        override def cancel(): Unit = {
+          cancelled = true
+          sub.onComplete()
+        }
+      }
 
-    // todo async requests in Tasks
-    Task
-      .evalAsync {
-        def sendHistorical(from: Int): Unit = {
-          var batchStart = from
-          while (true) {
-            getRange(batchStart, batchStart + BATCH_SIZE - 1) match {
-              case Success(batch) =>
-                log.info(s"Requested batch from ${batch.head.toHeight} to ${batch.last.toHeight}")
-                for (ba <- batch) {
-                  val ack = subject.onNext(ba)
-                  ack match {
-                    case Ack.Continue => ()
-                    case Ack.Stop     => return
-                  }
+      @tailrec
+      def go(remaining: Int): Unit = {
+        if (remaining > 0 && iterator.hasNext && !cancelled) {
+          val next       = iterator.next()
+          val blockBytes = next.getValue
+          if (blockBytes.nonEmpty) {
+            PBBlockchainUpdated.parseFrom(blockBytes).vanilla match {
+              case Success(value) =>
+                value match {
+                  case b: BlockAppended =>
+                    sub.onNext(b) match {
+                      case Ack.Continue => go(remaining - 1)
+                      case Ack.Stop     => sub.onComplete()
+                    }
+                  case _ => sub.onError(new IllegalStateException(s"Incorrect type of update in the database at height ${fromBlockKey(next.getKey)}"))
                 }
-                if (batch.length < BATCH_SIZE) return
-                batchStart += BATCH_SIZE
-              case Failure(exception) =>
-                log.error("Failed to get range, [$from, ${from + BATCH_SIZE - 1}]")
-                subject.onError(exception)
-                return
+              case Failure(exception) => sub.onError(exception)
             }
+          } else {
+            sub.onError(new IllegalStateException(s"Empty block bytes in the database at height ${fromBlockKey(next.getKey)}"))
           }
         }
-
-        sendHistorical(from)
-
-        // subscribe to current events and put them into buffer, at the same time
-
-        log.info(s"Historical events sent")
       }
-      .flatMap { _ =>
-        log.info(s"Subscribing stream to current events")
-        // todo will concurrent subscriptions work?
-        // will they be cancelled correctly?
-        Task.evalAsync(currentUpdates.subscribe(subject))
-      }
-      .runAsyncAndForget
 
-    subject
+      go(to - from + 1)
 
+      iterator.close()
+      sub.onComplete()
+
+      cancel
+    }
+  }
+
+  val BATCH_SIZE                = 10
+  val BACK_PRESSURE_BUFFER_SIZE = 1000
+
+  // I think the stream should be back-pressured
+  // @todo (maybe) parameterize strategy or at least buffer size
+  override def stream(from: Int): Observable[BlockchainUpdated] = {
+    Observable.create(OverflowStrategy.Unbounded) { sub =>
+      currentUpdates.subscribe(sub)
+    }
+
+//    val subject = ConcurrentSubject.publish[BlockchainUpdated]
+
+//    todo async requests in Tasks
+//    Task
+//      .evalAsync {
+//        def sendHistorical(from: Int): Unit = {
+//          var batchStart = from
+//          while (true) {
+//            getRange(batchStart, batchStart + BATCH_SIZE - 1) match {
+//              case Success(batch) =>
+//                log.info(s"Requested batch from ${batch.head.toHeight} to ${batch.last.toHeight}")
+//                for (ba <- batch) {
+//                  val ack = subject.onNext(ba)
+//                  ack match {
+//                    case Ack.Continue => ()
+//                    case Ack.Stop     => return
+//                  }
+//                }
+//                if (batch.length < BATCH_SIZE) return
+//                batchStart += BATCH_SIZE
+//              case Failure(exception) =>
+//                log.error("Failed to get range, [$from, ${from + BATCH_SIZE - 1}]")
+//                subject.onError(exception)
+//                return
+//            }
+//          }
+//        }
+
+//        sendHistorical(from)
+
+    // subscribe to current events and put them into buffer, at the same time
+//
+//        log.info(s"Historical events sent")
+//      }
+//      .flatMap { _ =>
+//        log.info(s"Subscribing stream to current events")
+//        // todo will concurrent subscriptions work?
+//        // will they be cancelled correctly?
+//        Task.evalAsync(currentUpdates.subscribe(subject))
+//      }
+//      .runAsyncAndForget
 //    @tailrec
 //    def producerLoop(sub: Subscriber[BlockchainUpdated], from: Option[Int]): Cancelable /* Task[Unit] */ =
 //      def send(upd: BlockchainUpdated): Task[Ack] = ???
@@ -201,4 +232,6 @@ class UpdatesRepoImpl(db: DB, currentUpdates: Observable[BlockchainUpdated])(imp
 
 object UpdatesRepoImpl {
   private def blockKey(height: Int): Array[Byte] = ByteBuffer.allocate(8).putInt(height).array()
+
+  private def fromBlockKey(key: Array[Byte]): Int = ByteBuffer.wrap(key).getInt
 }
