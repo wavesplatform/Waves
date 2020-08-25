6e7aea9ad7176bd38856c75ee8348497dc192a3a
Artyom Sayadyan <xrtm000@gmail.com>
2020-08-25 09:20:15 +0300

    Add RIDE V5 context

--- lang/shared/src/main/scala/com/wavesplatform/lang/directives/values/StdLibVersion.scala
+++ lang/shared/src/main/scala/com/wavesplatform/lang/directives/values/StdLibVersion.scala
@@ -11,10 +11,11 @@ case object V1 extends StdLibVersion(1)
 case object V2 extends StdLibVersion(2)
 case object V3 extends StdLibVersion(3)
 case object V4 extends StdLibVersion(4)
+case object V5 extends StdLibVersion(5)
 
 object StdLibVersion {
     implicit object VersionDic extends DirectiveDictionary[StdLibVersion] {
       override val default: StdLibVersion           = V3
-      override val all:     Iterable[StdLibVersion] = Seq(V1, V2, V3, V4)
+      override val all:     Iterable[StdLibVersion] = Seq(V1, V2, V3, V4, V5)
     }
 }
