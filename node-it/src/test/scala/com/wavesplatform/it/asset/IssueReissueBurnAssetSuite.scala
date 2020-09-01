ec1920a99acf3e7976458e622d7ae15d7028633a
Karasiq <yoba123@yandex.ru>
2020-09-01 19:07:31 +0300

    Tests fixes

--- node-it/src/test/scala/com/wavesplatform/it/asset/IssueReissueBurnAssetSuite.scala
+++ node-it/src/test/scala/com/wavesplatform/it/asset/IssueReissueBurnAssetSuite.scala
@@ -357,7 +357,7 @@ class IssueReissueBurnAssetSuite extends BaseSuite {
 
     miner.transfer(sender.keyPair, address.toAddress.toString, initialWavesBalance, setScriptFee * 2, waitForTx = true)
 
-    nodes.waitForTransaction(
+    nodes.waitForHeightAriseAndTxPresent(
       miner
         .signedBroadcast(
           SetScriptTransaction
