26a1a9509f62244c2db02d4327370760c7c39692
Anvar Kiekbaev <a.kiekbaev@chepiov.org>
2020-09-23 11:57:17 +0300

    NODE-2228: Pluggable bloom filters (#3261)

--- node/src/main/scala/com/wavesplatform/settings/DBSettings.scala
+++ node/src/main/scala/com/wavesplatform/settings/DBSettings.scala
@@ -1,9 +1,12 @@
 package com.wavesplatform.settings
 import scala.concurrent.duration.FiniteDuration
 
-case class DBSettings(directory: String,
-                      storeTransactionsByAddress: Boolean,
-                      storeInvokeScriptResults: Boolean,
-                      maxCacheSize: Int,
-                      maxRollbackDepth: Int,
-                      rememberBlocks: FiniteDuration)
+case class DBSettings(
+    directory: String,
+    storeTransactionsByAddress: Boolean,
+    storeInvokeScriptResults: Boolean,
+    maxCacheSize: Int,
+    maxRollbackDepth: Int,
+    rememberBlocks: FiniteDuration,
+    useBloomFilter: Boolean
+)
