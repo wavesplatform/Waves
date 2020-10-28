2b06fae19ec407164e48ce596bbb19a0c63c117e
Artyom Sayadyan <xrtm000@gmail.com>
2020-10-28 14:32:28 +0300

    Improve test

--- node/src/test/scala/com/wavesplatform/state/diffs/smart/predef/ScriptVersionsTest.scala
+++ node/src/test/scala/com/wavesplatform/state/diffs/smart/predef/ScriptVersionsTest.scala
@@ -15,7 +15,7 @@ import com.wavesplatform.lang.v1.compiler.Terms.EVALUATED
 import com.wavesplatform.lang.v1.estimator.v2.ScriptEstimatorV2
 import com.wavesplatform.lang.v1.parser.Parser
 import com.wavesplatform.state.diffs._
-import com.wavesplatform.state.{BinaryDataEntry, Blockchain, BooleanDataEntry, IntegerDataEntry, StringDataEntry}
+import com.wavesplatform.state.{BinaryDataEntry, Blockchain, BooleanDataEntry, EmptyDataEntry, IntegerDataEntry, StringDataEntry}
 import com.wavesplatform.transaction.Transaction
 import com.wavesplatform.transaction.smart.script.{ScriptCompiler, ScriptRunner}
 import com.wavesplatform.utils.EmptyBlockchain
@@ -164,15 +164,16 @@ class ScriptVersionsTest extends FreeSpec with PropertyChecks with Matchers with
           StringDataEntry("key", "value"),
           IntegerDataEntry("key", 1),
           BinaryDataEntry("key", ByteStr.decodeBase58("aaaa").get),
-          BooleanDataEntry("key", true)
+          BooleanDataEntry("key", true),
+          EmptyDataEntry("key")
         )
       } {
-        val tx = dataTransactionGen.sample.get.copy(data = Seq(entry))
+        val tx = dataTransactionGen(1, withDeleteEntry = true).sample.get.copy(data = Seq(entry))
         val blockchain = if (activateFix) fixedBlockchain else EmptyBlockchain
         if (version >= V4) {
-          if (!activateFix && isDApp)
+          if (!activateFix && isDApp && !entry.isInstanceOf[EmptyDataEntry]){
             eval(script(isDApp, version), tx, blockchain) should produce("Match error")
-          else
+          } else
             eval(script(isDApp, version), tx, blockchain) shouldBe Testing.evaluated(true)
         } else
           (the[RuntimeException] thrownBy script(isDApp, version)).getMessage should include("Undefined type")
