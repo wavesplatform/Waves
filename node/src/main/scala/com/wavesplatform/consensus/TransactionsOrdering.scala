f7f1696a2ab0dd18850e7a9246b2c256960e32cd
Artyom Sayadyan <xrtm000@gmail.com>
2020-11-16 16:40:19 +0300

    Prioritize Continuation as Invoke

--- node/src/main/scala/com/wavesplatform/consensus/TransactionsOrdering.scala
+++ node/src/main/scala/com/wavesplatform/consensus/TransactionsOrdering.scala
@@ -2,26 +2,33 @@ package com.wavesplatform.consensus
 
 import com.wavesplatform.state.Blockchain
 import com.wavesplatform.transaction.Asset.Waves
-import com.wavesplatform.transaction.smart.InvokeScriptTransaction
+import com.wavesplatform.transaction.smart.{ContinuationTransaction, InvokeScriptTransaction}
 import com.wavesplatform.transaction.{Authorized, Transaction}
+
+import scala.annotation.tailrec
 
 object TransactionsOrdering {
   trait WavesOrdering extends Ordering[Transaction] {
+    val blockchain: Blockchain
     def isWhitelisted(t: Transaction): Boolean = false
     def txTimestampOrder(ts: Long): Long
     private def orderBy(t: Transaction): (Boolean, Double, Long, Long) = {
       val byWhiteList = !isWhitelisted(t) // false < true
       val size        = t.bytes().length
       val commonFee   = if (t.assetFee._1 != Waves) 0 else -t.assetFee._2
-      val extraFee = t match {
-        case i: InvokeScriptTransaction if i.assetFee._1 == Waves => -i.extraFeePerStep
-        case _                                                    => 0
-      }
-      val byFee       = commonFee + extraFee
+      val byFee       = commonFee + extraFee(t)
       val byTimestamp = txTimestampOrder(t.timestamp)
 
       (byWhiteList, byFee.toDouble / size.toDouble, byFee, byTimestamp)
     }
+
+    @tailrec private def extraFee(t: Transaction): Long =
+      t match {
+        case i: InvokeScriptTransaction if i.assetFee._1 == Waves => -i.extraFeePerStep
+        case i: ContinuationTransaction                           => extraFee(i.resolveInvoke(blockchain)._2)
+        case _                                                    => 0
+      }
+
     override def compare(first: Transaction, second: Transaction): Int = {
       import Ordering.Double.TotalOrdering
       implicitly[Ordering[(Boolean, Double, Long, Long)]].compare(orderBy(first), orderBy(second))
@@ -31,6 +38,7 @@ object TransactionsOrdering {
   object InBlock extends WavesOrdering {
     // sorting from network start
     override def txTimestampOrder(ts: Long): Long = -ts
+    override val blockchain: Blockchain           = ???
   }
 
   case class InUTXPool(whitelistAddresses: Set[String], blockchain: Blockchain) extends WavesOrdering {
