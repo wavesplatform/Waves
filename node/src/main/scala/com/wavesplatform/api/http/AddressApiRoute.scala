7da921aaa5a9e4e5844e6b13c4f384df8c0d7348
Karasiq <yoba123@yandex.ru>
2020-12-02 15:22:15 +0300

    Change error messages for TooBigArrayAllocation

--- node/src/main/scala/com/wavesplatform/api/http/AddressApiRoute.scala
+++ node/src/main/scala/com/wavesplatform/api/http/AddressApiRoute.scala
@@ -205,9 +205,9 @@ case class AddressApiRoute(
   def seq: Route = {
     (path("seq" / IntNumber / IntNumber) & get) {
       case (start, end) =>
-        if (start >= 0 && end >= 0 && start <= end && end - start < MaxAddressesPerRequest) {
-          complete(wallet.privateKeyAccounts.map(_.toAddress).slice(start, end))
-        } else complete(TooBigArrayAllocation)
+        if (start < 0 || end < 0 || start > end) complete(GenericError("Invalid sequence"))
+        else if (end - start > MaxAddressesPerRequest) complete(TooBigArrayAllocation(MaxAddressesPerRequest))
+        else complete(wallet.privateKeyAccounts.map(_.toAddress).slice(start, end))
     }
   }
 
