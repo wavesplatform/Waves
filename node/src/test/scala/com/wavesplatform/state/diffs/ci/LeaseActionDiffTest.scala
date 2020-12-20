3d16ddc68363e7efff8076375e7f2417e3846fa5
Artyom Sayadyan <xrtm000@gmail.com>
2020-12-20 21:56:01 +0300

    Add multiple action tests

--- node/src/test/scala/com/wavesplatform/state/diffs/ci/LeaseActionDiffTest.scala
+++ node/src/test/scala/com/wavesplatform/state/diffs/ci/LeaseActionDiffTest.scala
@@ -1,5 +1,6 @@
 package com.wavesplatform.state.diffs.ci
 
+import cats.implicits._
 import com.wavesplatform.account.{Address, Alias}
 import com.wavesplatform.common.state.ByteStr
 import com.wavesplatform.common.utils.EitherExt2
@@ -21,6 +22,8 @@ import com.wavesplatform.{NoShrink, TransactionGen}
 import org.scalacheck.Gen
 import org.scalatest.{EitherValues, Matchers, PropSpec}
 import org.scalatestplus.scalacheck.{ScalaCheckPropertyChecks => PropertyChecks}
+
+import scala.util.Random
 
 class LeaseActionDiffTest extends PropSpec with PropertyChecks with Matchers with TransactionGen with NoShrink with WithDomain with EitherValues {
   private def features(activateV5: Boolean): FunctionalitySettings = {
@@ -100,6 +103,29 @@ class LeaseActionDiffTest extends PropSpec with PropertyChecks with Matchers wit
        """.stripMargin
     )
 
+  private def multipleLeaseCancelsDApp(leaseIds: Seq[ByteStr]): Script =
+    dApp(
+      s"""
+         | [
+         |   ${leaseIds.map(id => s"LeaseCancel(base58'$id')").mkString(", ")}
+         | ]
+       """.stripMargin
+    )
+
+  private def multipleActionsDApp(recipient: Recipient, amount: Long, leaseCount: Int, leaseCancelCount: Int, transfersCount: Int): Script =
+    dApp(
+      s"""
+         | ${(1 to leaseCount).map(i => s"let lease$i = Lease(${recipientStr(recipient)}, $amount, $i)").mkString("\n")}
+         | [
+         |   ${(1 to leaseCount).map(i => s"lease$i").mkString(", ")}
+         |   ${if (leaseCancelCount > 0) "," else ""}
+         |   ${(1 to leaseCancelCount).map(i => s"LeaseCancel(calculateLeaseId(lease$i))").mkString(", ")}
+         |   ${if (transfersCount > 0) "," else ""}
+         |   ${(1 to transfersCount).map(_ => s"ScriptTransfer(${recipientStr(recipient)}, 1, unit)").mkString(", ")}
+         | ]
+       """.stripMargin
+    )
+
   private def recipientStr(recipient: Recipient) =
     recipient match {
       case Recipient.Address(bytes) => s"Address(base58'$bytes')"
@@ -110,6 +136,7 @@ class LeaseActionDiffTest extends PropSpec with PropertyChecks with Matchers wit
       useAlias: Boolean = false,
       selfLease: Boolean = false,
       useLeaseCancelDApp: Boolean = false,
+      leaseCancelCount: Int = 1,
       cancelLeaseActionByTx: Boolean = false,
       customRecipient: Option[Recipient] = None,
       customAmount: Option[Long] = None,
@@ -151,19 +178,21 @@ class LeaseActionDiffTest extends PropSpec with PropertyChecks with Matchers wit
       val leaseAmount  = customAmount.getOrElse(generatedAmount)
       val setScriptFee = customSetScriptFee.getOrElse(fee)
       for {
-        genesis       <- GenesisTransaction.create(dAppAcc.toAddress, ENOUGH_AMT, ts)
-        genesis2      <- GenesisTransaction.create(invoker.toAddress, ENOUGH_AMT, ts)
-        invoke        <- InvokeScriptTransaction.selfSigned(1.toByte, invoker, dAppAcc.toAddress, None, Nil, fee, Waves, ts)
-        leaseFromDApp <- LeaseTransaction.selfSigned(2.toByte, dAppAcc, invoker.toAddress, leaseTxAmount1, fee, ts)
-        leaseToDApp   <- LeaseTransaction.selfSigned(2.toByte, invoker, dAppAcc.toAddress, leaseTxAmount2, fee, ts)
+        genesis  <- GenesisTransaction.create(dAppAcc.toAddress, ENOUGH_AMT, ts)
+        genesis2 <- GenesisTransaction.create(invoker.toAddress, ENOUGH_AMT, ts)
+        invoke   <- InvokeScriptTransaction.selfSigned(1.toByte, invoker, dAppAcc.toAddress, None, Nil, fee, Waves, ts)
+        leasesFromDApp <- (1 to leaseCancelCount).toList.traverse(
+          i => LeaseTransaction.selfSigned(2.toByte, dAppAcc, invoker.toAddress, leaseTxAmount1, fee, ts + i)
+        )
+        leaseToDApp <- LeaseTransaction.selfSigned(2.toByte, invoker, dAppAcc.toAddress, leaseTxAmount2, fee, ts + 100)
         leaseCancelId = if (cancelLeaseActionByTx) Lease.calculateId(Lease(recipient, leaseAmount, 0), invoke.id.value())
-        else leaseFromDApp.id.value()
-        leaseCancel <- LeaseCancelTransaction.signed(2.toByte, dAppAcc.publicKey, leaseCancelId, fee, ts, dAppAcc.privateKey)
-        dApp = if (useLeaseCancelDApp) singleLeaseCancelDApp(leaseFromDApp.id.value())
+        else leasesFromDApp.head.id.value()
+        leaseCancel <- LeaseCancelTransaction.signed(2.toByte, dAppAcc.publicKey, leaseCancelId, fee, ts + 100, dAppAcc.privateKey)
+        dApp = if (useLeaseCancelDApp) multipleLeaseCancelsDApp(leasesFromDApp.map(_.id.value()))
         else customDApp.getOrElse(singleLeaseDApp(recipient, leaseAmount))
-        setDApp <- SetScriptTransaction.selfSigned(1.toByte, dAppAcc, Some(dApp), setScriptFee, ts)
+        setDApp <- SetScriptTransaction.selfSigned(1.toByte, dAppAcc, Some(dApp), setScriptFee, ts + 100)
         preparingTxs = List(genesis, genesis2) ::: aliasTxs ::: List(setDApp)
-        leaseTxs     = List(leaseFromDApp, leaseToDApp)
+        leaseTxs     = leasesFromDApp :+ leaseToDApp
       } yield (preparingTxs, invoke, leaseAmount, dAppAcc.toAddress, invoker.toAddress, leaseTxs, leaseCancel)
     }.explicitGet()
 
@@ -404,6 +433,43 @@ class LeaseActionDiffTest extends PropSpec with PropertyChecks with Matchers wit
     }
   }
 
+  property(s"10 Lease actions") {
+    val recipient = accountGen.sample.get.toAddress
+    val amount    = positiveLongGen.sample.get
+    val dApp      = multipleActionsDApp(recipient.toRide, amount, leaseCount = 10, leaseCancelCount = 0, transfersCount = 0)
+    forAll(leasePreconditions(customDApp = Some(dApp))) {
+      case (preparingTxs, invoke, _, dAppAcc, invoker, _, _) =>
+        assertDiffAndState(
+          Seq(TestBlock.create(preparingTxs)),
+          TestBlock.create(Seq(invoke)),
+          v5Features
+        ) {
+          case (diff, _) =>
+            diff.errorMessage(invoke.id.value()) shouldBe empty
+            diff.portfolios(invoker) shouldBe Portfolio(-invoke.fee)
+            diff.portfolios(dAppAcc) shouldBe Portfolio(lease = LeaseBalance(in = 0, out = amount * 10))
+            diff.portfolios(recipient) shouldBe Portfolio(lease = LeaseBalance(in = amount * 10, out = 0))
+        }
+    }
+  }
+
+  property(s"11 Lease actions") {
+    val recipient = accountGen.sample.get.toAddress
+    val amount    = positiveLongGen.sample.get
+    val dApp      = multipleActionsDApp(recipient.toRide, amount, leaseCount = 11, leaseCancelCount = 0, transfersCount = 0)
+    forAll(leasePreconditions(customDApp = Some(dApp))) {
+      case (preparingTxs, invoke, _, _, _, _, _) =>
+        assertDiffAndState(
+          Seq(TestBlock.create(preparingTxs)),
+          TestBlock.create(Seq(invoke)),
+          v5Features
+        ) {
+          case (diff, _) =>
+            diff.errorMessage(invoke.id.value()).get.text shouldBe "Too many script actions: max: 10, actual: 11"
+        }
+    }
+  }
+
   property(s"LeaseCancel action with Lease action from same result") {
     val recipient = accountGen.sample.get.toAddress
     val amount    = positiveLongGen.sample.get
@@ -514,6 +580,80 @@ class LeaseActionDiffTest extends PropSpec with PropertyChecks with Matchers wit
         ) {
           case (diff, _) =>
             diff.errorMessage(invoke.id.value()).get.text shouldBe "Cannot cancel already cancelled lease"
+        }
+    }
+  }
+
+  property(s"10 LeaseCancel actions") {
+    forAll(leasePreconditions(useLeaseCancelDApp = true, leaseCancelCount = 10)) {
+      case (preparingTxs, invoke, _, dAppAcc, invoker, leaseTxs :+ _, _) =>
+        assertDiffAndState(
+          Seq(TestBlock.create(preparingTxs ++ leaseTxs)),
+          TestBlock.create(Seq(invoke)),
+          v5Features
+        ) {
+          case (diff, _) =>
+            diff.errorMessage(invoke.id.value()) shouldBe empty
+            diff.portfolios(invoker) shouldBe Portfolio(-invoke.fee, LeaseBalance(in = -leaseTxs.map(_.amount).sum, out = 0))
+            diff.portfolios(dAppAcc) shouldBe Portfolio(0, LeaseBalance(in = 0, out = -leaseTxs.map(_.amount).sum))
+        }
+    }
+  }
+
+  property(s"11 LeaseCancel actions") {
+    forAll(leasePreconditions(useLeaseCancelDApp = true, leaseCancelCount = 11)) {
+      case (preparingTxs, invoke, _, _, _, leaseTxs :+ _, _) =>
+        assertDiffAndState(
+          Seq(TestBlock.create(preparingTxs ++ leaseTxs)),
+          TestBlock.create(Seq(invoke)),
+          v5Features
+        ) {
+          case (diff, _) =>
+            diff.errorMessage(invoke.id.value()).get.text shouldBe "Too many script actions: max: 10, actual: 11"
+        }
+    }
+  }
+
+  property(s"10 multiple actions") {
+    val recipient        = accountGen.sample.get.toAddress
+    val amount           = positiveLongGen.sample.get
+    val leaseCount       = Random.nextInt(10) + 1
+    val leaseCancelCount = Random.nextInt(leaseCount).min(10 - leaseCount)
+    val transfersCount   = 10 - leaseCancelCount - leaseCount
+    val dApp             = multipleActionsDApp(recipient.toRide, amount, leaseCount, leaseCancelCount, transfersCount)
+    val leaseAmount      = (leaseCount - leaseCancelCount) * amount
+    forAll(leasePreconditions(customDApp = Some(dApp))) {
+      case (preparingTxs, invoke, _, dAppAcc, invoker, _, _) =>
+        assertDiffAndState(
+          Seq(TestBlock.create(preparingTxs)),
+          TestBlock.create(Seq(invoke)),
+          v5Features
+        ) {
+          case (diff, _) =>
+            diff.errorMessage(invoke.id.value()) shouldBe empty
+            diff.portfolios(invoker) shouldBe Portfolio(-invoke.fee)
+            diff.portfolios(dAppAcc) shouldBe Portfolio(-transfersCount, lease = LeaseBalance(in = 0, out = leaseAmount))
+            diff.portfolios(recipient) shouldBe Portfolio(transfersCount, lease = LeaseBalance(in = leaseAmount, out = 0))
+        }
+    }
+  }
+
+  property(s"11 multiple actions") {
+    val recipient        = accountGen.sample.get.toAddress
+    val amount           = positiveLongGen.sample.get
+    val leaseCount       = Random.nextInt(11) + 1
+    val leaseCancelCount = Random.nextInt(leaseCount).min(11 - leaseCount)
+    val transfersCount   = 11 - leaseCancelCount - leaseCount
+    val dApp             = multipleActionsDApp(recipient.toRide, amount, leaseCount, leaseCancelCount, transfersCount)
+    forAll(leasePreconditions(customDApp = Some(dApp))) {
+      case (preparingTxs, invoke, _, _, _, _, _) =>
+        assertDiffAndState(
+          Seq(TestBlock.create(preparingTxs)),
+          TestBlock.create(Seq(invoke)),
+          v5Features
+        ) {
+          case (diff, _) =>
+            diff.errorMessage(invoke.id.value()).get.text shouldBe "Too many script actions: max: 10, actual: 11"
         }
     }
   }
