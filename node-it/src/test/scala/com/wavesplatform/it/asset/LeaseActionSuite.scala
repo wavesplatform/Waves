1f4caff4310ab9097d1b00b450cf4fbc5e2ac146
Artyom Sayadyan <xrtm000@gmail.com>
2020-12-24 02:27:34 +0300

    Use different lease amounts for tx and action in LeaseActionSuite

--- node-it/src/test/scala/com/wavesplatform/it/asset/LeaseActionSuite.scala
+++ node-it/src/test/scala/com/wavesplatform/it/asset/LeaseActionSuite.scala
@@ -26,7 +26,8 @@ class LeaseActionSuite extends BaseTransactionSuite {
   private def compile(script: String): String =
     ScriptCompiler.compile(script, ScriptEstimatorV3).explicitGet()._1.bytes().base64
 
-  private val leaseAmount         = 100
+  private val dAppLeaseAmount     = 123
+  private val txLeaseAmount       = 456
   private lazy val dAppAcc        = firstKeyPair
   private lazy val dAppAddress    = firstAddress
   private lazy val invoker        = secondKeyPair
@@ -42,7 +43,7 @@ class LeaseActionSuite extends BaseTransactionSuite {
        |  @Callable(i)
        |  func lease() = {
        |    [
-       |      Lease(i.caller, $leaseAmount)
+       |      Lease(i.caller, $dAppLeaseAmount)
        |    ]
        |  }
        |
@@ -58,24 +59,24 @@ class LeaseActionSuite extends BaseTransactionSuite {
   }
 
   test("active leases") {
-    val leaseTxId     = sender.lease(dAppAcc, invokerAddress, leaseAmount, smartMinFee, TxVersion.V2, waitForTx = true).id
+    val leaseTxId     = sender.lease(dAppAcc, invokerAddress, txLeaseAmount, smartMinFee, TxVersion.V2, waitForTx = true).id
     val leaseTxHeight = sender.transactionStatus(leaseTxId).height.get
 
     val invokeId     = sender.invokeScript(invoker, dAppAddress, Some("lease"), Nil, fee = invokeFee, waitForTx = true)._1.id
     val invokeHeight = sender.transactionStatus(invokeId).height.get
 
     val recipient     = Recipient.Address(ByteStr.decodeBase58(invokerAddress).get)
-    val leaseActionId = Lease.calculateId(Lease(recipient, leaseAmount, 0), ByteStr.decodeBase58(invokeId).get).toString
+    val leaseActionId = Lease.calculateId(Lease(recipient, dAppLeaseAmount, 0), ByteStr.decodeBase58(invokeId).get).toString
 
     sender.activeLeases(dAppAddress) should contain theSameElementsAs Seq(
-      LeaseInfo(leaseTxId, leaseTxId, dAppAddress, invokerAddress, leaseAmount, leaseTxHeight),
-      LeaseInfo(leaseActionId, invokeId, dAppAddress, invokerAddress, leaseAmount, invokeHeight)
+      LeaseInfo(leaseTxId, leaseTxId, dAppAddress, invokerAddress, txLeaseAmount, leaseTxHeight),
+      LeaseInfo(leaseActionId, invokeId, dAppAddress, invokerAddress, dAppLeaseAmount, invokeHeight)
     )
 
     val leaseTxIdParam = List(CONST_BYTESTR(ByteStr.decodeBase58(leaseTxId).get).explicitGet())
     sender.invokeScript(dAppAcc, dAppAddress, Some("leaseCancel"), leaseTxIdParam, fee = invokeFee, waitForTx = true)
     sender.activeLeases(dAppAddress) shouldBe Seq(
-      LeaseInfo(leaseActionId, invokeId, dAppAddress, invokerAddress, leaseAmount, invokeHeight)
+      LeaseInfo(leaseActionId, invokeId, dAppAddress, invokerAddress, dAppLeaseAmount, invokeHeight)
     )
   }
 }
