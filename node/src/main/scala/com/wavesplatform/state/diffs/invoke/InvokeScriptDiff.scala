efdbbbbaa4dac14854ad41e213cd9569e31ee041
Mikhail Potanin <mpotanin@wavesplatform.com>
2020-12-28 17:14:17 +0300

    SC-687-crosscontract-invoke Fix comparation

--- node/src/main/scala/com/wavesplatform/state/diffs/invoke/InvokeScriptDiff.scala
+++ node/src/main/scala/com/wavesplatform/state/diffs/invoke/InvokeScriptDiff.scala
@@ -60,7 +60,7 @@ object InvokeScriptDiff {
 
           _ <- TracedResult(
             Either.cond(
-              invocationComplexity <= ContractLimits.MaxComplexityByVersion(version) && blockchain.continuationStates.get(dAppAddress).forall(_ != ContinuationState.InProgress),
+              invocationComplexity <= ContractLimits.MaxComplexityByVersion(version) && blockchain.continuationStates.get(dAppAddress).forall(!_.isInstanceOf[ContinuationState.InProgress]),
               (),
               GenericError("Continuation is not allowed for Invoke by script")
             )
