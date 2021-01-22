61bed2f9050ad3acf5ec3a7c6649d3b75cc6c867
Artyom Sayadyan <xrtm000@gmail.com>
2021-01-22 13:39:31 +0300

    Implement 'add liquidity'

--- node/src/test/scala/com/wavesplatform/state/diffs/ci/SyncDAppCases.scala
+++ node/src/test/scala/com/wavesplatform/state/diffs/ci/SyncDAppCases.scala
@@ -398,4 +398,170 @@ class SyncDAppCases
         }
     }
   }
+
+  property("add liquidity") {
+    val stakeAmount = 1000
+    val leaseAmount = 777
+
+    def stakingScript(usdN: ByteStr) = {
+      val script = s"""
+                      | {-# STDLIB_VERSION 5     #-}
+                      | {-# SCRIPT_TYPE ACCOUNT  #-}
+                      | {-# CONTENT_TYPE DAPP    #-}
+                      |
+                      | let usdN = base58'$usdN'
+                      |
+                      | @Callable(i)
+                      | func stake() = {
+                      |   if (i.payments[0].assetId != usdN)
+                      |     then
+                      |       throw("unexpected asset")
+                      |     else {
+                      |       let currentLiquidity = this.getInteger(i.caller.toString()).valueOrElse(0)
+                      |       [ IntegerEntry(i.caller.toString(), currentLiquidity + i.payments[0].amount) ]
+                      |     }
+                      |
+                      | }
+                    """.stripMargin
+      Some(ContractScript(V5, compileContractFromExpr(Parser.parseContract(script).get.value, V5)).explicitGet())
+    }
+
+    def exchangerScript(usdN: ByteStr, stakingAddress: Address, leasePool: Address, shareToken: ByteStr) = {
+      val script = s"""
+                      | {-# STDLIB_VERSION 5     #-}
+                      | {-# SCRIPT_TYPE ACCOUNT  #-}
+                      | {-# CONTENT_TYPE DAPP    #-}
+                      |
+                      | let usdN = base58'$usdN'
+                      | let staker = Address(base58'$stakingAddress')
+                      | let leasePool = Address(base58'$leasePool')
+                      | let shareToken = base58'$shareToken'
+                      |
+                      | @Callable(i)
+                      | func exchangeWavesUsdN() = {
+                      |   if (i.payments[0].assetId != unit && i.payments[1].assetId != usdN)
+                      |     then
+                      |       throw("unexpected assets")
+                      |     else {
+                      |       strict r = Invoke(staker, "stake", [], [i.payments[1]])
+                      |
+                      |       let amountKey = "AMOUNT_" + i.caller.toString()
+                      |       let idKey     = "ID_" + i.caller.toString()
+                      |
+                      |       let leasedAmount = this.getInteger(amountKey).valueOrElse(0)
+                      |       let leaseId      = this.getString(idKey)
+                      |
+                      |       let newLease   = Lease(leasePool, leasedAmount + i.payments[0].amount)
+                      |       let newLeaseId = calculateLeaseId(newLease)
+                      |
+                      |       let cancel = match leaseId {
+                      |         case id: String => [ LeaseCancel(fromBase58String(id)) ]
+                      |         case _          => [ StringEntry(idKey, toBase58String(newLeaseId)) ]
+                      |       }
+                      |
+                      |       cancel ++
+                      |       [
+                      |         newLease,
+                      |         ScriptTransfer(i.caller, 1, shareToken),
+                      |         IntegerEntry(amountKey, leasedAmount + i.payments[0].amount)
+                      |       ]
+                      |     }
+                      | }
+                    """.stripMargin
+      Some(ContractScript(V5, compileContractFromExpr(Parser.parseContract(script).get.value, V5)).explicitGet())
+    }
+
+    val scenario =
+      for {
+        invoker   <- accountGen
+        staker    <- accountGen
+        exchanger <- accountGen
+        leasePool <- accountGen
+        ts        <- timestampGen
+        fee       <- ciFee(1)
+
+        usdNIssue = IssueTransaction
+          .selfSigned(
+            2.toByte,
+            staker,
+            "USDN",
+            "",
+            ENOUGH_AMT,
+            8,
+            reissuable = true,
+            None,
+            fee,
+            ts + 1
+          )
+          .explicitGet()
+
+        shareTokenIssue = IssueTransaction
+          .selfSigned(
+            2.toByte,
+            exchanger,
+            "Share",
+            "",
+            ENOUGH_AMT,
+            8,
+            reissuable = true,
+            None,
+            fee,
+            ts + 1
+          )
+          .explicitGet()
+
+        gTx1 = GenesisTransaction.create(invoker.toAddress, ENOUGH_AMT, ts).explicitGet()
+        gTx2 = GenesisTransaction.create(staker.toAddress, ENOUGH_AMT, ts).explicitGet()
+        gTx3 = GenesisTransaction.create(exchanger.toAddress, ENOUGH_AMT, ts).explicitGet()
+
+        usdN       = usdNIssue.id.value()
+        shareToken = shareTokenIssue.id.value()
+
+        stakingScriptR = stakingScript(usdN)
+        exchangerR     = exchangerScript(usdN, staker.toAddress, leasePool.toAddress, shareToken)
+
+        s1 = SetScriptTransaction.selfSigned(1.toByte, staker, stakingScriptR, fee, ts + 5).explicitGet()
+        s2 = SetScriptTransaction.selfSigned(1.toByte, exchanger, exchangerR, fee, ts + 5).explicitGet()
+
+        transfer1 = TransferTransaction
+          .selfSigned(2.toByte, staker, invoker.toAddress, IssuedAsset(usdN), Int.MaxValue, Waves, fee, ByteStr.empty, ts + 5)
+          .explicitGet()
+
+        fc = Terms.FUNCTION_CALL(
+          FunctionHeader.User("exchangeWavesUsdN"),
+          Nil
+        )
+        invokeTx = InvokeScriptTransaction
+          .selfSigned(
+            TxVersion.V3,
+            invoker,
+            exchanger.toAddress,
+            Some(fc),
+            List(Payment(leaseAmount, Waves), Payment(stakeAmount, IssuedAsset(usdN))),
+            fee * 100,
+            Waves,
+            InvokeScriptTransaction.DefaultExtraFeePerStep,
+            ts + 10
+          )
+          .explicitGet()
+
+      } yield (
+        Seq(gTx1, gTx2, gTx3, usdNIssue, shareTokenIssue, s1, s2, transfer1),
+        invokeTx,
+        usdN,
+        shareToken
+      )
+
+    forAll(scenario) {
+      case (genesisTxs, invokeTx, usdN, shareToken) =>
+        assertDiffAndState(Seq(TestBlock.create(genesisTxs)), TestBlock.create(Seq(invokeTx), Block.ProtoBlockVersion), fsWithV5) {
+          case (diff, _) =>
+            diff.errorMessage(invokeTx.id.value()) shouldBe None
+            diff.portfolios(invokeTx.sender.toAddress).assets shouldBe Map(
+              IssuedAsset(usdN)       -> -stakeAmount,
+              IssuedAsset(shareToken) -> 1
+            )
+        }
+    }
+  }
 }
