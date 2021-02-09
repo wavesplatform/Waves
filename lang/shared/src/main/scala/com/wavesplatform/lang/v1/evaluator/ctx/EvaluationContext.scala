e880245a6911977e50c0132629539a17bb986d49
Artyom Sayadyan <xrtm000@gmail.com>
2021-02-09 15:47:58 +0300

    SC-694 Refactor log of RIDE variables (#3354)
    * SC-694 Refactor log of RIDE variables
    
    * Adapt TransactionValidationErrorPrintTest

--- lang/shared/src/main/scala/com/wavesplatform/lang/v1/evaluator/ctx/EvaluationContext.scala
+++ lang/shared/src/main/scala/com/wavesplatform/lang/v1/evaluator/ctx/EvaluationContext.scala
@@ -2,10 +2,13 @@ package com.wavesplatform.lang.v1.evaluator.ctx
 
 import cats._
 import com.wavesplatform.lang.v1.FunctionHeader
+import com.wavesplatform.lang.v1.compiler.Terms.LET
 import com.wavesplatform.lang.v1.compiler.Types.FINAL
 import com.wavesplatform.lang.v1.evaluator.Contextful.NoContext
-import com.wavesplatform.lang.v1.evaluator.{Contextful, LetLogCallback}
+import com.wavesplatform.lang.v1.evaluator.{Contextful, LetExecResult, LetLogCallback}
 import shapeless.{Lens, lens}
+
+import java.util
 
 case class EvaluationContext[C[_[_]], F[_]](
    environment: C[F],
@@ -14,7 +17,12 @@ case class EvaluationContext[C[_[_]], F[_]](
    functions: Map[FunctionHeader, BaseFunction[C]]
 )
 
-case class LoggedEvaluationContext[C[_[_]], F[_]](l: LetLogCallback[F], ec: EvaluationContext[C, F])
+case class LoggedEvaluationContext[C[_[_]], F[_]](l: LetLogCallback[F], ec: EvaluationContext[C, F]) {
+  val loggedLets: util.IdentityHashMap[LET, Unit] = new util.IdentityHashMap()
+
+  def log(let: LET, result: LetExecResult[F]): Unit =
+    loggedLets.computeIfAbsent(let, _ => l(let.name)(result))
+}
 
 object LoggedEvaluationContext {
   class Lenses[F[_], C[_[_]]] {
