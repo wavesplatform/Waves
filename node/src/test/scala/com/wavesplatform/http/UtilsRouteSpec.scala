f985ac2e0f8a529d932d6ae644650e249b335f21
Mikhail Potanin <mpotanin@wavesplatform.com>
2021-02-11 21:38:21 +0300

    SC-695-activations-tests

--- node/src/test/scala/com/wavesplatform/http/UtilsRouteSpec.scala
+++ node/src/test/scala/com/wavesplatform/http/UtilsRouteSpec.scala
@@ -61,6 +61,7 @@ class UtilsRouteSpec extends RouteSpec("/utils") with RestAPISettingsHelper with
     ),
     stub[Blockchain]("globalBlockchain")
   )
+  (utilsApi.blockchain.activatedFeatures _).when().returning(Map()).anyNumberOfTimes()
   private val route = utilsApi.route
 
   val script = FUNCTION_CALL(
@@ -576,6 +577,7 @@ class UtilsRouteSpec extends RouteSpec("/utils") with RestAPISettingsHelper with
     Post(routePath("/script/compileWithImports"), request) ~> route ~> check {
       val expectedScript =
         """
+          | {-# STDLIB_VERSION 3 #-}
           | {-# SCRIPT_TYPE ACCOUNT #-}
           | func inc(a: Int) = a + 1
           | let a = 5
