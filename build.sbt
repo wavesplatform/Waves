37f8fbb368c2190dc897402dafe8746bf005435f
Karasiq <yoba123@yandex.ru>
2021-02-12 15:01:59 +0300

    Docker image valid permissions for mounted data

--- build.sbt
+++ build.sbt
@@ -147,20 +147,15 @@ git.useGitDescribe := true
 git.uncommittedSignifier := Some("DIRTY")
 
 lazy val packageAll = taskKey[Unit]("Package all artifacts")
-packageAll := Def
-  .sequential(
-    root / clean,
-    Def.task {
-      (node / assembly).value
-      (`grpc-server` / Universal / packageZipTarball).value
+packageAll := {
+  (node / assembly).value
+  (`grpc-server` / Universal / packageZipTarball).value
 
-      val nodeDebFile = (node / Debian / packageBin).value
-      val grpcDebFile = (`grpc-server` / Debian / packageBin).value
-      IO.copyFile(nodeDebFile, new File(baseDirectory.value, "docker/target/waves.deb"))
-      IO.copyFile(grpcDebFile, new File(baseDirectory.value, "docker/target/grpc-server.deb"))
-    }
-  )
-  .value
+  val nodeDebFile = (node / Debian / packageBin).value
+  val grpcDebFile = (`grpc-server` / Debian / packageBin).value
+  IO.copyFile(nodeDebFile, new File(baseDirectory.value, "docker/target/waves.deb"))
+  IO.copyFile(grpcDebFile, new File(baseDirectory.value, "docker/target/grpc-server.deb"))
+}
 
 lazy val checkPRRaw = taskKey[Unit]("Build a project and run unit tests")
 checkPRRaw := Def
--- docker/build-scripts/entrypoint.sh
+++ docker/build-scripts/entrypoint.sh
@@ -2,11 +2,22 @@
 shopt -s nullglob
 NETWORKS="mainnet testnet stagenet"
 
-mkdir -p /var/lib/waves/log
+logEcho() {
+  echo $1 | gosu waves tee -a /var/log/waves/waves.log
+}
+
+mkdir -p $WVDATA $WVLOG
+chmod 700 $WVDATA $WVLOG || :
+
+user="$(id -u)"
+if [ "$user" = '0' ]; then
+  find $WVDATA \! -user waves -exec chown waves '{}' +
+  find $WVLOG \! -user waves -exec chown waves '{}' +
+fi
 
 [ -z "${WAVES_CONFIG}" ] && WAVES_CONFIG="/etc/waves/waves.conf"
 if [[ ! -f "$WAVES_CONFIG" ]]; then
-  echo "Custom '$WAVES_CONFIG' not found. Using a default one for '${WAVES_NETWORK,,}' network." | tee -a /var/log/waves/waves.log
+  logEcho "Custom '$WAVES_CONFIG' not found. Using a default one for '${WAVES_NETWORK,,}' network."
   if [[ $NETWORKS == *"${WAVES_NETWORK,,}"* ]]; then
     touch "$WAVES_CONFIG"
     echo "waves.blockchain.type=${WAVES_NETWORK}" >>$WAVES_CONFIG
@@ -27,21 +38,21 @@ fi
 
 [ -n "${WAVES_WALLET_PASSWORD}" ] && JAVA_OPTS="${JAVA_OPTS} -Dwaves.wallet.password=${WAVES_WALLET_PASSWORD}"
 [ -n "${WAVES_WALLET_SEED}" ] && JAVA_OPTS="${JAVA_OPTS} -Dwaves.wallet.seed=${WAVES_WALLET_SEED}"
-JAVA_OPTS="${JAVA_OPTS} -Dwaves.data-directory=/var/lib/waves/data -Dwaves.directory=/var/lib/waves"
+JAVA_OPTS="${JAVA_OPTS} -Dwaves.data-directory=$WVDATA/data -Dwaves.directory=$WVDATA"
 
-echo "Node is starting..." | tee -a /var/log/waves/waves.log
-echo "WAVES_HEAP_SIZE='${WAVES_HEAP_SIZE}'" | tee -a /var/log/waves/waves.log
-echo "WAVES_LOG_LEVEL='${WAVES_LOG_LEVEL}'" | tee -a /var/log/waves/waves.log
-echo "WAVES_NETWORK='${WAVES_NETWORK}'" | tee -a /var/log/waves/waves.log
-echo "WAVES_WALLET_SEED='${WAVES_WALLET_SEED}'" | tee -a /var/log/waves/waves.log
-echo "WAVES_WALLET_PASSWORD='${WAVES_WALLET_PASSWORD}'" | tee -a /var/log/waves/waves.log
-echo "WAVES_CONFIG='${WAVES_CONFIG}'" | tee -a /var/log/waves/waves.log
-echo "JAVA_OPTS='${JAVA_OPTS}'" | tee -a /var/log/waves/waves.log
+logEcho "Node is starting..."
+logEcho "WAVES_HEAP_SIZE='${WAVES_HEAP_SIZE}'"
+logEcho "WAVES_LOG_LEVEL='${WAVES_LOG_LEVEL}'"
+logEcho "WAVES_NETWORK='${WAVES_NETWORK}'"
+logEcho "WAVES_WALLET_SEED='${WAVES_WALLET_SEED}'"
+logEcho "WAVES_WALLET_PASSWORD='${WAVES_WALLET_PASSWORD}'"
+logEcho "WAVES_CONFIG='${WAVES_CONFIG}'"
+logEcho "JAVA_OPTS='${JAVA_OPTS}'"
 
 JAVA_OPTS="-Dlogback.stdout.level=${WAVES_LOG_LEVEL}
   -XX:+ExitOnOutOfMemoryError
   -Xmx${WAVES_HEAP_SIZE}
-  -Dlogback.file.directory=/var/log/waves
+  -Dlogback.file.directory=$WVLOG
   -Dconfig.override_with_env_vars=true
   ${JAVA_OPTS}
-  -cp '/usr/share/waves/lib/plugins/*:/usr/share/waves/lib/*'" waves "$WAVES_CONFIG"
+  -cp '/usr/share/waves/lib/plugins/*:/usr/share/waves/lib/*'" exec gosu waves waves "$WAVES_CONFIG"
