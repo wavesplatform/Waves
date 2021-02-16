1712946ceb1b75d01d91aff090e94786230654d3
Karasiq <yoba123@yandex.ru>
2021-02-16 18:37:09 +0300

    Modify docker build script for Jenkins release job

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-docker run --mount type=bind,source="$(pwd)",target=/src -it mozilla/sbt:8u232_1.3.8 /bin/sh -c '
+NETWORK=Mainnet
+if [[ -n $1 ]]; then
+  echo "Building with custom network: $1"
+  NETWORK=$1
+fi
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+docker run --mount type=bind,source="$DIR",target=/src -it mozilla/sbt:8u232_1.3.8 /bin/sh -c "
   cd /src &&
-  COURSIER_CACHE=$(pwd)/target/docker/coursier sbt "-Dsbt.boot.directory=$(pwd)/target/docker/sbt_cache" packageAll
-'
+  COURSIER_CACHE=\"$DIR/target/docker/coursier\" sbt \"-Dsbt.boot.directory=$DIR/target/docker/sbt_cache\" \"set ThisBuild/network := $1\" packageAll
+"
