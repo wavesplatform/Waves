2c081450c7203ddcbf05021f01b346e7a1e2e6c2
Karasiq <yoba123@yandex.ru>
2021-02-17 15:55:04 +0300

    Fix invoke script metadata

--- grpc-server/src/main/scala/com/wavesplatform/events/events.scala
+++ grpc-server/src/main/scala/com/wavesplatform/events/events.scala
@@ -423,7 +423,7 @@ object StateUpdate {
                 ist.funcCall.function.funcName,
                 ist.funcCall.args.map(x => Argument(argumentToPB(x))),
                 ist.payments.map(p => Amount(PBAmounts.toPBAssetId(p.assetId), p.amount)),
-                Seq(InvokeScriptResult.toPB(diff.scriptResults.getOrElse(ist.id(), InvokeScriptResult.empty)))
+                diff.scriptResults.get(ist.id()).map(InvokeScriptResult.toPB)
               )
             )
         }
