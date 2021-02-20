7a65c30d19c3d1f0d2636c40a019b05ac66e1b4a
Karasiq <yoba123@yandex.ru>
2021-02-20 16:42:59 +0300

    Fix build script

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-NETWORK=Mainnet
+NETWORK="Mainnet"
+
 if [[ -n $1 ]]; then
-  echo "Building with custom network: $1"
   NETWORK=$1
 fi
 
+echo "Building with network: $NETWORK"
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 docker run --mount type=bind,source="$DIR",target=/src -i mozilla/sbt:8u232_1.3.8 /bin/sh -c "
   cd /src &&
-  COURSIER_CACHE=\"$DIR/target/docker/coursier\" sbt \"-Dsbt.boot.directory=$DIR/target/docker/sbt_cache\" \"set ThisBuild/network := $1\" packageAll
+  COURSIER_CACHE=\"$DIR/target/docker/coursier\" sbt \"-Dsbt.boot.directory=$DIR/target/docker/sbt_cache\" \"set ThisBuild/network := $NETWORK\" packageAll
 "
