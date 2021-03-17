dc5c6f25dda4f0b206ab7fdd41e500e8fa1dc829
Mikhail Potanin <mpotanin@wavesplatform.com>
2021-03-17 15:41:32 +0300

    SC-729-isDataStorageUntouched
    isDataStorageUntouched isn't supported in REPL

--- lang/shared/src/main/scala/com/wavesplatform/lang/v1/repl/node/http/WebEnvironment.scala
+++ lang/shared/src/main/scala/com/wavesplatform/lang/v1/repl/node/http/WebEnvironment.scala
@@ -91,12 +91,7 @@ private[repl] case class WebEnvironment(settings: NodeConnectionSettings) extend
       filteredResult = entity.filter(_.`type` == dataType).map(_.value)
     } yield filteredResult
 
-  override def hasData(recipient: Recipient): Future[Boolean] = ???
-/*    for {
-      address <- extractAddress(recipient)
-      entities  <- getEntity[List, DataEntry, DataEntry](s"/addresses/data/$address?match=.*")
-    } yield entities.nonEmpty
-*/
+  override def hasData(recipient: Recipient): Future[Boolean] = Future.failed(new Exception("Not implemented"))
 
   implicit val addressResponseDecoder: Decoder[AddressResponse] = new Decoder[AddressResponse] {
     final def apply(c: HCursor): Decoder.Result[AddressResponse] =
