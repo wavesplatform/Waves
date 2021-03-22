8e9255259c453a55ef79731f6930b257756625d0
Karasiq <yoba123@yandex.ru>
2021-03-22 18:34:15 +0300

    Explicit version in docker build script

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 NETWORK="Mainnet"
+VER_CMD=""
 
 if [[ -n $1 ]]; then
   NETWORK=$1
+fi
+
+if [[ -n $2 ]]; then
+  echo "Building with version: $2"
+  VER_CMD="set ThisBuild/version := \\\"$2\\\""
+  echo $VER_CMD
 fi
 
 echo "Building with network: $NETWORK"
@@ -13,6 +20,6 @@ GROUP=$(id -g)
 
 docker run --mount type=bind,source="$DIR",target=/src -i mozilla/sbt:8u232_1.3.8 /bin/sh -c "
   cd /src &&
-  COURSIER_CACHE=\"$DIR/target/docker/coursier\" sbt \"-Dsbt.boot.directory=$DIR/target/docker/sbt_cache\" \"set ThisBuild/network := $NETWORK\" packageAll &&
+  COURSIER_CACHE=\"$DIR/target/docker/coursier\" exec sbt \"-Dsbt.boot.directory=$DIR/target/docker/sbt_cache\" \"set ThisBuild/network := $NETWORK\" \"$VER_CMD\" packageAll &&
   chown -R $USER:$GROUP .
 "
