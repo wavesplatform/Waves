62b9f5e78f6195494807ff3ae0855379370f411e
Artyom Sayadyan <xrtm000@gmail.com>
2021-03-31 00:08:17 +0300

    Better code

--- node/src/test/scala/com/wavesplatform/state/diffs/ci/SyncDAppComplexityCountTest.scala
+++ node/src/test/scala/com/wavesplatform/state/diffs/ci/SyncDAppComplexityCountTest.scala
@@ -52,7 +52,7 @@ class SyncDAppComplexityCountTest
     )
   )
 
-  def dApp(otherDApps: List[Address], paymentAsset: Option[IssuedAsset], transferAsset: Option[IssuedAsset], condition: String): Script = {
+  private def dApp(otherDApps: List[Address], paymentAsset: Option[IssuedAsset], transferAsset: Option[IssuedAsset], condition: String): Script = {
     val expr = {
       val script =
         s"""
@@ -80,7 +80,7 @@ class SyncDAppComplexityCountTest
   }
 
   // ~1900 complexity
-  val verifierScript: Script = {
+  private val verifierScript: Script = {
     val script = s"""
                     | {-# STDLIB_VERSION 5        #-}
                     | {-# SCRIPT_TYPE ACCOUNT     #-}
@@ -96,7 +96,7 @@ class SyncDAppComplexityCountTest
   }
 
   // ~2700 complexity
-  val groth: String =
+  private val groth: String =
     s"""
        | let key = base64'hwk883gUlTKCyXYA6XWZa8H9/xKIYZaJ0xEs0M5hQOMxiGpxocuX/8maSDmeCk3bo5ViaDBdO7ZBxAhLSe5k/5TFQyF5Lv7KN2tLKnwgoWMqB16OL8WdbePIwTCuPtJNAFKoTZylLDbSf02kckMcZQDPF9iGh+JC99Pio74vDpwTEjUx5tQ99gNQwxULtztsqDRsPnEvKvLmsxHt8LQVBkEBm2PBJFY+OXf1MNW021viDBpR10mX4WQ6zrsGL5L0GY4cwf4tlbh+Obit+LnN/SQTnREf8fPpdKZ1sa/ui3pGi8lMT6io4D7Ujlwx2RdCkBF+isfMf77HCEGsZANw0hSrO2FGg14Sl26xLAIohdaW8O7gEaag8JdVAZ3OVLd5Df1NkZBEr753Xb8WwaXsJjE7qxwINL1KdqA4+EiYW4edb7+a9bbBeOPtb67ZxmFqgyTNS/4obxahezNkjk00ytswsENg//Ee6dWBJZyLH+QGsaU2jO/W4WvRyZhmKKPdipOhiz4Rlrd2XYgsfHsfWf5v4GOTL+13ZB24dW1/m39n2woJ+v686fXbNW85XP/r'
        | let proof = base64'lvQLU/KqgFhsLkt/5C/scqs7nWR+eYtyPdWiLVBux9GblT4AhHYMdCgwQfSJcudvsgV6fXoK+DUSRgJ++Nqt+Wvb7GlYlHpxCysQhz26TTu8Nyo7zpmVPH92+UYmbvbQCSvX2BhWtvkfHmqDVjmSIQ4RUMfeveA1KZbSf999NE4qKK8Do+8oXcmTM4LZVmh1rlyqznIdFXPN7x3pD4E0gb6/y69xtWMChv9654FMg05bAdueKt9uA4BEcAbpkdHF'
@@ -105,10 +105,10 @@ class SyncDAppComplexityCountTest
     """.stripMargin
 
   // ~70 complexity
-  val sigVerify: String =
+  private val sigVerify: String =
     """ !sigVerify_32Kb(base58'', base58'', base58'') """
 
-  def assetScript(condition: String): Script = {
+  private def assetScript(condition: String): Script = {
     val script = s"""
                     | {-# STDLIB_VERSION 5        #-}
                     | {-# SCRIPT_TYPE ASSET       #-}
@@ -120,7 +120,7 @@ class SyncDAppComplexityCountTest
     ScriptCompiler.compile(script, ScriptEstimatorV3).explicitGet()._1
   }
 
-  def scenario(
+  private def scenario(
       dAppCount: Int,
       withPayment: Boolean,
       withThroughPayment: Boolean,
@@ -202,7 +202,7 @@ class SyncDAppComplexityCountTest
       dAppAccs.head.toAddress
     )
 
-  def assert(
+  private def assert(
       dAppCount: Int,
       complexity: Int,
       withPayment: Boolean = false,
