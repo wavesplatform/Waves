77be929125523a7ba7bcae91e848c8f378f5e65c
Sergey Nazarov <snazarov@web3tech.ru>
2021-04-06 11:16:52 +0300

    cleanup

--- lang/testkit/src/main/scala/com/wavesplatform/lang/v1/compiler/TestCompiler.scala
+++ lang/testkit/src/main/scala/com/wavesplatform/lang/v1/compiler/TestCompiler.scala
@@ -5,11 +5,10 @@ import com.wavesplatform.common.utils.EitherExt2
 import com.wavesplatform.lang.Global
 import com.wavesplatform.lang.contract.DApp
 import com.wavesplatform.lang.directives.DirectiveSet
-import com.wavesplatform.lang.directives.values.{Account, Expression, ScriptType, StdLibVersion, DApp => DAppType}
+import com.wavesplatform.lang.directives.values.{Account, StdLibVersion, DApp => DAppType}
 import com.wavesplatform.lang.script.{ContractScript, Script}
 import com.wavesplatform.lang.v1.evaluator.ctx.impl.waves.WavesContext
 import com.wavesplatform.lang.v1.evaluator.ctx.impl.{CryptoContext, PureContext}
-import com.wavesplatform.lang.v1.parser.Expressions.EXPR
 import com.wavesplatform.lang.v1.traits.Environment
 
 import scala.collection.mutable
@@ -25,19 +24,6 @@ class TestCompiler(version: StdLibVersion) {
 
   def compileContract(script: String): Script =
     ContractScript(version, compile(script).explicitGet()).explicitGet()
-
-  @deprecated("use TestCompiler instead", "1.3.1")
-  def compileExpr(expr: EXPR, version: StdLibVersion, scriptType: ScriptType): Terms.EXPR = {
-    val ctx =
-      PureContext.build(version).withEnvironment[Environment] |+|
-        CryptoContext.build(Global, version).withEnvironment[Environment] |+|
-        WavesContext.build(
-          Global,
-          DirectiveSet(version, scriptType, Expression).explicitGet()
-        )
-    ExpressionCompiler(ctx.compilerContext, expr).explicitGet()._1
-  }
-
 }
 
 object TestCompiler {
