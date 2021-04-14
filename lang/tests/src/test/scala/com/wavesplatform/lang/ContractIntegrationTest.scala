2b8d94e2a6915128bfb752183a40d8b15479557b
Artyom Sayadyan <xrtm000@gmail.com>
2021-04-14 19:04:28 +0300

    SC-625 Use evaluated complexity for block (#3416)
    * SC-625 Use evaluated complexity for block
    
    * Optimize imports
    
    * Support fee calculation for InvokeScript
    
    * Add test
    
    * Optimize imports
    
    * Fixes for tests
    
    * Correct after merge
    
    * Merge fixes
    
    * Adapt complexities in SyncDAppComplexityCountTest

--- lang/tests/src/test/scala/com/wavesplatform/lang/ContractIntegrationTest.scala
+++ lang/tests/src/test/scala/com/wavesplatform/lang/ContractIntegrationTest.scala
@@ -91,7 +91,7 @@ class ContractIntegrationTest extends PropSpec with PropertyChecks with ScriptGe
         DataItem.Bin("feeAssetId", ByteStr.empty),
       ),
       List(),
-      0
+      2147483615
     )
   }
 
@@ -105,7 +105,7 @@ class ContractIntegrationTest extends PropSpec with PropertyChecks with ScriptGe
       """.stripMargin,
       "foo",
       Range(1, 23).map(i => Terms.CONST_LONG(i)).toList
-    ).explicitGet()._1 shouldBe ScriptResultV3(List(DataItem.Lng("1", 22)), List(), 0)
+    ).explicitGet()._1 shouldBe ScriptResultV3(List(DataItem.Lng("1", 22)), List(), 2147483641)
   }
 
   property("@Callable exception error contains initialised values") {
@@ -157,8 +157,9 @@ class ContractIntegrationTest extends PropSpec with PropertyChecks with ScriptGe
     val parsed   = Parser.parseContract(script).get.value
     val compiled = ContractCompiler(ctx.compilerContext, parsed, V3).explicitGet()
 
-    ContractEvaluator(
+    ContractEvaluator.applyV2Coeval(
       ctx.evaluationContext(environment),
+      Map(),
       compiled,
       Invocation(
         Terms.FUNCTION_CALL(FunctionHeader.User(func), args),
@@ -171,8 +172,9 @@ class ContractIntegrationTest extends PropSpec with PropertyChecks with ScriptGe
         fee,
         feeAssetId
       ),
-      V3
-    )
+      V3,
+      Int.MaxValue
+    ).value().leftMap { case (e, _, log) => (e, log) }
   }
 
   def parseCompileAndVerify(script: String, tx: Tx): Either[ExecutionError, EVALUATED] = {
@@ -185,7 +187,7 @@ class ContractIntegrationTest extends PropSpec with PropertyChecks with ScriptGe
       ctx.evaluationContext(environment),
       EvaluatorV2.applyCompleted(_, _, V3),
       txObject
-    ).bimap(_._1, _._1)
+    )._3
   }
 
   property("Simple verify") {
@@ -305,7 +307,7 @@ class ContractIntegrationTest extends PropSpec with PropertyChecks with ScriptGe
         AssetTransfer(Recipient.Address(callerAddress), 1L, None),
         AssetTransfer(Recipient.Address(callerAddress), 2L, None)
       ),
-      0
+      2147483626
     )
   }
 
@@ -374,7 +376,7 @@ class ContractIntegrationTest extends PropSpec with PropertyChecks with ScriptGe
         AssetTransfer(Recipient.Address(callerAddress), 3, None),
         AssetTransfer(Recipient.Address(callerAddress), 4, None)
       ),
-      0
+      2147483605
     )
   }
 }
