7ed76fedc1ca04310a4934eed1087f2f29b448f8
Artyom Sayadyan <xrtm000@gmail.com>
2021-04-16 18:08:38 +0300

    Test isDataStorageUntouched for sync dApp

--- node/src/test/scala/com/wavesplatform/state/diffs/smart/predef/IsDataStorageUntouchedTest.scala
+++ node/src/test/scala/com/wavesplatform/state/diffs/smart/predef/IsDataStorageUntouchedTest.scala
@@ -1,7 +1,9 @@
 package com.wavesplatform.state.diffs.smart.predef
+import com.wavesplatform.account.Address
 import com.wavesplatform.common.utils.EitherExt2
 import com.wavesplatform.db.WithDomain
 import com.wavesplatform.lang.directives.values.V5
+import com.wavesplatform.lang.script.Script
 import com.wavesplatform.lang.v1.compiler.TestCompiler
 import com.wavesplatform.state.diffs.ENOUGH_AMT
 import com.wavesplatform.state.diffs.ci.ciFee
@@ -90,6 +92,48 @@ class IsDataStorageUntouchedTest
       d.removeAfter(genesis)
       d.appendBlock(invokeTx)
       d.blockchain.accountData(dApp, "virgin") shouldBe Some(BooleanDataEntry("virgin", true))
+    }
+  }
+
+  property("isDataStorageUntouched in sync dApp") {
+    def syncDApp(nextDApp: Address): Script = TestCompiler(V5).compileContract(
+      s"""
+         | {-# STDLIB_VERSION 5       #-}
+         | {-# CONTENT_TYPE   DAPP    #-}
+         | {-# SCRIPT_TYPE    ACCOUNT #-}
+         |
+         | @Callable(i)
+         | func default() = {
+         |   let address  = Address(base58'$nextDApp')
+         |   strict start = isDataStorageUntouched(address)
+         |   strict r     = Invoke(address, "default", [], [])
+         |   strict end   = isDataStorageUntouched(address)
+         |   [
+         |     BooleanEntry("start", start),
+         |     BooleanEntry("end", end)
+         |   ]
+         | }
+       """.stripMargin
+    )
+
+    val scenario =
+      for {
+        dApp1 <- accountGen
+        dApp2 <- accountGen
+        fee   <- ciFee()
+        genesis1 = GenesisTransaction.create(dApp1.toAddress, ENOUGH_AMT, ts).explicitGet()
+        genesis2 = GenesisTransaction.create(dApp2.toAddress, ENOUGH_AMT, ts).explicitGet()
+        setDApp1 = SetScriptTransaction.selfSigned(1.toByte, dApp1, Some(syncDApp(dApp2.toAddress)), fee, ts).explicitGet()
+        setDApp2 = SetScriptTransaction.selfSigned(1.toByte, dApp2, Some(contract), fee, ts).explicitGet()
+        invoke   = InvokeScriptTransaction.selfSigned(1.toByte, dApp1, dApp1.toAddress, None, Nil, fee, Waves, ts).explicitGet()
+      } yield (List(genesis1, genesis2, setDApp1, setDApp2), invoke)
+
+    val (genesisTxs, invokeTx) = scenario.sample.get
+    withDomain(RideV5) { d =>
+      d.appendBlock(genesisTxs: _*).id()
+      d.appendBlock(invokeTx)
+      d.blockchain.accountData(invokeTx.dAppAddressOrAlias.asInstanceOf[Address], "start") shouldBe Some(BooleanDataEntry("start", true))
+      d.blockchain.accountData(invokeTx.dAppAddressOrAlias.asInstanceOf[Address], "end") shouldBe Some(BooleanDataEntry("end", false))
     }
   }
 }
