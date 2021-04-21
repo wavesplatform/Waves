9cf2375b5724f5c078681799718437fa05c8ac1b
Artyom Sayadyan <xrtm000@gmail.com>
2021-04-21 12:02:48 +0300

    SC-756 Test take() with illegal characters (#3440)
    * Refactor tests
    
    * Test broken unicode usage in V5

--- lang/tests/src/test/scala/com/wavesplatform/lang/evaluator/BigIntTest.scala
+++ lang/tests/src/test/scala/com/wavesplatform/lang/evaluator/BigIntTest.scala
@@ -1,17 +1,15 @@
 package com.wavesplatform.lang.evaluator
+
 import com.wavesplatform.common.utils.EitherExt2
-import com.wavesplatform.lang.Common.{NoShrink, produce}
+import com.wavesplatform.lang.Common.produce
 import com.wavesplatform.lang.directives.values.{StdLibVersion, V5}
 import com.wavesplatform.lang.v1.compiler.Terms.{CONST_BIGINT, CONST_BOOLEAN, CONST_LONG, CONST_STRING}
 import com.wavesplatform.lang.v1.evaluator.ctx.impl.{PureContext, unit}
-import com.wavesplatform.lang.v1.testing.ScriptGen
-import org.scalatest.{Inside, Matchers, PropSpec}
-import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 
 import scala.math.BigDecimal.RoundingMode._
 import scala.util.Random
 
-class BigIntTest extends PropSpec with EvaluatorSpec with ScalaCheckPropertyChecks with ScriptGen with Matchers with NoShrink with Inside {
+class BigIntTest extends EvaluatorSpec {
   implicit val startVersion: StdLibVersion = V5
 
   private val maxValue = s"""parseBigIntValue("${PureContext.BigIntMax}")"""
