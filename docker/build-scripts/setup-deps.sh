93151838e7101363ea75a08c33455a587192f400
Sergey Nazarov <snazarov@web3tech.ru>
2021-06-09 12:47:20 +0300

    NODE-2330: Show version and chain ID in Swagger UI (#3502)

--- docker/build-scripts/setup-deps.sh
+++ docker/build-scripts/setup-deps.sh
@@ -3,7 +3,7 @@
 mkdir -p /usr/share/man/man1
 apt-get update && apt-get install -y wget unzip default-jre-headless gosu || exit 1
 
-YOURKIT_ARCHIVE="YourKit-JavaProfiler-2019.8-docker.zip"
+YOURKIT_ARCHIVE="YourKit-JavaProfiler-2021.3-docker.zip"
 wget --quiet "https://www.yourkit.com/download/docker/$YOURKIT_ARCHIVE" -P /tmp/ && unzip /tmp/$YOURKIT_ARCHIVE -d /usr/local
 
 # Clean
