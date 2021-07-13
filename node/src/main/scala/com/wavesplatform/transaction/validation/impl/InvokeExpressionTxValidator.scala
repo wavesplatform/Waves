aa694d521814f28865ccb0fb9e8bb58acaa144ce
Artyom Sayadyan <xrtm000@gmail.com>
2021-07-13 18:08:07 +0300

    Add validation on script type in InvokeExpressionTxValidator

--- node/src/main/scala/com/wavesplatform/transaction/validation/impl/InvokeExpressionTxValidator.scala
+++ node/src/main/scala/com/wavesplatform/transaction/validation/impl/InvokeExpressionTxValidator.scala
@@ -1,6 +1,7 @@
 package com.wavesplatform.transaction.validation.impl
 
 import com.wavesplatform.lang.v1.ContractLimits
+import com.wavesplatform.transaction.TxValidationError.GenericError
 import com.wavesplatform.transaction.smart.InvokeExpressionTransaction
 import com.wavesplatform.transaction.validation.{TxValidator, ValidatedV}
 
@@ -8,6 +9,7 @@ object InvokeExpressionTxValidator extends TxValidator[InvokeExpressionTransacti
   override def validate(tx: InvokeExpressionTransaction): ValidatedV[InvokeExpressionTransaction] =
     V.seq(tx)(
       V.fee(tx.fee),
-      V.invokeLength(tx.expressionBytes.size <= ContractLimits.MaxContractSizeInBytes)
+      V.invokeLength(tx.expressionBytes.size <= ContractLimits.MaxContractSizeInBytes),
+      V.cond(tx.expression.isFreeCall, GenericError("Script type for Invoke Expression Transaction should be CALL"))
     )
 }
