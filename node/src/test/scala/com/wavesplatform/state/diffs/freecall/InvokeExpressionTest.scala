0f0f3e6d383c648534f1e3a86e93d4625821353d
Artyom Sayadyan <xrtm000@gmail.com>
2021-07-14 19:01:37 +0300

    Test fail

--- node/src/test/scala/com/wavesplatform/state/diffs/freecall/InvokeExpressionTest.scala
+++ node/src/test/scala/com/wavesplatform/state/diffs/freecall/InvokeExpressionTest.scala
@@ -8,6 +8,7 @@ import com.wavesplatform.lang.directives.values.V6
 import com.wavesplatform.lang.script.v1.ExprScript
 import com.wavesplatform.lang.v1.compiler.TestCompiler
 import com.wavesplatform.state.diffs.ENOUGH_AMT
+import com.wavesplatform.state.diffs.FeeValidation.{FeeConstants, FeeUnit}
 import com.wavesplatform.state.diffs.ci.ciFee
 import com.wavesplatform.state.{BinaryDataEntry, BooleanDataEntry}
 import com.wavesplatform.test.PropSpec
@@ -25,7 +26,7 @@ class InvokeExpressionTest extends PropSpec with ScalaCheckPropertyChecks with T
   private val time = new TestTime
   private def ts   = time.getTimestamp()
 
-  private def expr(invoker: KeyPair, fee: Long): ExprScript =
+  private def expr(invoker: KeyPair, fee: Long, issue: Boolean): ExprScript =
     TestCompiler(V6).compileFreeCall(
       s"""
          | let address   = Address(base58'${invoker.toAddress}')
@@ -42,25 +43,29 @@ class InvokeExpressionTest extends PropSpec with ScalaCheckPropertyChecks with T
          | [
          |   BooleanEntry("check", check),
          |   BinaryEntry("transactionId", i.transactionId)
+         |   ${if (issue) """, Issue("name", "description", 1000, 4, true, unit, 0) """ else ""}
          | ]
        """.stripMargin
     )
 
-  private def scenario(freeCallFee: Boolean = true) =
+  private def scenario(enoughFee: Boolean = true, issue: Boolean = false) =
     for {
       invoker  <- accountGen
       address2 <- accountGen
-      fee      <- ciFee(freeCall = freeCallFee)
+      fee      <- ciFee(freeCall = enoughFee, nonNftIssue = if (issue) 1 else 0)
       gTx1   = GenesisTransaction.create(address2.toAddress, ENOUGH_AMT, ts).explicitGet()
       gTx2   = GenesisTransaction.create(invoker.toAddress, ENOUGH_AMT, ts).explicitGet()
-      call   = expr(invoker, fee)
+      call   = expr(invoker, fee, issue)
       invoke = InvokeExpressionTransaction.selfSigned(TxVersion.V1, invoker, call, fee, Waves, ts).explicitGet()
     } yield (Seq(gTx1, gTx2), invoke)
 
-  private def feeErrorMessage(invoke: InvokeExpressionTransaction) =
-    s"Fee in WAVES for InvokeExpressionTransaction (${invoke.fee} in WAVES) does not exceed minimal value of 1000000 WAVES."
+  private def feeErrorMessage(invoke: InvokeExpressionTransaction, issue: Boolean = false) = {
+    val expectingFee = FeeConstants(invoke.typeId) * FeeUnit + (if (issue) 1 else 0) * MinIssueFee
+    val issueErr     = if (issue) " with 1 assets issued" else ""
+    s"Fee in WAVES for InvokeExpressionTransaction (${invoke.fee} in WAVES)$issueErr does not exceed minimal value of $expectingFee WAVES."
+  }
 
-  property("global variable fields of invoke expression") {
+  property("successful applying to the state") {
     val (genesisTxs, invoke) = scenario().sample.get
     withDomain(RideV6) { d =>
       d.appendBlock(genesisTxs: _*)
@@ -71,7 +76,7 @@ class InvokeExpressionTest extends PropSpec with ScalaCheckPropertyChecks with T
   }
 
   property("insufficient fee leading to reject") {
-    val (genesisTxs, invoke) = scenario(freeCallFee = false).sample.get
+    val (genesisTxs, invoke) = scenario(enoughFee = false).sample.get
     withDomain(RideV6) { d =>
       d.appendBlock(genesisTxs: _*)
       intercept[Exception](d.appendBlock(invoke)).getMessage should include (feeErrorMessage(invoke))
@@ -79,11 +84,11 @@ class InvokeExpressionTest extends PropSpec with ScalaCheckPropertyChecks with T
   }
 
   property("insufficient fee leading to fail") {
-    val (genesisTxs, invoke) = scenario(freeCallFee = false).sample.get
+    val (genesisTxs, invoke) = scenario(enoughFee = false, issue = true).sample.get
     withDomain(RideV6) { d =>
       d.appendBlock(genesisTxs: _*)
       d.appendBlock(invoke)
-      d.blockchain.bestLiquidDiff.get.errorMessage(invoke.txId).get.text shouldBe feeErrorMessage(invoke)
+      d.blockchain.bestLiquidDiff.get.errorMessage(invoke.txId).get.text shouldBe feeErrorMessage(invoke, issue = true)
     }
   }
 
