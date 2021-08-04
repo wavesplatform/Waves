a09abae2667d2e2beba7dd93514d23330f8968f2
Artyom Sayadyan <xrtm000@gmail.com>
2021-08-04 19:46:13 +0300

    Improve ObjectTypesTest to avoid missing types declared in hjson

--- lang/tests/src/test/scala/com/wavesplatform/lang/ObjectTypesTest.scala
+++ lang/tests/src/test/scala/com/wavesplatform/lang/ObjectTypesTest.scala
@@ -5,7 +5,7 @@ import com.wavesplatform.common.utils.EitherExt2
 import com.wavesplatform.lang.directives.values._
 import com.wavesplatform.lang.directives.{DirectiveDictionary, DirectiveSet}
 import com.wavesplatform.lang.utils._
-import com.wavesplatform.lang.v1.compiler.Types.CASETYPEREF
+import com.wavesplatform.lang.v1.compiler.Types.{CASETYPEREF, FINAL}
 import com.wavesplatform.test._
 import org.scalatest.exceptions.TestFailedException
 
@@ -14,35 +14,40 @@ class ObjectTypesTest extends PropSpec {
     DirectiveDictionary[StdLibVersion].all
       .map(version => DirectiveSet(version, Account, Expression).explicitGet())
 
-  private def getDocFields(typeName: String, version: StdLibVersion): List[(String, String)] =
-    DocSource.typeData
-      .getOrElse(
-        (typeName, version.id),
-        throw new Exception(s"Type $typeName not found for $version")
-      )
-
   private def fieldsString(fields: List[(String, String)]): String =
     fields.map { case (name, t) => s"$name: $t" }.mkString("\n\n", "\n", "\n\n")
 
-  property("all object types") {
-    directives.foreach { ds =>
-      lazyContexts(ds)
-        .value()
-        .types
-        .toList
-        .collect {
-          case CASETYPEREF(name, fields, _) =>
-            val codeFields = fields.map { case (name, t) => (name, t.toString) }
-            val docFields  = getDocFields(name, ds.stdLibVersion)
-            if (docFields != codeFields)
+  private def check(scalaTypes: Seq[FINAL], version: StdLibVersion): Unit = {
+    val docTypes =
+      DocSource.typeData.collect { case ((typeName, v), fields) if v == version.id => (typeName, fields) }.toMap
+    val unusedDocTypes =
+      scalaTypes
+        .collect { case CASETYPEREF(typeName, fields, _) => typeName -> fields.map { case (fieldName, t) => (fieldName, t.toString) } }
+        .foldLeft(docTypes) {
+          case (remainingDocTypes, (scalaTypeName, scalaTypeFields)) =>
+            val docFields =
+              remainingDocTypes.getOrElse(
+                scalaTypeName,
+                throw new TestFailedException(s"Can't find type '$scalaTypeName' in $version documentation", 0)
+              )
+            if (docFields != scalaTypeFields)
               throw new TestFailedException(
-                s"For type '$name' in ${ds.stdLibVersion} " +
+                s"For type '$scalaTypeName' in $version " +
                   s"documented fields ${fieldsString(docFields)} " +
                   s"differ from " +
-                  s"code fields ${fieldsString(codeFields)}",
+                  s"code fields ${fieldsString(scalaTypeFields)}",
                 0
               )
+            remainingDocTypes - scalaTypeName
         }
-    }
+    if (unusedDocTypes.nonEmpty)
+      throw new TestFailedException(
+        s"For RIDE $version documented types is unused: ${unusedDocTypes.map(_._1).mkString(", ")}",
+        0
+      )
+  }
+
+  property("all object types") {
+    directives.foreach(ds => check(lazyContexts(ds).value().types, ds.stdLibVersion))
   }
 }
