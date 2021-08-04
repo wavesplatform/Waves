0a6b8f195d0e935a8b4f5dceaf79a6ecfdaf3463
Artyom Sayadyan <xrtm000@gmail.com>
2021-08-04 21:55:54 +0300

    Improve FunctionComplexityTest  to avoid missing functions declared in hjson

--- lang/tests/src/test/scala/com/wavesplatform/lang/v1/estimator/FunctionComplexityTest.scala
+++ lang/tests/src/test/scala/com/wavesplatform/lang/v1/estimator/FunctionComplexityTest.scala
@@ -13,8 +13,8 @@ import com.wavesplatform.test.PropSpec
 import org.scalatest.exceptions.TestFailedException
 
 class FunctionComplexityTest extends PropSpec {
-  val directives: Iterable[DirectiveSet] =
-  DirectiveDictionary[StdLibVersion].all
+  private val directives: Iterable[DirectiveSet] =
+    DirectiveDictionary[StdLibVersion].all
       .flatMap(
         version =>
           Seq(
@@ -24,39 +24,69 @@ class FunctionComplexityTest extends PropSpec {
                 else Seq())
       )
 
-  def docCost(function: BaseFunction[Environment], version: StdLibVersion): Int =
-    DocSource.funcData
-      .getOrElse(
-        (
-          function.name,
-          function.signature.args.map(_._2.toString).toList,
-          version.id
-        ),
-        throw new Exception(s"Function ${function.name}(${function.signature.args.map(_._2.toString).toList.mkString(", ")}) not found in $version")
+  private val dAppOnlyFunctions =
+    Set("invoke", "reentrantInvoke")
+
+  private val baseDataStorageFunctions =
+    Set("getString", "getBoolean", "getBinary", "getInteger")
+
+  private val allDataStorageFunctions =
+    baseDataStorageFunctions ++ baseDataStorageFunctions.map(_ + "Value")
+
+  private def check(functions: Array[BaseFunction[Environment]], ds: DirectiveSet): Unit = {
+    val docCosts =
+      DocSource.funcData.collect {
+        case ((name, signature, version), (_, _, complexity)) if version == ds.stdLibVersion.id => ((name, signature), complexity)
+      }.toMap
+    val unusedDocCosts =
+      functions
+        .filterNot(_.name.startsWith("_"))
+        .foldLeft(docCosts) {
+          case (remainingDocCosts, function) =>
+            val expr = FUNCTION_CALL(function.header, List.fill(function.args.size)(Terms.TRUE))
+            val estimatedCost = ScriptEstimatorV3(
+              varNames(ds.stdLibVersion, ds.contentType),
+              functionCosts(ds.stdLibVersion, ds.contentType),
+              expr
+            ).explicitGet() - function.args.size
+
+            val name = function.name
+            val args = function.signature.args.map(_._2.toString).toList
+            val expectedCost =
+              remainingDocCosts.getOrElse(
+                (name, args),
+                throw new TestFailedException(s"Function $name(${args.mkString(", ")}) not found for RIDE ${ds.stdLibVersion}", 0)
+              )
+
+            if (estimatedCost != expectedCost)
+              throw new TestFailedException(
+                s"Estimated complexity = $estimatedCost is not equal to doc complexity = $expectedCost for ${ds.stdLibVersion} $function",
+                0
+              )
+            remainingDocCosts - ((name, args))
+        }
+
+    def onlyDApp(costs: Map[(String, List[String]), Int]) =
+      if (ds.contentType != DApp)
+        costs.filterNot(c => dAppOnlyFunctions.contains(c._1._1))
+      else
+        costs
+
+    def onlyAccount(costs: Map[(String, List[String]), Int]) =
+      if (ds.scriptType == Asset)
+        costs.filterNot { case ((name, args), _) => allDataStorageFunctions.contains(name) && args.size == 1 }
+      else
+        costs
+
+    val checkedUnusedDocCosts = onlyDApp(onlyAccount(unusedDocCosts))
+    if (checkedUnusedDocCosts.nonEmpty)
+      throw new TestFailedException(
+        s"For RIDE ${ds.stdLibVersion} documented functions is unused: ${checkedUnusedDocCosts.map(_._1).mkString(", ")}",
+        0
       )
-      ._3
+  }
 
   property("all functions complexities") {
-    directives.foreach { ds =>
-      val ctx = lazyContexts(ds).value()
-      ctx.functions
-        .filterNot(_.name.startsWith("_"))
-        .foreach { function =>
-          val expr = FUNCTION_CALL(function.header, List.fill(function.args.size)(Terms.TRUE))
-          val estimatedCost = ScriptEstimatorV3(
-            varNames(ds.stdLibVersion, ds.contentType),
-            functionCosts(ds.stdLibVersion, ds.contentType),
-            expr
-          ).explicitGet() - function.args.size
-
-          val expectedCost = docCost(function, ds.stdLibVersion)
-
-          if (estimatedCost != expectedCost)
-            throw new TestFailedException(
-              s"Estimated complexity = $estimatedCost is not equal to doc complexity = $expectedCost for ${ds.stdLibVersion} $function",
-              0
-            )
-        }
-    }
+    directives.foreach(ds => check(lazyContexts(ds).value().functions, ds))
   }
 }
