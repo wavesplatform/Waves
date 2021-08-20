1bf2875c2d00f9e066249ecf9abf6c873e5e66d7
Sergey Nazarov <snazarov@web3tech.ru>
2021-08-20 19:12:30 +0300

    Fix microblock serialization

--- node/src/main/scala/com/wavesplatform/block/serialization/package.scala
+++ node/src/main/scala/com/wavesplatform/block/serialization/package.scala
@@ -5,12 +5,14 @@ import java.nio.ByteBuffer
 import com.google.common.primitives.{Bytes, Ints, Longs}
 import com.wavesplatform.block.Block.{PlainBlockVersion, ProtoBlockVersion}
 import com.wavesplatform.common.state.ByteStr
+import com.wavesplatform.protobuf.transaction.PBTransactions
+import com.wavesplatform.protobuf.utils.PBUtils
 import com.wavesplatform.serialization.ByteBufferOps
 import com.wavesplatform.transaction.{Transaction, TransactionParsers}
 
 package object serialization {
   private[block] def writeTransactionData(version: Byte, txs: Seq[Transaction]): Array[Byte] = {
-    val txsBytes     = txs.map(_.bytes())
+    val txsBytes     = txs.map(tx => if (version == ProtoBlockVersion) PBUtils.encodeDeterministic(PBTransactions.protobuf(tx)) else tx.bytes())
     val txsBytesSize = txsBytes.map(_.length + Ints.BYTES).sum
     val txsBuf       = ByteBuffer.allocate(txsBytesSize)
     txsBytes.foreach(tx => txsBuf.putInt(tx.length).put(tx))
