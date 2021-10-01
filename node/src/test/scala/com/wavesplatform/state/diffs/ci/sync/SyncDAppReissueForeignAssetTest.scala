3eeed49cc72960b1ea92a2b8cba471b48e6cce9b
Artyom Sayadyan <xrtm000@gmail.com>
2021-10-01 11:20:43 +0300

    Add SyncDAppReissueForeignAssetTest

--- node/src/test/scala/com/wavesplatform/state/diffs/ci/sync/SyncDAppReissueForeignAssetTest.scala
+++ node/src/test/scala/com/wavesplatform/state/diffs/ci/sync/SyncDAppReissueForeignAssetTest.scala
@@ -19,7 +19,7 @@ import com.wavesplatform.transaction.transfer.TransferTransaction
 import com.wavesplatform.transaction.{Asset, GenesisTransaction, TxVersion}
 import com.wavesplatform.{TestTime, TransactionGenBase}
 
-class SyncDAppLeaseBalanceCheckTest extends PropSpec with WithDomain with TransactionGenBase {
+class SyncDAppReissueForeignAssetTest extends PropSpec with WithDomain with TransactionGenBase {
 
   private val time = new TestTime
   private def ts   = time.getTimestamp()
@@ -27,28 +27,26 @@ class SyncDAppLeaseBalanceCheckTest extends PropSpec with WithDomain with Transa
   private def sigVerify(c: Boolean) =
     s""" strict c = ${if (c) (1 to 5).map(_ => "sigVerify(base58'', base58'', base58'')").mkString(" || ") else "true"} """
 
-  private def dApp1Script(dApp2: Address, bigComplexity: Boolean): Script =
+  private def dApp1Script(dApp2: Address, asset: Asset, bigComplexity: Boolean): Script =
     TestCompiler(V5).compileContract(
       s"""
          | @Callable(i)
          | func default() = {
          |    ${sigVerify(bigComplexity)}
          |    strict r = Address(base58'$dApp2').invoke("default", [], [])
-         |    [
-         |      ScriptTransfer(Address(base58'$dApp2'), 100, unit)
-         |    ]
+         |    []
          | }
        """.stripMargin
     )
 
-  private def dApp2Script(bigComplexity: Boolean): Script =
+  private def dApp2Script(asset: Asset, bigComplexity: Boolean): Script =
     TestCompiler(V5).compileContract(
       s"""
          | @Callable(i)
          | func default() = {
          |   ${sigVerify(bigComplexity)}
          |   [
-         |     Lease(i.caller, 100)
+         |     Reissue(base58'$asset', 1, true)
          |   ]
          | }
        """.stripMargin
@@ -62,18 +60,20 @@ class SyncDAppLeaseBalanceCheckTest extends PropSpec with WithDomain with Transa
       fee     <- ciFee()
       gTx1     = GenesisTransaction.create(invoker.toAddress, ENOUGH_AMT, ts).explicitGet()
       gTx2     = GenesisTransaction.create(dApp1.toAddress, ENOUGH_AMT, ts).explicitGet()
-      gTx3     = GenesisTransaction.create(dApp2.toAddress, fee, ts).explicitGet()
-      ssTx1    = SetScriptTransaction.selfSigned(1.toByte, dApp1, Some(dApp1Script(dApp2.toAddress, bigComplexityDApp1)), fee, ts).explicitGet()
-      ssTx2    = SetScriptTransaction.selfSigned(1.toByte, dApp2, Some(dApp2Script(bigComplexityDApp2)), fee, ts).explicitGet()
+      gTx3     = GenesisTransaction.create(dApp2.toAddress, fee * 3, ts).explicitGet()
+      itx      = IssueTransaction.selfSigned(1.toByte, dApp1, "name", "", 200, 0, true, None, fee, ts).explicitGet()
+      asset    = IssuedAsset(itx.id.value())
+      ssTx1    = SetScriptTransaction.selfSigned(1.toByte, dApp1, Some(dApp1Script(dApp2.toAddress, asset, bigComplexityDApp1)), fee, ts).explicitGet()
+      ssTx2    = SetScriptTransaction.selfSigned(1.toByte, dApp2, Some(dApp2Script(asset, bigComplexityDApp2)), fee, ts).explicitGet()
       invokeTx = InvokeScriptTransaction.selfSigned(TxVersion.V3, invoker, dApp1.toAddress, None, Nil, fee, Waves, ts).explicitGet()
-    } yield (Seq(gTx1, gTx2, gTx3, ssTx1, ssTx2), invokeTx)
+    } yield (Seq(gTx1, gTx2, gTx3, itx, ssTx1, ssTx2), invokeTx)
 
   private val settings =
     TestFunctionalitySettings
       .withFeatures(BlockV5, SynchronousCalls)
       .copy(syncDAppCheckTransfersHeight = 100)
 
-  property("negative balance always rejects tx after syncDAppCheckTransfersHeight") {
+  property("reissue foreign asset") {
     for {
       bigComplexityDApp1 <- Seq(false, true)
       bigComplexityDApp2 <- Seq(false, true)
@@ -85,9 +85,9 @@ class SyncDAppLeaseBalanceCheckTest extends PropSpec with WithDomain with Transa
 
         if (bigComplexityDApp1 || bigComplexityDApp2) {
           d.appendBlock(invoke)
-          d.liquidDiff.errorMessage(invoke.txId).get.text should include("Cannot lease more than own: Balance: 0")
+          d.liquidDiff.errorMessage(invoke.txId).get.text should include("Asset was issued by other address")
         } else {
-          (the[RuntimeException] thrownBy d.appendBlock(invoke)).getMessage should include("Cannot lease more than own: Balance: 0")
+          (the[RuntimeException] thrownBy d.appendBlock(invoke)).getMessage should include("Asset was issued by other address")
           d.appendBlock()
         }
       }
