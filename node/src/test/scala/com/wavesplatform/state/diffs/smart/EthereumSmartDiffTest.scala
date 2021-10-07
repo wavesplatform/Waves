cfdecd31d42d16250c1049adf00c376351b705fb
Artyom Sayadyan <xrtm000@gmail.com>
2021-10-07 17:47:59 +0300

    Test invoke

--- node/src/test/scala/com/wavesplatform/state/diffs/smart/EthereumSmartDiffTest.scala
+++ node/src/test/scala/com/wavesplatform/state/diffs/smart/EthereumSmartDiffTest.scala
@@ -12,7 +12,7 @@ import com.wavesplatform.state.diffs.smart.predef.checkEthTransfer
 import com.wavesplatform.test.{PropSpec, TestTime}
 import com.wavesplatform.transaction.Asset.Waves
 import com.wavesplatform.transaction.smart.SetScriptTransaction
-import com.wavesplatform.transaction.{EthereumTransaction, GenesisTransaction}
+import com.wavesplatform.transaction.{ABIConverter, EthereumTransaction, GenesisTransaction}
 import com.wavesplatform.utils.EthHelpers
 
 class EthereumSmartDiffTest extends PropSpec with WithDomain with EthHelpers {
@@ -30,6 +30,15 @@ class EthereumSmartDiffTest extends PropSpec with WithDomain with EthHelpers {
      """.stripMargin
   )
 
+  private val expectedArg = 1
+
+  private def dAppScript() = TestCompiler(V6).compileContract(
+    s"""
+       | @Callable(i)
+       | func default(a: Int) = [ BooleanEntry("check", a == $expectedArg) ]
+     """.stripMargin
+  )
+
   private val scenario =
     for {
       fee       <- ciFee()
@@ -41,6 +50,20 @@ class EthereumSmartDiffTest extends PropSpec with WithDomain with EthHelpers {
       verifier    = Some(script(ethTransfer, recipient.toAddress))
       setVerifier = () => SetScriptTransaction.selfSigned(1.toByte, recipient, verifier, fee, ts).explicitGet()
     } yield (Seq(gTx1, gTx2, setVerifier()), ethTransfer, setVerifier(), recipient.toAddress)
+
+  private val invokeScenario =
+    for {
+      fee  <- ciFee()
+      dApp <- accountGen
+      script    = dAppScript()
+      id        = ABIConverter(script).funcByMethodId.collectFirst { case (id, f) if f.name == "default" => id.drop(2) }.get
+      data      = s"${id}000000000000000000000000000000000000000000000000000000000000000${expectedArg}00000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000000"
+      invoke    = EthereumTransaction.Invocation(dApp.toAddress, data)
+      ethInvoke = EthereumTransaction(invoke, TestEthUnderlying, TestEthSignature, 'T'.toByte)
+      gTx1      = GenesisTransaction.create(ethInvoke.senderAddress(), ENOUGH_AMT, ts).explicitGet()
+      gTx2      = GenesisTransaction.create(dApp.toAddress, ENOUGH_AMT, ts).explicitGet()
+      setDApp   = SetScriptTransaction.selfSigned(1.toByte, dApp, Some(script), fee, ts).explicitGet()
+    } yield (Seq(gTx1, gTx2, setDApp), ethInvoke, dApp.toAddress)
 
   property("transferTransactionById") {
     val (preparingTxs, ethTransfer, checkTx, recipient) = scenario.sample.get
@@ -54,6 +77,16 @@ class EthereumSmartDiffTest extends PropSpec with WithDomain with EthHelpers {
       d.appendBlock()
       d.appendBlock(checkTx)
       d.liquidDiff.scriptsRun shouldBe 1
+    }
+  }
+
+  property("invoke") {
+    val (preparingTxs, ethInvoke, dApp) = invokeScenario.sample.get
+    withDomain(RideV6) { d =>
+      d.appendBlock(preparingTxs: _*)
+      d.appendBlock(ethInvoke)
+      d.liquidDiff.scriptsRun shouldBe 1
+      d.liquidDiff.accountData(dApp).data("check").value shouldBe true
     }
   }
 }
