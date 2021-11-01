7fc71bd605f7add63255d4c543aa3eafd1920a41
Sergey Nazarov <snazarov@web3tech.ru>
2021-11-01 13:43:12 +0300

    Fixed tests

--- node-it/src/test/scala/com/wavesplatform/it/sync/transactions/RebroadcastTransactionSuite.scala
+++ node-it/src/test/scala/com/wavesplatform/it/sync/transactions/RebroadcastTransactionSuite.scala
@@ -21,51 +21,52 @@ class RebroadcastTransactionSuite extends BaseTransactionSuite with NodesFromDoc
   override protected def nodeConfigs: Seq[Config] =
     Seq(configWithRebroadcastAllowed.withFallback(Miners.head), configWithRebroadcastAllowed.withFallback(NotMiner))
 
-  private def nodeA: Node = nodes.head
-  private def nodeB: Node = nodes.last
+  private def nodeAIsMiner: Node = nodes.head
+  private def nodeBIsNotMiner: Node = nodes.last
 
   test("should rebroadcast a transaction if that's allowed in config") {
-    val tx = TransferTransaction.selfSigned(2.toByte, nodeA.keyPair, Address.fromString(nodeB.address).explicitGet(), Waves, transferAmount, Waves, minFee, ByteStr.empty,  System.currentTimeMillis())
+    val tx = TransferTransaction.selfSigned(2.toByte, nodeAIsMiner.keyPair, Address.fromString(nodeBIsNotMiner.address).explicitGet(), Waves, transferAmount, Waves, minFee, ByteStr.empty,  System.currentTimeMillis())
       .explicitGet()
       .json()
 
-    val dockerNodeBId = docker.stopContainer(dockerNodes.apply().last)
-    val txId          = nodeA.signedBroadcast(tx).id
-    docker.startContainer(dockerNodeBId)
-    nodeA.waitForPeers(1)
+    val dockerNodeAId = docker.stopContainer(dockerNodes().head)
+    val txId          = nodeBIsNotMiner.signedBroadcast(tx).id
+    docker.startContainer(dockerNodeAId)
+    nodeBIsNotMiner.waitForPeers(1)
 
-    nodeB.ensureTxDoesntExist(txId)
-    nodeA.signedBroadcast(tx)
-    nodeB.waitForUtxIncreased(0)
-    nodeB.utxSize shouldBe 1
+    nodeAIsMiner.ensureTxDoesntExist(txId)
+    nodeBIsNotMiner.signedBroadcast(tx)
+    nodeAIsMiner.waitForUtxIncreased(0)
+    nodeAIsMiner.utxSize shouldBe 1
   }
 
   test("should not rebroadcast a transaction if that's not allowed in config") {
     dockerNodes().foreach(docker.restartNode(_, configWithRebroadcastNotAllowed))
+
     val tx = TransferTransaction
-      .selfSigned(2.toByte, nodeA.keyPair, Address.fromString(nodeB.address).explicitGet(), Waves, transferAmount, Waves, minFee, ByteStr.empty,  System.currentTimeMillis())
+      .selfSigned(2.toByte, nodeAIsMiner.keyPair, Address.fromString(nodeBIsNotMiner.address).explicitGet(), Waves, transferAmount, Waves, minFee, ByteStr.empty,  System.currentTimeMillis())
       .explicitGet()
       .json()
 
-    val dockerNodeBId = docker.stopContainer(dockerNodes.apply().last)
-    val txId          = nodeA.signedBroadcast(tx).id
-    docker.startContainer(dockerNodeBId)
-    nodeA.waitForPeers(1)
+    val dockerNodeAId = docker.stopContainer(dockerNodes().head)
+    val txId          = nodeBIsNotMiner.signedBroadcast(tx).id
+    docker.startContainer(dockerNodeAId)
+    nodeBIsNotMiner.waitForPeers(1)
 
-    nodeB.ensureTxDoesntExist(txId)
-    nodeA.signedBroadcast(tx)
+    nodeAIsMiner.ensureTxDoesntExist(txId)
+    nodeBIsNotMiner.signedBroadcast(tx)
     nodes.waitForHeightArise()
-    nodeB.utxSize shouldBe 0
-    nodeB.ensureTxDoesntExist(txId)
+    nodeAIsMiner.utxSize shouldBe 0
+    nodeAIsMiner.ensureTxDoesntExist(txId)
   }
 
   test("should not broadcast a transaction if there are not enough peers") {
     val tx = TransferTransaction
-      .selfSigned(2.toByte, nodeA.keyPair, Address.fromString(nodeB.address).explicitGet(), Waves, transferAmount, Waves, minFee, ByteStr.empty,  System.currentTimeMillis())
+      .selfSigned(2.toByte, nodeAIsMiner.keyPair, Address.fromString(nodeBIsNotMiner.address).explicitGet(), Waves, transferAmount, Waves, minFee, ByteStr.empty,  System.currentTimeMillis())
       .explicitGet()
       .json()
 
-    val testNode = dockerNodes().head
+    val testNode = dockerNodes().last
     try {
       docker.restartNode(testNode, configWithMinimumPeers(999))
       assertApiError(testNode.signedBroadcast(tx), CustomValidationError("There are not enough connections with peers \\(\\d+\\) to accept transaction").assertiveRegex)
