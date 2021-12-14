603d250387a76d3c279968c41a47cd4f9cbc0bcd
Karasiq <yoba123@yandex.ru>
2021-12-14 18:50:19 +0300

    Fix ContractCompilerTest

--- lang/tests/src/test/scala/com/wavesplatform/lang/compiler/ContractCompilerTest.scala
+++ lang/tests/src/test/scala/com/wavesplatform/lang/compiler/ContractCompilerTest.scala
@@ -6,6 +6,7 @@ import com.google.protobuf.ByteString
 import com.wavesplatform.common.state.ByteStr
 import com.wavesplatform.common.utils.EitherExt2
 import com.wavesplatform.lang.Global
+import com.wavesplatform.lang.contract.DApp
 import com.wavesplatform.lang.contract.DApp._
 import com.wavesplatform.lang.directives.DirectiveSet
 import com.wavesplatform.lang.directives.values.{DApp => DAppType, _}
