8934399eb9a49602e610bc2d906595204c7c21af
Artyom Sayadyan <xrtm000@gmail.com>
2022-03-09 09:29:43 +0300

    Test Portfolio.combine()

--- node/src/test/scala/com/wavesplatform/state/PortfolioTest.scala
+++ node/src/test/scala/com/wavesplatform/state/PortfolioTest.scala
@@ -2,7 +2,7 @@ package com.wavesplatform.state
 
 import com.wavesplatform.TestValues
 import com.wavesplatform.common.state.ByteStr
-import com.wavesplatform.test.{FunSuite, produce}
+import com.wavesplatform.test.FunSuite
 import com.wavesplatform.transaction.Asset.IssuedAsset
 
 import java.nio.charset.StandardCharsets
@@ -46,10 +46,34 @@ class PortfolioTest extends FunSuite {
     p.balance shouldBe 0
   }
 
+  test("prevents overflow of Waves") {
+    Portfolio(Long.MaxValue - 1L).combine(Portfolio(Long.MaxValue - 2L)) shouldBe Left("Portfolio balance sum overflow")
+  }
+
   test("prevents overflow of assets") {
     val assetId = TestValues.asset
     val arg1    = Portfolio(0L, LeaseBalance.empty, Map(assetId -> (Long.MaxValue - 1L)))
     val arg2    = Portfolio(0L, LeaseBalance.empty, Map(assetId -> (Long.MaxValue - 2L)))
-    arg1.combine(arg2) should produce("Portfolio assets balance sum overflow")
+    arg1.combine(arg2) shouldBe Left("Portfolio assets balance sum overflow")
+  }
+
+  test("prevents overflow of lease in balances") {
+    val arg1 = Portfolio(0L, LeaseBalance(in = Long.MaxValue - 1L, out = 0), Map())
+    val arg2 = Portfolio(0L, LeaseBalance(in = Long.MaxValue - 1L, out = 0), Map())
+    arg1.combine(arg2) shouldBe Left("Lease in sum overflow")
+  }
+
+  test("prevents overflow of lease out balances") {
+    val arg1 = Portfolio(0L, LeaseBalance(out = Long.MaxValue - 1L, in = 0), Map())
+    val arg2 = Portfolio(0L, LeaseBalance(out = Long.MaxValue - 1L, in = 0), Map())
+    arg1.combine(arg2) shouldBe Left("Lease out sum overflow")
+  }
+
+  test("prevents overflow of effective balance") {
+    Portfolio(
+      Long.MaxValue - 2L,
+      LeaseBalance(in = Long.MaxValue - 1L, out = 0),
+      Map()
+    ).effectiveBalance shouldBe Left("Effective balance sum overflow")
   }
 }
