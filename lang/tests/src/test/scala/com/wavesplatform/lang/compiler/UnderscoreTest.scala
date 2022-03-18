56bc66c20260cd6e44a4a0902f618fb5684ae252
Artyom Sayadyan <xrtm000@gmail.com>
2022-03-18 14:00:18 +0300

    Improve UnderscoreTest

--- lang/tests/src/test/scala/com/wavesplatform/lang/compiler/UnderscoreTest.scala
+++ lang/tests/src/test/scala/com/wavesplatform/lang/compiler/UnderscoreTest.scala
@@ -1,8 +1,9 @@
 package com.wavesplatform.lang.compiler
 
-import com.wavesplatform.lang.directives.values.{Expression, V3}
+import com.wavesplatform.common.utils.EitherExt2
+import com.wavesplatform.lang.directives.values.{Expression, V3, V5}
 import com.wavesplatform.lang.utils._
-import com.wavesplatform.lang.v1.FunctionHeader.User
+import com.wavesplatform.lang.v1.FunctionHeader.{Native, User}
 import com.wavesplatform.lang.v1.compiler.Terms._
 import com.wavesplatform.lang.v1.compiler.{Decompiler, ExpressionCompiler}
 import com.wavesplatform.lang.v1.evaluator.ctx.impl.PureContext
@@ -10,7 +11,7 @@ import com.wavesplatform.test.{PropSpec, _}
 
 class UnderscoreTest extends PropSpec {
   private def compile(script: String): Either[String, EXPR] =
-    ExpressionCompiler.compile(script, compilerContext(V3, Expression, false)).map(_._1)
+    ExpressionCompiler.compile(script, compilerContext(V5, Expression, false)).map(_._1)
 
   private def assert(a: String, f: String, x: String): Either[String, EXPR] = {
     val script =
@@ -53,8 +54,26 @@ class UnderscoreTest extends PropSpec {
     assert("_aa_a_a_", "_ff_f_f_", "_xx_x_x__") should produce("Parsed.Failure")
   }
 
-  property("internal functions can't be used by user") {
+  property("internal functions can't be used directly") {
     compile(" %Tuple2(1, 1) ") should produce("can't parse the expression")
     compile(""" %isInstanceOf(1, "Int") """) should produce("can't parse the expression")
+  }
+
+  property("internal functions names don't affect compiled expression") {
+    compile("(1, 1)") shouldBe Right(FUNCTION_CALL(Native(1300), List(CONST_LONG(1), CONST_LONG(1))))
+    compile(
+      """ match 1 {
+        |   case _: Int => true
+        | }
+      """.stripMargin
+    ) shouldBe Right(
+      LET_BLOCK(
+        LET("$match0", CONST_LONG(1)),
+        IF(
+          FUNCTION_CALL(Native(1), List(REF("$match0"), CONST_STRING("Int").explicitGet())), // Native(1) is isInstanceOf
+          CONST_BOOLEAN(true),
+          FUNCTION_CALL(Native(2), List(CONST_STRING("Match error").explicitGet()))
+        )
+      ))
   }
 }
