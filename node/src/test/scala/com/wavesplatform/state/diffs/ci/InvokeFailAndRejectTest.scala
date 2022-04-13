2f3e8deb246e2ad73810768ae46f46a610b44996
Artyom Sayadyan <xrtm000@gmail.com>
2022-04-13 16:34:29 +0300

    Test invoke with payment failing by ScriptTransfer script

--- node/src/test/scala/com/wavesplatform/state/diffs/ci/InvokeFailAndRejectTest.scala
+++ node/src/test/scala/com/wavesplatform/state/diffs/ci/InvokeFailAndRejectTest.scala
@@ -3,6 +3,9 @@ package com.wavesplatform.state.diffs.ci
 import com.wavesplatform.db.WithDomain
 import com.wavesplatform.db.WithState.AddrWithBalance
 import com.wavesplatform.lang.directives.values.StdLibVersion.V5
+import com.wavesplatform.lang.directives.values.V3
+import com.wavesplatform.lang.script.v1.ExprScript.ExprScriptImpl
+import com.wavesplatform.lang.v1.compiler.Terms.TRUE
 import com.wavesplatform.lang.v1.compiler.TestCompiler
 import com.wavesplatform.test.PropSpec
 import com.wavesplatform.transaction.Asset.IssuedAsset
@@ -54,6 +57,28 @@ class InvokeFailAndRejectTest extends PropSpec with WithDomain {
       d.appendBlock(setScript(secondSigner, dApp))
       d.appendBlock(invokeTx)
       d.liquidDiff.errorMessage(invokeTx.id()).get.text should include(s"Transaction is not allowed by script of the asset $asset")
+    }
+  }
+
+  property("invoke with ScriptTransfer fails by payment script") {
+    withDomain(RideV5, AddrWithBalance.enoughBalances(secondSigner)) { d =>
+      val failAssetIssue = issue(script = Some(assetFailScript))
+      val trueAssetIssue = issue(secondSigner, script = Some(ExprScriptImpl(V3, TRUE)))
+      val failAsset      = IssuedAsset(failAssetIssue.id())
+      val trueAsset      = IssuedAsset(trueAssetIssue.id())
+      val dApp = TestCompiler(V5).compileContract(
+        s"""
+           | @Callable(i)
+           | func default() = [
+           |   ScriptTransfer(i.caller, 1, base58'$trueAsset')
+           | ]
+         """.stripMargin
+      )
+      val invokeTx = invoke(secondAddress, payments = Seq(Payment(1, failAsset)))
+      d.appendBlock(failAssetIssue, trueAssetIssue)
+      d.appendBlock(setScript(secondSigner, dApp))
+      d.appendBlock(invokeTx)
+      d.liquidDiff.errorMessage(invokeTx.id()).get.text should include(s"Transaction is not allowed by script of the asset $failAsset")
     }
   }
 }
