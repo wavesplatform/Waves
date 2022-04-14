412bfb86b474d9aadee0088a1ecca56517070198
Artyom Sayadyan <xrtm000@gmail.com>
2022-04-14 06:28:27 +0300

    Add InvokeSponsorFeeTest

---
+++ node/src/test/scala/com/wavesplatform/state/diffs/ci/InvokeSponsorFeeTest.scala
@@ -0,0 +1,54 @@
+package com.wavesplatform.state.diffs.ci
+import com.wavesplatform.db.WithDomain
+import com.wavesplatform.db.WithState.AddrWithBalance
+import com.wavesplatform.lang.directives.values.StdLibVersion.V5
+import com.wavesplatform.lang.directives.values.V3
+import com.wavesplatform.lang.script.v1.ExprScript.ExprScriptImpl
+import com.wavesplatform.lang.v1.compiler.{Terms, TestCompiler}
+import com.wavesplatform.test.PropSpec
+import com.wavesplatform.transaction.Asset.IssuedAsset
+import com.wavesplatform.transaction.TxHelpers.{invoke, issue, secondSigner, setScript}
+
+class InvokeSponsorFeeTest extends PropSpec with WithDomain {
+  import DomainPresets._
+
+  property("invoke fails on sponsorship of foreign asset") {
+    withDomain(RideV5, AddrWithBalance.enoughBalances(secondSigner)) { d =>
+      val issueTx = issue()
+      val asset   = IssuedAsset(issueTx.id())
+      val dApp = TestCompiler(V5).compileContract(
+        s"""
+          | @Callable(i)
+          | func default() = [
+          |   SponsorFee(base58'$asset', 1)
+          | ]
+        """.stripMargin
+      )
+      val invokeTx = invoke()
+      d.appendBlock(issueTx)
+      d.appendBlock(setScript(secondSigner, dApp))
+      d.appendBlock(invokeTx)
+      d.liquidDiff.errorMessage(invokeTx.id()).get.text should include(s"SponsorFee assetId=$asset was not issued from address of current dApp")
+    }
+  }
+
+  property("invoke fails on sponsorship of smart asset") {
+    withDomain(RideV5, AddrWithBalance.enoughBalances(secondSigner)) { d =>
+      val issueTx = issue(secondSigner, script = Some(ExprScriptImpl(V3, Terms.TRUE)))
+      val asset   = IssuedAsset(issueTx.id())
+      val dApp = TestCompiler(V5).compileContract(
+        s"""
+          | @Callable(i)
+          | func default() = [
+          |   SponsorFee(base58'$asset', 1)
+          | ]
+        """.stripMargin
+      )
+      val invokeTx = invoke()
+      d.appendBlock(issueTx)
+      d.appendBlock(setScript(secondSigner, dApp))
+      d.appendBlock(invokeTx)
+      d.liquidDiff.errorMessage(invokeTx.id()).get.text should include(s"Sponsorship smart assets is disabled")
+    }
+  }
+}
