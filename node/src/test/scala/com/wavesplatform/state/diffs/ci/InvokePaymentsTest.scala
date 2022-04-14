9c0e2d7d39fef66bfba3f77779c5198836529dc2
Artyom Sayadyan <xrtm000@gmail.com>
2022-04-15 00:32:43 +0300

    Test that invoke allowed if Transfer Transaction is prohibited in payment asset

---
+++ node/src/test/scala/com/wavesplatform/state/diffs/ci/InvokePaymentsTest.scala
@@ -0,0 +1,38 @@
+package com.wavesplatform.state.diffs.ci
+import com.wavesplatform.db.WithDomain
+import com.wavesplatform.db.WithState.AddrWithBalance
+import com.wavesplatform.lang.directives.values.StdLibVersion.V5
+import com.wavesplatform.lang.v1.compiler.TestCompiler
+import com.wavesplatform.test.PropSpec
+import com.wavesplatform.transaction.Asset.IssuedAsset
+import com.wavesplatform.transaction.TxHelpers._
+import com.wavesplatform.transaction.smart.InvokeScriptTransaction.Payment
+
+class InvokePaymentsTest extends PropSpec with WithDomain {
+  import DomainPresets._
+
+  property("invoke allowed if Transfer Transaction is prohibited in payment asset") {
+    withDomain(RideV5, AddrWithBalance.enoughBalances(secondSigner)) { d =>
+      val assetScript = TestCompiler(V5).compileExpression(
+        """
+          | match tx {
+          |   case tx: TransferTransaction => false
+          |   case _                       => true
+          | }
+        """.stripMargin
+      )
+      val dApp = TestCompiler(V5).compileContract(
+        """
+          | @Callable(i)
+          | func default() = []
+        """.stripMargin
+      )
+      val issueTx = issue(script = Some(assetScript))
+      val asset   = IssuedAsset(issueTx.id())
+      d.appendBlock(setScript(secondSigner, dApp))
+      d.appendBlock(issueTx)
+      d.appendBlock(invoke(payments = Seq(Payment(1, asset))))
+      d.liquidDiff.scriptResults.head._2.error shouldBe None
+    }
+  }
+}
