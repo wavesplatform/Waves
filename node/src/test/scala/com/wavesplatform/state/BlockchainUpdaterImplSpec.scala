b8cbe173d3f28664b06a3a8f36736e5fb6788e7f
Artyom Sayadyan <xrtm000@gmail.com>
2022-05-19 17:20:53 +0300

    Fix checkPR error

--- node/src/test/scala/com/wavesplatform/state/BlockchainUpdaterImplSpec.scala
+++ node/src/test/scala/com/wavesplatform/state/BlockchainUpdaterImplSpec.scala
@@ -301,7 +301,7 @@ class BlockchainUpdaterImplSpec extends FreeSpec with EitherMatchers with WithDo
 
     "spendableBalanceChanged" in {
       withLevelDBWriter(RideV6) { levelDb =>
-        val ps    = PublishToOneSubject[(Address, Asset)]
+        val ps    = PublishToOneSubject[(Address, Asset)]()
         val items = ps.toListL.runToFuture
 
         val blockchain = new BlockchainUpdaterImpl(
