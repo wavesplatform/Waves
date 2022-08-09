f4853daf5529f97ef0c06a39b898a4cf49dfa58a
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2022-08-09 17:07:34 +0400

    Compact test cases

--- node/src/test/scala/com/wavesplatform/features/RideV6ActivationTest.scala
+++ node/src/test/scala/com/wavesplatform/features/RideV6ActivationTest.scala
@@ -5,7 +5,7 @@ import com.wavesplatform.common.utils.{Base64, EitherExt2}
 import com.wavesplatform.db.WithDomain
 import com.wavesplatform.db.WithState.AddrWithBalance
 import com.wavesplatform.lang.directives.values.*
-import com.wavesplatform.lang.script.ContractScript
+import com.wavesplatform.lang.script.{ContractScript, Script}
 import com.wavesplatform.lang.v1.compiler.{Terms, TestCompiler}
 import com.wavesplatform.lang.v1.{ContractLimits, FunctionHeader}
 import com.wavesplatform.test.*
@@ -20,237 +20,120 @@ class RideV6ActivationTest extends FreeSpec with WithDomain with OptionValues {
   private val chainId = DomainPresets.SettingsFromDefaultConfig.blockchainSettings.addressSchemeCharacter.toByte
 
   "After RideV6 activation" - {
-    "NODE-540 If a transaction exceeds the limit of writes number" - {
-      "<=1000 complexity - rejected" - Seq(110, ContractLimits.FailFreeInvokeComplexity).foreach { complexity =>
-        s"complexity = $complexity" in withDomain(DomainPresets.RideV6, AddrWithBalance.enoughBalances(defaultSigner)) { d =>
-          val setScriptTx = SetScriptTransaction
-            .selfSigned(TxVersion.V3, defaultSigner, Some(mkDApp(complexity)), 0.01.waves, System.currentTimeMillis())
-            .explicitGet()
-          d.appendBlock(setScriptTx)
-
-          d.createDiffE(invokeTx) should produce("Stored data count limit is exceeded")
+    val cases = Seq(
+      Case(
+        "NODE-540 If a transaction exceeds the limit of writes number",
+        "Stored data count limit is exceeded",
+        { complexity =>
+          val baseComplexity = 101 + 101 // Because we spend 101 on IntegerEntry and 101 on a list construction
+          mkV6ContractScript(
+            s""" let x = ${mkExprWithComplexity(complexity - baseComplexity)}
+               | ${(1 to 101).map(i => s"""IntegerEntry("i$i", x)""").mkString("[", ", ", "]")}
+               | """.stripMargin
+          )
         }
-      }
-
-      ">1000 complexity - failed" in {
-        withDomain(DomainPresets.RideV6, AddrWithBalance.enoughBalances(defaultSigner)) { d =>
-          val complexity = ContractLimits.FailFreeInvokeComplexity + 1
-          val setScriptTx = SetScriptTransaction
-            .selfSigned(TxVersion.V3, defaultSigner, Some(mkDApp(complexity)), 0.01.waves, System.currentTimeMillis())
-            .explicitGet()
-          d.appendBlock(setScriptTx)
-
-          d.appendBlock(invokeTx)
-          val invokeTxMeta = d.transactionsApi.transactionById(invokeTx.id()).value
-          invokeTxMeta.spentComplexity shouldBe complexity
-          invokeTxMeta.succeeded shouldBe false
+      ),
+      Case(
+        "NODE-542 If a transaction exceeds the limit of writes size",
+        "WriteSet size can't exceed 5120 bytes, actual: 5121 bytes",
+        { complexity =>
+          val baseComplexity = 2 + 2 // Because we spend 2 on BinaryEntry and IntegerEntry and 101 on a list construction
+          // See DataTxValidator.realUserPayloadSize: IntegerDataEntry - 8, "b" and "i" keys - 2
+          val limitedBinaryEntrySize = ContractLimits.MaxWriteSetSizeInBytes - 8 - 2
+          mkV6ContractScript(
+            s""" let x = ${mkExprWithComplexity(complexity - baseComplexity)}
+               | [
+               |   BinaryEntry("b", base64'${Base64.encode(Array.fill[Byte](limitedBinaryEntrySize + 1)(0))}'),
+               |   IntegerEntry("i", x)
+               | ]""".stripMargin
+          )
         }
-      }
-
-      def mkDApp(complexity: Int): ContractScript.ContractScriptImpl = {
-        val baseComplexity = 101 + 101 // Because we spend 101 on IntegerEntry and 101 on a list construction
-        TestCompiler(V6).compileContract(
-          s"""
-             |{-#STDLIB_VERSION 6 #-}
-             |{-#SCRIPT_TYPE ACCOUNT #-}
-             |{-#CONTENT_TYPE DAPP #-}
-             |
-             |@Callable(inv)
-             |func foo() = {
-             |  let x = ${mkExprWithComplexity(complexity - baseComplexity)}
-             |  ${(1 to 101).map(i => s"""IntegerEntry("i$i", x)""").mkString("[", ", ", "]")}
-             |}
-        """.stripMargin
-        )
-      }
-    }
-
-    "NODE-542 If a transaction exceeds the limit of writes size" - {
-      "<=1000 complexity - rejected" - Seq(110, ContractLimits.FailFreeInvokeComplexity).foreach { complexity =>
-        s"complexity = $complexity" in withDomain(DomainPresets.RideV6, AddrWithBalance.enoughBalances(defaultSigner)) { d =>
-          val setScriptTx = SetScriptTransaction
-            .selfSigned(TxVersion.V3, defaultSigner, Some(mkDApp(complexity)), 0.011.waves, System.currentTimeMillis())
-            .explicitGet()
-          d.appendBlock(setScriptTx)
-
-          d.createDiffE(invokeTx) should produce("WriteSet size can't exceed 5120 bytes, actual: 5121 bytes")
+      ),
+      Case(
+        "NODE-544 If a transaction exceeds the limit of writes number through WriteSet",
+        "Stored data count limit is exceeded",
+        { complexity =>
+          val baseComplexity = 2 * 101 + 101 + 1 // Because we spend 2*101 on DataEntry and 101 on a list construction and 1 for WriteSet
+          mkV3ContractScript(
+            s""" let x = ${mkExprWithComplexity(complexity - baseComplexity)}
+               | ${(1 to 101).map(i => s"""DataEntry("i$i", x)""").mkString("WriteSet([", ", ", "])")}
+               | """.stripMargin
+          )
         }
-      }
-
-      ">1000 complexity - failed" in {
-        withDomain(DomainPresets.RideV6, AddrWithBalance.enoughBalances(defaultSigner)) { d =>
-          val complexity = ContractLimits.FailFreeInvokeComplexity + 1
-          val setScriptTx = SetScriptTransaction
-            .selfSigned(TxVersion.V3, defaultSigner, Some(mkDApp(complexity)), 0.011.waves, System.currentTimeMillis())
-            .explicitGet()
-          d.appendBlock(setScriptTx)
-
-          d.appendBlock(invokeTx)
-          val invokeTxMeta = d.transactionsApi.transactionById(invokeTx.id()).value
-          invokeTxMeta.spentComplexity shouldBe complexity
-          invokeTxMeta.succeeded shouldBe false
+      ),
+      Case(
+        "NODE-546 If a transaction tries to write an empty key to the state",
+        "Empty keys aren't allowed in tx version >= 2",
+        { complexity =>
+          val baseComplexity = 1 + 1 // Because we spend 1 on IntegerEntry and 1 on a list construction
+          mkV6ContractScript(
+            s"""
+               | let x = ${mkExprWithComplexity(complexity - baseComplexity)}
+               | [IntegerEntry("", x)]
+               |""".stripMargin
+          )
         }
-      }
-
-      def mkDApp(complexity: Int): ContractScript.ContractScriptImpl = {
-        val baseComplexity = 2 + 2 // Because we spend 2 on BinaryEntry and IntegerEntry and 101 on a list construction
-        // See DataTxValidator.realUserPayloadSize: IntegerDataEntry - 8, "b" and "i" keys - 2
-        val limitedBinaryEntrySize = ContractLimits.MaxWriteSetSizeInBytes - 8 - 2
-        TestCompiler(V6).compileContract(
-          s"""
-             |{-#STDLIB_VERSION 6 #-}
-             |{-#SCRIPT_TYPE ACCOUNT #-}
-             |{-#CONTENT_TYPE DAPP #-}
-             |
-             |@Callable(inv)
-             |func foo() = {
-             |  let x = ${mkExprWithComplexity(complexity - baseComplexity)}
-             |  [
-             |    BinaryEntry("b", base64'${Base64.encode(Array.fill[Byte](limitedBinaryEntrySize + 1)(0))}'),
-             |    IntegerEntry("i", x)
-             |  ]
-             |}
-        """.stripMargin
-        )
-      }
-    }
-
-    "NODE-544 If a transaction exceeds the limit of writes number through WriteSet" - {
-      "<=1000 complexity - rejected" - Seq(110, ContractLimits.FailFreeInvokeComplexity).foreach { complexity =>
-        s"complexity = $complexity" in withDomain(DomainPresets.RideV6, AddrWithBalance.enoughBalances(defaultSigner)) { d =>
-          val setScriptTx = SetScriptTransaction
-            .selfSigned(TxVersion.V3, defaultSigner, Some(mkDApp(complexity)), 0.021.waves, System.currentTimeMillis())
-            .explicitGet()
-          d.appendBlock(setScriptTx)
-
-          d.createDiffE(invokeTx) should produce("Stored data count limit is exceeded")
+      ),
+      Case(
+        "NODE-548 If a transaction exceeds the limit of non-data actions",
+        "ScriptTransfer, Lease, LeaseCancel actions count limit is exceeded",
+        { complexity =>
+          val baseComplexity = 101 + 101 + 1 // Because we spend 101 on ScriptTransfer, 101 on a list construction and 1 for Address
+          mkV6ContractScript(
+            s""" let to = Address(base58'${defaultSigner.toAddress(chainId)}')
+               | let x = ${mkExprWithComplexity(complexity - baseComplexity)}
+               | ${(1 to 101).map(i => s"""ScriptTransfer(to, x, unit)""").mkString("[", ", ", "]")}
+               | """.stripMargin
+          )
         }
-      }
+      )
+    )
 
-      ">1000 complexity - failed" in {
-        withDomain(DomainPresets.RideV6, AddrWithBalance.enoughBalances(defaultSigner)) { d =>
-          val complexity = ContractLimits.FailFreeInvokeComplexity + 1
-          val setScriptTx = SetScriptTransaction
-            .selfSigned(TxVersion.V3, defaultSigner, Some(mkDApp(complexity)), 0.021.waves, System.currentTimeMillis())
-            .explicitGet()
-          d.appendBlock(setScriptTx)
-
-          d.appendBlock(invokeTx)
-          val invokeTxMeta = d.transactionsApi.transactionById(invokeTx.id()).value
-          invokeTxMeta.spentComplexity shouldBe complexity
-          invokeTxMeta.succeeded shouldBe false
+    cases.foreach { testCase =>
+      testCase.title - {
+        "<=1000 complexity - rejected" - Seq(110, ContractLimits.FailFreeInvokeComplexity).foreach { complexity =>
+          s"complexity = $complexity" in withDomain(DomainPresets.RideV6, AddrWithBalance.enoughBalances(defaultSigner)) { d =>
+            val setScriptTx = SetScriptTransaction
+              .selfSigned(TxVersion.V3, defaultSigner, Some(testCase.mkDApp(complexity)), 1.waves, System.currentTimeMillis())
+              .explicitGet()
+            d.appendBlock(setScriptTx)
+            d.createDiffE(invokeTx) should produce(testCase.rejectError)
+          }
         }
-      }
-
-      def mkDApp(complexity: Int): ContractScript.ContractScriptImpl = {
-        val baseComplexity = 2 * 101 + 101 + 1 // Because we spend 2*101 on DataEntry and 101 on a list construction and 1 for WriteSet
-        TestCompiler(V3).compileContract(
-          s"""
-             |{-#STDLIB_VERSION 3 #-}
-             |{-#SCRIPT_TYPE ACCOUNT #-}
-             |{-#CONTENT_TYPE DAPP #-}
-             |
-             |@Callable(inv)
-             |func foo() = {
-             |  let x = ${mkExprWithComplexity(complexity - baseComplexity)}
-             |  ${(1 to 101).map(i => s"""DataEntry("i$i", x)""").mkString("WriteSet([", ", ", "])")}
-             |}
-        """.stripMargin
-        )
-      }
-    }
-
-    "NODE-546 If a transaction tries to write an empty key to the state" - {
-      "<=1000 complexity - rejected" - Seq(110, ContractLimits.FailFreeInvokeComplexity).foreach { complexity =>
-        s"complexity = $complexity" in withDomain(DomainPresets.RideV6, AddrWithBalance.enoughBalances(defaultSigner)) { d =>
-          val setScriptTx = SetScriptTransaction
-            .selfSigned(TxVersion.V3, defaultSigner, Some(mkDApp(complexity)), 0.01.waves, System.currentTimeMillis())
-            .explicitGet()
-          d.appendBlock(setScriptTx)
-
-          d.createDiffE(invokeTx) should produce("Empty keys aren't allowed in tx version >= 2")
+        ">1000 complexity - failed" in {
+          withDomain(DomainPresets.RideV6, AddrWithBalance.enoughBalances(defaultSigner)) { d =>
+            val complexity = ContractLimits.FailFreeInvokeComplexity + 1
+            val setScriptTx = SetScriptTransaction
+              .selfSigned(TxVersion.V3, defaultSigner, Some(testCase.mkDApp(complexity)), 1.waves, System.currentTimeMillis())
+              .explicitGet()
+            d.appendBlock(setScriptTx)
+            d.appendBlock(invokeTx)
+            val invokeTxMeta = d.transactionsApi.transactionById(invokeTx.id()).value
+            invokeTxMeta.spentComplexity shouldBe complexity
+            invokeTxMeta.succeeded shouldBe false
+          }
         }
-      }
-
-      ">1000 complexity - failed" in {
-        withDomain(DomainPresets.RideV6, AddrWithBalance.enoughBalances(defaultSigner)) { d =>
-          val complexity = ContractLimits.FailFreeInvokeComplexity + 1
-          val setScriptTx = SetScriptTransaction
-            .selfSigned(TxVersion.V3, defaultSigner, Some(mkDApp(complexity)), 0.01.waves, System.currentTimeMillis())
-            .explicitGet()
-          d.appendBlock(setScriptTx)
-
-          d.appendBlock(invokeTx)
-          val invokeTxMeta = d.transactionsApi.transactionById(invokeTx.id()).value
-          invokeTxMeta.spentComplexity shouldBe complexity
-          invokeTxMeta.succeeded shouldBe false
-        }
-      }
-
-      def mkDApp(complexity: Int): ContractScript.ContractScriptImpl = {
-        val baseComplexity = 1 + 1 // Because we spend 1 on IntegerEntry and 1 on a list construction
-        TestCompiler(V6).compileContract(
-          s"""
-             |{-#STDLIB_VERSION 6 #-}
-             |{-#SCRIPT_TYPE ACCOUNT #-}
-             |{-#CONTENT_TYPE DAPP #-}
-             |
-             |@Callable(inv)
-             |func foo() = {
-             |  let x = ${mkExprWithComplexity(complexity - baseComplexity)}
-             |  [IntegerEntry("", x)]
-             |}
-        """.stripMargin
-        )
-      }
-    }
-
-    "NODE-548 If a transaction exceeds the limit of non-data actions" - {
-      "<=1000 complexity - rejected" - Seq(110, ContractLimits.FailFreeInvokeComplexity).foreach { complexity =>
-        s"complexity = $complexity" in withDomain(DomainPresets.RideV6, AddrWithBalance.enoughBalances(defaultSigner)) { d =>
-          val setScriptTx = SetScriptTransaction
-            .selfSigned(TxVersion.V3, defaultSigner, Some(mkDApp(complexity)), 0.01.waves, System.currentTimeMillis())
-            .explicitGet()
-          d.appendBlock(setScriptTx)
-
-          d.createDiffE(invokeTx) should produce("ScriptTransfer, Lease, LeaseCancel actions count limit is exceeded")
-        }
-      }
-
-      ">1000 complexity - failed" in {
-        withDomain(DomainPresets.RideV6, AddrWithBalance.enoughBalances(defaultSigner)) { d =>
-          val complexity = ContractLimits.FailFreeInvokeComplexity + 1
-          val setScriptTx = SetScriptTransaction
-            .selfSigned(TxVersion.V3, defaultSigner, Some(mkDApp(complexity)), 0.01.waves, System.currentTimeMillis())
-            .explicitGet()
-          d.appendBlock(setScriptTx)
-
-          d.appendBlock(invokeTx)
-          val invokeTxMeta = d.transactionsApi.transactionById(invokeTx.id()).value
-          invokeTxMeta.spentComplexity shouldBe complexity
-          invokeTxMeta.succeeded shouldBe false
-        }
-      }
-
-      def mkDApp(complexity: Int): ContractScript.ContractScriptImpl = {
-        val baseComplexity = 101 + 101 + 1 // Because we spend 101 on ScriptTransfer, 101 on a list construction and 1 for Address
-        TestCompiler(V6).compileContract(
-          s"""
-             |{-#STDLIB_VERSION 6 #-}
-             |{-#SCRIPT_TYPE ACCOUNT #-}
-             |{-#CONTENT_TYPE DAPP #-}
-             |
-             |@Callable(inv)
-             |func foo() = {
-             |  let to = Address(base58'${defaultSigner.toAddress(chainId)}')
-             |  let x = ${mkExprWithComplexity(complexity - baseComplexity)}
-             |  ${(1 to 101).map(i => s"""ScriptTransfer(to, x, unit)""").mkString("[", ", ", "]")}
-             |}
-        """.stripMargin
-        )
       }
     }
   }
+
+  private case class Case(title: String, rejectError: String, mkDApp: Int => Script)
+
+  private def mkV3ContractScript(funBody: String): Script = mkContractScript(V3, funBody)
+  private def mkV6ContractScript(funBody: String): Script = mkContractScript(V6, funBody)
+  private def mkContractScript(v: StdLibVersion, funBody: String): Script =
+    TestCompiler(v).compileContract(
+      s"""
+         |{-#STDLIB_VERSION ${v.id} #-}
+         |{-#SCRIPT_TYPE ACCOUNT #-}
+         |{-#CONTENT_TYPE DAPP #-}
+         |
+         |@Callable(inv)
+         |func foo() = {
+         |  $funBody
+         |}
+      """.stripMargin
+    )
 
   private def mkExprWithComplexity(complexity: Int): String = s"1 ${"+ 1" * complexity}"
 
