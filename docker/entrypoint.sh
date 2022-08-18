4d6672ebde8a61935258800464fc7c51aa9af1e7
Ivan Mashonskii <imashonskii@web3tech.ru>
2022-08-18 18:37:52 +0300

    NODE-2358 Scripts in Dockerfile

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,8 +5,6 @@ logEcho() {
   echo $1 | gosu waves tee -a /var/log/waves/waves.log
 }
 
-[ -n "${WAVES_WALLET_PASSWORD}" ] && OPTIONAL_ARGS="$OPTIONAL_ARGS -Dwaves.wallet.password=$WAVES_WALLET_PASSWORD"
-[ -n "${WAVES_WALLET_SEED}" ] && OPTIONAL_ARGS="$OPTIONAL_ARGS -Dwaves.wallet.seed=$WAVES_WALLET_SEED"
 JAVA_OPTS="${JAVA_OPTS} -Dwaves.defaults.blockchain.type=$WAVES_NETWORK"
 
 logEcho "Node is starting..."
@@ -19,7 +17,6 @@ JAVA_OPTS="-Dlogback.stdout.level=${WAVES_LOG_LEVEL}
   -Xmx${WAVES_HEAP_SIZE}
   -Dlogback.file.directory=$WVLOG
   -Dconfig.override_with_env_vars=true
-  ${JAVA_OPTS}
-  ${OPTIONAL_ARGS}"
+  ${JAVA_OPTS}"
 
 java $JAVA_OPTS -cp "${WAVES_INSTALL_PATH}/lib/plugins/*:$WAVES_INSTALL_PATH/lib/*" com.wavesplatform.Application "$WAVES_CONFIG"
