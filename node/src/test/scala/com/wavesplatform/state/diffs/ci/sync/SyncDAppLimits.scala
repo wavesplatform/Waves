5805e2c17458f6e8cb4d7d0fcac72e60de57e28c
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2022-08-22 14:42:39 +0400

    Simpler mkIntExprWithComplexity

--- node/src/test/scala/com/wavesplatform/state/diffs/ci/sync/SyncDAppLimits.scala
+++ node/src/test/scala/com/wavesplatform/state/diffs/ci/sync/SyncDAppLimits.scala
@@ -9,14 +9,14 @@ import com.wavesplatform.test.*
 import com.wavesplatform.transaction.TxHelpers
 import org.scalatest.{EitherValues, OptionValues}
 
-import scala.collection.Seq
+import scala.annotation.tailrec
 
 class SyncDAppLimits extends PropSpec with WithDomain with OptionValues with EitherValues {
   private val alice     = TxHelpers.signer(1) // signer(0) forges blocks and this affects the balance
   private val aliceAddr = alice.toAddress
 
   complexityLimitTest("NODE-521 A limit of complexity is 52000 if the first script is V6", V6, 52000)
-  complexityLimitTest("NODE-527 A limit of complexity is 26000 if the first script is V6", V5, 26000)
+  complexityLimitTest("NODE-527 A limit of complexity is 26000 if the first script is V5", V5, 26000)
   private def complexityLimitTest(title: String, v: StdLibVersion, complexityLimit: Int): Unit = {
     property(title) {
       withDomain(DomainPresets.RideV6, AddrWithBalance.enoughBalances(alice)) { d =>
@@ -24,7 +24,8 @@ class SyncDAppLimits extends PropSpec with WithDomain with OptionValues with Eit
           TxHelpers.setScript(
             alice,
             TestCompiler(v).compileContract(
-              // 1 for ">", 1+1 for strict, 75 for invoke, 1 for Address, 1 for "-", 1 for list
+              // 81 = 1 for ">", 1+1 for strict, 75 for invoke, 1 for Address, 1 for "-", 1 for list
+              // One call of foo(n > 0) has 2000 complexity
               s""" @Callable(inv)
                  | func foo(n: Int) = {
                  |   if (n > 0) then {
@@ -56,14 +57,12 @@ class SyncDAppLimits extends PropSpec with WithDomain with OptionValues with Eit
     }
   }
 
-  property("NODE-726 A limit of inner invokes is 100 before the ContinuationTransaction(id=18) feature activation") {
-    // 160 calls, 52000/325 = 160
+  property("NODE-726 A limit of inner invokes is 100 in RideV6") {
     withDomain(DomainPresets.RideV6, AddrWithBalance.enoughBalances(alice)) { d =>
       d.appendBlock(
         TxHelpers.setScript(
           alice,
           TestCompiler(V6).compileContract(
-            // 1 for ">", 1+1 for strict, 75 for invoke, 1 for Address, 1 for "-", 1 for list
             s""" @Callable(inv)
                | func foo(n: Int) = {
                |   if (n > 0) then {
@@ -79,8 +78,7 @@ class SyncDAppLimits extends PropSpec with WithDomain with OptionValues with Eit
         )
       )
 
-      val calls    = 101 // 52000 / 2000 + 1
-      val invokeTx = TxHelpers.invoke(aliceAddr, Some("foo"), Seq(CONST_LONG(calls)), invoker = alice)
+      val invokeTx = TxHelpers.invoke(aliceAddr, Some("foo"), Seq(CONST_LONG(101)), invoker = alice)
 
       val diff              = d.createDiffE(invokeTx).value
       val (_, scriptResult) = diff.scriptResults.headOption.value
@@ -88,27 +86,27 @@ class SyncDAppLimits extends PropSpec with WithDomain with OptionValues with Eit
 
       d.appendBlock(invokeTx)
       val invokeTxMeta = d.transactionsApi.transactionById(invokeTx.id()).value
-      // invokeTxMeta.spentComplexity shouldBe complexityLimit
       invokeTxMeta.succeeded shouldBe false
     }
   }
 
-  // TODO
-  property("NODE-727 A number of inner invokes is unlimited after the ContinuationTransaction(id=18) feature activation") {
-    // 160 calls, 52000/325 = 160
-    withDomain(DomainPresets.ContinuationTransaction, AddrWithBalance.enoughBalances(alice)) { d =>
+  property("NODE-727 A sum of inner invokes is unlimited in RideV6") {
+    withDomain(DomainPresets.RideV6, AddrWithBalance.enoughBalances(alice)) { d =>
       d.appendBlock(
         TxHelpers.setScript(
           alice,
           TestCompiler(V6).compileContract(
             s""" @Callable(inv)
-               | func foo(n: Int) = {
-               |   if (n > 0) then {
-               |     strict res = invoke(Address(base58'$aliceAddr'), "foo", [n - 1], [])
-               |     ([], 0)
-               |   } else {
-               |     ([], 0)
-               |   }
+               | func foo() = {
+               |   strict res1 = invoke(this, "bar", [99], [])
+               |   strict res2 = invoke(this, "bar", [98], [])
+               |   []
+               | }
+               |
+               | @Callable(inv)
+               | func bar(c: Int) = if (c == 0) then [] else {
+               |  strict r  = this.invoke("bar", [c - 1], [])
+               |  []
                | }
                | """.stripMargin
           ),
@@ -116,56 +114,35 @@ class SyncDAppLimits extends PropSpec with WithDomain with OptionValues with Eit
         )
       )
 
-      val calls    = 120 // 52000 / 2000 + 1
-      val invokeTx = TxHelpers.invoke(aliceAddr, Some("foo"), Seq(CONST_LONG(calls)), invoker = alice)
-
-      val diff              = d.createDiffE(invokeTx).value
-      val (_, scriptResult) = diff.scriptResults.headOption.value
-      scriptResult.error.value.text should include("????")
-
+      val invokeTx = TxHelpers.invoke(aliceAddr, Some("foo"), invoker = alice)
       d.appendBlock(invokeTx)
+
       val invokeTxMeta = d.transactionsApi.transactionById(invokeTx.id()).value
-      // invokeTxMeta.spentComplexity shouldBe complexityLimit
-      invokeTxMeta.succeeded shouldBe false
+      invokeTxMeta.succeeded shouldBe true
     }
   }
+
+  private val complexityExamples = Seq(
+    // 1 for toInt, 200 for log, 65+65 for parseBigInt
+    331 -> s"""toInt(log(parseBigIntValue("1625"), 2, parseBigIntValue("27"), 1, 2, HALFUP))""",
+    100 -> s"""log(1625, 2, 27, 1, 2, HALFUP)""",
+    12  -> s"""valueOrElse(getInteger("k"), 0)""" // 2 for valueOrElse, 10 for getInteger
+  )
 
   private def mkIntExprWithComplexity(targetComplexity: Int): String = {
-    if (targetComplexity == 0) "1"
-    else if (targetComplexity < 13) mkIntExprWithComplexityM1(targetComplexity)
-    else if (targetComplexity < 101) {
-      val m13 = mkIntExprWithComplexityM13(targetComplexity / 13)
-      s"$m13 + ${mkIntExprWithComplexity(targetComplexity % 13 - 1)}" // 1 for "+"
-    } else if (targetComplexity < 332) {
-      val m101 = mkIntExprWithComplexityM101(targetComplexity / 101)
-      s"$m101 + ${mkIntExprWithComplexity(targetComplexity % 101 - 1)}" // 1 for "+"
-    } else {
-      val m332 = mkIntExprWithComplexityM332(targetComplexity / 332)
-      s"$m332 + ${mkIntExprWithComplexity(targetComplexity % 332 - 1)}" // 1 for "+"
-    }
-  }
+    @tailrec
+    def loop(restComplexity: Int, restComplexityExamples: Seq[(Int, String)], acc: String): String =
+      if (restComplexity == 0) acc
+      else
+        restComplexityExamples match {
+          case (exprComplexity, expr) +: restXs =>
+            val uncompletedExprComplexity = exprComplexity + 1 // 1 for "+"
+            val times                     = restComplexity / uncompletedExprComplexity
+            loop(restComplexity % uncompletedExprComplexity, restXs, acc + s" + $expr" * times)
 
-  private def mkIntExprWithComplexityM1(n: Int): String = s"1${" + 1" * n}"
+          case Nil => loop(0, Nil, acc + " + 1" * restComplexity)
+        }
 
-  private def mkIntExprWithComplexityM13(n: Int): String = {
-    val baseExpr = s"""log(1625, 2, 27, 1, 2, HALFUP)""" // 2 for valueOrElse, 10 for getInteger
-    if (n <= 0) "1"
-    else if (n == 1) s"$baseExpr + 1" // 13
-    else s"$baseExpr + 1${s" + $baseExpr" * (n - 1)}"
-  }
-
-  private def mkIntExprWithComplexityM101(n: Int): String = {
-    val baseExpr = s"""log(1625, 2, 27, 1, 2, HALFUP)""" // 100
-    if (n <= 0) "1"
-    else if (n == 1) s"$baseExpr + 1" // 101
-    else s"$baseExpr + 1${s" + $baseExpr" * (n - 1)}"
-  }
-
-  private def mkIntExprWithComplexityM332(n: Int): String = {
-    // 331 = 1 for toInt, 200 for log, 65+65 for parseBigInt
-    val baseExpr = s"""toInt(log(parseBigIntValue("1625"), 2, parseBigIntValue("27"), 1, 2, HALFUP))"""
-    if (n <= 0) "1"
-    else if (n == 1) s"$baseExpr + 1"                 // 332
-    else s"$baseExpr + 1${s" + $baseExpr" * (n - 1)}" // 332 * n
+    loop(targetComplexity, complexityExamples, "1")
   }
 }
