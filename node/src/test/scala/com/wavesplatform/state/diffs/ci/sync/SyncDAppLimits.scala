94585b50d2c16d7baa09cb6aacaacce37e2fd324
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2022-08-22 19:31:21 +0400

    Complexity issue - WIP

--- node/src/test/scala/com/wavesplatform/state/diffs/ci/sync/SyncDAppLimits.scala
+++ node/src/test/scala/com/wavesplatform/state/diffs/ci/sync/SyncDAppLimits.scala
@@ -7,40 +7,63 @@ import com.wavesplatform.lang.v1.compiler.Terms.CONST_LONG
 import com.wavesplatform.lang.v1.compiler.TestCompiler
 import com.wavesplatform.test.*
 import com.wavesplatform.transaction.TxHelpers
+import com.wavesplatform.transaction.smart.SetScriptTransaction
 import org.scalatest.{EitherValues, OptionValues}
 
 import scala.annotation.tailrec
+import scala.util.chaining.scalaUtilChainingOps
 
 class SyncDAppLimits extends PropSpec with WithDomain with OptionValues with EitherValues {
   private val alice     = TxHelpers.signer(1) // signer(0) forges blocks and this affects the balance
   private val aliceAddr = alice.toAddress
 
-  complexityLimitTest("NODE-521 A limit of complexity is 52000 if the first script is V6", V6, 52000)
-  complexityLimitTest("NODE-527 A limit of complexity is 26000 if the first script is V5", V5, 26000)
-  private def complexityLimitTest(title: String, v: StdLibVersion, complexityLimit: Int): Unit = {
-    property(title) {
+  complexityLimitTests("NODE-521 A limit of complexity is 52000 if the first script is V6", V6, 52000)
+  complexityLimitTests("NODE-527 A limit of complexity is 26000 if the first script is V5", V5, 26000)
+  private def complexityLimitTests(title: String, v: StdLibVersion, complexityLimit: Int): Unit = {
+    def mkSetScriptTx(overflow: Boolean): SetScriptTransaction = TxHelpers.setScript(
+      alice,
+      TestCompiler(v).compileContract(
+        // One call of foo has 2000 complexity
+        s""" @Callable(inv)
+           | func foo(n: Int) = {
+           |   # 81 = 1 for strict, 1 for ">", 1 for strict, 75 for invoke, 1 for Address, 1 for list, 1 for "-"
+           |   strict complexInt1 = ${mkIntExprWithComplexity(2000 - 81)}
+           |   if (n > 1) then {
+           |     strict res = invoke(Address(base58'$aliceAddr'), "foo", [n - 1], [])
+           |     []
+           |   } else {
+           |     strict complexInt2 = ${mkIntExprWithComplexity(79 - 1)} # 1 for strict
+           |     []
+           |   }
+           | }
+           | """.stripMargin.tap(println)
+      ),
+      fee = 1.waves
+    )
+
+    property(s"$title - positive") {
       withDomain(DomainPresets.RideV6, AddrWithBalance.enoughBalances(alice)) { d =>
-        d.appendBlock(
-          TxHelpers.setScript(
-            alice,
-            TestCompiler(v).compileContract(
-              // 81 = 1 for ">", 1+1 for strict, 75 for invoke, 1 for Address, 1 for "-", 1 for list
-              // One call of foo(n > 0) has 2000 complexity
-              s""" @Callable(inv)
-                 | func foo(n: Int) = {
-                 |   if (n > 0) then {
-                 |     strict complexInt = ${mkIntExprWithComplexity(2000 - 81)}
-                 |     strict res = invoke(Address(base58'$aliceAddr'), "foo", [n - 1], [])
-                 |     ([], 0)
-                 |   } else {
-                 |     ([], 1 + 1)
-                 |   }
-                 | }
-                 | """.stripMargin
-            ),
-            fee = 1.waves
-          )
-        )
+        d.appendBlock(mkSetScriptTx(overflow = false))
+
+        val calls    = complexityLimit / 2000
+        val invokeTx = TxHelpers.invoke(aliceAddr, Some("foo"), Seq(CONST_LONG(calls)), invoker = alice)
+
+        val diff = d.createDiffE(invokeTx).value
+        println(diff.scriptsComplexity)
+        val (_, scriptResult) = diff.scriptResults.headOption.value
+        println(scriptResult.error)
+
+        d.appendBlock(invokeTx)
+        val invokeTxMeta = d.transactionsApi.transactionById(invokeTx.id()).value
+        invokeTxMeta.spentComplexity shouldBe complexityLimit
+
+        invokeTxMeta.succeeded shouldBe true
+      }
+    }
+
+    property(s"$title - negative") {
+      withDomain(DomainPresets.RideV6, AddrWithBalance.enoughBalances(alice)) { d =>
+        d.appendBlock(mkSetScriptTx(overflow = true))
 
         val calls    = complexityLimit / 2000 + 1
         val invokeTx = TxHelpers.invoke(aliceAddr, Some("foo"), Seq(CONST_LONG(calls)), invoker = alice)
@@ -90,7 +113,7 @@ class SyncDAppLimits extends PropSpec with WithDomain with OptionValues with Eit
     }
   }
 
-  property("NODE-727 A sum of inner invokes is unlimited in RideV6") {
+  property("NODE-763 A total number of inner invokes is unlimited in RideV6") {
     withDomain(DomainPresets.RideV6, AddrWithBalance.enoughBalances(alice)) { d =>
       d.appendBlock(
         TxHelpers.setScript(
@@ -136,9 +159,11 @@ class SyncDAppLimits extends PropSpec with WithDomain with OptionValues with Eit
       else
         restComplexityExamples match {
           case (exprComplexity, expr) +: restXs =>
-            val uncompletedExprComplexity = exprComplexity + 1 // 1 for "+"
-            val times                     = restComplexity / uncompletedExprComplexity
-            loop(restComplexity % uncompletedExprComplexity, restXs, acc + s" + $expr" * times)
+            if (restComplexity >= exprComplexity) {
+              val uncompletedExprComplexity = exprComplexity + 1 // 1 for "+"
+              val times                     = restComplexity / uncompletedExprComplexity
+              loop(restComplexity % uncompletedExprComplexity, restXs, acc + s" + $expr" * times)
+            } else loop(restComplexity, restXs, acc)
 
           case Nil => loop(0, Nil, acc + " + 1" * restComplexity)
         }
