929bcd0ea0c4f1bb2b14a8b2f3699caef32cd66e
Sergey Nazarov <snazarov@web3tech.ru>
2022-08-23 15:55:07 +0300

    Tweaked build-with-docker.sh

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 NETWORK="Mainnet"
+TARGET=/src/target
 
 if [[ -n $1 ]]; then
   NETWORK=$1
@@ -11,12 +12,11 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 docker run \
   -u $(id -u ${USER}):$(id -g ${USER}) \
-  --mount type=bind,source="$DIR",target=/src \
-  -e HOME=/src/target \
-  -e XDG_CONFIG_HOME=/src/target/.config \
-  -e XDG_CACHE_HOME=/src/target/.cache \
-  -e COURSIER_CACHE=/src/target/docker/coursier \
-  --rm -it wavesplatform/sbt:8u345-b01_1.7.1_2.13.8 /bin/sh -c "
-  cd /src &&
-  sbt --batch --mem 4096 -J-XX:+UseG1GC -Dsbt.ivy.home=/src/target/.ivy2 -Dsbt.global.base=/src/target/.sbt/1.0 -Dsbt.boot.directory=/src/target/docker/sbt_cache \"set ThisBuild/network := $NETWORK\" packageAll
-"
+  -v "$DIR":/src \
+  -e HOME=$TARGET \
+  -e XDG_CONFIG_HOME=$TARGET/.config \
+  -e XDG_CACHE_HOME=$TARGET/.cache \
+  -e SBT_OPTS="-XX:+UseG1GC -Dsbt.ivy.home=$TARGET/.ivy2 -Dsbt.global.base=$TARGET/.sbt/1.0" \
+  -w /src \
+  --rm -it wavesplatform/sbt:8u345-b01_1.7.1_2.13.8 \
+  /bin/sh -c "sbt --batch --mem 2048 \"set ThisBuild/network := $NETWORK\" packageAll"
