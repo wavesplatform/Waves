c194674b7e5952ef4acdb713ad0216c5b04b146b
Ivan Mashonskii <imashonskii@web3tech.ru>
2022-08-25 14:52:01 +0300

    NODE-2291 Review fixes

--- node/src/main/scala/com/wavesplatform/api/common/CommonAccountsApi.scala
+++ node/src/main/scala/com/wavesplatform/api/common/CommonAccountsApi.scala
@@ -58,7 +58,7 @@ object CommonAccountsApi {
   def apply(diff: () => Diff, db: DB, blockchain: Blockchain): CommonAccountsApi = new CommonAccountsApi {
 
     override def balance(address: Address, confirmations: Int = 0): Long =
-      blockchain.balance(address)
+      blockchain.balance(address, blockchain.height, confirmations)
 
     override def effectiveBalance(address: Address, confirmations: Int = 0): Long = {
       blockchain.effectiveBalance(address, confirmations)
