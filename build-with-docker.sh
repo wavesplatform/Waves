460a4b18cb269b311fa7a2796f052cbf6af1ba11
Sergey Nazarov <snazarov@web3tech.ru>
2022-09-12 11:37:57 +0300

    wip

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -6,10 +6,6 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 docker run \
   -u $(id -u ${USER}):$(id -g ${USER}) \
   -v "$DIR":/src \
-  -e HOME=$TARGET \
-  -e XDG_CONFIG_HOME=$TARGET/.config \
-  -e XDG_CACHE_HOME=$TARGET/.cache \
-  -e SBT_OPTS="-XX:+UseG1GC -Dsbt.ivy.home=$TARGET/.ivy2 -Dsbt.global.base=$TARGET/.sbt/1.0" \
   -w /src \
   --rm -it wavesplatform/sbt:8u345-b01_1.7.1_2.13.8 \
-  /bin/sh -c "sbt --batch --mem 2048 buildTarballsForDocker"
+  /bin/bash
