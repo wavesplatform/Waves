8ebff48ded65521393c50190cd03e7ba775c3ce2
Sergey Nazarov <snazarov@web3tech.ru>
2022-09-14 10:50:15 +0300

    wip

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-TARGET=/src/target
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+WAVES_VERSION=$(cut -d\" -f2 ../version.sbt)
 
 docker run \
-  -u $(id -u ${USER}):$(id -g ${USER}) \
-  -v "$DIR":/src \
-  -e HOME=$TARGET \
-  -e XDG_CONFIG_HOME=$TARGET/.config \
-  -e XDG_CACHE_HOME=$TARGET/.cache \
-  -e SBT_OPTS="-XX:+UseG1GC -Dsbt.ivy.home=$TARGET/.ivy2 -Dsbt.global.base=$TARGET/.sbt/1.0" \
+  -v "$PWD":/src \
+  -e HOME=/opt/sbt/home \
   -w /src \
-  --rm -it wavesplatform/sbt:8u345-b01_1.7.1_2.13.8 \
-  /bin/sh -c "sbt --batch --mem 2048 buildTarballsForDocker"
+  --rm -it wavesplatform/node-sbt-builder:$WAVES_VERSION \
+  /bin/sh -c "sbt --batch --mem 2048 packageAll"
---
+++ docker/build-node-sbt-builder.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SBT_VERSION=$(cut -d= -f2 ../project/build.properties)
+WAVES_VERSION=$(cut -d\" -f2 ../version.sbt)
+JAVA_VERSION=8u345-b01
+
+docker build -t node-sbt-builder:$WAVES_VERSION - <<EOF
+FROM eclipse-temurin:$JAVA_VERSION-jdk-jammy
+
+ENV PATH="/opt/sbt/bin:\$PATH"
+ENV HOME=/opt/sbt/home
+
+RUN curl -L https://github.com/sbt/sbt/releases/download/v$SBT_VERSION/sbt-$SBT_VERSION.tgz | tar -xzf - -C /opt
+
+RUN apt-get update && apt-get -y install git
+
+VOLUME /src
+
+RUN git clone -b v$WAVES_VERSION https://github.com/wavesplatform/Waves.git /src && \
+  cd /src && \
+  sbt --batch --mem 2048 ";node/compile;grpc-server/compile"
+
+EOF
