dde3e071df69c71a653081526caca1df22c7f130
Artyom Sayadyan <xrtm000@gmail.com>
2022-09-26 20:40:00 +0300

    Use default values for request structure

--- node/src/main/scala/com/wavesplatform/api/http/utils/UtilsInvocationRequest.scala
+++ node/src/main/scala/com/wavesplatform/api/http/utils/UtilsInvocationRequest.scala
@@ -11,37 +11,37 @@ import com.wavesplatform.lang.v1.evaluator.ContractEvaluator.Invocation
 import com.wavesplatform.lang.v1.traits.domain.Recipient.Address
 import com.wavesplatform.transaction.TxValidationError.GenericError
 import com.wavesplatform.transaction.smart
+import com.wavesplatform.transaction.smart.AttachedPaymentExtractor
 import com.wavesplatform.transaction.smart.InvokeScriptTransaction.Payment
-import com.wavesplatform.transaction.smart.{AttachedPaymentExtractor, InvokeTransaction}
 import play.api.libs.json.{Json, Reads}
 
 case class UtilsInvocationRequest(
-    call: Option[FunctionCallPart],
-    id: Option[String],
-    fee: Option[Long],
-    feeAssetId: Option[String],
-    sender: Option[String],
-    senderPublicKey: Option[String],
-    payment: Option[Seq[Payment]]
+    call: FunctionCallPart = FunctionCallPart("default", Nil),
+    id: String = ByteStr(new Array[Byte](32)).toString,
+    fee: Long = 0,
+    feeAssetId: Option[String] = None,
+    sender: Option[String] = None,
+    senderPublicKey: String = ByteStr(new Array[Byte](32)).toString,
+    payment: Seq[Payment] = Nil
 ) {
   def toInvocation: Either[ValidationError, Invocation] =
     for {
-      senderPK <- senderPublicKey.map(PublicKey.fromBase58String).getOrElse(Right(ByteStr(new Array[Byte](32))))
-      id       <- id.map(decodeBase58).getOrElse(Right(ByteStr(new Array[Byte](32))))
-      functionCall = call.map(InvokeScriptRequest.buildFunctionCall).getOrElse(InvokeTransaction.DefaultCall)
+      senderPK <- PublicKey.fromBase58String(senderPublicKey)
+      id       <- decodeBase58(id)
+      functionCall = InvokeScriptRequest.buildFunctionCall(call)
       feeAssetId <- feeAssetId.traverse(decodeBase58)
       sender <- sender
         .map(AddressOrAlias.fromString(_, checkChainId = false).map(a => Address(ByteStr(a.bytes))))
         .getOrElse(Right(Address(ByteStr(new Array[Byte](26)))))
       payments <- AttachedPaymentExtractor
-        .extractPayments(payment.getOrElse(Nil), V6, blockchainAllowsMultiPayment = true, smart.DApp)
+        .extractPayments(payment, V6, blockchainAllowsMultiPayment = true, smart.DApp)
         .leftMap(GenericError(_))
-    } yield Invocation(functionCall, sender, senderPK, sender, senderPK, payments, id, fee.getOrElse(0), feeAssetId)
+    } yield Invocation(functionCall, sender, senderPK, sender, senderPK, payments, id, fee, feeAssetId)
 
   private def decodeBase58(base58: String): Either[ValidationError, ByteStr] =
     ByteStr.decodeBase58(base58).toEither.leftMap(e => GenericError(String.valueOf(e.getMessage)))
 }
 
 object UtilsInvocationRequest {
-  implicit val reads: Reads[UtilsInvocationRequest] = Json.reads[UtilsInvocationRequest]
+  implicit val reads: Reads[UtilsInvocationRequest] = Json.using[Json.WithDefaultValues].reads[UtilsInvocationRequest]
 }
