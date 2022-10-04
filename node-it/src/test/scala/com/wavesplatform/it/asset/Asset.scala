1eedbe4ccafd45fd32f53f972ff7b114ae80379d
Artyom Sayadyan <xrtm000@gmail.com>
2022-10-04 18:16:19 +0300

    Remove all tests except WideStateGenerationSuite

--- node-it/src/test/scala/com/wavesplatform/it/asset/Asset.scala
+++
@@ -1,3 +0,0 @@
-package com.wavesplatform.it.asset
-
-case class Asset(assetType: String, name: String, description: String, quantity: Long, reissuable: Boolean, decimals: Byte, nonce: Long)
--- node-it/src/test/scala/com/wavesplatform/it/sync/grpc/GrpcBaseTransactionSuiteLike.scala
+++
@@ -1,9 +0,0 @@
-package com.wavesplatform.it.sync.grpc
-
-import com.wavesplatform.it.{GrpcIntegrationSuiteWithThreeAddress, GrpcWaitForHeight, Nodes}
-import org.scalatest._
-
-trait GrpcBaseTransactionSuiteLike extends GrpcWaitForHeight with GrpcIntegrationSuiteWithThreeAddress { this: TestSuite with Nodes =>
-}
-
-abstract class GrpcBaseTransactionSuite extends funsuite.AnyFunSuite with GrpcBaseTransactionSuiteLike
