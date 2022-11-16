65246d037f8e57a6f24b2a2996288d524e5b2270
vnikolaenko <vnikolaenko@web3tech.ru>
2022-11-16 19:41:05 +0300

    edited ContainsElement

--- lang/tests-js/src/test/scala/com/wavesplatform/test/builtInFunctions/list/ContainsElement.scala
+++ lang/tests-js/src/test/scala/com/wavesplatform/test/builtInFunctions/list/ContainsElement.scala
@@ -8,8 +8,8 @@ import utest.{Tests, test}
 
 object ContainsElement extends JsTestBase {
   // containsElement
-  private val containsElement                     = "containsElement(foo, bar)"
-  private val containsElementArgBeforeFunc        = "foo.containsElement(bar)"
+  private val containsElement                     = "containsElement(bar, foo)"
+  private val containsElementArgBeforeFunc        = "bar.containsElement(foo)"
   private val invalidContainsElement              = "containsElement(foo)"
   private val invalidContainsElementArgBeforeFunc = "foo.containsElement(foo, bar)"
   private val invalidErrorContainsElement         = testData.invalidFunctionError("containsElement", 2)
@@ -18,11 +18,7 @@ object ContainsElement extends JsTestBase {
     test.apply("check: containsElement function compiles with a stringList") {
       for (version <- testData.actualVersionsWithoutV3) {
         val precondition = new GeneratorContractsForBuiltInFunctions("", version)
-        val script = precondition.simpleRideCode(
-          randomStringArrayElement,
-          stringList,
-          containsElement
-        )
+        val script = precondition.simpleRideCode(randomStringArrayElement, stringList, containsElement)
         assertCompileSuccessDApp(script, version)
       }
     }
@@ -30,11 +26,7 @@ object ContainsElement extends JsTestBase {
     test.apply("check: containsElement function compiles with a intList") {
       for (version <- testData.actualVersionsWithoutV3) {
         val precondition = new GeneratorContractsForBuiltInFunctions("", version)
-        val script = precondition.simpleRideCode(
-          randomInt.toString,
-          intList,
-          containsElement
-        )
+        val script = precondition.simpleRideCode(randomInt.toString, intList, containsElement)
         assertCompileSuccessDApp(script, version)
       }
     }
@@ -42,11 +34,7 @@ object ContainsElement extends JsTestBase {
     test.apply("check: containsElement function compiles with a stringList (argument before function)") {
       for (version <- testData.actualVersionsWithoutV3) {
         val precondition = new GeneratorContractsForBuiltInFunctions("", version)
-        val script = precondition.simpleRideCode(
-          randomStringArrayElement,
-          stringList,
-          containsElementArgBeforeFunc
-        )
+        val script = precondition.simpleRideCode(randomStringArrayElement, stringList, containsElementArgBeforeFunc)
         assertCompileSuccessDApp(script, version)
       }
     }
@@ -54,11 +42,7 @@ object ContainsElement extends JsTestBase {
     test.apply("check: containsElement function compiles with a intList (argument before function)") {
       for (version <- testData.actualVersionsWithoutV3) {
         val precondition = new GeneratorContractsForBuiltInFunctions("", version)
-        val script = precondition.simpleRideCode(
-          randomInt.toString,
-          intList,
-          containsElementArgBeforeFunc
-        )
+        val script = precondition.simpleRideCode(randomInt.toString, intList, containsElementArgBeforeFunc)
         assertCompileSuccessDApp(script, version)
       }
     }
@@ -66,11 +50,7 @@ object ContainsElement extends JsTestBase {
     test.apply("compilation error: containsElement - Non-matching types") {
       for (version <- testData.actualVersionsWithoutV3) {
         val precondition = new GeneratorContractsForBuiltInFunctions("", version)
-        val script = precondition.simpleRideCode(
-          randomInt.toString,
-          randomAliasDataArrayElement,
-          containsElementArgBeforeFunc
-        )
+        val script = precondition.simpleRideCode(randomInt.toString, randomAliasDataArrayElement, containsElementArgBeforeFunc)
         assertCompileErrorDApp(script, version, testData.nonMatchingTypes(""))
       }
     }
@@ -78,11 +58,7 @@ object ContainsElement extends JsTestBase {
     test.apply("compilation error: containsElement - Non-matching types (argument before function)") {
       for (version <- testData.actualVersionsWithoutV3) {
         val precondition = new GeneratorContractsForBuiltInFunctions("", version)
-        val script = precondition.simpleRideCode(
-          randomInt.toString,
-          randomIssuesArrayElement,
-          containsElementArgBeforeFunc
-        )
+        val script = precondition.simpleRideCode(randomInt.toString, randomIssuesArrayElement, containsElementArgBeforeFunc)
         assertCompileErrorDApp(script, version, testData.nonMatchingTypes(""))
       }
     }
@@ -90,11 +66,7 @@ object ContainsElement extends JsTestBase {
     test.apply("compilation error: Can't find a function overload containsElement") {
       for (version <- testData.actualVersionsWithoutV3) {
         val precondition = new GeneratorContractsForBuiltInFunctions("", version)
-        val script = precondition.simpleRideCode(
-          randomInt.toString,
-          randomAliasDataArrayElement,
-          invalidContainsElement
-        )
+        val script = precondition.simpleRideCode(randomInt.toString, randomAliasDataArrayElement, invalidContainsElement)
         assertCompileErrorDApp(script, version, invalidErrorContainsElement)
       }
     }
@@ -102,11 +74,7 @@ object ContainsElement extends JsTestBase {
     test.apply("compilation error: Can't find a function overload containsElement (argument before function)") {
       for (version <- testData.actualVersionsWithoutV3) {
         val precondition = new GeneratorContractsForBuiltInFunctions("", version)
-        val script = precondition.simpleRideCode(
-          randomInt.toString,
-          randomAliasDataArrayElement,
-          invalidContainsElementArgBeforeFunc
-        )
+        val script = precondition.simpleRideCode(randomInt.toString, randomAliasDataArrayElement, invalidContainsElementArgBeforeFunc)
         assertCompileErrorDApp(script, version, invalidErrorContainsElement)
       }
     }
