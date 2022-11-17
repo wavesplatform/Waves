488ff2dd8d70c50ca20a089c6e1c71b6ce4d9d38
vnikolaenko <vnikolaenko@web3tech.ru>
2022-11-17 11:42:12 +0300

    added Pow tests for test/builtInFunctions/math

--- lang/tests-js/src/test/scala/com/wavesplatform/test/builtInFunctions/math/Pow.scala
+++ lang/tests-js/src/test/scala/com/wavesplatform/test/builtInFunctions/math/Pow.scala
@@ -3,101 +3,101 @@ package com.wavesplatform.test.builtInFunctions.math
 import com.wavesplatform.JsTestBase
 import com.wavesplatform.lang.directives.values.V5
 import testHelpers.GeneratorContractsForBuiltInFunctions
-import testHelpers.RandomDataGenerator.{randomAddressDataArrayElement, randomAliasDataArrayElement, randomInt, randomStringArrayElement}
+import testHelpers.RandomDataGenerator.{randomAddressDataArrayElement, randomAliasDataArrayElement, randomInt, randomStringArrayElement, randomUnionArrayElement}
 import utest.{Tests, test}
 
-object Median extends JsTestBase {
-  private val medianInt = s"median([callerTestData, $randomInt, $randomInt])"
-  private val medianIntArgBeforeFunc = s"[callerTestData, $randomInt, $randomInt].median()"
-  private val medianBigInt = s"median(callerTestData)"
-  private val medianBigIntArgBeforeFunc = s"callerTestData.median()"
-  private val invalidMedianInt = s"median()"
-  private val medianError: String = testData.invalidFunctionError("median", 1)
+object Pow extends JsTestBase {
+  private val union                      = randomUnionArrayElement
+  private val powInt                     = s"pow(callerTestData, 6, $randomInt, 4, $randomInt, $union)"
+  private val powIntArgBeforeFunc        = s"callerTestData.pow(6, $randomInt, $randomInt, 2, $union)"
+  private val powBigInt                  = s"pow(callerTestData, 6, callerTestData, 4, 2, $union)"
+  private val powBigIntArgBeforeFunc     = s"callerTestData.pow($randomInt, callerTestData, 4, 2, $union)"
+  private val invalidPowInt              = s"pow()"
+  private val powError: String = testData.invalidFunctionError("pow", 6)
 
   val tests: Tests = Tests {
-    test.apply("check: median Int function compiles") {
+    test.apply("check: pow Int function compiles") {
       for (version <- testData.actualVersionsWithoutV3) {
         val precondition = new GeneratorContractsForBuiltInFunctions("Int", version)
-        val script = precondition.onlyMatcherContract(randomInt.toString, medianInt)
+        val script = precondition.onlyMatcherContract(randomInt.toString, powInt)
         assertCompileSuccessDApp(script, version)
       }
     }
 
-    test.apply("check: median Int function compiles (argument before function)") {
+    test.apply("check: pow Int function compiles (argument before function)") {
       for (version <- testData.actualVersionsWithoutV3) {
         val precondition = new GeneratorContractsForBuiltInFunctions("Int", version)
-        val script = precondition.onlyMatcherContract(randomInt.toString, medianIntArgBeforeFunc)
+        val script = precondition.onlyMatcherContract(randomInt.toString, powIntArgBeforeFunc)
         assertCompileSuccessDApp(script, version)
       }
     }
 
-    test.apply("check: median BigInt function compiles") {
+    test.apply("check: pow BigInt function compiles") {
       for (version <- testData.versionsSupportingTheNewFeatures) {
         val precondition = new GeneratorContractsForBuiltInFunctions("BigInt", version)
-        val script = precondition.onlyMatcherContract(s"[toBigInt(${randomInt.toString})]", medianBigInt)
+        val script = precondition.onlyMatcherContract(s"toBigInt(${randomInt.toString})", powBigInt)
         assertCompileSuccessDApp(script, version)
       }
     }
 
-    test.apply("check: median BigInt function compiles (argument before function)") {
+    test.apply("check: pow BigInt function compiles (argument before function)") {
       for (version <- testData.versionsSupportingTheNewFeatures) {
         val precondition = new GeneratorContractsForBuiltInFunctions("BigInt", version)
-        val script = precondition.onlyMatcherContract(s"[toBigInt(${randomInt.toString})]", medianBigIntArgBeforeFunc)
+        val script = precondition.onlyMatcherContract(s"toBigInt(${randomInt.toString})", powBigIntArgBeforeFunc)
         assertCompileSuccessDApp(script, version)
       }
     }
 
-    test.apply("compilation error: invalid median function") {
+    test.apply("compilation error: invalid pow function") {
       for (version <- testData.actualVersionsWithoutV3) {
         val precondition = new GeneratorContractsForBuiltInFunctions("Int", version)
-        val script = precondition.onlyMatcherContract(randomInt.toString, invalidMedianInt)
+        val script = precondition.onlyMatcherContract(randomInt.toString, invalidPowInt)
         if (version < V5) {
-          assertCompileErrorDApp(script, version, medianError)
+          assertCompileErrorDApp(script, version, powError)
         } else {
           assertCompileErrorDApp(script, version, testData.CANT_FIND_A_FUNCTION_OVERLOAD)
         }
       }
     }
 
-    test.apply("compilation error: invalid median data") {
+    test.apply("compilation error: invalid pow data") {
       for (version <- testData.actualVersionsWithoutV3) {
         val precondition = new GeneratorContractsForBuiltInFunctions("Int", version)
-        val script = precondition.onlyMatcherContract(randomStringArrayElement, medianInt)
+        val script = precondition.onlyMatcherContract(randomStringArrayElement, powInt)
         if (version < V5) {
-          assertCompileErrorDApp(script, version, testData.nonMatchingTypes("List[Int]"))
+          assertCompileErrorDApp(script, version, testData.nonMatchingTypes("Int"))
         } else {
           assertCompileErrorDApp(script, version, testData.CANT_FIND_A_FUNCTION_OVERLOAD)
         }
       }
     }
 
-    test.apply("compilation error: invalid median data (argument before function)") {
+    test.apply("compilation error: invalid pow data (argument before function)") {
       for (version <- testData.actualVersionsWithoutV3) {
         val precondition = new GeneratorContractsForBuiltInFunctions("Int", version)
-        val script = precondition.onlyMatcherContract(randomAddressDataArrayElement, medianIntArgBeforeFunc)
+        val script = precondition.onlyMatcherContract(randomAddressDataArrayElement, powIntArgBeforeFunc)
         if (version < V5) {
-          assertCompileErrorDApp(script, version, testData.nonMatchingTypes("List[Int]"))
+          assertCompileErrorDApp(script, version, testData.nonMatchingTypes("Int"))
         } else {
           assertCompileErrorDApp(script, version, testData.CANT_FIND_A_FUNCTION_OVERLOAD)
         }
       }
     }
 
-    test.apply("compilation error: invalid median data BigInt") {
+    test.apply("compilation error: invalid pow data BigInt") {
       for (version <- testData.versionsSupportingTheNewFeatures) {
         val precondition = new GeneratorContractsForBuiltInFunctions("BigInt", version)
-        val script = precondition.onlyMatcherContract(randomStringArrayElement, medianBigInt)
+        val script = precondition.onlyMatcherContract(randomStringArrayElement, powBigInt)
         assertCompileErrorDApp(script, version, testData.CANT_FIND_A_FUNCTION_OVERLOAD)
       }
     }
 
-    test.apply("compilation error: invalid median data BigInt (argument before function)") {
+    test.apply("compilation error: invalid pow data BigInt (argument before function)") {
       for (version <- testData.versionsSupportingTheNewFeatures) {
         val precondition = new GeneratorContractsForBuiltInFunctions("BigInt", version)
-        val script = precondition.onlyMatcherContract(randomAliasDataArrayElement, medianBigIntArgBeforeFunc)
+        val script = precondition.onlyMatcherContract(randomAliasDataArrayElement, powBigIntArgBeforeFunc)
         assertCompileErrorDApp(script, version, testData.CANT_FIND_A_FUNCTION_OVERLOAD)
       }
     }
   }
-
 }
