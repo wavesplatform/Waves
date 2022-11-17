01eace855cee8e5853b075581fead8e345534bcc
vnikolaenko <vnikolaenko@web3tech.ru>
2022-11-17 14:28:58 +0300

    added Drop and DropRight tests

---
+++ lang/tests-js/src/test/scala/com/wavesplatform/test/builtInFunctions/string/Drop.scala
@@ -0,0 +1,93 @@
+package com.wavesplatform.test.builtInFunctions.string
+
+import com.wavesplatform.JsTestBase
+import testHelpers.GeneratorContractsForBuiltInFunctions
+import testHelpers.RandomDataGenerator.{randomAddressDataArrayElement, randomStringArrayElement, randomInt, randomUnionArrayElement}
+import utest.{Tests, test}
+
+object Drop extends JsTestBase {
+  private val drop                     = s"drop(callerTestData, $randomInt)"
+  private val dropArgBeforeFunction    = s"callerTestData.drop($randomInt)"
+  private val invalidDrop              = s"drop(callerTestData)"
+  private val invalidDropArgBeforeFunc = s"callerTestData.drop(callerTestData, $randomInt)"
+
+  val tests: Tests = Tests {
+    test.apply("check: function drop compiles") {
+      for (version <- testData.actualVersions) {
+        val precondition = new GeneratorContractsForBuiltInFunctions("", version)
+        val script = precondition.codeWithoutMatcher(
+          randomStringArrayElement,
+          drop,
+          testData.rideV3Result,
+          testData.GreaterV3ResultStringEntry
+        )
+        assertCompileSuccessDApp(script, version)
+      }
+    }
+
+    test.apply("check: function drop compiles (argument before function)") {
+      for (version <- testData.actualVersions) {
+        val precondition = new GeneratorContractsForBuiltInFunctions("", version)
+        val script = precondition.codeWithoutMatcher(
+          randomStringArrayElement,
+          dropArgBeforeFunction,
+          testData.rideV3Result,
+          testData.GreaterV3ResultStringEntry
+        )
+        assertCompileSuccessDApp(script, version)
+      }
+    }
+
+    test.apply("compilation error: Can't find a function overload, invalid data") {
+      for (version <- testData.actualVersions) {
+        val precondition = new GeneratorContractsForBuiltInFunctions("", version)
+        val script = precondition.codeWithoutMatcher(
+          randomAddressDataArrayElement,
+          drop,
+          testData.rideV3Result,
+          testData.GreaterV3ResultStringEntry
+        )
+        assertCompileErrorDApp(script, version, testData.CANT_FIND_A_FUNCTION_OVERLOAD)
+      }
+    }
+
+    test.apply("compilation error: Can't find a function overload, invalid data (argument before function)") {
+      for (version <- testData.actualVersions) {
+        val precondition = new GeneratorContractsForBuiltInFunctions("", version)
+        val script = precondition.codeWithoutMatcher(
+          randomUnionArrayElement,
+          dropArgBeforeFunction,
+          testData.rideV3Result,
+          testData.GreaterV3ResultStringEntry
+        )
+        assertCompileErrorDApp(script, version, testData.CANT_FIND_A_FUNCTION_OVERLOAD)
+      }
+    }
+
+    test.apply("compilation error: invalid function drop Can't find a function overload") {
+      for (version <- testData.actualVersions) {
+        val precondition = new GeneratorContractsForBuiltInFunctions("", version)
+        val script = precondition.codeWithoutMatcher(
+          randomStringArrayElement,
+          invalidDrop,
+          testData.rideV3Result,
+          testData.GreaterV3ResultStringEntry
+        )
+        assertCompileErrorDApp(script, version, testData.CANT_FIND_A_FUNCTION_OVERLOAD)
+      }
+    }
+
+    test.apply("compilation error: invalid function drop Can't find a function overload (argument before function)") {
+      for (version <- testData.actualVersions) {
+        val precondition = new GeneratorContractsForBuiltInFunctions("", version)
+        val script = precondition.codeWithoutMatcher(
+          randomStringArrayElement,
+          invalidDropArgBeforeFunc,
+          testData.rideV3Result,
+          testData.GreaterV3ResultStringEntry
+        )
+        assertCompileErrorDApp(script, version, testData.CANT_FIND_A_FUNCTION_OVERLOAD)
+      }
+    }
+  }
+}
---
+++ lang/tests-js/src/test/scala/com/wavesplatform/test/builtInFunctions/string/DropRight.scala
@@ -0,0 +1,94 @@
+package com.wavesplatform.test.builtInFunctions.string
+
+import com.wavesplatform.JsTestBase
+import testHelpers.GeneratorContractsForBuiltInFunctions
+import testHelpers.RandomDataGenerator.{randomAddressDataArrayElement, randomInt, randomStringArrayElement, randomUnionArrayElement}
+import utest.{Tests, test}
+
+object DropRight extends JsTestBase {
+  private val dropRight                     = s"dropRight(callerTestData, $randomInt)"
+  private val dropRightArgBeforeFunction    = s"callerTestData.dropRight($randomInt)"
+  private val invalidDropRight              = s"dropRight(callerTestData)"
+  private val invalidDropRightArgBeforeFunc = s"callerTestData.dropRight(callerTestData, $randomInt)"
+
+  val tests: Tests = Tests {
+    test.apply("check: function dropRight compiles") {
+      for (version <- testData.actualVersions) {
+        val precondition = new GeneratorContractsForBuiltInFunctions("", version)
+        val script = precondition.codeWithoutMatcher(
+          randomStringArrayElement,
+          dropRight,
+          testData.rideV3Result,
+          testData.GreaterV3ResultStringEntry
+        )
+        assertCompileSuccessDApp(script, version)
+      }
+    }
+
+    test.apply("check: function dropRight compiles (argument before function)") {
+      for (version <- testData.actualVersions) {
+        val precondition = new GeneratorContractsForBuiltInFunctions("", version)
+        val script = precondition.codeWithoutMatcher(
+          randomStringArrayElement,
+          dropRightArgBeforeFunction,
+          testData.rideV3Result,
+          testData.GreaterV3ResultStringEntry
+        )
+        assertCompileSuccessDApp(script, version)
+      }
+    }
+
+    test.apply("compilation error: Can't find a function overload, invalid data") {
+      for (version <- testData.actualVersions) {
+        val precondition = new GeneratorContractsForBuiltInFunctions("", version)
+        val script = precondition.codeWithoutMatcher(
+          randomAddressDataArrayElement,
+          dropRight,
+          testData.rideV3Result,
+          testData.GreaterV3ResultStringEntry
+        )
+        assertCompileErrorDApp(script, version, testData.CANT_FIND_A_FUNCTION_OVERLOAD)
+      }
+    }
+
+    test.apply("compilation error: Can't find a function overload, invalid data (argument before function)") {
+      for (version <- testData.actualVersions) {
+        val precondition = new GeneratorContractsForBuiltInFunctions("", version)
+        val script = precondition.codeWithoutMatcher(
+          randomUnionArrayElement,
+          dropRightArgBeforeFunction,
+          testData.rideV3Result,
+          testData.GreaterV3ResultStringEntry
+        )
+        assertCompileErrorDApp(script, version, testData.CANT_FIND_A_FUNCTION_OVERLOAD)
+      }
+    }
+
+    test.apply("compilation error: invalid function dropRight Can't find a function overload") {
+      for (version <- testData.actualVersions) {
+        val precondition = new GeneratorContractsForBuiltInFunctions("", version)
+        val script = precondition.codeWithoutMatcher(
+          randomStringArrayElement,
+          invalidDropRight,
+          testData.rideV3Result,
+          testData.GreaterV3ResultStringEntry
+        )
+        assertCompileErrorDApp(script, version, testData.CANT_FIND_A_FUNCTION_OVERLOAD)
+      }
+    }
+
+    test.apply("compilation error: invalid function dropRight Can't find a function overload (argument before function)") {
+      for (version <- testData.actualVersions) {
+        val precondition = new GeneratorContractsForBuiltInFunctions("", version)
+        val script = precondition.codeWithoutMatcher(
+          randomStringArrayElement,
+          invalidDropRightArgBeforeFunc,
+          testData.rideV3Result,
+          testData.GreaterV3ResultStringEntry
+        )
+        assertCompileErrorDApp(script, version, testData.CANT_FIND_A_FUNCTION_OVERLOAD)
+      }
+    }
+  }
+
+}
