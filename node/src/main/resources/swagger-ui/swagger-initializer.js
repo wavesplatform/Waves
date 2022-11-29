49541f8f22366bc6ff7a705b29e35464c0afe4b5
Sergey Nazarov <snazarov@uniqsoft.ae>
2022-11-29 16:51:45 +0400

    wip

--- node/src/main/resources/swagger-ui/swagger-initializer.js
+++ node/src/main/resources/swagger-ui/swagger-initializer.js
@@ -1,27 +1,22 @@
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
 
-    window.onload = function() {
-      // Begin Swagger UI call region
-      const ui = SwaggerUIBundle({
-        url: "/api-docs/openapi.yaml",
-        dom_id: '#swagger-ui',
-        deepLinking: true,
-        presets: [
-          SwaggerUIBundle.presets.apis,
-          SwaggerUIStandalonePreset
-        ],
-        plugins: [
-          SwaggerUIBundle.plugins.DownloadUrl
-        ],
-        layout: "StandaloneLayout",
-        tagsSorter: "alpha",
-        operationsSorter: "alpha"
-      })
-      // End Swagger UI call region
-
-      window.ui = ui
-    }
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    url: "/api-docs/openapi.yaml",
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout",
+    tagsSorter: "alpha",
+    operationsSorter: "alpha"
+  });
 
   //</editor-fold>
 };
