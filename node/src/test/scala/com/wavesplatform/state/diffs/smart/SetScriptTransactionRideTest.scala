a046f6e3132d908a8c02a7c9361ce414328913a2
Artyom Sayadyan <xrtm000@gmail.com>
2023-01-12 05:59:22 +0300

    Test both dApp and expression verifiers

--- node/src/test/scala/com/wavesplatform/state/diffs/smart/SetScriptTransactionRideTest.scala
+++ node/src/test/scala/com/wavesplatform/state/diffs/smart/SetScriptTransactionRideTest.scala
@@ -1,17 +1,18 @@
 package com.wavesplatform.state.diffs.smart
 
 import com.wavesplatform.db.WithDomain
+import com.wavesplatform.db.WithState.AddrWithBalance
 import com.wavesplatform.features.BlockchainFeatures.ContinuationTransaction
 import com.wavesplatform.lang.directives.values.V6
 import com.wavesplatform.lang.v1.compiler.TestCompiler
 import com.wavesplatform.test.DomainPresets.WavesSettingsOps
 import com.wavesplatform.test.{NumericExt, PropSpec, produce}
-import com.wavesplatform.transaction.TxHelpers.{defaultSigner, setScript}
+import com.wavesplatform.transaction.TxHelpers.{defaultSigner, secondSigner, setScript}
 import com.wavesplatform.transaction.TxVersion
 
 class SetScriptTransactionRideTest extends PropSpec with WithDomain {
   property(s"correct mapping SetScriptTransaction with > 32 KB script from $ContinuationTransaction") {
-    val verifier = TestCompiler(V6).compileContract(
+    val dAppVerifier = TestCompiler(V6).compileContract(
       s"""
          | @Verifier(tx)
          | func verify() = {
@@ -22,6 +23,14 @@ class SetScriptTransactionRideTest extends PropSpec with WithDomain {
          | }
        """.stripMargin
     )
+    val exprVerifier = TestCompiler(V6).compileExpression(
+      s"""
+         | match tx {
+         |   case ss: SetScriptTransaction => ss.script.value().size() == 32779
+         |   case _                        => throw()
+         | }
+       """.stripMargin
+    )
     val dApp = TestCompiler(V6).compileContract(
       s"""
          | let a = ${Seq.fill(2519)("sigVerify(base58'', base58'', base58'')").mkString(" && ")}
@@ -29,13 +38,20 @@ class SetScriptTransactionRideTest extends PropSpec with WithDomain {
          | func default() = []
        """.stripMargin
     )
-    withDomain(DomainPresets.ContinuationTransaction.setFeaturesHeight(ContinuationTransaction -> 3)) { d =>
-      d.appendBlock(setScript(defaultSigner, verifier))
+    withDomain(
+      DomainPresets.ContinuationTransaction.setFeaturesHeight(ContinuationTransaction -> 4),
+      AddrWithBalance.enoughBalances(secondSigner)
+    ) { d =>
+      d.appendBlock(setScript(defaultSigner, dAppVerifier), setScript(secondSigner, exprVerifier))
       d.appendBlockE(setScript(defaultSigner, dApp, 1.waves, TxVersion.V2)) should produce(
+        "value() called on unit value while accessing field 'script'"
+      )
+      d.appendBlockE(setScript(secondSigner, dApp, 1.waves, TxVersion.V2)) should produce(
         "value() called on unit value while accessing field 'script'"
       )
       d.appendBlock()
       d.appendAndAssertSucceed(setScript(defaultSigner, dApp, 1.waves, TxVersion.V2))
+      d.appendAndAssertSucceed(setScript(secondSigner, dApp, 1.waves, TxVersion.V2))
     }
   }
 }
