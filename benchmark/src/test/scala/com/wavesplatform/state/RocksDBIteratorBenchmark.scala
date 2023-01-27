e87fd44e606f8670f08e589104586bd5d4344b9f
Ivan Mashonskii <imashonskii@web3tech.ru>
2023-01-27 19:36:40 +0300

    Benchmarks for seekForPrev

---
+++ benchmark/src/test/scala/com/wavesplatform/state/RocksDBIteratorBenchmark.scala
@@ -0,0 +1,85 @@
+package com.wavesplatform.state
+
+import com.google.common.primitives.Ints
+
+import java.util.concurrent.TimeUnit
+import com.typesafe.config.ConfigFactory
+import com.wavesplatform.database.openDB
+import com.wavesplatform.settings.{WavesSettings, loadConfig}
+import com.wavesplatform.state.RocksDBIteratorBenchmark.*
+import org.openjdk.jmh.annotations.*
+import org.openjdk.jmh.infra.Blackhole
+import org.rocksdb.{ReadOptions, RocksDB, WriteBatch, WriteOptions}
+
+import java.nio.file.Files
+
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@BenchmarkMode(Array(Mode.AverageTime))
+@Threads(1)
+@Fork(1)
+@Warmup(iterations = 10)
+@Measurement(iterations = 100)
+class RocksDBIteratorBenchmark {
+  @Benchmark
+  def directOrderIterator(st: BaseSt, bh: Blackhole): Unit = {
+    bh.consume {
+      val iter = st.db.newIterator(st.readOptions)
+      iter.seek(st.firstKey)
+      while (iter.isValid) {
+        iter.key()
+        iter.value()
+        iter.next()
+      }
+      iter.close()
+    }
+  }
+
+  @Benchmark
+  def reverseOrderIterator(st: BaseSt, bh: Blackhole): Unit = {
+    bh.consume {
+      val iter = st.db.newIterator(st.readOptions)
+      iter.seekForPrev(st.lastKey)
+      while (iter.isValid) {
+        iter.key()
+        iter.value()
+        iter.prev()
+      }
+      iter.close()
+    }
+  }
+}
+
+object RocksDBIteratorBenchmark {
+
+  @State(Scope.Benchmark)
+  class BaseSt {
+    private val wavesSettings: WavesSettings =
+      WavesSettings.fromRootConfig(loadConfig(ConfigFactory.load()))
+
+    val db: RocksDB = {
+      val dir = Files.createTempDirectory("state-synthetic").toAbsolutePath.toString
+      openDB(wavesSettings.dbSettings.copy(directory = dir))
+    }
+
+    val keysPrefix            = "keysPrefix"
+    val firstKey: Array[Byte] = keysPrefix.getBytes ++ Ints.toByteArray(1)
+    val lastKey: Array[Byte]  = keysPrefix.getBytes ++ Ints.toByteArray(10000)
+
+    val kvs: Map[Array[Byte], Array[Byte]] = (1 to 10000).map { idx =>
+      (keysPrefix.getBytes ++ Ints.toByteArray(idx)) -> s"value$idx".getBytes
+    }.toMap
+
+    val readOptions: ReadOptions = new ReadOptions().setTotalOrderSeek(false).setPrefixSameAsStart(true)
+
+    private val wb: WriteBatch = new WriteBatch()
+    kvs.foreach { case (key, value) =>
+      wb.put(key, value)
+    }
+    db.write(new WriteOptions(), wb)
+
+    @TearDown
+    def close(): Unit = {
+      db.close()
+    }
+  }
+}
---
+++ benchmark/src/test/scala/com/wavesplatform/state/RocksDBSeekForPrevBenchmark.scala
@@ -0,0 +1,90 @@
+package com.wavesplatform.state
+
+import com.google.common.primitives.{Bytes, Shorts}
+import com.typesafe.config.ConfigFactory
+import com.wavesplatform.account.Address
+import com.wavesplatform.database.{
+  AddressId,
+  CurrentData,
+  DataNode,
+  KeyTags,
+  Keys,
+  openDB,
+  readCurrentData,
+  readDataNode,
+  writeCurrentData,
+  writeDataNode
+}
+import com.wavesplatform.settings.{WavesSettings, loadConfig}
+import com.wavesplatform.state.RocksDBSeekForPrevBenchmark.*
+import org.openjdk.jmh.annotations.*
+import org.openjdk.jmh.infra.Blackhole
+import org.rocksdb.{ReadOptions, RocksDB, WriteBatch, WriteOptions}
+
+import java.nio.file.Files
+import java.util.concurrent.TimeUnit
+
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@BenchmarkMode(Array(Mode.AverageTime))
+@Threads(1)
+@Fork(1)
+@Warmup(iterations = 10)
+@Measurement(iterations = 100)
+class RocksDBSeekForPrevBenchmark {
+  @Benchmark
+  def seekForPrev(st: BaseSt, bh: Blackhole): Unit = {
+    bh.consume {
+      val iter = st.db.newIterator(st.readOptions)
+      iter.seekForPrev(st.dataNodeKey(Height(Int.MaxValue)))
+      if (iter.isValid && iter.key().startsWith(st.dataNodeKeyPrefix)) {
+        readDataNode(st.keyString)(iter.value()).prevHeight
+      }
+      iter.close()
+    }
+  }
+
+  @Benchmark
+  def get(st: BaseSt, bh: Blackhole): Unit = {
+    bh.consume {
+      readCurrentData(st.keyString)(st.db.get(st.currentDataKey)).prevHeight
+    }
+  }
+}
+
+object RocksDBSeekForPrevBenchmark {
+
+  @State(Scope.Benchmark)
+  class BaseSt {
+    private val wavesSettings: WavesSettings =
+      WavesSettings.fromRootConfig(loadConfig(ConfigFactory.load()))
+
+    val db: RocksDB = {
+      val dir = Files.createTempDirectory("state-synthetic").toAbsolutePath.toString
+      openDB(wavesSettings.dbSettings.copy(directory = dir))
+    }
+
+    val address: Address     = Address(Array.fill(20)(1.toByte))
+    val addressId: AddressId = AddressId(1L)
+
+    val keyString                          = "key"
+    val currentDataKey: Array[Byte]        = Keys.data(address, keyString).keyBytes
+    val dataNodeKey: Height => Array[Byte] = Keys.dataAt(addressId, "key")(_).keyBytes
+    val dataNodeKeyPrefix: Array[Byte] = Bytes.concat(Shorts.toByteArray(KeyTags.DataHistory.id.toShort), addressId.toByteArray, keyString.getBytes)
+
+    private val dataEntry: StringDataEntry = StringDataEntry(keyString, "value")
+
+    val readOptions: ReadOptions = new ReadOptions()
+
+    private val wb: WriteBatch = new WriteBatch()
+    wb.put(currentDataKey, writeCurrentData(CurrentData(dataEntry, Height(10000), Height(9999))))
+    (1 to 1000).foreach { h =>
+      wb.put(dataNodeKey(Height(h)), writeDataNode(DataNode(dataEntry, Height(h - 1))))
+    }
+    db.write(new WriteOptions(), wb)
+
+    @TearDown
+    def close(): Unit = {
+      db.close()
+    }
+  }
+}
