681dbd031aec1a358e0493087a581aa0c1cf1a7f
Artyom Sayadyan <xrtm000@gmail.com>
2023-02-03 16:49:46 +0300

    HTTP API tests

--- node/src/test/scala/com/wavesplatform/http/AssetsRouteSpec.scala
+++ node/src/test/scala/com/wavesplatform/http/AssetsRouteSpec.scala
@@ -12,6 +12,7 @@ import com.wavesplatform.api.http.requests.{TransferV1Request, TransferV2Request
 import com.wavesplatform.common.state.ByteStr
 import com.wavesplatform.common.utils.EitherExt2
 import com.wavesplatform.db.WithDomain
+import com.wavesplatform.db.WithState.AddrWithBalance
 import com.wavesplatform.features.BlockchainFeatures
 import com.wavesplatform.history.{Domain, defaultSigner}
 import com.wavesplatform.lang.directives.values.V6
@@ -311,13 +312,43 @@ class AssetsRouteSpec extends RouteSpec("/assets") with Eventually with RestAPIS
     checkDetails(d, route, issue, issue.id().toString, assetDescr)
   }
 
-  routePath(s"/details/{id} - non-smart asset") in routeTest() { (d, route) =>
-    val tx = issueTransaction()
+  routePath(s"/details/{id} - non-smart asset") in withDomain(RideV6, AddrWithBalance.enoughBalances(defaultSigner)) { d =>
+    val route = seal(
+      AssetsApiRoute(
+        restAPISettings,
+        testWallet,
+        DummyTransactionPublisher.accepting,
+        d.blockchain,
+        TestTime(),
+        d.accountsApi,
+        d.assetsApi,
+        MaxDistributionDepth,
+        new RouteTimeout(60.seconds)(Schedulers.fixedPool(1, "heavy-request-scheduler"))
+      ).route
+    )
 
-    d.appendBlock(TxHelpers.genesis(tx.sender.toAddress))
-    d.appendBlock(tx)
+    val issues = (1 to 10).map(i => (i, issueTransaction())).toMap
 
-    checkDetails(d, route, tx, tx.id().toString, assetDesc)
+    d.appendMicroBlock(issues(1))
+    checkDetails(route, issues(1), issues(1).id().toString, assetDesc)
+
+    (2 to 6).foreach { i =>
+      d.appendMicroBlock(issues(i))
+      checkDetails(route, issues(i), issues(i).id().toString, assetDesc.copy(sequenceInBlock = i))
+    }
+
+    d.appendKeyBlock()
+    (1 to 6).foreach { i =>
+      checkDetails(route, issues(i), issues(i).id().toString, assetDesc.copy(sequenceInBlock = i))
+    }
+
+    d.appendBlock((7 to 10).map(issues): _*)
+    (1 to 6).foreach { i =>
+      checkDetails(route, issues(i), issues(i).id().toString, assetDesc.copy(sequenceInBlock = i))
+    }
+    (7 to 10).foreach { i =>
+      checkDetails(route, issues(i), issues(i).id().toString, assetDesc.copy(sequenceInBlock = i - 6, issueHeight = Height @@ 2))
+    }
   }
 
   routePath("/{assetId}/distribution/{height}/limit/{limit}") in routeTest() { (d, route) =>
@@ -589,18 +620,22 @@ class AssetsRouteSpec extends RouteSpec("/assets") with Eventually with RestAPIS
 
   private def checkDetails(domain: Domain, route: Route, tx: Transaction, assetId: String, assetDesc: AssetDescription): Unit = {
     domain.liquidAndSolidAssert { () =>
-      Get(routePath(s"/details/$assetId")) ~> route ~> check {
-        val response = responseAs[JsObject]
-        checkResponse(tx, assetDesc, assetId, response)
-      }
-      Get(routePath(s"/details?id=$assetId")) ~> route ~> check {
-        val responses = responseAs[List[JsObject]]
-        responses.foreach(response => checkResponse(tx, assetDesc, assetId, response))
-      }
-      Post(routePath("/details"), Json.obj("ids" -> List(s"$assetId"))) ~> route ~> check {
-        val responses = responseAs[List[JsObject]]
-        responses.foreach(response => checkResponse(tx, assetDesc, assetId, response))
-      }
+      checkDetails(route, tx, assetId, assetDesc)
+    }
+  }
+
+  private def checkDetails(route: Route, tx: Transaction, assetId: String, assetDesc: AssetDescription): Unit = {
+    Get(routePath(s"/details/$assetId")) ~> route ~> check {
+      val response = responseAs[JsObject]
+      checkResponse(tx, assetDesc, assetId, response)
+    }
+    Get(routePath(s"/details?id=$assetId")) ~> route ~> check {
+      val responses = responseAs[List[JsObject]]
+      responses.foreach(response => checkResponse(tx, assetDesc, assetId, response))
+    }
+    Post(routePath("/details"), Json.obj("ids" -> List(s"$assetId"))) ~> route ~> check {
+      val responses = responseAs[List[JsObject]]
+      responses.foreach(response => checkResponse(tx, assetDesc, assetId, response))
     }
   }
 
