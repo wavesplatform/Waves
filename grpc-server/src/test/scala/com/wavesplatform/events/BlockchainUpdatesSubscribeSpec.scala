121668cde16e8be96c7067a5cd2f7f448d47250b
vnikolaenko <vnikolaenko@web3tech.ru>
2023-04-12 11:28:50 +0300

    edited test return correct data for alias tx and added test return correct data for alias tx for smart account

--- grpc-server/src/test/scala/com/wavesplatform/events/BlockchainUpdatesSubscribeSpec.scala
+++ grpc-server/src/test/scala/com/wavesplatform/events/BlockchainUpdatesSubscribeSpec.scala
@@ -1,30 +1,56 @@
 package com.wavesplatform.events
 
 import com.wavesplatform.events.FakeObserver.{E, UpdatesRepoExt}
-import com.wavesplatform.events.api.grpc.protobuf.SubscribeRequest
+import com.wavesplatform.events.api.grpc.protobuf.{SubscribeEvent, SubscribeRequest}
+import com.wavesplatform.events.protobuf.BlockchainUpdated.Append
 import com.wavesplatform.settings.WavesSettings
 import com.wavesplatform.test.DomainPresets.RideV6
 import com.wavesplatform.test.FreeSpec
-import com.wavesplatform.transaction.TxHelpers
-import com.wavesplatform.transaction.TxHelpers.defaultAddress
+import com.wavesplatform.transaction.{CreateAliasTransaction, TxHelpers}
+import com.wavesplatform.transaction.TxHelpers.{secondAddress, secondSigner}
 import org.scalatest.concurrent.ScalaFutures
 
 class BlockchainUpdatesSubscribeSpec extends FreeSpec with WithBUDomain with ScalaFutures {
   val currentSettings: WavesSettings = RideV6
+  val testAlias                      = "test"
+  var balanceBefore                  = 0L
+  var balanceAfter                   = 0L
 
   "return correct data for alias tx" in withDomainAndRepo(currentSettings) { case (d, repo) =>
     d.appendBlock(TxHelpers.genesis(TxHelpers.defaultAddress))
     d.appendKeyBlock()
+    d.appendBlock(TxHelpers.transfer())
 
-    val testAlias     = "test"
-    val balanceBefore = d.balance(defaultAddress)
-    val alias         = TxHelpers.createAlias(testAlias)
-    val subscription  = repo.createFakeObserver(SubscribeRequest(1))
-
+    balanceBefore = d.balance(secondAddress)
+    val alias        = TxHelpers.createAlias(testAlias, secondSigner)
+    val subscription = repo.createFakeObserver(SubscribeRequest(2))
     d.appendMicroBlock(alias)
+    val append = subscription.fetchAllEvents(d.blockchain).map(_.getUpdate.getAppend).last
 
-    val balanceAfter                    = d.balance(defaultAddress)
-    val append                          = subscription.values.head.getUpdate.getAppend
+    balanceAfter = balanceBefore - alias.fee.value
+    assertions(append, alias)
+  }
+
+  // TODO - Доработать тест, сейчас нужно научиться создавать dApp account
+  "return correct data for alias tx for smart account" in withDomainAndRepo(currentSettings) { case (d, repo) =>
+    d.appendBlock(TxHelpers.genesis(TxHelpers.defaultAddress))
+    d.appendKeyBlock()
+    d.appendBlock(TxHelpers.transfer())
+
+
+    balanceBefore = d.balance(secondAddress)
+    val alias = TxHelpers.createAlias(testAlias, secondSigner)
+    val subscription = repo.createFakeObserver(SubscribeRequest(2))
+    d.appendMicroBlock(alias)
+    val append = subscription.fetchAllEvents(d.blockchain).map(_.getUpdate.getAppend).last
+
+    println(alias.fee.value)
+
+    balanceAfter = balanceBefore - alias.fee.value
+    assertions(append, alias)
+  }
+
+  def assertions(append: Append, alias: CreateAliasTransaction): Unit = {
     val tx                              = append.getMicroBlock.getMicroBlock.getMicroBlock.transactions
     val txId                            = append.transactionIds.head.toByteArray
     val transactionsMetadata            = append.transactionsMetadata
@@ -34,9 +60,10 @@ class BlockchainUpdatesSubscribeSpec extends FreeSpec with WithBUDomain with Sca
     tx.head.transaction.wavesTransaction.get.getFee.amount shouldEqual alias.fee.value
     tx.head.transaction.wavesTransaction.get.getCreateAlias.alias shouldBe testAlias
     txId shouldBe alias.id.apply().arr
-    transactionsMetadata.head.senderAddress.toByteArray shouldBe alias.sender.toAddress.bytes
-    transactionStateUpdatesBalances.address.toByteArray shouldBe alias.sender.toAddress.bytes
+    transactionsMetadata.head.senderAddress.toByteArray shouldBe secondAddress.bytes
+    transactionStateUpdatesBalances.address.toByteArray shouldBe secondAddress.bytes
     transactionStateUpdatesBalances.amountBefore shouldEqual balanceBefore
-    transactionStateUpdatesBalances.amountAfter shouldEqual balanceAfter
+    transactionStateUpdatesBalances.amountAfter.get.amount shouldEqual balanceAfter
+    transactionStateUpdatesBalances.amountAfter.get.assetId.isEmpty shouldBe true
   }
 }
