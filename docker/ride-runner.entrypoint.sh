0bab4af42f3ad9b74fd0a908b6e821102de7b640
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2023-06-25 13:48:46 +0400

    * Right JVM flags to log GC events; * New memory limits.

--- docker/ride-runner.entrypoint.sh
+++ docker/ride-runner.entrypoint.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 shopt -s nullglob
 
-# UnlockDiagnosticVMOptions is temporary
+# Useful options:
+#   -XX:+HeapDumpOnOutOfMemoryError
+#   -XX:HeapDumpPath=${RDATA}/heap-dumps
+#
 # GCLockerRetryAllocationCount to prevent false OOM
+#
+# UnlockDiagnosticVMOptions is required for GCLockerRetryAllocationCount, otherwise we get:
+#   Error: VM option 'GCLockerRetryAllocationCount' is diagnostic and must be enabled via -XX:+UnlockDiagnosticVMOptions.
+#   Error: The unlock option must precede 'GCLockerRetryAllocationCount'.
 JAVA_OPTS="-javaagent:${RIDE_INSTALL_PATH}/kanela-agent/kanela-agent-1.0.17.jar
   --add-opens=java.base/java.lang=ALL-UNNAMED
   --add-opens=java.base/java.math=ALL-UNNAMED
   --add-opens=java.base/java.util=ALL-UNNAMED
+  -XX:+UnlockDiagnosticVMOptions
+  -Xlog:gc::time,level,tags
   -XX:+ExitOnOutOfMemoryError
-  -XX:+HeapDumpOnOutOfMemoryError
-  -XX:HeapDumpPath=${RDATA}/heap-dumps
   -XX:+UseG1GC
   -XX:+ParallelRefProcEnabled
   -XX:+UseStringDeduplication
-  -XX:+UnlockDiagnosticVMOptions
   -XX:GCLockerRetryAllocationCount=100
   -Xmx${RIDE_HEAP_SIZE}
   -XX:MaxMetaspaceSize=152m
@@ -23,8 +29,7 @@ JAVA_OPTS="-javaagent:${RIDE_INSTALL_PATH}/kanela-agent/kanela-agent-1.0.17.jar
   -Dlogback.shutdown-hook-delay=2000
   -Dconfig.override_with_env_vars=true
   -Dwaves.defaults.blockchain.type=$RIDE_NETWORK
-  -Dwaves.defaults.directory=$RDATA
-  -Dride.heapDumps.enabled=true"
+  -Dwaves.defaults.directory=$RDATA"
 
 [ -n "${ASYNCPROF_OPTS}" ] && JAVA_OPTS="-agentpath:/usr/local/async-profiler/build/libasyncProfiler.so=$ASYNCPROF_OPTS $JAVA_OPTS"
 
