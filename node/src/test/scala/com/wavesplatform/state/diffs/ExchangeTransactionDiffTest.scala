73a3d61730ccdeec820f03a1cef83106b26f82d1
Artyom Sayadyan <xrtm000@gmail.com>
2023-07-05 20:27:03 +0300

    ExchangeTransactionDiffTest

--- node/src/test/scala/com/wavesplatform/state/diffs/ExchangeTransactionDiffTest.scala
+++ node/src/test/scala/com/wavesplatform/state/diffs/ExchangeTransactionDiffTest.scala
@@ -335,7 +335,7 @@ class ExchangeTransactionDiffTest extends PropSpec with Inside with WithDomain w
         val totalPortfolioDiff: Portfolio = blockDiff.portfolios.values.fold(Portfolio())(_.combine(_).explicitGet())
         totalPortfolioDiff.balance shouldBe 0
         totalPortfolioDiff.effectiveBalance.explicitGet() shouldBe 0
-        totalPortfolioDiff.assets.values.toSet shouldBe Set(0L)
+        totalPortfolioDiff.assets.values.toSet should (be (Set()) or be (Set(0)))
 
         blockDiff.portfolios(exchange.sender.toAddress).balance shouldBe exchange.buyMatcherFee + exchange.sellMatcherFee - exchange.fee.value
       }
