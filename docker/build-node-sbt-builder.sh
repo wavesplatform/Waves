2a23543b7f6d7c032c1c3699343feac617f39b1c
Sergey Nazarov <snazarov@uniqsoft.ae>
2023-07-10 15:35:59 +0300

    Pull newer images diring docker build

--- docker/build-node-sbt-builder.sh
+++ docker/build-node-sbt-builder.sh
@@ -5,5 +5,6 @@ WAVES_VERSION=$(cut -d\" -f2 ../version.sbt)
 docker build \
   --build-arg SBT_VERSION=$(cut -d= -f2 ../project/build.properties) \
   --build-arg WAVES_VERSION=$WAVES_VERSION \
+  --pull \
   -t wavesplatform/node-sbt-builder:$WAVES_VERSION \
   - < node-sbt-builder.Dockerfile
