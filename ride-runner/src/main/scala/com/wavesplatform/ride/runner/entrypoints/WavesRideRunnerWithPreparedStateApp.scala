35db16353317eb9c001141d9085fc0eed44b8037
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2023-08-09 10:54:48 +0400

    WavesRideRunnerWithPreparedStateApp: additional options, refactoring and improvements.

--- ride-runner/src/main/scala/com/wavesplatform/ride/runner/entrypoints/WavesRideRunnerWithPreparedStateApp.scala
+++ ride-runner/src/main/scala/com/wavesplatform/ride/runner/entrypoints/WavesRideRunnerWithPreparedStateApp.scala
@@ -13,7 +13,7 @@ import monix.execution.schedulers.CanBlock
 import monix.reactive.Observable
 import org.slf4j.LoggerFactory
 import play.api.libs.json.*
-import scopt.{DefaultOParserSetup, OParser}
+import scopt.{DefaultOParserSetup, OParser, Read}
 
 import java.io.File
 import java.nio.file.Path
@@ -29,59 +29,46 @@ object WavesRideRunnerWithPreparedStateApp {
 
     OParser.parse(commandParser, args, Args(), setup).foreach { args =>
       System.setProperty("logback.stdout.level", if (args.verbose) "TRACE" else "OFF")
-      val log        = LoggerFactory.getLogger("Main")
-      val input      = args.input.map(_.toPath.toAbsolutePath) // Otherwise Lightbend Config doesn't resolve correctly relative paths
-      val commonPath = input.reduceLeft(PathUtils.commonPath)
+      val log             = LoggerFactory.getLogger("Main")
+      val input           = args.input.map(_.toPath.toAbsolutePath) // Otherwise Lightbend Config doesn't resolve correctly relative paths
+      lazy val commonPath = input.reduceLeft(PathUtils.commonPath)
 
-      // TODO if one file
       Observable
         .fromIterable(input)
-        .mapParallelOrdered(args.parallelism) { path =>
-          Task {
-            if (path.getFileName.toString.endsWith(".conf"))
-              Try(ConfigFactory.parseFile(path.toFile).resolve().root().render(ConfigRenderOptions.concise())) match {
-                case Failure(e) => RunResult.Error(path, new RuntimeException("Can't parse HOCON file", e))
-
-                case Success(inputRawJson) =>
-                  val inputJson = RideRunnerInputParser.parseJson(inputRawJson)
-                  AppInitializer.setupChain(RideRunnerInputParser.getChainId(inputJson)) // We must setup chain first to parse addresses
-                  RunResult.Succeeded(path, run(inputJson))
-              }
-            else RunResult.Error(path, new RuntimeException("Isn't a HOCON file") with NoStackTrace)
-          }.onErrorRecover(RunResult.Error(path, _))
+        .mapParallelOrdered(args.threads) { path =>
+          val minimizedPath = if (args.minimizePaths) commonPath.relativize(path) else path
+          Task(run(path, minimizedPath)).onErrorRecover(RunResult.Error(minimizedPath, _))
         }
         .foldLeftL(Stats(0, 0, 0)) { case (r, x) =>
-          val file = commonPath.relativize(x.path)
+          args.printMode.printRunResult(x)
           x match {
-            case x: RunResult.Succeeded =>
-              println(s"[OK] $file:\n${Json.prettyPrint(x.result).split("\n").mkString("  ", "\n  ", "  ")}\n")
-              r.copy(succeeded = r.succeeded + 1)
-            case _: RunResult.Failed =>
-              println(s"[FAILED] $file\b")
-              r.copy(failed = r.failed + 1)
+            case _: RunResult.Succeeded => r.copy(succeeded = r.succeeded + 1)
+            case _: RunResult.Failed    => r.copy(failed = r.failed + 1)
             case x: RunResult.Error =>
-              log.warn(s"Error during running $file", x.exception)
-              System.err.println(s"[ERROR] $file: ${x.exception.getMessage}\b")
+              log.warn(s"Error during running ${x.path}", x.exception)
               r.copy(error = r.error + 1)
           }
         }
-        .tapEval { x => Task(println(s"Total $x")) }
+        .tapEval { x =>
+          Task {
+            if (args.printTotal) args.printMode.printTotal(x)
+          }
+        }
         .runSyncUnsafe()(global, CanBlock.permit)
     }
   }
 
-  private case class Stats(succeeded: Int, failed: Int, error: Int) {
-    override def toString: String = s"succeeded: $succeeded, failed: $failed, error: $error"
-  }
+  private def run(path: Path, pathForResult: Path): RunResult = {
+    if (path.getFileName.toString.endsWith(".conf"))
+      Try(ConfigFactory.parseFile(path.toFile).resolve().root().render(ConfigRenderOptions.concise())) match {
+        case Failure(e) => RunResult.Error(pathForResult, new RuntimeException("Can't parse HOCON file", e))
 
-  private sealed trait RunResult extends Product with Serializable {
-    def path: Path
-  }
-
-  private object RunResult {
-    case class Succeeded(path: Path, result: JsObject)                  extends RunResult
-    case class Failed(path: Path, actual: JsObject, expected: JsObject) extends RunResult
-    case class Error(path: Path, exception: Throwable)                  extends RunResult
+        case Success(inputRawJson) =>
+          val inputJson = RideRunnerInputParser.parseJson(inputRawJson)
+          AppInitializer.setupChain(RideRunnerInputParser.getChainId(inputJson)) // We must setup chain first to parse addresses
+          RunResult.Succeeded(pathForResult, run(inputJson))
+      }
+    else RunResult.Error(pathForResult, new RuntimeException("Isn't a HOCON file") with NoStackTrace)
   }
 
   def run(inputJson: JsValue): JsObject = {
@@ -112,17 +99,10 @@ object WavesRideRunnerWithPreparedStateApp {
     )
   }
 
-  private final case class Args(
-      input: List[File] = Nil,
-      parallelism: Int = Runtime.getRuntime.availableProcessors(),
-      mode: Mode = Mode.Run,
-      verbose: Boolean = false
-  )
-
-  sealed trait Mode
-  object Mode {
-    case object Run                             extends Mode
-    case class Test(junit: Option[File] = None) extends Mode
+  private implicit val printModeRead: Read[PrintMode] = Read.stringRead.map {
+    case "text" => PrintMode.Text
+    case "json" => PrintMode.Json
+    case x      => throw new IllegalArgumentException(s"Unknown print mode: $x")
   }
 
   private val commandParser = {
@@ -135,11 +115,26 @@ object WavesRideRunnerWithPreparedStateApp {
       head("RIDE script runner", Version.VersionString),
       opt[Unit]('v', "verbose")
         .text("Print logs")
-        .action((x, c) => c.copy(verbose = true)),
-      opt[Int]('p', "parallelism")
+        .action((_, c) => c.copy(verbose = true)),
+      opt[Int]('t', "threads")
         .optional()
-        .text("A number of parallel threads to process scripts. A number of available cores by default")
-        .action((x, c) => c.copy(parallelism = x)),
+        .text("A number of parallel threads to process scripts. A number of available cores by default.")
+        .action((x, c) => c.copy(threads = x)),
+      opt[PrintMode]('p', "printMode")
+        .optional()
+        .text("Print mode: 'text' or 'json'. 'text' by default.")
+        .action((x, c) => c.copy(printMode = x)),
+      opt[Unit]("printTotal")
+        .optional()
+        .text("Print statistics. Not printing by default.")
+        .action((_, c) => c.copy(printTotal = true)),
+      opt[Unit]("doNotMinimizePaths")
+        .optional()
+        .text(
+          "By default this tool finds a common part of paths of specified files and relativize all paths against it " +
+            "for security reasons. Use this option to disable this behavior."
+        )
+        .action((_, c) => c.copy(minimizePaths = false)),
       help("help").hidden(),
       arg[File]("<file>...")
         .unbounded()
@@ -147,16 +142,106 @@ object WavesRideRunnerWithPreparedStateApp {
         .text("Path to HOCON (conf) file (or files) with prepared state and run arguments.")
         .action((x, c) => c.copy(input = x.getAbsoluteFile :: c.input)),
       cmd("run")
-        .text("  Run scripts and print results to STDOUT.")
-        .action((_, c) => c.copy(mode = Mode.Run)),
+        .text("  Runs scripts and prints results to STDOUT.")
+        .action((_, c) => c.copy(runMode = RunMode.Run)),
       cmd("test")
-        .text("  Expects that provided files are tests.")
-        .action((_, c) => c.copy(mode = Mode.Test()))
+        .text("  Expects that provided files are tests. Runs tests and prints results to STDOUT.")
+        .action((_, c) => c.copy(runMode = RunMode.Test()))
         .children(
           opt[File]("junit")
             .text("Write a JUnit report to this file. An existed file will be overwritten.")
-            .action((x, c) => c.copy(mode = Mode.Test(Some(x))))
+            .action((x, c) => c.copy(runMode = RunMode.Test(Some(x))))
         )
     )
+  }
+
+  private final case class Args(
+      input: List[File] = Nil,
+      threads: Int = Runtime.getRuntime.availableProcessors(),
+      runMode: RunMode = RunMode.Run,
+      printMode: PrintMode = PrintMode.Text,
+      minimizePaths: Boolean = true,
+      printTotal: Boolean = false,
+      verbose: Boolean = false
+  )
+
+  private sealed trait RunMode extends Product with Serializable
+  private object RunMode {
+    case object Run                             extends RunMode
+    case class Test(junit: Option[File] = None) extends RunMode
+  }
+
+  private sealed trait PrintMode {
+    def asPrinted(x: Printable): String
+
+    def printRunResult(x: RunResult): Unit = x match {
+      case x: RunResult.Error => System.err.println(asPrinted(x))
+      case _                  => println(asPrinted(x))
+    }
+
+    def printTotal(x: Stats): Unit = println(asPrinted(x))
+  }
+
+  private object PrintMode {
+    case object Text extends PrintMode {
+      override def asPrinted(x: Printable): String = x.asString
+    }
+
+    case object Json extends PrintMode {
+      override def asPrinted(x: Printable): String = x.asJson.toString()
+    }
+  }
+
+  private case class Stats(succeeded: Int, failed: Int, error: Int) extends Printable {
+    override def asJson: JsObject = Json.obj(
+      "succeeded" -> succeeded,
+      "failed"    -> failed,
+      "error"     -> error
+    )
+
+    override def asString: String = s"Total succeeded: $succeeded, failed: $failed, error: $error"
+  }
+
+  private sealed trait RunResult extends Printable with Product with Serializable {
+    def path: Path
+  }
+
+  private object RunResult {
+    case class Succeeded(path: Path, result: JsObject) extends RunResult {
+      override def asJson: JsObject = Json.obj(
+        "path"   -> path.toString,
+        "status" -> "ok",
+        "result" -> result
+      )
+
+      override def asString: String = s"[OK] $path:\n${Json.prettyPrint(result).split("\n").mkString("  ", "\n  ", "  ")}"
+    }
+
+    case class Failed(path: Path, actual: JsObject, expected: JsObject) extends RunResult {
+      override def asJson: JsObject = Json.obj(
+        "path"     -> path.toString,
+        "status"   -> "failed",
+        "result"   -> actual,
+        "expected" -> expected
+      )
+
+      override def asString: String = s"[FAILED] $path\n"
+    }
+
+    case class Error(path: Path, exception: Throwable) extends RunResult {
+      override def asJson: JsObject = Json.obj(
+        "path"    -> path.toString,
+        "status"  -> "error",
+        "result"  -> JsNull,
+        "message" -> exception.getMessage
+      )
+
+      override def asString: String = s"[ERROR] $path: ${exception.getMessage}"
+    }
+  }
+
+  private sealed trait Printable {
+    def asJson: JsObject
+    def asString: String
   }
 }
