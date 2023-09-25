d1b232a621a735070cb1249bc98d886728c8bafd
Artyom Sayadyan <xrtm000@gmail.com>
2023-09-25 15:37:30 +0300

    NODE-2612 RIDE replaceByIndex()

---
+++ benchmark/src/test/scala/com/wavesplatform/lang/v1/ListReplaceByIndexBenchmark.scala
@@ -0,0 +1,71 @@
+package com.wavesplatform.lang.v1
+
+import com.wavesplatform.common.utils.EitherExt2
+import com.wavesplatform.lang.Common
+import com.wavesplatform.lang.directives.values.V8
+import com.wavesplatform.lang.v1.FunctionHeader.Native
+import com.wavesplatform.lang.v1.compiler.Terms.*
+import com.wavesplatform.lang.v1.evaluator.FunctionIds.REPLACE_BY_INDEX_OF_LIST
+import com.wavesplatform.lang.v1.evaluator.ctx.impl.PureContext
+import com.wavesplatform.lang.v1.traits.Environment
+import org.openjdk.jmh.annotations.*
+import org.openjdk.jmh.infra.Blackhole
+
+import scala.concurrent.duration.{MICROSECONDS, SECONDS}
+
+@OutputTimeUnit(MICROSECONDS)
+@BenchmarkMode(Array(Mode.AverageTime))
+@Threads(1)
+@Fork(1)
+@Warmup(iterations = 10, time = 1, timeUnit = SECONDS)
+@Measurement(iterations = 10, time = 1, timeUnit = SECONDS)
+class ListReplaceByIndexBenchmark {
+  @Benchmark
+  def listReplaceFirstByIndex(st: ListReplaceByIndexSt, bh: Blackhole): Unit =
+    bh.consume(eval(st.ctx, st.replaceFirst, V8))
+
+  @Benchmark
+  def listReplaceMiddleByIndex(st: ListReplaceByIndexSt, bh: Blackhole): Unit =
+    bh.consume(eval(st.ctx, st.replaceMiddle, V8))
+
+  @Benchmark
+  def listReplaceLastByIndex(st: ListReplaceByIndexSt, bh: Blackhole): Unit =
+    bh.consume(eval(st.ctx, st.replaceLast, V8))
+}
+
+@State(Scope.Benchmark)
+class ListReplaceByIndexSt {
+  val ctx = PureContext.build(V8, useNewPowPrecision = true).withEnvironment[Environment].evaluationContext(Common.emptyBlockchainEnvironment())
+
+  val list = ARR(Vector.fill(1000)(CONST_LONG(Long.MaxValue)), limited = true).explicitGet()
+
+  val replaceFirst =
+    FUNCTION_CALL(
+      Native(REPLACE_BY_INDEX_OF_LIST),
+      List(
+        list,
+        CONST_LONG(0),
+        CONST_LONG(777)
+      )
+    )
+
+  val replaceMiddle =
+    FUNCTION_CALL(
+      Native(REPLACE_BY_INDEX_OF_LIST),
+      List(
+        list,
+        CONST_LONG(500),
+        CONST_LONG(777)
+      )
+    )
+
+  val replaceLast =
+    FUNCTION_CALL(
+      Native(REPLACE_BY_INDEX_OF_LIST),
+      List(
+        list,
+        CONST_LONG(999),
+        CONST_LONG(777)
+      )
+    )
+}
---
+++ lang/tests/src/test/scala/com/wavesplatform/lang/evaluator/ListReplaceByIndexTest.scala
@@ -0,0 +1,30 @@
+package com.wavesplatform.lang.evaluator
+
+import com.wavesplatform.lang.directives.values.{StdLibVersion, V8}
+import com.wavesplatform.lang.v1.compiler.Terms.CONST_BOOLEAN
+import com.wavesplatform.test.produce
+
+class ListReplaceByIndexTest extends EvaluatorSpec {
+  private implicit val version: StdLibVersion = V8
+
+  property("successful results") {
+    val bigList = (1 to 1000).map(_ => """"a"""").mkString("[", ",", "]")
+    eval(
+      s"""
+         | [0].replaceByIndex(0, 9)               == [9]             &&
+         | [0, 1, 2, 3, 4].replaceByIndex(0, 9)   == [9, 1, 2, 3, 4] &&
+         | [0, 1, 2, 3, 4].replaceByIndex(2, 9)   == [0, 1, 9, 3, 4] &&
+         | [0, 1, 2, 3, 4].replaceByIndex(4, 9)   == [0, 1, 2, 3, 9] &&
+         | $bigList.replaceByIndex(0, "b")[0]     == "b"             && # can't compare the whole list due to
+         | $bigList.replaceByIndex(500, "b")[500] == "b"             && # "Comparable value too heavy" error
+         | $bigList.replaceByIndex(999, "b")[999] == "b"
+       """.stripMargin
+    ) shouldBe Right(CONST_BOOLEAN(true))
+  }
+
+  property("errors") {
+    eval("[].replaceByIndex(0, 1)") should produce("Can't replace an element in empty list")
+    eval("[1, 2, 3].replaceByIndex(-1, 1)") should produce("Index of the replacing element should be positive, but -1 was passed")
+    eval("[1, 2, 3].replaceByIndex(3, 1)") should produce("Index of the replacing element should be lower than list size = 3, but 3 was passed")
+  }
+}
