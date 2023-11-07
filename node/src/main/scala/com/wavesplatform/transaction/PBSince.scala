d5b9aa3c909b386da07282f6cae69fe23b8a7344
Artyom Sayadyan <xrtm000@gmail.com>
2023-11-07 20:18:22 +0300

    Simplified PBSince

--- node/src/main/scala/com/wavesplatform/transaction/PBSince.scala
+++ node/src/main/scala/com/wavesplatform/transaction/PBSince.scala
@@ -1,25 +1,20 @@
 package com.wavesplatform.transaction
 
-import com.wavesplatform.protobuf.transaction.PBTransactions
+import com.wavesplatform.transaction.PBSince.*
 
-trait PBSince { self: Transaction with VersionedTransaction =>
-  def protobufVersion: TxVersion
+import scala.annotation.nowarn
+
+sealed trait PBSince { self: Transaction & VersionedTransaction =>
+  lazy val protobufVersion: TxVersion = this match {
+    case _: V1 => TxVersion.V1
+    case _: V2 => TxVersion.V2: @nowarn // scalac: unreachable code
+    case _: V3 => TxVersion.V3
+  }
   final def isProtobufVersion: Boolean = self.version >= protobufVersion
-
-  override def bytesSize: Int =
-    if (isProtobufVersion) PBTransactions.protobuf(self).serializedSize else bytes().length
 }
 
 object PBSince {
-  trait V1 extends PBSince { self: Transaction with VersionedTransaction =>
-    override def protobufVersion: TxVersion = TxVersion.V1
-  }
-
-  trait V2 extends PBSince { self: Transaction with VersionedTransaction =>
-    override def protobufVersion: TxVersion = TxVersion.V2
-  }
-
-  trait V3 extends PBSince { self: Transaction with VersionedTransaction =>
-    override def protobufVersion: TxVersion = TxVersion.V3
-  }
+  trait V1 extends PBSince { self: Transaction & VersionedTransaction => }
+  trait V2 extends PBSince { self: Transaction & VersionedTransaction => }
+  trait V3 extends PBSince { self: Transaction & VersionedTransaction => }
 }
