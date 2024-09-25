0252400d11022f0a8123076d435663c40eae53d0
Vladimir Logachev <vladimir@logachev.dev>
2024-09-25 09:59:06 +0400

    Replace coproducts with unions

--- lang/shared/src/main/scala/com/wavesplatform/lang/v1/traits/Environment.scala
+++ lang/shared/src/main/scala/com/wavesplatform/lang/v1/traits/Environment.scala
@@ -13,10 +13,10 @@ import shapeless3.*
 object Environment {
   case class BalanceDetails(available: Long, regular: Long, generating: Long, effective: Long)
 
-  type InputEntity = Tx :+: Ord :+: PseudoTx :+: CNil
+  type InputEntity = Tx | Ord | PseudoTx
 
   case class AssetId(id: Array[Byte])
-  type Tthis = Recipient.Address :+: AssetId :+: CNil
+  type Tthis = Recipient.Address | AssetId
 }
 
 trait Environment[F[_]] {
