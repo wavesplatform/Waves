cd2da5e53c8e74bc4595dd09fac8daaefb295515
Vladimir Logachev <vladimir@logachev.dev>
2024-09-27 18:33:25 +0400

    Add EitherExt3

--- lang/shared/src/main/scala/com/wavesplatform/common/utils/package.scala
+++ lang/shared/src/main/scala/com/wavesplatform/common/utils/package.scala
@@ -5,6 +5,32 @@ import scala.util.{Failure, Success, Try}
 package object utils {
   val Base58Alphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
 
+  /** Scala 3 version */
+  object EitherExt3 {
+    extension [A, B](ei: Either[A, B]) {
+      def explicitGet(): B = ei match {
+        case Left(value)  => throw makeException(value)
+        case Right(value) => value
+      }
+
+      // used for destructuring in for-comprehensions
+      def withFilter(check: B => Boolean): Either[A, B] =
+        ei.filterOrElse(check, throw new MatchError(ei))
+
+      def foldToTry: Try[B] = ei.fold(
+        left => Failure(makeException(left)),
+        right => Success(right)
+      )
+
+      @inline
+      private def makeException(value: Any): Throwable = value match {
+        case err: Throwable => err
+        case _              => new RuntimeException(value.toString)
+      }
+
+    }
+  }
+
   implicit class EitherExt2[A, B](val ei: Either[A, B]) extends AnyVal {
     def explicitGet(): B = ei match {
       case Left(value)  => throw makeException(value)
