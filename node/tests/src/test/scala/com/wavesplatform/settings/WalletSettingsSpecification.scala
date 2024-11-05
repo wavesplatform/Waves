168bb70e532208a3db96d2fcffe2f99a95d509d4
Vladimir Logachev <vladimir@logachev.dev>
2024-11-05 11:42:38 +0400

    Fix WalletSettingsSpecification

--- node/tests/src/test/scala/com/wavesplatform/settings/WalletSettingsSpecification.scala
+++ node/tests/src/test/scala/com/wavesplatform/settings/WalletSettingsSpecification.scala
@@ -3,16 +3,16 @@ package com.wavesplatform.settings
 import com.typesafe.config.ConfigFactory
 import com.wavesplatform.common.state.ByteStr
 import com.wavesplatform.test.FlatSpec
-import net.ceedubs.ficus.Ficus._
-import net.ceedubs.ficus.readers.ArbitraryTypeReader._
+import pureconfig.ConfigSource
+import pureconfig.generic.auto.*
 
 class WalletSettingsSpecification extends FlatSpec {
   "WalletSettings" should "read values from config" in {
-    val config   = loadConfig(ConfigFactory.parseString("""waves.wallet {
-        |  password: "some string as password"
-        |  seed: "BASE58SEED"
-        |}""".stripMargin))
-    val settings = config.as[WalletSettings]("waves.wallet")
+    val config = loadConfig(ConfigFactory.parseString("""waves.wallet {
+                                                        |  password: "some string as password"
+                                                        |  seed: "BASE58SEED"
+                                                        |}""".stripMargin))
+    val settings = ConfigSource.fromConfig(config).at("waves.wallet").loadOrThrow[WalletSettings]
 
     settings.seed should be(Some(ByteStr.decodeBase58("BASE58SEED").get))
     settings.password should be(Some("some string as password"))
