1308144be464bbc67c19fd39b29d931bc25c3266
Vladimir Logachev <vladimir@logachev.dev>
2024-12-05 09:45:16 +0400

    Refactor more shapeless code

--- node/src/main/scala/com/wavesplatform/transaction/smart/package.scala
+++ node/src/main/scala/com/wavesplatform/transaction/smart/package.scala
@@ -4,8 +4,11 @@ import cats.syntax.either.*
 import com.wavesplatform.common.state.ByteStr
 import com.wavesplatform.lang.directives.DirectiveSet
 import com.wavesplatform.lang.directives.values.{Account, Expression, Asset as AssetType, DApp as DAppType}
-import com.wavesplatform.lang.v1.traits.Environment.{InputEntity, Tthis}
+import com.wavesplatform.lang.v1.traits.domain.PseudoTx
+import com.wavesplatform.lang.v1.traits.domain.Recipient
+import com.wavesplatform.lang.v1.traits.Environment.{InputEntity, Tthis, AssetId}
 import com.wavesplatform.state.Blockchain
+import com.wavesplatform.transaction.assets.exchange.Order
 import com.wavesplatform.transaction.smart.script.ScriptRunner.TxOrd
 import com.wavesplatform.transaction.smart.{DApp as DAppTarget}
 
@@ -16,18 +19,11 @@ package object smart {
       ds: DirectiveSet,
       scriptContainerAddress: Tthis
   ): Either[String, InputEntity] =
-    in.eliminate(
-      tx =>
-        RealTransactionWrapper(tx, blockchain, ds.stdLibVersion, paymentTarget(ds, scriptContainerAddress))
-          .map(Coproduct[InputEntity](_)),
-      _.eliminate(
-        order => Coproduct[InputEntity](RealTransactionWrapper.ord(order)).asRight[String],
-        _.eliminate(
-          scriptTransfer => Coproduct[InputEntity](scriptTransfer).asRight[String],
-          _ => ???
-        )
-      )
-    )
+    in match {
+      case tx: TransactionBase => RealTransactionWrapper(tx, blockchain, ds.stdLibVersion, paymentTarget(ds, scriptContainerAddress))
+      case order: Order => RealTransactionWrapper.ord(order).asRight
+      case scriptTransfer: PseudoTx => scriptTransfer.asRight
+    }
 
   def paymentTarget(
       ds: DirectiveSet,
@@ -36,11 +32,11 @@ package object smart {
     (ds.scriptType, ds.contentType) match {
       case (Account, DAppType)   => DAppTarget
       case (Account, Expression) => InvokerScript
-      case (AssetType, Expression) =>
-        scriptContainerAddress.eliminate(
-          _ => throw new Exception("Not a AssetId"),
-          _.eliminate(a => AssetScript(ByteStr(a.id)), v => throw new Exception(s"Fail processing tthis value $v"))
-        )
+      case (AssetType, Expression) => 
+        scriptContainerAddress match {
+          case _: Recipient.Address => throw new Exception("Not a AssetId")
+          case a: AssetId => AssetScript(ByteStr(a.id))
+        }
       case _ => ???
     }
 }
