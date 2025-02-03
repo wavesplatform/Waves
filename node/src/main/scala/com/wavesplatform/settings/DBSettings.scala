5ec04695c6cb08e4fa9a2bac543ab0c90de191eb
Vladimir Logachev <vladimir@logachev.dev>
2025-02-03 12:56:20 +0400

    Simplify DBSettings

--- node/src/main/scala/com/wavesplatform/settings/DBSettings.scala
+++ node/src/main/scala/com/wavesplatform/settings/DBSettings.scala
@@ -1,6 +1,7 @@
 package com.wavesplatform.settings
 
 import pureconfig.*
+import pureconfig.generic.semiauto.deriveReader
 
 case class DBSettings(
     directory: String,
@@ -10,37 +11,10 @@ case class DBSettings(
     storeStateHashes: Boolean,
     maxCacheSize: Int,
     maxRollbackDepth: Int,
-    cleanupInterval: Option[Int] = defaultCleanupInterval,
+    cleanupInterval: Option[Int],
     rocksdb: RocksDBSettings
 )
 
 object DBSettings {
-  // Note: This setup (default values + manual ConfigReader instance) 
-  // is a workaround for `pureconfig-generic-scala3` (it doesn't support default values from case classes yet)
-  val defaultCleanupInterval: Option[Int] = None
-
-  given ConfigReader[DBSettings] = ConfigReader.fromCursor(cur =>
-    for {
-      objCur <- cur.asObjectCursor
-      directory <- objCur.required[String]("directory")
-      storeTransactionsByAddress <- objCur.required[Boolean]("store-transactions-by-address")
-      storeLeaseStatesByAddress <- objCur.required[Boolean]("store-lease-states-by-address")
-      storeInvokeScriptResults <- objCur.required[Boolean]("store-invoke-script-results")
-      storeStateHashes <- objCur.required[Boolean]("store-state-hashes")
-      maxCacheSize <- objCur.required[Int]("max-cache-size")
-      maxRollbackDepth <- objCur.required[Int]("max-rollback-depth")
-      cleanupInterval <- objCur.optionalWithDefault("cleanup-interval", defaultCleanupInterval)
-      rocksdb <- objCur.required[RocksDBSettings]("rocksdb")
-    } yield DBSettings(
-      directory,
-      storeTransactionsByAddress,
-      storeLeaseStatesByAddress,
-      storeInvokeScriptResults,
-      storeStateHashes,
-      maxCacheSize,
-      maxRollbackDepth,
-      cleanupInterval,
-      rocksdb
-    )
-  )
+  given ConfigReader[DBSettings] = deriveReader[DBSettings]
 }
