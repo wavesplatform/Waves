cf75973756b2ca08cf33150ff1cf03e71e1c237e
Nikolay Eskov <mr.eskov1@yandex.ru>
2025-04-22 09:39:27 +0300

    Add gowaves 'TestInvokeActionsCountRestrictionsV6ToV5WithBlockRewardDistributionFailed'.

--- node/tests/src/test/scala/com/wavesplatform/state/diffs/ci/OverheadCallableCallTest.scala
+++ node/tests/src/test/scala/com/wavesplatform/state/diffs/ci/OverheadCallableCallTest.scala
@@ -222,4 +222,133 @@ class OverheadCallableCallTest extends PropSpec with WithDomain {
       d.appendBlockE(invoke6) should beLeft
     }
   }
+  property("TestInvokeActionsCountRestrictionsV6ToV5WithBlockRewardDistributionFailed") {
+    val invoker = TxHelpers.signer(0)
+    val dApp1   = TxHelpers.signer(1)
+    val dApp2   = TxHelpers.signer(2)
+    val dApp3   = TxHelpers.signer(3)
+
+    val balances = AddrWithBalance.enoughBalances(invoker, dApp1, dApp2, dApp3)
+
+    val dApp1Script =
+      TestCompiler(V6).compileContract(
+        s"""
+           | {-# STDLIB_VERSION 6 #-}
+           | {-# CONTENT_TYPE DAPP #-}
+           | {-# SCRIPT_TYPE ACCOUNT #-}
+           |
+           | let dApp2 = Address(base58'${dApp2.toAddress.toString}') # V6 script produces 10 transfer actions
+           | let dApp3 = Address(base58'${dApp3.toAddress.toString}') # V5 script produces no actions
+           |
+           | @Callable(i)
+           | func call() = {
+           |	 strict a1 = invoke(dApp2,  "call", [], [])
+           |	 strict b1 = invoke(dApp3,  "call", [], [])
+           |	 strict a2 = invoke(dApp2,  "call", [], [])
+           |	 strict b2 = invoke(dApp3,  "call", [], [])
+           |	 strict a3 = invoke(dApp2,  "call", [], [])
+           |	 strict b3 = invoke(dApp3,  "call", [], [])
+           |	 strict a4 = invoke(dApp2,  "call", [], [])
+           |	 strict b4 = invoke(dApp3,  "call", [], [])
+           |	 strict a5 = invoke(dApp2,  "call", [], [])
+           |	 strict b5 = invoke(dApp3,  "call", [], [])
+           |	 strict a6 = invoke(dApp2,  "call", [], [])
+           |	 strict b6 = invoke(dApp3,  "call", [], [])
+           |	 strict a7 = invoke(dApp2,  "call", [], [])
+           |	 strict b7 = invoke(dApp3,  "call", [], [])
+           |	 strict a8 = invoke(dApp2,  "call", [], [])
+           |	 strict b8 = invoke(dApp3,  "call", [], [])
+           |	 strict a9 = invoke(dApp2,  "call", [], [])
+           |	 strict b9 = invoke(dApp3,  "call", [], [])
+           |	 strict a10 = invoke(dApp2,  "call", [], [])
+           |	 strict b10 = invoke(dApp3,  "call", [], [])
+           |	 strict a11 = invoke(dApp2,  "call", [], [])
+           |	 strict b11 = invoke(dApp3,  "call", [], [])
+           |	 []
+           | }
+         """.stripMargin
+      )
+
+    val dApp2Script =
+      TestCompiler(V6).compileContract(
+        s"""
+           | {-# STDLIB_VERSION 6 #-}
+           | {-# CONTENT_TYPE DAPP #-}
+           | {-# SCRIPT_TYPE ACCOUNT #-}
+           |
+           | @Callable(i)
+           | func call() = [
+           |   ScriptTransfer(i.caller, 1, unit),
+           |   ScriptTransfer(i.caller, 2, unit),
+           |   ScriptTransfer(i.caller, 3, unit),
+           |   ScriptTransfer(i.caller, 4, unit),
+           |   ScriptTransfer(i.caller, 5, unit),
+           |   ScriptTransfer(i.caller, 6, unit),
+           |   ScriptTransfer(i.caller, 7, unit),
+           |   ScriptTransfer(i.caller, 8, unit),
+           |   ScriptTransfer(i.caller, 9, unit),
+           |   ScriptTransfer(i.caller, 10, unit)
+           | ]
+         """.stripMargin
+      )
+
+    val dApp3Script =
+      TestCompiler(V5).compileContract(
+        s"""
+           | {-# STDLIB_VERSION 5 #-}
+           | {-# CONTENT_TYPE DAPP #-}
+           | {-# SCRIPT_TYPE ACCOUNT #-}
+           |
+           | @Callable(i)
+           | func call() = {
+           |   strict a = i.caller
+           |   []
+           | }
+         """.stripMargin
+      )
+
+    val setScripts = Seq(
+      TxHelpers.setScript(dApp1, dApp1Script),
+      TxHelpers.setScript(dApp2, dApp2Script),
+      TxHelpers.setScript(dApp3, dApp3Script)
+    )
+
+    val invoke1 =
+      TxHelpers.invoke(
+        dApp1.toAddress,
+        invoker = invoker,
+        func = Some("call"),
+        args = Seq()
+      )
+
+    val feats = Seq(
+      SmallerMinimalGeneratingBalance -> 0,
+      NG                              -> 0,
+      MassTransfer                    -> 0,
+      SmartAccounts                   -> 0,
+      DataTransaction                 -> 0,
+      BurnAnyTokens                   -> 0,
+      FeeSponsorship                  -> 0,
+      FairPoS                         -> 0,
+      SmartAssets                     -> 0,
+      SmartAccountTrading             -> 0,
+      Ride4DApps                      -> 0,
+      BlockReward                     -> 0,
+      BlockV5                         -> 0,
+      SynchronousCalls                -> 0,
+      ConsensusImprovements           -> 0,
+      BlockRewardDistribution         -> 0,
+      CappedReward                    -> 0,
+      CeaseXtnBuyback                 -> 0,
+      LightNode                       -> 0
+    )
+
+    val featsWithRideV6   = feats ++ Seq(RideV6 -> 0)
+    val setsEnabledRideV6 = TestFunctionalitySettings.withFeaturesByHeight(featsWithRideV6*)
+
+    withDomain(domainSettingsWithFS(setsEnabledRideV6), balances) { d =>
+      d.appendBlock(setScripts*)
+      d.appendBlockE(invoke1) should beRight
+    }
+  }
 }
