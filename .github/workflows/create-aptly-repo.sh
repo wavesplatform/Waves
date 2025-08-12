131f8eba534fcc1a4f0fd330e7e17cd7b3426e5f
Sergey Nazarov <phearnot@renee.ru>
2025-08-12 19:40:51 +0300

    wip

--- .github/workflows/create-aptly-repo.sh
+++ .github/workflows/create-aptly-repo.sh
@@ -1,32 +1,34 @@
 #!/usr/bin/env bash
 set -e
 
+echo "$GPG_PRIVATE_KEY" > private-key.asc
+gpg --batch --import-options import-show --import private-key.asc
+rm -f private-key.asc
+
 all_deb_packages=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" \
   https://api.github.com/repos/wavesplatform/waves/releases |\
-  jq --raw-output '.[].assets[]|select(.name | test("\\.deb$"))|select(now - (.created_at|fromdate) < 31536000)|[.name,.browser_download_url,.digest]|@csv' |\
+  jq --raw-output '.[].assets[]|select(.name | test("\\.deb$"))|select(now - (.created_at|fromdate) < 1209600)|[.name,.browser_download_url,.digest]|@csv' |\
   tr -d \")
 
 mkdir -p packages
 
 for deb_package in ${all_deb_packages} ; do
   IFS="," read -r deb_file_name deb_url digest <<< "$deb_package"
-  wget -v -O "packages/$deb_file_name" $deb_url
+  wget -nv -O "packages/$deb_file_name" $deb_url
   if [[ "$digest" != "null" ]] ; then
     echo "$(echo $digest | cut -d: -f2) packages/$deb_file_name" | sha256sum --check --status - && echo CHECKSUM OK
   fi
 done
 
-echo $GPG_PRIVATE_KEY > private-key.asc
-gpg --batch --import-options import-show --import private-key.asc
-rm -f private-key.asc
-
 aptly repo create main
 aptly repo add main packages/
-aptly publish repo -architectures=all -distribution=jammy -gpg-key=$GPG_KEY_ID -passphrase=$GPG_PASSPHRASE main
+aptly publish repo -batch -architectures=all -distribution=jammy -gpg-key=$GPG_KEY_ID -passphrase=$GPG_PASSPHRASE main
 
-gpg --batch --yes --delete-secret-and-public-key $GPG_KEY_ID
+gpg --armor --export $GPG_KEY_ID > /home/runner/.aptly/public/pubkey.txt
 
-cat >> .aptly/public/index.tml <<EOF
+rm -rf .gnupg
+
+cat > /home/runner/.aptly/public/index.html <<EOF
 <html>
 <head>
 <title>Waves Platform APT Repository</title>
