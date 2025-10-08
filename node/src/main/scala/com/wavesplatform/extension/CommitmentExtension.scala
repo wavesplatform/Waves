8f33d46b79e470361262a02bf11f8c523b6d36b2
aboklag <aboklag@uniqsoft.ae>
2025-10-08 14:11:00 +0200

    Revert "feat(CommitmentExtension): Monitor new addresses and balances at each height"
    This reverts commit c70fdf64af0de7ea41925239fccdd245b9d4b9f0.

--- node/src/main/scala/com/wavesplatform/extension/CommitmentExtension.scala
+++ node/src/main/scala/com/wavesplatform/extension/CommitmentExtension.scala
@@ -1,7 +1,6 @@
 package com.wavesplatform.extension
 
 import com.google.common.primitives.Ints
-import com.wavesplatform.account.Address
 import com.wavesplatform.crypto
 import com.wavesplatform.crypto.bls.BlsKeyPair
 import com.wavesplatform.extensions.{Context, Extension}
@@ -12,16 +11,11 @@ import com.wavesplatform.utils.ScorexLogging
 import monix.eval.Task
 import monix.execution.Scheduler
 
-import scala.collection.mutable
 import scala.concurrent.Future
 import scala.concurrent.duration._
 
 class CommitmentExtension(context: Context) extends Extension with ScorexLogging {
   private implicit val scheduler: Scheduler = Scheduler.singleThread("commitment-extension")
-
-  // NOTE: This in-memory cache does not survive a restart.
-  // On restart, commitments might be sent again for the current period.
-  private val committedInPeriod = mutable.Map[Int, Set[Address]]()
 
   override def start(): Unit = {
     log.info("Starting CommitmentExtension")
@@ -30,98 +24,82 @@ class CommitmentExtension(context: Context) extends Extension with ScorexLogging
       val generationPeriodLength = settings.blockchainSettings.functionalitySettings.generationPeriodLength
       val wallet                 = context.wallet
 
-      def createTask(generationPeriodStart: Height, committedAccounts: Set[Address]): Task[Set[Address]] = Task {
-        var newlyCommitted = Set.empty[Address]
-        wallet.privateKeyAccounts.foreach { account =>
-          if (!committedAccounts.contains(account.toAddress)) {
-            val fee          = 10000000L
-            val wavesBalance = context.blockchain.balance(account.toAddress)
-
-            if (wavesBalance < fee) {
-              log.error(s"Insufficient balance for fee for account ${account.toAddress}. Required: $fee, available: $wavesBalance")
-            } else {
-              if (wavesBalance < 100 * 100000000L) {
-                log.warn(s"Balance is low for account ${account.toAddress}: $wavesBalance. It's less than 100 WAVES.")
-              }
-
-              val timestamp = context.time.getTimestamp()
-
-              val blsKP      = BlsKeyPair(account.privateKey)
-              val blsMessage = blsKP.publicKey.arr ++ Ints.toByteArray(generationPeriodStart)
-              val blsSig     = blsKP.sign(blsMessage)
-
-              val commitToGenTxE = CommitToGenerationTransaction
-                .create(
-                  sender = account.publicKey,
-                  endorserPublicKey = blsKP.publicKey,
-                  generationPeriodStart = generationPeriodStart,
-                  timestamp = timestamp,
-                  feeInWaves = fee,
-                  commitmentSignature = blsSig,
-                  proofs = Proofs.empty,
-                  chainId = settings.blockchainSettings.addressSchemeCharacter.toByte
-                )
-                .map(tx => tx.copy(proofs = Proofs(crypto.sign(account.privateKey, tx.bodyBytes()))))
-
-              commitToGenTxE.fold(
-                {
-                  case TxValidationError.InsufficientFee =>
-                    log.error(
-                      s"Failed to create transaction for account ${account.toAddress} due to insufficient fee: ${TxValidationError.InsufficientFee}"
-                    )
-                  case e => log.error(s"Failed to create CommitToGenerationTransaction for account ${account.toAddress}: $e")
-                },
-                tx => {
-                  log.info(s"Created CommitToGenerationTransaction with id: ${tx.id()} for account ${account.toAddress}")
-                  log.info(s"BLS Public Key: ${blsKP.publicKey.base58}")
-                  log.info(s"Commitment Signature: ${blsSig.base58}")
-                  context.broadcastTransaction(tx)
-                  newlyCommitted += account.toAddress
-                }
-              )
-            }
-          }
-        }
-        newlyCommitted
-      }
-
-      def waitForNewHeight(currentHeight: Int): Task[Int] = {
-        def check(): Task[Int] = {
-          val newHeight = context.blockchain.height
-          if (newHeight > currentHeight) {
-            Task.now(newHeight)
+      def waitForHeight(height: Int): Task[Unit] = {
+        def check(): Task[Unit] = {
+          if (context.blockchain.height >= height) {
+            Task.unit
           } else {
-            Task.sleep(7.seconds) >> check()
+            Task.sleep(1.second) >> check()
           }
         }
         check()
       }
 
-      def heightLoop(h: Int): Task[Unit] = {
-        waitForNewHeight(h).flatMap { newHeight =>
-          val n = Math.floor((newHeight - 1).toDouble / generationPeriodLength).toLong
-          val periodToCommit = (n + 1) * generationPeriodLength
+      def loop(n: Long): Task[Unit] = {
+        val txSendHeight   = n * generationPeriodLength + 1
+        val periodToCommit = (n + 1) * generationPeriodLength
 
-          committedInPeriod.keys.foreach { period =>
-            if (period < periodToCommit) {
-              committedInPeriod.remove(period)
+        log.info(s"Waiting for height $txSendHeight to create commitment for period starting at $periodToCommit")
+
+        for {
+          _ <- waitForHeight(txSendHeight.toInt)
+          _ <- createTask(Height(periodToCommit.toInt))
+          _ <- loop(n + 1)
+        } yield ()
+      }
+
+      def createTask(generationPeriodStart: Height): Task[Unit] = Task {
+        wallet.privateKeyAccounts.foreach { account =>
+          val fee          = 10000000L
+          val wavesBalance = context.blockchain.balance(account.toAddress)
+
+          if (wavesBalance < fee) {
+            log.error(s"Insufficient balance for fee for account ${account.toAddress}. Required: $fee, available: $wavesBalance")
+          } else {
+            if (wavesBalance < 100 * 100000000L) {
+              log.warn(s"Balance is low for account ${account.toAddress}: $wavesBalance. It's less than 100 WAVES.")
             }
-          }
 
-          val committedAccounts = committedInPeriod.getOrElse(periodToCommit.toInt, Set.empty)
-          log.debug(s"Checking for commitments at height $newHeight for period $periodToCommit. Known commitments: ${committedAccounts.size}")
+            val timestamp = context.time.getTimestamp()
 
-          createTask(Height(periodToCommit.toInt), committedAccounts).flatMap { newlyCommitted =>
-            if (newlyCommitted.nonEmpty) {
-              log.info(s"Committed for ${newlyCommitted.size} new accounts for period $periodToCommit")
-              committedInPeriod(periodToCommit.toInt) = committedAccounts ++ newlyCommitted
-            }
-            heightLoop(newHeight)
+            val blsKP      = BlsKeyPair(account.privateKey)
+            val blsMessage = blsKP.publicKey.arr ++ Ints.toByteArray(generationPeriodStart)
+            val blsSig     = blsKP.sign(blsMessage)
+
+            val commitToGenTxE = CommitToGenerationTransaction
+              .create(
+                sender = account.publicKey,
+                endorserPublicKey = blsKP.publicKey,
+                generationPeriodStart = generationPeriodStart,
+                timestamp = timestamp,
+                feeInWaves = fee,
+                commitmentSignature = blsSig,
+                proofs = Proofs.empty,
+                chainId = settings.blockchainSettings.addressSchemeCharacter.toByte
+              )
+              .map(tx => tx.copy(proofs = Proofs(crypto.sign(account.privateKey, tx.bodyBytes()))))
+
+            commitToGenTxE.fold(
+              {
+                case TxValidationError.InsufficientFee =>
+                  log.error(
+                    s"Failed to create transaction for account ${account.toAddress} due to insufficient fee: ${TxValidationError.InsufficientFee}"
+                  )
+                case e => log.error(s"Failed to create CommitToGenerationTransaction for account ${account.toAddress}: $e")
+              },
+              tx => {
+                log.info(s"Created CommitToGenerationTransaction with id: ${tx.id()} for account ${account.toAddress}")
+                log.info(s"BLS Public Key: ${blsKP.publicKey.base58}")
+                log.info(s"Commitment Signature: ${blsSig.base58}")
+                context.broadcastTransaction(tx)
+              }
+            )
           }
         }
       }
 
-      heightLoop(context.blockchain.height - 1).runAsyncLogErr
+      val initialN = Math.floor((context.blockchain.height - 1).toDouble / generationPeriodLength).toLong
+      loop(initialN).runAsyncLogErr
     } else {
       log.warn("DeterministicFinality feature is not activated. CommitmentExtension will not start.")
     }
