6d861955a6ad819d2aee4fd240e0c445fb1d736d
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2025-10-15 14:13:46 +0400

    BlsUtilsTest: new tests

--- node/tests/src/test/scala/com/wavesplatform/crypto/bls/BlsUtilsTest.scala
+++ node/tests/src/test/scala/com/wavesplatform/crypto/bls/BlsUtilsTest.scala
@@ -9,38 +9,36 @@ import supranational.blst.SecretKey
 import scala.util.Random
 
 class BlsUtilsTest extends FreeSpec with EitherValues {
+  private val privateKey1 = mkRandomSecretKey()
+  private val publicKey1  = mkBlsPublicKey(privateKey1)
+
+  private val privateKey2 = mkRandomSecretKey()
+  private val publicKey2  = mkBlsPublicKey(privateKey2)
+
+  private val privateKey3 = mkRandomSecretKey()
+  private val publicKey3  = mkBlsPublicKey(privateKey3)
+
+  private val message = "assertion".getBytes()
+
+  private val sig1 = signBasic(privateKey1, message)
+  private val sig2 = signBasic(privateKey2, message)
+  private val sig3 = signBasic(privateKey3, message)
+
   "aggregation in verifyAgg" - {
+    "aggregation of two same signatures" in {
+      val aggSig = BlsUtils.aggSign(BlsUtils.aggSign(sig1, sig2), sig1)
+
+      BlsUtils.verifyAgg(aggSig, message, Seq(publicKey1, publicKey2, publicKey1)).value shouldBe true
+      BlsUtils.verifyAgg(aggSig, message, Seq(publicKey1, publicKey2)).value shouldBe false
+    }
+
     "different order of signatures and keys" in {
-      val privateKey1 = mkRandomSecretKey()
-      val publicKey1  = mkBlsPublicKey(privateKey1)
-
-      val privateKey2 = mkRandomSecretKey()
-      val publicKey2  = mkBlsPublicKey(privateKey2)
-
-      val message = "assertion".getBytes()
-      val sig1    = signBasic(privateKey1, message)
-      val sig2    = signBasic(privateKey2, message)
-
       val aggSig = BlsUtils.aggSign(sig1, sig2)
 
       BlsUtils.verifyAgg(aggSig, message, Seq(publicKey2, publicKey1)).value shouldBe true
     }
 
     "associativity" in {
-      val privateKey1 = mkRandomSecretKey()
-      val publicKey1  = mkBlsPublicKey(privateKey1)
-
-      val privateKey2 = mkRandomSecretKey()
-      val publicKey2  = mkBlsPublicKey(privateKey2)
-
-      val privateKey3 = mkRandomSecretKey()
-      val publicKey3  = mkBlsPublicKey(privateKey3)
-
-      val message = "assertion".getBytes()
-      val sig1    = signBasic(privateKey1, message)
-      val sig2    = signBasic(privateKey2, message)
-      val sig3    = signBasic(privateKey3, message)
-
       val aggSig = Seq(sig1, sig2, sig3).reduceLeft(BlsUtils.aggSign)
 
       BlsUtils.verifyAgg(aggSig, message, Seq(publicKey2, publicKey1, publicKey3)).value shouldBe true
