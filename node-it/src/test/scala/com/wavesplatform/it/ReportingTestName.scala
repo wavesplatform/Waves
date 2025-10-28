be691739501ccd77e1e1fbad43a53d207748b3d3
Sergey Nazarov <phearnot@renee.ru>
2025-10-28 11:56:55 +0300

    Fixed tests

--- node-it/src/test/scala/com/wavesplatform/it/ReportingTestName.scala
+++ node-it/src/test/scala/com/wavesplatform/it/ReportingTestName.scala
@@ -1,9 +1,11 @@
 package com.wavesplatform.it
 
 import com.wavesplatform.api.http.DebugMessage
+import com.wavesplatform.it.ReportingTestName.CaptureCancel
 import com.wavesplatform.it.api.AsyncHttpApi.*
 import com.wavesplatform.utils.ScorexLogging
-import org.scalatest.{Args, Status, Suite, SuiteMixin}
+import org.scalatest.events.*
+import org.scalatest.*
 
 import scala.concurrent.duration.*
 import scala.concurrent.{Await, Future}
@@ -13,8 +15,9 @@ trait ReportingTestName extends SuiteMixin with ScorexLogging {
 
   abstract override protected def runTest(testName: String, args: Args): Status = {
     print(s"Test '$testName' started")
-    val r = super.runTest(testName, args)
-    print(s"Test '$testName' ${if (r.succeeds()) "SUCCEEDED" else "FAILED"}")
+    val reporter = new CaptureCancel(testName, args.reporter)
+    val r        = super.runTest(testName, args.copy(reporter = reporter))
+    print(s"Test '$testName' ${reporter.status.toString.toUpperCase}")
     r
   }
 
@@ -26,6 +29,25 @@ trait ReportingTestName extends SuiteMixin with ScorexLogging {
       Await.result(Future.traverse(nodes)(_.printDebugMessage(DebugMessage(formatted))), 10.seconds)
     } catch {
       case _: Throwable => ()
+    }
+  }
+}
+
+object ReportingTestName {
+  enum Status {
+    case Unknown, Succeeded, Failed, Ignored, Canceled
+  }
+  class CaptureCancel(testName: String, inner: Reporter) extends Reporter {
+    @volatile var status: Status = Status.Unknown
+    override def apply(event: Event): Unit = {
+      event match {
+        case TestSucceeded(testName = `testName`) => status = Status.Succeeded
+        case TestFailed(testName = `testName`)    => status = Status.Failed
+        case TestIgnored(testName = `testName`)   => status = Status.Ignored
+        case TestCanceled(testName = `testName`)  => status = Status.Canceled
+        case _                                    =>
+      }
+      inner.apply(event)
     }
   }
 }
