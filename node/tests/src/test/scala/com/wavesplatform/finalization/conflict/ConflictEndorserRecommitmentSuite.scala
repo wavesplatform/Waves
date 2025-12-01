09e395097dc8265e67a4c4f595ab97b8734e2ee3
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2025-12-01 10:06:57 +0400

    MultipleConflictEndorserSuite

--- node/tests/src/test/scala/com/wavesplatform/finalization/conflict/ConflictEndorserRecommitmentSuite.scala
+++ node/tests/src/test/scala/com/wavesplatform/finalization/conflict/ConflictEndorserRecommitmentSuite.scala
@@ -1,15 +1,13 @@
 package com.wavesplatform.finalization.conflict
 
 import com.wavesplatform.TestValues
-import com.wavesplatform.account.Address
 import com.wavesplatform.block.{Block, BlockEndorsement, FinalizationVoting}
 import com.wavesplatform.crypto.bls.BlsKeyPair
 import com.wavesplatform.db.WithState.AddrWithBalance
 import com.wavesplatform.features.BlockchainFeatures
 import com.wavesplatform.finalization.BaseFinalizationSpec
-import com.wavesplatform.history.Domain
 import com.wavesplatform.state.diffs.ENOUGH_AMT
-import com.wavesplatform.state.{Blockchain, ConflictGenerators, GeneratorIndex, GenesisBlockHeight, Height, Portfolio}
+import com.wavesplatform.state.{Blockchain, GeneratorIndex, GenesisBlockHeight, Height, Portfolio}
 import com.wavesplatform.test.DomainPresets.WavesSettingsOps
 import com.wavesplatform.test.FreeSpec
 import com.wavesplatform.transaction.CommitToGenerationTransaction.DepositInWavelets
@@ -77,11 +75,23 @@ class ConflictEndorserRecommitmentSuite extends BaseFinalizationSpec {
 
     val balanceAfter5 = balanceAfter4 - DepositInWavelets
 
-    d.checkCommitted(endorserAddrs*)
+    withClue(s"checkCommitted: ") {
+      d.blockchain.committedGenerators(d.blockchain.currentGenerationPeriod.value).map(_._1) should contain theSameElementsInOrderAs endorserAddrs
+    }
+
     d.blockchain.wavesPortfolio(conflictGeneratorAddr) shouldBe Portfolio(balance = balanceAfter5, generationDeposit = DepositInWavelets)
     d.blockchain.balanceAtHeight(conflictGeneratorAddr, d.blockchain.height).value shouldBe (5, balanceAfter5)
-    d.checkGeneratingBalance(conflictGeneratorAddr, balanceAfter5 - DepositInWavelets)
-    d.checkGeneratorBalanceFromApi(conflictGeneratorAddr, balanceAfter5 - DepositInWavelets)
+
+    withClue(s"checkGeneratingBalance: ") {
+      d.blockchain.generatingBalance(conflictGeneratorAddr) shouldBe balanceAfter5 - DepositInWavelets
+    }
+
+    withClue(s"checkGeneratorBalanceFromApi: ") {
+      d.generatorsApi
+        .generators(Height(d.blockchain.height))
+        .collectFirst { case x if x.address == conflictGeneratorAddr => x.balance }
+        .getOrElse(0L) shouldBe balanceAfter5 - DepositInWavelets
+    }
 
     d.blockchain.balanceSnapshots(conflictGeneratorAddr, from = 2, to = None) should contain theSameElementsInOrderAs Seq(
       bs(height = 5, regularBalance = balanceAfter5, deposits = 1), // Punished and committed on next
@@ -89,36 +99,5 @@ class ConflictEndorserRecommitmentSuite extends BaseFinalizationSpec {
       // height = 3 // Sent conflict endorsement
       bs(height = 2, regularBalance = balanceAfter2, deposits = 1) // Sent CommitToGeneration
     )
-  }
-
-  extension (d: Domain)(using Position) {
-    def checkCommitted(addresses: Address*): Assertion = withClue(s"checkCommitted: addresses=$addresses, ") {
-      d.blockchain.committedGenerators(d.blockchain.currentGenerationPeriod.value).map(_._1) should contain theSameElementsInOrderAs addresses
-    }
-
-    def checkHasConflict(h: Int, idx: Int): Assertion = withClue(s"checkHasConflict: h=$h, idx=$idx, ") {
-      d.blockchain.conflictGenerators(d.blockchain.generationPeriodOf(Height(h)).value) shouldBe mkConflictGenerators(h, idx)
-    }
-
-    def checkHasNoConflict(h: Int): Assertion = withClue(s"checkHasNoConflict: h=$h, ") {
-      d.blockchain.conflictGenerators(d.blockchain.generationPeriodOf(Height(h)).value) shouldBe ConflictGenerators.empty
-    }
-
-    def checkWavesAmount(x: BigInt): Assertion = withClue("wavesAmount: ") {
-      d.blockchain.wavesAmount(d.blockchain.height) shouldBe x
-    }
-
-    def checkGeneratingBalance(address: Address, balance: Long = 0L): Assertion = withClue(s"checkGeneratingBalance: address=$address, ") {
-      d.blockchain.generatingBalance(address) shouldBe balance
-      // GeneratingBalanceProvider.balance(d.blockchain, address) shouldBe balance
-    }
-
-    def checkGeneratorBalanceFromApi(address: Address, balance: Long = 0L, h: Int = d.blockchain.height): Assertion =
-      withClue(s"checkGeneratorBalanceFromApi: h=$h, address=$address, ") {
-        d.generatorsApi
-          .generators(Height(h))
-          .collectFirst { case x if x.address == address => x.balance }
-          .getOrElse(0L) shouldBe balance
-      }
   }
 }
