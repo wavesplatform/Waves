524f43b087155f35ca1ad056fa26c80cbdf2cf49
Vladimir Logachev <vladimir@logachev.dev>
2025-12-04 10:40:16 +0400

    Update node/src/main/scala/com/wavesplatform/state/StateHashBuilder.scala
    Co-authored-by: Vyatcheslav Suharnikov <arz.freezy@gmail.com>

--- node/src/main/scala/com/wavesplatform/state/StateHashBuilder.scala
+++ node/src/main/scala/com/wavesplatform/state/StateHashBuilder.scala
@@ -40,7 +40,7 @@ class StateHashBuilder {
   private val maps = Vector.fill(SectionId.maxId)(mutable.TreeMap.empty[ByteStr, Array[Byte]])
 
   private def addEntry(section: SectionId.Value, key: Array[Byte]*)(value: Array[Byte]*): Unit = {
-    val solidKey   = ByteStr(if (key.isEmpty) Array.emptyByteArray else key.reduce(_ ++ _))
+    val solidKey   = ByteStr(key.foldLeft(Array.emptyByteArray)(_ ++ _))
     val solidValue = value.foldLeft(Array.emptyByteArray)(_ ++ _)
     maps(section.id)(solidKey) = solidValue
   }
