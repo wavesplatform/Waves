a6e39e181c77b93c908175e660b6f78c706434f9
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2025-12-19 10:17:53 +0400

    Noting to validate in CommitToGenerationTxValidator

--- node/src/main/scala/com/wavesplatform/transaction/validation/impl/CommitToGenerationTxValidator.scala
+++ node/src/main/scala/com/wavesplatform/transaction/validation/impl/CommitToGenerationTxValidator.scala
@@ -1,12 +1,10 @@
 package com.wavesplatform.transaction.validation.impl
 
+import cats.data.Validated.Valid
 import com.wavesplatform.transaction.CommitToGenerationTransaction
 import com.wavesplatform.transaction.validation.*
 
 object CommitToGenerationTxValidator extends TxValidator[CommitToGenerationTransaction] {
-  override def validate(tx: CommitToGenerationTransaction): ValidatedV[CommitToGenerationTransaction] = {
-    V.seq(tx)(
-      V.cond(true, ???) // TODO: Check PK
-    )
-  }
+  override def validate(tx: CommitToGenerationTransaction): ValidatedV[CommitToGenerationTransaction] =
+    Valid(tx) // Nothing to validate
 }
