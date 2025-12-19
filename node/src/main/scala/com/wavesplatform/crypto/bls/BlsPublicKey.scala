77d382ac67f3cdb84323f7ef0b026903a992d69b
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2025-12-19 13:08:09 +0400

    BlsPublicKey is opaque type

--- node/src/main/scala/com/wavesplatform/crypto/bls/BlsPublicKey.scala
+++ node/src/main/scala/com/wavesplatform/crypto/bls/BlsPublicKey.scala
@@ -3,26 +3,26 @@ package com.wavesplatform.crypto.bls
 import com.wavesplatform.common.state.ByteStr
 import com.wavesplatform.transaction.TxValidationError.GenericError
 
-case class BlsPublicKey private (byteStr: ByteStr) extends AnyVal {
-  def arr: Array[Byte] = byteStr.arr
-
-  def verify(message: Array[Byte], signature: BlsSignature): Boolean =
-    BlsUtils.verifyBasic(signature.arr, message, arr)
-
-  def base58: String            = byteStr.toString
-  override def toString: String = byteStr.toString
-}
+opaque type BlsPublicKey = ByteStr
 
 object BlsPublicKey {
   val SizeInBytes = 48
 
-  private[bls] def unsafe(byteStr: ByteStr): BlsPublicKey = new BlsPublicKey(byteStr)
+  extension (self: BlsPublicKey) {
+    def byteStr: ByteStr = self
+    def arr: Array[Byte] = byteStr.arr
+
+    def verify(message: Array[Byte], signature: BlsSignature): Boolean = BlsUtils.verifyBasic(signature.arr, message, arr)
+
+    def base58: String = byteStr.toString
+  }
+
+  private[bls] def unsafe(byteStr: ByteStr): BlsPublicKey = byteStr
 
   def apply(arr: Array[Byte]): Either[GenericError, BlsPublicKey] = apply(ByteStr(arr))
-  def apply(byteStr: ByteStr): Either[GenericError, BlsPublicKey] =
-    Either.cond(
-      byteStr.arr.length == SizeInBytes,
-      new BlsPublicKey(byteStr),
-      GenericError(s"Unexpected BLS public key length: ${byteStr.arr.length}, expected: $SizeInBytes")
-    )
+  def apply(byteStr: ByteStr): Either[GenericError, BlsPublicKey] = Either.cond(
+    byteStr.arr.length == SizeInBytes,
+    byteStr,
+    GenericError(s"Unexpected BLS public key length: ${byteStr.arr.length}, expected: $SizeInBytes")
+  )
 }
