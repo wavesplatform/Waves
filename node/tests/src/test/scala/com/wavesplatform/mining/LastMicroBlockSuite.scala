bc0b7538a129db2a5328a78bb56a9cfb634a4ba3
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2026-03-04 15:16:20 +0400

    Additional test in LastMicroBlockSuite

--- node/tests/src/test/scala/com/wavesplatform/mining/LastMicroBlockSuite.scala
+++ node/tests/src/test/scala/com/wavesplatform/mining/LastMicroBlockSuite.scala
@@ -9,19 +9,21 @@ import com.wavesplatform.history.Domain
 import com.wavesplatform.settings.*
 import com.wavesplatform.state
 import com.wavesplatform.state.*
-import com.wavesplatform.test.{FreeSpec, TestSchedulerOps, TestTime}
+import com.wavesplatform.test.DomainPresets.WavesSettingsOps
+import com.wavesplatform.test.{CatchLogs, FreeSpec, NumericExt, TestSchedulerOps, TestTime, WithResourceManager}
 import com.wavesplatform.transaction.TxHelpers
 import com.wavesplatform.wallet.Wallet
 import io.netty.channel.group.DefaultChannelGroup
 import io.netty.util.concurrent.GlobalEventExecutor
 import monix.execution.schedulers.TestScheduler
 import monix.reactive.Observable
+import monix.reactive.subjects.ConcurrentSubject
 import org.scalatest.EitherValues
 import org.scalatest.time.SpanSugar.convertLongToGrainOfTime
 
 import scala.util.Using
 
-class LastMicroBlockSuite extends FreeSpec with WithDomain with TestSchedulerOps with EitherValues {
+class LastMicroBlockSuite extends FreeSpec with WithDomain with TestSchedulerOps with WithResourceManager with EitherValues {
   private val thisNodeAcc1 = Wallet.generateNewAccount(Domain.DefaultWalletSeed, nonce = 0)
   private val thisNodeAcc2 = Wallet.generateNewAccount(Domain.DefaultWalletSeed, nonce = 1)
   private val otherNodeAcc = TxHelpers.defaultSigner
@@ -150,4 +152,78 @@ class LastMicroBlockSuite extends FreeSpec with WithDomain with TestSchedulerOps
       lastBlock.header.reference shouldBe refLiquidBlockId
     }
   }.get
+
+  "transfer in the last microblock of period, but it removed" in withManager { manager =>
+    val channels     = manager(new DefaultChannelGroup(GlobalEventExecutor.INSTANCE))
+    val thisNodeAcc2 = Wallet.generateNewAccount(Domain.DefaultWalletSeed, nonce = 1)
+    val thisNodeAcc3 = Wallet.generateNewAccount(Domain.DefaultWalletSeed, nonce = 2)
+    var miner        = Miner.StrictDisabledMiner
+
+    withDomain(
+      defaultSettings
+        .configure(_.copy(generationPeriodLength = 3))
+        .copy(minerSettings = defaultSettings.minerSettings.copy(maxTransactionsInMicroBlock = 2)), // Fixes 5 seconds delay
+      AddrWithBalance.enoughBalances(otherNodeAcc, thisNodeAcc1, thisNodeAcc2) :+ AddrWithBalance(thisNodeAcc3.toAddress, 1000.waves - 1),
+      miner = Miner.forwardTo(miner)
+    ) { d =>
+      val minerScheduler    = TestScheduler()
+      val appenderScheduler = TestScheduler()
+      d.wallet.generateNewAccounts(3)
+
+      val utxEvents = ConcurrentSubject.publish[Unit](using minerScheduler)
+      val minerImpl = new MinerImpl(
+        channels,
+        d.blockchain,
+        d.settings,
+        d.testTime,
+        d.utxPool,
+        BlockEndorser.Disabled,
+        EndorsementStorage.Disabled,
+        d.wallet,
+        d.posSelector,
+        minerScheduler,
+        appenderScheduler,
+        utxEvents
+      ) with CatchLogs
+      miner = minerImpl
+
+      d.appender.appendBlock(d.createBlock(version = Block.ProtoBlockVersion, txs = Nil, strictTime = true, generator = otherNodeAcc))
+
+      log.debug("Trigger forging of block 3")
+      val blockTs3 = Seq(thisNodeAcc1, thisNodeAcc2).map(d.nextBlockTime).min
+      d.testTime.setTimeIfGreater(blockTs3)
+
+      val block4Ts = Seq(thisNodeAcc1, thisNodeAcc2).map(d.nextBlockTime).min
+      appenderScheduler.tickNext("appender-1")
+      minerScheduler.tickNext("miner-1")
+      appenderScheduler.tickNext("appender-2")
+
+      log.debug("Forge first microblock")
+      d.utxPool.putIfNew(TxHelpers.transfer(thisNodeAcc3))
+      utxEvents.onNext(())
+      minerScheduler.tickNext("miner-2")
+      appenderScheduler.tickNext("appender-3")
+
+      log.debug("Forge last microblock")
+      d.testTime.setTimeIfGreater(block4Ts - 1) // To exclude the latest microblock, see min-micro-block-age
+      Seq(thisNodeAcc1, thisNodeAcc2).map { kp =>
+        d.utxPool.putIfNew(TxHelpers.transfer(kp, amount = d.blockchain.balance(kp.toAddress) - 1.waves))
+      }
+      utxEvents.onNext(())
+      minerScheduler.tickNext("miner-3")
+      appenderScheduler.tickNext("appender-4")
+
+      log.debug("Trigger thisNode forging")
+      d.testTime.setTimeIfGreater(block4Ts + 1)
+      (4 to 5).foreach { i =>
+        minerScheduler.tickNext(s"miner-$i")
+        appenderScheduler.tickNext(s"appender-${i + 1}")
+      }
+
+      val messages = minerImpl.inMemoryLog.getMessages
+      Seq(thisNodeAcc1, thisNodeAcc2, thisNodeAcc3).foreach { kp =>
+        messages.find(_.contains(s"${kp.toAddress} is lower than required for generation")) shouldBe defined
+      }
+    }
+  }
 }
